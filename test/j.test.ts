e227607aada34eff9f1eda8d4ad56d52c983aa95
devJang <devjang2016@gmail.com>
2021-05-04 13:04:27 +0900

    feat: init project

---
+++ test/j.test.ts
@@ -0,0 +1,5 @@
+describe("jest test", () => {
+  it("should be true", () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ test/r.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import App from "../src/App";
+
+describe("RTL Test", () => {
+  it("should render", () => {
+    render(<App />);
+    expect(screen.getByText("react-shopping-cart")).toBeInTheDocument();
+  });
+});
