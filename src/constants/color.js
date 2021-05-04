70eb4c62b25420276b3e15d9eaa2730ddc6f036a
sunhpark42 <sunhpark42@gmail.com>
2021-05-04 23:12:23 +0900

    feat: 색상 코드 상수화

---
+++ src/constants/color.js
@@ -0,0 +1,12 @@
+const COLOR = {
+  MINT: '#2AC1BC',
+  HIGHLIGHT_MINT: '#94E0DD',
+  BROWN: '#73675C',
+  WHITE: '#FFFFFF',
+  'GRAY-100': '#E5E5E5',
+  'GRAY-200': '#DDDDDD',
+  'GRAY-300': '#AAAAAA',
+  BLACK: '#333333',
+};
+
+export default COLOR;
