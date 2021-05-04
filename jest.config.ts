5851e2c3052ec3e4ba0384e6c985072828ebc899
devJang <devjang2016@gmail.com>
2021-05-04 13:04:27 +0900

    feat: init project

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "^.+\\.svg$": "jest-svg-transformer",
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+  },
+};
