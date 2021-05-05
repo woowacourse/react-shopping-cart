adbd32299285f6bce1bef799db6ac673f008c975
Shim MunSeong <Puterism.K@gmail.com>
2021-05-05 16:09:24 +0900

    chore: Storybook 환경 세팅 및 설정
    Co-authored-by: JO YUN HO <bedro27@gmail.com>
    Co-authored-by: jieun song <zigsong@users.noreply.github.com>

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,12 @@
+import { configure, addDecorator } from '@storybook/react';
+import { MemoryRouter } from 'react-router-dom';
+
+import * as React from 'react';
+import { ThemeProvider } from 'emotion-theming';
+
+import { globalStyle, theme } from '../src/App.styles';
+import { Global } from '@emotion/react';
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
@@ -7,3 +16,16 @@ export const parameters = {
     },
   },
 };
+
+addDecorator((story) => <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>);
+
+addDecorator((style) => <ThemeProvider theme={theme}>{style()}</ThemeProvider>);
+
+addDecorator((style) => (
+  <>
+    <Global styles={globalStyle} />
+    {style()}
+  </>
+));
+
+configure(require.context('../src', true, /\.stories\.js?$/), module);
