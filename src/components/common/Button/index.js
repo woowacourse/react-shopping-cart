866c880249aac9ed84cf822677b4432c11f30328
Sunny.K <sunnyk5780@gmail.com>
2021-05-05 19:39:51 +0900

    feat: Button 컴포넌트 구현

---
+++ src/components/common/Button/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import * as Styled from './style';
+
+const Button = ({ type, width, height, fontSize, color, backgroundColor, borderColor, children }) => {
+  return (
+    <Styled.Button
+      type={type}
+      width={width}
+      height={height}
+      color={color}
+      fontSize={fontSize}
+      backgroundColor={backgroundColor}
+      borderColor={borderColor}
+    >
+      {children}
+    </Styled.Button>
+  );
+};
+
+Button.propTypes = {
+  type: PropTypes.string,
+  width: PropTypes.string.isRequired,
+  height: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  fontSize: PropTypes.string,
+  backgroundColor: PropTypes.string,
+  borderColor: PropTypes.string,
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.element, PropTypes.elementType]),
+};
+
+Button.defaultProps = {
+  children: '',
+};
+
+export default Button;
---
+++ src/components/common/Button/style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import PALETTE from '../../../constants/palette';
+
+export const Button = styled.button`
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
+  ${({ color }) => color && `color: ${color};`}
+  ${({ fontSize }) => fontSize && `font-size: ${fontSize};`}
+  ${({ backgroundColor }) => backgroundColor && `background-color: ${backgroundColor};`}
+  border: ${({ borderColor }) => (borderColor ? `1px solid ${borderColor}` : 'none')};
+  cursor: pointer;
+`;
