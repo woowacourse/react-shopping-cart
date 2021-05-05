ec1103ecaf8c73fd03134390d94119e2211b43a8
Shim MunSeong <Puterism.K@gmail.com>
2021-05-05 10:50:15 +0900

    init: 프로젝트 세팅
    Co-authored-by: JO YUN HO <bedro27@gmail.com>
    Co-authored-by: jieun song <zigsong@users.noreply.github.com>

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', '@typescript-eslint', 'jest'],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  overrides: [
+    {
+      files: ['**/*.stories.*'],
+      rules: {
+        'import/no-anonymous-default-export': 'off',
+      },
+    },
+  ],
+};
