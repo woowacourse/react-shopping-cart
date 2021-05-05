8977c0fbd32669e10ba1095180592996b561cbfb
365kim <365listener@gmail.com>
2021-05-05 20:39:48 +0900

    refactor: Page 컴포넌트를 atoms 디렉토리 하위로 이동

--- src/components/commons/index.js
+++ src/components/commons/index.js
@@ -1,9 +1,9 @@
 /* atoms */
 export { CartIcon } from './atoms/CartIcon';
 export { Line } from './atoms/Line';
+export { Page } from './atoms/Page';
 
 /* molecules */
 export { Header } from './molecules/Header';
 export { NavBar } from './molecules/NavBar';
-export { Page } from './molecules/Page';
 export { ProductItem } from './molecules/ProductItem';
