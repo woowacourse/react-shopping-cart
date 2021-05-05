2390244c17ea9090752910f6a53fb8e77d5fd0d8
sunhpark42 <sunhpark42@gmail.com>
2021-05-05 15:28:59 +0900

    feat: MainContainer 컴포넌트 구현

---
+++ src/components/shared/MainContainer/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Container } from './style';
+
+const MainContainer = ({ children }) => {
+  return <Container>{children}</Container>;
+};
+
+export default MainContainer;
