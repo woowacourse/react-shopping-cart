5bc8a93c0da98da80ca4e72ec348b69b4da6476e
HyuuunjuKim <43339385+HyuuunjuKim@users.noreply.github.com>
2021-05-05 14:28:57 +0900

    feat: Header 컴포넌트 생성 및 스토리북 작성

---
+++ src/components/Header/Header.styles.js
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+const Container = styled.h2`
+  width: 100%;
+  border-bottom: 4px solid #333333;
+  color: #333333;
+  font-weight: 700;
+  font-size: 32px;
+  text-align: center;
+  padding-bottom: 25px;
+  letter-spacing: 0.5px;
+`;
+
+export { Container };
