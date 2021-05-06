a4e6a4fb61c1b758717a12816e953b2f2a5f560f
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-05-06 14:49:20 +0900

    feat: Checkbox 컴포넌트 제작
    Co-authored-by: JUNMO HAN <wnsah052@naver.com>

---
+++ src/components/checkbox/Checkbox.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledCheckboxContainer = styled.div`
+  display: inline-block;
+  vertical-align: middle;
+`;
+
+const Icon = styled.svg`
+  fill: none;
+  stroke: white;
+  stroke-width: 2.5px;
+`;
+
+const HiddenCheckbox = styled.input.attrs({ type: 'checkbox' })`
+  border: 0;
+  clip: rect(0 0 0 0);
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  white-space: nowrap;
+  width: 1px;
+`;
+
+const StyledCheckbox = styled.div`
+  display: inline-block;
+  width: 28px;
+  height: 28px;
+  background-color: ${(props) => (props.isChecked ? '#22A6A2' : 'white')};
+  border-radius: 3px;
+  border: 1px solid #22a6a2;
+
+  ${Icon} {
+    visibility: ${(props) => (props.isChecked ? 'visible' : 'hidden')};
+  }
+`;
+
+const StyledLabelText = styled.span`
+  margin-left: 12px;
+`;
+
+const Checkbox = ({ isChecked, children, onChange }) => (
+  <label>
+    <StyledCheckboxContainer>
+      <HiddenCheckbox checked={isChecked} onChange={onChange} />
+      <StyledCheckbox isChecked={isChecked}>
+        <Icon viewBox="0 0 24 24">
+          <polyline points="21 7 10 18 4 12" />
+        </Icon>
+      </StyledCheckbox>
+    </StyledCheckboxContainer>
+    {children ? <StyledLabelText>{children}</StyledLabelText> : ''}
+  </label>
+);
+
+export default Checkbox;
--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Checkbox from '../components/checkbox/Checkbox';
 
-const ShoppingCart = (props) => <div>ShoppingCart</div>;
+const ShoppingCart = () => {
+  const [isChecked, setCheck] = useState(false);
+
+  const handleCheckbox = ({ target }) => {
+    setCheck(target.checked);
+  };
+
+  return (
+    <Checkbox onChange={handleCheckbox} isChecked={isChecked}>
+      선택해제
+    </Checkbox>
+  );
+};
 
 export default ShoppingCart;
