c5101864c24b03cbd7b017e2247f144bfd2b01b2
Kyle <mkitigy@gmail.com>
2021-05-06 20:32:06 +0900

    feat: CheckBox 추가

---
+++ src/components/common/CheckBox/index.styles.js
@@ -0,0 +1,23 @@
+import styled, { css } from 'styled-components';
+
+export const HiddenCheckBox = styled.input.attrs(() => ({
+  type: 'checkbox',
+}))`
+  width: 1px;
+  opacity: 0;
+  position: absolute;
+`;
+
+export const StyledCheckBox = styled.span`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 20px;
+  height: 20px;
+  text-align: center;
+  color: white;
+  border: 1px solid var(--color-mint);
+  border-radius: 4px;
+  background-color: ${({ checked }) =>
+    checked ? css`var(--color-mint)` : css`var(--color-white)`};
+`;
