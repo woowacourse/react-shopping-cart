939664fc4d33e5c8dae1a237cf7218e80878e19c
shinsehantan <shinsehantan@users.noreply.github.com>
2021-05-10 14:22:20 +0900

    feat: 상품 목록, 구매 목록 mock data 생성

--- src/data/mock.js
+++ src/data/mock.js
@@ -1,3 +1,38 @@
+export const paymentItems = [
+  {
+    id: '1',
+    image: 'https://cdn-mart.baemin.com/goods/custom/20200525/11153-main-01.png',
+    name: '[\ub4e0\ub4e0] \uc720\ubd80 \uc2ac\ub77c\uc774\uc2a4 500g',
+    price: '4900',
+    quantity: 3,
+    checked: true,
+  },
+  {
+    id: '2',
+    image: 'https://cdn-mart.baemin.com/goods/custom/20200525/11157-main-01.png',
+    name: '[\ub4e0\ub4e0] \uc9c4\ub9db\uc0b4 1kg',
+    price: '7100',
+    quantity: 1,
+    checked: true,
+  },
+  {
+    id: '3',
+    image: 'https://cdn-mart.baemin.com/goods/custom/20200525/11161-main-01.png',
+    name: '[\ub4e0\ub4e0] \uc2e0-\ud0a4\ub9ac\ubaa8\ucc0c 1kg',
+    price: '12000',
+    quantity: 1,
+    checked: true,
+  },
+  {
+    id: '4',
+    image: 'https://cdn-mart.baemin.com/goods/custom/20200525/11263-main-01.png',
+    name: '[\ub4e0\ub4e0] \ud751\uace4\uc57d 250g',
+    price: '1300',
+    quantity: 10,
+    checked: true,
+  },
+];
+
 export const products = [
   {
     id: '1',
