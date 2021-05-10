368429b8658e1465fc53320aa2d969c59a798014
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-05-10 21:55:40 +0900

    feat: 주문/결제 페이지로 이동하는 기능 구현
    Co-authored-by: JUNMO HAN <wnsah052@naver.com>

--- src/pages/OrderPayment.jsx
+++ src/pages/OrderPayment.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import OrderListItem, { ORDER_LIST_ITEM_TYPE } from '../components/orderListItem/OrderListItem';
 import PageTitle from '../components/pageTitle/PageTitle';
@@ -18,23 +19,28 @@ const OrderPaymentAmountWrapper = styled.div`
   right: 0;
 `;
 
-const OrderPayment = ({ productListState }) => (
-  <>
-    <PageTitle>주문/결제</PageTitle>
-    <Content>
-      <div>
-        <SelectedProductList
-          listType={SELECTED_PRODUCT_LIST_TYPE.ORDER_PAYMENT}
-          itemType={ORDER_LIST_ITEM_TYPE.ORDER_PAYMENT}
-          productList={productListState}
-          ListItem={OrderListItem}
-        />
-      </div>
-      <OrderPaymentAmountWrapper>
-        <PaymentAmount type={PAYMENT_AMOUNT_TYPE.ORDER_PAYMENT} price={325000} />
-      </OrderPaymentAmountWrapper>
-    </Content>
-  </>
-);
+const OrderPayment = () => {
+  const shoppingCartList = useSelector((state) => state.shoppingCart.shoppingCartList);
+  const orderPaymentList = shoppingCartList.filter((item) => item.isChecked);
+
+  return (
+    <>
+      <PageTitle>주문/결제</PageTitle>
+      <Content>
+        <div>
+          <SelectedProductList
+            listType={SELECTED_PRODUCT_LIST_TYPE.ORDER_PAYMENT}
+            itemType={ORDER_LIST_ITEM_TYPE.ORDER_PAYMENT}
+            productList={orderPaymentList}
+            ListItem={OrderListItem}
+          />
+        </div>
+        <OrderPaymentAmountWrapper>
+          <PaymentAmount type={PAYMENT_AMOUNT_TYPE.ORDER_PAYMENT} price={325000} />
+        </OrderPaymentAmountWrapper>
+      </Content>
+    </>
+  );
+};
 
 export default OrderPayment;
