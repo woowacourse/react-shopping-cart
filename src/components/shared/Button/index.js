c9626b2fca59f99a6b94eb473ac5bfb88f759814
sunhpark42 <sunhpark42@gmail.com>
2021-05-10 21:26:57 +0900

    refactor: 사용되지 않는 변수 삭제

--- src/components/shared/Button/index.js
+++ src/components/shared/Button/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Container, ButtonText } from './style';
+import { Container } from './style';
 
 const Button = ({
   children,
