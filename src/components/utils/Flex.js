9a54a746b25e911806939fb98f33b8083c58f507
shinsehantan <shinsehantan@users.noreply.github.com>
2021-05-10 17:56:23 +0900

    feat: NotFoundPage 컴포넌트 구현
    - Flex 컴포넌트 구현

--- src/components/utils/Flex.js
+++ src/components/utils/Flex.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 
-function Flex() {
-  return <div>플렉스 컴포넌트입니다.</div>;
+export const FlexBox = styled.div`
+  display: flex;
+  flex-direction: ${(props) => props.flexDirection};
+  flex-flow: ${(props) => props.flexFlow};
+  justify-content: ${(props) => props.justifyContent};
+  align-items: ${(props) => props.alignItems};
+  flex-wrap: ${(props) => props.flexWrap};
+  flex-basis: ${(props) => props.flexBasis};
+  flex-grow: ${(props) => props.flexGrow};
+  flex-shrink: ${(props) => props.flexShrink};
+
+  && {
+    ${(props) => props.css}
+  }
+`;
+
+function Flex({ children, ...props }) {
+  return <FlexBox {...props}>{children}</FlexBox>;
 }
 
 export default Flex;
--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,7 +1,66 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import Flex from '../components/utils/Flex';
+import Image from '../components/utils/Image';
+import Button from '../components/utils/Button';
+
+import notFoundImg from '../asset/not-found.jpeg';
+
+import styled, { css } from 'styled-components';
+
+const FlexStyle = css`
+  width: 100%;
+  height: 80vh;
+`;
+
+const ContentWrapper = styled.div`
+  margin: 25px;
+  text-align: center;
+`;
+
+const ContentText = styled.p`
+  font-size: 24px;
+  line-height: 36px;
+  color: #1c827f;
+  margin: 0;
+  padding: 0;
+`;
+
+const LinkStyle = styled(NavLink)`
+  text-decoration: none;
+
+  &:visited {
+    color: #ffffff;
+    font-weight: 700;
+  }
+`;
+
+const StyledButton = styled(Button)`
+  border-radius: 10px;
+  margin: 10px;
+`;
 
 function NotFoundPage() {
-  return <div>없는페이지 입니다.</div>;
+  return (
+    <Flex justifyContent="center" alignItems="center" flexDirection="column" css={FlexStyle}>
+      <Image src={notFoundImg} alt="없는 페이지를 나타내는 이미지" />
+      <ContentWrapper>
+        <ContentText>죄송합니다. 페이지가 없거나 오류가 발생하였습니다.</ContentText>
+        <ContentText>현재 페이지가 존재하지 않거나, 현재 이용할 수 없는 페이지 입니다.</ContentText>
+        <StyledButton
+          width="130px"
+          height="40px"
+          color="#ffffff"
+          border="none"
+          backgroundColor="#2ac1bc"
+          fontSize="18px"
+        >
+          <LinkStyle to="/">쇼핑하러 가기</LinkStyle>
+        </StyledButton>
+      </ContentWrapper>
+    </Flex>
+  );
 }
 
 export default NotFoundPage;
