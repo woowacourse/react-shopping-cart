a398b11f966779da1cb9b26cbe07e6be31ef2d5a
365kim <365listener@gmail.com>
2021-05-12 12:07:46 +0900

    feat: 상품목록 반응형 그리드 적용
    Co-authored-by: 0imbean0 <0imbean0@users.noreply.github.com>

--- src/components/pages/ProductListPage/style.js
+++ src/components/pages/ProductListPage/style.js
@@ -10,6 +10,26 @@ export const Main = styled.main;
 export const ProductList = styled.ul`
   list-style: none;
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(1, 1fr);
   padding: 0;
+
+  @media (min-width: 520px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (min-width: 700px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (min-width: 992px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+
+  @media (min-width: 1200px) {
+    grid-template-columns: repeat(5, 1fr);
+  }
+
+  @media (min-width: 1600px) {
+    grid-template-columns: repeat(6, 1fr);
+  }
 `;
