71df1220e5d882bd901cd8950937f051ccd0c5b4
jho2301 <jho2301@gmail.com>
2021-05-13 07:26:34 +0900

    chore: json-server 헤로쿠 배포설정

--- src/constants/API.ts
+++ src/constants/API.ts
@@ -1,4 +1,4 @@
 // JSON Server
-export const API_BASE_URL = 'http://localhost:7000';
+export const API_BASE_URL = 'https://wooteco-lv2-react-cart.herokuapp.com/api';
 
 export type HTTPMethod = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
