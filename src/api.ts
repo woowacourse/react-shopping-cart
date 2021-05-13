7b9a9c4eb27e6daf4ce8e712d861c4d370be0577
zigsong <wldms5764@gmail.com>
2021-05-14 08:54:09 +0900

    feat: api server 주소 변경

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'https://react-shopping-cart-zig.herokuapp.com/',
 });
 
 export default api;
