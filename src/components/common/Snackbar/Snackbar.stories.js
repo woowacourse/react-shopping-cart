8e23120af47876f1553f2d25a0b7495bc43ef116
iborymagic <mlch8023@gmail.com>
2021-05-13 21:52:01 +0900

    fix: Snackbar 컴포넌트 React.Portal로 수정 - 스타일 수정 - Storybook 오류 수정

--- src/components/common/Snackbar/Snackbar.stories.js
+++ src/components/common/Snackbar/Snackbar.stories.js
@@ -12,4 +12,5 @@ const Template = (args) => <Snackbar {...args} />;
 export const Default = Template.bind({});
 Default.args = {
   text: '장바구니에 상품이 추가되었습니다.',
+  setMessage: () => {},
 };
--- src/components/common/Snackbar/style.js
+++ src/components/common/Snackbar/style.js
@@ -1,5 +1,4 @@
 import styled, { keyframes } from 'styled-components';
-import PALETTE from '../../../constants/palette';
 
 const fadein = keyframes`
   from {
@@ -7,14 +6,14 @@ const fadein = keyframes`
     opacity: 0;
   }
   to {
-    bottom: 10px;
+    bottom: 20px;
     opacity: 1;
   }
 `;
 
 const fadeout = keyframes`
   from {
-    bottom: 10px;
+    bottom: 20px;
     opacity: 1;
   }
   to {
@@ -28,13 +27,14 @@ export const SnackbarContainer = styled.div`
   background-color: ${({ backgroundColor }) => (backgroundColor ? backgroundColor : '#333')};
   color: #fff;
   text-align: center;
-  border-radius: 0.5rem;
-  padding: 16px 30px 16px 30px;
+  border-radius: 0.4rem;
+  padding: 25px 50px 25px 50px;
   position: fixed;
   z-index: 1;
   left: 50%;
   transform: translateX(-50%);
-  bottom: 10px;
-  -webkit-animation: ${fadein} 0.5s, ${fadeout} 0.5s ${({ time }) => (time ? time : '3s')};
-  animation: ${fadein} 0.5s, ${fadeout} 0.5s ${({ time }) => (time ? time : '3s')};
+  bottom: 20px;
+  box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.5);
+  -webkit-animation: ${fadein} 0.5s, ${fadeout} 0.5s ${({ time }) => (time ? time : '2s')};
+  animation: ${fadein} 0.5s, ${fadeout} 0.5s ${({ time }) => (time ? time : '2s')};
 `;
