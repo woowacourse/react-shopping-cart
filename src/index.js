53e7cdf17e48d2e1fd6f500fd33830d8f807a9b0
Min Kyung Kang <mnk918p@gmail.com>
2021-05-15 01:04:32 +0900

    fix: settings.py

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+import axios from 'axios';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+axios.defaults.baseURL =
+  process.env.NODE_ENV === 'development'
+    ? '/'
+    : 'https://mk27.pythonanywhere.com';
 
 ReactDOM.render(
   <React.StrictMode>
