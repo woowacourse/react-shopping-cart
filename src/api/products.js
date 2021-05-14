40829c051c634e1a15cb7f85a3a9772fbc52354e
shinsehantan <shinsehantan@users.noreply.github.com>
2021-05-15 03:07:09 +0900

    refactor: axios get 요청 주소 변경

--- src/api/products.js
+++ src/api/products.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const getProducts = async () => {
-  const response = await axios.get('http://localhost:3001/products');
+  const response = await axios.get('/products');
   return response.data;
 };
