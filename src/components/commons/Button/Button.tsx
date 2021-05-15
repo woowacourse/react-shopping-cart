ca4518dafcf3a3d40a19541c559a4699472ce095
swon3210 <swon3210@gmail.com>
2021-05-15 23:22:29 +0900

    feat: Button 컴포넌트에 type prop 추가
    - 기본 type 은 "button"

--- src/components/commons/Button/Button.tsx
+++ src/components/commons/Button/Button.tsx
@@ -1,16 +1,17 @@
 import * as Styled from './Button.styles';
 
 export interface Props
-  extends Pick<React.ButtonHTMLAttributes<HTMLButtonElement>, 'disabled' | 'onClick' | 'children'> {
+  extends Pick<React.ButtonHTMLAttributes<HTMLButtonElement>, 'disabled' | 'onClick' | 'children' | 'type'> {
   /** 버튼의 크기 및 폰트 크기 유형 */
   size: 'SM' | 'MD' | 'LG';
   backgroundColor?: string;
   fontColor?: string;
 }
 
-const Button = ({ disabled, onClick, size, backgroundColor, fontColor, children }: Props) => {
+const Button = ({ disabled, onClick, size, backgroundColor, fontColor, children, type = 'button' }: Props) => {
   return (
     <Styled.Button
+      type={type}
       disabled={disabled}
       onClick={onClick}
       size={size}
