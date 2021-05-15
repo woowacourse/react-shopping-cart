19433ffb4ad7ba32fb8598ef117749e56e5b72ae
bigsaigon333 <likepeppermint@gmail.com>
2021-05-15 09:56:42 +0900

    feat: 상품 상세 페이지의 레이아웃을 구현한다

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { productAPI } from "../../utils/api";
+import { formatPrice } from "../../utils/utils";
+import Button from "../@shared/Button/Button";
+import * as S from "./ProductDetail.styled";
 
 const ProductDetail = () => {
   const { productId } = useParams();
 
-  return <span>{productId}</span>;
+  const [detail, setDetail] = useState(null);
+
+  useEffect(() => {
+    const getProductDetail = async (id) => {
+      try {
+        const productDetail = await productAPI.fetchById(id);
+
+        setDetail(productDetail);
+      } catch (error) {
+        // TODO: error handling 추가
+      }
+    };
+
+    getProductDetail(productId);
+  }, [productId]);
+
+  const { name, price = 0, image_url: imageURL } = detail ?? {};
+
+  return (
+    <S.ProductDetail>
+      <S.ImgContainer>
+        <S.Img src={imageURL} alt={name} />
+        <S.Name>{name}</S.Name>
+      </S.ImgContainer>
+      <S.PriceContainer>
+        <S.PriceLabel>금액</S.PriceLabel>
+        <S.Price>{formatPrice(price)}원</S.Price>
+      </S.PriceContainer>
+      <Button type="primary">
+        <S.ButtonContent>장바구니</S.ButtonContent>
+      </Button>
+    </S.ProductDetail>
+  );
 };
 
 export default ProductDetail;
---
+++ src/components/ProductDetail/ProductDetail.styled.jsx
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+import { COLOR } from "../../constants/style";
+
+export const ProductDetail = styled.section`
+  margin: 0 auto;
+  width: 40rem;
+`;
+
+export const ImgContainer = styled.div`
+  padding: 1.5rem 2.25rem;
+  border-bottom: 4px solid ${COLOR.GRAY.LIGHT_100};
+
+  & > *:not(:last-child) {
+    margin-bottom: 1.25rem;
+  }
+`;
+
+export const Img = styled.img`
+  width: 100%;
+  aspect-ratio: 1 / 1;
+`;
+
+export const Name = styled.h1`
+  font-size: 2rem;
+  line-height: 3rem;
+  font-weight: 700;
+`;
+
+export const PriceContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem;
+`;
+
+export const PriceLabel = styled.b`
+  font-size: 1.5rem;
+  line-height: 2.25rem;
+`;
+
+export const Price = styled.strong`
+  font-size: 2rem;
+  line-height: 3rem;
+`;
+
+export const ButtonContent = styled.span`
+  font-size: 2rem;
+  line-height: 3rem;
+  font-weight: 700;
+`;
