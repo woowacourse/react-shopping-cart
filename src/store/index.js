51548093b1beac5235ddeebf29e7e033233ee27b
bigsaigon333 <likepeppermint@gmail.com>
2021-05-16 20:36:51 +0900

    feat: localStorage에 redux state를 저장한다

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { loadState, saveState } from "../utils/localStorage";
+import { debounce } from "../utils/utils";
 import cartReducer from "./modules/cartSlice";
 import orderReducer from "./modules/orderSlice";
 
-export default configureStore({
+const store = configureStore({
   reducer: { cart: cartReducer, order: orderReducer },
+  preloadedState: loadState(),
 });
+
+store.subscribe(
+  debounce(() => {
+    saveState(store.getState());
+  })
+);
+
+export default store;
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -8,3 +8,19 @@ export const formatPrice = (price) => {
 
   return price.toLocaleString("en-US");
 };
+
+export const debounce = (callback, delay = 1000) => {
+  if (typeof callback !== "function") {
+    throw new TypeError("debounce 함수의 첫번째 인자는 함수여야 합니다");
+  }
+
+  let id = null;
+
+  return (...args) => {
+    if (id) {
+      clearTimeout(id);
+    }
+
+    id = setTimeout(() => callback(...args), delay);
+  };
+};
