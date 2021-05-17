be3e19f91d25839706996030429ba66e7df58a1f
yungo1846 <yungo1468@naver.com>
2021-05-17 17:08:53 +0900

    refactor: Checkbox label id를 uuid로 변경

--- src/components/commons/Checkbox/Checkbox.tsx
+++ src/components/commons/Checkbox/Checkbox.tsx
@@ -1,4 +1,5 @@
 import * as Styled from './Checkbox.styles';
+import { v4 as uuidv4 } from 'uuid';
 
 export interface Props {
   labelText?: string;
@@ -7,16 +8,18 @@ export interface Props {
 }
 
 const Checkbox = ({ labelText, isChecked, onCheck }: Props) => {
+  const uuid = uuidv4();
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const isChecked = event.target.checked;
     onCheck?.(isChecked);
   };
+
   return (
     <Styled.Checkbox>
       {labelText ? (
         <>
-          <Styled.Input type="checkbox" id={labelText} checked={isChecked} onChange={onChange} />
-          <Styled.Label htmlFor={labelText}>{labelText}</Styled.Label>
+          <Styled.Input type="checkbox" id={uuid} checked={isChecked} onChange={onChange} />
+          <Styled.Label htmlFor={uuid}>{labelText}</Styled.Label>
         </>
       ) : (
         <Styled.Input type="checkbox" checked={isChecked} onChange={onChange} />
