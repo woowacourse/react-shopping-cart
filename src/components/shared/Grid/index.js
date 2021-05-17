c22dec86ea7a7b434071cfe93e2d8a427c5b0cf0
2sooy <1401660@naver.com>
2021-05-17 09:04:33 +0900

    refactor: Grid 컴포넌트 col의 default value 설정

--- src/components/shared/Grid/index.js
+++ src/components/shared/Grid/index.js
@@ -6,11 +6,11 @@ import { Container } from './style';
  * 그리드 간격은 가로 64px, 세로 16px 입니다.
  */
 const Grid = ({ col, children }) => {
-  return <Container col={col}>{children}</Container>;
+  return <Container col={col || '4'}>{children}</Container>;
 };
 
 Grid.propTypes = {
-  col: PropTypes.string.isRequired,
+  col: PropTypes.string,
   children: PropTypes.node.isRequired,
 };
 
