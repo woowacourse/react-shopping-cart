038c38c762095be058ecc5a4e7459f46aac48d60
shinsehantan <shinsehantan@users.noreply.github.com>
2021-05-17 16:51:54 +0900

    feat: redux toolkit 설치 및 configureStore 적용

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { configureStore } from '@reduxjs/toolkit';
 import { BrowserRouter } from 'react-router-dom';
-
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './modules';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer, composeWithDevTools());
+const store = configureStore({
+  reducer: rootReducer,
+});
 
 ReactDOM.render(
   <Provider store={store}>
