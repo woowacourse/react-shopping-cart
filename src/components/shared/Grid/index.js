a50ee65a68fdbdfdd1166021b05e0f5587f292d5
sunhpark42 <sunhpark42@gmail.com>
2021-05-19 19:26:10 +0900

    refactor: Grid 컴포넌트 주석 삭제

--- src/components/shared/Grid/index.js
+++ src/components/shared/Grid/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Container } from './style';
 
-/**
- * 그리드 간격은 가로 64px, 세로 16px 입니다.
- */
 const Grid = ({ col = 1, children, columnGap = '64px', rowGap = '16px' }) => {
   return (
     <Container col={col} columnGap={columnGap} rowGap={rowGap}>
