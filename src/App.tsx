57db010e98f678f358d0d46e180d01429258cb61
zigsong <wldms5764@gmail.com>
2021-05-19 12:29:34 +0900

    refactor: separate router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Global, ThemeProvider } from '@emotion/react';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
@@ -7,13 +7,7 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { SnackbarProvider } from 'notistack';
 import BaseLayout from 'components/layout/BaseLayout/BaseLayout';
-import ProductsPage from 'pages/ProductsPage/ProductsPage';
-import CartPage from 'pages/CartPage/CartPage';
-import OrderPage from 'pages/OrderPage/OrderPage';
-import ProductDetailPage from 'pages/ProductDetailPage/ProductDetailPage';
-import OrderCompletePage from 'pages/OrderCompletePage/OrderCompletePage';
-import OrderListPage from 'pages/OrderListPage/OrderListPage';
-import OrderDetailPage from 'pages/OrderDetailPage/OrderDetailPage';
+import Routes from 'Routes';
 import rootReducer from 'modules';
 import Styled, { globalStyle, theme } from './App.styles';
 
@@ -34,32 +28,7 @@ const App = () => {
             <BaseLayout>
               <Global styles={globalStyle} />
               <Styled.Page>
-                <Switch>
-                  <Route exact path="/">
-                    <ProductsPage />
-                  </Route>
-                  <Route exact path="/products">
-                    <ProductsPage />
-                  </Route>
-                  <Route path="/products/detail">
-                    <ProductDetailPage />
-                  </Route>
-                  <Route path="/cart">
-                    <CartPage />
-                  </Route>
-                  <Route exact path="/order">
-                    <OrderPage />
-                  </Route>
-                  <Route path="/order/complete">
-                    <OrderCompletePage />
-                  </Route>
-                  <Route path="/order/list">
-                    <OrderListPage />
-                  </Route>
-                  <Route path="/order/detail">
-                    <OrderDetailPage />
-                  </Route>
-                </Switch>
+                <Routes />
               </Styled.Page>
             </BaseLayout>
           </Router>
