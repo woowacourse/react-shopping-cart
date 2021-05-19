506521fccd6dd4ae349e4b00f7c2834f04f20949
sunhpark42 <sunhpark42@gmail.com>
2021-05-19 18:13:52 +0900

    feat: Grid 컴포넌트에 grap 관련 props 추가

--- src/components/shared/Grid/index.js
+++ src/components/shared/Grid/index.js
@@ -5,13 +5,19 @@ import { Container } from './style';
 /**
  * 그리드 간격은 가로 64px, 세로 16px 입니다.
  */
-const Grid = ({ col, children }) => {
-  return <Container col={col}>{children}</Container>;
+const Grid = ({ col, children, columnGap = '64px', rowGap = '16px' }) => {
+  return (
+    <Container col={col} columnGap={columnGap} rowGap={rowGap}>
+      {children}
+    </Container>
+  );
 };
 
 Grid.propTypes = {
   col: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
+  columnGap: PropTypes.string,
+  rowGap: PropTypes.string,
 };
 
 export default Grid;
--- src/components/shared/Grid/style.js
+++ src/components/shared/Grid/style.js
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 export const Container = styled.section`
   display: grid;
   grid-template-columns: repeat(${({ col }) => col}, 1fr);
-  grid-column-gap: 64px;
-  grid-row-gap: 32px;
+  grid-column-gap: ${({ columnGap }) => columnGap};
+  grid-row-gap: ${({ rowGap }) => rowGap};
 `;
