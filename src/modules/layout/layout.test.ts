b7de0d497a91bf21b7ccd116be83db19c13d3f6c
swon3210 <swon3210@gmail.com>
2021-05-30 21:12:55 +0900

    test: snackbar & confirmModal 메세지 상태 반영 여부도 테스트에 추가

--- src/modules/layout/layout.test.ts
+++ src/modules/layout/layout.test.ts
@@ -4,52 +4,56 @@ import { LayoutState } from './type';
 
 describe('layout reducer', () => {
   describe('layout/snackbar', () => {
+    const snackbarMessage = 'message';
+
     let layoutState: LayoutState = {
       isConfirmModalShown: false,
       isSnackbarShown: false,
       snackbarMessage: '',
+      confirmModalMessage: '',
     };
 
     it(SHOW_SNACKBAR, () => {
-      layoutState = layoutReducer(layoutState, { type: SHOW_SNACKBAR });
+      layoutState = layoutReducer(layoutState, { type: SHOW_SNACKBAR, payload: snackbarMessage });
       expect(layoutState).toEqual<LayoutState>({
-        isConfirmModalShown: false,
+        ...layoutState,
         isSnackbarShown: true,
-        snackbarMessage: '',
+        snackbarMessage: snackbarMessage,
       });
     });
 
     it(HIDE_SNACKBAR, () => {
       layoutState = layoutReducer(layoutState, { type: HIDE_SNACKBAR });
       expect(layoutState).toEqual<LayoutState>({
-        isConfirmModalShown: false,
+        ...layoutState,
         isSnackbarShown: false,
-        snackbarMessage: '',
       });
     });
   });
 
   describe('layout/confirmModal', () => {
+    const confirmModalMessage = 'message';
+
     let layoutState: LayoutState = {
       isConfirmModalShown: false,
       isSnackbarShown: false,
       snackbarMessage: '',
+      confirmModalMessage: '',
     };
 
     it(SHOW_CONFIRM_MODAL, () => {
-      layoutState = layoutReducer(layoutState, { type: SHOW_CONFIRM_MODAL });
+      layoutState = layoutReducer(layoutState, { type: SHOW_CONFIRM_MODAL, payload: 'message' });
       expect(layoutState).toEqual<LayoutState>({
+        ...layoutState,
         isConfirmModalShown: true,
-        isSnackbarShown: false,
-        snackbarMessage: '',
+        snackbarMessage: confirmModalMessage,
       });
     });
     it(HIDE_CONFIRM_MODAL, () => {
       layoutState = layoutReducer(layoutState, { type: HIDE_CONFIRM_MODAL });
       expect(layoutState).toEqual<LayoutState>({
+        ...layoutState,
         isConfirmModalShown: false,
-        isSnackbarShown: false,
-        snackbarMessage: '',
       });
     });
   });
