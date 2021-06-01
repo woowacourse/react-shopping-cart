b9f6c0e9350c00d3e1d7ee2f1429a91a4d352160
Min Kyung Kang <mnk918p@gmail.com>
2021-06-01 23:10:16 +0900

    feat: 장바구니에 담긴 상품 개수 보여주기

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,28 +1,44 @@
 import React from 'react';
-import { InnerNav, LogoWrapper, Menu, Nav } from './index.styles';
+import {
+  InnerNav,
+  LogoWrapper,
+  Menu,
+  Nav,
+  NavItem,
+  Alert,
+  AlertNavItem,
+} from './index.styles';
 import { ROUTE } from '../../constants';
 import { Cart } from '../../assets/svg';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-const NavBar = () => (
-  <Nav>
-    <InnerNav>
-      <LogoWrapper>
-        <span>
-          <Cart width="30" height="30" color="white" />
-        </span>
-        <Link to={ROUTE.HOME}>WOOWA SHOP</Link>
-      </LogoWrapper>
-      <Menu>
-        <li>
-          <Link to={ROUTE.CART}>장바구니</Link>
-        </li>
-        <li>
-          <Link to={ROUTE.COMPLETED_ORDER}>주문목록</Link>
-        </li>
-      </Menu>
-    </InnerNav>
-  </Nav>
-);
+const NavBar = () => {
+  const cartLength = useSelector(state => state.product.product.cartItems)
+    .length;
+  return (
+    <Nav>
+      <InnerNav>
+        <LogoWrapper>
+          <span>
+            <Cart width="30" height="30" color="white" />
+          </span>
+          <Link to={ROUTE.HOME}>WOOWA SHOP</Link>
+        </LogoWrapper>
+        <Menu>
+          <AlertNavItem>
+            <Link to={ROUTE.CART}>
+              장바구니
+              {cartLength ? <Alert>{cartLength}</Alert> : ''}
+            </Link>
+          </AlertNavItem>
+          <NavItem>
+            <Link to={ROUTE.COMPLETED_ORDER}>주문목록</Link>
+          </NavItem>
+        </Menu>
+      </InnerNav>
+    </Nav>
+  );
+};
 
 export default NavBar;
--- src/components/NavBar/index.styles.js
+++ src/components/NavBar/index.styles.js
@@ -34,8 +34,37 @@ export const LogoWrapper = styled.div`
 export const Menu = styled.ul`
   display: flex;
   align-items: center;
+`;
 
-  & > li {
-    margin-left: 1rem;
+export const NavItem = styled.li`
+  margin-left: 1.5rem;
+
+  &:hover *,
+  & *:hover {
+    opacity: 1;
+    font-weight: 800;
   }
+`;
+
+export const AlertNavItem = styled(NavItem)`
+  position: relative;
+  cursor: pointer;
+  padding: 0.5rem 0 0.5rem 1rem;
+`;
+
+export const Alert = styled.span`
+  position: absolute;
+  background-color: #ec0000;
+  opacity: 0.825;
+  line-height: 1.875;
+  border-radius: 50%;
+  font-size: 0.625rem;
+  font-weight: 800;
+  top: -0.225rem;
+  right: -1rem;
+  min-width: 1.3rem;
+  padding: 2.5px;
+  align-items: center;
+  justify-content: center;
+  display: flex;
 `;
