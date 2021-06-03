b8e4d042e9cda2eebe4d667e4d4be27c2dfb4c30
seojihwan <seojihwan213@naver.com>
2021-06-03 19:16:41 +0900

    test: fetch mocking을 위한 setupFile 추가

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,7 @@ const config: Config.InitialOptions = {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/assetsTransformer.ts",
   },
+  setupFiles: ["./setupJest.ts"],
 };
 
 export default config;
