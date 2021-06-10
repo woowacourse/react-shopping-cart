f50211d92a9805bd7a522ce4236e2764016f7250
365kim <365listener@gmail.com>
2021-06-10 18:05:14 +0900

    refactor: ROUTE path 수정

--- src/constants/route.js
+++ src/constants/route.js
@@ -1,10 +1,8 @@
-const REPO_NAME = '/react-shopping-cart';
-
 export const ROUTE = {
-  HOME: `${REPO_NAME}`,
-  PRODUCT_LIST: `${REPO_NAME}/product-list`,
-  PRODUCT_DETAIL: `${REPO_NAME}/product-detail`,
-  CART: `${REPO_NAME}/cart`,
-  CHECKOUT: `${REPO_NAME}/checkout`,
-  ORDER_LIST: `${REPO_NAME}/order-list`,
+  HOME: `/`,
+  PRODUCT_LIST: `/product-list`,
+  PRODUCT_DETAIL: `/product-detail`,
+  CART: `/cart`,
+  CHECKOUT: `/checkout`,
+  ORDER_LIST: `/order-list`,
 };
