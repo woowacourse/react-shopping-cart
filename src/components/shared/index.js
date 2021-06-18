92c8a898b75866b6158175dcda9a54f4470b969c
2sooy <1401660@naver.com>
2021-06-18 14:18:00 +0900

    feat: 로딩 컴포넌트 추가

--- src/components/shared/index.js
+++ src/components/shared/index.js
@@ -5,3 +5,5 @@ export { default as HighlightText } from './HighlightText';
 export { default as NumericInput } from './NumericInput';
 export { default as Product } from './Product';
 export { default as IconButton } from './IconButton';
+export { default as Thumbnail } from './Thumbnail';
+export { default as Loader } from './Loader';
