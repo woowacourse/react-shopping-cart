fd72b2a200498bf4b0e257c1e1d2f8abf812d128
2sooy <1401660@naver.com>
2021-06-18 16:10:21 +0900

    chore: 기존 리듀서 파일 제거

--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers, createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { cartReducer } from './cartReducer';
-import { itemListReducer } from './itemListReducer';
-import { orderListReducer } from './orderListReducer';
-
-const rootReducer = combineReducers({ itemListReducer, cartReducer, orderListReducer });
-
-export const store = createStore(rootReducer, composeWithDevTools());
