d183cbe3df4ff7c0e47a8bafa365856577da7bf6
airman5573 <rpf5573@kakao.com>
2022-05-10 20:42:01 +0900

    chore: 프로젝트 기본 설정
    1. webpack
    2. babel
    3. scss
    4.emotion
    5. eslint & prettier

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   rules: {
-    'header-max-length': [0, 'always', 100],
+    "header-max-length": [0, "always", 100],
   },
 };
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { join } = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: join(__dirname, "/src/index.jsx"),
+  devtool: "source-map",
+  devServer: {
+    historyApiFallback: true,
+  },
+  output: {
+    filename: "main.js",
+    path: join(__dirname, "/dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(scss)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "url-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+    new HtmlWebpackPlugin({
+      template: join(__dirname, "public/index.html"),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  resolve: {
+    extensions: [".js", ".jsx", ".scss"],
+  },
+};
