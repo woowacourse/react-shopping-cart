05720577f907e989717c54937b103e74757753a6
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-05-10 20:33:14 +0900

    💄 : root 폰트 사이즈 지정

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,6 +1,10 @@
 import { css } from '@emotion/react';
 import 'styles/utils/Fontawesome.css';
 
-const GlobalStyles = css``;
+const GlobalStyles = css`
+  :root {
+    font-size: 16px;
+  }
+`;
 
 export default GlobalStyles;
