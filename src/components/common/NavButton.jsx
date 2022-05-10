4bc1dc515661b49b6ce7cf83a245fbecd975e933
Soyi Jeon <soyi.dev@gmail.com>
2022-05-10 15:11:17 +0900

    feat: NavButton 컴포넌트 생성 및 스토리 작성
    Co-authored-by: TaeYoon <uni613@naver.com>

---
+++ src/components/common/NavButton.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledNavButton = styled.button`
+  padding: 0 4px 4px;
+
+  background-color: transparent;
+  border: none;
+  color: #fff;
+  font-size: 1.5rem;
+  text-align: center;
+  cursor: pointer;
+
+  :hover {
+    border-bottom: 2px solid #fff;
+  }
+`;
+
+function NavButton({ children, ...rest }) {
+  return <StyledNavButton {...rest}>{children}</StyledNavButton>;
+}
+
+export default NavButton;
