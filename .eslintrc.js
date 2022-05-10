d66b906ceeebe815fb2bb63576f9c28fe5ee468f
Daye Lee <508yeah@gmail.com>
2022-05-10 16:28:56 +0900

    chore: eslint 및 prettier 설정
    Co-authored-by: kamwoo <kamwoo@users.noreply.github.com>

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+};
