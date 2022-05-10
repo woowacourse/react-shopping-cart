0b030c0812bb85136412518df1a6476344e2a741
DomMorello <zuzudnf@gmail.com>
2022-05-10 16:27:34 +0900

    feat: Header 컴포넌트 작성
    Co-authored-by: kwannee <yleh1126@naver.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import './App.css';
+import GlobalStyle from './GlobalStyle';
+
+import Header from './components/Header/Header';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+
+function Header() {
+  return (
+    <header>
+      <Styled.Nav>
+        <Styled.NavTitleWrapper>
+          <Styled.NavTitle>WOOWA SHOP</Styled.NavTitle>
+        </Styled.NavTitleWrapper>
+        <Styled.NavButtonWrapper>
+          <Styled.NavButton type="button">장바구니</Styled.NavButton>
+          <Styled.NavButton type="button">주문목록</Styled.NavButton>
+        </Styled.NavButtonWrapper>
+      </Styled.Nav>
+    </header>
+  );
+}
+
+export default Header;
+
+const Styled = {
+  Nav: styled.nav`
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+    height: 80px;
+
+    background: #2ac1bc;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.3);
+  `,
+  NavTitleWrapper: styled.div`
+    display: flex;
+    align-items: center;
+  `,
+  NavTitle: styled.h1`
+    font-size: 40px;
+    font-weight: 900;
+
+    color: #ffffff;
+  `,
+  NavButtonWrapper: styled.div`
+    display: flex;
+    gap: 15px;
+  `,
+  NavButton: styled.button`
+    background-color: transparent;
+    font-weight: 500;
+    font-size: 24px;
+    border: none;
+
+    color: #ffffff;
+  `,
+};
