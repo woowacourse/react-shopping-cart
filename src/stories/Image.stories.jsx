791af4742a0d605fdda655d3058a98eb37ec0beb
KIM YEJI <rladpwl0512@gmail.com>
2022-05-10 19:41:35 +0900

    :white_check_mark: test: 이미지 컴포넌트 storybook 작성

---
+++ src/stories/Image.stories.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Image from '../components/Image';
+
+export default {
+  title: 'Component/Image',
+  component: Image,
+  argTypes: {
+    size: { controls: 'text' },
+    src: { controls: 'text' },
+  },
+};
+
+const DefaultImageTemplate = (args) => <Image {...args} />;
+
+export const DefaultImage = DefaultImageTemplate.bind({});
+DefaultImage.args = {
+  src: 'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201704/10/8a043cc8-818b-4b85-a962-7914b83777de.jpg',
+  size: '282px',
+};
