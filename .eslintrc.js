027d4e96230eecf096b9079a48de6f1384415b24
prefer2 <sunho6200@gmail.com>
2022-05-10 15:32:22 +0900

    chore: storybook 설치
    Co-authored-by: jhy979 <jhy979@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,12 @@ module.exports = {
     es2021: true,
     node: true,
   },
-
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:storybook/recommended',
+  ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
