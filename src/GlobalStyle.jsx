056d1cbbc74f5b379e91fe81ed17caa9f3bc64cc
DomMorello <zuzudnf@gmail.com>
2022-05-10 16:27:04 +0900

    style: 글로벌 스타일 지정
    Co-authored-by: kwannee <yleh1126@naver.com>

---
+++ src/GlobalStyle.jsx
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: "Helvetica", "Arial", sans-serif;
+    width: 100vw;
+    margin: auto 0;
+  }
+`;
+
+export default GlobalStyle;
