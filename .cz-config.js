c27bf4dccdb842d5fcf6e225400a777d31833fcf
airman5573 <rpf5573@kakao.com>
2022-05-10 19:46:38 +0900

    chore: git 커밋 메세지의 규칙을 설정한다

---
+++ .cz-config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  types: [
+    { value: 'feat', name: '✨ feat:\tAdding a new feature' },
+    { value: 'fix', name: '🐛 fix:\tFixing a bug' },
+    { value: 'docs', name: '📝 docs:\tAdd or update documentation' },
+    {
+      value: 'style',
+      name: '💄 style:\tAdd or update styles, ui or ux',
+    },
+    {
+      value: 'refactor',
+      name: '♻️  refactor:\tCode change that neither fixes a bug nor adds a feature',
+    },
+    {
+      value: 'perf',
+      name: '⚡️ perf:\tCode change that improves performance',
+    },
+    {
+      value: 'test',
+      name: '✅ test:\tAdding tests cases',
+    },
+    {
+      value: 'chore',
+      name: '🚚 chore:\tChanges to the build process or auxiliary tools\n\t\tand libraries such as documentation generation',
+    },
+    { value: 'revert', name: '⏪️ revert:\tRevert to a commit' },
+    { value: 'wip', name: '🚧 wip:\tWork in progress' },
+    {
+      value: 'build',
+      name: '👷 build:\tAdd or update regards to build process',
+    },
+    {
+      value: 'ci',
+      name: '💚 ci:\tAdd or update regards to build process',
+    },
+  ],
+
+  scopes: [
+    { name: 'ui' },
+    { name: 'naming' },
+    { name: 'lint' },
+    { name: 'settings' },
+    { name: 'component' },
+    { name: 'storybook' },
+  ],
+
+  allowCustomScopes: true,
+  // skip any questions you want
+  skipQuestions: [],
+  subjectLimit: 100,
+};
---
+++ commitlint.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  rules: {
+    'header-max-length': [0, 'always', 100],
+  },
+};
