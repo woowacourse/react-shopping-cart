60c1230a2361dc9fd6216640da67e4c866edfa71
Yo Wook Kim <uk960214@gmail.com>
2022-05-10 21:05:23 +0900

    refactor: 불필요한 div 제거
    Co-authored-by: juunzzi <juunzzi@users.noreply.github.com>

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -4,16 +4,14 @@ import { ROUTE } from '../route';
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path={ROUTE.home.route} element={<Home />} />
-          <Route path={ROUTE.shoppingCart.route} element={<div>shopping cart</div>} />
-          <Route path={ROUTE.orderList.route} element={<div>order list</div>} />
-          <Route path={ROUTE.productDetail.route} element={<div>product detail</div>} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={ROUTE.home.route} element={<Home />} />
+        <Route path={ROUTE.shoppingCart.route} element={<div>shopping cart</div>} />
+        <Route path={ROUTE.orderList.route} element={<div>order list</div>} />
+        <Route path={ROUTE.productDetail.route} element={<div>product detail</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
