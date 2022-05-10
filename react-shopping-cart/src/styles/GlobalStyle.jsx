d6d13f73b4929201948b7b6917d5b37c9287084b
woose28 <dldntmdsla@naver.com>
2022-05-10 17:20:36 +0900

    feat: global style 적용
    Co-authored-by: ronci <ronci@users.noreply.github.com>

---
+++ react-shopping-cart/src/styles/GlobalStyle.jsx
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+	  padding: 0;
+	  border: 0;
+	  font-size: 100%;
+	  font: inherit;
+	  vertical-align: baseline;
+    box-sizing: border-box;
+  }
+`;
+
+export default GlobalStyle;
