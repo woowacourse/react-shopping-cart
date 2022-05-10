95821d5a3059f3f4e462bd94ee3cf8d31b26f559
Hwang Jung Min <hwangstar156@gmail.com>
2022-05-10 17:01:59 +0900

    feat: ProductCardInfo 스토리 작성및 추가
    Co-authored-by: al-bur <al-bur@users.noreply.github.com>

---
+++ react-shopping-cart/src/component/ProductCardInfo/ProductCardInfo.jsx
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+import ProductName from "../@shared/ProductName/ProductName";
+import ProductPrice from "../@shared/ProductPrice/ProductPrice";
+import { ReactComponent as Cart } from "../../assets/cart.svg";
+
+const StyledCart = styled(Cart)`
+  width: 20px;
+  height: 20px;
+  & path {
+    fill: black;
+  }
+
+  &:hover {
+    & path {
+      fill: ${({ theme }) => theme.colors.cyon};
+    }
+  }
+`;
+
+const ProductCardInfoBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const ProductCardInfoContainer = styled.div`
+  display: flex;
+  cursor: pointer;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+function ProductCardInfo({ name, price }) {
+  return (
+    <ProductCardInfoContainer>
+      <ProductCardInfoBox>
+        <ProductName type="card">{name}</ProductName>
+        <ProductPrice type="card">{price}</ProductPrice>
+      </ProductCardInfoBox>
+      <StyledCart />
+    </ProductCardInfoContainer>
+  );
+}
+
+export default ProductCardInfo;
