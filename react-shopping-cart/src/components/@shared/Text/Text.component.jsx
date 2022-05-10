d8bc128eeadb68d6b152cc499c6f2290c82ba8b4
woose28 <dldntmdsla@naver.com>
2022-05-10 20:40:00 +0900

    refactor: Text 컴포넌트 수정
    - fontSize를 규격화
    - bold 설정 추가
    - storybook 추가
    
    Co-authored-by: ronci <ronci@users.noreply.github.com>

--- react-shopping-cart/src/components/@shared/Text/Text.component.jsx
+++ react-shopping-cart/src/components/@shared/Text/Text.component.jsx
@@ -1,11 +1,38 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Text = styled.div`
   font-style: normal;
-  font-weight: 400;
-  font-size: ${props => `${props.fontSize ?? 16}px`};
   letter-spacing: 0.5px;
   color: #333333;
+  ${({ bold }) =>
+    bold &&
+    css`
+      font-weight: bold;
+    `}
+  ${({ fontSize }) => {
+    switch (fontSize) {
+      case 'small':
+        return css`
+          font-size: 16px;
+        `;
+      case 'medium':
+        return css`
+          font-size: 20px;
+        `;
+      case 'large':
+        return css`
+          font-size: 24px;
+        `;
+      case 'extraLarge':
+        return css`
+          font-size: 32px;
+        `;
+      default:
+        return css`
+          font-size: 20px;
+        `;
+    }
+  }}
 `;
 
 export default Text;
--- react-shopping-cart/src/components/@shared/Text/Text.stories.jsx
+++ react-shopping-cart/src/components/@shared/Text/Text.stories.jsx
@@ -3,12 +3,32 @@ import Text from './Text.component';
 export default {
   title: 'Text',
   component: Text,
+  argTypes: {
+    fontSize: { control: 'select', options: ['small', 'medium', 'large', 'extraLarge'] },
+  },
 };
 
-export const ItemNameText = args => <Text {...args}>PET보틀-정사각(420ml)</Text>;
-ItemNameText.args = {};
+export const SmallFontSizeText = args => <Text {...args}>작은 글씨체</Text>;
+SmallFontSizeText.args = {
+  fontSize: 'small',
+};
 
-export const ItemPriceText = args => <Text {...args}>43,400원</Text>;
-ItemPriceText.args = {
-  fontSize: 20,
+export const MediumFontSizeText = args => <Text {...args}>중간 글씨체</Text>;
+MediumFontSizeText.args = {
+  fontSize: 'medium',
+};
+
+export const LargeFontSizeText = args => <Text {...args}>큰 글씨체</Text>;
+LargeFontSizeText.args = {
+  fontSize: 'large',
+};
+
+export const ExtraLargeFontSizeText = args => <Text {...args}>엄청 큰 글씨체</Text>;
+ExtraLargeFontSizeText.args = {
+  fontSize: 'extraLarge',
+};
+
+export const BoldText = args => <Text {...args}>두꺼운 글씨체</Text>;
+BoldText.args = {
+  bold: true,
 };
