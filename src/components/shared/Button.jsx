a8b9c9a29e694b84ce9e8090d6ea11d4fb5d3a42
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-05-10 17:12:08 +0900

    feat: implement  base button component
    Co-authored-by: jihyeok-um <jihyeok-um@users.noreply.github.com>

---
+++ src/components/shared/Button.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  width: fit-content;
+  text-align: center;
+  padding: 10px;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  color: white;
+`;
+
+const Button = ({ children, ...rest }) => {
+  return <StyledButton {...rest}>{children}</StyledButton>;
+};
+
+export default Button;
