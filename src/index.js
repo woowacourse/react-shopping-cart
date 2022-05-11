e003b0111e6691fd139341f0377937d79327f7ad
kimsanglok <evertree6031@gmail.com>
2022-05-11 16:20:50 +0900

    feat: 브라우저에서 맨 처음 렌더링 시, productList 불러오기
    Co-authored-by: liswktjs <liswktjs@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -3,12 +3,34 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import rootReducer from 'modules';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import { setProductList } from 'modules/productList';
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+const loadProductList = () => {
+  fetch('http://localhost:8080/productList', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((res) => store.dispatch(setProductList(res)));
+};
+
+loadProductList();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
 );
