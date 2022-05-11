980358a69cc0b560574a5fd2be0777c1c9ff0f23
Hwang Jung Min <hwangstar156@gmail.com>
2022-05-11 21:37:56 +0900

    feat: checkBox 스토리 작성및 추가

---
+++ react-shopping-cart/src/component/@shared/CheckBox/CheckBox.jsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+const CheckBox = styled.input`
+  appearance: none;
+  border: 1px solid #2ac1bc;
+  border-radius: 2px;
+  width: 18px;
+  height: 18px;
+  cursor: pointer;
+
+  :focus {
+    outline: none;
+  }
+
+  :checked {
+    background-color: #2ac1bc;
+  }
+
+  :after {
+    content: "✔";
+    width: 100%;
+    height: 100%;
+    font-size: 0.75rem;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+export default CheckBox;
