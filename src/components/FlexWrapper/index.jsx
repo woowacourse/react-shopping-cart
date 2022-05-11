1536a8a38a676f8bf962cedc08cd43a1410bce22
KIM YEJI <rladpwl0512@gmail.com>
2022-05-11 13:39:20 +0900

    :sparkles: feat: flex wrapper 컴포넌트 생성

---
+++ src/components/FlexWrapper/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import FlexWrapperStyled from './style';
+
+function FlexWrapper({ children }) {
+  return <FlexWrapperStyled>{children}</FlexWrapperStyled>;
+}
+
+export default FlexWrapper;
