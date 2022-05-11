c9eea3c9ec9de8f6165562a453ab7ba967017544
JUDONGHYEOK <judonghyeok@gmail.com>
2022-05-11 13:50:54 +0900

    feat: thunk middleware 구현
    Co-authored-by: kkojae91 <jaejeung3210@gmail.com>

---
+++ src/lib/thunk.js
@@ -0,0 +1,12 @@
+export default function createThunkMiddleware(extraArgument) {
+  const middleware =
+    ({ dispatch, getState }) =>
+    (next) =>
+    (action) => {
+      if (typeof action === "function") {
+        return action(dispatch, getState, extraArgument);
+      }
+      return next(action);
+    };
+  return middleware;
+}
