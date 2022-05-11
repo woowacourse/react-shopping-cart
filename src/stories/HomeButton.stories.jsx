efb1a63101432c6b9739521839eb6d1dfa0bc0da
KIM YEJI <rladpwl0512@gmail.com>
2022-05-11 11:11:15 +0900

    :white_check_mark: test: 홈버튼 컴포넌트 storybook 작성

---
+++ src/stories/HomeButton.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import HomeButton from '../containers/HomeButton';
+
+export default {
+  title: 'Container/HomeButton',
+  component: HomeButton,
+};
+
+const Template = () => <HomeButton />;
+
+export const Primary = Template.bind({});
