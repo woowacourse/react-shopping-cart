9f40149b56b1c9865e70256d01b0add69b590265
KIM YEJI <rladpwl0512@gmail.com>
2022-05-11 17:35:42 +0900

    :white_check_mark: test: product 컴포넌트 storybook 작성

---
+++ src/stories/Product.stories.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Product from 'templates/Product';
+
+export default {
+  title: 'Template/Product',
+  component: Product,
+};
+
+const Template = () => <Product />;
+
+export const Primary = Template.bind({});
