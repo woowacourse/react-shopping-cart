7e527c8a026b0ecf2634d4a40b5e4eb71174470f
KIM YEJI <rladpwl0512@gmail.com>
2022-05-11 11:17:07 +0900

    :sparkles: feat: margin right 컴포넌트 생성

---
+++ src/components/MarginRightWrapper/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import MarginRightWrapperStyled from './style';
+
+function MarginRightWrapper({ marginRight, children }) {
+  return <MarginRightWrapperStyled marginRight={marginRight}>{children}</MarginRightWrapperStyled>;
+}
+
+export default MarginRightWrapper;
---
+++ src/components/MarginRightWrapper/style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const MarginRightWrapperStyled = styled.div(
+  ({ marginRight }) => `
+  margin-right: ${marginRight}px;
+`,
+);
+
+export default MarginRightWrapperStyled;
