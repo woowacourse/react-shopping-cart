cffe812b2327ce220264dde85238867d51fd2877
KIM YEJI <rladpwl0512@gmail.com>
2022-05-11 14:07:50 +0900

    :sparkles: feat: header 메뉴 생성

---
+++ src/containers/CartPageButton/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import MarginRightWrapper from 'components/MarginRightWrapper';
+import WhiteButton from 'components/WhiteButton';
+
+function CartPageButton() {
+  return (
+    <MarginRightWrapper>
+      <WhiteButton>장바구니</WhiteButton>
+    </MarginRightWrapper>
+  );
+}
+
+export default CartPageButton;
---
+++ src/containers/OrderListButton/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import WhiteButton from 'components/WhiteButton';
+
+function CartPageButton() {
+  return <WhiteButton>주문목록</WhiteButton>;
+}
+
+export default CartPageButton;
