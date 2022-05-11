718409a4d7dcb836738d5de866870e9dbe29c2be
YUNHO KANG <ansghkdbsgh@gmail.com>
2022-05-12 00:07:16 +0900

    refactor: useSnackbar 훅 분리

---
+++ src/hooks/useSnackBar.ts
@@ -0,0 +1,22 @@
+import { useDispatch } from 'react-redux';
+import { useRef, useCallback } from 'react';
+import { SnackbarActionType } from 'redux/actions/snackbar';
+
+const useSnackBar = () => {
+  const timerRef = useRef(null);
+  const snackbarDispatch = useDispatch();
+  const openSnackbar = useCallback(() => {
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+      timerRef.current = null;
+    }
+    snackbarDispatch({ type: SnackbarActionType.OPEN_SNACKBAR, payload: 'cart' });
+    timerRef.current = setTimeout(() => {
+      snackbarDispatch({ type: SnackbarActionType.CLOSE_SNACKBAR });
+    }, 3000);
+  }, []);
+
+  return { openSnackbar };
+};
+
+export default useSnackBar;
--- src/pages/ItemList.tsx
+++ src/pages/ItemList.tsx
@@ -1,35 +1,22 @@
-import { useEffect, useCallback, useRef } from 'react';
 import { getItemList } from 'redux/action-creators/itemListThunk';
 import ItemContainer from 'components/ItemList/ItemContainer';
 import styled from 'styled-components';
 import { ItemListAction } from 'redux/actions/itemList';
 import { useAppDispatch } from 'hooks/useAppDispatch';
 import useCartList from 'hooks/useCartList';
-import { useDispatch } from 'react-redux';
-import { SnackbarActionType } from 'redux/actions/snackbar';
 import { useAppSelector } from 'hooks/useAppSelector';
+import useSnackBar from 'hooks/useSnackBar';
+import { useEffect } from 'react';
 
 const ItemList = () => {
   const { data: itemList, error, loading } = useAppSelector(state => state.itemListReducer);
   const dispatch = useAppDispatch<ItemListAction>();
-  // @TODO: 에러 처리
+
   const { cartList, updateCartItemQuantity } = useCartList();
-  const timerRef = useRef(null);
-  const snackbarDispatch = useDispatch();
+  const { openSnackbar } = useSnackBar();
 
   useEffect(() => {
     dispatch(getItemList());
-  }, []);
-
-  const openSnackbar = useCallback(() => {
-    if (timerRef.current) {
-      clearTimeout(timerRef.current);
-      timerRef.current = null;
-    }
-    snackbarDispatch({ type: SnackbarActionType.OPEN_SNACKBAR, payload: 'cart' });
-    timerRef.current = setTimeout(() => {
-      snackbarDispatch({ type: SnackbarActionType.CLOSE_SNACKBAR });
-    }, 3000);
   }, []);
 
   return (
