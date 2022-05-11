8d04fab0f7620f64a7ee77e781f4c886a87a7e23
cks3066 <cks3066@naver.com>
2022-05-11 16:01:31 +0900

    refactor: 폴더 구조 변경
    Co-authored-by: Koy <inkyo.dev@gmail.com>

---
+++ src/pages/ProductList/index.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import * as Styled from "./styles";
+
+import Product from "../../components/Product";
+import { loadProductsAPI, ProductState, selectProductState } from "../../redux/modules/products";
+import { AppDispatch } from "../../redux/store";
+
+function ProductList() {
+  const { productList, loading, error }: ProductState = useSelector(selectProductState);
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    dispatch(loadProductsAPI());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (error) {
+      alert(error.message);
+    }
+  }, [error]);
+
+  return (
+    <Styled.ProductsWrapper>
+      {loading
+        ? "로딩 중..."
+        : productList.map((product) => <Product key={product.id} productInfo={product} />)}
+    </Styled.ProductsWrapper>
+  );
+}
+
+export default ProductList;
---
+++ src/pages/ProductList/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const ProductsWrapper = styled.div`
+  display: flex;
+  justify-content: start;
+  flex-wrap: wrap;
+  gap: 20px;
+  padding: 50px 240px;
+`;
+
+export { ProductsWrapper };
