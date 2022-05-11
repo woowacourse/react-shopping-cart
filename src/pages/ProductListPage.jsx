c655437e54642f632a3ae1adac90e6471dc8f09f
intae92 <kangit2013@gmail.com>
2022-05-11 11:34:44 +0900

    feat: item 정보 스타일 추가
    Co-authored-by: Ahn <jin7969@users.noreply.github.com>

--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -2,28 +2,30 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { GiShoppingCart } from 'react-icons/gi';
 import styled from 'styled-components';
+import { COLORS } from '../styles/theme';
 
 function ProductListPage(props) {
   const productList = useSelector((state) => state.productList);
-  console.log(productList);
 
   return (
     <StyledContent>
       <StyledGridContainer>
         {productList.map((product) => {
           const { id, name, price, imageUrl } = product;
-          console.log('product', product);
+
           return (
             <StyledItem key={id}>
               <StyledImageBox>
                 <StyledImg src={imageUrl} />
               </StyledImageBox>
               <StyledItemInfoBox>
-                <div>
-                  <div>{name}</div>
-                  <div>{price}</div>
-                </div>
-                <GiShoppingCart size={25} />
+                <StyledItemInfo>
+                  <StyledItemName>{name}</StyledItemName>
+                  <StyledItemPrice>{Number(price).toLocaleString()} 원</StyledItemPrice>
+                </StyledItemInfo>
+                <StyledIconButton onClick={() => alert('기능 추가중...')}>
+                  <GiShoppingCart size={25} />
+                </StyledIconButton>
               </StyledItemInfoBox>
             </StyledItem>
           );
@@ -50,7 +52,7 @@ const StyledGridContainer = styled.div`
 
 const StyledItem = styled.div`
   width: 250px;
-  height: 320px;
+  height: 330px;
 `;
 
 const StyledImageBox = styled.div`
@@ -69,7 +71,33 @@ const StyledItemInfoBox = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin: 16px 8px 0 8px;
+  margin: 16px 8px 0px 8px;
 `;
 
+const StyledItemInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const StyledItemName = styled.span`
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 22px;
+  letter-spacing: 0.5px;
+`;
+const StyledItemPrice = styled.span`
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 27px;
+  letter-spacing: 0.5px;
+`;
+
+const StyledIconButton = styled.button`
+  border: none;
+  background: none;
+  cursor: pointer;
+  &:hover {
+    transform: scale(1.1);
+    color: ${COLORS.PRIMARY};
+  }
+`;
 export default ProductListPage;
