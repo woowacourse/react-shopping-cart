4d893ea465f61e8b325b81a61bc41034e15f89c6
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-05-11 20:37:53 +0900

    ♻️ : API 통신 결과 상태 상수 분리
    Co-authored-by: Usage <kyr9389@naver.com>

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,11 @@
-// eslint-disable-next-line import/prefer-default-export
 export const ICON_CODE = {
   CARROT: 'f787',
   CART: 'f07a',
 };
+
+export const REQUEST_STATUS = {
+  SUCCESS: Symbol('리퀘스트 성공 결과'),
+  FAIL: Symbol('리퀘스트 실패 결과'),
+};
+
+export const HOST_NAME = 'https://json.takealook.kr';
