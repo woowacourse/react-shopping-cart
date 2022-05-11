bc665629bf523d5b0cc5f8f7ddeb8411e930a813
Yo Wook Kim <uk960214@gmail.com>
2022-05-11 17:18:41 +0900

    refactor: api 이미지 url 형식변경
    Co-authored-by: juunzzi <juunzzi@users.noreply.github.com>

--- src/components/product/ProductCard/ProductCard.jsx
+++ src/components/product/ProductCard/ProductCard.jsx
@@ -1,11 +1,10 @@
 import Image from '../../common/Image/Image';
 import * as Styled from './ProductCard.style';
 import Icon from '../../common/Icon';
-import { API_URL } from '../../../api/constants';
 function ProductCard({ product: { imageURL, name, price } }) {
   return (
     <Styled.Container>
-      <Image src={`${API_URL}${imageURL}`} alt="베이컨이 들어간 프라이팬" />
+      <Image src={imageURL} alt="베이컨이 들어간 프라이팬" />
       <Styled.Content>
         <Styled.Description>
           <Styled.Name>{name}</Styled.Name>
