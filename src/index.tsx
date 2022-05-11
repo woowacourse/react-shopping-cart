e69b72677b80bade4ba24b5567da8b23b53291ac
cks3066 <cks3066@naver.com>
2022-05-11 11:27:39 +0900

    design: 글로벌 스타일 적용
    Co-authored-by: Koy <inkyo.dev@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,20 @@ import App from "./App";
 import store from "./redux/store";
 import { Provider } from "react-redux";
 
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+   body{
+     margin: 0;
+   }
+`;
+
+export default GlobalStyle;
+
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <Provider store={store}>
+    <GlobalStyle />
     <App />
   </Provider>
 );
