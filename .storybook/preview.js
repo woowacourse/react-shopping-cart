0a144b89e2e265bafec0e76859a5c7172a16ddc8
JUDONGHYEOK <judonghyeok@gmail.com>
2022-05-11 15:04:29 +0900

    fix: storybook thunk 적용
    Co-authored-by: kkojae91 <jaejeung3210@gmail.com>

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,11 @@
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import GlobalStyles from "../src/globalStyles";
+import createThunkMiddleware from "../src/lib/thunk";
 import reducer from "../src/modules/products";
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(createThunkMiddleware()));
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
