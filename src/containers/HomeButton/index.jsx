1dccc23ef2e57d2330f23cdf3e9402b5c96b0256
KIM YEJI <rladpwl0512@gmail.com>
2022-05-11 13:44:27 +0900

    :sparkles: feat: 홈버튼 수직 정렬

--- src/containers/HomeButton/index.jsx
+++ src/containers/HomeButton/index.jsx
@@ -3,17 +3,18 @@ import React from 'react';
 import Image from 'components/Image';
 import WhiteText from 'components/WhiteText';
 import MarginRightWrapper from 'components/MarginRightWrapper';
+import FlexAlignCenter from 'components/FlexWrapper/FlexAlignCenter';
 
 function HomeButton() {
   return (
-    <div>
+    <FlexAlignCenter>
       <MarginRightWrapper marginRight="14">
         <Image src="/img/shopping-cart-white.png" width="50" height="44" />
       </MarginRightWrapper>
       <WhiteText fontSize="40" fontWeight="700">
         WOOWA SHOP
       </WhiteText>
-    </div>
+    </FlexAlignCenter>
   );
 }
 
