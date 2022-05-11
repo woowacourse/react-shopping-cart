5f899639cdd2b1ddc1d44c03f6c51e42776fad71
Hwang Jung Min <hwangstar156@gmail.com>
2022-05-11 17:53:40 +0900

    feat: 헤더와 장바구니에 라우팅기능 추가

--- react-shopping-cart/src/App.js
+++ react-shopping-cart/src/App.js
@@ -4,19 +4,31 @@ import { ReactComponent as Cart } from "./assets/cart.svg";
 import { RowFlexWrapper } from "./styles/Wrapper";
 import NavigateButton from "./component/@shared/NavigateButton/NavigateButton";
 import ProductListPage from "./pages/ProductListPage/ProductListPage";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import ProductDetailPage from "./pages/ProductDetailPage/ProductDetailPage";
 
 function App() {
+  const navigate = useNavigate();
+
+  const handleNavigateHome = () => {
+    navigate("/");
+  };
+
+  const handleNavigateShoppingCart = () => {
+    navigate("/shopping-cart");
+  };
+
   return (
     <>
       <Header>
-        <PageTitle>
+        <PageTitle onClick={handleNavigateHome}>
           <Cart />
           <div>WOOWA SHOP</div>
         </PageTitle>
         <RowFlexWrapper gap="20px">
-          <NavigateButton>장바구니</NavigateButton>
+          <NavigateButton onClick={handleNavigateShoppingCart}>
+            장바구니
+          </NavigateButton>
           <NavigateButton>주문목록</NavigateButton>
         </RowFlexWrapper>
       </Header>
@@ -30,6 +42,7 @@ function App() {
           path="/"
         />
         <Route path="/:idx" element={<ProductDetailPage />} />
+        <Route path="/shopping-cart" element={<div>장바구니</div>} />
       </Routes>
     </>
   );
