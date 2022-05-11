005b02b3daff242fe87d3c5cf572ff05ef310128
KIM YEJI <rladpwl0512@gmail.com>
2022-05-11 13:40:10 +0900

    :sparkles: feat: flex align-center 컴포넌트 생성

---
+++ src/components/FlexWrapper/FlexAlignCenter/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import FlexAlignCenterStyled from './style';
+
+function FlexAlignCenter({ children }) {
+  return <FlexAlignCenterStyled>{children}</FlexAlignCenterStyled>;
+}
+
+export default FlexAlignCenter;
