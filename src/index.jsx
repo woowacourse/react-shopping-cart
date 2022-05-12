ccb95f8c4b78d36911a691a1801eb31369dbdf99
intae92 <kangit2013@gmail.com>
2022-05-12 15:02:51 +0900

    chore: stric mode 활성화
    Co-authored-by: Ahn <jin7969@users.noreply.github.com>

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
@@ -7,11 +8,11 @@ import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
