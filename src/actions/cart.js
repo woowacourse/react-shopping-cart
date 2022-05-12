cc1d60884f8a7697bcce3efed4557f11b11de677
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-05-12 15:17:32 +0900

    ♻️ : 장바구니 추가 액션 변수명 수정
    Co-authored-by: Usage <kyr9389@naver.com>

--- src/actions/cart.js
+++ src/actions/cart.js
@@ -1,8 +1,8 @@
 import { CARTS_ACTIONS } from './types';
 
-const addCartList = (products, count = 1) => {
-  const addProduct = { ...products, count, isChecked: true };
-  return { type: CARTS_ACTIONS.ADD_CART_LIST, payload: addProduct };
+const addCartList = (product, count = 1) => {
+  const targetProduct = { ...product, count, isChecked: true };
+  return { type: CARTS_ACTIONS.ADD_CART_LIST, payload: targetProduct };
 };
 
 export { addCartList };
