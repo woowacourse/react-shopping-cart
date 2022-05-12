91fd711773db2103a81f3dfd9ce2fa87f6d6db33
Hwang Jung Min <hwangstar156@gmail.com>
2022-05-12 15:27:02 +0900

    feat: json-server 배포한 url로 변경
    Co-authored-by: al-bur <al-bur@users.noreply.github.com>

--- react-shopping-cart/src/api/index.js
+++ react-shopping-cart/src/api/index.js
@@ -1,5 +1,7 @@
 export const fetchProducts = async () => {
-  const res = await fetch("http://localhost:3002/products");
+  const res = await fetch(
+    "https://jungmin-shopping-cart-server.herokuapp.com/products"
+  );
   if (!res.ok) {
     throw new Error("로드에 실패했습니다");
   }
@@ -7,7 +9,9 @@ export const fetchProducts = async () => {
 };
 
 export const fetchCarts = async () => {
-  const res = await fetch("http://localhost:3002/carts");
+  const res = await fetch(
+    "https://jungmin-shopping-cart-server.herokuapp.com/carts"
+  );
   if (!res.ok) {
     throw new Error("로드에 실패했습니다");
   }
@@ -15,13 +19,16 @@ export const fetchCarts = async () => {
 };
 
 export const addProductToCart = async (product) => {
-  const res = await fetch("http://localhost:3002/carts", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(product),
-  });
+  const res = await fetch(
+    "https://jungmin-shopping-cart-server.herokuapp.com/carts",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product),
+    }
+  );
   if (!res.ok) {
     throw new Error("등록에 실패했습니다");
   }
@@ -29,12 +36,15 @@ export const addProductToCart = async (product) => {
 };
 
 export const deleteProductFromCart = async (id) => {
-  const res = await fetch(`http://localhost:3002/carts/${id}`, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  const res = await fetch(
+    `https://jungmin-shopping-cart-server.herokuapp.com/carts/${id}`,
+    {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
   if (!res.ok) {
     throw new Error("삭제에 실패했습니다");
   }
