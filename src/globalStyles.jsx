97741f2c73ef5d02a39a71596c402b765d4e1911
JUDONGHYEOK <judonghyeok@gmail.com>
2022-05-12 16:39:42 +0900

    refactor: style font 적용
    Co-authored-by: kkojae91 <jaejeung3210@gmail.com>

--- src/globalStyles.jsx
+++ src/globalStyles.jsx
@@ -1,21 +1,11 @@
 import { css, Global } from "@emotion/react";
 
-const baseUrl = process.env.PUBLIC_URL;
 const GlobalStyles = () => (
   <Global
     styles={css`
-      @font-face {
-        font-family: "NotoSansKR", sans-serif;
-        src: url(${baseUrl} + "/assets/fonts/NotoSansKR-Black.otf"),
-          url(${baseUrl} + "/assets/fonts/NotoSansKR-Bold.otf"),
-          url(${baseUrl} + "/assets/fonts/NotoSansKR-Light.otf"),
-          url(${baseUrl} + "/assets/fonts/NotoSansKR-Medium.otf"),
-          url(${baseUrl} + "/assets/fonts/NotoSansKR-Regular.otf"),
-          url(${baseUrl} + "/assets/fonts/NotoSansKR-Thin.otf");
-      }
       * {
         box-sizing: border-box;
-        font-family: NotoSansKR;
+        font-family: "Noto Sans KR", sans-serif;
         margin: 0;
         padding: 0;
       }
@@ -34,6 +24,7 @@ const GlobalStyles = () => (
       button {
         cursor: pointer;
       }
+
       .App {
         padding: 0 10%;
         margin: 140px 0;
