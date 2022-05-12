c19a43dc0026472949103cb1d732d97ea6cb6cc3
gytks4 <gytks4@naver.com>
2022-05-12 17:22:37 +0900

    refactor: useThunkFetch 적용

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,18 +1,14 @@
 import ItemList from 'components/ItemList';
 import Paginator from 'components/common/Paginator';
-import { useAppSelector } from 'hooks/useAppSelector';
 import { ItemListAction } from 'redux/actions/itemList';
-import { useAppDispatch } from 'hooks/useAppDispatch';
 import { getItemList } from 'redux/action-creators/itemListThunk';
-import { useEffect } from 'react';
+import useThunkFetch from 'hooks/useThunkFetch';
 
 const Main = () => {
-  const { data: itemList, error, loading } = useAppSelector(state => state.itemListReducer);
-  const dispatch = useAppDispatch<ItemListAction>();
-
-  useEffect(() => {
-    dispatch(getItemList());
-  }, []);
+  const { data: itemList } = useThunkFetch<ItemListAction>(
+    state => state.itemListReducer,
+    getItemList
+  );
 
   return (
     <>
