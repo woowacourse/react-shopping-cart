e40d9a3a6eac583fa10328af0060a798b2ba0b1c
moonheekim0118 <hellomooneekim@gmail.com>
2022-05-15 02:09:00 +0900

    refactor: apiClient 객체 매번 생성하지 않도록 수정

--- src/utils/apiClient.js
+++ src/utils/apiClient.js
@@ -1,12 +1,9 @@
 import axios from 'axios';
+const { REACT_APP_API_URL } = process.env;
 
-const apiClient = () => {
-  const { REACT_APP_API_URL } = process.env;
-
-  return axios.create({
-    baseURL: REACT_APP_API_URL,
-    responseType: 'json',
-  });
-};
+const apiClient = axios.create({
+  baseURL: REACT_APP_API_URL,
+  responseType: 'json',
+});
 
 export default apiClient;
