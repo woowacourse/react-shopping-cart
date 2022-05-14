4c7468d81f0a503170f482251fa353e45379ea87
kkojae <jaejeung3210@gmail.com>
2022-05-15 00:48:50 +0900

    refactor: uuid 제거후 key값 index로 변경

--- src/components/Items/index.jsx
+++ src/components/Items/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import Item from "../Item";
 import ItemSkeleton from "../ItemSkeleton";
-import { v4 as uuidv4 } from "uuid";
 
 const LOAD_ITEM_AMOUNT = 10;
 
@@ -13,8 +12,8 @@ const Items = ({ products: { data, loading } }) => {
   };
 
   if (loading) {
-    return Array.from({ length: LOAD_ITEM_AMOUNT }).map(() => (
-      <ItemSkeleton key={uuidv4()} />
+    return Array.from({ length: LOAD_ITEM_AMOUNT }).map((_, index) => (
+      <ItemSkeleton key={`skeleton-${index}`} />
     ));
   }
 
