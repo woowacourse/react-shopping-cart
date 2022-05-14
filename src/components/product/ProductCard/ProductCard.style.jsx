f10e974eef0aa3acbda25dcd74a0fd936a2a128f
Yo Wook Kim <uk960214@gmail.com>
2022-05-14 17:38:55 +0900

    feat: 이미지 완전 로딩 전 배경색 추가로 이미지 전환 개선

--- src/components/product/ProductCard/ProductCard.style.jsx
+++ src/components/product/ProductCard/ProductCard.style.jsx
@@ -42,7 +42,6 @@ export const TransparentButton = styled.button`
 `;
 
 export const Placeholder = styled.div`
-  border-radius: 4px;
   width: 100%;
   aspect-ratio: ${({ shape }) => (shape === 'square' ? '1 / 1' : '10 / 1')};
   background-image: linear-gradient(90deg, #e0e0e0 0px, #ededed 30px, #e0e0e0 60px);
