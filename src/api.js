52564ff72820444515fdf8ef60d3ebd8a3a67625
kkojae <jaejeung3210@gmail.com>
2022-05-14 19:35:25 +0900

    refactor: BASE_URL 환경변수로 변경

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://shopping-cart-server2.herokuapp.com";
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 axios.defaults.baseURL = BASE_URL;
 
