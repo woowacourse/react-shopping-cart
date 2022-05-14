c22a7d6165f03583d2b316aa6e174b7799088f85
moonheekim0118 <hellomooneekim@gmail.com>
2022-05-14 16:31:16 +0900

    refactor: globalStyle 분리

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { createGlobalStyle } from 'styled-components';
+import GlobalStyle from 'style/globalStyle';
 import { Provider } from 'react-redux';
 import configureStore from 'store/configureStore';
 
 const store = configureStore();
-
-export const GlobalStyle = createGlobalStyle`
-
-* {
-  box-sizing:border-box;
-  margin: 0;
-  padding: 0;
-}
-`;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
