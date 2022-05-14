492e733cd155b51a2a694e3e20c8d898f8a8ccf0
wonsss <bigdatajang@gmail.com>
2022-05-15 02:31:49 +0900

    fix: 스토리북에 ThemeProvider 데코레이터 추가

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,7 @@
 import { addDecorator } from '@storybook/react';
 import { MemoryRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import ThemeStyle from 'styles/ThemeStyle';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -11,4 +13,8 @@ export const parameters = {
   },
 };
 
-addDecorator((story) => <MemoryRouter>{story()}</MemoryRouter>);
+addDecorator((story) => (
+  <ThemeProvider theme={ThemeStyle.lightTheme}>
+    <MemoryRouter>{story()}</MemoryRouter>{' '}
+  </ThemeProvider>
+));
