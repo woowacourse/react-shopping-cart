4b362adbbe3192ddef44317716f5aba6abc78bf4
moonheekim0118 <hellomooneekim@gmail.com>
2022-05-14 21:44:25 +0900

    refactor: mock데이터 asset 활용

--- src/components/ProductContainer/ProductContainer.stories.js
+++ src/components/ProductContainer/ProductContainer.stories.js
@@ -1,5 +1,6 @@
 import ProductContainer from 'components/ProductContainer/ProductContainer';
 import ProductItem from 'components/ProductItem/ProductItem';
+import productList from 'assets/mock';
 
 export default {
   title: 'components/ProductContainer',
@@ -10,40 +11,7 @@ const Template = (args) => <ProductContainer {...args} />;
 
 export const Example = Template.bind({});
 
-const mockProducts = [
-  {
-    imgUrl:
-      'https://woowacourse.github.io/shopping-cart-css/assets/images/product.png',
-    name: 'PET보틀-정사각(420ml)',
-    price: '48000',
-  },
-  {
-    imgUrl:
-      'https://woowacourse.github.io/shopping-cart-css/assets/images/product.png',
-    name: 'PET보틀-정사각(420ml)',
-    price: '48000',
-  },
-  {
-    imgUrl:
-      'https://woowacourse.github.io/shopping-cart-css/assets/images/product.png',
-    name: 'PET보틀-정사각(420ml)',
-    price: '48000',
-  },
-  {
-    imgUrl:
-      'https://woowacourse.github.io/shopping-cart-css/assets/images/product.png',
-    name: 'PET보틀-정사각(420ml)',
-    price: '48000',
-  },
-  {
-    imgUrl:
-      'https://woowacourse.github.io/shopping-cart-css/assets/images/product.png',
-    name: 'PET보틀-정사각(420ml)',
-    price: '48000',
-  },
-];
-
-const productItems = mockProducts.map((product, index) => (
+const productItems = productList.map((product, index) => (
   <ProductItem
     key={index}
     name={product.name}
