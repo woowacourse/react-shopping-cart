b731b2f3589d99f2e4212aa39c4ef06a3b4911eb
moonheekim0118 <hellomooneekim@gmail.com>
2022-05-14 23:28:22 +0900

    test: storybook ThemeProvider 추가

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,6 @@
 import { addDecorator } from '@storybook/react';
 import { MemoryRouter } from 'react-router-dom';
+import StyleTheme from '../src/style/theme';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -11,4 +12,8 @@ export const parameters = {
   },
 };
 
-addDecorator((story) => <MemoryRouter>{story()}</MemoryRouter>);
+addDecorator((story) => (
+  <MemoryRouter>
+    <StyleTheme>{story()}</StyleTheme>
+  </MemoryRouter>
+));
