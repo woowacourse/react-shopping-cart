15ee229cc8aee98224da6fe4eb677ca148a798a9
Soyi Jeon <soyi.dev@gmail.com>
2022-05-15 21:09:20 +0900

    refactor: 공백 라인 추가

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import GlobalStyle from "./GlobalStyle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <BrowserRouter basename="react-shopping-cart">
