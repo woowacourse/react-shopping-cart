cd7a8f3f0a2611e6cc741ea3996ee444632de305
sangminsong <ssmin940606@gmail.com>
2022-05-15 23:30:35 +0900

    refactor: 페이지네이션 컴포넌트 조금 더 유연한 컴포넌트로 변경

--- react-shopping-cart/src/components/@shared/Pagination/Pagination.stories.jsx
+++ react-shopping-cart/src/components/@shared/Pagination/Pagination.stories.jsx
@@ -8,13 +8,10 @@ export default {
 
 export const DefaultPagination = (args) => <Pagination {...args}></Pagination>;
 
+const paginationList = Array.from({ length: 5 }).map((_, index) => (
+  <PaginationButton key={index + 1}>{index + 1}</PaginationButton>
+));
+
 DefaultPagination.args = {
-  children: [
-    <PaginationButton>1</PaginationButton>,
-    <PaginationButton>2</PaginationButton>,
-    <PaginationButton>3</PaginationButton>,
-    <PaginationButton>4</PaginationButton>,
-    <PaginationButton>5</PaginationButton>,
-    <PaginationButton>6</PaginationButton>,
-  ],
+  children: paginationList,
 };
