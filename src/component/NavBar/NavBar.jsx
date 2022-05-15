42365f4b55dc623a26bb0525de1993f5dab0558a
Daye Lee <508yeah@gmail.com>
2022-05-15 21:03:31 +0900

    style: 내비게이션 바 넓이 수정

--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { Button } from 'component/common';
 
 const StyledNavBar = styled.div`
+  width: 100vw;
   height: 80px;
   display: flex;
   justify-content: center;
