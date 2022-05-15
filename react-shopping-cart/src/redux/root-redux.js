243b7afbca114f3de42d1b8cca33c8e5dd925ea9
sangminsong <ssmin940606@gmail.com>
2022-05-15 15:30:39 +0900

    chore: 사용하지 않는 dependency 제거
    - redux-persist 제거

--- react-shopping-cart/src/redux/root-redux.js
+++ react-shopping-cart/src/redux/root-redux.js
@@ -1,18 +1,10 @@
-import { combineReducers } from "redux";
-import productsReducer from "./products/products.reducer";
-import { persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import cartsReducer from "./carts/carts.reducer";
-
-const persistConfig = {
-  key: "root",
-  storage,
-  whitelist: ["products", "carts"],
-};
+import { combineReducers } from 'redux';
+import productsReducer from './products/products.reducer';
+import cartsReducer from './carts/carts.reducer';
 
 const rootReducer = combineReducers({
   products: productsReducer,
   carts: cartsReducer,
 });
 
-export default persistReducer(persistConfig, rootReducer);
+export default rootReducer;
--- react-shopping-cart/src/redux/store.js
+++ react-shopping-cart/src/redux/store.js
@@ -1,19 +1,14 @@
-import { createStore, applyMiddleware } from "redux";
-import { persistStore } from "redux-persist";
-import logger from "redux-logger";
-import { composeWithDevTools } from "redux-devtools-extension";
-import createSagaMiddleware from "redux-saga";
-import rootSaga from "./root-saga";
-import rootReducer from "./root-redux";
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './root-saga';
+import rootReducer from './root-redux';
 
 const sagaMiddleware = createSagaMiddleware();
 const middlewares = [logger, sagaMiddleware];
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(...middlewares))
-);
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
 sagaMiddleware.run(rootSaga);
-const persistor = persistStore(store);
 
-export { store, persistor };
+export { store };
