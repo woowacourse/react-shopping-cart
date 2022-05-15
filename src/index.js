51216b670f193ef5bc18bac17619077e606a1d01
moonheekim0118 <hellomooneekim@gmail.com>
2022-05-16 00:59:33 +0900

    feat: msw 설정 및 모킹 핸들러 작성 - products list get mocking api 작성 - 개별 product get mocking api 작성

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import App from './App';
 import { Provider } from 'react-redux';
 import configureStore from 'store/configureStore';
 
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = require('./mocks/browser');
+  worker.start();
+}
+
 const store = configureStore();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
---
+++ src/mocks/handlers.js
@@ -0,0 +1,22 @@
+import { rest } from 'msw';
+import { productList } from 'assets/mock';
+
+export const handlers = [
+  rest.get('/product/products/:id', (req, res, ctx) => {
+    const { id } = req.params;
+    return res(
+      ctx.json({
+        id: +id,
+        name: '캐스터네츠 커스텀캣타워H_가드형',
+        imgUrl:
+          'https://image.ohou.se/i/bucketplace-v2-development/uploads/productions/1608536490_103005_1.jpg?gif=1&w=1280&h=1280&c=c',
+        price: '619000',
+        quantity: 10,
+      }),
+    );
+  }),
+
+  rest.get('/products', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(productList));
+  }),
+];
