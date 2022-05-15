5c321d2ab489cf89853ee4d829df7c0fb2300cf6
intae92 <kangit2013@gmail.com>
2022-05-15 17:35:01 +0900

    style: styled imageBox component 크기 상수화

--- src/components/common/index.jsx
+++ src/components/common/index.jsx
@@ -1,13 +1,39 @@
 import styled from 'styled-components';
 
+const SIZE_MAP = {
+  large: 430,
+  middle: 250,
+  small: 100,
+};
+
 const StyledImageBox = styled.div`
-  width: ${(props) => `${props.width === 'large' ? 430 : props.width === 'middle' ? 250 : 100}`}px;
+  width: ${(props) =>
+    `${
+      props.width === 'large'
+        ? SIZE_MAP.large
+        : props.width === 'middle'
+        ? SIZE_MAP.middle
+        : SIZE_MAP.small
+    }`}px;
   height: ${(props) =>
-    `${props.height === 'large' ? 430 : props.height === 'middle' ? 250 : 100}`}px;
+    `${
+      props.height === 'large'
+        ? SIZE_MAP.large
+        : props.height === 'middle'
+        ? SIZE_MAP.middle
+        : SIZE_MAP.small
+    }`}px;
 `;
 
 const StyledImg = styled.img`
-  width: ${(props) => `${props.width === 'large' ? 430 : props.width === 'middle' ? 250 : 100}`}px;
+  width: ${(props) =>
+    `${
+      props.width === 'large'
+        ? SIZE_MAP.large
+        : props.width === 'middle'
+        ? SIZE_MAP.middle
+        : SIZE_MAP.small
+    }`}px;
   height: auto;
   border-radius: 8px;
   object-fit: cover;
