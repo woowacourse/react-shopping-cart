334e26d680d68216a50dabf931745571c691704b
Soyi Jeon <soyi.dev@gmail.com>
2022-05-15 23:29:51 +0900

    feat: 에러 페이지에 홈 버튼 추가

--- src/pages/ErrorPage/index.jsx
+++ src/pages/ErrorPage/index.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { PATH } from "./../../constants/index";
+import BoxButton from "./../../components/common/BoxButton";
+import styled from "styled-components";
 
 function ErrorPage() {
-  return <h2>잘못된 접근입니다.</h2>;
+  return (
+    <div>
+      <h2>잘못된 접근입니다.</h2>
+      <Link to={PATH.ROOT}>
+        <ButtonWrapper>
+          <BoxButton>홈으로</BoxButton>
+        </ButtonWrapper>
+      </Link>
+    </div>
+  );
 }
+
+const ButtonWrapper = styled.div`
+  margin: 60px 20px 0px;
+`;
 
 export default ErrorPage;
