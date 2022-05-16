8244d9fe7cce170522867feb15c6f57d3b653216
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-05-17 00:02:06 +0900

    💄 : Storybook 글로벌 스타일 적용

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,21 @@
+import { Global } from '@emotion/react';
+import GlobalStyles from 'styles/GlobalStyles';
+
+export const decorators = [
+  (Story) => (
+    <>
+      <Global styles={GlobalStyles} />
+      <Story />
+    </>
+  ),
+];
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
