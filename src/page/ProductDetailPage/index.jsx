bc353b93c2c069f17f9e8c1ff3c386de63f32245
jhy979 <jhy979@gmail.com>
2022-05-16 17:44:10 +0900

    refactor: useLocation 대신 useParams 사용 및 useFetch 커스텀훅 생성

--- src/page/ProductDetailPage/index.jsx
+++ src/page/ProductDetailPage/index.jsx
@@ -1,33 +1,50 @@
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-import {useLocation} from 'react-router-dom';
 
 import DetailItem from 'component/DetailItem';
 
 import * as S from 'page/ProductDetailPage/style';
 import useCartItem from 'hook/useCartItem';
+import useFetch from 'constant/useFetch';
+import {useParams} from 'react-router-dom';
+import Loader from 'component/Loader';
+import ErrorBoundary from 'component/ErrorBoundary/ErrorBoundary';
+import NotFoundPage from 'page/NotFoundPage';
 
 export default function ProductDetailPage() {
-  const {
-    state: {itemImgURL, itemName, itemPrice, id, disable},
-  } = useLocation();
-
-  const [disableStatus, setDisableStatus] = useState(disable);
+  const [disableStatus, setDisableStatus] = useState(false);
   const {addCartItem} = useCartItem();
+  const {id} = useParams();
+  const {pending, data, error} = useFetch(`${process.env.REACT_APP_PRODUCT_API_URL}/${id}`);
 
   return (
     <S.DetailItemPageLayout>
-      <DetailItem
-        itemImgURL={itemImgURL}
-        itemName={itemName}
-        itemPrice={itemPrice}
-        id={id}
-        disabled={disableStatus}
-        handleCartButtonClick={() => {
-          addCartItem({itemImgURL, itemName, itemPrice, id, count: 1});
-          setDisableStatus(true);
-        }}
-      />
+      {pending && <Loader />}
+      <ErrorBoundary
+        fallback={<NotFoundPage>해당 상품이 없어요😢</NotFoundPage>}
+        pending={pending}
+        error={error}
+      >
+        {data && (
+          <DetailItem
+            itemImgURL={data.image}
+            itemName={data.name}
+            itemPrice={data.price}
+            id={id}
+            disabled={disableStatus}
+            handleCartButtonClick={() => {
+              addCartItem({
+                itemImgURL: data.image,
+                itemName: data.name,
+                itemPrice: data.price,
+                id,
+                count: 1,
+              });
+              setDisableStatus(true);
+            }}
+          />
+        )}
+      </ErrorBoundary>
     </S.DetailItemPageLayout>
   );
 }
