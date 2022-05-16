ff99719f2cee348b97bd052820c096ded2ad0482
kamwoo <rkadndud06@naver.com>
2022-05-16 21:32:42 +0900

    feat: LoadingSpinner 컴포넌트 작성

--- src/page/ProductList/ProductList.jsx
+++ src/page/ProductList/ProductList.jsx
@@ -4,24 +4,15 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { productsAsyncThunk } from 'store/action/productsActions';
 
-import { GridLayout, Spinner } from 'component/common';
-import { NavBar, Product } from 'component';
+import { GridLayout } from 'component/common';
+import { Product } from 'component';
 
-import { FRUITS } from 'constant';
+import LoadingSpinner from 'component/LoadingSpinner/LoadingSpinner';
 
-const Body = styled.div`
+const Content = styled.div`
   display: flex;
   justify-content: center;
   padding: 60px 0;
-`;
-
-const SpinnerBox = styled.div`
-  width: 300px;
-  display: flex;
-  justify-content: space-between;
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
 `;
 
 function ProductList() {
@@ -33,26 +24,16 @@ function ProductList() {
   }, []);
 
   return (
-    <>
-      <NavBar />
-      <Body>
-        {products.length ? (
-          <GridLayout>
-            {products.map(product => (
-              <Product key={product.id} {...product} />
-            ))}
-          </GridLayout>
-        ) : (
-          <SpinnerBox>
-            {FRUITS.map(fruit => (
-              <Spinner key={fruit}>
-                <span style={{ fontSize: '50px' }}>{fruit}</span>
-              </Spinner>
-            ))}
-          </SpinnerBox>
-        )}
-      </Body>
-    </>
+    <Content>
+      {!products.length && <LoadingSpinner />}
+      {products.length && (
+        <GridLayout>
+          {products.map(product => (
+            <Product key={product.id} {...product} />
+          ))}
+        </GridLayout>
+      )}
+    </Content>
   );
 }
 
