c1da1019b9c2b82221c76f0276ea06542ca0a9dd
jhy979 <jhy979@gmail.com>
2022-05-16 15:35:58 +0900

    refactor: 상품리스트 페이지로 도메인 이동, item 컴포넌트 props 수정

--- src/component/Item/index.jsx
+++ src/component/Item/index.jsx
@@ -1,9 +1,5 @@
-import React, {useState} from 'react';
-import {useDispatch} from 'react-redux';
+import React from 'react';
 import PropTypes from 'prop-types';
-import {useNavigate} from 'react-router-dom';
-
-import {CART} from 'store/modules/cart';
 
 import {ReactComponent as BlackCartIcon} from 'assets/blackCartIcon.svg';
 import Button from 'component/common/Button';
@@ -12,22 +8,15 @@ import {PATH} from 'constant';
 
 import * as S from 'component/Item/style';
 
-export default function Item({itemImgURL, itemName, itemPrice, id, disabled}) {
-  const [disable, setDisable] = useState(disabled);
-
-  const navigation = useNavigate();
-  const dispatch = useDispatch();
-
-  const handleImageClick = () =>
-    navigation(`${PATH.DETAIL}/${id}`, {
-      state: {itemImgURL, itemName, itemPrice, id, disable},
-    });
-
-  const handleCartIconClick = () => {
-    setDisable(true);
-    dispatch({type: CART.ADD, payload: {id, itemImgURL, itemName, itemPrice, count: 1}});
-  };
-
+export default function Item({
+  itemImgURL,
+  itemName,
+  itemPrice,
+  id,
+  disabled,
+  handleImageClick = () => void 0,
+  handleIconClick = () => void 0,
+}) {
   return (
     <S.ItemLayout>
       <img
@@ -44,7 +33,7 @@ export default function Item({itemImgURL, itemName, itemPrice, id, disabled}) {
           </S.ItemNameLink>
           <S.ItemPriceSpan>{itemPrice.toLocaleString()} 원</S.ItemPriceSpan>
         </S.NamePriceBox>
-        <Button disabled={disable} onClick={handleCartIconClick}>
+        <Button disabled={disabled} onClick={handleIconClick}>
           <BlackCartIcon />
         </Button>
       </S.InfoBox>
@@ -58,4 +47,6 @@ Item.propTypes = {
   itemName: PropTypes.string,
   itemPrice: PropTypes.number,
   disabled: PropTypes.bool,
+  handleImageClick: PropTypes.func,
+  handleIconClick: PropTypes.func,
 };
--- src/page/ProductListPage/index.jsx
+++ src/page/ProductListPage/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, {useEffect} from 'react';
 import PropTypes from 'prop-types';
 import {useSelector, useDispatch} from 'react-redux';
@@ -10,17 +11,23 @@ import Loader from 'component/Loader';
 import Empty from 'assets/empty.png';
 
 import * as S from 'page/ProductListPage/style';
+import {useNavigate} from 'react-router-dom';
+import useCartItem from 'hook/useCartItem';
+import {PATH} from 'constant';
 
 export default function ProductListPage() {
   const dispatch = useDispatch();
+  const navigation = useNavigate();
+
   const productList = useSelector((state) => state.productListReducer.productList);
   const pending = useSelector((state) => state.productListReducer.pending);
+  const cart = useSelector((state) => state.cartReducer.cart);
+
+  const {addCartItem} = useCartItem();
 
   useEffect(() => {
     dispatch(getProductList());
   }, []);
-
-  const cart = useSelector((state) => state.cartReducer.cart);
 
   return (
     <S.ProductListPageLayout>
@@ -28,16 +35,39 @@ export default function ProductListPage() {
       {!pending &&
         (productList.length ? (
           <S.ProductListBox>
-            {productList.map(({id, image, name, price}) => (
-              <Item
-                itemImgURL={image}
-                itemName={name}
-                itemPrice={price}
-                id={id}
-                key={id}
-                disabled={cart.some((cartItem) => cartItem.id === id)}
-              />
-            ))}
+            {productList.map(({id, image, name, price}) => {
+              const isIncludeCart = cart.some((cartItem) => cartItem.id === id);
+              return (
+                <Item
+                  itemImgURL={image}
+                  itemName={name}
+                  itemPrice={price}
+                  id={id}
+                  key={id}
+                  disabled={isIncludeCart}
+                  handleIconClick={() => {
+                    addCartItem({
+                      itemImgURL: image,
+                      itemName: name,
+                      itemPrice: price,
+                      id,
+                      count: 1,
+                    });
+                  }}
+                  handleImageClick={() =>
+                    navigation(`${PATH.DETAIL}/${id}`, {
+                      state: {
+                        itemImgURL: image,
+                        itemName: name,
+                        itemPrice: price,
+                        id,
+                        disable: isIncludeCart,
+                      },
+                    })
+                  }
+                />
+              );
+            })}
           </S.ProductListBox>
         ) : (
           <img src={Empty} height="600px" />
