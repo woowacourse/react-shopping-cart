715313b15a779ea3510640ec7c58164b931b121d
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-05-17 02:40:19 +0900

    ✨ : 상품 목록 로드 액션, 페이지 설정 인자 추가

--- src/actions/products.js
+++ src/actions/products.js
@@ -2,26 +2,28 @@ import { requestGetProductList } from 'api/products';
 import { createRequestState } from 'lib/requestUtils';
 import { PRODUCTS_ACTIONS } from './types';
 
-const getProductList = () => async (dispatch) => {
-  await requestGetProductList(1)({
-    PENDING: () =>
-      dispatch({
-        type: PRODUCTS_ACTIONS.UPDATE_PRODUCT_LIST,
-        payload: createRequestState.loading(),
-      }),
+const getProductList =
+  (page = 1) =>
+  async (dispatch) => {
+    await requestGetProductList(page)({
+      PENDING: () =>
+        dispatch({
+          type: PRODUCTS_ACTIONS.UPDATE_PRODUCT_LIST,
+          payload: createRequestState.loading(),
+        }),
 
-    SUCCESS: (result) =>
-      dispatch({
-        type: PRODUCTS_ACTIONS.UPDATE_PRODUCT_LIST_SUCCESS,
-        payload: createRequestState.success(result),
-      }),
+      SUCCESS: (result) =>
+        dispatch({
+          type: PRODUCTS_ACTIONS.UPDATE_PRODUCT_LIST_SUCCESS,
+          payload: createRequestState.success(result),
+        }),
 
-    ERROR: (result) =>
-      dispatch({
-        type: PRODUCTS_ACTIONS.UPDATE_PRODUCT_LIST_ERROR,
-        payload: createRequestState.error(result),
-      }),
-  });
-};
+      ERROR: (result) =>
+        dispatch({
+          type: PRODUCTS_ACTIONS.UPDATE_PRODUCT_LIST_ERROR,
+          payload: createRequestState.error(result),
+        }),
+    });
+  };
 
 export { getProductList };
