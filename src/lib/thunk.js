8b2aea6532d30d16e398a9b4d14d248507d86bc6
JUDONGHYEOK <judonghyeok@gmail.com>
2022-05-17 00:04:19 +0900

    refactor: thunk 출처표시
    Co-authored-by: kkojae91 <jaejeung3210@gmail.com>

--- src/lib/thunk.js
+++ src/lib/thunk.js
@@ -1,3 +1,7 @@
+/*
+ * @see {@link https://github.com/reduxjs/redux-thunk}
+ */
+
 export default function createThunkMiddleware(extraArgument) {
   const middleware =
     ({ dispatch, getState }) =>
