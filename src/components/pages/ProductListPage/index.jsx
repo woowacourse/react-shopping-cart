49db150ad1f1c2ca0097873adb4b14fc7f0ec2f3
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-05-16 22:14:07 +0900

    refactor: redux-thunk 적용
    product list 가져오는 기능

--- src/components/pages/ProductListPage/index.jsx
+++ src/components/pages/ProductListPage/index.jsx
@@ -1,32 +1,21 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { initializeProductList } from "../../../reducers/productList";
-import { useFetch } from "../../../hooks/useFetch";
+import { getProductList } from "../../../reducers/productList";
 
 import Spinner from "../../common/Spinner";
 import ProductCard from "./ProductCard";
 import GridContainer from "../../common/GridContainer";
 
-import { BASE_SERVER_URL, PRODUCT_LIST_PATH } from "../../../constants";
-
-const productListURL = `${BASE_SERVER_URL}${PRODUCT_LIST_PATH}`;
-
 function ProductListPage() {
   const dispatch = useDispatch();
-  const productList = useSelector((state) => state.productList);
-
-  const {
-    isLoading,
-    errorMessage,
-    data: fetchedData,
-  } = useFetch(productListURL, []);
+  const productList = useSelector((state) => state.productList.data);
+  const isLoading = useSelector((state) => state.productList.isLoading);
+  const errorMessage = useSelector((state) => state.productList.errorMessage);
 
   useEffect(() => {
-    if (!isLoading && !errorMessage) {
-      dispatch(initializeProductList({ products: fetchedData }));
-    }
-  }, [isLoading, errorMessage]);
+    dispatch(getProductList());
+  }, []);
 
   if (isLoading) return <Spinner />;
   if (errorMessage)
--- src/reducers/productList.js
+++ src/reducers/productList.js
@@ -1,16 +1,62 @@
+import { BASE_SERVER_URL, PRODUCT_LIST_PATH } from "../constants";
+
 const PRODUCT_LIST_ACTION = {
-  INIT: "productList/INIT",
+  GET_LIST: "productList/GET_LIST",
+  GET_LIST_SUCCESS: "productList/GET_SUCCESS",
+  GET_LIST_ERROR: "productList/GET_ERROR",
 };
 
-export const initializeProductList = (payload) => ({
-  type: PRODUCT_LIST_ACTION.INIT,
-  payload,
-});
+export const getProductList = () => async (dispatch) => {
+  const productListURL = `${BASE_SERVER_URL}${PRODUCT_LIST_PATH}`;
 
-const reducer = (state = [], action) => {
+  dispatch({ type: PRODUCT_LIST_ACTION.GET_LIST });
+  try {
+    const response = await fetch(productListURL);
+
+    if (!response.ok) {
+      throw new Error(`fetch error`);
+    }
+
+    const data = await response.json();
+    if (!data) {
+      throw new Error(`No Data`);
+    }
+
+    dispatch({
+      type: PRODUCT_LIST_ACTION.GET_LIST_SUCCESS,
+      products: data,
+    });
+  } catch (error) {
+    dispatch({ type: PRODUCT_LIST_ACTION.GET_LIST_ERROR, errorMessage: error });
+  }
+};
+
+const initialState = {
+  isLoading: false,
+  data: [],
+  errorMessage: "",
+};
+
+const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case PRODUCT_LIST_ACTION.INIT:
-      return action.payload.products;
+    case PRODUCT_LIST_ACTION.GET_LIST:
+      return {
+        isLoading: true,
+        data: [],
+        errorMessage: "",
+      };
+    case PRODUCT_LIST_ACTION.GET_LIST_SUCCESS:
+      return {
+        isLoading: false,
+        data: action.products,
+        errorMessage: "",
+      };
+    case PRODUCT_LIST_ACTION.GET_LIST_ERROR:
+      return {
+        isLoading: false,
+        data: [],
+        errorMessage: action.errorMessage,
+      };
     default:
       return state;
   }
