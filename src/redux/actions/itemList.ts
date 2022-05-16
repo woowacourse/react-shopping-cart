45f2aa21c75be8887a5478309cdf5dd572fe5a2c
gytks4 <gytks4@naver.com>
2022-05-16 20:50:59 +0900

    style: 상수 사이에 underscore 추가

--- src/redux/actions/itemList.ts
+++ src/redux/actions/itemList.ts
@@ -1,9 +1,9 @@
 import { Item } from 'types/domain';
 
 export const enum ItemListActionType {
-  GET_ITEM_LIST_START = 'items/GET_ITEMLIST_START',
-  GET_ITEM_LIST_SUCCESS = 'items/GET_ITEMLIST_SUCCESS',
-  GET_ITEM_LIST_FAILURE = 'items/GET_ITEMLIST_FAILURE',
+  GET_ITEM_LIST_START = 'items/GET_ITEM_LIST_START',
+  GET_ITEM_LIST_SUCCESS = 'items/GET_ITEM_LIST_SUCCESS',
+  GET_ITEM_LIST_FAILURE = 'items/GET_ITEM_LIST_FAILURE',
 }
 
 interface GetItemListActionStart {
