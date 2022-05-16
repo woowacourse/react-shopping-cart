64702d59074786f38051b036fd70bb3b0f866f57
intae92 <kangit2013@gmail.com>
2022-05-16 14:53:18 +0900

    chore: 배포

--- src/api.js
+++ src/api.js
@@ -1,18 +1,19 @@
 import axios from 'axios';
 
 const DEV_URL = 'http://localhost:4000';
+const PROD_URL = 'https://coke-react-shopping-cart.herokuapp.com';
 
 const getProductList = async () => {
   return await axios({
     method: 'GET',
-    url: `${DEV_URL}/products`,
+    url: `${PROD_URL}/products`,
   });
 };
 
 const getProductItem = async (id) => {
   return await axios({
     method: 'GET',
-    url: `${DEV_URL}/products/${id}`,
+    url: `${PROD_URL}/products/${id}`,
   });
 };
 
