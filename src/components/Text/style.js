6649dda1f96683ed10a5f143aa488eb4a3bfb6e8
(yunjin) <sion11290805@gmail.com>
2022-05-16 18:53:45 +0900

    refactor: Text 컴포넌트 불필요 인자 제거

--- src/components/Text/style.js
+++ src/components/Text/style.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const TextStyled = styled.p(
-  ({ color, fontSize, fontWeight, theme }) => `
+  ({ color, fontSize, fontWeight }) => `
   margin: 0;
   font-size: ${fontSize || '1rem'};
   font-weight: ${fontWeight || 500};
