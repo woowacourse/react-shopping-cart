1bc6d648877d4f18e9207f34eacfbe1af7193330
(yunjin) <sion11290805@gmail.com>
2022-05-16 18:53:19 +0900

    feat: FlexWrapp 컴포넌트 인자 추가

--- src/components/FlexWrapper/style.js
+++ src/components/FlexWrapper/style.js
@@ -1,12 +1,18 @@
 import styled, { css } from 'styled-components';
 
 const FlexWrapperStyled = styled.div`
-  ${({ flexFlow = 'row wrap', justifyContent = 'flex-start', alignItems = 'stretch' }) =>
+  ${({
+    flexFlow = 'row wrap',
+    justifyContent = 'flex-start',
+    alignItems = 'stretch',
+    alignContent = 'flex-start',
+  }) =>
     css`
       display: flex;
       flex-flow: ${flexFlow};
       justify-content: ${justifyContent};
       align-items: ${alignItems};
+      align-content: ${alignContent};
     `}
 `;
 
