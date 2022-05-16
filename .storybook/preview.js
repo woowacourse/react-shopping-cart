4ffcfdd6e59a36d3ae50635eb74f9ae898d753e1
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-05-17 01:05:46 +0900

    💄 : 스토리북 리덕스 적용

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,15 @@
+import { Provider } from 'react-redux';
+import store from 'store';
+
 import { Global } from '@emotion/react';
 import GlobalStyles from 'styles/GlobalStyles';
 
 export const decorators = [
   (Story) => (
-    <>
+    <Provider store={store}>
       <Global styles={GlobalStyles} />
       <Story />
-    </>
+    </Provider>
   ),
 ];
 
