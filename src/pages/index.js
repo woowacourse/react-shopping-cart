ae21a147e78c03def2f6b7ed270d29fa1e553451
Marco <bigdatajang@gmail.com>
2022-05-17 14:43:38 +0900

    refactor: 파일 경로 변경
    파일과 디렉토리의 명칭이 같아서 경로가 중복되던 이슈를 별도의 index파일을 만듦으로써 간소화

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import { lazy } from 'react';
 
-export const ProductList = lazy(() => import('pages/ProductList/ProductList'));
-export const Product = lazy(() => import('pages/Product/Product'));
-export const Cart = lazy(() => import('pages/Cart/Cart'));
-export const NotFound = lazy(() => import('pages/NotFound/NotFound'));
+export const ProductList = lazy(() => import('pages/ProductList'));
+export const Product = lazy(() => import('pages/Product'));
+export const Cart = lazy(() => import('pages/Cart'));
+export const NotFound = lazy(() => import('pages/NotFound'));
