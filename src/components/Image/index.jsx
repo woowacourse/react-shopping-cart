4f3712bba38a8a8fe89f505e319dbe5dcc6862d8
KIM YEJI <rladpwl0512@gmail.com>
2022-05-17 15:51:30 +0900

    :sparkles: feat: 이미지를 못불러 올 경우 fallback 이미지 추가

--- src/components/Image/index.jsx
+++ src/components/Image/index.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ImageStyled from './style';
-
 function Image({ src, width, height, alt }) {
-  return <ImageStyled src={src} width={width} height={height} alt={alt} />;
+  const fallback =
+    'https://www.peregrine-bryant.co.uk/img/uploadsfiles/2018/05/placeholder-test.png';
+  const [imgSrc, setImgSrc] = useState(src);
+  const onError = () => setImgSrc(fallback);
+
+  return (
+    <ImageStyled
+      src={imgSrc ? imgSrc : fallback}
+      onError={onError}
+      width={width}
+      height={height}
+      alt={alt}
+    />
+  );
 }
 
 export default Image;
