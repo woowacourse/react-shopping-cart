4e8b059ef6e1dbc83f5b4f4f4a773719e90de138
YongRae_Kim (Usage) <kyr9389@naver.com>
2022-05-17 22:47:38 +0900

    ♻️ : router 로직 ShoppingCartApp.jsx로 이동

--- src/ShoppingCartApp.jsx
+++ src/ShoppingCartApp.jsx
@@ -1,5 +1,25 @@
 import ProductList from 'pages/ProductList';
 
-const ShoppingCartApp = () => <ProductList />;
+import Snackbar from 'components/Snackbar';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from 'store';
+
+import { Global } from '@emotion/react';
+import GlobalStyles from 'styles/GlobalStyles';
+
+const ShoppingCartApp = () => (
+  <Provider store={store}>
+    <Global styles={GlobalStyles} />
+    <Snackbar />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="*" element={<ProductList />} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>
+);
 
 export default ShoppingCartApp;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import ShoppingCartApp from 'ShoppingCartApp';
-import Snackbar from 'components/Snackbar';
+import { worker } from 'mocks/browser';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from 'store';
-
-import { Global } from '@emotion/react';
-import GlobalStyles from 'styles/GlobalStyles';
+if (process.env.NODE_ENV === 'development') {
+  worker.start();
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-    <Global styles={GlobalStyles} />
-    <Snackbar />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ShoppingCartApp />} />
-        <Route path="*" element={<ShoppingCartApp />} />
-      </Routes>
-    </BrowserRouter>
-  </Provider>,
-);
+root.render(<ShoppingCartApp />);
