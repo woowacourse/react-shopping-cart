b7c92c17a455d50e75de6e6b790318e3707444e8
moonheekim0118 <hellomooneekim@gmail.com>
2022-05-17 15:21:13 +0900

    feat: msw 장바구니 추가 / 삭제 로직 추가

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,30 +1,57 @@
 import { rest } from 'msw';
-import { productList, cartList } from 'assets/mock';
+import { productList } from 'assets/mock';
+
+let cartStorage = [];
 
 export const handlers = [
   rest.get('/products/:id', (req, res, ctx) => {
-    const { id } = req.params;
-    return res(
-      ctx.json({
-        id: +id,
-        name: '캐스터네츠 커스텀캣타워H_가드형',
-        imgUrl:
-          'https://image.ohou.se/i/bucketplace-v2-development/uploads/productions/1608536490_103005_1.jpg?gif=1&w=1280&h=1280&c=c',
-        price: '619000',
-        quantity: 10,
-      }),
-    );
+    const id = +req.params.id;
+
+    const product = productList.find(({ id: productId }) => productId === +id);
+    if (!product) return res(ctx.status(400));
+    return res(ctx.json(product));
   }),
 
   rest.get('/products', (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(productList));
   }),
 
+  rest.delete('/cart/:id', (req, res, ctx) => {
+    const id = +req.params.id;
+    const index = cartStorage.findIndex(
+      ({ id: productId }) => productId === id,
+    );
+
+    if (cartStorage[index].quantity === 1) {
+      cartStorage = cartStorage.filter((_, itemIndex) => index !== itemIndex);
+    } else {
+      cartStorage[index].quantity = cartStorage[index].quantity - 1;
+    }
+    return res(ctx.status(200), ctx.json(cartStorage));
+  }),
+
   rest.post('/cart/:id', (req, res, ctx) => {
-    return res(ctx.status(200));
+    const id = +req.params.id;
+
+    const index = cartStorage.findIndex(
+      ({ id: productId }) => productId === id,
+    );
+    const product = productList.find(({ id: productId }) => productId === id);
+
+    if (index === -1) {
+      cartStorage.push({ ...product, quantity: 1 });
+    } else {
+      const newCartList = cartStorage.map((item, itemIndex) => {
+        if (index === itemIndex)
+          return { ...item, quantity: item.quantity + 1 };
+        return item;
+      });
+      cartStorage = newCartList;
+    }
+    return res(ctx.status(200), ctx.json(cartStorage));
   }),
 
   rest.get('/cart', (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(cartList));
+    return res(ctx.status(200), ctx.json(cartStorage));
   }),
 ];
