4c3ece1896d03c7677fabc5ba9c6fb05bebd6a60
최광희 <polyckh@gmail.com>
2022-05-18 00:54:38 +0900

    refactor: 상품 상세 페이지 컨테이너에서 useFetch 재활용

--- src/components/ProductDetail/ProductDetailContainer.jsx
+++ src/components/ProductDetail/ProductDetailContainer.jsx
@@ -1,45 +1,28 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
+import API_URL from '../../constants/api';
+import PATH from '../../constants/path';
+import useFetch from '../../hooks/useFetch';
 import { loadProduct } from '../../store/product';
 import ProductDetail from './ProductDetail';
-
-const useFetch = (url) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [data, setData] = useState(null);
-
-  const apiCall = async () => {
-    setIsLoading(true);
-
-    try {
-      const result = await axios.get(url);
-      setData(result.data);
-    } catch (err) {
-      setError(err);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    apiCall();
-  }, []);
-
-  return [isLoading, data, error];
-};
 
 function ProductDetailContainer() {
   const dispatch = useDispatch();
   const { id } = useParams();
   const { isLoading, product, error } = useSelector((state) => state.product);
-  const [isCartLoading, cart, cartError] = useFetch(
-    `${process.env.REACT_APP_SERVER_URL}/carts/${id}`
-  );
+  const {
+    isLoading: isCartLoading,
+    result: cart,
+    error: cartError,
+    apiCall: loadCart,
+  } = useFetch({
+    url: `${API_URL}/${PATH.CARTS}/${id}`,
+  });
 
   useEffect(() => {
+    loadCart();
     dispatch(loadProduct(id));
   }, []);
 
