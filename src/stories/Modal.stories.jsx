7b86a8e43234f3241a8f6b0447fa6e9e727311d5
(yunjin) <sion11290805@gmail.com>
2022-05-17 18:43:07 +0900

    test: AlreadyInCart 컴포넌트 stroybook 작성

--- src/stories/Modal.stories.jsx
+++ src/stories/Modal.stories.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Modal from 'components/Modal';
 import AddCartModal from 'containers/AddCartModal';
+import AlreadyCartModal from 'containers/AlreadyInCartModal';
 
 export default {
   title: 'containers/Modal',
@@ -8,5 +9,7 @@ export default {
 };
 
 const AddCartModalTemplate = () => <AddCartModal />;
+const AlreadyCartModalTemplate = () => <AlreadyCartModal />;
 
 export const AddCartModalPrimary = AddCartModalTemplate.bind({});
+export const AlreadyCartModalPrimary = AlreadyCartModalTemplate.bind({});
