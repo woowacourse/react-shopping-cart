e4d70343b0898d8ff5850b349df9b48a92d20b2a
arnold <arnold@arnoldui-MacBookAir.local>
2022-05-17 18:05:24 +0900

    feat: 404 페이지

--- src/Router.js
+++ src/Router.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from 'components/Header';
 
 import ProductsPage from './pages/ProductsPage';
+import NotFoundPage from 'pages/NotFoundPage';
 
 const Router = () => (
   <BrowserRouter>
     <Header />
     <Routes>
       <Route path="/" element={<ProductsPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   </BrowserRouter>
 );
---
+++ src/pages/NotFoundPage/style.js
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+
+export default styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: url(/img/404_error_desktop.jpg);
+
+  div {
+    position: fixed;
+    left: 30px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+
+  div p {
+    margin-bottom: 10px;
+    font-size: 2.4rem;
+    color: ${({ theme }) => theme.black};
+  }
+
+  button {
+    cursor: pointer;
+    padding: 10px;
+    border: none;
+    border-radius: 5px;
+    background-color: ${({ theme }) => theme.green};
+    color: white;
+  }
+
+  @media screen and (max-width: 700px) {
+    background-image: url(/img/404_error_mobile.jpg);
+
+    div {
+      top: unset;
+      bottom: 50px;
+      left: 50%;
+      transform: translateX(-50%);
+    }
+  }
+
+  @media screen and (max-width: 500px) {
+    div p {
+      font-size: 1.8rem;
+    }
+  }
+`;
