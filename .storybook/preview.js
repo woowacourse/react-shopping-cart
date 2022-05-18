b6275e8c3c10c913192fafe2517474e5234b4602
DomMorello <zuzudnf@gmail.com>
2022-05-18 16:44:37 +0900

    fix: storybook global decorators로 MemoryRouter추가 밑 GlobalStyle 추가

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,21 @@
+import { MemoryRouter } from 'react-router-dom';
+import GlobalStyle from '../src/GlobalStyle';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <MemoryRouter>
+      <GlobalStyle />
+      <Story />
+    </MemoryRouter>
+  ),
+];
