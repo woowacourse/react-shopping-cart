627e0fb86725e1c3cdcdea2342bf341aa19726dd
YongRae_Kim (Usage) <kyr9389@naver.com>
2022-05-18 19:57:26 +0900

    ♻️ : useNavigate를 Link로 변경

--- src/components/Layout/Header/index.jsx
+++ src/components/Layout/Header/index.jsx
@@ -1,30 +1,26 @@
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import { 아이콘_코드 } from 'constants/';
 import * as CommonStyled from 'components/@common/CommonStyle/styles';
 import * as Styled from './styles';
 
-const Header = () => {
-  const navigate = useNavigate();
+const Header = () => (
+  <Styled.Container>
+    <CommonStyled.FlexWrapper>
+      <Styled.LeftMenuButton>전체 카테고리</Styled.LeftMenuButton>
+    </CommonStyled.FlexWrapper>
 
-  const handleClickLogo = () => {
-    navigate('/');
-  };
+    <Link to="/">
+      <Styled.Logo />
+    </Link>
 
-  return (
-    <Styled.Container>
-      <CommonStyled.FlexWrapper>
-        <Styled.LeftMenuButton>전체 카테고리</Styled.LeftMenuButton>
-      </CommonStyled.FlexWrapper>
-
-      <Styled.Logo onClick={handleClickLogo} />
-
-      <Styled.RightMenu>
+    <Styled.RightMenu>
+      <Link to="/cart">
         <Styled.RightMenuButton icon={아이콘_코드.CART}>장바구니</Styled.RightMenuButton>
-        <Styled.RightMenuButton icon={아이콘_코드.USER}>주문 목록</Styled.RightMenuButton>
-      </Styled.RightMenu>
-    </Styled.Container>
-  );
-};
+      </Link>
+      <Styled.RightMenuButton icon={아이콘_코드.USER}>주문 목록</Styled.RightMenuButton>
+    </Styled.RightMenu>
+  </Styled.Container>
+);
 
 export default Header;
