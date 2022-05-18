bcd5cad1dc86801e128f46724e075b3b1f59bb43
Soyi Jeon <soyi.dev@gmail.com>
2022-05-18 14:28:19 +0900

    feat: msw mock handler 작성 및 mockData 추가

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,16 +1,24 @@
 import { rest } from "msw";
 
-export const handlers = [
-  rest.post("/login", (req, res, ctx) => {
-    const { username } = req.body;
+import mockData from "./mockData.json";
+import { API_SERVER } from "../constants";
 
+const { BASE_URL, PATH } = API_SERVER;
+
+const productListUrl = `${BASE_URL}${PATH.PRODUCT_LIST}`;
+const productDetailUrl = `${BASE_URL}${PATH.PRODUCT_LIST}/:productId`;
+
+export const handlers = [
+  rest.get(productListUrl, (req, res, ctx) => {
+    return res(ctx.json(mockData.products));
+  }),
+
+  rest.get(productDetailUrl, (req, res, ctx) => {
+    const { productId } = req.params;
     return res(
-      ctx.json({
-        id: "f79e82e8-c34a-4dc7-a49e-9fadc0979fda",
-        username,
-        firstName: "John",
-        lastName: "Maverick",
-      })
+      ctx.json(
+        mockData.products.find((product) => product.id === Number(productId))
+      )
     );
   }),
 ];
