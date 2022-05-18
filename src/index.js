f1960a59c3f8859fd27ce98c6acdc4363861de73
Soyi Jeon <soyi.dev@gmail.com>
2022-05-18 14:04:55 +0900

    feat: msw 초기 세팅

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import GlobalStyle from "./GlobalStyle";
 
+if (process.env.NODE_ENV === "development") {
+  const { worker } = require("./mocks/browser");
+  worker.start();
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
