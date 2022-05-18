e3448d8e9adc344d2c1cbe7dff2c10697501a0c7
arnold <arnold@arnoldui-MacBookAir.local>
2022-05-18 14:30:06 +0900

    style: 상품 개수 추가 삭제 폼

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 
 import Wrapper from './style';
 
 const Product = ({ imgSrc, title, price }) => {
+  const [showQuantity, setShowQuantity] = useState(false);
+
+  const handleClickCartImage = useCallback(() => {
+    setShowQuantity(!showQuantity);
+  }, [showQuantity]);
+
   return (
-    <Wrapper>
+    <Wrapper showQuantity={showQuantity}>
       <img className="thumbnail" src={imgSrc} alt={`${title} 상품`} />
       <div className="bottom flex-row-space-between">
         <div>
           <p className="title">{title}</p>
           <p className="price">{price.toLocaleString()}</p>
         </div>
-        <img src="/img/shopping-cart-black.png" alt="장바구니" />
+        <div className="circle minus">
+          <p>-</p>
+        </div>
+        <div className="circle quantity">
+          <p>1</p>
+        </div>
+        <div className="circle plus">
+          <p>+</p>
+        </div>
+        <div className="img-wrapper" onClick={handleClickCartImage}>
+          <img src="/img/shopping-cart-black.png" alt="장바구니" />
+        </div>
       </div>
     </Wrapper>
   );
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,6 +1,8 @@
 const theme = {
   green: '#2AC1BC',
   black: '#333333',
+  blue: '#00aaff',
+  red: '#ff1252',
   font: 'BMJUA_ttf',
 };
 
