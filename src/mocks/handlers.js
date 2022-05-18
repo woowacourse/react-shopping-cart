9dc6a2d374a795234cc640ec3f76081e62d70a5a
YongRae_Kim (Usage) <kyr9389@naver.com>
2022-05-19 01:06:26 +0900

    🤡 : 전체 상품 요청 mock data 추가 및 적용

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,8 +1,11 @@
 import { rest } from 'msw';
+import { data } from './getProducts';
 
 const HOST_NAME = process.env.REACT_APP_API_URL;
 
 export const handlers = [
+  rest.get(`${HOST_NAME}/products`, (req, res, ctx) => res(ctx.status(200), ctx.json(data))),
+
   rest.get(`${HOST_NAME}/product`, (req, res, ctx) => {
     const productId = req.url.searchParams.get('id');
 
