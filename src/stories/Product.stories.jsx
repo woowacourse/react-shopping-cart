c188dc381d943383c0b048bdd394cb9eb8674b65
(yunjin) <sion11290805@gmail.com>
2022-05-18 16:45:06 +0900

    test: storybook 수정
    상품 스키마 변경으로 인한 수정

--- src/stories/Product.stories.jsx
+++ src/stories/Product.stories.jsx
@@ -11,8 +11,8 @@ const Template = (args) => <Product {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  imgSrc:
+  product_img_src:
     'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201704/10/8a043cc8-818b-4b85-a962-7914b83777de.jpg',
-  title: '사나',
-  price: 300000,
+  product_title: '사나',
+  product_price: 300000,
 };
