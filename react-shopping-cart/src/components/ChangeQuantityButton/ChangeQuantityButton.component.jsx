b752acf5fb60f18d325e9f3ec394dc0fe9c9c45e
2019015414 <2019015414@hanyang.ac.kr>
2022-05-18 17:53:05 +0900

    feat: ChangeQuantityButton 컴포넌트 및 storybook 구현

---
+++ react-shopping-cart/src/components/ChangeQuantityButton/ChangeQuantityButton.component.jsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+import BorderBox from 'components/@shared/BorderBox/BorderBox.component';
+
+const ChangeQuantityButtonContainer = styled.div`
+  display: grid;
+  grid-template-rows: 30px 30px;
+  grid-template-columns: 73px 42px;
+
+  ${BorderBox}:nth-child(1) {
+    grid-row: 1 / span 2;
+  }
+`;
+
+function ChangeQuantityButton({ quantity, onClickAddProduct, onClickReduceProduct }) {
+  return (
+    <ChangeQuantityButtonContainer>
+      <BorderBox textAlign="center" lineHeight="30px" fontSize="24px">
+        {quantity}
+      </BorderBox>
+      <BorderBox textAlign="center" lineHeight="8px" onClick={onClickAddProduct}>
+        ▲
+      </BorderBox>
+      <BorderBox textAlign="center" lineHeight="8px" onClick={onClickReduceProduct}>
+        ▼
+      </BorderBox>
+    </ChangeQuantityButtonContainer>
+  );
+}
+
+export default ChangeQuantityButton;
