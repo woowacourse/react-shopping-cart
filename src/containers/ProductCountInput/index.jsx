f4bdd14394c44a9f2ba80b7b12420688339bf105
(yunjin) <sion11290805@gmail.com>
2022-05-18 20:01:52 +0900

    feat: ProductCountInput 컨테이너 추가

---
+++ src/containers/ProductCountInput/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Input from 'components/Input';
+
+function ProductCountInput() {
+  return (
+    <Input type="text" width="74px" height="60px" border="1px solid black" fontSize="36px"></Input>
+  );
+}
+
+export default ProductCountInput;
--- src/stories/Input.stories.jsx
+++ src/stories/Input.stories.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
 import Input from 'components/Input';
+import Checkbox from 'containers/Checkbox';
+import ProductCountInput from 'containers/ProductCountInput';
 
 export default {
-  title: 'component/Input',
+  title: 'containers/Input',
   component: Input,
 };
 
 const Template = () => <Input />;
+const CheckboxTemplate = () => <Checkbox />;
+const ProductCountInputTemplate = () => <ProductCountInput />;
 
 export const Primary = Template.bind({});
+export const CheckboxPrimary = CheckboxTemplate.bind({});
+export const ProductCountInputPrimary = ProductCountInputTemplate.bind({});
