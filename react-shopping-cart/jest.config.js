f09238db3ea9c4aebd6e3f4a2c78befc09bcd4a0
Hwang Jung Min <hwangstar156@gmail.com>
2022-05-19 00:23:25 +0900

    test: test 설정 추가

--- react-shopping-cart/jest.config.js
+++ react-shopping-cart/jest.config.js
@@ -2,9 +2,10 @@ module.exports = {
   roots: ["<rootDir>"],
   transform: {
     "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.js$": "babel-jest",
   },
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  moduleFileExtensions: ["tsx", "js", "json", "node", "ts"],
+  moduleFileExtensions: ["tsx", "js", "json", "node", "ts", "svg"],
   collectCoverage: true,
   clearMocks: true,
   coverageDirectory: "coverage",
@@ -13,5 +14,6 @@ module.exports = {
   moduleNameMapper: {
     // jest가 절대경로를 인식하기 위함
     "^src/(.*)$": "<rootDir>/src/$1",
+    "^.+\\.svg$": "jest-svg-transformer",
   },
 };
