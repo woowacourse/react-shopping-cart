bb262e7471dc7a359ffdfe64f7e7ff72009530e9
최광희 <polyckh@gmail.com>
2022-05-18 19:14:08 +0900

    feat: cart 페이지 ui 구현

--- src/components/shared/basics.jsx
+++ src/components/shared/basics.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const BasicButton = styled.button`
   background: transparent;
@@ -24,4 +24,34 @@ const basicImageSize = {
 export const BasicImage = styled.img`
   width: ${({ size }) => basicImageSize[size]?.width ?? 282}px;
   height: ${({ size }) => basicImageSize[size]?.height ?? 282}px;
+`;
+
+export const Flex = styled.div`
+  ${({
+    direction = 'row',
+    wrap = 'nowrap',
+    justify = 'normal',
+    align = 'normal',
+    gap = '0',
+  }) => css`
+    display: flex;
+    flex-direction: ${direction};
+    flex-wrap: ${wrap};
+    justify-content: ${justify};
+    align-items: ${align};
+    gap: ${gap};
+  `}
+`;
+
+const DIVIDE_LINE_WEIGHT = {
+  bold: 2,
+  thin: 1,
+};
+
+export const BasicDivideLine = styled.hr`
+  ${({ weight = 'bold', mv = 0, color = 'black' }) => css`
+    border: ${DIVIDE_LINE_WEIGHT[weight]}px solid ${color};
+    margin: ${mv}px 0;
+  `}
+  width: 100%;
 `;
