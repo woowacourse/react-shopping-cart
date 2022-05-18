223cca9d18568cffd4ca482e335478e4222f9395
(yunjin) <sion11290805@gmail.com>
2022-05-18 15:22:52 +0900

    test: 스토리북 추가
    장바구니 담기 실패 모달 추가

--- src/stories/Modal.stories.jsx
+++ src/stories/Modal.stories.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+
 import Modal from 'components/Modal';
 import AddCartModal from 'containers/AddCartModal';
 import AlreadyCartModal from 'containers/AlreadyInCartModal';
+import AddCartErrorModal from 'containers/AddCartErrorModal';
 
 export default {
   title: 'containers/Modal',
@@ -10,6 +12,8 @@ export default {
 
 const AddCartModalTemplate = () => <AddCartModal />;
 const AlreadyCartModalTemplate = () => <AlreadyCartModal />;
+const AddCartErrorModalTemplate = () => <AddCartErrorModal />;
 
 export const AddCartModalPrimary = AddCartModalTemplate.bind({});
 export const AlreadyCartModalPrimary = AlreadyCartModalTemplate.bind({});
+export const AddCartErrorModalPrimary = AddCartErrorModalTemplate.bind({});
