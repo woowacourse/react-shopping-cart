a607b76a9f801a529be3ab60efc2cf10e42acf53
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-05-19 10:39:06 +0900

    ♻️ : 비동기 처리 컴포넌트 적용

--- src/pages/ProductList/index.jsx
+++ src/pages/ProductList/index.jsx
@@ -3,9 +3,9 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getProductList } from 'actions/products';
 import { addCartList } from 'actions/cart';
 
+import { SwitchAsync, Case } from 'components/@common/SwitchAsync';
 import StatusMessage from 'components/@common/StatusMessage';
 import ProductItem from 'components/ProductItem';
-import useAsyncContainer from 'hooks/useAsyncContainer';
 
 import * as Styled from './styles';
 
@@ -22,38 +22,41 @@ export function ProductList() {
     error: errorMessage,
   } = useSelector((state) => state.products.products);
 
-  const AsyncContainer = useAsyncContainer({
-    isLoading,
-    isError: !!errorMessage,
-    isContentLoaded: productList.length > 0,
-  });
-
   const handleAddCart = ({ id, image, name, price }) => {
     dispatch(addCartList({ id, image, name, price }));
     alert(`${name}가 장바구니에 추가되었습니다 🧺`);
   };
 
   return (
-    <AsyncContainer
-      loadingFallback={
-        <StatusMessage status="loading">상품 목록을 불러오고 있습니다.</StatusMessage>
-      }
-      errorFallback={<StatusMessage status="error">{errorMessage}</StatusMessage>}
+    <SwitchAsync
+      isLoading={isLoading}
+      isError={!!errorMessage}
+      isContentLoaded={productList.length > 0}
     >
-      <Styled.ProductListWrapper>
-        {productList &&
-          productList.map(({ id, name, goodsPrice, listImage }) => (
-            <ProductItem
-              key={id}
-              id={id}
-              image={listImage}
-              name={name}
-              price={goodsPrice}
-              onClick={handleAddCart}
-            />
-          ))}
-      </Styled.ProductListWrapper>
-    </AsyncContainer>
+      <Case.Success>
+        <Styled.ProductListWrapper>
+          {productList &&
+            productList.map(({ id, name, goodsPrice, listImage }) => (
+              <ProductItem
+                key={id}
+                id={id}
+                image={listImage}
+                name={name}
+                price={goodsPrice}
+                onClick={handleAddCart}
+              />
+            ))}
+        </Styled.ProductListWrapper>
+      </Case.Success>
+
+      <Case.Loading>
+        <StatusMessage status="loading">상품 목록을 불러오고 있습니다.</StatusMessage>
+      </Case.Loading>
+
+      <Case.Error>
+        <StatusMessage status="error">{errorMessage}</StatusMessage>
+      </Case.Error>
+    </SwitchAsync>
   );
 }
 export default ProductList;
