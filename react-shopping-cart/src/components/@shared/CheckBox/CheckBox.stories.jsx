2c657c91bda6f29ba6a67db7f772cf8650b3ac0e
2019015414 <2019015414@hanyang.ac.kr>
2022-05-19 10:56:39 +0900

    feat: CheckBox 컴포넌트 및 storybook 구현

---
+++ react-shopping-cart/src/components/@shared/CheckBox/CheckBox.stories.jsx
@@ -0,0 +1,9 @@
+import CheckBox from './CheckBox.component';
+
+export default {
+  title: 'Shared/CheckBox',
+  component: CheckBox,
+};
+
+export const Default = args => <CheckBox />;
+Default.args = {};
