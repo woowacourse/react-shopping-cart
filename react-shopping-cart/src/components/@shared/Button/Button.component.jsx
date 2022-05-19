e2ca013f0853fb51674898f02f55a5969f8c89b7
2019015414 <2019015414@hanyang.ac.kr>
2022-05-19 17:37:40 +0900

    feat: Button 컴포넌트 및 storybook 구현

---
+++ react-shopping-cart/src/components/@shared/Button/Button.component.jsx
@@ -0,0 +1,19 @@
+import styled, { css } from 'styled-components';
+
+const Button = styled.button`
+  background: ${({ theme }) => theme.colors['MINT_001']};
+  color: ${({ theme }) => theme.colors['WHITE_001']};
+  font-size: 24px;
+  padding: 20px;
+  text-align: center;
+  cursor: pointer;
+
+  ${({ width, height, mt }) =>
+    css`
+      width: ${width};
+      height: ${height};
+      margin-top: ${mt};
+    `}
+`;
+
+export default Button;
