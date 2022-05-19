b1036c611e9f298d969fc660d5b927c840756f51
YUNHO KANG <ansghkdbsgh@gmail.com>
2022-05-19 17:37:10 +0900

    feat: 장바구니페이지 상품 리스트 틀 구현

--- src/components/Cart/CartList.tsx
+++ src/components/Cart/CartList.tsx
@@ -1,29 +1,158 @@
-import React from 'react';
-import { CartItem } from 'types/domain';
-import Loading from 'components/common/Loading';
-import RequestFail from 'components/common/RequestFail';
-import useThunkFetch from 'hooks/useThunkFetch';
-import { getCartList } from 'redux/action-creators/cartListThunk';
-const CartList = () => {
-  const {
-    data: cartList,
-    error,
-    loading,
-  } = useThunkFetch(state => state.cartListReducer, getCartList);
+import React, { Fragment } from 'react';
+import { ReactComponent as TrashCanIcon } from 'assets/trashCanIcon.svg';
+import { CartItem, Item } from 'types/domain';
+import styled from 'styled-components';
+import CroppedImage from 'components/common/CroppedImage';
+import { flexCenter } from 'styles/mixin';
 
-  if (loading) return <Loading />;
-  if (error) return <RequestFail />;
+const CartList = ({ cartList, cartDetail }: { cartList: CartItem[]; cartDetail: Item[] }) => {
+  console.log('f', cartList, cartDetail);
 
   return (
-    <div>
-      {cartList.map((item: CartItem) => (
-        <React.Fragment key={item.id}>
-          <div>id는 {item.id}</div>
-          <div>quantity는 {item.quantity}</div>
-        </React.Fragment>
-      ))}
-    </div>
+    <StyledRoot>
+      <StyledHeader>든든상품배송 {`${cartList.length}`} 개</StyledHeader>
+      <StyledList>
+        {cartList.map(cartItem => {
+          const id = cartItem.id;
+          const quantity = cartItem.quantity;
+          const detail = cartDetail.filter(item => item.id === id)[0];
+
+          return (
+            <StyledListItem key={cartItem.id}>
+              <input type='checkbox' />
+              <CroppedImage src={detail.thumbnailUrl} width='150px' height='144px' alt='상품' />
+              <StyledItemTitle>{detail.title}</StyledItemTitle>
+              <StyledRight>
+                <TrashCan></TrashCan>
+                <QuantityController>
+                  <QuantityPlace>
+                    <div>{cartItem.quantity}</div>
+                  </QuantityPlace>
+                  <IncreaseButton>▲</IncreaseButton>
+                  <DecreaseButton>▼</DecreaseButton>
+                </QuantityController>
+                <TotalPrice>
+                  {(cartItem.quantity * detail.price)
+                    .toString()
+                    .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',')}{' '}
+                  원
+                </TotalPrice>
+              </StyledRight>
+            </StyledListItem>
+          );
+        })}
+      </StyledList>
+    </StyledRoot>
   );
 };
+
+const StyledRoot = styled.div`
+  display: inline-block;
+  width: 736px;
+  height: 724px;
+`;
+
+const StyledHeader = styled.p`
+  padding: 5px 0px;
+
+  border-bottom: solid silver 4px;
+  font-size: 20px;
+`;
+
+const StyledList = styled.div`
+  width: 736px;
+  height: 700px;
+  overflow: auto;
+
+  padding-right: 10px;
+
+  &::-webkit-scrollbar {
+    width: 6px;
+    background-color: inherit;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: inherit;
+  }
+  &::-webkit-scrollbar-track {
+    background-color: inherit;
+  }
+`;
+
+const StyledListItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+
+  border-bottom: solid silver 1.5px;
+  padding: 20px 0;
+`;
+
+const StyledItemTitle = styled.div`
+  width: 260px;
+  padding: 0px 20px;
+  font-family: 'Noto Sans KR';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+`;
+
+const StyledRight = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  gap: 20px;
+  align-items: flex-end;
+  width: 200px;
+`;
+
+const TrashCan = styled(TrashCanIcon)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`;
+
+const QuantityController = styled.div`
+  display: grid;
+  grid-template-areas:
+    ' qp ib'
+    'qp db';
+
+  border: solid grey 1px;
+`;
+
+const QuantityPlace = styled.div`
+  ${flexCenter}
+
+  font-size: 24px;
+  grid-area: qp;
+  width: 73px;
+  height: 60px;
+  border: solid grey 1px;
+`;
+
+const IncreaseButton = styled.button`
+  ${flexCenter}
+
+  grid-area: ib;
+  width: 42px;
+  height: 30px;
+  border: solid grey 1px;
+  cursor: pointer;
+`;
+
+const DecreaseButton = styled.button`
+  ${flexCenter}
+
+  grid-area: db;
+  width: 42px;
+  height: 30px;
+  border: solid grey 1px;
+  cursor: pointer;
+`;
+
+const TotalPrice = styled.p`
+  font-size: 16px;
+`;
 
 export default CartList;
--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,9 +1,43 @@
 import CartList from 'components/Cart/CartList';
+import Loading from 'components/common/Loading';
+import RequestFail from 'components/common/RequestFail';
+import { useEffect } from 'react';
+import { useAppDispatch } from 'hooks/useAppDispatch';
+import { useAppSelector } from 'hooks/useAppSelector';
+import { getCartList } from 'redux/action-creators/cartListThunk';
+import { CartListAction } from 'redux/actions/cartList';
+import { LOCAL_BASE_URL } from 'apis';
+import { useFetch } from 'hooks/useFetch';
+import { Item } from 'types/domain';
 
 const Cart = () => {
+  const { data: cartList, error, loading } = useAppSelector(state => state.cartListReducer);
+  const dispatch = useAppDispatch<CartListAction>();
+
+  const cartListString =
+    `${LOCAL_BASE_URL}/itemList?` +
+    cartList
+      .map(item => {
+        return `id=${item.id}`;
+      })
+      .join('&');
+
+  const {
+    data: cartDetail,
+    loading: loading2,
+    error: error2,
+  }: { data: Item[]; loading: boolean; error: string } = useFetch(cartListString);
+
+  useEffect(() => {
+    dispatch(getCartList());
+  }, []);
+
+  if (loading || loading2) return <Loading />;
+  if (error || error2) return <RequestFail />;
+
   return (
     <div>
-      <CartList />
+      <CartList cartList={cartList} cartDetail={cartDetail} />
     </div>
   );
 };
