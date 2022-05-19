d5a59614b6343c4ca9d1b08e7ea297c2978ca3c1
kamwoo <rkadndud06@naver.com>
2022-05-19 13:17:01 +0900

    fix: 상세 페이지 이동 오류 수정

--- src/constant/path.js
+++ src/constant/path.js
@@ -1,7 +1,7 @@
 const PATH = {
-  PRODUCT_LIST: 'ProductList',
-  SHOPPING_CART: 'ShoppingCart',
-  PRODUCT_DETAIL: 'ProductDetail',
+  PRODUCT_LIST: '/ProductList',
+  SHOPPING_CART: '/ShoppingCart',
+  PRODUCT_DETAIL: '/ProductDetail',
 };
 
 export { PATH };
