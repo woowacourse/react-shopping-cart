21f47515bc73d0bd8e5917f7d020ff259ea17460
Ahn <tn3748@naver.com>
2022-05-19 15:48:15 +0900

    refactor: json-server 삭제

--- src/index.jsx
+++ src/index.jsx
@@ -24,7 +24,7 @@ async function main() {
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
     <React.StrictMode>
-      <BrowserRouter basename="/react-shopping-cart">
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Provider store={store}>
           <App />
         </Provider>
