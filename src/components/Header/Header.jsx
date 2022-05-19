4d7dfd1b06cc664df71fb9979f19e8ff00f7ddd6
moonheekim0118 <hellomooneekim@gmail.com>
2022-05-19 21:10:46 +0900

    feat: 헤더 메뉴 누르면 이동하도록 구현

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,15 @@
 import Styled from './style';
 import MenuItem from 'components/MenuItem/MenuItem';
 import bigCart from 'assets/svg/bigCart.svg';
+import { PATH_NAME } from 'constants';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleClickCartMenu = () => {
+    navigate(PATH_NAME.CART);
+  };
   return (
     <Styled.Wrapper>
       <Styled.Logo to="/">
@@ -10,7 +17,7 @@ const Header = () => {
         <Styled.LogoText>댕냥상회</Styled.LogoText>
       </Styled.Logo>
       <Styled.MenuContainer>
-        <MenuItem>장바구니</MenuItem>
+        <MenuItem onClick={handleClickCartMenu}>장바구니</MenuItem>
         <MenuItem>주문목록</MenuItem>
       </Styled.MenuContainer>
     </Styled.Wrapper>
--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -1,12 +1,13 @@
 import Styled from './style';
 import PropTypes from 'prop-types';
 
-const MenuItem = ({ children }) => {
-  return <Styled.Wrapper>{children}</Styled.Wrapper>;
+const MenuItem = ({ children, onClick }) => {
+  return <Styled.Wrapper onClick={onClick}>{children}</Styled.Wrapper>;
 };
 
 MenuItem.propTypes = {
   children: PropTypes.string,
+  onClick: PropTypes.func,
 };
 
 export default MenuItem;
