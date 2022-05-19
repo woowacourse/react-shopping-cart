18268122a8fbeef668526960fe5c29ebade4ac9c
2019015414 <2019015414@hanyang.ac.kr>
2022-05-19 14:50:54 +0900

    feat: redux action 추가

--- react-shopping-cart/src/redux/actions/index.js
+++ react-shopping-cart/src/redux/actions/index.js
@@ -1,7 +1,9 @@
 export const ADD_ITEM = 'ADD_ITEM';
-
 export const DELETE_ITEM = 'DELETE_ITEM';
+export const INCREASE_QUANTITY = 'INCREASE_QUANTITY';
+export const DECREASE_QUANTITY = 'DECREASE_QUANTITY';
 
 export const addItem = id => ({ type: ADD_ITEM, payload: { id } });
-
 export const deleteItem = id => ({ type: DELETE_ITEM, payload: { id } });
+export const increaseQuantity = id => ({ type: INCREASE_QUANTITY, payload: { id } });
+export const decreaseQuantity = id => ({ type: DECREASE_QUANTITY, payload: { id } });
--- react-shopping-cart/src/redux/reducers/shoppingCart.js
+++ react-shopping-cart/src/redux/reducers/shoppingCart.js
@@ -1,4 +1,4 @@
-import { ADD_ITEM, DELETE_ITEM } from 'redux/actions';
+import { ADD_ITEM, DELETE_ITEM, INCREASE_QUANTITY, DECREASE_QUANTITY } from 'redux/actions';
 
 const initialState = [];
 
@@ -10,6 +10,26 @@ function shoppingCart(state = initialState, action) {
     case DELETE_ITEM:
       return state.filter(product => product.id !== action.payload.id);
 
+    case INCREASE_QUANTITY: {
+      const newState = [...state];
+
+      const currentState = newState.find(product => product.id === Number(action.payload.id));
+      const index = newState.findIndex(product => product.id === Number(action.payload.id));
+      newState[index] = { ...currentState, quantity: currentState['quantity'] + 1 };
+
+      return newState;
+    }
+
+    case DECREASE_QUANTITY: {
+      const newState = [...state];
+
+      const currentState = newState.find(product => product.id === Number(action.payload.id));
+      const index = newState.findIndex(product => product.id === Number(action.payload.id));
+      const currentQuantity = currentState['quantity'] === 0 ? 0 : currentState['quantity'] - 1;
+      newState[index] = { ...currentState, quantity: currentQuantity };
+
+      return newState;
+    }
     default:
       return state;
   }
