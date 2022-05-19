c4a347bc2bdab2939cd553d48f572176c62bb94b
wonsss <bigdatajang@gmail.com>
2022-05-20 01:38:40 +0900

    feat: 수량 변경 버튼의 화살표 버튼을 누르면 input value 가 변경된다.
    input element의 stepUp, stepDown 메서드를 사용한다.
    number타입의 input의 화살표를 css로 없앤다.

--- src/components/QuantityInput/QuantityInput.jsx
+++ src/components/QuantityInput/QuantityInput.jsx
@@ -1,13 +1,39 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import { useRef, useState } from 'react';
 
 const QuantityInput = ({ cartQuantity }) => {
+  const numberInput = useRef(null);
+  const [quantity, setQuantity] = useState(cartQuantity);
+  const handleChangeInput = (e) => {
+    setQuantity(e.target.value);
+  };
+
+  const handleClickStepUp = () => {
+    numberInput.current.stepUp(1);
+  };
+  const handleClickStepDown = () => {
+    numberInput.current.stepDown(1);
+  };
+
   return (
     <Styled.InputWrapper>
-      <Styled.NumberInput value={cartQuantity} />
+      <Styled.NumberInput
+        type="number"
+        step="1"
+        min="1"
+        max="100"
+        ref={numberInput}
+        value={quantity}
+        onChange={handleChangeInput}
+      />
       <div>
-        <Styled.NumberInputButton>▲</Styled.NumberInputButton>
-        <Styled.NumberInputButton>▼</Styled.NumberInputButton>
+        <Styled.NumberInputButton onClick={handleClickStepUp}>
+          ▲
+        </Styled.NumberInputButton>
+        <Styled.NumberInputButton onClick={handleClickStepDown}>
+          ▼
+        </Styled.NumberInputButton>
       </div>
     </Styled.InputWrapper>
   );
@@ -29,6 +55,11 @@ const Styled = {
     border: 1px solid #ddd;
     text-align: center;
     font-size: 24px;
+    &::-webkit-outer-spin-button,
+    &::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
   `,
   NumberInputButton: styled.button`
     display: flex;
