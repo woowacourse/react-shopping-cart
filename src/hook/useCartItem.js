c900ae3f3fae2dd6602a226a32a71ace31f7cff5
jhy979 <jhy979@gmail.com>
2022-05-19 16:25:58 +0900

    fix: 장바구니에서 삭제 시 체크박스 클릭 정보도 같이 삭제

--- src/hook/useCartItem.js
+++ src/hook/useCartItem.js
@@ -1,15 +1,16 @@
 import {useDispatch} from 'react-redux';
 import {useNavigate} from 'react-router-dom';
 import {CART} from 'store/modules/cart';
+import {SELECTED_ITEM} from 'store/modules/selectedItem';
 
 export default function useCartItem(path = null) {
   const dispatch = useDispatch();
   const navigation = useNavigate();
-
   const deleteCartItem = (payload) => {
     const deleteConfirm = window.confirm('장바구니에서 삭제하시겠습니까?');
     if (deleteConfirm) {
       dispatch({type: CART.DELETE, payload});
+      dispatch({type: SELECTED_ITEM.DELETE, payload});
       return;
     }
 
