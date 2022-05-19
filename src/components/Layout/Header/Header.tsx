c9c4bf34af56b440e3da15622df0849a25ec5c3a
이아현 <lah1203@naver.com>
2022-05-19 13:36:42 +0900

    feat: 헤더 장바구니 버튼에 배지 추가

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,17 +1,26 @@
+import { CartStoreState } from 'types';
 import Link from 'components/@shared/Link';
 import Logo from 'components/Logo/Logo';
 import PATH from 'constants/path';
 import RightMenu from './RightMenu';
 import styled from 'styled-components';
+import { useSelector } from 'react-redux';
 
 function Header() {
+  const cart = useSelector(
+    (state: { cart: CartStoreState }) => state.cart.cart
+  );
+
   return (
     <StyledHeader>
       <Link to="/">
         <Logo />
       </Link>
       <RightMenu>
-        <Link to={PATH.CART}>장바구니</Link>
+        <Link to={PATH.CART}>
+          장바구니
+          <Badge>{cart.length}</Badge>
+        </Link>
         <Link to={PATH.BASE}>주문목록</Link>
       </RightMenu>
     </StyledHeader>
@@ -32,6 +41,24 @@ const StyledHeader = styled.div`
 
   background: ${({ theme: { colors } }) => colors.redPink};
   color: ${({ theme: { colors } }) => colors.white};
+`;
+
+const Badge = styled.div`
+  display: inline-block;
+  position: absolute;
+  top: 10px;
+  text-align: center;
+
+  width: 15px;
+  height: 15px;
+  border: 0.5px solid ${({ theme: { colors } }) => colors.white};
+  border-radius: 50%;
+
+  background: ${({ theme: { colors } }) => colors.pink};
+  color: ${({ theme: { colors } }) => colors.black};
+
+  font-size: 14px;
+  font-weight: normal !important;
 `;
 
 export default Header;
