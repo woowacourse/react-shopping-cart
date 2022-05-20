8b995ea80e3f586c5643eff71efe30320a7eb596
junzzi <jjh01027717502@gmail.com>
2022-05-20 14:52:59 +0900

    style: theme 설정

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -1,14 +1,12 @@
-export const size = {
-  mobile: 480,
-  tablet: 800,
-  desktop: 1024,
-};
+import { DefaultTheme } from 'styled-components';
+export const enum size {
+  mobile = 480,
+  tablet = 800,
+  desktop = 1024,
+}
 
-export default {
-  mainColor: '#141852',
-  buttonDefault: '#73675C',
-
-  textColorWhite: '#ffffff',
+const theme: DefaultTheme = {
+  brandColor_1: '#141852',
 
   mobile: `@media (max-width: ${size.mobile}px)`,
   tablet: `@media (max-width: ${size.tablet}px)`,
@@ -16,4 +14,15 @@ export default {
 
   innerWidth: '1100px',
   minWidth: '300px',
+
+  blackColor_1: '#333333',
+  blackColor_2: '#00000099',
+
+  greyColor_1: '#dddddd',
+
+  brownColor_1: '#73675C',
+
+  whiteColor_1: '#ffffff',
 };
+
+export default theme;
---
+++ src/styles/styled.d.ts
@@ -0,0 +1,23 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    brandColor_1: string;
+
+    mobile: string;
+    tablet: string;
+    desktop: string;
+
+    innerWidth: string;
+    minWidth: string;
+
+    blackColor_1: string;
+    blackColor_2: string;
+
+    greyColor_1: string;
+
+    brownColor_1: string;
+
+    whiteColor_1: string;
+  }
+}
