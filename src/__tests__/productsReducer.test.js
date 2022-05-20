1ff46270173a1dc8e10b94eceda18e97a33aa20a
moonheekim0118 <hellomooneekim@gmail.com>
2022-05-20 14:47:08 +0900

    fix: 오타 수정

--- src/__tests__/productsReducer.test.js
+++ src/__tests__/productsReducer.test.js
@@ -4,8 +4,8 @@ import productsReducer, {
 import * as actions from 'reducers/products/products.actions';
 import { productList } from 'assets/mock';
 
-describe('상품 불러오기 요청 액션에 따라 상태가 변경된다.', () => {
-  test('(REQUEST) 개별 상품 불러오기 요청이 들어오면, 상태가 정상적으로 수정되어야 한다.', () => {
+describe('전체 상품 불러오기 요청 액션에 따라 상태가 변경된다.', () => {
+  test('(REQUEST) 전체 상품 불러오기 요청이 들어오면, 상태가 정상적으로 수정되어야 한다.', () => {
     expect(productsReducer(initialState, actions.getProductsRequest())).toEqual(
       {
         ...initialState,
@@ -16,7 +16,7 @@ describe('상품 불러오기 요청 액션에 따라 상태가 변경된다.', 
     );
   });
 
-  test('(SUCCESS) 개별 상품 불러오기 요청이 성공하면, 상태가 정상적으로 수정되어야 한다.', () => {
+  test('(SUCCESS) 전체 상품 불러오기 요청이 성공하면, 상태가 정상적으로 수정되어야 한다.', () => {
     expect(
       productsReducer(initialState, actions.getProductsSuccess(productList)),
     ).toEqual({
@@ -28,7 +28,7 @@ describe('상품 불러오기 요청 액션에 따라 상태가 변경된다.', 
     });
   });
 
-  test('(ERROR) 개별 상품 불러오기 요청이 실패하면 상태가 정상적으로 수정되어야 한다.', () => {
+  test('(ERROR) 전체 상품 불러오기 요청이 실패하면 상태가 정상적으로 수정되어야 한다.', () => {
     expect(productsReducer(initialState, actions.getProductsError())).toEqual({
       ...initialState,
       isLoading: false,
