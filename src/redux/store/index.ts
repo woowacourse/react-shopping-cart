64e4a2da14715bae491661d1a317898b38982418
이아현 <lah1203@naver.com>
2022-05-20 15:56:47 +0900

    fix: 모듈 제거에 따라 코드 삭제
    - redux-promise 제거

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -2,13 +2,9 @@ import { applyMiddleware, createStore } from 'redux';
 
 import ReduxThunk from 'redux-thunk';
 import { persistStore } from 'redux-persist';
-import promiseMiddleware from 'redux-promise-middleware';
 import rootReducer from 'redux/reducers';
 
-const store = createStore(
-  rootReducer,
-  applyMiddleware(promiseMiddleware, ReduxThunk)
-);
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 const persistor = persistStore(store);
 
