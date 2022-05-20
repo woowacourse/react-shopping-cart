3764cb7f3fb5ecd713661f817c030cd3e6633b35
KIM YEJI <rladpwl0512@gmail.com>
2022-05-20 16:41:52 +0900

    :sparkles: feat: line 컴포넌트 생성

---
+++ src/components/Line/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const LineStyled = styled.hr(
+  ({ width, height, color }) => `
+    width: ${width};
+    background-color: ${color};
+    height: ${height};
+  `,
+);
+
+export default LineStyled;
