538b1f170f168aa7c6898d0117b11138f5ee7581
sangminsong <ssmin940606@gmail.com>
2022-05-21 13:43:06 +0900

    feat: 장바구니 삭제 확인창 추가
    - 실수로 장바구니에서 삭제되지 않도록 ux 추가

---
+++ react-shopping-cart/src/hooks/useCart.jsx
@@ -0,0 +1,41 @@
+import { useDispatch } from 'react-redux';
+
+import {
+  addProductToCartStart,
+  deleteProductFromCartStart,
+  fetchCartsStart,
+} from 'redux/carts/carts.action';
+
+import { CURRENT_USER } from 'constants/index';
+
+function useCart() {
+  const dispatch = useDispatch();
+
+  const handleDeleteProductFromCart = (e, id) => {
+    e.stopPropagation();
+
+    if (window.confirm('장바구니에서 삭제하시겠습니까?')) {
+      dispatch(deleteProductFromCartStart(id));
+      dispatch(fetchCartsStart());
+    }
+  };
+
+  const handleAddProductToCart = (e, { name, price, id, thumbnail }) => {
+    e.stopPropagation();
+
+    dispatch(
+      addProductToCartStart({
+        name,
+        price,
+        id: `${CURRENT_USER}${id}`,
+        thumbnail,
+        user: CURRENT_USER,
+      })
+    );
+    dispatch(fetchCartsStart());
+  };
+
+  return { handleAddProductToCart, handleDeleteProductFromCart };
+}
+
+export default useCart;
