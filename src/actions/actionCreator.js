b99714426e06c4b29805e02583975effbfaf2622
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-05-22 02:39:00 +0900

    feat: create order actions

--- src/actions/actionCreator.js
+++ src/actions/actionCreator.js
@@ -1,7 +1,19 @@
-import { CART_ACTIONS, PRODUCT_LIST_ACTIONS } from 'actions/action';
+import { CART_ACTIONS, PRODUCT_LIST_ACTIONS, ORDER_ACTIONS } from 'actions/action';
 
 const doPutProductToCart = ({ id, quantity }) => ({ type: CART_ACTIONS.PUT, id, quantity });
 
 const doInitializeCart = ({ products }) => ({ type: PRODUCT_LIST_ACTIONS.INITIALIZE, products });
 
-export { doPutProductToCart, doInitializeCart };
+const doAddProdcutToOrder = ({ id }) => ({ type: ORDER_ACTIONS.ADD, id });
+
+const doDeleteProductFromOrder = ({ id }) => ({ type: ORDER_ACTIONS.DELETE, id });
+
+const doInitializeOrder = () => ({ type: ORDER_ACTIONS.INITIALIZE });
+
+export {
+  doPutProductToCart,
+  doInitializeCart,
+  doAddProdcutToOrder,
+  doDeleteProductFromOrder,
+  doInitializeOrder,
+};
