5cb599289e13f6c0275212f5a8229718c3bbad5b
Daye Lee <508yeah@gmail.com>
2022-05-21 09:30:56 +0900

    feat: 404 페이지 컴포넌트 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ProductList, ShoppingCart } from 'page';
+import { NotFound, ProductList, ShoppingCart } from 'page';
 import { PATH } from 'constant';
 
 function App() {
@@ -8,6 +8,7 @@ function App() {
       <Routes>
         <Route path={PATH.PRODUCT_LIST_PAGE} element={<ProductList />} />
         <Route path={PATH.SHOPPING_CART_PAGE} element={<ShoppingCart />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
--- src/page/index.js
+++ src/page/index.js
@@ -1,2 +1,3 @@
+export { default as NotFound } from 'page/NotFound/NotFound';
 export { default as ProductList } from 'page/ProductList/ProductList';
 export { default as ShoppingCart } from 'page/ShoppingCart/ShoppingCart';
