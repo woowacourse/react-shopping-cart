a48bd3d9ffcb88461f555c89d2e61afc6d6330bc
kimsanglok <evertree6031@gmail.com>
2022-05-21 19:02:55 +0900

    feat: 메인 페이지 로딩 - LoadingSpinner -> Skeleton 변경

--- src/pages/ProductListPage/index.tsx
+++ src/pages/ProductListPage/index.tsx
@@ -3,9 +3,9 @@ import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
 import Product from 'components/Product';
-import LoadingSpinner from 'components/@common/LoadingSpinner';
 import Flex from 'components/@common/Flex';
 import Text from 'components/@common/Text';
+import Skeleton from 'components/@common/Skeleton';
 
 import { loadProductList } from 'api/product';
 import { RootState } from 'store';
@@ -25,11 +25,7 @@ const ProductList = () => {
       .catch(() => navigate('/notFound'));
   }, []);
 
-  if (isLoading) {
-    return <LoadingSpinner />;
-  }
-
-  if (productList.length === 0) {
+  if (!isLoading && productList.length === 0) {
     return (
       <Text align="center" size="20px" weight={600}>
         상품 목록이 존재하지 않습니다.
@@ -39,6 +35,7 @@ const ProductList = () => {
 
   return (
     <Flex wrap="wrap" gap="40px">
+      {isLoading && Array.from({ length: 8 }, () => <Skeleton />)}
       {productList.map(({ id, name, price, thumbnail }) => (
         <Product key={id} id={id} name={name} price={price} thumbnail={thumbnail} />
       ))}
