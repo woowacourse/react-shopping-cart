8f48dc0643924a45c909505769be8c72eee6462a
kimsanglok <evertree6031@gmail.com>
2022-05-21 19:41:25 +0900

    refactor: route-based code splitting 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import GlobalStyles from 'styles/globalStyles';
-
-import Layout from 'components/@common/Layout';
 import ProductListPage from 'pages/ProductListPage';
-import ProductDetailPage from 'pages/ProductDetailPage';
-import CartPage from 'pages/CartPage';
-import NotFoundPage from 'pages/NotFoundPage';
+import Layout from 'components/@common/Layout';
+
+const ProductDetailPage = lazy(() => import('pages/ProductDetailPage'));
+const CartPage = lazy(() => import('pages/CartPage'));
+const NotFoundPage = lazy(() => import('pages/NotFoundPage'));
 
 const App = () => {
   return (
     <div>
       <GlobalStyles />
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="" element={<ProductListPage />} />
-          <Route path="product/:id" element={<ProductDetailPage />} />
-          <Route path="cart" element={<CartPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<div>loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="" element={<ProductListPage />} />
+            <Route path="product/:id" element={<ProductDetailPage />} />
+            <Route path="cart" element={<CartPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </div>
   );
 };
