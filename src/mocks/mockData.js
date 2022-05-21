fa409a5ee865aed1c0d2d375ba5e53a1fa7ed42b
jhy979 <jhy979@gmail.com>
2022-05-21 14:11:16 +0900

    feat: 상품 정보 get, 장바구니 정보 get,post,delete,patch MSW 처리

--- src/mocks/mockData.js
+++ src/mocks/mockData.js
@@ -53,11 +53,3 @@ export const MOCK_PRODUCT_LIST = [
     price: 20200,
   },
 ];
-
-export const MOCK_PRODUCT_DETAIL = {
-  id: 1,
-  name: '올인원 세트-물티수저',
-  image:
-    'https://cdn-mart.baemin.com/goods/13/D6-PB-20277_%EC%98%AC%EC%9D%B8%EC%9B%90-%EC%84%B8%ED%8A%B8_%EB%AC%BC%ED%8B%B0%EC%88%98%EC%A0%80_%EC%8D%B8%EB%84%A4%EC%9D%BC.jpg',
-  price: 57600,
-};
