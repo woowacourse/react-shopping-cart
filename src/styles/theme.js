d2dc61db9786a08778fae42eb3537ca64e8471c0
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-05-21 23:12:52 +0900

    💄 : 테마 상수 업데이트

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,20 +1,30 @@
 const LAYOUT = {
   BORDER_RADIUS: 5,
+  ROOT_PIXEL_SIZE: 16,
 };
 
 const COLORS = {
   BLACK: '#000000',
+  BLACK_015: 'rgba(0,0,0, 0.15)',
+
   WHITE: '#FFFFFF',
 
   GRAY_50: '#555555',
+  GRAY_70: '#7B7B7B',
   GRAY_100: '#AAAAAA',
   GRAY_150: '#DDDDDD',
   GRAY_200: '#F6F6F6',
   GRAY_250: '#F3F3F3',
 
+  MINT_400: '#49A09D',
+  MINT_500: '#5FBEBB',
+
   BLUE_150: '#0066ff',
 };
 
-const BRAND_COLORS = {};
+const BRAND_COLORS = {
+  PRIMARY: COLORS.MINT_500,
+  PRIMARY_FONT: COLORS.WHITE,
+};
 
 export { LAYOUT, COLORS, BRAND_COLORS };
