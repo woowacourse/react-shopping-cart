14a26be65f0ce170fc61a02b306448f3bf33d369
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-05-21 23:07:11 +0900

    ✨ : 카운터 컴포넌트 추가

---
+++ src/components/@common/Counter/index.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+import { getNumberFormatter } from 'lib/formatterUtils';
+
+import Icon from 'components/@common/Icon';
+
+import { ICON_CODE } from 'constants/';
+import * as S from './styles';
+
+function Counter({ onClickUpButton, onClickDownButton, onChangeInputValue, children }) {
+  return (
+    <S.Container>
+      <S.CounterText onChange={onChangeInputValue} value={getNumberFormatter(children)} />
+
+      <S.ButtonContainer>
+        <S.ControlButton onClick={onClickUpButton}>
+          <Icon icon={ICON_CODE.UP} />
+        </S.ControlButton>
+        <S.ControlButton onClick={onClickDownButton}>
+          <Icon icon={ICON_CODE.DOWN} />
+        </S.ControlButton>
+      </S.ButtonContainer>
+    </S.Container>
+  );
+}
+
+Counter.defaultProps = {
+  children: 1500,
+};
+
+Counter.propTypes = {
+  children: PropTypes.number,
+};
+
+export default Counter;
