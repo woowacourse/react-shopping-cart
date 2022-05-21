ead673f1b3aa15c430aba6c132aa3e2de3a74f19
Daye Lee <508yeah@gmail.com>
2022-05-21 10:15:32 +0900

    feat: 결과 컴포넌트 구현

--- src/component/index.js
+++ src/component/index.js
@@ -3,4 +3,5 @@ export { default as ListProduct } from 'component/ListProduct/ListProduct';
 export { default as LoadingSpinner } from 'component/LoadingSpinner/LoadingSpinner';
 export { default as NavBar } from 'component/NavBar/NavBar';
 export { default as PageLayout } from 'component/PageLayout/PageLayout';
+export { default as Result } from 'component/Result/Result';
 export { default as Selector } from 'component/Selector/Selector';
