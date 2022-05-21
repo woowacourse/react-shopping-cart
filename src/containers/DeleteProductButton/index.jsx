74421e3ddba0eb1d41e31d079cb8c75c89d821f8
KIM YEJI <rladpwl0512@gmail.com>
2022-05-21 19:39:40 +0900

    :sparkles: feat: 상품 삭제 버튼 컴포넌트 생성

---
+++ src/containers/DeleteProductButton/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import BoxButton from 'components/BoxButton';
+
+function DeleteProductButton() {
+  return (
+    <BoxButton
+      color="#FFF"
+      message="상품삭제"
+      fontSize="16px"
+      width="117px"
+      height="50px"
+      border="#BBB"
+      fontColor="#333"
+    />
+  );
+}
+
+export default DeleteProductButton;
