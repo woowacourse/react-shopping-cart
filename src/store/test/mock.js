9fd8e6181f36b77607666ebe8bceb02c72233f5c
Soyi Jeon <soyi.dev@gmail.com>
2022-05-21 16:48:22 +0900

    test: API 처리 관련 dispatch action 테스트 작성
    - 렌더링 테스트 코드 제거
    - store 내부 test 파일로 테스트 관련 파일 이동

---
+++ src/store/test/mock.js
@@ -0,0 +1,32 @@
+export const INITIAL_CART_STATE = {
+  loading: true,
+  data: [],
+  errorMessage: null,
+};
+
+export const MOCK_CART_ITEM_LIST = [
+  {
+    id: 1,
+    thumbnailUrl:
+      "https://cdn-mart.baemin.com/sellergoods/bulk/20211224-151131/22548-main-01.jpg",
+    name: "[든든] 방울토마토5kg (1~2번과)",
+    price: 25000,
+    quantity: 3,
+  },
+  {
+    id: 2,
+    thumbnailUrl:
+      "https://cdn-mart.baemin.com/sellergoods/main/a46bf28d-fe78-4844-b476-ad1750f63f09.jpg",
+    name: "[든든] 참외 2kg (6~12입)",
+    price: 22500,
+    quantity: 2,
+  },
+  {
+    id: 3,
+    thumbnailUrl:
+      "https://cdn-mart.baemin.com/sellergoods/main/8043fc74-69cf-46cd-9250-ed91ac34c32e.png",
+    name: "[든든] 미국산 레몬 1팩(3입)",
+    price: 3500,
+    quantity: 1,
+  },
+];
