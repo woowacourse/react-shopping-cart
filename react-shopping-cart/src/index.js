56e3d5b9b75d444b95af19992e0edf10df0053e5
sangminsong <ssmin940606@gmail.com>
2022-05-21 17:53:07 +0900

    feat: MSW 추가
    - 현재 백엔드에서 API를 다 개발하지 않았다는 가정하에, MSW를 생성하여 request와 response를 임시로 지정해주어 서버가 없어도 개발을 진행하고 완수할 수 있음
    - 단, step1에서 이미 server를 구축하여 통신을 해주고 있었기 때문에, MSW를 사용하면서 어느 것을 기준으로 해야할지 약간의 혼동이 있었음
    
    - 결론은, 기존의 서버는 없다고 생각하고 MSW만을 기준으로 개발, 하지만 백엔드와의 협의를 통해 API에 대한 규격을 제대로 맞춰서 MSW를 만들어야 추후 개발에 브레이크가 생기지 않을 것 같음

--- react-shopping-cart/src/index.js
+++ react-shopping-cart/src/index.js
@@ -1,4 +1,5 @@
 import App from 'App';
+import startMocking from 'mocks/startMocking';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
@@ -11,6 +12,12 @@ import { store } from 'redux/store';
 
 import GlobalStyle from 'styles/GlobalStyle';
 import theme from 'styles/theme';
+
+// 현재는 백엔드와 협업을 하고 있지 않고 MSW를 사용하여 개발하고 있는 상황이라고 전제하려고 합니다.
+// 그래서 데모 사이트도 MSW를 사용하여 배포해주었습니다.
+// if (process.env.NODE_ENV === 'development') {
+startMocking();
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
