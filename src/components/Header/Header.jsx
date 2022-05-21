ee54be154cbdce57d447908c1aecc0263b83aca8
최광희 <polyckh@gmail.com>
2022-05-21 23:14:49 +0900

    feat: 헤더에 장바구니 수량 표시

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,13 +5,15 @@ import PATH from '../../constants/path';
 import { CART_SIZE, COLOR } from '../../constants/styles';
 
 import { ReactComponent as CartIcon } from '../shared/CartIcon.svg';
-import { BasicButton } from '../shared/basics';
+import { BasicButton, Flex } from '../shared/basics';
+import { useSelector } from 'react-redux';
 
 function Header() {
+  const { carts } = useSelector((state) => state.carts);
   return (
     <header>
-      <Styled.Nav>
-        <Styled.NavTitleWrapper>
+      <Styled.Nav justify="space-around">
+        <Flex align="center">
           <Link to={PATH.ROOT}>
             <CartIcon
               width={CART_SIZE.LARGE.WIDTH}
@@ -20,10 +22,15 @@ function Header() {
             />
             <Styled.NavTitle>WOOWA SHOP</Styled.NavTitle>
           </Link>
-        </Styled.NavTitleWrapper>
-        <Styled.NavButtonWrapper>
+        </Flex>
+        <Styled.NavButtonWrapper align="center">
           <Link to={PATH.CARTS}>
-            <Styled.NavButton type="button">장바구니</Styled.NavButton>
+            <Styled.NavCartButtonWrapper>
+              <Styled.NavButton type="button">장바구니</Styled.NavButton>
+              <Styled.Badge align="center" justify="center">
+                {carts.length}
+              </Styled.Badge>
+            </Styled.NavCartButtonWrapper>
           </Link>
           <Link to={PATH.ORDERS}>
             <Styled.NavButton type="button">주문목록</Styled.NavButton>
@@ -37,18 +44,12 @@ function Header() {
 export default Header;
 
 const Styled = {
-  Nav: styled.nav`
-    display: flex;
-    justify-content: space-around;
+  Nav: styled(Flex)`
     width: 100%;
     height: 80px;
 
     background: ${COLOR.PRIMARY};
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.3);
-  `,
-  NavTitleWrapper: styled.div`
-    display: flex;
-    align-items: center;
   `,
   NavTitle: styled.h1`
     display: inline;
@@ -58,16 +59,26 @@ const Styled = {
     color: #ffffff;
     margin-left: 15px;
   `,
-
-  NavButtonWrapper: styled.div`
-    display: flex;
-    align-items: center;
+  NavButtonWrapper: styled(Flex)`
     gap: 15px;
+  `,
+  NavCartButtonWrapper: styled.div`
+    position: relative;
   `,
   NavButton: styled(BasicButton)`
     font-weight: 500;
     font-size: 24px;
 
     color: #ffffff;
+  `,
+  Badge: styled(Flex)`
+    position: absolute;
+    color: white;
+    top: -15px;
+    right: -15px;
+    border-radius: 50%;
+    background-color: red;
+    width: 25px;
+    height: 25px;
   `,
 };
