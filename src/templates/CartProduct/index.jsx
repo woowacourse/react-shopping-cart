a86befdef0d383679345651d42efb068d253da35
KIM YEJI <rladpwl0512@gmail.com>
2022-05-21 17:11:03 +0900

    :sparkles: feat: 카트 페이지 상품 컴포넌트 생성

---
+++ src/templates/CartProduct/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import Checkbox from 'components/Checkbox';
+import Image from 'components/Image';
+import BlackText from 'components/BlackText';
+import TrashcanButton from 'containers/TrashcanButton';
+import NumberInput from 'components/NumberInput';
+import CartProductStyled from './style';
+import MarginWrapper from 'components/MarginWrapper';
+import FlexWrapper from 'components/FlexWrapper';
+
+function CartProduct({ imgSrc, title, price }) {
+  return (
+    <CartProductStyled>
+      <FlexWrapper justifyContent="space-between">
+        <div>
+          <FlexWrapper>
+            <MarginWrapper marginRight="15px">
+              <Checkbox />
+            </MarginWrapper>
+            <MarginWrapper marginRight="20px">
+              <Image src={imgSrc} width="144px" height="147px" alt="상품 이미지" />
+            </MarginWrapper>
+            <BlackText fontSize="20px" fontWeight="400">
+              {title}
+            </BlackText>
+          </FlexWrapper>
+        </div>
+        <FlexWrapper flexDirection="column" alignItems="flex-end" justifyContent="space-between">
+          <TrashcanButton />
+          <NumberInput />
+          <BlackText fontSize="16px" fontWeight="400">
+            {price}원
+          </BlackText>
+        </FlexWrapper>
+      </FlexWrapper>
+    </CartProductStyled>
+  );
+}
+
+export default CartProduct;
---
+++ src/templates/CartProduct/style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const CartProductStyled = styled.div`
+  border-top: 1px solid #ccc;
+  width: 736px;
+  padding-top: 22px;
+`;
+
+export default CartProductStyled;
