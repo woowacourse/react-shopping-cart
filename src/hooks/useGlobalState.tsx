c2b4e9941dc7bb31b352461a08961ef170152163
kimsanglok <evertree6031@gmail.com>
2022-05-22 04:15:36 +0900

    refactor: 불필요한 import 구문 제거

--- src/hooks/useGlobalState.tsx
+++ src/hooks/useGlobalState.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { RootState } from 'store';
-import { ProductListAction } from 'store/productList/reducer';
-import { CartProductListAction } from 'store/cartProductList/reducer';
-import { AppDispatch } from 'types';
 import useAppDispatch from './useAppDispatch';
 
 type Key = 'product' | 'cart';
 
 const useGlobalState = (key: Key) => {
   const dispatch = useAppDispatch();
-
   const state = useSelector((state: RootState) => state[key]);
 
   return { dispatch, state };
