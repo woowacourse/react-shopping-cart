27e0d9416252e75586af381e0f3eaf34e0e2f355
Marco <bigdatajang@gmail.com>
2022-05-22 06:32:53 +0900

    fix: test 실행 스크립트 문제 해결
    윈도우에서는 잘 됐는데 왜 맥북에서는 테스트가 갑자기 잘 안될까..

--- src/components/CartOrder/__tests__/cartOrder.test.jsx
+++ src/components/CartOrder/__tests__/cartOrder.test.jsx
@@ -1,6 +1,6 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import CartOrder from 'components/CartOrder';
-import React from 'react';
+import '@testing-library/jest-dom';
 
 const mockNavigate = jest.fn();
 const mockDispatch = jest.fn();
