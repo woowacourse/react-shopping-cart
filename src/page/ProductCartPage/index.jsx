b32e75f2f22292bb069fa18492742602b880b875
jhy979 <jhy979@gmail.com>
2022-05-21 12:58:33 +0900

    feat: 장바구니 페이지 get 에러 처리

--- src/page/ProductCartPage/index.jsx
+++ src/page/ProductCartPage/index.jsx
@@ -5,14 +5,19 @@ import PropTypes from 'prop-types';
 import CheckBox from 'component/common/CheckBox';
 import ContentBox from 'component/common/ContentBox';
 import CartItem from 'component/CartItem';
+import ErrorBoundary from 'component/common/ErrorBoundary';
 
 import * as S from 'page/ProductCartPage/style';
 
 import useCartItem from 'hook/useCartItem';
 import useSelectedItem from 'hook/useSelectedItem';
+import Loading from 'component/Loader';
+import NotFoundPage from 'page/NotFoundPage';
 
 export default function ProductCartPage() {
   const cartItem = useSelector((state) => state.cartReducer.cart);
+  const error = useSelector((state) => state.cartReducer.error);
+  const pending = useSelector((state) => state.cartReducer.pending);
   const selectedItem = useSelector((state) => state.selectedItemReducer.selectedItem);
 
   const {
@@ -63,27 +68,34 @@ export default function ProductCartPage() {
 
           <S.ListHeaderSpan>장바구니 상품 ({cartItem.length}개)</S.ListHeaderSpan>
           <S.CartListBox>
-            {cartItem.map(({itemImgURL, itemName, itemPrice, quantity, id}) => {
-              const initialChecked = selectedItem.includes(id);
-              return (
-                <React.Fragment key={id}>
-                  <CartItem
-                    initialChecked={initialChecked}
-                    itemImgURL={itemImgURL}
-                    itemName={itemName}
-                    itemPrice={itemPrice}
-                    quantity={quantity}
-                    id={id}
-                    handleDeleteIconClick={() => deleteCartItem(id)}
-                    handleCheckedTrue={addSelectedItem}
-                    handleCheckedFalse={deleteSelectedItem}
-                    handleIncrease={() => increaseQuantity({quantity, id})}
-                    handleDecrease={() => decreaseQuantity({quantity, id})}
-                  />
-                  <hr />
-                </React.Fragment>
-              );
-            })}
+            {pending && <Loading />}
+            <ErrorBoundary
+              error={error}
+              pending={pending}
+              fallback={<NotFoundPage>에러가 발생했어요.</NotFoundPage>}
+            >
+              {cartItem.map(({itemImgURL, itemName, itemPrice, quantity, id}) => {
+                const initialChecked = selectedItem.includes(id);
+                return (
+                  <React.Fragment key={id}>
+                    <CartItem
+                      initialChecked={initialChecked}
+                      itemImgURL={itemImgURL}
+                      itemName={itemName}
+                      itemPrice={itemPrice}
+                      quantity={quantity}
+                      id={id}
+                      handleDeleteIconClick={() => deleteCartItem(id)}
+                      handleCheckedTrue={addSelectedItem}
+                      handleCheckedFalse={deleteSelectedItem}
+                      handleIncrease={() => increaseQuantity({quantity, id})}
+                      handleDecrease={() => decreaseQuantity({quantity, id})}
+                    />
+                    <hr />
+                  </React.Fragment>
+                );
+              })}
+            </ErrorBoundary>
           </S.CartListBox>
         </S.SelectCartBox>
 
