b4170a85dd4cbd63903a6b65e8bd8dee284ffa8d
jswith <typingjs@gmail.com>
2022-05-22 16:46:18 +0900

    style(ui): not found page ui 변경

--- src/pages/not-found/components/NotFound.jsx
+++ src/pages/not-found/components/NotFound.jsx
@@ -1,16 +1,26 @@
+import { css } from "@emotion/react";
+
+import NotFoundImg from "@/assets/images/404.png";
+
 function NotFound() {
   return (
     <div
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        fontSize: 64,
-        width: "100%",
-        height: "100%",
-      }}
+      css={css`
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        min-height: 90vh;
+      `}
     >
-      404
+      <img src={NotFoundImg} alt="존재하지 않는 페이지" />
+      <div
+        css={css`
+          font-size: 25px;
+        `}
+      >
+        존재하지 않는 페이지입니다.
+      </div>
     </div>
   );
 }
