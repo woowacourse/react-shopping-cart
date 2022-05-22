599c799fc08a213cd81d7a37389e51af1b17c950
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-05-23 01:48:16 +0900

    refactor: move Button component to shared directory

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as Image } from 'components/Image';
 export { default as CartIcon } from 'components/CartIcon';
 export { default as QuantityController } from 'components/QuantityController';
-export { default as Button } from 'components/Button';
+export { default as Button } from 'components/@shared/Button';
 export { default as Header } from 'components/Header';
