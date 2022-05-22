42f9ba677a18dc81b688a809bfdb0916254cdded
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-05-23 02:01:58 +0900

    refactor: combine export statement

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,15 @@
-export { default as Image } from 'components/Image';
-export { default as CartIcon } from 'components/CartIcon';
-export { default as QuantityController } from 'components/QuantityController';
 export { default as Button } from 'components/@shared/Button';
+
+export { default as CartIcon } from 'components/CartIcon';
+export { default as CartProductItem } from 'components/CartProductItem';
+export { default as CheckBox } from 'components/CheckBox';
+export { default as Counter } from 'components/Counter';
 export { default as Header } from 'components/Header';
+export { default as Image } from 'components/Image';
+export { default as ProductItem } from 'components/ProductItem';
+export { default as QuantityController } from 'components/QuantityController';
+export { default as TotalPrice } from 'components/TotalPrice';
+export { default as UnderlineText } from 'components/UnderlineText';
+
+export { default as GlobalStyles } from 'components/GlobalStyles';
+export { default as theme } from 'components/theme';
---
+++ src/page/index.js
@@ -0,0 +1,3 @@
+export { default as CartPage } from 'page/CartPage';
+export { default as ProductDetailPage } from 'page/ProductDetailPage';
+export { default as ProductListPage } from 'page/ProductListPage';
