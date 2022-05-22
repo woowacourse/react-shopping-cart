52bce6e58ab608f34c011ce091761e8175cfa821
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-05-22 18:45:27 +0900

    🐛 : getNumberFormatter 함수 버그 수정
    - String 타입이 넘어올 시 오류 출력되는 문제 해결

--- src/lib/formatterUtils.js
+++ src/lib/formatterUtils.js
@@ -1,6 +1,6 @@
 import { LAYOUT } from 'styles/theme';
 
-const getNumberFormatter = (number) => number.toLocaleString('ko-KR');
+const getNumberFormatter = (number) => Number(number).toLocaleString('ko-KR');
 
 const getPixelToRem = (size) => (size / LAYOUT.ROOT_PIXEL_SIZE).toFixed(2);
 
