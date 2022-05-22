7bf29828b14f2111d92b301c5eef9d626eddad99
airman5573 <rpf5573@kakao.com>
2022-05-22 13:52:53 +0900

    refactor(cleanup): 불필요한 코드를 삭제한다

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,9 @@
 import React from "react";
 import Header from "@shared/header/Header";
 import styles from "@/app.module";
-import Modal from "@shared/modal/Modal";
-import useModal from "@shared/modal/useModal";
 import Home from "@home/Home";
 import Cart from "@cart/Cart";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useSelector } from "react-redux";
-
-function NestedComponent() {
-  const { show } = useModal();
-  return (
-    <button type="button" onClick={show}>
-      Show Modal
-    </button>
-  );
-}
-
-function ModalTestApp() {
-  const { isVisible, hide } = useModal();
-
-  return (
-    <>
-      <Modal isVisible={isVisible}>
-        <button type="button" onClick={hide}>
-          Hide Modal!
-        </button>
-      </Modal>
-      <NestedComponent />
-    </>
-  );
-}
-
-function Sibiling() {
-  const state = useSelector((state) => state);
-  console.log('re-render in Sibiling');
-  return <div>Hi ~</div>;
-}
 
 function App() {
   return (
@@ -48,7 +15,6 @@ function App() {
           <Route path="/cart" element={<Cart />} />
         </Routes>
       </BrowserRouter>
-      <Sibiling />
     </div>
   );
 }
