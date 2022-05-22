238346c7e4f75066b87ff9f1193bb6fd7541aaee
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-05-23 03:17:29 +0900

    ♻️ : 장바구니 리듀서 변경된 구조에 맞게 변경

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,15 +1,26 @@
+/* eslint-disable no-param-reassign */
+import produce from 'immer';
+
 import { CARTS_ACTIONS } from 'actions/types';
+import { createAsyncState } from 'lib/requestUtils';
 
 const initialState = {
-  items: [],
+  items: createAsyncState.initial([]),
 };
 
 export default (state = initialState, action) => {
-  const { type, payload } = action;
+  const { type, payload = {}, async = {} } = action;
 
   switch (type) {
-    case CARTS_ACTIONS.ADD_CART_LIST:
-      return { ...state, items: [...state.items, payload] };
+    case CARTS_ACTIONS.UPDATE_CART_LIST_SUCCESS:
+      return produce(state, (draft) => {
+        draft.items = { ...draft.items, ...async };
+        draft.items.content = payload;
+      });
+
+    case CARTS_ACTIONS.UPDATE_CART_LIST_PENDING:
+    case CARTS_ACTIONS.UPDATE_CART_LIST_ERROR:
+      return { ...state, items: { ...state.items, ...async } };
 
     default:
       return state;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
 import products from './products';
-import carts from './cart';
+import cart from './cart';
 
-export default combineReducers({ products, carts });
+export default combineReducers({ products, cart });
