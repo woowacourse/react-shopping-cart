1f35780fc7b686a755cafd39442853828144d4ec
JUDONGHYEOK <judonghyeok@gmail.com>
2022-05-23 06:08:42 +0900

    feat: ItemDetails 페이지내에서 장바구니버튼 활성화

--- src/components/ItemDetails/index.tsx
+++ src/components/ItemDetails/index.tsx
@@ -2,11 +2,19 @@ import { useTheme } from "@emotion/react";
 import { themeType } from "../../ThemeProvider";
 import * as S from "./index.styles";
 
+interface ItemDetailsProps {
+  imgUrl?: string;
+  title?: string;
+  price?: number;
+  onClickShoppingCartButton?: () => void;
+}
+
 const ItemDetails = ({
   imgUrl = "../../assets/image/no-image.png",
   title = "NO_TITLE",
   price = 200000,
-}) => {
+  onClickShoppingCartButton,
+}: ItemDetailsProps) => {
   const {
     color: { white, brown, darkGray },
   } = useTheme() as themeType;
@@ -18,7 +26,12 @@ const ItemDetails = ({
         <p>금액</p>
         <p>{price?.toLocaleString("ko-KR")}원</p>
       </S.PriceContainer>
-      <S.ShoppingCartButton type="button" color={white} backgroundColor={brown}>
+      <S.ShoppingCartButton
+        type="button"
+        color={white}
+        backgroundColor={brown}
+        onClick={onClickShoppingCartButton}
+      >
         장바구니
       </S.ShoppingCartButton>
     </S.ItemContainer>
--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,12 +1,14 @@
 import { useParams } from "react-router-dom";
 import ItemDetails from "../../components/ItemDetails";
+import useCart from "../../hooks/useCart";
 import useProduct from "../../hooks/useProduct";
 import * as S from "./index.styles";
 
 const Detail = () => {
   const { id } = useParams();
-
+  const { createNewCart } = useCart();
   const { product } = useProduct(Number(id));
+
   if (product.isLoading) {
     return <div></div>;
   }
@@ -16,7 +18,14 @@ const Detail = () => {
   return (
     <S.DetailPageContainer>
       <S.DetailContainer>
-        {product?.isLoading || <ItemDetails {...product?.data} />}
+        {product?.isLoading || (
+          <ItemDetails
+            {...product?.data}
+            onClickShoppingCartButton={() => {
+              createNewCart(Number(id));
+            }}
+          />
+        )}
       </S.DetailContainer>
     </S.DetailPageContainer>
   );
