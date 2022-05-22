7c611cd3f013603e5efdcb67b09ece22373f6fd0
Daye Lee <508yeah@gmail.com>
2022-05-22 16:14:55 +0900

    feat: 장바구니 결제 예상 금액 구현

--- src/pages/ShoppingCart/ShoppingCart.jsx
+++ src/pages/ShoppingCart/ShoppingCart.jsx
@@ -7,6 +7,8 @@ import ACTION_TYPE from 'redux/cart/cartActions';
 import { Button } from 'components/@common';
 import { CartProduct, PageLayout, Result, Selector } from 'components';
 
+import { getObjectArrayIdxOfValue } from 'utils';
+
 const ShoppingCartBox = styled.div`
   height: 100%;
   display: flex;
@@ -43,10 +45,19 @@ const DivisionLine = styled.hr`
 
 function ShoppingCart() {
   const { products, checkedProducts } = useSelector(store => store.cart);
+  const [totalPrice, setTotalPrice] = useState(0);
   const [checked, setChecked] = useState(true);
   const dispatch = useDispatch();
 
   useEffect(() => {
+    const total = checkedProducts.map(({ id }) => {
+      const productIdx = getObjectArrayIdxOfValue(products, { key: 'id', value: id });
+      const { quantity, price } = products[productIdx];
+
+      return quantity * price;
+    });
+
+    setTotalPrice(total.reduce((acc, cur) => acc + cur, 0));
     setChecked(products.length === checkedProducts.length);
   }, [products, checkedProducts]);
 
@@ -78,7 +89,11 @@ function ShoppingCart() {
             <CartProduct key={`cart${product.id}`} {...product} />
           ))}
         </div>
-        <Result title="결제예상금액" price={0} button={`주문하기(${checkedProducts.length}개)`} />
+        <Result
+          title="결제예상금액"
+          price={totalPrice}
+          button={`주문하기(${checkedProducts.length}개)`}
+        />
       </ShoppingCartBox>
     </PageLayout>
   );
