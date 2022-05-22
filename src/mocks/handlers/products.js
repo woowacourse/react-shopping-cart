fcc6cd03cf99eee9f12c0aa31148adb73581d8ec
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-05-22 09:15:13 +0900

    ✨  : 상품 목록 로드 Mocking API 실제 서버 동작과 동일하게 설정
    - 페이지 번호 및 페이지 별 컨텐츠 갯수 지정
    - 응답 헤더를 통해 컨텐츠의  총 갯수 반환

--- src/mocks/handlers/products.js
+++ src/mocks/handlers/products.js
@@ -2,11 +2,18 @@
 import { rest } from 'msw';
 import { MOCK_DB } from 'mocks/db';
 
-const HOST_NAME = process.env.REACT_APP_API_URL;
-
 const productsHandlers = [
-  rest.get(`${HOST_NAME}/shopping`, (req, res, ctx) => {
-    return res(ctx.json(MOCK_DB.shopping));
+  rest.get('./shopping', (req, res, ctx) => {
+    const { _page: page = 1, _limit: limit = 12 } = Object.fromEntries(
+      req.url.searchParams.entries(),
+    );
+
+    const limitStartIndex = Number((page - 1) * limit);
+    const limitEndIndex = limitStartIndex + Number(limit);
+
+    const result = MOCK_DB.shopping.slice(limitStartIndex, limitEndIndex);
+
+    return res(ctx.json(result), ctx.set({ 'x-total-count': MOCK_DB.shopping.length }));
   }),
 ];
 
