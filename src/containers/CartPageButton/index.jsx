75fef491e14088e1761ed2b55191a17030cb4d92
KIM YEJI <rladpwl0512@gmail.com>
2022-05-23 02:39:45 +0900

    :sparkles: feat: 장바구니 상품 추가 시, 장바구니 메뉴바에 상태 업데이트

--- src/containers/CartPageButton/index.jsx
+++ src/containers/CartPageButton/index.jsx
@@ -3,13 +3,23 @@ import { Link } from 'react-router-dom';
 
 import MarginWrapper from 'components/MarginWrapper';
 import WhiteButton from 'components/WhiteButton';
+import { useSelector } from 'react-redux';
+import Circle from 'components/Circle';
+import FlexWrapper from 'components/FlexWrapper';
 
 function CartPageButton() {
+  const carts = useSelector((state) => state.cart.carts);
+
   return (
     <MarginWrapper marginRight="4px">
-      <Link to="/cart-list">
-        <WhiteButton fontSize="1.5rem">장바구니</WhiteButton>
-      </Link>
+      <FlexWrapper>
+        <MarginWrapper marginRight="4px">
+          <Link to="/cart-list">
+            <WhiteButton fontSize="1.5rem">장바구니</WhiteButton>
+          </Link>
+        </MarginWrapper>
+        <Circle>{carts.length}</Circle>
+      </FlexWrapper>
     </MarginWrapper>
   );
 }
