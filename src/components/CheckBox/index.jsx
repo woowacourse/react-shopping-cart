a0ff2b03dd62f55a3b89f4e4ed8204b19ef521cd
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-05-23 00:23:56 +0900

    feat(CheckBox): specify props type

--- src/components/CheckBox/index.jsx
+++ src/components/CheckBox/index.jsx
@@ -1,12 +1,18 @@
-import Styled from './index.style';
+import PropTypes from 'prop-types';
+import Styled from 'components/CheckBox/index.style';
 
 const CheckBox = ({ checked, handleChange }) => {
   return (
-    <Styled.CheckBoxLabel className="container">
+    <Styled.Container>
       <input type="checkbox" checked={checked} onChange={handleChange} />
-      <span className="checkmark" />
-    </Styled.CheckBoxLabel>
+      <Styled.CheckMark />
+    </Styled.Container>
   );
+};
+
+CheckBox.propTypes = {
+  checked: PropTypes.bool.isRequired,
+  handleChange: PropTypes.func.isRequired,
 };
 
 export default CheckBox;
