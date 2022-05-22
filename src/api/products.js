bd1d6afc918df82a94251aa3253be0e15d34e395
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-05-23 06:29:41 +0900

    ♻️ : 상품 목록 페이지별 불러오는 상품 수량 수정

--- src/api/products.js
+++ src/api/products.js
@@ -1,6 +1,6 @@
 import { request } from 'lib/requestUtils';
 
 const requestGetProductList = (page = 1) =>
-  request(`/shopping?_page=${page}&_limit=12`, { method: 'GET' });
+  request(`/shopping?_page=${page}&_limit=36`, { method: 'GET' });
 
 export { requestGetProductList };
