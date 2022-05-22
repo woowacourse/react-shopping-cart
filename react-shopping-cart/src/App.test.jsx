5e75de298f3eeb48caff66951b6db9a527eeecc6
2019015414 <2019015414@hanyang.ac.kr>
2022-05-22 19:13:08 +0900

    test: 상품 컴포넌트 렌더 테스트 추가

--- react-shopping-cart/src/App.test.jsx
+++ react-shopping-cart/src/App.test.jsx
@@ -1,8 +1,37 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import ProductListItem from 'components/ProductListItem/ProductListItem.component';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from 'styles/GlobalStyle';
+import theme from 'styles/theme';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('컴포넌트 렌더 테스트', () => {
+  test('상품 컴포넌트는 썸네일, 상품 이름, 가격, 장바구니 담기 버튼을 렌더해야 한다.', () => {
+    // given
+    const product = {
+      id: 1,
+      thumbnail: 'https://cdn-mart.baemin.com/sellergoods/bulk/20220502-173334/12619-main-01.jpg',
+      name: '[든든] 전지베이컨 500g',
+      price: 6390,
+    };
+    const handleToggleShoppingCart = jest.fn();
+
+    // when
+    render(
+      <>
+        <GlobalStyle />
+        <ThemeProvider theme={theme}>
+          <ProductListItem
+            {...product}
+            isContained="false"
+            handleToggleShoppingCart={handleToggleShoppingCart}
+          />
+        </ThemeProvider>
+      </>
+    );
+
+    // then
+    expect(screen.getByRole('img')).toHaveAttribute('src', product.thumbnailUrl);
+    expect(screen.getByText(product.name)).toBeInTheDocument();
+    expect(screen.getByText(`${product.price.toLocaleString()}원`)).toBeInTheDocument();
+  });
 });
