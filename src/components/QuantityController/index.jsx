727173db2030763ad9debb14b1a14f4d359ca628
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-05-23 01:05:38 +0900

    feat(QuantityController): specify props type

--- src/components/QuantityController/index.jsx
+++ src/components/QuantityController/index.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import Styled from 'components/QuantityController/index.style';
 
 const QuantityController = ({ handleClick, quantity, increase, decrease }) => {
@@ -8,6 +9,13 @@ const QuantityController = ({ handleClick, quantity, increase, decrease }) => {
       <Styled.Increase onClick={increase} />
     </Styled.Container>
   );
+};
+
+QuantityController.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+  quantity: PropTypes.number.isRequired,
+  increase: PropTypes.func.isRequired,
+  decrease: PropTypes.func.isRequired,
 };
 
 export default QuantityController;
