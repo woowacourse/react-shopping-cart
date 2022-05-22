ded909ec778ca3ceff1549b755d909865a27c92c
Yo Wook Kim <uk960214@gmail.com>
2022-05-23 00:57:42 +0900

    refactor: index.js 리팩토링

--- src/index.js
+++ src/index.js
@@ -10,37 +10,33 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from 'styles/GlobalStyles';
 import Theme from 'styles/Theme';
 
-async function main() {
-  if (process.env.NODE_ENV === 'development') {
-    if (window.location.pathname === '/react-shopping-cart') {
-      window.location.pathname = '/react-shopping-cart/';
-      return;
-    }
-
-    const { worker } = require('./mocks/browser');
-    await worker.start({
-      serviceWorker: {
-        url: '/react-shopping-cart/mockServiceWorker.js',
-      },
-      onUnhandledRequest(req) {
-        if (!req.url.pathname.startsWith('/react-shopping-cart/')) {
-          console.warn('Found an unhandled %s request to %s', req.method, req.url.href);
-        }
-      },
-      quiet: true,
-    });
+if (process.env.NODE_ENV === 'development') {
+  if (window.location.pathname === '/react-shopping-cart') {
+    window.location.pathname = '/react-shopping-cart/';
   }
 
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(
-    <React.StrictMode>
-      <GlobalStyle />
-      <ThemeProvider theme={Theme}>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </ThemeProvider>
-    </React.StrictMode>,
-  );
+  const { worker } = require('./mocks/browser');
+  worker.start({
+    serviceWorker: {
+      url: '/react-shopping-cart/mockServiceWorker.js',
+    },
+    onUnhandledRequest(req) {
+      if (!req.url.pathname.startsWith('/react-shopping-cart/')) {
+        console.warn('Found an unhandled %s request to %s', req.method, req.url.href);
+      }
+    },
+    quiet: true,
+  });
 }
-main();
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <GlobalStyle />
+    <ThemeProvider theme={Theme}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
+  </React.StrictMode>,
+);
