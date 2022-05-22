00fbb9b530e604875c3fa7cdf0072123e191f148
airman5573 <rpf5573@kakao.com>
2022-05-22 14:41:11 +0900

    refactor(component): icon에 button을 입힌다

--- src/components/icons/delete-icon/DeleteIcon.jsx
+++ src/components/icons/delete-icon/DeleteIcon.jsx
@@ -1,8 +1,19 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
+import styles from "./delete-icon.module";
 
-function DeleteIcon() {
-  return <FontAwesomeIcon icon={faTrashCan} />;
+const cn = require("classnames");
+
+function DeleteIcon({ onClick, className }) {
+  return (
+    <button
+      type="button"
+      onClick={() => onClick && onClick()}
+      className={cn(styles.deleteIcon, className)}
+    >
+      <FontAwesomeIcon icon={faTrashCan} />
+    </button>
+  );
 }
 
 export default DeleteIcon;
