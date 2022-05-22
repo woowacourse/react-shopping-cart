4f9be7231020a2e89efd87fe625609ce94328bea
airman5573 <rpf5573@kakao.com>
2022-05-22 17:30:34 +0900

    chore(tailwind): pixel값을 사용한다
    tailwind에서 기본으로 제공하는 util대신 pixel단위의 util class를 사용한다

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
+/* eslint-disable camelcase */
+const px0_10 = { ...Array.from(Array(11)).map((_, i) => `${i}px`) };
+const px0_100 = { ...Array.from(Array(101)).map((_, i) => `${i}px`) };
+const px0_200 = { ...Array.from(Array(201)).map((_, i) => `${i}px`) };
+
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      borderWidth: px0_10,
+      fontSize: px0_100,
+      lineHeight: px0_100,
+      minWidth: px0_200,
+      minHeight: px0_200,
+      spacing: px0_200,
+    },
   },
   plugins: [],
 };
