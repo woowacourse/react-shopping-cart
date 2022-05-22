d7ff8ccd37db63e54c7c0f0d94ce4796e74daacb
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-05-23 02:47:21 +0900

    refactor: refactor details

--- src/page/ProductListPage/index.jsx
+++ src/page/ProductListPage/index.jsx
@@ -2,23 +2,20 @@ import { useCallback, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
 
-import ProductItem from 'components/ProductItem';
+import { ProductItem } from 'components';
 
 import store from 'store/store';
 import { doInitializeCart } from 'actions/actionCreator';
 
 import Styled from 'page/ProductListPage/index.style';
-import { SERVER_URL } from 'constants';
 
 const ProductListPage = () => {
   const { products } = useSelector(state => state.reducer);
 
   const getProducts = useCallback(async () => {
-    if (products.length > 0) {
-      return;
-    }
+    if (products.length > 0) return;
 
-    const response = await axios.get(SERVER_URL + 'products');
+    const response = await axios.get('products');
 
     store.dispatch(doInitializeCart({ products: response.data }));
   }, [products]);
