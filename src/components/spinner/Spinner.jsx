7c5c98bd724430abb390ed7118da8cd75d220f77
jswith <typingjs@gmail.com>
2022-05-22 17:48:09 +0900

    style(ui): 이미지 로딩 스피너 추가

---
+++ src/components/spinner/Spinner.jsx
@@ -0,0 +1,7 @@
+import StyledSpinner from "./Spinner.styled";
+
+function Spinner() {
+  return <StyledSpinner />;
+}
+
+export default Spinner;
--- src/pages/home/components/thumbnail/Thumbnail.jsx
+++ src/pages/home/components/thumbnail/Thumbnail.jsx
@@ -1,24 +1,15 @@
-import { css } from "@emotion/react";
 import { useState } from "react";
 import StyledThumbnail from "@/pages/home/components/thumbnail/Thumbnail.styled";
+import Spinner from "@/components/spinner/Spinner";
 
 function Thumbnail({ src, name }) {
   const [isLoaded, setIsLoaded] = useState(false);
   return (
     <StyledThumbnail className="thumbnail">
-      {!isLoaded && <div css={indicator}>isLoading</div>}
+      {!isLoaded && <Spinner />}
       <img src={src} alt={name} onLoad={() => setIsLoaded(true)} />
     </StyledThumbnail>
   );
 }
-
-const indicator = css`
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: white;
-`;
 
 export default Thumbnail;
