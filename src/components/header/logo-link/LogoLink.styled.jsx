8ea4f08c4a625f5b5f33518cf8afb8ac0683e755
jswith <typingjs@gmail.com>
2022-05-23 01:26:30 +0900

    style: logo-link h1 태그로 변경

--- src/components/header/logo-link/LogoLink.styled.jsx
+++ src/components/header/logo-link/LogoLink.styled.jsx
@@ -1,12 +1,9 @@
 import styled from "@emotion/styled";
 import { noneStyles } from "@/styled";
 
-const StyledLogoLink = styled.div`
+const StyledLogoLink = styled.h1`
   .logo-link {
     ${noneStyles.a}
-    display: inline-flex;
-    align-items: center;
-    justify-content: flex-start;
     color: #ffffff;
     font-size: 30px;
     font-weight: 900;
