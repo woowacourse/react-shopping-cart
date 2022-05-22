516415e88402d68aecc0648d76504def91b46592
Jeongmin Woo <greenblues1190@gmail.com>
2022-05-23 05:01:38 +0900

    feat: CartPage 라우터에 추가

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import theme from './styles/theme';
 import Header from './components/Header/Header';
 import MainPage from './pages/MainPage/MainPage';
 import ProductPage from './pages/ProductPage/ProductPage';
+import CartPage from './pages/CartPage/CartPage';
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
           <Routes>
             <Route path="/" element={<MainPage />} />
             <Route path="/product/:id" element={<ProductPage />} />
+            <Route path="/cart" element={<CartPage />} />
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </StyledContent>
@@ -31,6 +33,7 @@ const StyledContent = styled.div`
   min-height: 200px;
   margin: 60px 0;
   display: flex;
+  flex-direction: column;
   width: 100%;
   justify-content: center;
   align-items: center;
