1c452b2c9a2731cb2cf87245787cad30403c522b
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-05-22 11:52:59 +0900

    💄 : Button 컴포넌트 스타일 수정
    - 사이즈 지정이 없을 시 상위 요소의 사이즈에 따라 변경되도록 설정
    - 버튼 타입에서 아이콘/배경 컬러 변경

--- src/components/@common/Button/styles.js
+++ src/components/@common/Button/styles.js
@@ -5,47 +5,51 @@ import { LAYOUT, COLORS } from 'styles/theme';
 
 const BUTTON_TYPE = {
   BUTTON: css`
-    padding: 0.8rem 1.2rem;
+    padding: 0.6em 1.2em;
+    border: 1px solid ${COLORS.GRAY_150};
+    background-color: ${COLORS.WHITE};
+    border-radius: unset;
   `,
 
   ICON: css`
-    padding: 0.25rem 0.35rem;
+    padding: 0.25em 0.35em;
+    border: 1px solid ${COLORS.GRAY_150};
+    background-color: ${COLORS.GRAY_250};
   `,
 };
 
 const Container = styled.button`
   cursor: pointer;
 
-  font-size: 1rem;
-  border: 1px solid ${COLORS.GRAY_150};
-  background-color: ${COLORS.GRAY_250};
-  transition: background-color 0.3s ease, color 0.3s ease;
+  font-size: 1em;
+  transition: filter 0.3s ease, color 0.3s ease;
   border-radius: ${LAYOUT.BORDER_RADIUS}px;
+  color: inherit;
 
   ${({ containerType }) => BUTTON_TYPE[containerType]}
 
   &:hover {
-    color: ${COLORS.BLUE_150};
-    background-color: ${COLORS.GRAY_200};
+    color: ${COLORS.BLACK};
+    filter: brightness(0.95);
   }
 
   &::before {
     content: '\\${({ icon }) => icon}';
 
-    ${({ containerType }) =>
-      containerType === 'ICON' &&
-      css`
-        font-size: 0.8rem;
-      `}
     font-family: 'Font Awesome 6 Free';
     font-style: normal;
     font-weight: 900;
 
     ${({ containerType }) =>
-      containerType === 'BUTTON' &&
-      css`
-        padding-right: 0.5rem;
-      `}
+      (containerType === 'BUTTON' &&
+        css`
+          padding-right: 0.5em;
+          opacity: 0.7;
+        `) ||
+      (containerType === 'ICON' &&
+        css`
+          font-size: 0.8em;
+        `)}
   }
 `;
 
