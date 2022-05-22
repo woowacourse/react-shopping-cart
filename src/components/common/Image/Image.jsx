65c59919a62cf9fae89238f62e04f5fcb3297f87
Yo Wook Kim <uk960214@gmail.com>
2022-05-23 00:43:05 +0900

    refactor: 이미지에서 불필요한 height prop 제거

--- src/components/common/Image/Image.jsx
+++ src/components/common/Image/Image.jsx
@@ -4,13 +4,13 @@ import errorImage from 'components/common/Image/error.svg';
 
 import * as Styled from 'components/common/Image/Image.style';
 
-function Image({ src, alt, width = '100%', height = '100%' }) {
+function Image({ src, alt, width = '100%' }) {
   const handleError = (e) => {
     e.target.src = errorImage;
   };
 
   return (
-    <Styled.ImageContainer width={width} height={height}>
+    <Styled.ImageContainer width={width}>
       <Styled.Image src={src} alt={alt} onError={handleError} />
     </Styled.ImageContainer>
   );
