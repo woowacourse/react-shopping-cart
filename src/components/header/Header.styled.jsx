199fff334af6e0a427ee69afa859c120aae4c438
jswith <typingjs@gmail.com>
2022-05-23 01:32:06 +0900

    style: header margin 수정

--- src/components/header/Header.styled.jsx
+++ src/components/header/Header.styled.jsx
@@ -2,7 +2,6 @@ import styled from "@emotion/styled";
 
 const StyledHeader = styled.header`
   background-color: #03cf5b;
-  margin-bottom: 25px;
 
   position: sticky;
   z-index: 999;
@@ -11,7 +10,6 @@ const StyledHeader = styled.header`
   left: 0;
 
   .logo-link {
-    padding: 10px 0;
     flex-shrink: 0;
     width: 300px;
   }
--- src/components/header/logo-link/LogoLink.styled.jsx
+++ src/components/header/logo-link/LogoLink.styled.jsx
@@ -4,6 +4,9 @@ import { noneStyles } from "@/styled";
 const StyledLogoLink = styled.h1`
   .logo-link {
     ${noneStyles.a}
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
     color: #ffffff;
     font-size: 30px;
     font-weight: 900;
