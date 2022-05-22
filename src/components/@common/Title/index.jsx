a75c1e53972957652848302e02eebb61a0b1a05a
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-05-22 12:02:14 +0900

    🚚 : PageTItle 컴포넌트 Title으로 이름 변경

--- src/components/@common/Title/index.jsx
+++ src/components/@common/Title/index.jsx
@@ -5,7 +5,7 @@ import Icon from 'components/@common/Icon';
 
 import * as S from './styles';
 
-function PageTitle({ size, description, children }) {
+function Title({ size, description, children }) {
   return (
     <S.Container size={size}>
       <S.PageTitle>{children}</S.PageTitle>
@@ -18,14 +18,14 @@ function PageTitle({ size, description, children }) {
   );
 }
 
-PageTitle.defaultProps = {
+Title.defaultProps = {
   size: 24,
   description: null,
 };
 
-PageTitle.propTypes = {
+Title.propTypes = {
   size: PropTypes.number,
   description: PropTypes.string,
 };
 
-export default PageTitle;
+export default Title;
--- src/components/@common/Title/index.stories.js
+++ src/components/@common/Title/index.stories.js
@@ -1,15 +1,15 @@
-import PageTitle from './index';
+import Title from './index';
 
 export default {
-  title: 'Component/@Common/PageTitle',
-  component: PageTitle,
+  title: 'Component/@Common/Title',
+  component: Title,
   parameters: {
     layout: 'fullscreen',
   },
   argTypes: {},
 };
 
-const Template = (args) => <PageTitle {...args} />;
+const Template = (args) => <Title {...args} />;
 
 export const DefaultTitle = Template.bind({});
 DefaultTitle.args = { description: '페이지 설명', children: '타이틀' };
