9e6c38e049e5ef53e0f65bee1028bca9830c7d0e
airman5573 <rpf5573@kakao.com>
2022-05-23 16:47:45 +0900

    refactor(redux): 상위에서 state를 가져온다
    주소창에서 바로 장바구니로 넘어간 경우 ProductList를 거쳐가지 않았기 때문에 상품 정보가 없다. 그래서 상위에서 상품 정보를 가져온다

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "@shared/header/Header";
 import styles from "@/app.module";
 import Home from "@home/Home";
 import Cart from "@cart/Cart";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import createAction from "@redux/createAction";
+import { useDispatch, useSelector } from "react-redux";
+import ACTION_TYPE from "@redux/actions";
+import Alert from "@shared/alert/Alert";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const productList = useSelector(({ productList }) => productList);
+  const dispatch = useDispatch();
+  const getProductList = () => {
+    return async (dispatch) => {
+      // eslint-disable-next-line no-undef
+      const fetchResult = await fetch(`${API_URL}/products`);
+      const productList = await fetchResult.json();
+      setLoading(false);
+      setError(null);
+      dispatch(createAction(ACTION_TYPE.UPDATE_PRODUCT_LIST, productList));
+    };
+  };
+  useEffect(() => {
+    dispatch(getProductList());
+  }, [dispatch]);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error!</p>;
+  if (productList.length === 0)
+    return <Alert variant="danger">상품이 비어있습니다</Alert>;
+
   return (
     <div>
       <BrowserRouter>
--- src/pages/home/components/product-list/ProductList.jsx
+++ src/pages/home/components/product-list/ProductList.jsx
@@ -1,34 +1,11 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import ACTION_TYPE from "@redux/actions";
-import createAction from "@redux/createAction";
+import { useSelector } from "react-redux";
 import ProductItem from "@home/components/product-item/ProductItem";
 import styles from "@home/components/product-list/product-list.module";
 
 const cn = require("classnames");
 
 function ProductList({ className }) {
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const productList = useSelector(({ productList }) => productList);
-  const dispatch = useDispatch();
-  const getProductList = () => {
-    return async (dispatch) => {
-      // eslint-disable-next-line no-undef
-      const fetchResult = await fetch(`${API_URL}/products`);
-      const productList = await fetchResult.json();
-      setLoading(false);
-      setError(null);
-      dispatch(createAction(ACTION_TYPE.UPDATE_PRODUCT_LIST, productList));
-    };
-  };
-
-  useEffect(() => {
-    dispatch(getProductList());
-  }, []);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error!</p>;
+  const productList = useSelector((state) => state.productList);
   return (
     <div className={cn(styles.productList, className)}>
       {productList.map((item) => (
