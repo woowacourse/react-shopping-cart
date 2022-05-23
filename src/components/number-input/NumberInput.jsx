3cf505fa855d9242e0c796c51bdb8018049cce45
airman5573 <rpf5573@kakao.com>
2022-05-23 16:20:13 +0900

    refactor(component): 상태 관리를 상위에 맡긴다

--- src/components/number-input/NumberInput.jsx
+++ src/components/number-input/NumberInput.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect, flushSync } from "react";
 import CaretUp from "@assets/images/caret-up.svg";
 import CaretDown from "@assets/images/caret-down.svg";
 import styles from "./number-input.module";
@@ -13,8 +12,6 @@ function NumberInput({
   maxLength = 3,
   className,
 }) {
-  const [val, setVal] = useState(value);
-
   const handleChange = (e) => {
     const { target } = e;
     const { selectionStart } = target;
@@ -23,32 +20,29 @@ function NumberInput({
     if (newValue.length < target.value.length) {
       cursor = Math.max(0, selectionStart - 1);
     }
-    flushSync(() => {
-      setVal(newValue);
-    });
+
     queueMicrotask(() => {
       target.setSelectionRange(cursor, cursor);
     });
+    onChange && onChange(newValue);
   };
 
-  const handleIncrease = () => setVal((val) => Number(val) + step);
+  const handleIncrease = () => {
+    const newValue = Number(value) + step;
+    onChange && onChange(newValue);
+  };
 
-  const handleDecrease = () =>
-    setVal((val) => {
-      const newValue = Number(val) - step;
-      return positive ? Math.max(0, newValue) : newValue;
-    });
-
-  useEffect(() => {
-    onChange && onChange(Number(val));
-  }, [val]);
+  const handleDecrease = () => {
+    const newValue = Number(value) - step;
+    onChange && onChange(positive ? Math.max(0, newValue) : newValue);
+  };
 
   return (
     <div className={cn(styles.numberInput, className)}>
       <input
         type="text"
         onChange={handleChange}
-        value={val}
+        value={value}
         maxLength={maxLength}
       />
       <div className={styles.steps}>
