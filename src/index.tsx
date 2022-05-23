21f6725fdf44c54410f1058aa9099770f635d5c2
JUDONGHYEOK <judonghyeok@gmail.com>
2022-05-23 14:53:19 +0900

    feat: msw 추가

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,15 @@ import { Provider } from "react-redux";
 import ThemeProvider from "./ThemeProvider";
 import { store } from "./modules";
 
+if (process.env.NODE_ENV === "development") {
+  const { worker } = require("./mocks/browser.ts");
+  worker.start({
+    serviceWorker: {
+      url: "/react-shopping-cart/mockServiceWorker.js",
+    },
+  });
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
