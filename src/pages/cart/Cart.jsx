d90f38f4c921804f95159128045946552541f7cb
airman5573 <rpf5573@kakao.com>
2022-05-23 16:20:59 +0900

    refactor(component): 상태값이 있는 경우에 렌더링 한다

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,13 +1,33 @@
 import styles from "@cart/cart.module";
 import PageTitle from "@shared/page-title/PageTitle";
+import { useSelector } from "react-redux";
+import productObjsEquality from "@redux/equalities/productObjsEquality";
+import Alert from "@shared/alert/Alert";
+import CartForm from "./components/cart-form/CartForm";
+import CartTotal from "./components/cart-total/CartTotal";
 
 const cn = require("classnames");
 
 function Cart() {
+  const productObjs = useSelector(
+    (state) => state.productObjs,
+    productObjsEquality
+  );
+  const isEmptyProductObjs = Object.keys(productObjs).length === 0;
   return (
     <div className="wrapper">
       <div className={cn(styles.cart)}>
         <PageTitle>장바구니</PageTitle>
+        {isEmptyProductObjs ? (
+          <Alert variant="danger" className="mt-30">
+            상품 목록이 비어있습니다
+          </Alert>
+        ) : (
+          <div className="flex space-between">
+            <CartForm />
+            <CartTotal />
+          </div>
+        )}
       </div>
     </div>
   );
