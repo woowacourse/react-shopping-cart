b87b0c746212b0fa0cf14ddddd2dd98ef529c3f7
KIM YEJI <rladpwl0512@gmail.com>
2022-05-23 11:23:18 +0900

    :sparkles: feat: 홈버튼 클릭 시, 메인 페이지로 이동

--- src/containers/HomeButton/index.jsx
+++ src/containers/HomeButton/index.jsx
@@ -4,21 +4,26 @@ import Image from 'components/Image';
 import WhiteButton from 'components/WhiteButton';
 import MarginWrapper from 'components/MarginWrapper';
 import FlexWrapper from 'components/FlexWrapper';
+import { Link } from 'react-router-dom';
 
 function HomeButton() {
   return (
     <FlexWrapper alignItmes="center">
       <MarginWrapper marginRight="14px">
-        <Image
-          src={process.env.PUBLIC_URL + '/img/shopping-cart-white.png'}
-          width="50"
-          height="44"
-          alt="홈페이지 로고"
-        />
+        <Link to="/react-shopping-cart">
+          <Image
+            src={process.env.PUBLIC_URL + '/img/shopping-cart-white.png'}
+            width="50"
+            height="44"
+            alt="홈페이지 로고"
+          />
+        </Link>
       </MarginWrapper>
-      <WhiteButton fontSize="2.5rem" fontWeight="700">
-        APEACH SHOP
-      </WhiteButton>
+      <Link to="/react-shopping-cart">
+        <WhiteButton fontSize="2.5rem" fontWeight="700">
+          APEACH SHOP
+        </WhiteButton>
+      </Link>
     </FlexWrapper>
   );
 }
