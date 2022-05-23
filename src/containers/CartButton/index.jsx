a0f8c8396750c62ba8938ec41613f981bdc80063
KIM YEJI <rladpwl0512@gmail.com>
2022-05-23 10:33:45 +0900

    :sparkles: feat: 장바구니에 같은 상품 넣었을 때, 수량 상태 업데이트

--- src/containers/CartButton/index.jsx
+++ src/containers/CartButton/index.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 import Image from 'components/Image';
 import { useDispatch, useSelector } from 'react-redux';
-import { requestCartAdd, requestCartAddFail } from 'modules/cart';
+import { requestCartAdd, requestCartAddFail, requestExistProductAdd } from 'modules/cart';
 import CartButtonStyled from './style';
 
 function CartButton({ id }) {
@@ -11,11 +11,26 @@ function CartButton({ id }) {
   const dispatch = useDispatch();
 
   const handleCartButtonClick = async () => {
+    // 만약, id가 carts에 이미 존재한다면 -> 개수를 추가한다
+    // 최초 추가될 때, product 속성에 개수 속성:1을 더해줄까?
+    const productsId = carts.map((cart) => Number(cart.id));
+    if (productsId.includes(id)) {
+      // 수량을 업데이트 해주는 dispatch 액션 추가
+
+      // 1. id가 같은 인덱스를 찾는다
+      const productIdx = productsId.indexOf(id);
+      // 2. 그 인덱스의 qunatity에 +1을 해준다 (위에서 인덱스 던져주기, dispatch 액션)
+      const newCarts = [...carts];
+      newCarts[productIdx].quantity += 1;
+
+      dispatch(requestExistProductAdd(newCarts));
+      return;
+    }
     try {
       const res = await axios.get(`/products/${id}`);
       const product = await res.data;
 
-      dispatch(requestCartAdd(product));
+      dispatch(requestCartAdd({ ...product, quantity: 1 }));
     } catch (error) {
       dispatch(requestCartAddFail(error));
     }
--- src/modules/cart.js
+++ src/modules/cart.js
@@ -1,7 +1,9 @@
 const REQUEST_CART_ADD = 'REQUEST_CART_ADD';
 const REQUEST_CART_ADD_FAIL = 'REQUEST_CART_ADD_FAIL';
+const REQUEST_EXIST_PRODUCT_ADD = 'REQUEST_EXIST_PRODUCT_ADD';
 
 export const requestCartAdd = (product) => ({ type: REQUEST_CART_ADD, product });
+export const requestExistProductAdd = (newCarts) => ({ type: REQUEST_EXIST_PRODUCT_ADD, newCarts });
 export const requestCartAddFail = (error) => ({ type: REQUEST_CART_ADD_FAIL, error });
 
 const initialState = {
@@ -20,6 +22,11 @@ export default function cart(state = initialState, action) {
       return {
         ...state,
         errorMessage: action.error,
+      };
+    case REQUEST_EXIST_PRODUCT_ADD:
+      return {
+        ...state,
+        carts: action.newCarts,
       };
     default:
       return state;
