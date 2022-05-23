ae5595d1aa97f3ed2f995039b05cf991d0316f4f
moonheekim0118 <hellomooneekim@gmail.com>
2022-05-23 20:35:39 +0900

    refactor: 상품 목록 Redux 제거

--- src/pages/ProductList/index.jsx
+++ src/pages/ProductList/index.jsx
@@ -4,48 +4,60 @@ import ProductContainer from 'components/ProductContainer/ProductContainer';
 import ProductItem from 'components/ProductItem/ProductItem';
 import Skeleton from 'components/Skeleton/Skeleton';
 import ImgWrapper from 'components/ImgWrapper/ImgWrapper';
-import useProducts from 'hooks/useProducts';
 import useAddCartItem from 'hooks/useAddCartItem';
+import useFetch from 'hooks/useFetch';
+import { useEffect } from 'react';
 
 const ProductList = () => {
-  const { isLoading, isSucceed, isError, products } = useProducts();
+  const {
+    isLoading,
+    isError,
+    data: products,
+    fetchData,
+  } = useFetch({
+    method: 'get',
+    url: '/products',
+  });
+
   const { addCarItem } = useAddCartItem();
-  const isEmpty = !isLoading && products.length === 0;
+  const isEmpty = products && !isLoading && products.length === 0;
+
+  useEffect(() => {
+    if (!products) {
+      fetchData();
+    }
+  }, []);
 
   const handleClickCartButton = (id) => (e) => {
     e.stopPropagation();
     addCarItem(id);
   };
 
-  const getLoadingStatus = () => {
-    return Array.from({ length: 8 }).map((_, index) => (
-      <Skeleton key={index} sizeType="small" />
-    ));
-  };
+  if (isError) return <ImgWrapper src={errorApiImg} />;
+  if (isEmpty) return <ImgWrapper src={emptyImg} />;
+  if (isLoading)
+    return (
+      <ProductContainer>
+        {Array.from({ length: 8 }).map((_, index) => (
+          <Skeleton key={index} sizeType="small" />
+        ))}
+      </ProductContainer>
+    );
 
   return (
-    <>
-      {isError ? (
-        <ImgWrapper src={errorApiImg} />
-      ) : isEmpty ? (
-        <ImgWrapper src={emptyImg} />
-      ) : (
-        <ProductContainer>
-          {isLoading && getLoadingStatus()}
-          {isSucceed &&
-            products.map(({ name, price, imgUrl, id }) => (
-              <ProductItem
-                id={id}
-                name={name}
-                price={price}
-                imgUrl={imgUrl}
-                key={id}
-                onClickCartButton={handleClickCartButton(id)}
-              />
-            ))}
-        </ProductContainer>
-      )}
-    </>
+    <ProductContainer>
+      {products &&
+        products.map(({ name, price, imgUrl, id }) => (
+          <ProductItem
+            id={id}
+            name={name}
+            price={price}
+            imgUrl={imgUrl}
+            key={id}
+            onClickCartButton={handleClickCartButton(id)}
+          />
+        ))}
+    </ProductContainer>
   );
 };
 
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import productsReducer from 'reducers/products/products.reducer';
 import cartReducer from 'reducers/cart/cart.reducer';
 
 const rootReducer = () =>
   combineReducers({
-    products: productsReducer,
     cart: cartReducer,
   });
 
