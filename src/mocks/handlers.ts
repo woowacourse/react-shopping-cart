d3151601ea7084cd64f20332cb63effec0f0a88a
이아현 <lah1203@naver.com>
2022-05-23 16:18:56 +0900

    refactor: msw handler filter 메서드 변경
    - https://github.com/woowacourse/react-shopping-cart/pull/108#discussion_r878790177
    - filter에서 좀 더 어울리는 메서드인 find로 변경

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -10,9 +10,9 @@ const handlers = [
     return res(ctx.status(200), ctx.json(db.products));
   }),
   rest.get(`${baseURL}${PATH.REQUEST_PRODUCT}/:id`, (req, res, ctx) => {
-    const product = db.products.filter(
+    const product = db.products.find(
       (productInfo) => productInfo.id === Number(req.params.id)
-    )[0];
+    );
 
     return res(ctx.status(200), ctx.json(product));
   }),
