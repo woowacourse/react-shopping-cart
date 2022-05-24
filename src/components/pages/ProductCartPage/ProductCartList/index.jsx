649629c4aeb8d317cfc19bbcf84dce12b9617e5f
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-05-24 21:55:33 +0900

    refactor: handle~ 함수 로직 이동
    ProductCartPage -> ProductCartList 컴포넌트로 이동

--- src/components/pages/ProductCartPage/ProductCartList/index.jsx
+++ src/components/pages/ProductCartPage/ProductCartList/index.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+import { useStore } from "hooks/useStore";
+import { deleteCartList, updateCartCount } from "reducers/cartList";
+
 import CheckBox from "components/common/CheckBox";
 import ProductCartItem from "../ProductCartItem";
 import {
@@ -7,17 +10,76 @@ import {
   CartListCount,
   DeleteCartButton,
 } from "./styled";
+import Spinner from "components/common/Spinner";
+import ErrorPage from "components/pages/ErrorPage";
 
-function ProductCartList({
-  cartList,
-  checkList,
-  handleChangeAllCheckbox,
-  handleDeleteAllItem,
-  handleClickIncreaseButton,
-  handleClickDecreaseButton,
-  handleClickDeleteItemButton,
-  handleChangeCheckbox,
-}) {
+function ProductCartList({ checkList, setCheckList }) {
+  const {
+    data: cartList,
+    isLoading,
+    errorMessage,
+    dispatch,
+  } = useStore("cartList");
+
+  const handleChangeAllCheckbox = () => {
+    if (checkList.length === 0) {
+      setCheckList(cartList.map((cartItem) => cartItem.id));
+      return;
+    }
+    setCheckList([]);
+  };
+
+  const handleDeleteAllItem = () => {
+    checkList.forEach((carItemId) => {
+      dispatch(deleteCartList(carItemId));
+    });
+    setCheckList([]);
+  };
+
+  const handleClickIncreaseButton = (id) => () => {
+    if (!checkList.includes(id)) setCheckList((prev) => [...prev, id]);
+    dispatch(updateCartCount(id, "increase"));
+  };
+
+  const handleClickDecreaseButton = (id, count) => () => {
+    if (count <= 1) return;
+    if (!checkList.includes(id)) setCheckList((prev) => [...prev, id]);
+    dispatch(updateCartCount(id, "decrease"));
+  };
+
+  const handleClickDeleteItemButton = (id) => () => {
+    dispatch(deleteCartList(id));
+    setCheckList((prev) => prev.filter((cartItemId) => cartItemId !== id));
+  };
+
+  const handleChangeCheckbox = (id) => () => {
+    if (checkList.includes(id)) {
+      setCheckList((prev) => prev.filter((cartItemId) => cartItemId !== id));
+      return;
+    }
+    setCheckList((prev) => [...prev, id]);
+  };
+
+  const renderListContent = () => {
+    if (isLoading) return <Spinner />;
+    if (errorMessage) return <ErrorPage>에러: ${errorMessage} </ErrorPage>;
+    return (
+      <>
+        {cartList.map((cartItem) => (
+          <ProductCartItem
+            product={cartItem}
+            checkList={checkList}
+            handleClickIncreaseButton={handleClickIncreaseButton}
+            handleClickDecreaseButton={handleClickDecreaseButton}
+            handleClickDeleteItemButton={handleClickDeleteItemButton}
+            handleChangeCheckbox={handleChangeCheckbox}
+            key={cartItem.id}
+          />
+        ))}
+      </>
+    );
+  };
+
   return (
     <>
       <CartListControlContainer>
@@ -34,17 +96,7 @@ function ProductCartList({
       <CartListCount>
         든든배송 상품 ({cartList?.length ?? "%ERROR%"}개)
       </CartListCount>
-      {cartList.map((cartItem) => (
-        <ProductCartItem
-          product={cartItem}
-          checkList={checkList}
-          handleClickIncreaseButton={handleClickIncreaseButton}
-          handleClickDecreaseButton={handleClickDecreaseButton}
-          handleClickDeleteItemButton={handleClickDeleteItemButton}
-          handleChangeCheckbox={handleChangeCheckbox}
-          key={cartItem.id}
-        />
-      ))}
+      {renderListContent()}
     </>
   );
 }
--- src/components/pages/ProductCartPage/index.jsx
+++ src/components/pages/ProductCartPage/index.jsx
@@ -1,14 +1,8 @@
 import React, { useEffect, useState } from "react";
 
 import { useStore } from "hooks/useStore";
-import {
-  deleteCartList,
-  getCartList,
-  updateCartCount,
-} from "reducers/cartList";
+import { getCartList } from "reducers/cartList";
 
-import Spinner from "components/common/Spinner";
-import ErrorPage from "components/pages/ErrorPage";
 import PaymentAmount from "./PaymentAmount";
 import ProductCartList from "./ProductCartList";
 import {
@@ -19,54 +13,9 @@ import {
 } from "./styled";
 
 function ProductCartPage() {
-  const {
-    data: cartList,
-    isLoading,
-    errorMessage,
-    dispatch,
-  } = useStore("cartList");
+  const { data: cartList, dispatch } = useStore("cartList");
 
-  console.log(cartList);
   const [checkList, setCheckList] = useState([]);
-
-  const handleChangeAllCheckbox = () => {
-    if (checkList.length === 0) {
-      setCheckList(cartList.map((cartItem) => cartItem.id));
-      return;
-    }
-    setCheckList([]);
-  };
-
-  const handleDeleteAllItem = () => {
-    checkList.forEach((carItemId) => {
-      dispatch(deleteCartList(carItemId));
-    });
-    setCheckList([]);
-  };
-
-  const handleClickIncreaseButton = (id) => () => {
-    if (!checkList.includes(id)) setCheckList((prev) => [...prev, id]);
-    dispatch(updateCartCount(id, "increase"));
-  };
-
-  const handleClickDecreaseButton = (id, count) => () => {
-    if (count <= 1) return;
-    if (!checkList.includes(id)) setCheckList((prev) => [...prev, id]);
-    dispatch(updateCartCount(id, "decrease"));
-  };
-
-  const handleClickDeleteItemButton = (id) => () => {
-    dispatch(deleteCartList(id));
-    setCheckList((prev) => prev.filter((cartItemId) => cartItemId !== id));
-  };
-
-  const handleChangeCheckbox = (id) => () => {
-    if (checkList.includes(id)) {
-      setCheckList((prev) => prev.filter((cartItemId) => cartItemId !== id));
-      return;
-    }
-    setCheckList((prev) => [...prev, id]);
-  };
 
   const [totalPrice, totalCount] = cartList.reduce(
     (acc, { id, price, count }) => {
@@ -78,23 +27,6 @@ function ProductCartPage() {
     },
     [0, 0]
   );
-
-  const renderListContent = () => {
-    if (isLoading) return <Spinner />;
-    if (errorMessage) return <ErrorPage>에러: ${errorMessage} </ErrorPage>;
-    return (
-      <ProductCartList
-        cartList={cartList}
-        checkList={checkList}
-        handleChangeAllCheckbox={handleChangeAllCheckbox}
-        handleDeleteAllItem={handleDeleteAllItem}
-        handleClickIncreaseButton={handleClickIncreaseButton}
-        handleClickDecreaseButton={handleClickDecreaseButton}
-        handleClickDeleteItemButton={handleClickDeleteItemButton}
-        handleChangeCheckbox={handleChangeCheckbox}
-      />
-    );
-  };
 
   useEffect(() => {
     dispatch(getCartList());
@@ -108,7 +40,9 @@ function ProductCartPage() {
   return (
     <CartPageContainer>
       <CartPageHeader>장바구니</CartPageHeader>
-      <CartPageList>{renderListContent()}</CartPageList>
+      <CartPageList>
+        <ProductCartList checkList={checkList} setCheckList={setCheckList} />
+      </CartPageList>
       <CartPagePayment>
         <PaymentAmount
           position="sticky"
