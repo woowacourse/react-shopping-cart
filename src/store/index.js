df131e37c8a40f7f3d63d439f85376d2dd0bb68b
Ahn <tn3748@naver.com>
2022-05-24 17:47:27 +0900

    feat: productList reducer 삭제

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,9 @@
 import { legacy_createStore as createStore, applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import ReduxThunk from 'redux-thunk';
-import productReducer from './product/product.reducer';
 import cartReducer from './cart/cart.reducer';
 
 const rootReducer = combineReducers({
-  product: productReducer,
   cart: cartReducer,
 });
 
