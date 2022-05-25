f755d491c7683256a744629682e60fda57b196eb
woose28 <dldntmdsla@naver.com>
2022-05-25 20:36:56 +0900

    refactor: ProductList 페이지 조건부 렌더링 수정
    - early return 방식으로 조건부 렌더링 적용

--- react-shopping-cart/src/pages/ProductList/ProductList.page.jsx
+++ react-shopping-cart/src/pages/ProductList/ProductList.page.jsx
@@ -19,15 +19,17 @@ function ProductList() {
     }
   }, []);
 
+  if (status === STATUS.LOADING) {
+    return <Loading style={{ marginTop: '60px' }} />;
+  }
+
+  if (status === STATUS.ERROR) {
+    return <Error style={{ marginTop: '60px' }}>서버에 연결할 수 없습니다.</Error>;
+  }
+
   return (
     <FlexWrapper style={{ margin: '60px 0 60px' }} isColumnDirection={true}>
-      {status === STATUS.LOADING ? (
-        <Loading />
-      ) : status === STATUS.ERROR ? (
-        <Error>서버에 연결할 수 없습니다.</Error>
-      ) : (
-        <ProductListContainer productList={productList} />
-      )}
+      <ProductListContainer productList={productList} />
     </FlexWrapper>
   );
 }
