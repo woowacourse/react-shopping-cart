89465cb18715dd2d5b63d274070963654151c491
kimsanglok <evertree6031@gmail.com>
2022-05-26 01:58:49 +0900

    refactor: debounce - 유틸함수로 분리

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,3 +7,12 @@ export const isExistInList = <T extends Primitive>(list: T[], value: T) => {
 export const sum = (list: number[]) => {
   return list.reduce((acc, cur) => (acc += cur), 0);
 };
+
+export const debounce = (callback: any, time: number) => {
+  let timer: ReturnType<typeof setTimeout>;
+
+  return () => {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(callback, time);
+  };
+};
