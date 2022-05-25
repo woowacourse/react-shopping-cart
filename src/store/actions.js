27b1d79b6037b0bec0418dbce43c186ade307d7d
Soyi Jeon <soyi.dev@gmail.com>
2022-05-25 20:34:05 +0900

    refactor: actionCreator 제거
    - thunk 내부에서 직접 action type과 payload를 넣어주도록 변경

--- src/store/actions.js
+++ src/store/actions.js
@@ -18,67 +18,44 @@ export const ACTIONS = {
   DELETE_CART_ITEM_ERROR: "DELETE_CART_ITEM_ERROR",
 };
 
-const actionsCreator = {
-  getCartItemList: () => ({ type: ACTIONS.GET_CART_ITEM_LIST }),
-  getCartItemListSuccess: (cartItemList) => ({
-    type: ACTIONS.GET_CART_ITEM_LIST_SUCCESS,
-    payload: cartItemList,
-  }),
-  getCartItemListError: (errMessage) => ({
-    type: ACTIONS.GET_CART_ITEM_LIST_ERROR,
-    payload: errMessage,
-  }),
-
-  postCartItem: () => ({ type: ACTIONS.POST_CART_ITEM }),
-  postCartItemSuccess: (cartItem) => ({
-    type: ACTIONS.POST_CART_ITEM_SUCCESS,
-    payload: cartItem,
-  }),
-  postCartItemError: (errMessage) => ({
-    type: ACTIONS.POST_CART_ITEM_ERROR,
-    payload: errMessage,
-  }),
-
-  deleteCartItem: () => ({ type: ACTIONS.DELETE_CART_ITEM }),
-  deleteCartItemSuccess: (cartItem) => ({
-    type: ACTIONS.DELETE_CART_ITEM_SUCCESS,
-    payload: cartItem,
-  }),
-  deleteCartItemError: (errMessage) => ({
-    type: ACTIONS.DELETE_CART_ITEM_ERROR,
-    payload: errMessage,
-  }),
-};
-
 export const getCartItemList = () => async (dispatch, getState) => {
-  dispatch(actionsCreator.getCartItemList());
+  dispatch({ type: ACTIONS.GET_CART_ITEM_LIST });
 
   try {
     const fetchedCartItemList = await requestGetCartItemList();
-    dispatch(actionsCreator.getCartItemListSuccess(fetchedCartItemList));
+    dispatch({
+      type: ACTIONS.GET_CART_ITEM_LIST_SUCCESS,
+      payload: fetchedCartItemList,
+    });
   } catch (err) {
-    dispatch(actionsCreator.getCartItemListError(err.message));
+    dispatch({ type: ACTIONS.GET_CART_ITEM_LIST_ERROR, payload: err.message });
   }
 };
 
 export const postCartItem = (productList) => async (dispatch, getState) => {
-  dispatch(actionsCreator.postCartItem());
+  dispatch({ type: ACTIONS.POST_CART_ITEM });
 
   try {
     const newCartItemList = await requestPostCartItem(productList);
-    dispatch(actionsCreator.postCartItemSuccess(newCartItemList));
+    dispatch({
+      type: ACTIONS.POST_CART_ITEM_SUCCESS,
+      payload: newCartItemList,
+    });
   } catch (err) {
-    dispatch(actionsCreator.postCartItemError(err.message));
+    dispatch({ type: ACTIONS.POST_CART_ITEM_ERROR, payload: err.message });
   }
 };
 
 export const deleteCartItem = (productIdList) => async (dispatch, getState) => {
-  dispatch(actionsCreator.deleteCartItem());
+  dispatch({ type: ACTIONS.DELETE_CART_ITEM });
 
   try {
     const newCartItemList = await requestDeleteCartItem(productIdList);
-    dispatch(actionsCreator.deleteCartItemSuccess(newCartItemList));
+    dispatch({
+      type: ACTIONS.DELETE_CART_ITEM_SUCCESS,
+      payload: newCartItemList,
+    });
   } catch (err) {
-    dispatch(actionsCreator.deleteCartItemError(err.message));
+    dispatch({ type: ACTIONS.DELETE_CART_ITEM_ERROR, payload: err.message });
   }
 };
