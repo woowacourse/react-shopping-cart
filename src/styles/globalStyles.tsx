49c08272f8b0cbbd4d952eb79bec4f140cdcc94a
kimsanglok <evertree6031@gmail.com>
2022-05-25 15:14:48 +0900

    chore: globalStyle prettier 적용

--- src/styles/globalStyles.tsx
+++ src/styles/globalStyles.tsx
@@ -1,43 +1,65 @@
-import { createGlobalStyle } from 'styled-components';
+import * as styled from 'styled-components';
 
-const globalStyles = createGlobalStyle`
+const globalStyles = styled.createGlobalStyle`
   * {
     box-sizing: border-box;
   }
 
-  body, div, span,
-  h1, h2, h3, h4, h5, h6, p,
-  a,ol, ul, li,fieldset, form, label, 
-  article, figure, figcaption, footer, header, nav, section
-  {
+  body,
+  div,
+  span,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  a,
+  ol,
+  ul,
+  li,
+  fieldset,
+  form,
+  label,
+  article,
+  figure,
+  figcaption,
+  footer,
+  header,
+  nav,
+  section {
     margin: 0;
     padding: 0;
     border: 0;
     font-size: 100%;
     font: inherit;
-    vertical-align: baseline;    
+    vertical-align: baseline;
   }
-  
-  body{
-    margin : 0;
+
+  body {
+    margin: 0;
     line-height: 1;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu',
+      'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
 
-  a{
-    text-decoration:none;
+  a {
+    text-decoration: none;
     color: inherit;
   }
 
-  footer, header, nav, section {
+  footer,
+  header,
+  nav,
+  section {
     display: block;
   }
 
-  ol, ul {
+  ol,
+  ul {
     list-style: none;
   }
 
