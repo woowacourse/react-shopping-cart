c5bd182a8f4843e9a49106622b7b204c379d5573
kamwoo <rkadndud06@naver.com>
2022-05-25 17:47:35 +0900

    test: reducer 각 테스트 실행전 상태 초기화 로직 추가

--- src/store/reducer/cartReducer.test.js
+++ src/store/reducer/cartReducer.test.js
@@ -7,6 +7,10 @@ describe('장바구니 상품 저장상태 테스트', () => {
     cartedProductsState = cartReducer(cartedProductsState, action);
   };
 
+  beforeEach(() => {
+    cartedProductsState = [];
+  });
+
   const product = {
     id: 1,
     image: 'https://i.pinimg.com/474x/4a/47/d5/4a47d5956eb090ff702e3b2cb47fdf98.jpg',
@@ -36,16 +40,30 @@ describe('장바구니 상품 저장상태 테스트', () => {
     });
 
     reducer({
+      type: CART_ACTION_TYPES.ADD_PRODUCT,
+      payload: {
+        product,
+      },
+    });
+
+    reducer({
       type: CART_ACTION_TYPES.SUBTRACT_PRODUCT,
       payload: {
         product,
       },
     });
 
-    expect(cartedProductsState[0]).toEqual(product);
+    expect(cartedProductsState[0].count).toEqual(1);
   });
 
   test('해당 상품 클릭 요청이 들어오면 장바구니에 해당 상품을 클릭 상태를 변경할 수 있다.', () => {
+    reducer({
+      type: CART_ACTION_TYPES.ADD_PRODUCT,
+      payload: {
+        product,
+      },
+    });
+
     reducer({
       type: CART_ACTION_TYPES.CHECK_PRODUCT,
       payload: {
--- src/store/reducer/snackBarReducer.test.js
+++ src/store/reducer/snackBarReducer.test.js
@@ -7,6 +7,12 @@ describe('스낵바 메세지 저장상태 테스트', () => {
     messageState = snackBarReducer(messageState, action);
   };
 
+  beforeEach(() => {
+    messageState = {
+      text: '',
+    };
+  });
+
   test('메세지를 전달하면 저장할 수 있다.', () => {
     const message = '안녕하세요.';
 
@@ -21,6 +27,15 @@ describe('스낵바 메세지 저장상태 테스트', () => {
   });
 
   test('메세지 초기화 요청이 들어오면 저장된 메세지를 비울 수 있다.', () => {
+    const message = '안녕하세요.';
+
+    reducer({
+      type: SNACKBAR_ACTION_TYPE.NEW_MESSAGE,
+      payload: {
+        message,
+      },
+    });
+
     reducer({
       type: SNACKBAR_ACTION_TYPE.CLEAR_MESSAGE,
     });
