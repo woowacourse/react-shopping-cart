5a9ae9e7260c29357658f30c45a6434a2872ec3a
이아현 <lah1203@naver.com>
2022-05-25 23:37:11 +0900

    refactor: 장바구니 성공 section 분리

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -4,14 +4,11 @@ import {
   Product,
   ProductStoreState,
 } from 'types/index';
-import { useCallback, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 
-import CART_MESSAGE from 'constants/message';
 import CONDITION from 'constants/condition';
-import CartItem from 'components/CartItem/CartItem';
-import CheckBox from 'components/@shared/CheckBox';
-import { cartActions } from 'redux/actions';
+import CartContent from 'components/CartContent/CartContent';
 import { getProducts } from 'redux/thunks';
 import styled from 'styled-components';
 
@@ -48,93 +45,18 @@ function CartPage() {
     );
   }, [cart, productList]);
 
-  const calculateTotalMoney = useCallback(() => {
-    return cartItems.reduce((prevMoney, item) => {
-      const { product, stock, checked } = item;
-
-      if (!checked) return prevMoney;
-
-      return prevMoney + product.price * stock;
-    }, 0);
-  }, [cartItems]);
-
-  const isAllChecked = useCallback(() => {
-    return cartItems.every((item) => item.checked === true);
-  }, [cartItems]);
-
-  const onChangeAllChecked = useCallback(
-    (e: React.MouseEvent<HTMLElement> | React.ChangeEvent<HTMLElement>) => {
-      e.preventDefault();
-
-      dispatch(cartActions.toggleCheckAllProduct(!isAllChecked()));
-    },
-    [dispatch, isAllChecked]
-  );
-
-  const onClickCheckedDeleteButton = useCallback(() => {
-    if (window.confirm(CART_MESSAGE.ASK_DELETE)) {
-      dispatch(cartActions.deleteCheckedToCart());
-    }
-  }, [dispatch]);
-
-  const renderSwitch = useCallback(() => {
+  const renderSwitch = () => {
     switch (condition) {
       case CONDITION.LOADING:
         return <Message>Loading...</Message>;
       case CONDITION.COMPLETE:
-        return (
-          <>
-            <StyledContentBox>
-              <StyledProductContainer>
-                <StyledProductOptions>
-                  <StyledAllCheckOption>
-                    <CheckBox
-                      id="all-check"
-                      checked={isAllChecked()}
-                      onChange={onChangeAllChecked}
-                    />
-                    <p>전체 선택/해제</p>
-                  </StyledAllCheckOption>
-                  <StyledDeleteButton
-                    type="button"
-                    onClick={onClickCheckedDeleteButton}
-                  >
-                    선택 상품 삭제
-                  </StyledDeleteButton>
-                </StyledProductOptions>
-                {cartItems.map(({ product, stock, checked }) => (
-                  <CartItem
-                    product={product}
-                    stock={stock}
-                    checked={checked}
-                    key={product.id}
-                  />
-                ))}
-              </StyledProductContainer>
-              <StyledTotalContainer>
-                <h3>결제예상금액</h3>
-                <hr />
-                <StyledTotalMoney>
-                  {calculateTotalMoney().toLocaleString('ko-KR')} 원
-                </StyledTotalMoney>
-                <StyledOrderButton type="button">주문하기</StyledOrderButton>
-              </StyledTotalContainer>
-            </StyledContentBox>
-          </>
-        );
+        return <CartContent cartItems={cartItems} />;
       case CONDITION.ERROR:
         return (
           <Message>상품 정보를 가져오는데 오류가 발생하였습니다 😱</Message>
         );
     }
-  }, [
-    condition,
-    calculateTotalMoney,
-    cartItems,
-    isAllChecked,
-    onChangeAllChecked,
-    onClickCheckedDeleteButton,
-  ]);
+  };
 
   return (
     <StyledPage>
@@ -163,81 +85,6 @@ const StyledPage = styled.div`
   hr {
     width: 100%;
   }
-`;
-
-const StyledContentBox = styled.div`
-  display: grid;
-  grid-template-columns: repeat(6, 1fr);
-  gap: 80px;
-
-  width: 100%;
-  margin-top: 30px;
-`;
-
-const StyledProductContainer = styled.div`
-  grid-column: 1 / 5;
-`;
-
-const StyledProductOptions = styled.div`
-  display: flex;
-  justify-content: space-between;
-
-  height: 30px;
-  margin-bottom: 20px;
-`;
-
-const StyledAllCheckOption = styled.div`
-  display: flex;
-  align-items: flex-start;
-  gap: 10px;
-
-  font-size: 15px;
-`;
-
-const StyledDeleteButton = styled.button`
-  width: 100px;
-  border: 1px solid ${({ theme: { colors } }) => colors.lightGray};
-
-  background: ${({ theme: { colors } }) => colors.white};
-`;
-
-const StyledTotalContainer = styled.div`
-  grid-column: 5 / 7;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  position: sticky;
-  top: 100px;
-
-  height: fit-content;
-  padding: 0 20px 20px;
-  border: 1px solid ${({ theme: { colors } }) => colors.lightGray};
-
-  background: ${({ theme: { colors } }) => colors.white};
-
-  h3 {
-    line-height: 50px;
-  }
-
-  hr {
-    width: calc(100% + 40px);
-    margin: 0 -20px;
-  }
-`;
-
-const StyledTotalMoney = styled.div`
-  line-height: 5px;
-  border-bottom: 10px solid ${({ theme: { colors } }) => colors.pink};
-  margin: 30px 0;
-`;
-
-const StyledOrderButton = styled.button`
-  width: 80%;
-  height: 40px;
-  border-radius: 2px;
-
-  background: ${({ theme: { colors } }) => colors.redPink};
-  color: ${({ theme: { colors } }) => colors.white};
 `;
 
 const Message = styled.div`
