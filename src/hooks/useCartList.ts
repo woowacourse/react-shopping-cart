ec02e2de055bb1613b748595f0110a2e8b94b85e
junzzi <jjh01027717502@gmail.com>
2022-05-26 16:47:50 +0900

    feat: useCartList 훅 개발

---
+++ src/hooks/useCartList.ts
@@ -0,0 +1,66 @@
+import { CartActionType, fetchDeleteCartAsync, fetchPatchCartAsync } from '@/store/cart/action';
+import { useCallback, useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+export const useCartList = () => {
+  const { loadingCartProductId, checkedCartItem, cartList } = useSelector(
+    (state: any) => state.cart,
+  );
+
+  const amount = useMemo(
+    () =>
+      cartList.reduce(
+        (prev, cart) =>
+          checkedCartItem.includes(cart.id) ? prev + cart.price * cart.quantity : prev,
+        0,
+      ),
+    [cartList, checkedCartItem],
+  );
+
+  const dispatch = useDispatch();
+
+  const isCartItemLoading = useCallback(id => loadingCartProductId === id, [loadingCartProductId]);
+
+  const isCartItemChecked = useCallback(id => checkedCartItem.includes(id), [checkedCartItem]);
+
+  const decreaseCartItemCount = useCallback(cart => {
+    const { id, quantity } = cart;
+
+    dispatch(fetchPatchCartAsync(id, { ...cart, quantity: quantity - 1 }) as any);
+  }, []);
+
+  const increaseCartItemCount = useCallback(cart => {
+    const { id, quantity } = cart;
+
+    dispatch(fetchPatchCartAsync(id, { ...cart, quantity: quantity + 1 }) as any);
+  }, []);
+
+  const deleteCartItem = useCallback(cart => {
+    const { id } = cart;
+
+    if (confirm('정말로 삭제하시겠습니까?')) {
+      dispatch(fetchDeleteCartAsync(id) as any);
+    }
+  }, []);
+
+  const checkCartItem = useCallback(cart => {
+    const { id } = cart;
+
+    dispatch({ type: CartActionType.CHECK_CART_ITEM, payload: { id } });
+  }, []);
+
+  return {
+    amount,
+    dispatch,
+    cartItemStatusUtil: {
+      isCartItemLoading,
+      isCartItemChecked,
+    },
+    cartItemEvent: {
+      decreaseCartItemCount,
+      increaseCartItemCount,
+      deleteCartItem,
+      checkCartItem,
+    },
+  };
+};
