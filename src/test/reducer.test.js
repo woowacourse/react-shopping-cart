899caebd10404b2d8382ee441bbde5e587823baf
JASUN LEE <liswktjs@gmail.com>
2022-05-26 20:16:18 +0900

    fix: redux 코드 수정에 따른 테스트 코드 수정

--- src/test/reducer.test.js
+++ src/test/reducer.test.js
@@ -1,11 +1,16 @@
-import cartReducer, { ADD_ITEM, DELETE_CHECKED_ITEM, DELETE_ITEM, MINUS_ITEM } from 'modules/cart';
+import cartReducer, {
+  ADD_ITEM,
+  DELETE_CHECKED_ITEM,
+  DELETE_ITEM,
+  MINUS_ITEM,
+} from 'reduxModule/cart';
 
 describe('cartReducer 작동 테스트 ', () => {
   test('장바구니 추가 요청을 보냈을 때에 해당 상품의 수량이 1인 채로 cartList에 새롭게 추가된다. ', () => {
     const productId = 2;
     const result = cartReducer({ cartList: [] }, { type: ADD_ITEM, payload: { id: 3 } });
     expect(result).toEqual({
-      cartList: [{ id: 3, quantity: 1 }],
+      cartList: [{ id: 3, quantity: 1, isChecked: false }],
     });
   });
 
@@ -13,7 +18,11 @@ describe('cartReducer 작동 테스트 ', () => {
     const existCartList = {
       cartList: [{ id: 3, quantity: 1 }],
     };
-    const result = cartReducer(existCartList, { type: ADD_ITEM, payload: { id: 3 } });
+    const result = cartReducer(existCartList, {
+      type: ADD_ITEM,
+      payload: { id: 3 },
+      isChecked: false,
+    });
     expect(result).toEqual({
       cartList: [{ id: 3, quantity: 2 }],
     });
@@ -22,8 +31,8 @@ describe('cartReducer 작동 테스트 ', () => {
   test('장바구니에 있는 상품의 수량을 1씩 줄일 수 있다.', () => {
     const existCartList = {
       cartList: [
-        { id: 3, quantity: 1 },
-        { id: 1, quantity: 2 },
+        { id: 3, quantity: 1, isChecked: false },
+        { id: 1, quantity: 2, isChecked: false },
       ],
     };
     const result = cartReducer(existCartList, {
@@ -34,8 +43,8 @@ describe('cartReducer 작동 테스트 ', () => {
     });
     expect(result).toEqual({
       cartList: [
-        { id: 3, quantity: 1 },
-        { id: 1, quantity: 1 },
+        { id: 3, quantity: 1, isChecked: false },
+        { id: 1, quantity: 1, isChecked: false },
       ],
     });
   });
@@ -43,8 +52,8 @@ describe('cartReducer 작동 테스트 ', () => {
   test('장바구니에 있는 상품을 삭제할 수 있다', () => {
     const existCartList = {
       cartList: [
-        { id: 3, quantity: 1 },
-        { id: 1, quantity: 2 },
+        { id: 3, quantity: 1, isChecked: false },
+        { id: 1, quantity: 2, isChecked: false },
       ],
     };
     const result = cartReducer(existCartList, {
@@ -54,29 +63,26 @@ describe('cartReducer 작동 테스트 ', () => {
       },
     });
     expect(result).toEqual({
-      cartList: [{ id: 3, quantity: 1 }],
+      cartList: [{ id: 3, quantity: 1, isChecked: false }],
     });
   });
 
   test('체크된 항목들을 장바구니에서 삭제할 수 있다', () => {
     const existCartList = {
       cartList: [
-        { id: 3, quantity: 1 },
-        { id: 1, quantity: 2 },
-        { id: 2, quantity: 1 },
-        { id: 4, quantity: 4 },
+        { id: 3, quantity: 1, isChecked: false },
+        { id: 1, quantity: 2, isChecked: true },
+        { id: 2, quantity: 1, isChecked: false },
+        { id: 4, quantity: 4, isChecked: true },
       ],
     };
     const result = cartReducer(existCartList, {
       type: DELETE_CHECKED_ITEM,
-      payload: {
-        checkedList: [4, 1],
-      },
     });
     expect(result).toEqual({
       cartList: [
-        { id: 3, quantity: 1 },
-        { id: 2, quantity: 1 },
+        { id: 3, quantity: 1, isChecked: false },
+        { id: 2, quantity: 1, isChecked: false },
       ],
     });
   });
