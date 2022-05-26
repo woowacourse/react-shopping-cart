76caa6ed65d7fc77748b1c050726903398b5ee6f
jswith <typingjs@gmail.com>
2022-05-26 15:56:48 +0900

    refactor: notFound 컴포넌트 스타일 부여 방식 변경

--- src/pages/not-found/NotFound.jsx
+++ src/pages/not-found/NotFound.jsx
@@ -1,27 +1,13 @@
-import { css } from "@emotion/react";
-
 import NotFoundImg from "@/assets/images/404.png";
+import StyledNotFoundMessage from "./NotFoundMessage.styled";
+import StyledNotFoundContainer from "./NotFoundContainer.styled";
 
 function NotFound() {
   return (
-    <div
-      css={css`
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        min-height: 90vh;
-      `}
-    >
+    <StyledNotFoundContainer>
       <img src={NotFoundImg} alt="존재하지 않는 페이지" />
-      <div
-        css={css`
-          font-size: 25px;
-        `}
-      >
-        존재하지 않는 페이지입니다.
-      </div>
-    </div>
+      <StyledNotFoundMessage>존재하지 않는 페이지입니다.</StyledNotFoundMessage>
+    </StyledNotFoundContainer>
   );
 }
 
