1723d07753fb8f59d9969444500009bb47be83be
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-05-26 20:45:00 +0900

    ⚰️ : 사용하지 않는 Import 제거

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -2,7 +2,6 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import * as cartThunk from 'actions/cart/thunk';
-import { updateCartItemAllChecked, updateCartItemChecked } from 'actions/cart/action';
 
 function useCart() {
   const { items: cartItems } = useSelector((state) => state.cart);
