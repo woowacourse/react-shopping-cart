16f26cc6f5368cf5ea9f0984883d5f2df581f06e
jswith <typingjs@gmail.com>
2022-05-26 15:50:01 +0900

    refactor: error 컴포넌트 스타일 부여 방식 변경

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,27 +1,15 @@
-import { css } from "@emotion/react";
-
 import ErrorImg from "@/assets/images/error.png";
+import StyledErrorContainer from "./ErrorContainer.styled";
+import StyledErrorMessage from "./ErrorMessage.styled";
 
 function Error() {
   return (
-    <div
-      css={css`
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        min-height: 90vh;
-      `}
-    >
+    <StyledErrorContainer>
       <img src={ErrorImg} alt="에러 페이지" />
-      <div
-        css={css`
-          font-size: 25px;
-        `}
-      >
+      <StyledErrorMessage>
         에러가 발생했습니다. 관리자에게 문의해주세요.
-      </div>
-    </div>
+      </StyledErrorMessage>
+    </StyledErrorContainer>
   );
 }
 
