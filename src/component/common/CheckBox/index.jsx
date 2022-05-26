2c736cb798ccf3d27f495c85da496d4611b977e9
kamwoo <rkadndud06@naver.com>
2022-05-26 15:33:30 +0900

    style: checkBox 사이즈에 따른 스타일 추가

--- src/component/common/CheckBox/index.jsx
+++ src/component/common/CheckBox/index.jsx
@@ -1,9 +1,8 @@
 import styled from 'styled-components';
-
-export default function CheckBox({ description, checked, onCheckChange }) {
+export default function CheckBox({ size, description, checked, onCheckChange }) {
   return (
     <Styled.Description>
-      <Styled.CheckInput type="checkbox" onChange={onCheckChange} checked={checked} />
+      <Styled.CheckInput type="checkbox" onChange={onCheckChange} checked={checked} size={size} />
       {description}
     </Styled.Description>
   );
@@ -11,12 +10,27 @@ export default function CheckBox({ description, checked, onCheckChange }) {
 
 CheckBox.defaultProps = {
   description: '',
+  size: 'medium',
+};
+
+const sizes = {
+  small: {
+    width: '14px',
+    height: '14px',
+  },
+  medium: {
+    width: '28px',
+    height: '28px',
+  },
+  large: {
+    width: '42px',
+    height: '42px',
+  },
 };
 
 const Styled = {
   CheckInput: styled.input`
-    width: 28px;
-    height: 28px;
+    ${props => sizes[props.size]}
   `,
 
   Description: styled.label`
