e741f00abac6995b76dff4eeaadbb7a105fc6b40
Daye Lee <508yeah@gmail.com>
2022-05-27 22:51:40 +0900

    refactor: 리듀서 로직 분리

--- src/redux/cart/cartReducer.js
+++ src/redux/cart/cartReducer.js
@@ -1,85 +1,89 @@
 import ACTION_TYPE from 'redux/cart/cartActions';
-import { getObjectArrayValuesOfKey } from 'utils';
+import { findElementIndex, getObjectArrayValuesOfKey, removeElement, updateObject } from 'utils';
 
 const initialState = {
   products: [],
   checkedProducts: [],
 };
 
-const cartReducer = (state = initialState, action) => {
+const addProductToCart = (state, { payload }) => {
   const { products, checkedProducts } = state;
-  const { type, payload } = action;
+  const targetProductIdx = findElementIndex(products, 'id', payload.id);
 
-  const newState = { ...state };
+  if (targetProductIdx !== -1) {
+    const newProducts = [...products];
 
-  switch (type) {
-    case ACTION_TYPE.ADD_PRODUCT_TO_CART: {
-      const newProducts = [...products];
-      const targetProductIdx = products.findIndex(product => product.id === payload.id);
+    newProducts[targetProductIdx].quantity += 1;
 
-      if (targetProductIdx !== -1) {
-        newProducts[targetProductIdx].quantity += 1;
-        newState.products = [...newProducts];
+    return updateObject(state, { products: newProducts });
+  }
 
-        return newState;
-      }
+  return updateObject(state, {
+    products: products.concat({ ...payload, quantity: 1 }),
+    checkedProducts: checkedProducts.concat({ id: payload.id }),
+  });
+};
 
-      newState.products = [...products, { ...payload, quantity: 1 }];
-      newState.checkedProducts = [...checkedProducts, { id: payload.id }];
+const subtractCartProductQuantity = (state, { payload }) => {
+  const { products } = state;
+  const newProducts = [...products];
+  const targetProductIdx = findElementIndex(products, 'id', payload.id);
 
-      return newState;
-    }
-    case ACTION_TYPE.SUBTRACT_CART_PRODUCT_QUANTITY: {
-      const newProducts = [...products];
-      const targetProductIdx = products.findIndex(product => product.id === payload.id);
+  newProducts[targetProductIdx].quantity -= 1;
 
-      newProducts[targetProductIdx].quantity -= 1;
-      newState.products = [...newProducts];
+  return updateObject(state, { products: newProducts });
+};
 
-      return newState;
-    }
-    case ACTION_TYPE.TOGGLE_ALL_CART_PRODUCTS_CHECK: {
-      if (!payload.checked) {
-        newState.checkedProducts = [];
+const toggleAllCartProductsCheck = (state, { payload }) => {
+  return updateObject(state, { checkedProducts: payload.checked ? state.products : [] });
+};
 
-        return newState;
-      }
+const toggleCartProductCheck = (state, { payload }) => {
+  const { checkedProducts } = state;
+  const isCheckedProduct = checkedProducts.find(product => product.id === payload.id);
 
-      newState.checkedProducts = [...products];
+  if (isCheckedProduct) {
+    return updateObject(state, { checkedProducts: removeElement(checkedProducts, payload.id) });
+  }
 
-      return newState;
-    }
-    case ACTION_TYPE.TOGGLE_CART_PRODUCT_CHECK: {
-      const isCheckedProduct = checkedProducts.find(product => product.id === payload.id);
+  return updateObject(state, { checkedProducts: checkedProducts.concat({ id: payload.id }) });
+};
 
-      if (isCheckedProduct) {
-        newState.checkedProducts = checkedProducts.filter(product => product.id !== payload.id);
+const removeSelectedProductsFromCart = state => {
+  const { products, checkedProducts } = state;
+  const checkedProductIds = getObjectArrayValuesOfKey(checkedProducts, 'id');
 
-        return newState;
-      }
+  return updateObject(state, {
+    products: products.filter(product => !checkedProductIds.includes(product.id)),
+    checkedProducts: [],
+  });
+};
 
-      newState.checkedProducts = [...checkedProducts, { id: payload.id }];
+const removeProductFromCart = (state, { payload }) => {
+  return updateObject(state, {
+    products: removeElement(state.products, payload.id),
+    checkedProducts: removeElement(state.checkedProducts, payload.id),
+  });
+};
 
-      return newState;
-    }
-    case ACTION_TYPE.REMOVE_SELECTED_PRODUCTS_FROM_CART: {
-      const checkedProductIds = getObjectArrayValuesOfKey(checkedProducts, 'id');
-
-      newState.products = products.filter(product => !checkedProductIds.includes(product.id));
-      newState.checkedProducts = [];
-
-      return newState;
-    }
-    case ACTION_TYPE.REMOVE_PRODUCT_FROM_CART: {
-      newState.products = products.filter(product => product.id !== payload.id);
-      newState.checkedProducts = checkedProducts.filter(product => product.id !== payload.id);
-
-      return newState;
-    }
+function cartReducer(state = initialState, action) {
+  switch (action.type) {
+    case ACTION_TYPE.ADD_PRODUCT_TO_CART:
+      return addProductToCart(state, action);
+    case ACTION_TYPE.SUBTRACT_CART_PRODUCT_QUANTITY:
+      return subtractCartProductQuantity(state, action);
+    case ACTION_TYPE.TOGGLE_ALL_CART_PRODUCTS_CHECK:
+      return toggleAllCartProductsCheck(state, action);
+    case ACTION_TYPE.TOGGLE_CART_PRODUCT_CHECK:
+      return toggleCartProductCheck(state, action);
+    case ACTION_TYPE.REMOVE_SELECTED_PRODUCTS_FROM_CART:
+      return removeSelectedProductsFromCart(state);
+    case ACTION_TYPE.REMOVE_PRODUCT_FROM_CART:
+      return removeProductFromCart(state, action);
     default:
       return state;
   }
-};
+}
 
 export default cartReducer;
 
