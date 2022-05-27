7dab9121956d96f848c3f63eee3d860c2aa4d982
junzzi <jjh01027717502@gmail.com>
2022-05-27 20:00:58 +0900

    refactor: clearCache 메소드 timeout도 삭제하도록 수정

--- src/api/cache/index.ts
+++ src/api/cache/index.ts
@@ -24,16 +24,16 @@ export const { clearCache, caching } = (() => {
     // 자원의 수정을 요청하는 fetcher에서 이 메소드를 호출한다.
     // 캐시 데이터를 클리어하여 새로운 요청을 하도록 한다.
     clearCache(key) {
+      clearTimeout(timeout[key]);
+      timeout[key] = null;
       cache[key] = null;
     },
 
     caching: async (api, key) => {
       const cacheData = getCacheData(key);
-
       if (cacheData) {
         return cacheData;
       }
-
       const data = await api();
 
       setCacheData(key, data);
