6f7c81355611278c935d93b90b82dafa088f69f9
arnold <arnold@arnoldui-MacBookAir.local>
2022-05-27 17:03:39 +0900

    refactor: 컬러 소문자화

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,11 +1,11 @@
 const theme = {
-  green: '#2AC1BC',
-  green2: '#22A6A2',
+  green: '#2ac1bc',
+  green2: '#22a6a2',
   black: '#333333',
   blue: '#00aaff',
   red: '#ff1252',
-  gray: '#DDDDDD',
-  gray2: '#AAAAAA',
+  gray: '#dddddd',
+  gray2: '#aaaaaa',
   font: 'BMJUA_ttf',
 };
 
