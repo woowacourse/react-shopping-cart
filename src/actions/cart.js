42da63b838992f42115efbeaf90d3747152696c9
YongRae_Kim (Usage) <kyr9389@naver.com>
2022-05-28 02:08:12 +0900

    ♻️ : 장바구니 액션 로직 변경

--- src/actions/cart.js
+++ src/actions/cart.js
@@ -1,22 +1,19 @@
 import { 장바구니_액션 } from './types';
 
 const addCartList = (product, cartList) => {
-  let isExist = false;
-  let count = 1;
+  const foundExistProduct = cartList.find((item) => item.id === product.id);
 
-  cartList.every((item) => {
-    if (item.id === product.id) {
-      isExist = true;
-      count = Number(item.count) + 1;
-      return false;
-    }
-    return true;
-  });
+  if (foundExistProduct) {
+    return {
+      type: 장바구니_액션.ADD_EXIST_PRODUCT,
+      payload: { ...product, count: Number(foundExistProduct.count) + 1, isChecked: true },
+    };
+  }
 
-  const targetProduct = { ...product, count, isChecked: true };
-
-  if (isExist) return { type: 장바구니_액션.ADD_EXIST_PRODUCT, payload: targetProduct };
-  return { type: 장바구니_액션.ADD_NEW_PRODUCT, payload: targetProduct };
+  return {
+    type: 장바구니_액션.ADD_NEW_PRODUCT,
+    payload: { ...product, count: 1, isChecked: true },
+  };
 };
 
 const deleteCartItem = (productId) => ({ type: 장바구니_액션.DELETE_PRODUCT, payload: productId });
