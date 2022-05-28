2533099794693223080a5d1397463fd7af7fa7e3
airman5573 <rpf5573@kakao.com>
2022-05-28 22:42:59 +0900

    perf(redux): 동일성 체크 함수를 개선한다

--- src/redux/equalities/productListEquality.js
+++ src/redux/equalities/productListEquality.js
@@ -1,13 +1,19 @@
+/* eslint-disable max-depth */
 import { shallowEqual } from "react-redux";
 
 function productListEquality(arrA, arrB) {
   if (arrA.length !== arrB.length) return false;
   for (let i = 0; i < arrA.length; i += 1) {
-    if (!shallowEqual(arrA[i], arrB[i])) {
-      return false;
-    }
-    if (!shallowEqual(arrA[i].thumbnail_image, arrB[i].thumbnail_image)) {
-      return false;
+    const keys = Object.keys(arrA[i]);
+    for (let j = 0; j < keys.length; j += 1) {
+      const key = keys[j];
+      if (key === "thumbnail_image") {
+        if (!shallowEqual(arrA[i][key], arrB[i][key])) {
+          return false;
+        }
+      } else if (arrA[i][key] !== arrB[i][key]) {
+        return false;
+      }
     }
   }
   return true;
