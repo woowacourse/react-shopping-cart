a65ecaf5fe5426bd094566ec8d7465490b366e11
Jeongmin Woo <greenblues1190@gmail.com>
2022-05-30 06:55:53 +0900

    refactor: 불필요한 import 제거

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styled from 'styled-components';
 import CartItem from '../../components/CartItem/CartItem';
 import CheckBox from '../../components/CheckBox/CheckBox';
