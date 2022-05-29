0c02112496fdacab74f7e2fe74f55710de2c331c
airman5573 <rpf5573@kakao.com>
2022-05-29 12:26:25 +0900

    test(reducer): test case를 추가한다

--- src/__test__/cart.test.js
+++ src/__test__/cart.test.js
@@ -40,8 +40,92 @@ describe("장바구니 리듀서 테스트", () => {
       initialState,
       createAction(ACTION_TYPE.ADD_PRODUCT_TO_CART, "jBR5pmE5")
     );
+
     expect(newState.cart).toEqual({
       jBR5pmE5: { quantity: 1, selected: false },
     });
+  });
+
+  test("장바구니에서 상품을 삭제한다", () => {
+    // Cart에 상품을 먼저 추가한다
+    const newInitialState = structuredClone(initialState);
+    newInitialState.cart = {
+      jBR5pmE5: { quantity: 1, selected: false },
+    };
+
+    const newState = reducer(
+      newInitialState,
+      createAction(ACTION_TYPE.DELETE_PRODUCT_IN_CART, "jBR5pmE5")
+    );
+
+    expect(newState.cart).toEqual({});
+  });
+
+  test("장바구니에서 상품을 하나 선택한다", () => {
+    // Cart에 상품을 먼저 추가한다
+    const newInitialState = structuredClone(initialState);
+    newInitialState.cart = {
+      jBR5pmE5: { quantity: 1, selected: false },
+    };
+
+    const newState = reducer(
+      newInitialState,
+      createAction(ACTION_TYPE.SELECT_PRODUCT_IN_CART, "jBR5pmE5")
+    );
+
+    expect(newState.cart["jBR5pmE5"].selected).toEqual(true);
+  });
+
+  test("장바구니에서 상품을 모두 선택한다", () => {
+    // Cart에 상품을 먼저 추가한다
+    const newInitialState = structuredClone(initialState);
+    newInitialState.cart = {
+      jBR5pmE5: { quantity: 1, selected: false },
+      "83kW9mUr": { quantity: 1, selected: false },
+    };
+
+    const newState = reducer(
+      newInitialState,
+      createAction(ACTION_TYPE.SELECT_ALL_PRODUCTS_IN_CART)
+    );
+
+    const isAllSelected = Object.keys(newState.cart).some(
+      (key) => newState.cart[key].selected
+    );
+
+    expect(isAllSelected).toEqual(true);
+  });
+
+  test("장바구니에서 상품을 하나 삭제한다", () => {
+    // Cart에 상품을 먼저 추가한다
+    const newInitialState = structuredClone(initialState);
+    newInitialState.cart = {
+      jBR5pmE5: { quantity: 1, selected: false },
+    };
+
+    const newState = reducer(
+      newInitialState,
+      createAction(ACTION_TYPE.DELETE_PRODUCT_IN_CART, "jBR5pmE5")
+    );
+
+    expect(newState.cart).toEqual({});
+  });
+
+  test("장바구니에서 선택된 상품을 모두 삭제한다", () => {
+    const newInitialState = structuredClone(initialState);
+    newInitialState.cart = {
+      jBR5pmE5: { quantity: 1, selected: true },
+      "83kW9mUr": { quantity: 1, selected: true },
+    };
+
+    const newState = reducer(
+      newInitialState,
+      createAction(ACTION_TYPE.DELETE_MULTIPLE_PRODUCTS_IN_CART, [
+        "jBR5pmE5",
+        "83kW9mUr",
+      ])
+    );
+
+    expect(newState.cart).toEqual({});
   });
 });
