0a1e3b4151d1cd5d8387d10f79fa4e7b37b01630
Jeongmin Woo <greenblues1190@gmail.com>
2022-05-30 05:46:47 +0900

    refactor: Checkbox.jsx를 tsx로 수정

--- src/components/CheckBox/CheckBox.tsx
+++ src/components/CheckBox/CheckBox.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 
-function CheckBox(props) {
+type InputProps = React.HTMLProps<HTMLInputElement>;
+
+function CheckBox({ ref, as, ...props }: InputProps) {
   return <StyledCheckbox {...props} type="checkbox" />;
 }
 
