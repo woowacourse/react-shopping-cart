fe0611d1bcddccc4ecb40b0f554ad1b933fe9f14
junzzi <jjh01027717502@gmail.com>
2022-06-02 01:16:50 +0900

    feat: EditPassword, SignUp Router 수정

--- src/route.ts
+++ src/route.ts
@@ -8,14 +8,16 @@ export const ROUTE: {
   Login: string;
   Edit: string;
   Leave: string;
+  EditPassword: string;
 } = {
   Home: '/',
   ShoppingCart: '/shopping-cart',
   OrderList: '/order-list',
   ProductDetail: '/products/:productId',
-  SignUp: '/customers/sign-up',
+  SignUp: '/customers/signup',
   Login: '/customers/login',
   Edit: '/customers/edit',
   Leave: '/customers/leave',
+  EditPassword: '/customers/edit/password',
   NotFound: '/*',
 };
