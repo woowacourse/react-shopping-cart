7715db543498e2b19d75e827f26c3f49b251a415
junzzi <jjh01027717502@gmail.com>
2022-06-02 01:11:14 +0900

    feat: Header 컴포넌트 로그인 link 추가

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,8 +1,11 @@
+import { useSelector } from 'react-redux';
 import { ROUTE } from '../../../route';
 import Icon from '../Icon/Icon';
 import * as Styled from './Header.style';
 
 function Header() {
+  const { isLoggedIn } = useSelector((state: any) => state.customer);
+
   return (
     <Styled.Container>
       <Styled.Inner>
@@ -16,6 +19,11 @@ function Header() {
         <Styled.NavButton>
           <Styled.NavLink to={ROUTE.ShoppingCart}>장바구니</Styled.NavLink>
           <Styled.NavLink to={ROUTE.OrderList}>주문목록</Styled.NavLink>
+          {isLoggedIn ? (
+            <Styled.NavLink to={ROUTE.Edit}>프로필</Styled.NavLink>
+          ) : (
+            <Styled.NavLink to={ROUTE.Login}>로그인</Styled.NavLink>
+          )}
         </Styled.NavButton>
       </Styled.Inner>
     </Styled.Container>
