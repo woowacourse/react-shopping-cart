e51c32f5bcfc68240be0ff04dc8079dd94e16694
junzzi <jjh01027717502@gmail.com>
2022-06-01 15:18:03 +0900

    feat: Edit, Login 페이지 UI 개발

--- src/pages/Customers/Edit/Edit.tsx
+++ src/pages/Customers/Edit/Edit.tsx
@@ -1,7 +1,43 @@
+import Button from '@/components/common/Button/Button';
 import PageTemplate from '@/components/common/PageTemplate/PageTemplate';
-
+import Input from '@/components/common/Input/Input';
+import CustomerFormTemplate from '@/components/customer/CustomerFormTemplate/CustomerFormTemplate';
+import { useCustomerForm } from '@/hooks/useCustomerForm';
 function Edit() {
-  return <PageTemplate>Edit</PageTemplate>;
+  const {
+    formValue: { phoneNumber, address },
+    formHandler: { onChangeAddress, onChangePhoneNumber, onSubmitEditForm },
+  } = useCustomerForm({
+    usernameValue: '준찌',
+    addressValue: '서울시 노원구',
+    phoneNumberValue: '01027717502',
+  });
+
+  return (
+    <PageTemplate>
+      <CustomerFormTemplate formTitle="정보수정" onSubmit={onSubmitEditForm}>
+        <Input
+          id="phone-number"
+          placeholder="숫자만 입력"
+          value={phoneNumber.value}
+          onChange={onChangePhoneNumber}
+          isError={phoneNumber.isError}
+          description="휴대폰 번호 11자리를 입력해주세요 (- 제외)"
+          labelText="핸드폰 번호"
+        />
+
+        <Input
+          id="address"
+          placeholder="시 단위 까지만 입력"
+          value={address.value}
+          onChange={onChangeAddress}
+          labelText="주소"
+        />
+
+        <Button padding="8px">확인</Button>
+      </CustomerFormTemplate>
+    </PageTemplate>
+  );
 }
 
 export default Edit;
--- src/pages/Customers/Login/Login.tsx
+++ src/pages/Customers/Login/Login.tsx
@@ -1,7 +1,43 @@
+import Button from '@/components/common/Button/Button';
 import PageTemplate from '@/components/common/PageTemplate/PageTemplate';
-
+import Input from '@/components/common/Input/Input';
+import { useCustomerForm } from '@/hooks/useCustomerForm';
+import { Link } from 'react-router-dom';
+import { ROUTE } from '@/route';
+import CustomerFormTemplate from '@/components/customer/CustomerFormTemplate/CustomerFormTemplate';
 function Login() {
-  return <PageTemplate>Login</PageTemplate>;
+  const {
+    formValue: { username, password },
+    formHandler: { onChangeUsername, onChangePassword, onSubmitEditForm },
+  } = useCustomerForm();
+
+  return (
+    <PageTemplate>
+      <CustomerFormTemplate formTitle="로그인" onSubmit={onSubmitEditForm}>
+        <Input
+          id="name"
+          placeholder="유저 네임"
+          value={username.value}
+          onChange={onChangeUsername}
+          labelText="유저 네임"
+        />
+        <Input
+          id="password"
+          type="password"
+          placeholder="비밀번호"
+          value={password.value}
+          onChange={onChangePassword}
+          labelText="비밀번호"
+        />
+
+        <Button padding="8px">확인</Button>
+        <p>
+          <span>아직 회원이 아니신가요?</span>
+          <Link to={ROUTE.SignUp}>회원가입 하기</Link>
+        </p>
+      </CustomerFormTemplate>
+    </PageTemplate>
+  );
 }
 
 export default Login;
