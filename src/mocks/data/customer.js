70d62af3aec043dd180dfae78689bc7c7c628909
junzzi <jjh01027717502@gmail.com>
2022-06-02 02:25:57 +0900

    msw: mocking API 구현

--- src/mocks/data/customer.js
+++ src/mocks/data/customer.js
@@ -2,7 +2,7 @@ export const customerList = {
   current: [
     {
       username: 'kkojji',
-      password: '123456jh',
+      password: '1234qwer',
       phoneNumber: '01027717502',
       address: '서울시 노원구',
     },
--- src/mocks/handlers/customer.js
+++ src/mocks/handlers/customer.js
@@ -3,6 +3,29 @@ import { rest } from 'msw';
 import { customerList } from '../data/customer';
 
 export const customerHanlders = [
+  rest.get(`${API_URL}/customers`, (req, res, ctx) => {
+    const { headers } = req;
+    const accessToken = headers['_headers'].authorization.split('Bearer ')[1];
+
+    const customer = customerList.current.find(customer => customer.username === accessToken);
+
+    if (!customer) {
+      return res(
+        ctx.status(400, 'unauthorized'),
+        ctx.json({ error: { messages: ['토큰 정보가 잘 못 되었습니다.'] } }),
+      );
+    }
+
+    return res(
+      ctx.status(200, 'ok'),
+      ctx.json({
+        username: customer.username,
+        phoneNumber: customer.phoneNumber,
+        address: customer.address,
+      }),
+    );
+  }),
+
   rest.post(`${API_URL}/customers/signup`, (req, res, ctx) => {
     const { body: customer } = req;
 
@@ -16,19 +39,90 @@ export const customerHanlders = [
 
   rest.post(`${API_URL}/customers/login`, (req, res, ctx) => {
     const { username, password } = req.body;
-    // username먼저 확인
-    // password 확인
-    // 없는 경우
+
     if (
       customerList.current.some(
         customer => customer.username === username && customer.password === password,
       )
     ) {
-      return res(ctx.status(200, 'ok'), ctx.json({ accessToken: 'jwt.token.here' }));
+      return res(ctx.status(200, 'ok'), ctx.json({ accessToken: username }));
     }
-    // username, password가 잘못된 값이 들어왔을 경우 ctx.status(400, 'failed')
-    // id password 불일치 401 unauthorized
-    //
-    return res(ctx.status(400, 'unauthorized'), ctx.json({ error: { message: ['login failed'] } }));
+
+    return res(
+      ctx.status(400, 'unauthorized'),
+      ctx.json({ error: { messages: ['login failed'] } }),
+    );
+  }),
+
+  rest.put(`${API_URL}/customers`, (req, res, ctx) => {
+    const { phoneNumber, address } = req.body;
+    const { headers } = req;
+
+    const accessToken = headers['_headers'].authorization.split('Bearer ')[1];
+
+    if (customerList.current.every(customer => customer.username !== accessToken)) {
+      return res(
+        ctx.status(401, 'unauthorized'),
+        ctx.json({ error: { messages: ['잘못된 토큰 정보입니다.'] } }),
+      );
+    }
+
+    customerList.current = customerList.current.map(customer => {
+      if (customer.username === accessToken) {
+        return {
+          ...customer,
+          phoneNumber,
+          address,
+        };
+      }
+      return customer;
+    });
+
+    return res(ctx.status(200, 'ok'));
+  }),
+
+  rest.patch(`${API_URL}/customers/password`, (req, res, ctx) => {
+    const { password } = req.body;
+    const { headers } = req;
+
+    const accessToken = headers['_headers'].authorization.split('Bearer ')[1];
+
+    if (customerList.current.every(customer => customer.username !== accessToken)) {
+      return res(
+        ctx.status(401, 'unauthorized'),
+        ctx.json({ error: { messages: ['잘못된 토큰 정보입니다.'] } }),
+      );
+    }
+
+    customerList.current = customerList.current.map(customer => {
+      if (customer.username === accessToken) {
+        return {
+          ...customer,
+          password,
+        };
+      }
+      return customer;
+    });
+
+    return res(ctx.status(204, 'no-content'));
+  }),
+
+  rest.delete(`${API_URL}/customers`, (req, res, ctx) => {
+    const { headers } = req;
+
+    const accessToken = headers['_headers'].authorization.split('Bearer ')[1];
+
+    if (customerList.current.every(customer => customer.username !== accessToken)) {
+      return res(
+        ctx.status(401, 'unauthorized'),
+        ctx.json({ error: { messages: ['잘못된 토큰 정보입니다.'] } }),
+      );
+    }
+
+    customerList.current = customerList.current.filter(
+      customer => customer.username !== accessToken,
+    );
+
+    return res(ctx.status(204, 'no-content'));
   }),
 ];
