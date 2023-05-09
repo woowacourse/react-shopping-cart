85d84cf6cb4aaa2ede4eb480f797404eaea39d84
n0eyes <doll4586633@naver.com>
2023-05-09 20:41:52 +0900

    feat: PageName 추가

---
+++ src/components/common/PageName/PageName.styles.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Root = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 10rem;
+  width: 100%;
+
+  border-bottom: solid 5px black;
+`;
+
+export const Name = styled.div`
+  font-size: 3.2rem;
+`;
---
+++ src/components/common/PageName/PageName.tsx
@@ -0,0 +1,11 @@
+import * as S from './PageName.styles';
+
+const PageName = () => {
+  return (
+    <S.Root>
+      <S.Name>상품 목록</S.Name>
+    </S.Root>
+  );
+};
+
+export default PageName;
