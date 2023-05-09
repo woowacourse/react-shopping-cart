990ee1ece09b4f0c77740138b437b08bd9fe5fc2
ashleysyheo <ashleysyheo@gmail.com>
2023-05-09 15:34:58 +0900

    feat: ProductItem 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Header from './components/Header/Header';
+import ProductItem from './components/ProductItem/ProductItem';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ProductItem />
     </div>
   );
 }
