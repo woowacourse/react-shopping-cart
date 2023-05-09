2dbe591396b49322580183f34858ae00c3f9b7ad
ashleysyheo <ashleysyheo@gmail.com>
2023-05-09 16:07:03 +0900

    feat: ProductList 컴포넌트 생성

---
+++ src/components/ProductList/ProductList.tsx
@@ -0,0 +1,34 @@
+import ProductItem from '../ProductItem/ProductItem';
+import styles from './styles.module.css';
+
+const ProductList = () => {
+  // data 아이템 받아 온다
+
+  return (
+    <div className={styles.container}>
+      <ProductItem />
+      <ProductItem />
+      <ProductItem />
+      <ProductItem />
+      <ProductItem />
+      <ProductItem />
+      <ProductItem />
+      <ProductItem />
+      <ProductItem />
+      <ProductItem />
+      <ProductItem />
+      <ProductItem />
+      <ProductItem />
+      <ProductItem />
+      <ProductItem />
+      <ProductItem />
+      <ProductItem />
+      <ProductItem />
+      <ProductItem />
+      <ProductItem />
+      <ProductItem />
+    </div>
+  );
+};
+
+export default ProductList;
