eaa04f32641afee80aefd1cdad37ab081c7d8b09
sh981013s <sh981013s@gmail.com>
2023-05-09 14:09:21 +0900

    chore: lint setting 적용

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,44 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier"
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
-}
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'prefer-const': 'warn',
+    eqeqeq: 'error',
+    'no-multi-spaces': 'error',
+    'no-var': 'error',
+    'prefer-arrow-callback': 'error',
+    'object-shorthand': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
+    "quotes": ["error", "single"],
+    '@typescript-eslint/no-empty-function': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
