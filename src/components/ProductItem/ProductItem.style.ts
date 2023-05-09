32ab82f5eb24005fa3c052d85fd7d7c5e902a513
ukkodeveloper <ukkodeveloper@gmail.com>
2023-05-09 18:00:11 +0900

    feat: product item 컴포넌트 스타일 구현

---
+++ src/components/ProductItem/ProductItem.style.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const ProductItemBox = styled.div`
+  width: 300px;
+  height: 400px;
+  padding: 16px;
+  background-color: red;
+`;
+
+export const ProductItemImageBox = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const ProductItemImage = styled.img`
+  width: 282px;
+  height: 282px;
+`;
+
+export const ProductDetails = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const ProductInfo = styled.div``;
--- src/components/ProductItem/ProductItem.tsx
+++ src/components/ProductItem/ProductItem.tsx
@@ -1,23 +1,27 @@
+import * as S from './ProductItem.style';
+
 interface ProductItemProps {
   id: number;
   name: string;
   price: number;
   imageUrl: string;
+  quantity: number;
 }
 
-function ProductItem({ id, name, price, imageUrl }: ProductItemProps) {
+function ProductItem({ id, name, price, imageUrl, quantity }: ProductItemProps) {
   return (
-    <div>
-      <img src={imageUrl} />
-      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <div>
+    <S.ProductItemBox>
+      <S.ProductItemImageBox>
+        <S.ProductItemImage src={imageUrl} />
+      </S.ProductItemImageBox>
+      <S.ProductDetails>
+        <S.ProductInfo>
           <div>{name}</div>
           <div>{price}</div>
-        </div>
-        <div>🛒</div>
-      </div>
-    </div>
+        </S.ProductInfo>
+        {quantity > 0 ? <input type="number" /> : <button>🛒</button>}
+      </S.ProductDetails>
+    </S.ProductItemBox>
   );
 }
-
 export default ProductItem;
