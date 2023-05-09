4e9fb00cbbc78da8ce4296b0354bc52c15527201
Suyoung <jsy8492@gmail.com>
2023-05-09 15:15:52 +0900

    feat: CSS reset을 위한 GlobalStyle 작성

---
+++ src/GlobalStyle.tsx
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  *, *::before, *::after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+export default GlobalStyle;
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import GlobalStyle from './GlobalStyle';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<GlobalStyle />
+		<App />
+	</React.StrictMode>,
 );
