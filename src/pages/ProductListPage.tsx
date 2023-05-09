ce1e34e1b4c5cf1d426d5774a1c6f7363cb8496c
ashleysyheo <ashleysyheo@gmail.com>
2023-05-09 17:42:37 +0900

    feat: cardListState 생성

--- src/pages/ProductListPage.tsx
+++ src/pages/ProductListPage.tsx
@@ -3,7 +3,7 @@ import { atom } from 'recoil';
 import Header from '../components/Header/Header';
 import ProductList from '../components/ProductList/ProductList';
 import initialListData from '../data/mockData.json';
-import { ProductItemData } from '../types';
+import { CartItemData, ProductItemData } from '../types';
 
 export const productListState = atom<ProductItemData[]>({
   key: 'productList',
@@ -13,6 +13,29 @@ export const productListState = atom<ProductItemData[]>({
       const storeKey = 'productList';
       const savedValue = localStorage.getItem(storeKey); // 로컬 setSelf json 데이터
       savedValue !== null ? setSelf(JSON.parse(savedValue)) : setSelf(initialListData);
+
+      onSet((newValue, _, isReset) => {
+        isReset
+          ? localStorage.removeItem(storeKey)
+          : localStorage.setItem(storeKey, JSON.stringify(newValue));
+      });
+    },
+  ],
+});
+
+export const cartListState = atom<CartItemData[]>({
+  key: 'cartList',
+  default: [],
+  effects: [
+    ({ setSelf, onSet }) => {
+      const storeKey = 'cartList';
+      const savedValue = localStorage.getItem(storeKey);
+
+      if (savedValue !== null) {
+        setSelf(JSON.parse(savedValue));
+      } else {
+        localStorage.setItem(storeKey, '[]');
+      }
 
       onSet((newValue, _, isReset) => {
         isReset
