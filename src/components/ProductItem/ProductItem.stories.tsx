1426fedec410b5551ef378732f1f6d70c1ca491a
ukkodeveloper <ukkodeveloper@gmail.com>
2023-05-09 18:00:42 +0900

    test: product item 컴포넌트 스타일북 케이스 추가

--- src/components/ProductItem/ProductItem.stories.tsx
+++ src/components/ProductItem/ProductItem.stories.tsx
@@ -10,11 +10,22 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default = {
+export const QuantityZero = {
   args: {
     id: 1,
-    name: '고양이',
+    name: '귀여운 고양이',
     price: 100000000,
     imageUrl: 'https://placekitten.com/300/300',
+    quantity: 0,
+  },
+} satisfies Story;
+
+export const QuantityOverZero = {
+  args: {
+    id: 2,
+    name: '매서운 강아지',
+    price: 1324000000,
+    imageUrl: 'https://placedog.net/300/300',
+    quantity: 1,
   },
 } satisfies Story;
