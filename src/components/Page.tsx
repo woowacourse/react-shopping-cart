1fd1c95128ffc319fa7b2f1d7dca16bf0b5b472d
guridaek <xylitol79@gmail.com>
2023-05-09 17:58:53 +0900

    feat: Page Component 구현

---
+++ src/components/Page.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+import { styled } from "styled-components";
+
+const Page = ({ children }: { children: ReactNode }) => {
+  return <PageWrapper>{children}</PageWrapper>;
+};
+
+const PageWrapper = styled.section`
+  display: flex;
+  padding: 50px 5%;
+`;
+
+export default Page;
