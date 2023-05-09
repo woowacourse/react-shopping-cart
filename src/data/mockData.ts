cb82ad0126e032a411243b3d9c1d94d51fbc014f
jeongwusi <jeongwusi@gmail.com>
2023-05-09 14:43:40 +0900

    ✨ feat: 목데이터 생성
    Co-authored-by: d0dam
    <smallkdb@gmail.com>

---
+++ src/data/mockData.ts
@@ -0,0 +1,81 @@
+type Product = {
+  id: number;
+  name: string;
+  price: number;
+  imageUrl: string;
+};
+
+export const products: Product[] = [
+  {
+    id: 1,
+    name: 'PET보틀-정사각(420ml)',
+    price: 10000,
+    imageUrl: 'https://cdn-mart.baemin.com/sellergoods/main/2ddb9f04-c15d-4647-b6e7-30afb9e8d072.jpg?h=300&w=300',
+  },
+  {
+    id: 2,
+    name: 'PET보틀-정사각(420ml)',
+    price: 20000,
+    imageUrl: 'https://cdn-mart.baemin.com/sellergoods/main/2ddb9f04-c15d-4647-b6e7-30afb9e8d072.jpg?h=300&w=300',
+  },
+  {
+    id: 3,
+    name: 'PET보틀-정사각(420ml)',
+    price: 30000,
+    imageUrl: 'https://cdn-mart.baemin.com/sellergoods/main/2ddb9f04-c15d-4647-b6e7-30afb9e8d072.jpg?h=300&w=300',
+  },
+  {
+    id: 4,
+    name: 'PET보틀-정사각(420ml)',
+    price: 40000,
+    imageUrl: 'https://cdn-mart.baemin.com/sellergoods/main/2ddb9f04-c15d-4647-b6e7-30afb9e8d072.jpg?h=300&w=300',
+  },
+  {
+    id: 5,
+    name: 'PET보틀-정사각(420ml)',
+    price: 50000,
+    imageUrl: 'https://cdn-mart.baemin.com/sellergoods/main/2ddb9f04-c15d-4647-b6e7-30afb9e8d072.jpg?h=300&w=300',
+  },
+  {
+    id: 6,
+    name: 'PET보틀-정사각(420ml)',
+    price: 60000,
+    imageUrl: 'https://cdn-mart.baemin.com/sellergoods/main/2ddb9f04-c15d-4647-b6e7-30afb9e8d072.jpg?h=300&w=300',
+  },
+  {
+    id: 7,
+    name: 'PET보틀-정사각(420ml)',
+    price: 70000,
+    imageUrl: 'https://cdn-mart.baemin.com/sellergoods/main/2ddb9f04-c15d-4647-b6e7-30afb9e8d072.jpg?h=300&w=300',
+  },
+  {
+    id: 8,
+    name: 'PET보틀-정사각(420ml)',
+    price: 80000,
+    imageUrl: 'https://cdn-mart.baemin.com/sellergoods/main/2ddb9f04-c15d-4647-b6e7-30afb9e8d072.jpg?h=300&w=300',
+  },
+  {
+    id: 9,
+    name: 'PET보틀-정사각(420ml)',
+    price: 90000,
+    imageUrl: 'https://cdn-mart.baemin.com/sellergoods/main/2ddb9f04-c15d-4647-b6e7-30afb9e8d072.jpg?h=300&w=300',
+  },
+  {
+    id: 10,
+    name: 'PET보틀-정사각(420ml)',
+    price: 100000,
+    imageUrl: 'https://cdn-mart.baemin.com/sellergoods/main/2ddb9f04-c15d-4647-b6e7-30afb9e8d072.jpg?h=300&w=300',
+  },
+  {
+    id: 11,
+    name: 'PET보틀-정사각(420ml)',
+    price: 110000,
+    imageUrl: 'https://cdn-mart.baemin.com/sellergoods/main/2ddb9f04-c15d-4647-b6e7-30afb9e8d072.jpg?h=300&w=300',
+  },
+  {
+    id: 12,
+    name: 'PET보틀-정사각(420ml)',
+    price: 120000,
+    imageUrl: 'https://cdn-mart.baemin.com/sellergoods/main/2ddb9f04-c15d-4647-b6e7-30afb9e8d072.jpg?h=300&w=300',
+  },
+];
