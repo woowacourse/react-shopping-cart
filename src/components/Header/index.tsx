1402103a668add32612e6138d185806ae56cacfc
jeongwusi <jeongwusi@gmail.com>
2023-05-09 17:44:33 +0900

    ♻️ refactor: 헤더 타이틀 수정
    Co-authored-by: d0dam<smallkdb@gmail.com>

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,10 @@
-import { ReactComponent as ShopIcon } from '../../assets/shop-icon.svg';
+import { ReactComponent as Logo } from '../../assets/logo.svg';
 import styles from './index.module.css';
 
 const Header = () => {
   return (
     <header className={styles.container}>
-      <h1 className={styles.title}>
-        <ShopIcon />
-        SHOP
-      </h1>
+      <Logo />
       <div className={styles.cart}>
         <button>장바구니</button>
         <div className={styles['cart-count']}>2</div>
