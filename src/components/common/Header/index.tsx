1cff00d66305fbbb198c662a9ed6fbb68f814071
Patrick <kuyt3122@naver.com>
2023-05-09 21:04:54 +0900

    feat: eslint import/order 설정 적용

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,5 +1,5 @@
-import { styled } from 'styled-components';
 import { HEADER_LOGO } from '@assets';
+import { styled } from 'styled-components';
 import { theme } from '@styles/theme';
 
 const Header = () => {
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
-import App from './App';
 import GlobalStyle from '@styles/globalStyle';
 import { theme } from '@styles/theme';
+import App from './App';
 
 const router = createBrowserRouter([
   {
