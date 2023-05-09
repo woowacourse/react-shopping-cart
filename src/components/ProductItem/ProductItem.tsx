38847bea3f213841d192d656fd40d2962d38d551
ashleysyheo <ashleysyheo@gmail.com>
2023-05-09 17:50:55 +0900

    feat: 카트 아이템 추가 기능 추가

--- src/components/ProductItem/ProductItem.tsx
+++ src/components/ProductItem/ProductItem.tsx
@@ -1,3 +1,7 @@
+import { useRecoilState } from 'recoil';
+
+import CartIcon from '../../assets/cart-icon.svg';
+import { cartListState } from '../../pages/ProductListPage';
 import { ProductItemData } from '../../types';
 import styles from './style.module.css';
 
@@ -6,6 +10,13 @@ interface ProductItemProps {
 }
 
 const ProductItem = ({ information }: ProductItemProps) => {
+  const [cartList, setCartList] = useRecoilState(cartListState);
+
+  const handleCartListAdd = () => {
+    // product id를 가지는 카트 아이템 있는지 확인하기 -> 그다음에 올리기
+    setCartList([...cartList, { id: 1, quantity: 1, product: information }]);
+  };
+
   return (
     <div className={styles.container}>
       <img src={information.imageUrl} alt={information.name} className={styles.image} />
