3171840ef328a417153f6d9f597c3ec0575fb4c9
Leejin-Yang <ejin1996@gmail.com>
2023-05-09 15:07:57 +0900

    chore: 초기 환경 설정
    Co-authored-by: sᴏʟʙɪ ☔️ <solbi2004@naver.com>

---
+++ src/GlobalStyle.tsx
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  :root {
+    --primary-color:#333333;
+    --background-color: #F5F5F5;
+    --alert-color: #04C09E;
+
+    --gray-100:#DDD;
+    --gray-200: #CCC;
+    --gray-300: #BBB;
+    --gray-400: #AAA;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    background-color: var(--background-color);
+    font-family: 'Apple SD Gothic Neo', 'Noto Sans KR'
+  }
+`;
+
+export default GlobalStyle;
