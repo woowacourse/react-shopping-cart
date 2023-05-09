061ab52861449095b4dcc907b4524a7c366f139d
nlom0218 <nlom0218@naver.com>
2023-05-09 21:38:46 +0900

    test: 쇼핑 아이템 기본 스토리 작성

--- src/components/ShoppingItem/ShoppingItem.stories.tsx
+++ src/components/ShoppingItem/ShoppingItem.stories.tsx
@@ -13,3 +13,13 @@ const meta: Meta<typeof ShoppingItem> = {
 export default meta;
 
 type Story = StoryObj<typeof ShoppingItem>;
+
+export const DefaultShoppingItem: Story = {
+  args: {
+    id: 1,
+    price: 20000,
+    name: '[밀키트 SET] 아메리칸식 디너',
+    imageUrl:
+      'https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+  },
+};
