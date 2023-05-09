91bf17d580221a65bfdbeb4ce5402f067cff3fb6
yeopto <gunyeop530@gmail.com>
2023-05-09 18:12:54 +0900

    style: Noto sans font 적용

--- src/styles/ResetStyle.tsx
+++ src/styles/ResetStyle.tsx
@@ -5,7 +5,6 @@ const style = css`
   * {
     margin: 0;
     padding: 0;
-    font: inherit;
     color: inherit;
   }
   *,
@@ -27,6 +26,7 @@ const style = css`
   html,
   body {
     height: 100%;
+    font-family: 'Noto Sans KR', sans-serif !important;
   }
   img,
   picture,
