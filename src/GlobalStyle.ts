fcbc5f01f33b207b95bad75d2b0933d56e586006
Jungwoo <evencoding@gmail.com>
2023-05-09 21:03:44 +0900

    style: grey 색상에 대한 팔레트 추가, font-family 적용
    Co-authored-by: HyeryongChoi <chex1004@gmail.com>

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -5,11 +5,21 @@ const GlobalStyle = createGlobalStyle`
   ${reset}
 
   :root {
-    --primary-color: ##5f0080;
+    --primary-color: #5f0080;
+    --grey-100: #ffffff;
+    --grey-200: #dddddd;
+    --grey-300: #aaaaaa;
+    --grey-400: #333333;
+    --grey-500: #000000;
   }
 
   *, *::before, *::after {
     box-sizing: border-box;
+  }
+
+  body {
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
+    Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
 `;
 
