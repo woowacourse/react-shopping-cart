bb627b51bfd3243b3447e824b4a8715c14032f85
bassyu <yrkdgus@gmail.com>
2023-05-09 17:07:55 +0900

    chore(webpack.config): file-loader 설정 추가

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,14 @@ module.exports = {
         use: "ts-loader",
         exclude: /node_modules/,
       },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp)$/i,
+        use: [  
+          {
+            loader: 'file-loader',
+          },
+        ]
+      }
     ],
   },
   plugins: [
