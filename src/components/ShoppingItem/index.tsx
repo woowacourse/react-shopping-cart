b3f188aef5f9bc7a5f4ea9bbaf601ae7a8d2a87a
nlom0218 <nlom0218@naver.com>
2023-05-09 22:03:54 +0900

    feat: 쇼핑 아이템 컴포넌트 화면 구성

--- src/components/ShoppingItem/index.tsx
+++ src/components/ShoppingItem/index.tsx
@@ -1,5 +1,61 @@
-function ShoppingItem() {
-  return <div></div>;
+import QuantityController from '@Components/QuantityController';
+import styled from 'styled-components';
+
+type ShoppingItemProps = {
+  id: number;
+  price: number;
+  name: string;
+  imageUrl: string;
+};
+
+function ShoppingItem({ id, price, name, imageUrl }: ShoppingItemProps) {
+  return (
+    <Container aria-label="하나의 판매 품목 정보">
+      <ShoppingItemImage src={imageUrl} alt={name}></ShoppingItemImage>
+      <ShoppingItemContents>
+        <ShoppingItemLayout>
+          <ShoppingItemName aria-label="판매 품목 이름">{name}</ShoppingItemName>
+          <ShoppingItemPrice aria-label="판매 품목 가격">{price.toLocaleString()} 원</ShoppingItemPrice>
+        </ShoppingItemLayout>
+        <QuantityController quantity={9999} />
+      </ShoppingItemContents>
+    </Container>
+  );
 }
 
 export default ShoppingItem;
+
+const Container = styled.div`
+  width: 282px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ShoppingItemImage = styled.img`
+  height: 282px;
+  margin-bottom: 18px;
+`;
+
+const ShoppingItemContents = styled.div`
+  display: grid;
+  grid-template-columns: 1fr auto;
+  align-items: flex-start;
+  column-gap: 20px;
+  color: #4f4f4f;
+`;
+
+const ShoppingItemLayout = styled.div`
+  margin-left: 10px;
+`;
+
+const ShoppingItemName = styled.div`
+  line-height: 20px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+const ShoppingItemPrice = styled.div`
+  font-size: 20px;
+  margin-top: 5px;
+`;
