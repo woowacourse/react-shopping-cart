72b51c278a93cd42197c54fee2da6675793ba710
ashleysyheo <ashleysyheo@gmail.com>
2023-05-09 17:42:16 +0900

    feat: 장바구니 아이템 개수 표시 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,17 @@
+import { useRecoilValue } from 'recoil';
+
 import Logo from '../../assets/logo.svg';
+import { cartListState } from '../../pages/ProductListPage';
 import styles from './style.module.css';
 
 const Header = () => {
-  // useRecoilValue
-
+  const cartItemList = useRecoilValue(cartListState);
+  console.log(cartItemList);
   return (
     <header className={styles.container}>
       <img src={Logo} alt="logo" />
       <button type="button">
-        장바구니 <span>0</span>
+        장바구니 <span className={styles.cartItemCount}>{cartItemList.length}</span>
       </button>
     </header>
   );
