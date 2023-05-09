84a331f58d4bc70f2a62163eb722b9a91b858703
Dain Lee <feb.dain@gmail.com>
2023-05-09 15:50:15 +0900

    design: reset.css 추가 및 글로벌 스타일 적용

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 import App from './App';
+import { GlobalStyle } from './GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <RecoilRoot>
+      <GlobalStyle />
       <App />
     </RecoilRoot>
   </React.StrictMode>
