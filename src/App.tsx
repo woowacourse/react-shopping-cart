429b09b7e74752520f95aec4be4cc0a7e422839c
nlom0218 <nlom0218@naver.com>
2023-05-09 13:47:57 +0900

    chore: 프리티어 설정

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,8 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React {a}
         </a>
       </header>
     </div>
