43acfea0a5e502073314cd532a31d1bcceb4b09b
Leejin-Yang <ejin1996@gmail.com>
2023-05-09 21:07:31 +0900

    test: AmountCounter 스토리 작성
    Co-authored-by: sᴏʟʙɪ ☔️ <solbi2004@naver.com>

---
+++ src/stories/AmountCounter.stories.tsx
@@ -0,0 +1,17 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import AmountCounter from '../components/Common/AmountCounter';
+
+const meta = {
+  title: 'Common/AmountCounter',
+  component: AmountCounter,
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof AmountCounter>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+};
