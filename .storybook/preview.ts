d136e30cbd1bf2d3800366a7dcf9ae87b80b041c
TaeeunKim <xodms0309@naver.com>
2023-05-09 17:58:14 +0900

    chore: storybook에 Global Style 적용
    Co-authored-by: hozzijeong <ghwls0905@naver.com>

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,22 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from '@storybook/react';
+import { withThemeFromJSXProvider } from '@storybook/addon-styling';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '../src/styles/GlobalStyle';
+import theme from '../src/styles/theme';
+
+export const decorators = [
+  withThemeFromJSXProvider({
+    GlobalStyles: GlobalStyle, // Adds your GlobalStyle component to all stories
+    themes: {
+      theme: theme,
+    },
+    Provider: ThemeProvider,
+  }),
+];
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
