8a6e1df29e7adb59a6105bfa6c6974da925041d0
gyeongza <gyeongza@naver.com>
2023-05-09 14:38:27 +0900

    docs: 기능 구현 목록 수정
    Co-authored-by: semnil5202 <semnil5202@gmail.com>

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from 'styled-components';
+import './reset.css';
+
+export const GlobalStyle = createGlobalStyle`
+  @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap");
+
+  * {
+    box-sizing: border-box;
+    font-family: "Noto Sans KR", sans-serif !important;
+  }
+  
+  html,
+  body {
+    /* hidden scroll */
+    scrollbar-width: none;
+  }
+
+  body::-webkit-scrollbar {
+    /* hidden scroll */
+    display: none;
+  }
+
+  /* Colors *****************************************/
+  :root {
+   
+  }
+
+  #root {
+    
+  }
+
+  .label-text {
+   
+  }
+`;
