f16602cfda4e6e28d280d0dab34e0a5a195a4798
Leejin-Yang <ejin1996@gmail.com>
2023-05-09 16:39:15 +0900

    test: ProductItem 스토리 작성
    Co-authored-by: sᴏʟʙɪ ☔️ <solbi2004@naver.com>

---
+++ .storybook/preview.tsx
@@ -0,0 +1,20 @@
+import GlobalStyle from '../src/GlobalStyle';
+import React from 'react';
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyle />
+      <Story />
+    </>
+  ),
+];
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
---
+++ src/stories/ProductItem.stories.tsx
@@ -0,0 +1,26 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import ProductItem from '../components/Product/ProductItem';
+
+const meta = {
+  title: 'Product/ProductItem',
+  component: ProductItem,
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof ProductItem>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const mockCard = {
+  id: 1,
+  name: 'PET보틀-정사각(420ml)',
+  price: 43400,
+  imageUrl: 'images/정사각-420.jpeg',
+};
+
+export const Default: Story = {
+  args: {
+    product: mockCard,
+  },
+};
