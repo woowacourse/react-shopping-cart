edbc70a13b15e90e9f5fd48eba7e839d674b0fa1
yeopto <gunyeop530@gmail.com>
2023-05-10 14:55:03 +0900

    feat: PageTemplate 컴포넌트 추가
    Co-authored-by: SangwonKang <eksovus@gmail.com>

---
+++ src/components/templates/PageTemplate.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+import type { PropsWithChildren } from 'react';
+
+const PageTemplate = ({ children }: PropsWithChildren) => {
+  return <PageWrapper>{children}</PageWrapper>;
+};
+
+export default PageTemplate;
+
+const PageWrapper = styled.div`
+  width: 100%;
+  margin: 62px 0;
+  display: flex;
+  justify-content: center;
+`;
