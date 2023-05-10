48a3d12018f7ba32ac8dadba495fd99dc09d3225
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-05-10 16:03:29 +0900

    refactor: fetch API를 통해 Products data 바인딩

--- src/pages/ProductListPage.tsx
+++ src/pages/ProductListPage.tsx
@@ -1,17 +1,22 @@
+import { useState, useEffect } from 'react';
+import { Product } from 'types/product';
 import ProductCardList from 'components/ProductCardList/ProductCardList';
 
-const dummy = {
-  id: '1',
-  imageUrl:
-    'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMzA0MTRfMjA2%2FMDAxNjgxNDQ0NzExNTY2.oQJxSkWybX9upYDFCq8NeV9jQT4s8ilqSDjsN3Fjiksg.OX2y8cOp_hhosojH-oo7HbO7PWtV7r3g6J8g-9rjoHsg.JPEG.diane4743%2FIMG_6127.jpg&type=sc960_832',
-  name: '삼겹살',
-  price: 10000,
-};
-
 const ProductListPage = () => {
-  return (
-    <ProductCardList products={[dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy]} />
-  );
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch(`${process.env.PUBLIC_URL}/data/mockProducts.json`);
+      const products = await response.json();
+
+      setProducts(products);
+    };
+
+    fetchProducts();
+  }, []);
+
+  return <ProductCardList products={products} />;
 };
 
 export default ProductListPage;
