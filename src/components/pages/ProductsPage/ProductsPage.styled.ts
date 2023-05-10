d17f7188ed339405f7467b56a7f69ee3d269bc1b
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-05-10 16:03:43 +0900

    feat: ProductsPage 컴포넌트 구현

---
+++ src/components/pages/ProductsPage/ProductsPage.styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const ProductsPage = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100vw;
+  height: 100vh;
+`;
---
+++ src/components/pages/ProductsPage/ProductsPage.tsx
@@ -0,0 +1,12 @@
+import * as Styled from './ProductsPage.styled';
+import ProductList from '../../ProductList/ProductList';
+
+const ProductsPage = () => {
+  return (
+    <Styled.ProductsPage>
+      <ProductList />
+    </Styled.ProductsPage>
+  );
+};
+
+export default ProductsPage;
