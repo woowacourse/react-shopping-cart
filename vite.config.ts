300362f7ac0bfba8f2113cc511eeac1a8ab5baa2
ukkodeveloper <ukkodeveloper@gmail.com>
2023-05-10 15:36:27 +0900

    fix: 사용하지 않는 라이브러리 import 제거

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
