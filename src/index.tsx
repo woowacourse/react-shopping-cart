6e04f3c4935dadae384d70231e97222248ddb8bd
AhnGeunTaek <dksrmsxor@gmail.com>
2023-05-10 19:39:05 +0900

    chore: Recoil 설치 및 기본 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import { RecoilRoot } from "recoil";
+import "./index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </React.StrictMode>
 );
