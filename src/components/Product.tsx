60a87488d5c829cd64d27008003fd455ccdd3204
Jungwoo <evencoding@gmail.com>
2023-05-10 15:54:23 +0900

    feat: Product 컴포넌트 생성
    Co-authored-by: HyeryongChoi <chex1004@gmail.com>

---
+++ src/components/Product.tsx
@@ -0,0 +1,61 @@
+import { styled } from 'styled-components';
+import { CartIcon } from '../assets/svg';
+
+export default function Product({ data }: any) {
+  return (
+    <Style.Container>
+      <Style.ProductImage path={data.imageUrl} />
+      <Style.ProductInfo>
+        <div>
+          <Style.ProductName>{data.name}</Style.ProductName>
+          <Style.ProductPrice>{data.price}원</Style.ProductPrice>
+        </div>
+        <Style.CartIconWrapper>
+          <CartIcon fill="#AAAAAA" />
+        </Style.CartIconWrapper>
+      </Style.ProductInfo>
+    </Style.Container>
+  );
+}
+
+const Style = {
+  Container: styled.div`
+    width: 282px;
+  `,
+
+  ProductImage: styled.div<{ path: string }>`
+    width: 282px;
+    height: 282px;
+
+    background-image: ${(props) => `url(${props.path})`};
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+  `,
+
+  ProductInfo: styled.div`
+    display: flex;
+    justify-content: space-between;
+
+    padding: 18px 12px 0 12px;
+    letter-spacing: 0.5px;
+  `,
+
+  ProductName: styled.p`
+    margin-bottom: 8px;
+  `,
+
+  ProductPrice: styled.p`
+    font-size: 20px;
+  `,
+
+  CartIconWrapper: styled.button`
+    padding: 0;
+    margin: 0;
+    border: none;
+    background-color: inherit;
+    height: fit-content;
+
+    cursor: pointer;
+  `,
+};
