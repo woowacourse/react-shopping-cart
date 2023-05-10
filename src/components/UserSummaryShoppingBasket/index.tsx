7a79c7f7ff70f90ef82338e0e47a1521ceed8c42
nlom0218 <nlom0218@naver.com>
2023-05-10 14:34:53 +0900

    test: 사용자의 장바구니 수량을 나타내는 컴포넌트에 PlusIcon 추가하기

--- src/components/UserSummaryShoppingBasket/index.tsx
+++ src/components/UserSummaryShoppingBasket/index.tsx
@@ -4,7 +4,8 @@ function UserSummaryShoppingBasket({ username, quantity }: { username?: string; 
   return (
     <Container>
       <Username>{username && `${username}의 `}장바구니</Username>
-      <Quantity>{quantity}</Quantity>
+      <Quantity>{quantity > 99 ? '99' : quantity}</Quantity>
+      {quantity > 99 && <PlusIcon aria-label="99개 이상입니다.">+</PlusIcon>}
     </Container>
   );
 }
@@ -17,7 +18,8 @@ const Container = styled.div`
 `;
 
 const Username = styled.div`
-  margin-right: 10px;
+  margin-right: 5px;
+  color: #ffffff;
 `;
 
 const Quantity = styled.div`
@@ -33,4 +35,12 @@ const Quantity = styled.div`
   align-items: center;
   justify-content: center;
   font-size: 12px;
+`;
+
+const PlusIcon = styled.span`
+  position: relative;
+  bottom: 9px;
+  right: 1px;
+  color: #ffffff;
+  font-weight: 700;
 `;
