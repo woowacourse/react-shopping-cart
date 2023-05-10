33f1ca57126c4519fe71ee739260a854b1697d52
yeopto <gunyeop530@gmail.com>
2023-05-10 13:50:07 +0900

    feat: ProductItem 컴포넌트를 만들다
    Co-authored-by: SangwonKang <eksovus@gmail.com>

---
+++ src/components/box/ProductItem.tsx
@@ -0,0 +1,51 @@
+import styled from '@emotion/styled';
+// import { CartIcon } from '../../assets';
+import type { Product } from '../../types/types';
+import { Text } from '../common/Text/Text';
+import InputStepper from '../common/InputStepper/InputStepper';
+
+const ProductItem = ({ product }: { product: Product }) => {
+  return (
+    <ProductWrapper>
+      <ProductImage src={product.imageUrl} alt={product.name} />
+      <ProductInfoWrapper>
+        <ProductTextWrapper>
+          <Text size="smallest" weight="light" color="#333333">
+            {product.name}
+          </Text>
+          <Text size="small" weight="light" color="#333333" lineHeight="33px">
+            {product.price} 원
+          </Text>
+        </ProductTextWrapper>
+        <InputStepper size="small" />
+        {/* <CartIcon width={25} height={22} fill="#AAAAAA" style={{ transform: 'scaleX(-1)' }} /> */}
+      </ProductInfoWrapper>
+    </ProductWrapper>
+  );
+};
+
+export default ProductItem;
+
+const ProductWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 282px;
+`;
+const ProductImage = styled.img`
+  width: 100%;
+  height: 282px;
+`;
+
+const ProductInfoWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  width: 100%;
+  padding: 12px;
+`;
+
+const ProductTextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
