94f972c38cc761b8112af79474b5331a02ffc573
Suyoung <jsy8492@gmail.com>
2023-05-10 16:54:44 +0900

    refactor: ProductList 스타일 수정

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,10 +1,11 @@
 import { styled } from 'styled-components';
-import type { Product } from '../../types/product';
 import ProductItem from '../ProductItem/ProductItem';
+import type { Product } from '../../types/product';
 
 interface ProductListProps {
   products: Product[];
 }
+
 const ProductList = ({ products }: ProductListProps) => {
   return (
     <section>
@@ -21,9 +22,15 @@ const ProductList = ({ products }: ProductListProps) => {
 
 const ProductListContainer = styled.ul`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(282px, 1fr));
-  grid-column-gap: 20px;
+  grid-template-columns: repeat(auto-fit, minmax(282px, 1fr));
+  grid-column-gap: 30px;
   grid-row-gap: 48px;
+
+  place-items: center;
+
+  @media screen and (min-width: 1200px) {
+    grid-template-columns: repeat(4, minmax(282px, 1fr));
+  }
 `;
 
 export default ProductList;
