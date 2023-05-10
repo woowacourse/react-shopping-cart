1513763d5e52ceb1b40cb2ca0c0197e2fe0dc179
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-05-10 20:11:42 +0900

    style : 이미지 추가
    Co-authored-by: 2yunseong <2yunseong@users.noreply.github.com>

--- src/asset/index.ts
+++ src/asset/index.ts
@@ -1,2 +1,4 @@
 export { ReactComponent as HeaderIc } from "./shopping-cart-white.svg";
 export { ReactComponent as AddCartIc } from "./shopping-cart-gray.svg";
+export { ReactComponent as UpButtonIc } from "./up-button.svg";
+export { ReactComponent as DownButtonIc } from "./down-button.svg";
