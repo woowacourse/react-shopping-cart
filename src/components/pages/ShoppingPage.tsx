fa9756dee15a50e81bb99689a2955f39745b62de
yeopto <gunyeop530@gmail.com>
2023-05-10 14:57:12 +0900

    feat: ShoppingPage 컴포넌트 구현
    Co-authored-by: SangwonKang <eksovus@gmail.com>

---
+++ src/components/pages/ShoppingPage.tsx
@@ -0,0 +1,12 @@
+import PageTemplate from '../templates/PageTemplate';
+import ProductList from '../list/ProductList';
+
+const ShoppingPage = () => {
+  return (
+    <PageTemplate>
+      <ProductList />
+    </PageTemplate>
+  );
+};
+
+export default ShoppingPage;
