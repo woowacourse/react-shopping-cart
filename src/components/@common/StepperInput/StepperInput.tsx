15661c8fca01c6a7e8ed0f4907bbe052c6428f4f
sh981013s <sh981013s@gmail.com>
2023-05-10 17:34:00 +0900

    feat: stepperInput max value 적용

--- src/components/@common/StepperInput/StepperInput.tsx
+++ src/components/@common/StepperInput/StepperInput.tsx
@@ -8,7 +8,7 @@ type StepperInputProps = {
 };
 
 const StepperInput = ({ width = 65, height = 28, value, onChange }: StepperInputProps) => {
-  return <Styled.StepperInputContainer type='number' step='1' min='0' width={width} height={height} value={value} onChange={onChange} />;
+  return <Styled.StepperInputContainer type='number' step='1' min='0' max='99' width={width} height={height} value={value} onChange={onChange} />;
 };
 
 export default StepperInput;
