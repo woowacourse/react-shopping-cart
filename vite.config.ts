88a3f269604b669b88c9b440a6884bc408a1832a
ukkodeveloper <ukkodeveloper@gmail.com>
2023-05-10 15:13:30 +0900

    fix: 배포시 빌드 버그 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,7 @@ import react from '@vitejs/plugin-react';
 import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  return {
-    plugins: [
-      react(),
-      // 개발 모드에서만 ESLint를 적용하도록 설정
-      command === 'serve' && eslintPlugin({ include: 'src/**/*.{js,jsx,ts,tsx}' }),
-    ].filter(Boolean), // 필터를 사용하여 'false'를 제거
-    base: '/react-shopping-cart/',
-  };
+export default defineConfig({
+  plugins: [react()],
+  base: '/react-shopping-cart/',
 });
