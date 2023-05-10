e23a78955c83ed7079df953c51d7f01aa61efd99
ukkodeveloper <ukkodeveloper@gmail.com>
2023-05-10 15:45:08 +0900

    fix: storybook 테스트를 위한 router-dom providers에 추가

--- src/stories/Providers.tsx
+++ src/stories/Providers.tsx
@@ -2,14 +2,18 @@ import { ReactNode } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../style/theme';
 import GlobalStyle from '../style/globalStyle';
+import { BrowserRouter } from 'react-router-dom';
 
 function Providers({ children }: { children: ReactNode }) {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        {children}
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+
+          {children}
+        </ThemeProvider>
+      </BrowserRouter>
     </>
   );
 }
