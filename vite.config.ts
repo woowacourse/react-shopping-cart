68b67fe3fdb754d7c0106ad1e98aa2ff380e1778
ukkodeveloper <ukkodeveloper@gmail.com>
2023-05-10 14:52:42 +0900

    chore: 배포 중 eslint 적용하지 않도록 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/react-shopping-cart/',
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [
+      react(),
+      // 개발 모드에서만 ESLint를 적용하도록 설정
+      command === 'serve' && eslintPlugin({ include: 'src/**/*.{js,jsx,ts,tsx}' }),
+    ].filter(Boolean), // 필터를 사용하여 'false'를 제거
+    base: '/react-shopping-cart/',
+  };
 });
