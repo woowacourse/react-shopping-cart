8ace40951fb2b0a1cff0bb80be4d2ee7133d85d7
ashleysyheo <ashleysyheo@gmail.com>
2023-05-10 13:02:43 +0900

    refactor: Header 로고 가운데 정렬

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,20 @@
 import { useRecoilValue } from 'recoil';
 
-import Logo from '../../assets/logo.svg';
+import Logo from '../../assets/logo.png';
 import { cartListState } from '../../pages/ProductListPage';
 import styles from './style.module.css';
 
 const Header = () => {
   const cartItemList = useRecoilValue(cartListState);
-  console.log(cartItemList);
+
   return (
-    <header className={styles.container}>
-      <img src={Logo} alt="logo" />
-      <button type="button">
-        장바구니 <span className={styles.cartItemCount}>{cartItemList.length}</span>
-      </button>
+    <header>
+      <div className={styles.container}>
+        <img src={Logo} alt="logo" />
+        <button type="button">
+          장바구니 <span className={styles.cartItemCount}>{cartItemList.length}</span>
+        </button>
+      </div>
     </header>
   );
 };
