183342dc849d2ae0dcb63eba6ca66d3da9035f60
cruelladevil <dev.timetravel@gmail.com>
2023-05-10 16:55:18 +0900

    feat: ProductPage 구현

---
+++ src/pages/ProductPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+import ProductList from '../components/ProductList';
+
+const ProductPage: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <ProductList />
+    </>
+  );
+};
+
+export default ProductPage;
