0467b2c5a26e1595ee1fba4715e1a0b812b3c1f6
Jungwoo <evencoding@gmail.com>
2023-05-10 15:58:02 +0900

    feat: Button 컴포넌트에 대한 스토리 구현
    Co-authored-by: HyeryongChoi <chex1004@gmail.com>

---
+++ src/stories/common/Button.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Button from '../../components/common/Button';
+
+const meta = {
+  title: 'ShoppingCart/common/Button',
+  component: Button,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+  },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Square: Story = {
+  args: {
+    designType: 'square',
+  },
+};
+
+export const Rectangle: Story = {
+  args: {
+    bgColor: 'primary',
+    designType: 'rectangle',
+  },
+};
