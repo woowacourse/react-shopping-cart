0fa43e0ff9db66153ad64770b10a62fa2eb5804e
AhnGeunTaek <dksrmsxor@gmail.com>
2023-05-10 19:36:55 +0900

    feat: ProductCardList 컴포넌트 생성

---
+++ src/components/ProductCardList/ProductCardList.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+import ProductCard from "../ProductCard/ProductCard";
+import mockData from "../../mockData/mockData.json";
+
+const ProductCardList = () => {
+  return (
+    <ProductCardListContainer>
+      {mockData.map((product) => (
+        <ProductCard
+          key={product.id}
+          productImage={product.imageUrl}
+          productName={product.name}
+          productPrice={product.price}
+        />
+      ))}
+    </ProductCardListContainer>
+  );
+};
+
+const ProductCardListContainer = styled.div`
+  display: grid;
+  padding-top: 61.16px;
+
+  grid-template-columns: repeat(4, 1fr);
+  grid-column-gap: 47.16px;
+  grid-row-gap: 85.81px;
+  width: 1270.43px;
+  height: auto;
+  margin: 0 auto 100px auto;
+`;
+
+export default ProductCardList;
