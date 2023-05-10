f0179c3773915c7e19147d079609c4f08ecda484
kyw0716 <kyw0716@naver.com>
2023-05-10 17:29:45 +0900

    refactor: useMockData 커스텀 훅 추가
    Co-authored-by: tkdrb12 <tkdrb15@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import mockData from './assets/mockData.json';
 import { ProductCardList } from './components/productCardList/ProductCardList';
 import { Layout } from './layout';
-import { atom } from 'recoil';
-
-export const cartListState = atom<number[]>({
-  key: 'cartListState',
-  default: [],
-});
+import { useMockData } from './hooks/useMockData';
 
 function App() {
+  const { mockData } = useMockData();
+
   return (
     <Layout>
       <ProductCardList products={mockData} />
