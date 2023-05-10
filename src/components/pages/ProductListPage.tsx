6a5c05af06e185844da52fa0d1a5f06181944677
bassyu <yrkdgus@gmail.com>
2023-05-10 17:12:07 +0900

    feat(ProductListPage): 메인 페이지 구현

---
+++ src/components/pages/ProductListPage.tsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import { Header, ProductList } from "../../components";
+import mockProduct from "../../../public/assets/mockProducts.json";
+
+export default function ProductListPage() {
+  const products = mockProduct;
+
+  return (
+    <$Wrapper>
+      <Header />
+      <main>
+        <ProductList products={products} />
+      </main>
+    </$Wrapper>
+  );
+}
+
+const $Wrapper = styled.div`
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100%;
+  padding-top: 80px;
+
+  main {
+    width: 1272px;
+    padding: 60px 0;
+  }
+`;
