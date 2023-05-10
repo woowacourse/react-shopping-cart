052148a54a9a0030af325a82f8fa8e88a3dd0dfb
cruelladevil <dev.timetravel@gmail.com>
2023-05-10 16:49:17 +0900

    feat: Router, RecoilRoot 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import ProductPage from './pages/ProductPage';
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <RecoilRoot>
+        <Routes>
+          <Route path="/" element={<ProductPage />} />
+        </Routes>
+      </RecoilRoot>
+    </BrowserRouter>
+  );
 }
 
 export default App;
