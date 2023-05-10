c8e032658188a85970e8da845e396cf3442f0ce9
Jungwoo <evencoding@gmail.com>
2023-05-10 18:11:12 +0900

    test: Stepper 컴포넌트 Story의 button, Input 유효성 테스트
    Co-authored-by: HyeryongChoi <chex1004@gmail.com>

--- src/stories/common/Stepper.stories.tsx
+++ src/stories/common/Stepper.stories.tsx
@@ -1,5 +1,8 @@
-import type { Meta, StoryFn } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 import Stepper from '../../components/Stepper';
+import { expect } from '@storybook/jest';
+import { within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 const meta = {
   title: 'ShoppingCart/common/Stepper',
@@ -11,8 +14,104 @@ const meta = {
 } satisfies Meta<typeof Stepper>;
 
 export default meta;
-type Story = StoryFn<typeof meta>;
+type Story = StoryObj<typeof meta>;
 
-export const Default: Story = () => {
-  return <Stepper initCount={1} />;
+export const Default: Story = {
+  args: {
+    initCount: 1,
+  },
+};
+
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export const ClickPlusButton: Story = {
+  args: {
+    initCount: 1,
+  },
+
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const countInput = await canvas.findByRole('textbox');
+    const plusButton = await canvas.findByText('+');
+
+    await delay(400);
+    userEvent.click(plusButton);
+
+    expect(countInput).toHaveValue('2');
+  },
+};
+
+export const ClickMinusButton: Story = {
+  args: {
+    initCount: 5,
+  },
+
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const countInput = await canvas.findByRole('textbox');
+    const minusButton = await canvas.findByText('-');
+
+    await delay(400);
+    userEvent.click(minusButton);
+
+    expect(countInput).toHaveValue('4');
+  },
+};
+
+export const ButtonToDisableIfMinCount: Story = {
+  args: {
+    initCount: 2,
+  },
+
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const countInput = await canvas.findByRole('textbox');
+    const minusButton = await canvas.findByText('-');
+
+    await delay(400);
+    userEvent.click(minusButton);
+
+    expect(countInput).toHaveValue('1');
+    expect(minusButton).toBeDisabled();
+  },
+};
+
+export const ButtonToDisableIfMaxCount: Story = {
+  args: {
+    initCount: 98,
+  },
+
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const countInput = await canvas.findByRole('textbox');
+    const plusButton = await canvas.findByText('+');
+
+    await delay(400);
+    userEvent.click(plusButton);
+
+    expect(countInput).toHaveValue('99');
+    expect(plusButton).toBeDisabled();
+  },
+};
+
+export const CanNotBeOverMaxCount: Story = {
+  args: {
+    initCount: 1,
+  },
+
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const countInput = await canvas.findByRole('textbox');
+    const plusButton = await canvas.findByText('+');
+
+    await userEvent.type(countInput, '100', { delay: 500 });
+
+    expect(countInput).toHaveValue('99');
+    expect(plusButton).toBeDisabled();
+  },
 };
