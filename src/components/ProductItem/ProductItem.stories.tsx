2723b60f04ddb51086df0eeeec3ee3f24befcd21
Q Kim <qoli0703@gmail.com>
2023-05-11 17:14:13 +0900

    test: imageUrl 변경

--- src/components/ProductItem/ProductItem.stories.tsx
+++ src/components/ProductItem/ProductItem.stories.tsx
@@ -1,12 +1,10 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { RecoilRoot } from 'recoil';
 
-import catImage from '../../assets/cat.png';
-
 import ProductItem from './ProductItem';
 
 /**
- * 설명창
+ * 상품 1개 컴포넌트
  */
 
 const meta: Meta<typeof ProductItem> = {
@@ -27,28 +25,13 @@ export default meta;
 
 type Story = StoryObj<typeof ProductItem>;
 
-/**
- * 테스트
- */
-
 export const Default: Story = {
   args: {
     product: {
       id: 1,
       name: '1',
       price: 1,
-      imageUrl: catImage,
-    },
-  },
-};
-
-export const Clicked: Story = {
-  args: {
-    product: {
-      id: 1,
-      name: '1',
-      price: 1,
-      imageUrl: catImage,
+      imageUrl: 'https://images.unsplash.com/photo-1507477338202-487281e6c27e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
     },
   },
 };
