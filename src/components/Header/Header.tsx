58c0ee60752a2a9eedc14a514da38bafac24ba0d
ashleysyheo <ashleysyheo@gmail.com>
2023-05-11 10:50:42 +0900

    refactor: 헤더 장바구니 버튼 수정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import { memo } from 'react';
 import { useRecoilValue } from 'recoil';
 
+import CartIcon from '../../assets/cart-icon.svg';
 import Logo from '../../assets/logo.png';
 import { cartListState } from '../../pages/ProductListPage';
 import styles from './style.module.css';
@@ -11,9 +12,13 @@ const Header = () => {
   return (
     <header>
       <div className={styles.container}>
-        <img src={Logo} alt="logo" />
+        <img src={Logo} alt="logo" className={styles.logo} />
         <button type="button">
-          장바구니 <span className={styles.cartItemCount}>{cartItemList.length}</span>
+          {cartItemList.length > 0 && (
+            <span className={styles.cartItemCount}>{cartItemList.length}</span>
+          )}
+          <img src={CartIcon} alt="cart icon" className={styles.cartIcon} />
+          <span className={styles.label}>장바구니</span>
         </button>
       </div>
     </header>
