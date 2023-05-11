6caf18d8b523b1a825abd68e602a6b1cd5089bf0
jeongwusi <jeongwusi@gmail.com>
2023-05-11 15:15:03 +0900

    ♻️ refactor: 404페이지 템플릿 구현
    Co-authored-by: d0dam<smallkdb@gmail.com>

--- src/Page/NotFound/index.tsx
+++ src/Page/NotFound/index.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import styles from './index.module.css';
 
 const NotFound = () => {
-  return <div>잘못된 페이지입니다.</div>;
+  return (
+    <div className={styles.container}>
+      <h2>Oops! Page not found.</h2>
+      <h1>404</h1>
+      <p>We can't find the page you're looking for.</p>
+      <a href="/">Go back home</a>
+    </div>
+  );
 };
 
 export default NotFound;
