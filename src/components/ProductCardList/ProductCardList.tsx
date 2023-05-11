2f241521bf286b701881f05dd51fd5b31fac4a7e
wzrabbit <curious.wzrabbit@gmail.com>
2023-05-12 00:28:49 +0900

    🎨 style: 반응형으로 리스트의 열 수가 변경되도록 개선

--- src/components/ProductCardList/ProductCardList.tsx
+++ src/components/ProductCardList/ProductCardList.tsx
@@ -21,13 +21,24 @@ const ProductCardList = () => {
 const ProductCardListContainer = styled.div`
   display: grid;
   padding-top: 61.16px;
-
   grid-template-columns: repeat(4, 1fr);
   grid-column-gap: 47.16px;
   grid-row-gap: 85.81px;
-  width: 1270.43px;
+  max-width: 1270.43px;
   height: auto;
   margin: 0 auto 100px auto;
+
+  @media (max-width: 1300px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (max-width: 960px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (max-width: 630px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
 `;
 
 export default ProductCardList;
