ff1c46a8701dc89e38edc2f226189792a900603b
inyeong-kang <tomatorkd@naver.com>
2023-05-11 15:25:49 +0900

    chore: output path 수정

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
   },
   devServer: {
     static: {
