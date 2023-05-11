6feaa54bc7394eee95f6bf6b959086d29739afaf
SangwonKang <72087183+ksone02@users.noreply.github.com>
2023-05-11 16:03:34 +0900

    feat: fetch 로딩 상태 구현

--- src/hooks/useDataFetching.ts
+++ src/hooks/useDataFetching.ts
@@ -4,20 +4,24 @@ import { useEffect, useState } from 'react';
 
 const useDataFetching = <T>(path: string) => {
   const [data, setData] = useState<T | undefined>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  const getData = () =>
-    axios
+  const getData = async () => {
+    setIsLoading(true);
+    await axios
       .get(path)
       .then((result) => setData(result.data))
       .catch((error) => {
         console.log(error);
       });
+    setIsLoading(false);
+  };
 
   useEffect(() => {
     getData();
   }, []);
 
-  return { data };
+  return { data, isLoading };
 };
 
 export default useDataFetching;
