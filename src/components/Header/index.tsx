99b3cb4d91bfbea30285c4dcd699344f1c27bc5e
jeongwusi <jeongwusi@gmail.com>
2023-05-11 15:20:20 +0900

    ✨ feat: 헤더 로고 버튼 홈으로 돌아가는 기능 추가
    Co-authored-by: d0dam<smallkdb@gmail.com>

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,12 +2,15 @@ import { useRecoilValue } from 'recoil';
 import { ReactComponent as Logo } from '../../assets/logo.svg';
 import styles from './index.module.css';
 import { $CartCountTotal } from '../../recoil/atom';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   const cartCountTotal = useRecoilValue($CartCountTotal);
   return (
     <header className={styles.container}>
-      <Logo />
+      <Link to="/">
+        <Logo />
+      </Link>
       <div className={styles.cart}>
         <button>장바구니</button>
         <div className={styles['cart-count']}>{cartCountTotal}</div>
