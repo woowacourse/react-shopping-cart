c286b3cbb8c74b8aa2ed0f21db233bfb6902f921
Patrick <kuyt3122@naver.com>
2023-05-11 16:55:49 +0900

    feat: constants 절대 경로 및 import 순서 설정, testId 파일 생성

---
+++ src/constants/testId.ts
@@ -0,0 +1,4 @@
+export const BUCKET_COUNTER_TOP_BUTTON = 'bucketCounterTopButton';
+export const BUCKET_COUNTER_BOTTOM_BUTTON = 'bucketCounterBottomButton';
+export const ADD_CART_BUTTON = 'addCartButton';
+export const CART_COUNT_INPUT = 'cartCountInput';
