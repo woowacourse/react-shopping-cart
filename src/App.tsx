f7691f5197c1193e14654cb6c45aef12b566877d
cruelladevil <dev.timetravel@gmail.com>
2023-05-11 10:59:34 +0900

    feat: NotFoundPage 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import ProductPage from './pages/ProductPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <RecoilRoot>
         <Routes>
           <Route path="/" element={<ProductPage />} />
+          <Route path="*" element={<NotFoundPage>요청하신 페이지를 찾을 수 없습니다.</NotFoundPage>} />
         </Routes>
       </RecoilRoot>
     </BrowserRouter>
