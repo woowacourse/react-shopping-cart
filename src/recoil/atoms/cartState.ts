bf16877379273d12e0f08444b60c7c392ca5197c
solo5star <solo_5@naver.com>
2023-05-11 02:24:47 +0000

    feat: 장바구니 상태 localStorage저장
    Co-authored-by: afds4567 <afds4567@gmail.com>

--- src/recoil/atoms/cartState.ts
+++ src/recoil/atoms/cartState.ts
@@ -1,9 +1,11 @@
 import { atom } from 'recoil';
 import type { Cart } from '../../type';
+import localStorageEffect from '../effects/localStorageEffect';
 
 const cartState = atom<Cart>({
   key: 'cartState',
   default: [],
+  effects: [localStorageEffect('cartState')],
 });
 
 export default cartState;
