683e9a7747c14c85870c2a8d29a3c306c81aea9a
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-05-11 11:02:23 +0900

    feat: 라우트 설정
    Co-authored-by: Rulu <hafnium1923@users.noreply.github.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
+import ProductPage from './pages/ProductPage';
 import GlobalStyle from './styles/GlobalStyle';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '',
+        element: <ProductPage />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <GlobalStyle />
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
