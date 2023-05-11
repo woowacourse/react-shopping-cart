0b8f37efb3fd29fe7a625b5bc8e0a0557fadd557
Leejin-Yang <ejin1996@gmail.com>
2023-05-11 11:41:03 +0900

    test: AmountCounter 스토리 value 수정
    Co-authored-by: sᴏʟʙɪ ☔️ <solbi2004@naver.com>

--- src/stories/AmountCounter.stories.tsx
+++ src/stories/AmountCounter.stories.tsx
@@ -1,27 +1,45 @@
 import { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
 
 import AmountCounter from '../components/Common/AmountCounter';
-import { RecoilRoot } from 'recoil';
 
-const meta = {
+interface AmountCounterProps {
+  count: number;
+  addCount: () => void;
+  subtractCount: () => void;
+}
+
+const meta: Meta = {
   title: 'Common/AmountCounter',
   component: AmountCounter,
   tags: ['autodocs'],
-  argTypes: {},
-  decorators: [
-    (Story) => (
-      <RecoilRoot>
-        <Story />
-      </RecoilRoot>
-    ),
-  ],
-} satisfies Meta<typeof AmountCounter>;
+  args: { count: 1, addCount: () => {}, subtractCount: () => {} },
+};
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {
-    count: 0,
-  },
+export const Default: Story = (args: AmountCounterProps) => {
+  const [count, setCount] = useState(1);
+
+  const addCount = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  const subtractCount = () => {
+    if (count > 1) {
+      setCount((prev) => prev - 1);
+    }
+  };
+
+  return (
+    <AmountCounter
+      {...args}
+      count={count}
+      addCount={addCount}
+      subtractCount={subtractCount}
+    />
+  );
 };
+
+Default.args = {};
