9b39ee1d002fbb62d6d0e72314d3c15a847a1253
solo5star <solo_5@naver.com>
2023-05-11 08:42:35 +0000

    fix: 상대 경로로 수정
    Co-authored-by: afds4567 <afds4567@gmail.com>

--- src/api/FixtureClient.ts
+++ src/api/FixtureClient.ts
@@ -6,7 +6,7 @@ class FixtureClient extends Client {
   }
 
   override async get(path: string) {
-    return super.get(`/fixtures${path}.json`);
+    return super.get(`./fixtures${path}.json`);
   }
 }
 
