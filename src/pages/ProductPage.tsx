c1820cbe6567473f0e44391dd8725a0a9cbfe554
cruelladevil <dev.timetravel@gmail.com>
2023-05-11 11:03:32 +0900

    feat: Suspense, Errorboundary 적용

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import Header from '../components/Header';
 import ProductList from '../components/ProductList';
+import LoadingPage from './LoadingPage';
+import ErrorBoundary from '../components/ErrorBoundary';
 
 const ProductPage: React.FC = () => {
   return (
     <>
       <Header />
-      <ProductList />
+      <ErrorBoundary fallback={<div>Something went wrong.</div>}>
+        <Suspense fallback={<LoadingPage />}>
+          <ProductList />
+        </Suspense>
+      </ErrorBoundary>
     </>
   );
 };
