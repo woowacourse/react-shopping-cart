650dc00c1e5fffae2c5605a3695bb90b749e970b
gyeongza <gyeongza@naver.com>
2023-05-11 11:08:25 +0900

    fix: 스토리북 이미지 오류 수정
    Co-authored-by: semnil5202 <semnil5202@gmail.com>

--- src/stories/Image.stories.tsx
+++ src/stories/Image.stories.tsx
@@ -14,8 +14,7 @@ export const Small: Story = {
   args: {
     $width: '144px',
     $height: '144px',
-    source:
-      'https://image.idus.com/image/files/819fc904143c42d3bc973a07a089faff_720.jpg',
+    source: '/assets/2.png',
     alternative: '상품 이미지',
   },
 };
@@ -24,8 +23,7 @@ export const Large: Story = {
   args: {
     $width: '282px',
     $height: '282px',
-    source:
-      'https://image.idus.com/image/files/819fc904143c42d3bc973a07a089faff_720.jpg',
+    source: '/assets/2.png',
     alternative: '상품 이미지',
   },
 };
