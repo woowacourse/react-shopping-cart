acd39ff3a3512372aa9a9c08df881ced7f965bd8
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-05-11 14:59:18 +0900

    feat: cartState에 로컬스토리지 동기화 effects 추가

--- src/state/CartAtom.ts
+++ src/state/CartAtom.ts
@@ -1,16 +1,31 @@
-import { atom, selector, selectorFamily } from 'recoil';
+import { CART_KEY } from 'constants/storeKey';
+import { AtomEffect, atom, selector, selectorFamily } from 'recoil';
 import { CartProduct } from 'types/product';
+import store from 'utils/localStorage';
+
+const zeroQuantityFilterEffect: AtomEffect<CartProduct[]> = ({ onSet, setSelf }) => {
+  onSet((newValue) => {
+    setSelf(newValue.filter((v) => v.quantity > 0));
+  });
+};
+
+const localStorageEffect =
+  <T>(key: string, initialValue: T): AtomEffect<T> =>
+  ({ onSet, setSelf, trigger }) => {
+    if (trigger === 'get') {
+      const storageValue = store.getStorage<T>(key) ?? initialValue;
+      setSelf(storageValue);
+    }
+
+    onSet((newValue) => {
+      store.setStorage(key, newValue);
+    });
+  };
 
 export const cartState = atom<CartProduct[]>({
   key: 'cartState',
   default: [],
-  effects: [
-    ({ onSet, setSelf }) => {
-      onSet((newValue) => {
-        setSelf(newValue.filter((v) => v.quantity > 0));
-      });
-    },
-  ],
+  effects: [zeroQuantityFilterEffect, localStorageEffect<CartProduct[]>(CART_KEY, [])],
 });
 
 export const filteredCartProductState = selectorFamily({
