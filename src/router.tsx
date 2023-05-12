c757aa7fd82b4858e3e0db570055747450273081
inyeong-kang <tomatorkd@naver.com>
2023-05-12 17:57:56 +0900

    refactor: path 상수 분리

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom';
 import ProductListPage from './components/pages/ProductListPage';
+import { PATH } from './constants/path';
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: PATH.PRODUCT_LIST,
     element: <ProductListPage />,
   },
 ]);
