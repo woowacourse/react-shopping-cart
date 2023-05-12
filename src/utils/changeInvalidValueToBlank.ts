e1d950342c6cc21d753e62b25865e4c5d494d286
Dain Lee <feb.dain@gmail.com>
2023-05-12 11:26:46 +0900

    refactor: 불필요한 타입 지정 제거

--- src/utils/changeInvalidValueToBlank.ts
+++ src/utils/changeInvalidValueToBlank.ts
@@ -1,5 +1,3 @@
-type Condition = RegExp;
-
-export const changeInvalidValueToBlank = (value: string, regex: Condition) => {
+export const changeInvalidValueToBlank = (value: string, regex: RegExp) => {
   return value.replace(regex, '').slice(0, 2);
 };
