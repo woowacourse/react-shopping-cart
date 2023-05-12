7fd4541bcaec5cec62ea352a04b57d7f12dcb23a
ashleysyheo <ashleysyheo@gmail.com>
2023-05-13 01:59:06 +0900

    refactor: Header 컴포넌트 스토리 파일 위치 변경

--- src/stories/common/Header.stories.ts
+++ src/stories/common/Header.stories.ts
@@ -1,9 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import Header from '../components/common/Header/Header';
+import Header from '../../components/common/Header/Header';
 
 const meta = {
-  title: 'ShoppingCart/common/Header',
+  title: 'ShoppingCart/Common/Header',
   component: Header,
 } satisfies Meta<typeof Header>;
 
