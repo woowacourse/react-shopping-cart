bc6a1c2f8aec5563d28fb183aa5d465eb30b3845
ashleysyheo <ashleysyheo@gmail.com>
2023-05-12 09:31:39 +0900

    refactor: Button 컴포넌트에서 불필요한 props 타입 정의 제거

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, forwardRef } from 'react';
+import { forwardRef } from 'react';
 import type { ComponentPropsWithRef, ForwardedRef } from 'react';
 
 import * as S from './Button.styles';
@@ -6,7 +6,6 @@ import * as S from './Button.styles';
 export interface ButtonProps extends ComponentPropsWithRef<'button'> {
   variant?: 'default' | 'primary' | 'secondary' | 'danger' | 'textButton';
   size?: 'small' | 'medium' | 'large';
-  children: ReactNode | string;
 }
 
 const Button = (
