1093f7fead240e6f347c8554e4a0d5f3d4cb2ed7
yogjin <youngjin988@gmail.com>
2023-05-13 03:09:16 +0900

    feat: msw 초기 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = require('./mocks/browser');
+  worker.start();
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
