2f6895efe9006b6473410763cef79129277c4845
woo-jk <201702042@o.cnu.ac.kr>
2023-05-12 15:26:46 +0900

    refactor: useCounter 최솟값, 최댓값 인자로 받도록 수정

--- src/components/Counter/useCounter.ts
+++ src/components/Counter/useCounter.ts
@@ -1,12 +1,17 @@
-const useCounter = (count: number, onChangeCount: (count: number) => void) => {
+const useCounter = (
+  count: number,
+  onChangeCount: (count: number) => void,
+  min = 0,
+  max = 99,
+) => {
   const increaseCount = () => {
-    if (count >= 99) return;
+    if (count >= max) return;
 
     onChangeCount(count + 1);
   };
 
   const decreaseCount = () => {
-    if (count <= 0) return;
+    if (count <= min) return;
 
     onChangeCount(count - 1);
   };
