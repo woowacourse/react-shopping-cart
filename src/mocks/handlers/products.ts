17bbd89a527c3233a63f8beebeca5eace27d0706
Suyoung <jsy8492@gmail.com>
2023-05-13 20:31:12 +0900

    feat: 상품 조회 handler 작성

--- src/mocks/handlers/products.ts
+++ src/mocks/handlers/products.ts
@@ -1,11 +1,22 @@
 import { rest } from 'msw';
 import products from '../fixtures/products.json';
-
-const PRODUCTS_BASE_URL = '/api/products';
+import { PRODUCTS_BASE_URL } from '../../constants';
 
 export const productsHandlers = [
   // 상품 목록 조회 API
   rest.get(PRODUCTS_BASE_URL, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(products));
+  }),
+
+  // 상품 조회 API
+  rest.get(`${PRODUCTS_BASE_URL}/:id`, (req, res, ctx) => {
+    const { id } = req.params;
+    const product = products.find((product) => String(product.id) === id);
+
+    if (product === undefined) {
+      return res(ctx.status(404));
+    }
+
+    return res(ctx.status(200), ctx.json(product));
   }),
 ];
