36a4b2755837bd30c3d95c9e574225d943d856fb
ashleysyheo <ashleysyheo@gmail.com>
2023-05-14 00:26:45 +0900

    style: Header 장바구니 Button 스타일 수정

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
     <S.HeaderContainer>
       <S.HeaderContentContainer>
         <S.Logo src={Logo} alt="logo" onClick={() => navigate('/')} />
-        <S.CartButton type="button" aria-label="cart">
+        <S.CartButton type="button" aria-label="cart" variant="textButton">
           {cartListItemCount > 0 && <S.CartItemCount>{cartListItemCount}</S.CartItemCount>}
           <S.CartIcon src={CartIcon} alt="cart icon" />
           <S.HeaderButtonLabel>장바구니</S.HeaderButtonLabel>
