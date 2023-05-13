ab5b62be61eacc518fa05a876b1a04b5aeea984e
Leejin-Yang <ejin1996@gmail.com>
2023-05-13 21:30:06 +0900

    test: 카트 버튼 클릭 시 헤더의 장바구니 품복 카운트 테스트 작성

--- src/stories/pages/ProductsListPage.stories.tsx
+++ src/stories/pages/ProductsListPage.stories.tsx
@@ -1,7 +1,9 @@
 import { Meta, StoryObj } from '@storybook/react';
+import { userEvent, waitFor, within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+import { BrowserRouter } from 'react-router-dom';
 
 import ProductsListPage from '../../pages/ProductsListPage';
-import { BrowserRouter } from 'react-router-dom';
 
 const meta = {
   title: 'Pages/ProductsListPage',
@@ -15,6 +17,11 @@ const meta = {
       </BrowserRouter>
     ),
   ],
+  parameters: {
+    viewport: {
+      defaultViewport: 'desktop',
+    },
+  },
 } satisfies Meta<typeof ProductsListPage>;
 
 export default meta;
@@ -22,4 +29,27 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {},
+};
+
+export const Interaction: Story = {
+  args: {},
+
+  play: async ({ canvasElement, step }) => {
+    window.localStorage.clear();
+
+    const canvas = within(canvasElement);
+
+    const productList = await canvas.findByRole('list');
+    const productListCanvas = within(productList);
+    const cartButtons = await productListCanvas.findAllByRole('button');
+
+    await step('Click Cart Button', async () => {
+      await userEvent.click(cartButtons[0]);
+      await userEvent.click(cartButtons[1]);
+    });
+
+    await waitFor(() => {
+      expect(canvas.getByText(2)).toBeInTheDocument();
+    });
+  },
 };
