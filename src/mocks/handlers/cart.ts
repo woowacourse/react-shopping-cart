3d434b74ac6ff3cea1cb05cb52898b80e1b96638
Suyoung <jsy8492@gmail.com>
2023-05-13 19:53:46 +0900

    refactor: handlers api 요청 url을 별도의 상수로 분리

--- src/mocks/handlers/cart.ts
+++ src/mocks/handlers/cart.ts
@@ -1,10 +1,11 @@
 import { rest } from 'msw';
 
 const cart = localStorage.getItem('cart') ?? [];
+const CART_BASE_URL = '/api/cart-items';
 
 export const cartHandlers = [
   // 장바구니 목록 조회 API
-  rest.get('/api/cart-items', (req, res, ctx) => {
+  rest.get(CART_BASE_URL, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(cart));
   }),
 ];
--- src/mocks/handlers/products.ts
+++ src/mocks/handlers/products.ts
@@ -1,9 +1,11 @@
 import { rest } from 'msw';
 import products from '../fixtures/products.json';
 
+const PRODUCTS_BASE_URL = '/api/products';
+
 export const productsHandlers = [
   // 상품 목록 조회 API
-  rest.get('/api/products', (req, res, ctx) => {
+  rest.get(PRODUCTS_BASE_URL, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(products));
   }),
 ];
