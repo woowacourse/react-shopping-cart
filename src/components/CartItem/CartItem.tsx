e7c95879ee13f22952ff42b496a38a529cf50d67
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-05-13 23:48:30 +0900

    feat: CardItem 컴포넌트 UI 구현

---
+++ src/components/CartItem/CartItem.tsx
@@ -0,0 +1,82 @@
+import { styled } from 'styled-components';
+import ProductImg from '../ProductCard/ProductImg/ProductImg';
+import { ReactComponent as TrashCan } from '../../assets/icon/trash-can.svg';
+import Counter from '../common/Counter/Counter';
+
+const CartItem = () => {
+  return (
+    <Wrapper>
+      <CheckBoxWrapper>
+        <Checkbox type='checkbox'></Checkbox>
+      </CheckBoxWrapper>
+
+      <ProductImg
+        imageUrl='https://s3-alpha-sig.figma.com/img/c91b/8c58/919fc1af3ddd4c1bba09101cf51013cb?Expires=1684713600&Signature=gJXoDh3Kff5YuZh~dwngjm~n7o37JjnnftCnctPE5Y-QYKTWJAa6~laE1rxwy1s1FhgaJmU1IKg9OAHq0iwUob-hH9TEy3RUYgz-HyXqetKOgEw8Wp4937mBA~2cWyMJjp56bjQgLLbBqM8up2ddQQJLPsyfVdDHr-~jZFL-JEDT72NnlNfyO2qG~W6clycdMfrtkB~YxPPiwHSai1bTEKx6b7vaR79k7OtXO3F3kq9TzQH9vNXM6X9GQtRsw9zjc35JDS2CDoFsAh5u~xThB1udtnHfmTQMdm-lqB0NpwewqYB0GF8qov4mbSCPh20HtPIpLkM5-V1QPEXvyiUbiA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'
+        size={{ width: '144px', height: '144px' }}
+      />
+
+      <DetailWrapper>
+        <ProductName>[든든] 야채바삭 김말이 700g</ProductName>
+        <DeleteButton>
+          <TrashCan />
+        </DeleteButton>
+        <CounterWrapper>
+          <Counter plusOne={() => {}} minusOne={() => {}} />
+        </CounterWrapper>
+        <Price>7200원</Price>
+      </DetailWrapper>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+
+  width: 780px;
+  height: 160px;
+`;
+
+const Checkbox = styled.input`
+  width: 28px;
+  height: 28px;
+`;
+
+const CheckBoxWrapper = styled.div`
+  height: 100%;
+
+  padding: 8px 12px;
+`;
+
+const ProductName = styled.span``;
+
+const DetailWrapper = styled.div`
+  position: relative;
+
+  flex: 1;
+  height: 100%;
+
+  padding: 12px;
+`;
+
+const DeleteButton = styled.button`
+  position: absolute;
+
+  top: 12px;
+  right: 12px;
+`;
+
+const CounterWrapper = styled.div`
+  position: absolute;
+
+  top: 64px;
+  right: 12px;
+`;
+
+const Price = styled.span`
+  position: absolute;
+
+  bottom: 12px;
+  right: 12px;
+`;
+export default CartItem;
