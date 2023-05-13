e74c8e390eb5f005af22b4dd9566a24e1d3712d9
Suyoung <jsy8492@gmail.com>
2023-05-13 18:21:36 +0900

    chore: MSW 설치 및 필요한 모듈 작성

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,13 @@ import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createHashRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
+import { worker } from './mocks/browser';
 import App from './App';
 import GlobalStyle from './GlobalStyle';
+
+if (process.env.NODE_ENV === 'development') {
+  worker.start();
+}
 
 const router = createHashRouter([
   {
