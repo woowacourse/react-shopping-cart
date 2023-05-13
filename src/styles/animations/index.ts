239e03d9832063a87ad220e8c966b624c92d06be
ashleysyheo <ashleysyheo@gmail.com>
2023-05-13 20:34:47 +0900

    style: keyframe 애니메이션 추가

--- src/styles/animations/index.ts
+++ src/styles/animations/index.ts
@@ -9,4 +9,22 @@ const fadeIn = keyframes`
   }
 `;
 
-export { fadeIn };
+const fadeOut = keyframes`
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+`;
+
+const moveUp = keyframes`
+  from {
+    transform: translateX(-50%) translateY(50%);
+  }
+  to {
+    transform: translateX(-50%) translateY(0%);
+  }    
+`;
+
+export { fadeIn, fadeOut, moveUp };
