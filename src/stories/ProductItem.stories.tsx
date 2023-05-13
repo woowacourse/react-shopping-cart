2459f18acf71c91201c4551e9c423e2cc4270c9d
Leejin-Yang <ejin1996@gmail.com>
2023-05-13 20:15:46 +0900

    test: ProductItem 컴포넌트에서 카트 버튼 클릭 테스트 작성

--- src/stories/ProductItem.stories.tsx
+++ src/stories/ProductItem.stories.tsx
@@ -1,7 +1,9 @@
 import { Meta, StoryObj } from '@storybook/react';
+import { RecoilRoot } from 'recoil';
+import { userEvent, waitFor, within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
 
 import ProductItem from '../components/Product/ProductItem';
-import { RecoilRoot } from 'recoil';
 
 const meta = {
   title: 'Product/ProductItem',
@@ -30,5 +32,27 @@ const mockProduct = {
 export const Default: Story = {
   args: {
     product: mockProduct,
+  },
+};
+
+export const ClickCartButton: Story = {
+  args: {
+    product: mockProduct,
+  },
+
+  play: async ({ canvasElement, step }) => {
+    const canvas = within(canvasElement);
+
+    window.localStorage.clear();
+
+    const firstCartButton = await canvas.findByRole('button');
+
+    await step('Click Cart Button', async () => {
+      await userEvent.click(firstCartButton);
+    });
+
+    await waitFor(() => {
+      expect(canvas.getByDisplayValue(1)).toBeInTheDocument();
+    });
   },
 };
