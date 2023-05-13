19a29c28c5c1784e1e1759789881757cfb16e537
Suyoung <jsy8492@gmail.com>
2023-05-13 19:32:44 +0900

    feat: 장바구니 목록 조회 handler 작성

--- src/mocks/handlers/cart.ts
+++ src/mocks/handlers/cart.ts
@@ -1,3 +1,10 @@
 import { rest } from 'msw';
 
-export const cartHandlers = [];
+const cart = localStorage.getItem('cart') ?? [];
+
+export const cartHandlers = [
+  // 장바구니 목록 조회 API
+  rest.get('/api/cart-items', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(cart));
+  }),
+];
