65498fd66d663ea94589822a84862363ddacb706
Q Kim <qoli0703@gmail.com>
2023-05-13 17:03:09 +0900

    test: 실제 뷰에서 사용하지 않는 스토리 제거

--- src/components/ProductList/ProductList.stories.tsx
+++ src/components/ProductList/ProductList.stories.tsx
@@ -1,8 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { RecoilRoot, useRecoilValue } from 'recoil';
+import { RecoilRoot } from 'recoil';
 
 import ProductList from './ProductList';
-import { cartLengthSelector } from '../../recoil/myCartState';
 
 /**
  * 상품 목록 컴포넌트
@@ -26,27 +25,4 @@ export default meta;
 
 type Story = StoryObj<typeof ProductList>;
 
-/**
- * 기본형
- */
-
 export const Default: Story = {};
-
-/**
- * 왼쪽 상단에 현재 장바구니에 담긴 물건 종류 표시
- */
-
-const Wrapper = () => {
-  const cartLength = useRecoilValue(cartLengthSelector);
-
-  return (
-    <>
-      <h1>{cartLength}</h1>
-      <ProductList />
-    </>
-  );
-};
-
-export const ShowCartLength: Story = {
-  render: Wrapper,
-};
