33b4a6bb816875bc8646f0452163a8d8e4023dbe
regularPark <srk99279@naver.com>
2023-05-13 21:34:03 +0900

    refactor:  컴포넌트의 `autoComplete`의 기본값 설정 및 재사용성을 위한 props화

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -2,15 +2,18 @@ import { ChangeEventHandler, InputHTMLAttributes, forwardRef } from 'react';
 import styled, { CSSProp } from 'styled-components';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  autoComplete?: string;
   styled: CSSProp;
   onChange?: ChangeEventHandler<HTMLInputElement>;
 }
 
 type Ref = HTMLInputElement;
 
-const Input = forwardRef<Ref, InputProps>(({ ...props }, ref) => {
-  return <S.Input ref={ref} {...props} autoComplete="on" />;
-});
+const Input = forwardRef<Ref, InputProps>(
+  ({ autoComplete = 'on', ...props }, ref) => {
+    return <S.Input ref={ref} {...props} autoComplete={autoComplete} />;
+  }
+);
 
 const S = {
   Input: styled.input<{ styled: CSSProp }>`
