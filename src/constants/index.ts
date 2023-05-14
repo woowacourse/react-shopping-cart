51d3612e3b68fa9281fc5900b1474a793e740582
nangkyeonglim <nangkyeonglim@gmail.com>
2023-05-14 18:39:49 +0900

    refactor: page path 상수화

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,7 @@
 export const LOCAL_STORAGE_KEY = {
   CART_ITEMS: 'cartItems',
 };
+
+export const PAGE_PATH = {
+  HOME: '/',
+};
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,10 +1,11 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import ListPage from '../pages/ListPage/ListPage';
+import { PAGE_PATH } from '../constants';
 
 const Router = () => {
   const browserRouter = createBrowserRouter([
     {
-      path: '/',
+      path: PAGE_PATH.HOME,
       element: <ListPage />,
     },
   ]);
