d3371163222970ed4b7190e63aa3e2e887d4193b
sh981013s <sh981013s@gmail.com>
2023-05-14 16:42:50 +0900

    refactor: localStoragekey 상수화

--- src/utils/localStorageCartList.ts
+++ src/utils/localStorageCartList.ts
@@ -1,7 +1,8 @@
 import { CartList } from '../types/CartList.ts';
+import localStorageKeys from '../constants/localStorageKeys.ts';
 
 export const getCartListFromLocalStorage = () => {
-  const cartList = localStorage.getItem('cartList');
+  const cartList = localStorage.getItem(localStorageKeys.cartList);
   if (cartList) {
     return JSON.parse(cartList);
   }
