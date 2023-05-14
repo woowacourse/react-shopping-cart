4bb45c06087cced71175a77ba0a7bad0a399d838
n0eyes <doll4586633@naver.com>
2023-05-14 23:11:23 +0900

    refactor: ErrorPage routes를 상수로 변경

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,5 +1,6 @@
 import * as S from './ErrorPage.styles';
 import Flex from '../../components/common/Flex';
+import { PAGE_ROUTES } from '../../constants/routes';
 
 const ErrorPage: React.FC<{ status?: number }> = ({ status }) => {
   return (
@@ -7,7 +8,7 @@ const ErrorPage: React.FC<{ status?: number }> = ({ status }) => {
       <S.ErrorMessage>
         <h1>{status ?? '예상치 못한'} 에러가 발생했습니다.</h1>
       </S.ErrorMessage>
-      <S.LinkToHome to="/">Home으로 돌아가기</S.LinkToHome>
+      <S.LinkToHome to={PAGE_ROUTES.HOME}>Home으로 돌아가기</S.LinkToHome>
     </Flex>
   );
 };
