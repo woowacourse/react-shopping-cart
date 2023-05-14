3db292bcdef859f753f0af2f1bbccbb040a39df2
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-05-14 22:09:48 +0900

    refactor: ProductStepper Stepper로 컴포넌트 주입하게 수정

--- src/components/ProductStepper/ProductStepper.styled.ts
+++ src/components/ProductStepper/ProductStepper.styled.ts
@@ -5,3 +5,17 @@ export const StyledProductStepper = styled.div`
   justify-content: center;
   align-items: center;
 `;
+
+export const StyledProductStepperFlexBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const StyledProductStepperButtonFlexBox = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 28px;
+  height: 28px;
+`;
--- src/components/ProductStepper/ProductStepper.tsx
+++ src/components/ProductStepper/ProductStepper.tsx
@@ -1,8 +1,16 @@
 import useProductCount from '@hooks/useProductCount';
 
-import { StyledProductStepper } from '@components/ProductStepper/ProductStepper.styled';
+import { PRODUCT_COUNT, STEP_UNIT } from '@constants/product';
+
+import {
+  StyledProductStepper,
+  StyledProductStepperButtonFlexBox,
+  StyledProductStepperFlexBox,
+} from '@components/ProductStepper/ProductStepper.styled';
 import StepperEntryButton from '@components/StepperEntryButton/StepperEntryButton';
 import Stepper from '@commons/Stepper/Stepper';
+import Input from '@components/commons/Input/Input';
+import Button from '@components/commons/Button/Button';
 
 interface ProductStepperProps {
   productId: number;
@@ -18,10 +26,49 @@ const ProductStepper = (props: ProductStepperProps) => {
         <StepperEntryButton onClick={() => setProductCount(prev => prev + 1)} />
       ) : (
         <Stepper
-          productCount={productCount}
-          step={1}
-          setProductCount={setProductCount}
-        />
+          step={productCount}
+          setStep={setProductCount}
+          stepUnit={STEP_UNIT}
+          minStep={PRODUCT_COUNT.MIN}
+          maxStep={PRODUCT_COUNT.MAX}
+        >
+          {({ step, handleNumberInput, handleIncrement, handleDecrement }) => (
+            <StyledProductStepperFlexBox>
+              <Input
+                width="48px"
+                height="28px"
+                type="number"
+                value={step}
+                inputMode="numeric"
+                aria-label="상품 개수 입력"
+                alt="상품 개수 입력창"
+                onChange={handleNumberInput}
+              />
+              <StyledProductStepperButtonFlexBox>
+                <Button
+                  ariaLabel="상품 1개 추가"
+                  backgroundColor="#white"
+                  text="▲"
+                  fontSize="xx-small"
+                  color="#333333"
+                  onClick={handleIncrement}
+                  type="button"
+                  name="상품 추가 버튼"
+                />
+                <Button
+                  ariaLabel="상품 1개 삭제"
+                  backgroundColor="#white"
+                  text="▼"
+                  fontSize="xx-small"
+                  color="#333333"
+                  onClick={handleDecrement}
+                  type="button"
+                  name="상품 삭제 버튼"
+                />
+              </StyledProductStepperButtonFlexBox>
+            </StyledProductStepperFlexBox>
+          )}
+        </Stepper>
       )}
     </StyledProductStepper>
   );
