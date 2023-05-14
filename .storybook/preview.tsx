c3c99a111b54ed339f9dd7e3a917aabcb3ff65e4
nangkyeonglim <nangkyeonglim@gmail.com>
2023-05-15 00:54:22 +0900

    test: storybook에 router, globalStyle 적용

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,4 +1,20 @@
+import React from 'react';
 import type { Preview } from '@storybook/react';
+import ResetStyle from '../src/styles/ResetStyle';
+import GlobalStyle from '../src/styles/GlobalStyle';
+import { BrowserRouter } from 'react-router-dom';
+
+export const decorators = [
+  (Story) => {
+    return (
+      <BrowserRouter>
+        <ResetStyle />
+        <GlobalStyle />
+        <Story />
+      </BrowserRouter>
+    );
+  },
+];
 
 const preview: Preview = {
   parameters: {
