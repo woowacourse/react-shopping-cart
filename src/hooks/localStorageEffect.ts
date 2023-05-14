3562b3f412d2b0a74c27da4a4dda27765f6276ab
chex <chex1004@gmail.com>
2023-05-14 21:25:45 +0900

    refactor: '!=' -> '!=='로 수정

--- src/hooks/localStorageEffect.ts
+++ src/hooks/localStorageEffect.ts
@@ -5,7 +5,7 @@ export const localStorageEffect: <T>(key: string) => AtomEffect<T> =
   ({ setSelf, onSet }) => {
     const savedValue = localStorage.getItem(key);
 
-    if (savedValue != null) {
+    if (savedValue !== null) {
       setSelf(JSON.parse(savedValue));
     }
 
