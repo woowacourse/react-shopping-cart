4e01fed64b29db370e9dab49199587314d971c2c
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-05-15 00:08:36 +0900

    fix: modal useEffect 추가

--- src/components/commons/Modal/Modal.tsx
+++ src/components/commons/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 
 interface ModalProps {
@@ -11,6 +11,10 @@ const Modal = (props: ModalProps) => {
   const [isOpenModal, setIsOpenModal] = useState(initIsOpenModal);
 
   const handleModalClose = () => setIsOpenModal(false);
+
+  useEffect(() => {
+    setIsOpenModal(initIsOpenModal);
+  }, [initIsOpenModal]);
 
   if (!isOpenModal) {
     return null;
