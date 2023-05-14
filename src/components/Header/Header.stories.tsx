d4945a987b05325727cda53a2720e13047c840b8
nlom0218 <nlom0218@naver.com>
2023-05-14 17:59:45 +0900

    refactor: Header 스토리에 BrowserRouter 추가

--- src/components/Header/Header.stories.tsx
+++ src/components/Header/Header.stories.tsx
@@ -1,16 +1,21 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
 import Header from '.';
 
 /**
  * `Header`은 페이지의 헤더 컴포넌트입니다.
- *
- * 가로의 최대 너비가 고정되어 있으므로 창을 넓혀 보시는 것을 권장합니다.
  */
 const meta: Meta<typeof Header> = {
   title: 'Header',
-  decorators: [(storyFn) => <RecoilRoot>{storyFn()}</RecoilRoot>],
+  decorators: [
+    (storyFn) => (
+      <RecoilRoot>
+        <BrowserRouter>{storyFn()}</BrowserRouter>
+      </RecoilRoot>
+    ),
+  ],
   component: Header,
 };
 
@@ -18,4 +23,6 @@ export default meta;
 
 type Story = StoryObj<typeof Header>;
 
-export const DefaultHeader: Story = {};
+export const DefaultHeader: Story = {
+  args: {},
+};
