71914b4f04c0501b894976506e11b316b77f2f9f
semnil5202 <semnil5202@gmail.com>
2023-05-15 02:32:29 +0900

    refactor(Header): style 로직 폴더 분리

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -4,14 +4,14 @@ import { Logo } from '../components/Logo';
 import { TotalCartCount } from '../components/TotalCartCount';
 import { addedCartState } from '../atoms/AddedCartState';
 import { useRecoilValue } from 'recoil';
-import { styled } from 'styled-components';
+import * as Styled from './styles/Header.styles';
 
 export const Header = () => {
   const AddedCardState = useRecoilValue(addedCartState);
 
   return (
-    <HeaderWrapper>
-      <StyledHeader>
+    <Styled.Wrapper>
+      <Styled.HeaderWrapper>
         <Logo $color="#ffffff" />
         <CartButton
           onClick={() => {
@@ -21,34 +21,13 @@ export const Header = () => {
           <AddToCartTitle size="24px" color="#ffffff">
             장바구니
           </AddToCartTitle>
-          <TotalCartCountWrapper>
+          <Styled.TotalCartCountWrapper>
             {AddedCardState.length !== 0 && (
               <TotalCartCount count={AddedCardState.length} />
             )}
-          </TotalCartCountWrapper>
+          </Styled.TotalCartCountWrapper>
         </CartButton>
-      </StyledHeader>
-    </HeaderWrapper>
+      </Styled.HeaderWrapper>
+    </Styled.Wrapper>
   );
 };
-
-const HeaderWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 80px;
-  background-color: #333333;
-`;
-
-const StyledHeader = styled.div`
-  width: 1200px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const TotalCartCountWrapper = styled.div`
-  width: 32px;
-  height: 32px;
-`;
