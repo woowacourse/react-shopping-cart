e129d08a92e5194d50112e4f7fb6581a020e8345
yogjin <youngjin988@gmail.com>
2023-05-14 15:21:22 +0900

    feat: 에러 페이지에서 홈으로 이동하는 버튼 추가

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,16 +1,36 @@
-import { useRouteError } from 'react-router-dom';
+import ROUTE_PATH from 'Router';
+import { useRouteError, useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
 export default function ErrorPage() {
-  const error = useRouteError();
-  console.error(error);
+  const response = useRouteError();
+  const navigate = useNavigate();
+
+  const goBackPage = () => {
+    navigate(ROUTE_PATH.root);
+  };
 
   return (
-    <div id="error-page">
-      <h1>ERROR</h1>
-      <p>예상치 못한 오류가 발생했어요🥲</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+    <Container>
+      <ErrorContainer>
+        <h1>ERROR: {response.statusText}</h1>
+        <p>예상치 못한 오류가 발생했어요🥲</p>
+        <p>
+          <i>{response.error.message}</i>
+        </p>
+        <GoBackButton onClick={goBackPage}>홈으로 이동하기</GoBackButton>
+      </ErrorContainer>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const ErrorContainer = styled.div``;
+
+const GoBackButton = styled.button``;
