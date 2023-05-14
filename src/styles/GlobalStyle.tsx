d9f0ac3ff53db3269074adb717ca2c9e089267d8
nangkyeonglim <nangkyeonglim@gmail.com>
2023-05-14 16:37:46 +0900

    refactor: font import하는 방식 변경

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -1,10 +1,8 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
-  @import url('//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSans-kr.css');
-
   body, button, input {
-    font-family: 'Spoqa Han Sans', 'Spoqa Han Sans JP', 'Sans-serif';
+    font-family: 'Spoqa Han Sans Neo', 'Spoqa Han Sans JP', 'Sans-serif';
   }
   
   :root {
