90a31d7690328e736946863ca0f3489150236e8c
nangkyeonglim <nangkyeonglim@gmail.com>
2023-05-15 00:55:57 +0900

    feat: StepperInput 컴포넌트 구현

--- src/components/@common/StepperInput/StepperInput.tsx
+++ src/components/@common/StepperInput/StepperInput.tsx
@@ -1,14 +1,69 @@
-import * as Styled from './StepperInput.styles.tsx';
+import UpTriangle from '../../../assets/triangle.svg';
+import DownTriangle from '../../../assets/triangle-down.svg';
+import { ChangeEvent, useEffect, useState } from 'react';
+import * as Styled from './StepperInput.styles';
 
 type StepperInputProps = {
-  width?: number;
-  height?: number;
-  value: number;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  min?: number;
+  max?: number;
+  step?: number;
+  initialValue?: number;
+  getValue: (value: number) => void;
 };
 
-const StepperInput = ({ width = 65, height = 28, value, onChange }: StepperInputProps) => {
-  return <Styled.StepperInputContainer type='number' step='1' min='0' max='99' width={width} height={height} value={value} onChange={onChange} />;
+export const regEx = {
+  Number: /^[0-9]+$/,
+};
+
+export const isNumber = (target: string) => {
+  return regEx.Number.test(target);
+};
+
+const StepperInput = ({ min = 0, max = 99, step = 1, initialValue = 0, getValue }: StepperInputProps) => {
+  const [inputValue, setInputValue] = useState(String(initialValue));
+
+  useEffect(() => {
+    if (inputValue !== '') getValue(Number(inputValue));
+  }, [getValue, inputValue]);
+
+  const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value === '') setInputValue('');
+    if (!isNumber(e.target.value)) return;
+
+    const currentValue = Number(e.target.value);
+
+    if (currentValue >= min && currentValue <= max) setInputValue(String(currentValue));
+  };
+
+  const handleBlurInput = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value === '') setInputValue(String(min));
+  };
+
+  const handleIncreaseStepper = () => {
+    const NextValue = Number(inputValue) + step;
+
+    if (NextValue <= max) setInputValue(String(NextValue));
+  };
+
+  const handleDecreaseStepper = () => {
+    const NextValue = Number(inputValue) - step;
+
+    if (NextValue >= min) setInputValue(String(NextValue));
+  };
+
+  return (
+    <Styled.StepperInputWrapper>
+      <Styled.Input type='text' value={inputValue} onChange={handleChangeInput} onBlur={handleBlurInput} />
+      <Styled.StepperWrapper>
+        <Styled.Stepper type='button' onClick={handleIncreaseStepper}>
+          <img src={UpTriangle} alt='수량올리기' />
+        </Styled.Stepper>
+        <Styled.Stepper type='button' onClick={handleDecreaseStepper}>
+          <img src={DownTriangle} alt='수량내리기' />
+        </Styled.Stepper>
+      </Styled.StepperWrapper>
+    </Styled.StepperInputWrapper>
+  );
 };
 
 export default StepperInput;
