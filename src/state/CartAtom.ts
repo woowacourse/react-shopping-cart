abc4ebdfc0a9f6e347d40a88ec8e1ab5583ad126
Dohyeon Lee <dlehgus1110@naver.com>
2023-05-14 21:36:55 +0900

    fix(CartAtom): 장바구니 자료구조 변경으로 인한 전역상태 동기화 effect의 오류 수정
    1. effect 추상화
    effect명 변경, storage/직렬화/역직렬화 함수 추상화
    
    2. Map 자료구조 직렬화/역직렬화 함수 추가

--- src/state/CartAtom.ts
+++ src/state/CartAtom.ts
@@ -1,7 +1,6 @@
 import { CART_KEY } from 'constants/storeKey';
 import { AtomEffect, atom } from 'recoil';
 import { CartProducts } from 'types/product';
-import store from 'utils/localStorage';
 
 const zeroQuantityFilterEffect: AtomEffect<CartProducts> = ({ onSet, setSelf }) => {
   onSet((cartProducts) => {
@@ -11,21 +10,55 @@ const zeroQuantityFilterEffect: AtomEffect<CartProducts> = ({ onSet, setSelf }) 
   });
 };
 
-const localStorageEffect =
-  <T>(key: string, initialValue: T): AtomEffect<T> =>
+type Persister = {
+  getItem: (key: string) => string | null;
+  setItem: (key: string, value: string) => void;
+};
+
+type PersistAtomEffectParams<T> = {
+  key: string;
+  initialValue: T;
+  options: {
+    persister?: Persister;
+    serializer?: (value: T) => string;
+    deserializer?: (serializedValue: string) => T;
+  };
+};
+
+const persistAtomEffect =
+  <T>({ key, initialValue, options }: PersistAtomEffectParams<T>): AtomEffect<T> =>
   ({ onSet, setSelf, trigger }) => {
+    const { persister = localStorage, serializer = JSON.stringify, deserializer = JSON.parse } = options;
+
     if (trigger === 'get') {
-      const storageValue = store.getStorage<T>(key) ?? initialValue;
-      setSelf(storageValue);
+      const storedValue = persister.getItem(key);
+      const state = storedValue ? deserializer(storedValue) : initialValue;
+
+      setSelf(state);
     }
 
     onSet((newValue) => {
-      store.setStorage(key, newValue);
+      persister.setItem(key, serializer(newValue));
     });
   };
+
+const mapToString = (value: CartProducts) => {
+  return JSON.stringify(Array.from(value));
+};
+
+const stringToMap = (value: string): CartProducts => {
+  return new Map(JSON.parse(value));
+};
 
 export const cartProductsState = atom<CartProducts>({
   key: 'cartState',
   default: new Map(),
-  effects: [zeroQuantityFilterEffect, localStorageEffect<CartProducts>(CART_KEY, new Map())],
+  effects: [
+    zeroQuantityFilterEffect,
+    persistAtomEffect<CartProducts>({
+      key: CART_KEY,
+      initialValue: new Map(),
+      options: { serializer: mapToString, deserializer: stringToMap },
+    }),
+  ],
 });
