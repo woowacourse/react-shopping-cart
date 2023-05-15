e3cbd84cb51b272ce69fff0bd4562a27fe03a6a3
Dahye <06robin11@gmail.com>
2023-05-16 00:05:35 +0900

    refactor:라우트 경로 상수화 사용

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,4 +1,5 @@
 import Logo from '../../../assets/png/logo.png';
+import { PAGE_ROUTES } from '../../../constants/routes';
 import CartBadge from '../CartBadge/CartBadge';
 import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
 import Flex from '../Flex';
@@ -8,12 +9,12 @@ const Header = () => {
   return (
     <S.Root>
       <Flex width="80%" justify="space-between" align="center">
-        <S.LinkToHome to="/">
+        <S.LinkToHome to={PAGE_ROUTES.HOME}>
           <S.Logo src={Logo} alt="shopping cart logo" />
           <S.Title>SHOP</S.Title>
         </S.LinkToHome>
         <Flex>
-          <S.LinkToCart to="/">장바구니</S.LinkToCart>
+          <S.LinkToCart to={PAGE_ROUTES.CART}>장바구니</S.LinkToCart>
           <ErrorBoundary>
             <CartBadge />
           </ErrorBoundary>
--- src/pages/ErrorPage/Page404.tsx
+++ src/pages/ErrorPage/Page404.tsx
@@ -1,5 +1,6 @@
 import * as S from './Page404.styles';
 import Flex from '../../components/common/Flex';
+import { PAGE_ROUTES } from '../../constants/routes';
 
 const Page404 = () => {
   return (
@@ -7,7 +8,7 @@ const Page404 = () => {
       <S.ErrorMessage>
         요청하신 페이지는 존재하지 않는 페이지입니다.
       </S.ErrorMessage>
-      <S.LinkToHome to="/">Home으로 돌아가기</S.LinkToHome>
+      <S.LinkToHome to={PAGE_ROUTES.HOME}>Home으로 돌아가기</S.LinkToHome>
     </Flex>
   );
 };
