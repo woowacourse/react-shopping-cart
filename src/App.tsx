76a4f8c8b19ead53b35f9245800c09b1499e698d
regularPark <srk99279@naver.com>
2023-05-15 23:23:41 +0900

    feat: react-router-dom 설치 및 사용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import MainPage from './pages/MainPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
-  return <MainPage />;
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
