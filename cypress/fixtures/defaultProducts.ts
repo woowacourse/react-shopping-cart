d05506371d9b30d7ea81b4e27fa469c18f7d6c93
turtle601 <turtle601@naver.com>
2023-05-15 22:47:10 +0900

    refactor(cypress): mockdata 타입시스템 적용

---
+++ cypress/fixtures/defaultProducts.ts
@@ -0,0 +1,14 @@
+import { Product } from './../../src/types/product';
+
+const defaultProducts: Product = {
+  id: 1,
+  name: '정사각(420ml)',
+  price: 43400,
+  imageUrl:
+    'https://item.kakaocdn.net/do/d0abc6fe74e616536cf07626699bbc707154249a3890514a43687a85e6b6cc82',
+};
+
+export const createMockProduct = (overwrites: Partial<Product> = {}) => ({
+  ...defaultProducts,
+  ...overwrites,
+});
