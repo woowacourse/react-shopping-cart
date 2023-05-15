c2bcf3a5c92b02c8409ad26e2d1270ef7b04ddaa
inyeong-kang <tomatorkd@naver.com>
2023-05-15 15:03:30 +0900

    refactor: 숫자인지 검증 로직 추가

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -1,3 +1,5 @@
 export const isNumeric = (value: string) => {
-  return value !== '' && value.split('').every((char) => '0123456789'.includes(char));
+  if (!value || value.trim() === '') return false;
+
+  return /^\d+$/.test(value);
 };
