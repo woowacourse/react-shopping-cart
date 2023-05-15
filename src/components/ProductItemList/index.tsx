c09d382d8e2ca2e8d91062cef971ad194d8196fb
d0dam <smallkdb@gmail.com>
2023-05-16 01:21:33 +0900

    refactor: 제품 리스트 에러 발생시 토스트가 나오도록 수정

--- src/components/ProductItemList/index.tsx
+++ src/components/ProductItemList/index.tsx
@@ -3,9 +3,19 @@ import ProductItem from '../ProductItem';
 import styles from './index.module.scss';
 import { Product } from '../../types';
 import useGetQuery from '../../hooks/useGetQuery';
+import { $ToastMessageList } from '../../recoil/atom';
+import { useSetRecoilState } from 'recoil';
+import { useEffect } from 'react';
 
 function ProductItemList() {
-  const { data: productsData } = useGetQuery<Product[]>('./products');
+  const { data: productsData, error } = useGetQuery<Product[]>('./products');
+  const setMessageList = useSetRecoilState($ToastMessageList);
+
+  useEffect(() => {
+    if (!error) {
+      setMessageList(prev => [...prev, '제품 리스트를 불러오는 과정에서 에러가 발생했습니다.']);
+    }
+  }, [error]);
 
   return (
     <section className={styles.container}>
