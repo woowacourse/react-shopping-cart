0f9d5c31b48b53d292165afc132d9feca04a1ef4
Leejin-Yang <ejin1996@gmail.com>
2023-05-15 22:09:42 +0900

    test: CartProductList 스토리 작성

---
+++ src/stories/CartProductList.stories.tsx
@@ -0,0 +1,31 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import CartProductList from '../components/Product/CartProductList';
+import { RecoilRoot } from 'recoil';
+import { fetchCartProducts } from '../apis/cartProducts';
+
+const meta = {
+  title: 'Product/CartProductList',
+  component: CartProductList,
+  tags: ['autodocs'],
+  argTypes: {},
+  decorators: [
+    (Story) => (
+      <RecoilRoot>
+        <Story />
+      </RecoilRoot>
+    ),
+  ],
+  render: (args, { loaded: { products } }) => <CartProductList {...products} />,
+} satisfies Meta<typeof CartProductList>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  loaders: [
+    async () => ({
+      products: await fetchCartProducts(),
+    }),
+  ],
+};
