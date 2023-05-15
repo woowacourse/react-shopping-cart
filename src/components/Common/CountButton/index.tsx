438e4475e9f5450a7cbc189cf6e30b70b33d14df
d0dam <smallkdb@gmail.com>
2023-05-15 22:55:20 +0900

    refactor: CountButton을 외부에서 제어하도록 변경

--- src/components/Common/CountButton/index.tsx
+++ src/components/Common/CountButton/index.tsx
@@ -1,34 +1,21 @@
-import { useEffect, useState } from 'react';
 import styles from './index.module.scss';
 import { ReactComponent as ArrowDown } from '../../../assets/arrow-down.svg';
 import { ReactComponent as ArrowUp } from '../../../assets/arrow-up.svg';
 
 interface CountButtonProps {
-  getCount: (count: number) => void;
+  count: number;
+  handleUpButton: React.MouseEventHandler<HTMLButtonElement>;
+  handleDownButton: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-function CountButton({ getCount }: CountButtonProps) {
-  const [count, setCount] = useState(1);
-
-  useEffect(() => {
-    getCount(count);
-  }, [count, getCount]);
-
-  const upCount = () => {
-    setCount(prev => prev + 1);
-  };
-
-  const downCount = () => {
-    setCount(prev => prev - 1);
-  };
-
+function CountButton({ count, handleUpButton, handleDownButton }: CountButtonProps) {
   return (
     <div className={styles['counter-container']}>
       <div>{count}</div>
-      <button onClick={upCount}>
+      <button onClick={handleUpButton}>
         <ArrowUp />
       </button>
-      <button onClick={downCount}>
+      <button onClick={handleDownButton}>
         <ArrowDown />
       </button>
     </div>
