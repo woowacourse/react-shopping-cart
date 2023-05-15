3ace8ca992104fb148dcddda2044d5df91302fec
bassyu <yrkdgus@gmail.com>
2023-05-15 16:23:32 +0900

    chore(useCart): 사용하지 않는 모듈 import 삭제

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -1,8 +1,6 @@
 import type { CartItemType } from '../types';
-import { useEffect } from 'react';
 import { useRecoilState } from 'recoil';
 import { cartState } from '../recoil/state';
-import { LOCAL_STORAGE_KEY } from '../constants';
 
 export default function useCart() {
   const [cart, setCart] = useRecoilState(cartState);
