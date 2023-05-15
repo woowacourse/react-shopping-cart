a3c5a574d9e11e0d13c29e9f473bca8872a4a70b
nangkyeonglim <nangkyeonglim@gmail.com>
2023-05-15 17:01:47 +0900

    test: cypress 테스트 수정

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -9,12 +9,13 @@ describe('정상 작동 기능 테스트', () => {
     cy.get('[data-cy="headerlogo"]').should('have.text', 'SHOP');
   });
 
-  it('카드 장바구니 버튼 클릭 시 stepper Input이 정상적으로 출력된다.', () => {
+  it('카드 장바구니 버튼 클릭 시 장바구니에 담긴 물품 종류 개수가 헤더에 표시된다.', () => {
     cy.get('@firstProductItem').find('button').click();
     cy.get('@firstProductItem').find('input').should('be.visible');
     cy.get('@firstProductItem').find('input').should('have.value', '1');
-    cy.get('@firstProductItem').find('input').type('0');
 
-    cy.get('[data-cy="totalQuantity"]').should('have.text', '10');
+    cy.get('@firstProductItem').next().find('button').click();
+
+    cy.get('[data-cy="totalQuantity"]').should('have.text', '2');
   });
 });
