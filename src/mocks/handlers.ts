91a2e47ec8a07401ae85519b3d133559e00bea4e
ashleysyheo <ashleysyheo@gmail.com>
2023-05-15 22:55:43 +0900

    feat: msw 카트 아이템 POST 요청 코드 추가

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,8 +2,38 @@ import { rest } from 'msw';
 
 import { CART_LIST_LOCAL_STORAGE_KEY } from '../constants';
 import productListData from '../data/mockData.json';
-import { CartItemData } from '../types';
-import { getFromLocalStorage } from '../utils/localStorage';
+import { CartItemData, PostCartItemRequestBody } from '../types';
+import { getFromLocalStorage, saveToLocalStorage } from '../utils/localStorage';
+
+const getCartData = () => {
+  return getFromLocalStorage<CartItemData[]>(CART_LIST_LOCAL_STORAGE_KEY) ?? [];
+};
+
+const setCartData = (newCartList: CartItemData[]) => {
+  saveToLocalStorage(CART_LIST_LOCAL_STORAGE_KEY, newCartList);
+};
+
+const changeCartItemQuantity = (cartList: CartItemData[], productId: number, quantity: number) => {
+  const selectedCartItemIndex = cartList.findIndex((cartItem) => cartItem.product.id === productId);
+
+  if (selectedCartItemIndex === -1) {
+    const newCartId = Number(new Date());
+    const product = productListData.find((productItem) => productItem.id === productId)!;
+
+    return [...cartList, { id: newCartId, quantity, product }];
+  }
+
+  const updatedCartList = [
+    ...cartList.slice(0, selectedCartItemIndex),
+    {
+      ...cartList[selectedCartItemIndex],
+      quantity: quantity + cartList[selectedCartItemIndex].quantity,
+    },
+    ...cartList.slice(selectedCartItemIndex + 1),
+  ];
+
+  return updatedCartList;
+};
 
 const handlers = [
   rest.get('/api/products', (req, res, ctx) => {
@@ -14,6 +44,16 @@ const handlers = [
     const cartList = getFromLocalStorage<CartItemData>(CART_LIST_LOCAL_STORAGE_KEY);
 
     return res(ctx.delay(2000), ctx.status(200), ctx.json(cartList));
+  }),
+
+  rest.post<CartItemData>('/api/carts/add', async (req, res, ctx) => {
+    const { productId, quantity } = await req.json<PostCartItemRequestBody>();
+    const currentCartData = getCartData();
+
+    const newCartList = changeCartItemQuantity(currentCartData, productId, quantity);
+    setCartData(newCartList);
+
+    return res(ctx.status(200), ctx.json(newCartList));
   }),
 ];
 
