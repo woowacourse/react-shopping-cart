e9da2c5cfd24fc8bad350c4a73ee4807ba185241
jeongwusi <jeongwusi@gmail.com>
2023-05-15 11:39:23 +0900

    ♻️ refactor: fetch 관련 로직 분리

---
+++ src/api/cartApi.ts
@@ -0,0 +1,32 @@
+export const addToCart = async (productId: number) => {
+  const response = await fetch('./cart-items', {
+    method: 'POST',
+    body: JSON.stringify({
+      productId,
+    }),
+  });
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+};
+
+export const updateCartItem = async (id: number, count: number) => {
+  const response = await fetch(`/cart-items/${id}`, {
+    method: 'PATCH',
+    body: JSON.stringify({
+      quantity: count,
+    }),
+  });
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+};
+
+export const deleteCartItem = async (id: number) => {
+  const response = await fetch(`/cart-items/${id}`, {
+    method: 'DELETE',
+  });
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+};
