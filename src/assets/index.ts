7b1f9a341ada924e8e8db6f0388018e3db711fbd
ashleysyheo <ashleysyheo@gmail.com>
2023-05-15 22:49:25 +0900

    feat: x svg 아이콘 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,2 +1,3 @@
-export { ReactComponent as MinusIcon } from './minus-icon.svg';
 export { ReactComponent as AddIcon } from './add-icon.svg';
+export { ReactComponent as CloseIcon } from './close-icon.svg';
+export { ReactComponent as MinusIcon } from './minus-icon.svg';
