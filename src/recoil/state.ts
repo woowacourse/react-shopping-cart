e64b2420074cbf1cac3cf180de0bf0b59d8353f6
bassyu <yrkdgus@gmail.com>
2023-05-15 18:49:26 +0900

    refactor(recoil/state): localStorageEffect 추출

--- src/recoil/state.ts
+++ src/recoil/state.ts
@@ -1,17 +1,23 @@
 import type { CartType } from '../types';
+import type { AtomEffect } from 'recoil';
 import { atom, selector } from 'recoil';
 import { LOCAL_STORAGE_KEY } from '../constants';
 
+const localStorageEffect: <T>(key: string) => AtomEffect<T> =
+  (key) =>
+  ({ setSelf, onSet }) => {
+    const localStorageValue = localStorage.getItem(key);
+    if (localStorageValue !== null) setSelf(JSON.parse(localStorageValue));
+
+    onSet((newValue, _, isReset) => {
+      isReset ? localStorage.removeItem(key) : localStorage.setItem(key, JSON.stringify(newValue));
+    });
+  };
+
 export const cartState = atom<CartType>({
   key: 'cartState',
-  default: JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY.cart) ?? '[]'),
-  effects: [
-    ({ onSet }) => {
-      onSet((newCart) => {
-        localStorage.setItem(LOCAL_STORAGE_KEY.cart, JSON.stringify(newCart));
-      });
-    },
-  ],
+  default: [],
+  effects: [localStorageEffect(LOCAL_STORAGE_KEY.cart)],
 });
 
 export const cartCountState = selector({
