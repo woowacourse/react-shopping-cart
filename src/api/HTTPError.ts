12a6cc560b3d83535cb0e2ebcbbd1db7b047ab9f
ashleysyheo <ashleysyheo@gmail.com>
2023-05-15 14:01:30 +0900

    feat: HTTPError 생성

---
+++ src/api/HTTPError.ts
@@ -0,0 +1,11 @@
+class HTTPError extends Error {
+  statusCode: number;
+
+  constructor(statusCode: number, message?: string) {
+    super(message);
+    this.name = 'HTTPError';
+    this.statusCode = statusCode;
+  }
+}
+
+export default HTTPError;
