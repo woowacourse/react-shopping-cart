0e2280bf0581be0578269a1e56cab94f55fe13f2
wzrabbit <curious.wzrabbit@gmail.com>
2023-05-15 17:13:33 +0900

    ✨ feat: FullWidthTitle 컴포넌트 구현

--- src/pages/CartView.tsx
+++ src/pages/CartView.tsx
@@ -1,9 +1,20 @@
+import { styled } from "styled-components";
+import FullWidthTitle from "../components/common/FullWidthTitle/FullWidthTitle";
+
 const CartView = () => {
   return (
-    <p style={{ color: "white", paddingTop: "100px" }}>
-      여기 대충 장바구니 페이지가 올 예정
-    </p>
+    <Container>
+      <FullWidthTitle>장바구니</FullWidthTitle>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  padding: 100px 15px 0 15px;
+  max-width: 1320px;
+  height: 600px;
+  background-color: #00002210;
+  margin: auto;
+`;
 
 export default CartView;
