1c17f374f2d4e408dcdd27775e5d9b4abe065975
ashleysyheo <ashleysyheo@gmail.com>
2023-05-15 22:57:49 +0900

    feat: 카트 아이템 추가 POST 요청 함수 추가

--- src/api/cartAPI.ts
+++ src/api/cartAPI.ts
@@ -4,4 +4,20 @@ const getCartList = async () => {
   return await fetchAPI('/api/carts');
 };
 
-export { getCartList };
+const postCartItem = async (productId: number, quantity: number) => {
+  const data = {
+    quantity,
+    productId,
+  };
+  const jsonData = JSON.stringify(data);
+
+  return await fetchAPI('/api/carts/add', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: jsonData,
+  });
+};
+
+export { getCartList, postCartItem };
