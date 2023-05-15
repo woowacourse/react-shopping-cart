a16398a71a05185b43e445d752f1b5cef5c503e1
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-05-16 03:16:57 +0900

    refactor: recoil-persist 제거
    - recoil 상태 유지를 하지 않는 것으로 계획 변경

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,6 @@
-import { useCallback, useEffect } from 'react';
-import { useSetRecoilState } from 'recoil';
-import { cartState } from '../../recoil/cartAtoms';
 import ProductList from '../../components/ProductList';
 
-
 function Home() {
-  const setCartList = useSetRecoilState(cartState);
-  const loadCartList = useCallback(async () => {
-    // fetch('/cart-items');
-  }, [setCartList]);
-
-  useEffect(() => {
-    loadCartList();
-  }, [loadCartList]);
 
   return (
     <ProductList />
--- src/recoil/cartAtoms.ts
+++ src/recoil/cartAtoms.ts
@@ -1,16 +1,9 @@
 import { atom, selector } from 'recoil';
 import { Cart } from '../types/types';
-import { recoilPersist } from 'recoil-persist';
-
-const { persistAtom } = recoilPersist({
-  key: 'recoil-persist',
-  storage: sessionStorage,
-});
 
 export const cartState = atom<Cart[]>({
   key: 'cartState',
   default: [],
-  effects_UNSTABLE: [persistAtom],
 });
 
 export const cartCountSelector = selector({
