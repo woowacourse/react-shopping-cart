8c5c3f056de93657b1319d5aeb5093e0234a07e1
ashleysyheo <ashleysyheo@gmail.com>
2023-05-15 14:06:20 +0900

    refactor: recoil productListState api 호출 데이터로 초기 데이터 세팅

--- src/store/product.ts
+++ src/store/product.ts
@@ -1,12 +1,16 @@
 import { atom } from 'recoil';
 
-import initialListData from '../data/mockData.json';
+import { getProductList } from '../api/productAPI';
 import { ProductItemData } from '../types';
 
 const productListState = atom<ProductItemData[]>({
   key: 'productList',
   default: [],
-  effects: [({ setSelf }) => setSelf(initialListData)],
+  effects: [
+    ({ setSelf }) => {
+      setSelf(getProductList().then((savedValue) => savedValue ?? []));
+    },
+  ],
 });
 
 export { productListState };
