665af1a2049a4e667b060e0d647725cf6b194f72
jeongwusi <jeongwusi@gmail.com>
2023-05-16 20:06:52 +0900

    ♻️ refactor: promise체이닝을 async-await로 수정

--- src/components/ProductItemList/index.tsx
+++ src/components/ProductItemList/index.tsx
@@ -1,18 +1,27 @@
 import { useEffect } from 'react';
 import ProductItem from '../ProductItem';
-
 import styles from './index.module.css';
 import { $Products } from '../../recoil/atom';
 import { useRecoilState } from 'recoil';
 import { Product } from '../../types';
+import { toast } from 'react-toastify';
+import errorMessage from '../../constant/errorMessage';
 
 const ProductItemList = () => {
   const [products, setProducts] = useRecoilState($Products);
 
   useEffect(() => {
-    fetch('./products/')
-      .then(res => res.json())
-      .then(data => setProducts(data));
+    const fetchData = async () => {
+      try {
+        const response = await fetch('./products/');
+        const data = await response.json();
+        setProducts(data);
+      } catch (e) {
+        toast.error(errorMessage);
+      }
+    };
+
+    fetchData();
   }, [setProducts]);
 
   return (
