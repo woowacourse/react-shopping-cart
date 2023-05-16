754ca1528df28f0b668f0f4004e780d974a832a3
cruelladevil <dev.timetravel@gmail.com>
2023-05-16 14:42:13 +0900

    feat: cart에서 quantity를 가져오는 selectorFamily 구현

--- src/atoms/product.ts
+++ src/atoms/product.ts
@@ -1,6 +1,7 @@
 import { Product } from './../types/product';
-import { selector } from 'recoil';
+import { selector, selectorFamily } from 'recoil';
 import mockData from '../data/mockProducts.json';
+import { cartState } from './cart';
 
 export const fetchProductSelector = selector({
   key: 'FetchProductSelector',
@@ -9,4 +10,15 @@ export const fetchProductSelector = selector({
 
     return products;
   },
+});
+
+export const cartQuantityReadOnlyState = selectorFamily({
+  key: 'cartQuantityReadOnlyState',
+  get:
+    (id) =>
+    ({ get }) => {
+      const quantity = get(cartState).find(({ product }) => product.id === id)?.quantity;
+
+      return quantity || 0;
+    },
 });
