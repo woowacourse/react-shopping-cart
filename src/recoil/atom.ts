a63b0946b92e16c589740e30d4e633f8b7f56bd2
guridaek <xylitol79@gmail.com>
2023-05-16 09:56:05 +0900

    refactor: localStorage util 제거

--- src/recoil/atom.ts
+++ src/recoil/atom.ts
@@ -1,9 +1,20 @@
 import { atom } from "recoil";
 import mockData from "../mockData.json";
 import { ItemType } from "../types/domain";
-import { getLocalStorage } from "../utils";
 
-const cartItems = getLocalStorage<ItemType[]>("cart");
+const getCartItems = (): ItemType[] => {
+  const item = localStorage.getItem("cart");
+
+  try {
+    return item ? JSON.parse(item) : [];
+  } catch {
+    console.log("로컬 스토리지에서 데이터를 파싱하지 못했습니다!");
+
+    return [];
+  }
+};
+
+const cartItems = getCartItems();
 
 export const itemsState = atom({
   key: "items",
