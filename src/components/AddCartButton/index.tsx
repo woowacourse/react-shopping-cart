cb8bccad6154b276c79b264d6896dba4b605c33d
d0dam <smallkdb@gmail.com>
2023-05-17 04:27:19 +0900

    refactor: useCart를 사용해 코드 리팩토링

--- src/components/AddCartButton/index.tsx
+++ src/components/AddCartButton/index.tsx
@@ -1,55 +1,34 @@
 import { ReactComponent as ShopIcon } from '../../assets/mini-shop-icon.svg';
-import styles from './index.module.scss';
 import CountButton from '../Common/CountButton';
-import useMutationQuery from '../../hooks/useMutationQuery';
-import { useState } from 'react';
-import type { CartItem } from '../../types';
-import { useRecoilState, useSetRecoilState } from 'recoil';
-import { $Cart, $ToastMessageList } from '../../recoil/atom';
+import useCart from '../../hooks/useCart';
+import styles from './index.module.scss';
+import type { Product } from '../../types';
 
 interface AddCardButtonProps {
+  product: Product;
   id: number;
 }
 
-function AddCartButton({ id }: AddCardButtonProps) {
-  const setMessageList = useSetRecoilState($ToastMessageList);
-  const [count, setCount] = useState(0);
-  const [cart, setCart] = useRecoilState($Cart);
-  const { mutateQuery, loading, error } = useMutationQuery<Record<string, number>, CartItem>('/cart-items');
+function AddCartButton({ product, id }: AddCardButtonProps) {
+  const { cartIdList, cartItemState, addCartItem, addQuantity, deleteCartItem } = useCart(id);
 
-  const handleClick = async () => {
-    await mutateQuery('POST', { productId: id });
-    setCart(prev => [...prev, id]);
-    setCount(prev => prev + 1);
-
-    if (!(loading || error)) {
-      setMessageList(prev => [...prev, '장바구니에 등록되었습니다.']);
-    }
-  };
-
-  const handleUpButton = async () => {
-    await mutateQuery('PATCH', { quantity: count + 1 }, String(id));
-    setCount(prev => prev + 1);
-  };
-
+  const handleClick = async () => await addCartItem(product);
+  const handleUpButton = async () => await addQuantity(1);
   const handleDownButton = async () => {
-    await mutateQuery('PATCH', { quantity: count - 1 }, String(id));
-
-    if (count === 1) {
-      await mutateQuery('DELETE', undefined, String(id));
-      setCart(prev => prev.filter(item => item !== id));
-      if (!(loading || error)) {
-        setMessageList(prev => [...prev, '장바구니에서 삭제되었습니다.']);
-      }
+    if (cartItemState.quantity === 1) {
+      await deleteCartItem();
     }
-
-    setCount(prev => prev - 1);
+    await addQuantity(-1);
   };
 
   return (
     <div className={styles['container']}>
-      {cart.find(cartId => cartId === id) ? (
-        <CountButton count={count} handleUpButton={handleUpButton} handleDownButton={handleDownButton} />
+      {cartIdList.includes(id) ? (
+        <CountButton
+          count={cartItemState.quantity}
+          handleUpButton={handleUpButton}
+          handleDownButton={handleDownButton}
+        />
       ) : (
         <button onClick={handleClick}>
           <ShopIcon />
--- src/components/CartProductItem/index.tsx
+++ src/components/CartProductItem/index.tsx
@@ -2,25 +2,45 @@ import { ReactComponent as TrashBox } from '../../assets/trash-box.svg';
 import styles from './index.module.scss';
 import type { CartItem } from '../../types';
 import CountButton from '../Common/CountButton';
+import { ChangeEventHandler } from 'react';
+import useCart from '../../hooks/useCart';
 
 interface CartProductItemProps {
   cartItem: CartItem;
+  deleteCart: React.MouseEventHandler<HTMLButtonElement>;
+  toggleCheck: ChangeEventHandler<HTMLInputElement>;
+  checked: boolean;
 }
 
-function CartProductItem({ cartItem }: CartProductItemProps) {
-  const { id, quantity, product } = cartItem;
-  const { name, imageUrl, price } = product;
+function CartProductItem({ cartItem, deleteCart, toggleCheck, checked }: CartProductItemProps) {
+  const { product } = cartItem;
+  const { id, name, imageUrl, price } = product;
+  const { cartItemState, addQuantity } = useCart(id);
+
+  const handleUpButton = async () => await addQuantity(1);
+  const handleDownButton = async () => {
+    if (cartItemState.quantity > 0) {
+      await addQuantity(-1);
+    }
+  };
 
   return (
     <div className={styles.container}>
-      <input type="checkbox" className={styles['check-box']} />
+      <input type="checkbox" className={styles['check-box']} onChange={toggleCheck} checked={checked} />
       <img src={imageUrl} alt={name} className={styles.image} />
       <div className={styles['item-info']}>
         <div>
           <div className={styles['product-title']}>{name}</div>
-          <TrashBox size={24} />
+          <button onClick={deleteCart}>
+            <TrashBox size={24} />
+          </button>
         </div>
-        <CountButton large count={quantity} handleUpButton={() => {}} handleDownButton={() => {}} />
+        <CountButton
+          large
+          count={cartItemState.quantity}
+          handleUpButton={handleUpButton}
+          handleDownButton={handleDownButton}
+        />
         <div className={styles['product-price']}>{price.toLocaleString()} 원</div>
       </div>
     </div>
