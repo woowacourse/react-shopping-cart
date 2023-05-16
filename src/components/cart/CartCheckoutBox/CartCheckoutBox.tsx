1a0e2f52c4b641d38ab73c2a39e53b7b25ea16f2
ashleysyheo <ashleysyheo@gmail.com>
2023-05-16 15:11:33 +0900

    feat: CartCheckoutBox 컴포넌트 생성

---
+++ src/components/cart/CartCheckoutBox/CartCheckoutBox.tsx
@@ -0,0 +1,41 @@
+import { useRecoilValueLoadable } from 'recoil';
+
+import { SHIPPING_FEE } from '../../../constants';
+import { cartListSubTotalState } from '../../../store/cart';
+import { priceFormatter } from '../../../utils/formatter';
+import Button from '../../common/Button/Button';
+import { Text } from '../../common/Text/Text.styles';
+import * as S from './CartCheckoutBox.styles';
+
+const CartCheckoutBox = () => {
+  const cartListSubTotal = useRecoilValueLoadable(cartListSubTotalState);
+  const totalPrice = cartListSubTotal.contents + SHIPPING_FEE;
+
+  return (
+    <S.CartCheckoutBoxWrapper>
+      <S.CheckoutInformationContainer>
+        <S.CheckoutInformationTextContainer>
+          <Text>총 상품 가격</Text>
+          <S.CheckoutValueText>
+            {(cartListSubTotal.state === 'hasValue' && priceFormatter(cartListSubTotal.contents)) ||
+              0}
+            원
+          </S.CheckoutValueText>
+        </S.CheckoutInformationTextContainer>
+        <S.CheckoutInformationTextContainer>
+          <Text>총 배송비</Text>
+          <S.CheckoutValueText>{priceFormatter(SHIPPING_FEE)}원</S.CheckoutValueText>
+        </S.CheckoutInformationTextContainer>
+        <S.CheckoutTotalPriceContainer>
+          <Text>결제 예상 금액</Text>
+          <S.CheckoutTotalPriceValueText>
+            {(cartListSubTotal.state === 'hasValue' && priceFormatter(totalPrice)) || 0}원
+          </S.CheckoutTotalPriceValueText>
+        </S.CheckoutTotalPriceContainer>
+        <Button variant="primary">주문하기</Button>
+      </S.CheckoutInformationContainer>
+    </S.CartCheckoutBoxWrapper>
+  );
+};
+
+export default CartCheckoutBox;
