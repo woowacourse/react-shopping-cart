94ebed3e77b99473a9e46560635f083a802ac7d7
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-05-16 10:26:21 +0900

    refator(fetcher): Base url 분리

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,5 +1,7 @@
+const BASE_URL = `${process.env.PUBLIC_URL}`;
+
 const fetcher = async <T>(url: string, options?: RequestInit): Promise<T> => {
-  const response = await fetch(url, options);
+  const response = await fetch(`${BASE_URL}${url}`, options);
 
   if (!response.ok) {
     throw new Error(`데이터 요청에 실패했습니다. 요청 URL: ${url}`);
