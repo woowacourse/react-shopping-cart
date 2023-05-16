d4bded2c5d073428bc93559454f8e2b3718faa2b
d0dam <smallkdb@gmail.com>
2023-05-17 04:22:51 +0900

    feat: Cart 상품들과 상품 총합을 계산하는 전역 상태 선언

--- src/recoil/atom.ts
+++ src/recoil/atom.ts
@@ -1,8 +1,35 @@
-import { atom } from 'recoil';
+import { atom, atomFamily, selector } from 'recoil';
+import { CartItem } from '../types';
 
-export const $Cart = atom<number[]>({
-  key: 'Cart',
+export const $CartIdList = atom<number[]>({
+  key: 'CartIdList',
   default: [],
+});
+
+export const $CartItemState = atomFamily<CartItem, number>({
+  key: 'CartItemState',
+  default: id => {
+    const initialCart: CartItem = {
+      id,
+      quantity: 0,
+      product: {
+        id,
+        imageUrl: '',
+        name: '',
+        price: 0,
+      },
+    };
+    return initialCart;
+  },
+});
+
+export const $CartTotalPrice = selector<number>({
+  key: 'CartTotalPrice',
+  get: ({ get }) =>
+    get($CartIdList).reduce((acc, id) => {
+      const { quantity, product } = get($CartItemState(id));
+      return quantity * product.price + acc;
+    }, 0),
 });
 
 export const $ToastMessageList = atom<string[]>({
