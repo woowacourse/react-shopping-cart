d531b1a949624fe629c667515adf134ceff5c4ec
d0dam <smallkdb@gmail.com>
2023-05-17 05:50:15 +0900

    feat: 장바구니가 비었을 때와 반응형 구현

--- src/Page/Cart/index.tsx
+++ src/Page/Cart/index.tsx
@@ -1,18 +1,32 @@
+import { ReactComponent as AlertBlank } from '../../assets/baemin-alert-blank.svg';
 import { useRecoilValue } from 'recoil';
 import CartProductItemList from '../../components/CartProductItemList';
 import PaymentsView from '../../components/PaymentsView';
 import styles from './index.module.scss';
-import { $CartTotalPrice } from '../../recoil/atom';
+import { $CartIdList, $CartTotalPrice } from '../../recoil/atom';
 
 function Cart() {
   const cartTotalPrice = useRecoilValue($CartTotalPrice);
+  const cartIdList = useRecoilValue($CartIdList);
+
+  if (cartIdList.length > 0) {
+    return (
+      <main className={styles.container}>
+        <h2 className={styles.title}>장바구니</h2>
+        <section className={styles['main-view']}>
+          <CartProductItemList />
+          <PaymentsView priceTotal={cartTotalPrice} parcelPrice={3000} />
+        </section>
+      </main>
+    );
+  }
 
   return (
     <main className={styles.container}>
       <h2 className={styles.title}>장바구니</h2>
-      <section className={styles['main-view']}>
-        <CartProductItemList />
-        <PaymentsView priceTotal={cartTotalPrice} parcelPrice={3000} />
+      <section className={styles['main-view-blank']}>
+        <AlertBlank />
+        <p>장바구니가 비어있어요!</p>
       </section>
     </main>
   );
