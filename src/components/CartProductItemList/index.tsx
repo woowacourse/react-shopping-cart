238996e0f9b624ffabe603f83de10a2ff7840dad
d0dam <smallkdb@gmail.com>
2023-05-17 04:27:45 +0900

    feat: input checkbox와 삭제 관련된 기능 구현

--- src/components/CartProductItemList/index.tsx
+++ src/components/CartProductItemList/index.tsx
@@ -1,14 +1,17 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from 'react';
-import { useSetRecoilState } from 'recoil';
+import { useRecoilState, useSetRecoilState } from 'recoil';
 import CartProductItem from '../CartProductItem';
 import useGetQuery from '../../hooks/useGetQuery';
-import { $ToastMessageList } from '../../recoil/atom';
+import { $CartIdList, $ToastMessageList } from '../../recoil/atom';
 import styles from './index.module.scss';
 import type { CartItem } from '../../types';
+import useMutationQuery from '../../hooks/useMutationQuery';
 
 function CartProductItemList() {
-  const { data: cartProductsData, error } = useGetQuery<CartItem[]>('./cart-items');
+  const { data: cartProductsData, error, refreshQuery } = useGetQuery<CartItem[]>('./cart-items');
+  const { mutateQuery } = useMutationQuery<Record<string, number>, CartItem>('./cart-items');
+  const [cartIdList, setCartIdList] = useRecoilState($CartIdList);
   const setMessageList = useSetRecoilState($ToastMessageList);
 
   useEffect(() => {
@@ -17,18 +20,60 @@ function CartProductItemList() {
     }
   }, [error]);
 
+  const checkAllCartItem: React.ChangeEventHandler<HTMLInputElement> = ({ target: { checked } }) => {
+    if (checked && cartProductsData) {
+      return setCartIdList(cartProductsData.map(({ product }) => product.id));
+    }
+    return setCartIdList([]);
+  };
+
+  const checkCartItem =
+    (id: number) =>
+    ({ target: { checked } }: React.ChangeEvent<HTMLInputElement>) => {
+      if (!checked) {
+        return setCartIdList(prev => prev.filter(cartId => cartId !== id));
+      }
+      return setCartIdList(prev => [...prev, id]);
+    };
+
+  const deleteCartItem = (id: number) => async () => {
+    await mutateQuery('DELETE', undefined, String(id));
+    refreshQuery();
+
+    setCartIdList(prev => prev.filter(cartId => cartId !== id));
+  };
+
+  const deleteCheckedCartItem = () => {
+    cartIdList.forEach(async id => {
+      await mutateQuery('DELETE', undefined, String(id));
+    });
+    refreshQuery();
+    setCartIdList([]);
+  };
+
   return (
     <div className={styles.container}>
-      <h3 className={styles.title}>배송 상품 {'(3개)'}</h3>
+      <h3 className={styles.title}>배송 상품 {`(${cartIdList.length}개)`}</h3>
       <section className={styles['cart-container']}>
         {cartProductsData?.map((item: CartItem) => (
-          <CartProductItem key={item.id} cartItem={item} />
+          <CartProductItem
+            key={item.id}
+            cartItem={item}
+            deleteCart={deleteCartItem(item.product.id)}
+            toggleCheck={checkCartItem(item.product.id)}
+            checked={cartIdList.includes(item.product.id)}
+          />
         ))}
       </section>
       <div className={styles['check-menu']}>
-        <input type="checkbox" className={styles['check-box']} />
-        <div>전체 선택 (2/3)</div>
-        <button>선택 삭제</button>
+        <input
+          type="checkbox"
+          className={styles['check-box']}
+          onChange={checkAllCartItem}
+          checked={cartIdList.length === cartProductsData?.length}
+        />
+        <div>전체 선택 ({`${cartIdList.length}/${cartProductsData?.length}`})</div>
+        <button onClick={deleteCheckedCartItem}>선택 삭제</button>
       </div>
     </div>
   );
