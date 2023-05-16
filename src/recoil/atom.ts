d48230f6481782d3bcf1ff29c37a25ac87a432f2
jeongwusi <jeongwusi@gmail.com>
2023-05-16 20:05:27 +0900

    ♻️ refactor: recoil의 Cart 관련된 부분 수정

--- src/recoil/atom.ts
+++ src/recoil/atom.ts
@@ -1,19 +1,11 @@
-import { atom, selector } from 'recoil';
+import { atom } from 'recoil';
 
-export const $CartCount = atom<Record<number, number>>({
-  key: 'CartCount',
-  default: {},
+export const $Cart = atom<number[]>({
+  key: 'Cart',
+  default: [],
 });
 
 export const $Products = atom({
   key: 'products',
   default: [],
-});
-
-export const $CartCountTotal = selector({
-  key: 'CartCountTotal',
-  get: ({ get }) => {
-    const total = Object.values(get($CartCount)).reduce((acc, count) => acc + count, 0);
-    return total;
-  },
 });
