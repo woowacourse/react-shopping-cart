eab01841af12794d8b8f637611e244c86cdd6306
inyeong-kang <tomatorkd@naver.com>
2023-05-16 11:21:32 +0900

    refactor: Atom effects로 localstorage 데이어 persist 하기

--- src/recoil/state.ts
+++ src/recoil/state.ts
@@ -2,9 +2,24 @@ import type { CartItemType } from '../types';
 import { atom, selector } from 'recoil';
 import { LOCAL_STORAGE_KEY } from '../constants';
 
+const localStorageEffect =
+  (key: string) =>
+  ({ setSelf, onSet }: any) => {
+    const savedValue = localStorage.getItem(key);
+
+    if (savedValue != null) {
+      setSelf(JSON.parse(savedValue));
+    }
+
+    onSet((newValue: any, _: any, isReset: boolean) => {
+      isReset ? localStorage.removeItem(key) : localStorage.setItem(key, JSON.stringify(newValue));
+    });
+  };
+
 export const cartState = atom<CartItemType[]>({
   key: 'cartState',
-  default: JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY.cart) ?? '[]'),
+  default: [],
+  effects: [localStorageEffect(LOCAL_STORAGE_KEY.cart)],
 });
 
 export const cartCountState = selector<number>({
