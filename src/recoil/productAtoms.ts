5093da32c02749eb3bd6c31d371b3d4f73f2954d
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-05-16 14:18:24 +0900

    refactor: 상품 목록을 fetch에 연결 및 msw 모킹 구현

--- src/recoil/productAtoms.ts
+++ src/recoil/productAtoms.ts
@@ -1,13 +1,11 @@
 import { selector } from 'recoil';
 import type { ProductItem } from '../types/types';
-import mockData from '../assets/mockData.json'
 
 export const fetchProductList = selector<ProductItem[]>({
   key: 'fetchProductList',
   get: async () => {
-    // const response = await fetch('/products');
-    // const data = await response.json();
-    // return data;
-    return mockData;
+    const response = await fetch('/products');
+    const data = await response.json();
+    return data;
   },
 });
