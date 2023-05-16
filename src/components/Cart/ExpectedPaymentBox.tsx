cee8d08575c3e15c2be8206909c8029f0cfdc796
Leejin-Yang <ejin1996@gmail.com>
2023-05-16 17:12:47 +0900

    feat: ExpectedPaymentBox 컴포넌트 작성

---
+++ src/components/Cart/ExpectedPaymentBox.tsx
@@ -0,0 +1,80 @@
+import { styled } from 'styled-components';
+
+const ExpectedPaymentBox = () => {
+  return (
+    <ExpectedPaymentContainer>
+      <ExpectedPaymentTitle>결제예상금액</ExpectedPaymentTitle>
+      <ExpectedPaymentInfo>
+        <PaymentInfoItem>
+          <dt>총 상품가격</dt>
+          <dd>21,700원</dd>
+        </PaymentInfoItem>
+        <PaymentInfoItem>
+          <dt>총 배송비</dt>
+          <dd>3,000원</dd>
+        </PaymentInfoItem>
+        <PaymentInfoItem>
+          <dt>총 주문금액</dt>
+          <dd>24,700원</dd>
+        </PaymentInfoItem>
+      </ExpectedPaymentInfo>
+      <OrderButtonWrapper>
+        <Button type='button'>주문하기</Button>
+      </OrderButtonWrapper>
+    </ExpectedPaymentContainer>
+  );
+};
+
+const ExpectedPaymentContainer = styled.div`
+  width: 450px;
+  border: 1px solid ${({ theme }) => theme.colors.gray100};
+`;
+
+const ExpectedPaymentTitle = styled.h2`
+  height: 80px;
+  padding: 0 30px;
+  line-height: 80px;
+  font-size: 24px;
+  font-weight: 400;
+  border-bottom: 3px solid ${({ theme }) => theme.colors.gray100};
+`;
+
+const ExpectedPaymentInfo = styled.div`
+  padding: 30px 30px 0;
+
+  & > dl + dl {
+    margin: 16px 0 0 0;
+  }
+
+  & > dl:last-of-type {
+    margin: 40px 0 0 0;
+  }
+`;
+
+const PaymentInfoItem = styled.dl`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  & > dt,
+  dd {
+    font-size: 20px;
+    font-weight: 600;
+    line-height: 1.5;
+  }
+`;
+
+const OrderButtonWrapper = styled.div`
+  padding: 0 30px 30px;
+  margin: 40px 0 0 0;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  height: 75px;
+  color: ${({ theme }) => theme.colors.white};
+  background: ${({ theme }) => theme.colors.black};
+  font-size: 24px;
+`;
+
+export default ExpectedPaymentBox;
