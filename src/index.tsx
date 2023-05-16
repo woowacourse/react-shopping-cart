86c6cb6d5367cda7b7121a2b374085bb498e6ff4
yeopto <gunyeop530@gmail.com>
2023-05-16 17:49:25 +0900

    feat: msw 셋팅

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 import App from './components/App';
 import { ResetStyle } from './styles/ResetStyle';
+import { worker } from './mock/workers';
+
+if (process.env.NODE_ENV === 'development') {
+  worker.start();
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
