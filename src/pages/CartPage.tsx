630c3cd1bd72f30de268baac06f725f3f935b8b7
Dain Lee <feb.dain@gmail.com>
2023-05-16 11:17:51 +0900

    feat: CartPage 생성

---
+++ src/pages/CartPage.tsx
@@ -0,0 +1,11 @@
+import Header from '../components/common/Header';
+
+const CartPage = () => {
+  return (
+    <div>
+      <Header title="STORE" />
+    </div>
+  );
+};
+
+export default CartPage;
