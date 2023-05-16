c4e9c4a8a92893f6ed7efa06bfc8febc5751f7b0
SangwonKang <72087183+ksone02@users.noreply.github.com>
2023-05-16 19:16:26 +0900

    fix: Story config 오탈자 수정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.stories.@(|ts|tsx)'],
+  stories: ['../src/**/*.stories.@(ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
