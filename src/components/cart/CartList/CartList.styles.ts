0389e24ddebf40707a0eda545965d0541c3d9eba
ashleysyheo <ashleysyheo@gmail.com>
2023-05-16 09:51:39 +0900

    feat: CartList 컴포넌트 생성

---
+++ src/components/cart/CartList/CartList.styles.ts
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+import { Text } from '../../common/Text/Text.styles';
+
+const CartListContainer = styled.section`
+  margin-top: 24px;
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.spacer.spacing4};
+
+  &.center {
+    margin: 0;
+    padding: 72px;
+    justify-content: center;
+    align-items: center;
+    gap: initial;
+
+    & button {
+      width: 200px;
+    }
+  }
+`;
+
+const CartListEmptyImage = styled.img`
+  width: 150px;
+  height: 150px;
+  margin-bottom: ${({ theme }) => theme.spacer.spacing3};
+`;
+
+const CartListEmptyMessage = styled(Text)`
+  margin-bottom: ${({ theme }) => theme.spacer.spacing4};
+  font-weight: 600;
+`;
+
+export { CartListContainer, CartListEmptyImage, CartListEmptyMessage };
