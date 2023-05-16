9ff468a7d3edb46f3684dc15a87fb3c1672fcf4a
wzrabbit <curious.wzrabbit@gmail.com>
2023-05-16 11:38:43 +0900

    ✨ feat: RectangleQuantityInput 컴포넌트 구현
    - 직사각형 형태의 수량 stepper

---
+++ src/components/RectangleQuantityInput/RectangleQuantityInput.tsx
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+import { AiFillCaretUp, AiFillCaretDown } from "react-icons/ai";
+import useQuantityUpdater from "../../hooks/useQuantityUpdater";
+import type { ProductQuantityInputProps } from "../../types";
+
+const RectangleQuantityInput = ({
+  productId,
+  step = 1,
+}: ProductQuantityInputProps) => {
+  const { inputValue, setIsFocused, updateInputValue, incrementInputValue } =
+    useQuantityUpdater({ productId: productId });
+
+  return (
+    <Container>
+      <Input
+        value={inputValue}
+        onChange={updateInputValue}
+        onBlur={() => setIsFocused(false)}
+      />
+      <IncreaseButton type="button" onClick={() => incrementInputValue(step)}>
+        <ArrowUpIcon />
+      </IncreaseButton>
+      <DecreaseButton type="button" onClick={() => incrementInputValue(-step)}>
+        <ArrowDownIcon />
+      </DecreaseButton>
+    </Container>
+  );
+};
+
+const colors = {
+  pureWhite: "#fff",
+  gold: "#ffdf7e",
+  darkGray: "#222",
+  pureBlack: "#000",
+};
+
+const Container = styled.div`
+  display: grid;
+  width: 114px;
+  height: 60px;
+  text-align: center;
+  grid-template-columns: 73px 41px;
+  grid-template-rows: 29px 29px;
+  grid-template-areas:
+    "input upButton"
+    "input downButton";
+
+  & * {
+    background-color: transparent;
+    color: ${colors.gold};
+  }
+`;
+
+const Input = styled.input`
+  border: 1px solid ${colors.gold};
+  grid-area: input;
+  color: ${colors.gold};
+  font-size: 24px;
+  font-family: "Rubik";
+  text-align: center;
+`;
+
+const IncreaseButton = styled.button`
+  border: 1px solid ${colors.gold};
+  grid-area: upButton;
+`;
+
+const DecreaseButton = styled.button`
+  border: 1px solid ${colors.gold};
+  grid-area: downButton;
+`;
+
+const ArrowUpIcon = styled(AiFillCaretUp)``;
+
+const ArrowDownIcon = styled(AiFillCaretDown)``;
+
+export default RectangleQuantityInput;
---
+++ src/stories/RectangleQuantityInput.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import RectangleQuantityInput from "../components/RectangleQuantityInput/RectangleQuantityInput";
+
+/**
+ * `RectangleQuantityInput` 컴포넌트는 품목의 수량을 지정할 수 있는 인풋 형태의 컴포넌트입니다.
+ * 수량과 관계없이 인풋을 보여줍니다.
+ */
+
+const meta = {
+  title: "RectangleQuantityInput",
+  component: RectangleQuantityInput,
+} satisfies Meta<typeof RectangleQuantityInput>;
+
+type Story = StoryObj<typeof meta>;
+
+export const Standard: Story = {
+  args: { productId: 8 },
+};
+
+export default meta;
