e5f71b771b03d4922be9ca7658233e2d6145e519
2yunseong <dbsdltjd3701@naver.com>
2023-05-16 19:22:20 +0900

    feat: Error Boundary Fallback 컴포넌트 개선 중

--- src/components/common/ErrorBoundary.tsx
+++ src/components/common/ErrorBoundary.tsx
@@ -37,7 +37,7 @@ const ErrorWrapper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-
+  ${({ theme }) => theme.fonts.warringMessage}
   width: 100%;
   height: 100vh;
 `;
