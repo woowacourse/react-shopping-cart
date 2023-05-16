92426d3980389bd2c7340871efacb7559a181275
Suyoung <jsy8492@gmail.com>
2023-05-16 21:10:27 +0900

    refactor: CartListItem 컴포넌트 스타일 수정

--- src/components/cart/CartListItem/CartListItem.tsx
+++ src/components/cart/CartListItem/CartListItem.tsx
@@ -23,44 +23,48 @@ const CartListItem = (cartItem: CartItem) => {
 
   return (
     <Container>
-      <Checkbox />
-
-      <Image src={imageSrc} loading="lazy" alt={name} />
-      <Title>{name}</Title>
-      <Right>
-        <DeleteButton type="button" aria-label="장바구니에서 삭제하기">
-          <svg
-            fill="none"
-            width="24"
-            height="24"
-            shapeRendering="geometricPrecision"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="1.5"
-            viewBox="0 0 24 24"
-          >
-            <path d="M3 6h18" />
-            <path d="M19 6v14a2 2 0 01-2 2H7a2 2 0 01-2-2V6m3 0V4a2 2 0 012-2h4a2 2 0 012 2v2" />
-            <path d="M10 11v6" />
-            <path d="M14 11v6" />
-          </svg>
-        </DeleteButton>
-        <Counter
-          count={quantity}
-          onChange={handleChangeQuantity}
-          onBlur={handleRemoveProduct}
-        />
-        <Price>{formatPrice(price)}</Price>
-      </Right>
+      <Inner>
+        <Checkbox />
+        <Image src={imageSrc} loading="lazy" alt={name} />
+        <Title>{name}</Title>
+        <Right>
+          <DeleteButton type="button" aria-label="장바구니에서 삭제하기">
+            <svg
+              fill="none"
+              width="24"
+              height="24"
+              shapeRendering="geometricPrecision"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="1.5"
+              viewBox="0 0 24 24"
+            >
+              <path d="M3 6h18" />
+              <path d="M19 6v14a2 2 0 01-2 2H7a2 2 0 01-2-2V6m3 0V4a2 2 0 012-2h4a2 2 0 012 2v2" />
+              <path d="M10 11v6" />
+              <path d="M14 11v6" />
+            </svg>
+          </DeleteButton>
+          <Counter
+            count={quantity}
+            onChange={handleChangeQuantity}
+            onBlur={handleRemoveProduct}
+          />
+          <Price>{formatPrice(price)}</Price>
+        </Right>
+      </Inner>
     </Container>
   );
 };
 
-const Container = styled.div`
+const Container = styled.li`
+  height: 203px;
+`;
+
+const Inner = styled.div`
   display: flex;
-  width: 735px;
-  height: 180px;
+  padding-top: 23px;
 `;
 
 const Title = styled.p`
@@ -78,7 +82,7 @@ const Right = styled.div`
   align-items: flex-end;
   justify-content: space-between;
   height: 145px;
-  margin-left: 155px;
+  margin-left: auto;
 `;
 
 const DeleteButton = styled.button`
