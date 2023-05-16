784d7da91aef0a26b9e14ef72aafbbfe4d554c76
Gilpop8663 <wolfye@naver.com>
2023-05-16 17:10:30 +0900

    feat: useAtomLocalStorage 구현
    새로 고침 시 아톰의 기본값을 설정할 수 있고 아톰과 로컬스토리지의 데이터를 같이 상태 관리하는 훅입니다

---
+++ src/hooks/useAtomLocalStorage.ts
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+import { RecoilState, useRecoilState } from 'recoil';
+
+const useAtomLocalStorage = <T>(
+  initialValue: RecoilState<T>,
+  key: string
+): [T, (data: T) => void] => {
+  const [value, setData] = useRecoilState(initialValue);
+
+  const setValue = (data: T) => {
+    setData(data);
+    localStorage.setItem(key, JSON.stringify(data));
+  };
+
+  useEffect(() => {
+    const saveData = JSON.parse(localStorage.getItem(key) || '[]');
+
+    if (saveData && saveData.length > 0) {
+      setData(saveData);
+    }
+  }, [key, setData]);
+
+  return [value, setValue];
+};
+
+export default useAtomLocalStorage;
