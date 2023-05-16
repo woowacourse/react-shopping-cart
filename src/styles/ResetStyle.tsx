a41c9025441aa10c0d29487e2eda504410109247
SangwonKang <72087183+ksone02@users.noreply.github.com>
2023-05-16 19:01:30 +0900

    refactor: ResetStyle 주석 개행

--- src/styles/ResetStyle.tsx
+++ src/styles/ResetStyle.tsx
@@ -1,6 +1,7 @@
 import { Global, css } from '@emotion/react';
 
 // 출처: https://velog.io/@teo/2022-CSS-Reset-%EB%8B%A4%EC%8B%9C-%EC%8D%A8%EB%B3%B4%EA%B8%B0
+
 const style = css`
   * {
     margin: 0;
