2a5ddf3a09a3624578e7942028e2d4a9a84e23ee
inyeong-kang <tomatorkd@naver.com>
2023-05-17 17:38:39 +0900

    chore: 컴포넌트 경로 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+export { default as Counter } from './leafs/Counter/Counter';
 export { default as Header } from './leafs/Header/Header';
 export { default as Product } from './leafs/Product/Product';
 export { default as ProductList } from './trees/ProductList/ProductList';
