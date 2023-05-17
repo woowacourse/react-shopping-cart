698538215273037930446290ddb860c60f1f13a8
semnil5202 <semnil5202@gmail.com>
2023-05-17 16:07:38 +0900

    feat(CartList): '장바구니' 타이틀을 표시

--- src/pages/CartList.tsx
+++ src/pages/CartList.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { Typography as Title } from '../ui/Typography';
+import * as Styled from './styles/CartList.styles';
 
 export const CartList = () => {
-  return <div>CartList</div>;
+  return (
+    <Styled.Wrapper>
+      <Styled.TitleWrapper>
+        <Title size="28px" weight="600">
+          장바구니
+        </Title>
+      </Styled.TitleWrapper>
+    </Styled.Wrapper>
+  );
 };
---
+++ src/pages/styles/CartList.styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.section`
+  margin: 60px auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const TitleWrapper = styled.div`
+  width: 1200px;
+  height: 68px;
+  border-bottom: 1px solid #333333;
+  display: flex;
+  justify-content: center;
+  align-items: start;
+`;
