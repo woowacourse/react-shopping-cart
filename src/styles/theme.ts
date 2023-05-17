2ceb0a4dd1e5dbb8833679516fda68caa7141a51
yogjin <youngjin988@gmail.com>
2023-05-17 10:29:30 +0900

    design: gray color palette 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,18 @@
 const colors = {
   primary: '#2AC1BC',
   white: '#ffffff',
-  gray_3: '#333333',
+  gray: {
+    warm_6: '#1E1E25',
+    warm_5: '#3B3B46',
+    warm_4: '#585865',
+    warm_3: '#747481',
+    warm_2: '#93939F',
+    warm_1: '#D6D6DD',
+    warm_0: '#F9FAFC',
+    scale_2: '#9BA0A9',
+    scale_1: '#E2E4E6',
+    scale_0: '#F2F2F5',
+  },
 };
 
 const theme = { colors };
