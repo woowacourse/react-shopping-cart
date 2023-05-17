9573dbad781a0ace386eb4026c875ad51aa460f1
Q Kim <qoli0703@gmail.com>
2023-05-17 19:48:37 +0900

    test: 스토리북에서 msw 사용가능하게 설정

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,20 @@ import type { Preview } from '@storybook/react';
 import { withThemeFromJSXProvider } from '@storybook/addon-styling';
 
 import GlobalStyles from '../src/GlobalStyles';
+import { worker } from '../src/mocks/browser';
+
+(async () => {
+  if (window.location.pathname === '/react-shopping-cart') {
+    window.location.pathname += '/';
+    return;
+  }
+
+  await worker.start({
+    serviceWorker: {
+      url: '/mockServiceWorker.js',
+    },
+  });
+})();
 
 const preview: Preview = {
   parameters: {
