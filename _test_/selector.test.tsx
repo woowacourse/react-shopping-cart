2fff75fea5652226ac635ce6c02eae28ff445dc7
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-05-17 21:37:48 +0900

    test : Selector의 OptionContent컴포넌트 랜더링 테스트 작성

---
+++ _test_/selector.test.tsx
@@ -0,0 +1,23 @@
+import {
+  cleanup,
+  queryByTestId,
+  queryByText,
+  render,
+  screen,
+} from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { OptionContent } from "../src/components/common/selector/optionContent";
+import React from "react";
+
+/**
+ * @jest-environment jsdom
+ */
+
+describe("Selector Option의 Content 랜더링 테스트", () => {
+  test("custom된 children요소가 없으면 기본 스타일의 Selector Option Content가 랜더링 된다", () => {
+    render(<OptionContent />);
+    const content = screen.getByTestId("content");
+
+    expect(content).toBeInTheDocument();
+  });
+});
