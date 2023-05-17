24159510f234722d8eefab89e00d5d819824d32c
hozzijeong <ghwls0905@naver.com>
2023-05-17 18:47:37 +0900

    feat: cartList 페이지 파일 생성 및 라우터 추가

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,19 @@
 import { createBrowserRouter } from 'react-router-dom';
+import CartList from 'src/pages/CartList';
 import ProductList from 'src/pages/ProductList';
 
-const CartRouter = createBrowserRouter([
-  {
-    path:"/",
-    element:<ProductList/>
-
-  },
-],{basename:process.env.PUBLIC_URL})
+const CartRouter = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <ProductList />,
+    },
+    {
+      path: '/cart-list',
+      element: <CartList />,
+    },
+  ],
+  { basename: process.env.PUBLIC_URL }
+);
 
 export default CartRouter;
