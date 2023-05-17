15be29d1375d8f9447ea004a981533d3618402b7
kangyeongmin <kym6050@hanyang.ac.kr>
2023-05-17 13:11:56 +0900

    feat: 상품 목록 fetch 구현

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,11 +1,32 @@
+import { useEffect, useState } from "react";
+import { useRecoilState, useSetRecoilState } from "recoil";
 import { Header, Page, ProductList } from "../components";
+import { productsState } from "../recoil/atom";
 
 const Main = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const setProducts = useSetRecoilState(productsState);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("*/api/products");
+        const data = await response.json();
+        setProducts(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <Header />
       <Page>
-        <ProductList />
+        {isLoading ? <div>상품 목록 불러오는 중...</div> : <ProductList />}
       </Page>
     </>
   );
