1a8735bd5bbe35d2e4fbbf8ad64af358da0e6c10
Dain Lee <feb.dain@gmail.com>
2023-05-17 11:23:56 +0900

    feat: 장바구니에 담긴 총 상품 가격 계산하는 기능 구현

--- src/components/cart/Order.tsx
+++ src/components/cart/Order.tsx
@@ -1,19 +1,28 @@
+import { useRecoilValue } from 'recoil';
 import { css, styled } from 'styled-components';
+import { cartState } from '../../recoil';
+import { CartItem } from '../../types';
 import Button from '../common/Button';
 
 const Order = () => {
+  const cart = useRecoilValue(cartState);
+
+  const totalPrice = cart.reduce((acc: 0, cur: CartItem) => {
+    return acc + cur.product.price * cur.quantity;
+  }, 0);
+
   return (
     <S.Wrapper>
       <S.Title>결제예상금액</S.Title>
       <ul>
         <S.List>
-          총 상품가격<span>21,700원</span>
+          총 상품가격<span>{`${totalPrice.toLocaleString()}원`}</span>
         </S.List>
         <S.List>
           총 배송비 <span>3,000원</span>
         </S.List>
         <S.List>
-          총 주문금액<span>24,700원</span>
+          총 주문금액<span>{`${(totalPrice + 3000).toLocaleString()}원`}</span>
         </S.List>
       </ul>
       <Button css={orderButtonStyle}>주문하기</Button>
