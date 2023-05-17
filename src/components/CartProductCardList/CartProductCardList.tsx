a49b207200e998caeee62309f53160a602473968
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-05-17 20:13:47 +0900

    feat(CartProductCardList): 컴포넌트 구현

---
+++ src/components/CartProductCardList/CartProductCardList.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import CartProductCard from './CartProductCard/CartProductCard';
+import { useRecoilValue } from 'recoil';
+import { cartProductsState } from 'state/cartProducts';
+
+const CartProductCardList = () => {
+  const cartProducts = useRecoilValue(cartProductsState);
+
+  return (
+    <CartProductFlex>
+      {[...cartProducts.entries()].map(([id, cartProduct]) => (
+        <CartProductCard key={id} cartProduct={cartProduct} />
+      ))}
+    </CartProductFlex>
+  );
+};
+
+export default CartProductCardList;
+
+const CartProductFlex = styled.ul`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  border-top: 3px solid #dddddd;
+`;
