dddb718ebf548be3d7d7857e5187fe2bbd571df3
hozzijeong <ghwls0905@naver.com>
2023-05-17 18:47:59 +0900

    feat: 헤더 로고 및 장바구니 글씨 클릭 시에 해당하는 경로로 이동 구현

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import { useRecoilValue } from 'recoil';
 import { countCartListSelector } from 'src/recoil/cartList';
 import * as S from './Header.styles';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   const cartCount = useRecoilValue(countCartListSelector);
@@ -8,13 +9,17 @@ const Header = () => {
   return (
     <S.HeaderContainer>
       <S.HeaderContentContainer>
-        <S.HeaderWrapper gap={20}>
-          <S.Logo>THE CHOONSIK</S.Logo>
-        </S.HeaderWrapper>
-        <S.HeaderWrapper gap={8}>
-          <S.CartTitle>장바구니</S.CartTitle>
-          <S.CartCounter>{cartCount}</S.CartCounter>
-        </S.HeaderWrapper>
+        <Link to="/">
+          <S.HeaderWrapper gap={20}>
+            <S.Logo>THE CHOONSIK</S.Logo>
+          </S.HeaderWrapper>
+        </Link>
+        <Link to="cart-list">
+          <S.HeaderWrapper gap={8}>
+            <S.CartTitle>장바구니</S.CartTitle>
+            <S.CartCounter>{cartCount}</S.CartCounter>
+          </S.HeaderWrapper>
+        </Link>
       </S.HeaderContentContainer>
     </S.HeaderContainer>
   );
