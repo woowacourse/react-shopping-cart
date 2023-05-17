b38588b1f8073c5a41c914114bf0682b4b657ab2
solo5star <solo_5@naver.com>
2023-05-17 19:26:56 +0900

    test: fixture 데이터  추가

---
+++ src/mocks/fixtures/cart.ts
@@ -0,0 +1,42 @@
+import type { CartItem, Product } from '../../type';
+
+type CartResponse = Array<
+  Omit<CartItem, 'productId'> & {
+    product: Product;
+  }
+>;
+
+const cart: Omit<CartResponse, 'productId'> = [
+  {
+    id: 1,
+    quantity: 5,
+    product: {
+      id: 2,
+      name: 'PET보틀-밀크티(370ml)',
+      price: 73400,
+      imageUrl: './images/products/2.png',
+    },
+  },
+  {
+    id: 2,
+    quantity: 1,
+    product: {
+      id: 8,
+      name: 'PET보틀-원형(600ml)',
+      price: 44500,
+      imageUrl: './images/products/8.png',
+    },
+  },
+  {
+    id: 3,
+    quantity: 3,
+    product: {
+      id: 10,
+      name: 'PET보틀-밀크티(370ml)',
+      price: 73400,
+      imageUrl: './images/products/10.png',
+    },
+  },
+];
+
+export default cart;
