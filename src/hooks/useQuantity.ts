817e30cce3e85df25a71e623dd9c8a8775427f94
kangyeongmin <kym6050@hanyang.ac.kr>
2023-05-17 13:11:31 +0900

    refactor: 수량 변경 로직 보완

--- src/hooks/useQuantity.ts
+++ src/hooks/useQuantity.ts
@@ -5,40 +5,46 @@ import { ProductType } from "../types/domain";
 import { cartProductsSelector } from "../recoil/selector";
 import {
   KEY_LOCALSTORAGE_CART,
+  MAX_LENGTH_QUANTITY,
   MAX_QUANTITY,
   MIN_QUANTITY,
 } from "../constants";
+import { setLocalStorage } from "../utils";
 
-export const useQuantity = (ProductId: number) => {
-  const totalProducts = useRecoilValue(productsState);
+export const useQuantity = (productId: number) => {
+  const products = useRecoilValue(productsState);
   const [cartProducts, setCartProducts] = useRecoilState(cartProductsSelector);
+
   const [quantity, setQuantity] = useState<string>(
-    totalProducts.find((Product: ProductType) => Product.id === ProductId)
-      .quantity
+    products.find((product: ProductType) => product.id === productId).quantity
   );
 
   useEffect(() => {
-    localStorage.setItem(KEY_LOCALSTORAGE_CART, JSON.stringify(cartProducts));
+    setLocalStorage(KEY_LOCALSTORAGE_CART, cartProducts);
   }, [cartProducts]);
 
   const setNewQuantity = (newQuantity: number) => {
     if (newQuantity > MAX_QUANTITY || newQuantity < MIN_QUANTITY) return;
 
     setQuantity(newQuantity.toString());
-    setCartProducts({ id: ProductId, quantity: newQuantity.toString() });
+    setCartProducts({ id: productId, quantity: newQuantity.toString() });
   };
 
   const handleQuantityChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value.length > MAX_LENGTH_QUANTITY) return;
+
     setQuantity(e.target.value);
   };
 
   const handleQuantityBlured = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (quantity === "" || Number(quantity) < MIN_QUANTITY)
+    if (
+      e.target.value === "" ||
+      e.target.value === "-0" ||
+      Number(quantity) < MIN_QUANTITY
+    )
       e.target.value = MIN_QUANTITY.toString();
-    if (Number(quantity) > MAX_QUANTITY)
-      e.target.value = MAX_QUANTITY.toString();
 
-    setCartProducts({ id: ProductId, quantity: e.target.value });
+    setCartProducts({ id: productId, quantity: e.target.value });
   };
 
   return {
