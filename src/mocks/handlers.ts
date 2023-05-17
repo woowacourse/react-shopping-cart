056e13dc81eaf7f95b1012583dcf46fec56cc301
hozzijeong <ghwls0905@naver.com>
2023-05-17 16:59:57 +0900

    feat: 상품 조회 API 추가

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -4,9 +4,19 @@ import json from './productList.json';
 const products = json.choonsikProducts;
 
 export const handlers = [
-  // 상품 조회 API
+  // 상품 목록 조회 API
   rest.get('/api/products', (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(products));
   }),
+
+  // 상품 조회 API
+  rest.get('/api/products/:productId}', (req, res, ctx) => {
+    const { productId } = req.params;
+
+    const product = products.find(({ id }) => id === Number(productId)) ?? null;
+
+    return res(product ? ctx.status(200) : ctx.status(401), ctx.json(product));
+  }),
   // 상품 추가 API
+  // rest.post("/")
 ];
