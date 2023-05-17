c7ab81b3a4cfcc9e8b1afabfa436f5bd52b79a21
ukkodeveloper <ukkodeveloper@gmail.com>
2023-05-17 21:38:18 +0900

    feat: cartList 상태 selectorfamily로 구현 및 적용

--- src/components/CartQuantityField/CartQuantityField.tsx
+++ src/components/CartQuantityField/CartQuantityField.tsx
@@ -4,49 +4,39 @@ import * as S from './CartQuantityField.style';
 import * as T from '../../types/ProductType';
 
 import QuantityCounter from '../common/QuantityCounter';
-import useCartList from '../../hooks/useCartList';
 import cartIcon from '../../assets/cart.svg';
-
-import { MAX_CART_QUANTITY, MIN_CART_QUANTITY } from '../../constants/cartConstants';
+import { useCartItemById } from '../../hooks/cartListState/cartListState';
 
 interface CartQuantityFieldProps {
   product: T.ProductItem;
 }
 
 function CartQuantityField({ product }: CartQuantityFieldProps) {
-  const { addCart, increaseCart, getQuantityByProductId, decreaseCart, setCartQuantity } =
-    useCartList();
+  const [quantity, setQuantity] = useCartItemById(product.id);
 
-  const quantity = getQuantityByProductId(product.id);
   const isQuantityZero = quantity > 0;
-
-  const handleChangeQuantity = (event: ChangeEvent<HTMLInputElement>) => {
-    const quantity = Number(event.target.value.replaceAll('/', '').replace(/\D/g, ''));
-
-    if (quantity >= MIN_CART_QUANTITY || quantity <= MAX_CART_QUANTITY) {
-      setCartQuantity(product.id, quantity);
-    }
-  };
 
   return (
     <S.ControllerWrapper>
       {isQuantityZero ? (
         <QuantityCounter
           quantity={quantity}
-          onChange={handleChangeQuantity}
+          onChange={(event) => {
+            setQuantity(Number(event.target.value));
+          }}
           onIncrease={() => {
-            increaseCart(product.id);
+            setQuantity(quantity + 1);
           }}
           onDecrease={() => {
-            decreaseCart(product.id);
+            setQuantity(quantity - 1);
           }}
-          ariaIncreaseLabel={`${product.name}의 장바구니에 담긴 개수인 ${quantity}에서 하나 더하기`}
-          ariaDecreaseLabel={`${product.name}의 장바구니에 담긴 개수인 ${quantity}에서 하나 빼기`}
+          ariaIncreaseLabel={`${product.name}의 장바구니에 담긴 개수에서 하나 더하기`}
+          ariaDecreaseLabel={`${product.name}의 장바구니에 담긴 개수에서 하나 빼기`}
         />
       ) : (
         <S.CartIcon
           onClick={() => {
-            addCart(product);
+            setQuantity(1);
           }}
           type="button"
           aria-label={`${product.name}를 장바구니에 담기`}
---
+++ src/hooks/cartListState/cartListState.ts
@@ -0,0 +1,82 @@
+import { DefaultValue, atom, selector, selectorFamily, useRecoilState } from 'recoil';
+import { CartItem, CartItemWithProduct } from '../../types/ProductType';
+
+const cartListState = atom<CartItem[]>({
+  key: 'cartListState',
+  default: selector({
+    key: 'cartListState/default',
+    get: async () => {
+      const response = await fetch('/cart-items');
+      const cartProducts: CartItemWithProduct[] = await response.json();
+      const cartItems: CartItem[] = cartProducts.map((productInCart) => ({
+        id: productInCart.id,
+        quantity: productInCart.quantity,
+      }));
+
+      return cartItems;
+    },
+  }),
+});
+
+const cartItemState = selectorFamily<number, number>({
+  key: 'cartItemState',
+  get:
+    (id) =>
+    ({ get }) => {
+      const cartList = get(cartListState);
+      const cartProduct = cartList.filter((cartProduct) => cartProduct.id === id)[0];
+      return cartProduct?.quantity ?? 0;
+    },
+  set:
+    (id) =>
+    ({ get, set }, quantity) => {
+      // TODO: 분리하기
+
+      if (!(quantity instanceof DefaultValue) && quantity < 99 && quantity >= 0) {
+        const cartList = get(cartListState);
+
+        // TODO: add
+        if (!cartList.some((item) => item.id === id)) {
+          set(cartListState, (prevCartList) => [
+            ...prevCartList,
+            {
+              id,
+              quantity: 1,
+            },
+          ]);
+
+          return;
+        }
+
+        // TODO: delete
+        if (quantity === 0) {
+          set(cartListState, (prevCartList) => prevCartList.filter((item) => item.id !== id));
+
+          return;
+        }
+
+        // TODO: post
+        set(cartListState, (prevCartList) => {
+          return prevCartList.map((item) => {
+            if (item.id === id) {
+              return {
+                ...item,
+                quantity,
+              };
+            } else {
+              return item;
+            }
+          });
+        });
+      }
+    },
+});
+
+// fetch(`/cart-items/`, {
+//   method: 'POST',
+//   body: JSON.stringify({
+//     id,
+//   }),
+// });
+
+export const useCartItemById = (id: number) => useRecoilState(cartItemState(id));
