099c83118064b1908aa16698e2ba39ddb1dfa95b
inyeong-kang <tomatorkd@naver.com>
2023-05-17 13:40:36 +0900

    test: NotFound story 구현

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,6 @@ export { default as Product } from './leafs/Product/Product';
 export { default as ProductList } from './trees/ProductList/ProductList';
 export { default as Root } from './pages/Root';
 export { default as Error } from './pages/Error';
-export { default as NotFound } from './pages/NotFound';
+export { default as NotFound } from './pages/NotFound/NotFound';
 export { default as ProductListPage } from './pages/ProductListPage';
 export { default as CartListPage } from './pages/CartListPage';
