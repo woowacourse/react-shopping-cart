1cda80a5504190c96a4b3b298b158f8bc03d614b
TaeeunKim <xodms0309@naver.com>
2023-05-17 13:50:02 +0900

    design: theme에 색상 추가 및 폰트 네이밍 변경

--- src/styles/styles.d.ts
+++ src/styles/styles.d.ts
@@ -5,11 +5,13 @@ declare module 'styled-components' {
       white: string;
       primary: string;
       secondary: string;
+      gray: string;
     };
     font: {
-      product: string;
       header: string;
-      price: string;
+      small: string;
+      medium: string;
+      large: string;
     };
   }
 }
