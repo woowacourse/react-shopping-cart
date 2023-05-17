c52996e599cad8cd2924544e5a012f6056fbf9af
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-05-17 09:01:08 +0900

    test(CartQuantityStepper): 스토리 추가

---
+++ src/components/CartQuantityStepper/CartQuantityStepper.stories.tsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import CartQuantityStepper from 'components/CartQuantityStepper/CartQuantityStepper';
+
+const meta = {
+  component: CartQuantityStepper,
+  title: 'CartQuantityStepper',
+  decorators: [
+    (Story) => (
+      <div style={{ width: '50px', height: '50px', position: 'relative' }}>
+        <Story />
+      </div>
+    ),
+  ],
+  argTypes: {
+    decreaseQuantity: {
+      description: '- 버튼을 눌렀을 때 실행 될 함수입니다.',
+    },
+    increaseQuantity: {
+      description: '+ 버튼을 눌렀을 때 실행 될 함수입니다.',
+    },
+    initialIncrement: {
+      description: '장바구니 icon을 눌렀을 때 실행 될 함수입니다.',
+    },
+    quantity: {
+      description: '장바구니에 담긴 상품의 수량입니다.',
+    },
+    $position: {
+      description: 'stepper의 랜더링 위치를 나타내는 객체입니다.',
+    },
+  },
+} satisfies Meta<typeof CartQuantityStepper>;
+
+export default meta;
+
+type Story = StoryObj<typeof CartQuantityStepper>;
+
+const CartQuantityStepperWithHooks = () => {
+  const [quantity, setQuantity] = useState(0);
+
+  const increaseQuantity = () => {
+    setQuantity((prevQuantity) => prevQuantity + 1);
+  };
+
+  const decreaseQuantity = () => {
+    setQuantity((prevQuantity) => prevQuantity - 1);
+  };
+
+  return (
+    <CartQuantityStepper
+      quantity={quantity}
+      initialIncrement={increaseQuantity}
+      decreaseQuantity={decreaseQuantity}
+      increaseQuantity={increaseQuantity}
+      $position={{ $left: '10px', $top: '10px' }}
+    ></CartQuantityStepper>
+  );
+};
+
+export const Default: Story = {
+  render: () => <CartQuantityStepperWithHooks />,
+};
