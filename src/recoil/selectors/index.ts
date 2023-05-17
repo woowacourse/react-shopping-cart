7ca2a67bf5bd472a23fc8c1cd27e0baf663750b6
Suyoung <jsy8492@gmail.com>
2023-05-17 15:12:37 +0900

    feat: 장바구니 목록 selector 작성

--- src/recoil/selectors/index.ts
+++ src/recoil/selectors/index.ts
@@ -1,7 +1,7 @@
 import { selector, selectorFamily } from 'recoil';
 import { cartState } from '../atoms';
-import { Product } from '../../types/product';
-import { PRODUCTS_BASE_URL } from '../../constants';
+import { CartItem, Product } from '../../types/product';
+import { CART_BASE_URL, PRODUCTS_BASE_URL } from '../../constants';
 
 export const productListQuery = selector<Product[]>({
   key: 'productList',
@@ -15,6 +15,21 @@ export const productListQuery = selector<Product[]>({
     const products = await response.json();
 
     return products;
+  },
+});
+
+export const cartItemsQuery = selector<CartItem[]>({
+  key: 'cartItems',
+  get: async () => {
+    const response = await fetch(CART_BASE_URL);
+
+    if (!response.ok) {
+      throw new Error('장바구니 목록을 불러올 수 없습니다.');
+    }
+
+    const cartItems = await response.json();
+
+    return cartItems;
   },
 });
 
