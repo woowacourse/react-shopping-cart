0a1273799bb7dc90d58a0f231e21928f8f579c41
ukkodeveloper <ukkodeveloper@gmail.com>
2023-05-17 14:37:49 +0900

    feat: productList 실제 api fetch 로 변경

--- src/hooks/useLoadProductList.ts
+++ src/hooks/useLoadProductList.ts
@@ -1,17 +1,15 @@
 import { useCallback } from 'react';
 
-import * as T from '../types/ProductType';
 import useSetProductList from './useSetProductList';
-import mockApi from '../api/mockApi';
-import safeJsonParse from '../utils/safeJsonParse';
 
 const useLoadProductList = () => {
   const setProductList = useSetProductList();
 
   const loadProductList = useCallback(async () => {
-    const response = await mockApi('/products');
+    const response = await fetch('/products');
+    const productList = await response.json();
 
-    setProductList(safeJsonParse<T.ProductItem[]>(response.data) ?? []);
+    setProductList(productList);
   }, [setProductList]);
 
   return { loadProductList };
