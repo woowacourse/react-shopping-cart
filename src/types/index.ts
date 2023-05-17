2bcb1d0ced2e3ed92845b43b2ea2eaa9bddfc2ff
wzrabbit <curious.wzrabbit@gmail.com>
2023-05-17 19:45:07 +0900

    ✨ feat: 타입 파라미터 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,11 +3,14 @@ interface ProductCardProps {
   productImage: string;
   productName: string;
   productPrice: number;
+  productQuantity: number;
 }
 
 interface ProductQuantityInputProps {
+  initialValue: number;
   productId: number;
   step?: number;
+  notifyFunction?: (value: number) => void;
 }
 
 export type { ProductCardProps, ProductQuantityInputProps };
