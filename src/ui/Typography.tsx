b8bc988bbbf4357b42a9bd13b244f5e391130029
semnil5202 <semnil5202@gmail.com>
2023-05-17 11:09:12 +0900

    refactor(Typography): prop default value 지정
    기존의 default props 변경에서 default value를 지정하는 방식으로 변경한다.

--- src/ui/Typography.tsx
+++ src/ui/Typography.tsx
@@ -7,16 +7,15 @@ interface TextProps {
   color?: string;
 }
 
-export const Typography = ({ children, size, weight, color }: TextProps) => {
+export const Typography = ({
+  children,
+  size = '16px',
+  weight = 'normal',
+  color = '#333333',
+}: TextProps) => {
   return (
     <Styled.Paragraph size={size} weight={weight} color={color}>
       {children}
     </Styled.Paragraph>
   );
-};
-
-Typography.defaultProps = {
-  size: '16px',
-  weight: 'normal',
-  color: '#333333',
 };
