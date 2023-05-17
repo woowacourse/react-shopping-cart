50e62a6e7cfc53a459fc9b0f630a1e9e60169057
yogjin <youngjin988@gmail.com>
2023-05-18 01:03:41 +0900

    refactor: 장바구니 페이지 마크업 수정

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -18,114 +18,111 @@ export const CartPage = () => {
         </FlexBox>
         <CartListTitle>배송 상품 (3개)</CartListTitle>
       </CartListTitleWrapper>
-
       <FlexBox justify="space-around" gap="70px">
-        <FlexBox direction="column">
-          <FlexBox direction="column" width="600px" margin="10px 0">
-            <CartProductCardList
-              cartProducts={[
-                {
+        <FlexBox direction="column" width="600px" margin="10px 0">
+          <CartProductCardList
+            cartProducts={[
+              {
+                id: 1,
+                quantity: 1,
+                product: {
                   id: 1,
-                  quantity: 1,
-                  product: {
-                    id: 1,
-                    name: '순살치킨 해마로 1kg 냉동',
-                    price: 10800,
-                    imageUrl:
-                      'https://cdn-mart.baemin.com/sellergoods/main/28786eaa-d9f0-456c-b318-07236fe17ab2.jpg?h=400&w=400',
-                  },
+                  name: '순살치킨 해마로 1kg 냉동',
+                  price: 10800,
+                  imageUrl:
+                    'https://cdn-mart.baemin.com/sellergoods/main/28786eaa-d9f0-456c-b318-07236fe17ab2.jpg?h=400&w=400',
                 },
-                {
+              },
+              {
+                id: 2,
+                quantity: 2,
+                product: {
                   id: 2,
-                  quantity: 2,
-                  product: {
-                    id: 2,
-                    name: '사조오양 치킨텐더 1000gx1개',
-                    price: 9900,
-                    imageUrl:
-                      'https://cdn-mart.baemin.com/sellergoods/main/f9923d11-5ba9-4301-a73c-fc4817544f6a.jpg?h=400&w=400',
-                  },
+                  name: '사조오양 치킨텐더 1000gx1개',
+                  price: 9900,
+                  imageUrl:
+                    'https://cdn-mart.baemin.com/sellergoods/main/f9923d11-5ba9-4301-a73c-fc4817544f6a.jpg?h=400&w=400',
                 },
-                {
+              },
+              {
+                id: 3,
+                quantity: 3,
+                product: {
                   id: 3,
-                  quantity: 3,
-                  product: {
-                    id: 3,
-                    name: '사세통상 순살치킨가라아게(냉동 1kg/ea)',
-                    price: 10000,
-                    imageUrl:
-                      'https://cdn-mart.baemin.com/sellergoods/main/ca728030-5e96-45f7-aa25-b7ebc0a1de7a.jpg?h=400&w=400',
-                  },
+                  name: '사세통상 순살치킨가라아게(냉동 1kg/ea)',
+                  price: 10000,
+                  imageUrl:
+                    'https://cdn-mart.baemin.com/sellergoods/main/ca728030-5e96-45f7-aa25-b7ebc0a1de7a.jpg?h=400&w=400',
                 },
-                {
+              },
+              {
+                id: 1,
+                quantity: 1,
+                product: {
                   id: 1,
-                  quantity: 1,
-                  product: {
-                    id: 1,
-                    name: '순살치킨 해마로 1kg 냉동',
-                    price: 10800,
-                    imageUrl:
-                      'https://cdn-mart.baemin.com/sellergoods/main/28786eaa-d9f0-456c-b318-07236fe17ab2.jpg?h=400&w=400',
-                  },
+                  name: '순살치킨 해마로 1kg 냉동',
+                  price: 10800,
+                  imageUrl:
+                    'https://cdn-mart.baemin.com/sellergoods/main/28786eaa-d9f0-456c-b318-07236fe17ab2.jpg?h=400&w=400',
                 },
-                {
+              },
+              {
+                id: 2,
+                quantity: 2,
+                product: {
                   id: 2,
-                  quantity: 2,
-                  product: {
-                    id: 2,
-                    name: '사조오양 치킨텐더 1000gx1개',
-                    price: 9900,
-                    imageUrl:
-                      'https://cdn-mart.baemin.com/sellergoods/main/f9923d11-5ba9-4301-a73c-fc4817544f6a.jpg?h=400&w=400',
-                  },
+                  name: '사조오양 치킨텐더 1000gx1개',
+                  price: 9900,
+                  imageUrl:
+                    'https://cdn-mart.baemin.com/sellergoods/main/f9923d11-5ba9-4301-a73c-fc4817544f6a.jpg?h=400&w=400',
                 },
-                {
+              },
+              {
+                id: 3,
+                quantity: 3,
+                product: {
                   id: 3,
-                  quantity: 3,
-                  product: {
-                    id: 3,
-                    name: '사세통상 순살치킨가라아게(냉동 1kg/ea)',
-                    price: 10000,
-                    imageUrl:
-                      'https://cdn-mart.baemin.com/sellergoods/main/ca728030-5e96-45f7-aa25-b7ebc0a1de7a.jpg?h=400&w=400',
-                  },
+                  name: '사세통상 순살치킨가라아게(냉동 1kg/ea)',
+                  price: 10000,
+                  imageUrl:
+                    'https://cdn-mart.baemin.com/sellergoods/main/ca728030-5e96-45f7-aa25-b7ebc0a1de7a.jpg?h=400&w=400',
                 },
-                {
+              },
+              {
+                id: 1,
+                quantity: 1,
+                product: {
                   id: 1,
-                  quantity: 1,
-                  product: {
-                    id: 1,
-                    name: '순살치킨 해마로 1kg 냉동',
-                    price: 10800,
-                    imageUrl:
-                      'https://cdn-mart.baemin.com/sellergoods/main/28786eaa-d9f0-456c-b318-07236fe17ab2.jpg?h=400&w=400',
-                  },
+                  name: '순살치킨 해마로 1kg 냉동',
+                  price: 10800,
+                  imageUrl:
+                    'https://cdn-mart.baemin.com/sellergoods/main/28786eaa-d9f0-456c-b318-07236fe17ab2.jpg?h=400&w=400',
                 },
-                {
+              },
+              {
+                id: 2,
+                quantity: 2,
+                product: {
                   id: 2,
-                  quantity: 2,
-                  product: {
-                    id: 2,
-                    name: '사조오양 치킨텐더 1000gx1개',
-                    price: 9900,
-                    imageUrl:
-                      'https://cdn-mart.baemin.com/sellergoods/main/f9923d11-5ba9-4301-a73c-fc4817544f6a.jpg?h=400&w=400',
-                  },
+                  name: '사조오양 치킨텐더 1000gx1개',
+                  price: 9900,
+                  imageUrl:
+                    'https://cdn-mart.baemin.com/sellergoods/main/f9923d11-5ba9-4301-a73c-fc4817544f6a.jpg?h=400&w=400',
                 },
-                {
+              },
+              {
+                id: 3,
+                quantity: 3,
+                product: {
                   id: 3,
-                  quantity: 3,
-                  product: {
-                    id: 3,
-                    name: '사세통상 순살치킨가라아게(냉동 1kg/ea)',
-                    price: 10000,
-                    imageUrl:
-                      'https://cdn-mart.baemin.com/sellergoods/main/ca728030-5e96-45f7-aa25-b7ebc0a1de7a.jpg?h=400&w=400',
-                  },
+                  name: '사세통상 순살치킨가라아게(냉동 1kg/ea)',
+                  price: 10000,
+                  imageUrl:
+                    'https://cdn-mart.baemin.com/sellergoods/main/ca728030-5e96-45f7-aa25-b7ebc0a1de7a.jpg?h=400&w=400',
                 },
-              ]}
-            />
-          </FlexBox>
+              },
+            ]}
+          />
         </FlexBox>
         <FlexBox margin="30px 0" style={{ position: 'sticky', top: '95px' }}>
           <ProductOrderTable onClickOrderButton={() => {}} />
