53172997733ff584e04cafe72ab35ca8d593466d
chex <chex1004@gmail.com>
2023-05-17 11:29:54 +0900

    fix: exhaustive-deps-warning 해결

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,17 +1,17 @@
-import { Dispatch, SetStateAction, useEffect } from 'react';
+import { Dispatch, SetStateAction, useCallback, useEffect } from 'react';
 import { SetterOrUpdater } from 'recoil';
 import { fetchApi } from '../api';
 
 type SetData<T> = SetterOrUpdater<T> | Dispatch<SetStateAction<T>>;
 
 export const useSetFetchedData = <T>(url: string, setData: SetData<T>) => {
-  const setFetchedData = async () => {
+  const setFetchedData = useCallback(async () => {
     const rawData = await fetchApi(url);
     setData(rawData);
-  };
+  }, [url, setData]);
 
   useEffect(() => {
     const timerId = setTimeout(() => setFetchedData(), 3000);
     return () => clearTimeout(timerId);
-  }, [url, setData]);
+  }, [setFetchedData]);
 };
