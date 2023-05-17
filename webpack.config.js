4939a37cf7aa463939ab4807e3d57ed496037794
inyeong-kang <tomatorkd@naver.com>
2023-05-17 12:35:27 +0900

    chore: webpack 경로 설정

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
   },
   devServer: {
     static: {
@@ -41,6 +42,7 @@ module.exports = {
     },
     compress: true,
     port: 9000,
+    historyApiFallback: true,
   },
   mode: 'development',
 };
