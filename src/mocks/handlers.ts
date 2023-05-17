e3dfcadc848dcfd2cd90d3d99ff73558582ff886
ukkodeveloper <ukkodeveloper@gmail.com>
2023-05-17 19:06:36 +0900

    feat: msw 장바구니 리스트 mocking 핸들러 구현

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,6 +1,7 @@
 import { rest } from 'msw';
+import { CartProduct, ProductItem } from '../types/ProductType';
 
-const productList = [
+const productList: ProductItem[] = [
   {
     id: 1,
     name: 'PET보틀-정사각(420ml)',
@@ -75,8 +76,85 @@ const productList = [
   },
 ];
 
+let cartList: CartProduct[] = [
+  {
+    id: 3,
+    quantity: 5,
+    product: {
+      id: 3,
+      name: 'PET보틀-정사각(370ml)',
+      price: 41000,
+      imageUrl: 'http://placekitten.com/200/200',
+    },
+  },
+  {
+    id: 1,
+    quantity: 10,
+    product: {
+      id: 1,
+      name: 'PET보틀-정사각(420ml)',
+      price: 43400,
+      imageUrl: 'http://placekitten.com/200/200',
+    },
+  },
+];
+
 export const handlers = [
   rest.get('/products', (req, res, ctx) => {
-    return res(ctx.delay(2000), ctx.status(200), ctx.json(productList));
+    return res(ctx.delay(1000), ctx.status(200), ctx.json(productList));
+  }),
+
+  rest.get('/cart-items', (req, res, ctx) => {
+    return res(ctx.delay(1000), ctx.status(200), ctx.json(cartList));
+  }),
+
+  rest.post('/cart-items', async (req, res, ctx) => {
+    const { productId: idData } = await req.json();
+    const productId = Number(idData);
+
+    const product = productList.find((product) => product.id === productId);
+
+    // TODO: 에러처리
+    if (cartList.some(({ id }) => id === productId)) return;
+
+    if (product) {
+      cartList.push({
+        id: productId,
+        quantity: 1,
+        product: product,
+      });
+    }
+
+    return res(ctx.delay(2000), ctx.status(200), ctx.json('SUCCESS'));
+  }),
+
+  rest.delete('/cart-items/:productId', async (req, res, ctx) => {
+    const { productId: idData } = await req.params;
+    const productId = Number(idData);
+
+    cartList = cartList.filter((product) => product.id !== productId);
+    console.log('>>> cartList:', cartList);
+
+    return res(ctx.delay(2000), ctx.status(200), ctx.json('SUCCESS'));
+  }),
+
+  rest.patch('/cart-items/:productId', async (req, res, ctx) => {
+    const { productId: idData } = req.params;
+    const productId = Number(idData);
+
+    const { quantity: quantityData } = await req.json();
+    const quantity: number = quantityData;
+
+    cartList = cartList.map((cartProduct) => {
+      if (cartProduct.id === productId) {
+        return {
+          ...cartProduct,
+          quantity,
+        };
+      } else {
+        return cartProduct;
+      }
+    });
+    return res(ctx.delay(1000), ctx.status(200), ctx.json('SUCCESS'));
   }),
 ];
