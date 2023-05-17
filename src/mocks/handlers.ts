19a6fc32c9f3641459ca4dba5a8b38526227539a
nlom0218 <nlom0218@naver.com>
2023-05-17 15:57:48 +0900

    feat: 상품 로컬스토리지에 저장

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,11 +1,19 @@
 import { rest } from 'msw';
 
+import { Product } from '@Types/index';
+
+import localStorageHelper from '@Utils/localStorageHelper';
+
 import { PRODUCTS_URL } from '@Constants/index';
 
 import mockData from './mockData.json';
 
 export const handlers = [
   rest.get(PRODUCTS_URL, (_, res, ctx) => {
-    return res(ctx.status(200), ctx.json(mockData));
+    if (!localStorageHelper.hasKey('products')) localStorageHelper.setInitValue('products', mockData);
+
+    const products = localStorageHelper.getValue<Product[]>('products');
+
+    return res(ctx.status(200), ctx.json(products));
   }),
 ];
