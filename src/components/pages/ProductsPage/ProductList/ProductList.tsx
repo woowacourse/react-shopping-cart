e8db63663b3d8c2412add6a5910663b4bf083da0
Q Kim <qoli0703@gmail.com>
2023-05-17 10:56:56 +0900

    style: 코드에 공백 추가

--- src/components/pages/ProductsPage/ProductList/ProductList.tsx
+++ src/components/pages/ProductsPage/ProductList/ProductList.tsx
@@ -7,7 +7,7 @@ import ErrorPage from '../../ErrorPage/ErrorPage';
 import LoadingPage from '../../LoadingPage/LoadingPage';
 
 const ProductList = () => {
-  const { data: productList, status} = useFetchData<Product[]>('./mockData.json', []);
+  const { data: productList, status } = useFetchData<Product[]>('./mockData.json', []);
 
   if (status === 0) {
     return <LoadingPage />;
