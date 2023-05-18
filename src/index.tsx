9ec6d041383a4e4582fccd5584c3169342f8275d
Jungwoo <evencoding@gmail.com>
2023-05-18 20:41:24 +0900

    chore: msw 라이브러리 환경 설정

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 import App from './App';
 
+import { worker } from './mocks/browser';
+if (process.env.NODE_ENV === 'development') {
+  worker.start();
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
---
+++ src/mocks/handlers.ts
@@ -0,0 +1,19 @@
+import { rest } from 'msw';
+
+import mockData from '../assets/mock.json';
+import cartMock from '../assets/cartMock.json';
+
+const minDelay = 300;
+const maxDelay = 2000;
+
+const delay = Math.floor(Math.random() * (maxDelay - minDelay + 1)) + minDelay;
+
+export const handlers = [
+  rest.get('/products', (req, res, ctx) => {
+    return res(ctx.delay(delay), ctx.status(200), ctx.json(mockData));
+  }),
+
+  rest.get('/cart-items', (req, res, ctx) => {
+    return res(ctx.delay(delay), ctx.status(200), ctx.json(cartMock));
+  }),
+];
