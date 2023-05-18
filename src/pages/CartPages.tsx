c0a38f8b0b38ee22c1812fae59bfe301a69a0145
woo-jk <201702042@o.cnu.ac.kr>
2023-05-18 22:41:28 +0900

    feat: 장바구니에 상품이 없을 때 화면 구현

--- src/pages/CartPages.tsx
+++ src/pages/CartPages.tsx
@@ -3,17 +3,37 @@ import Header from '../components/common/Header/Header';
 import CartList from '../components/cart/CartList/CartList';
 import PaymentAmount from '../components/cart/PaymentAmount/PaymentAmount';
 import CheckedCartListProvider from '../provider/CheckedListProvider';
+import useCartService from '../hooks/useCartService';
+import { useNavigate } from 'react-router-dom';
 
 const CartPage = () => {
+  const navigate = useNavigate();
+  const { cartList } = useCartService();
+
+  const handleLinkButtonClick = () => {
+    navigate('/');
+  };
+
   return (
     <CheckedCartListProvider>
       <Header />
       <Layout>
         <Title>장바구니</Title>
-        <Contents>
-          <CartList />
-          <PaymentAmount />
-        </Contents>
+        {cartList.length ? (
+          <Contents>
+            <>
+              <CartList />
+              <PaymentAmount />
+            </>
+          </Contents>
+        ) : (
+          <EmptyCartView>
+            장바구니에 상품이 존재하지 않습니다.
+            <LinkButton onClick={handleLinkButtonClick}>
+              상품 담으러 가기
+            </LinkButton>
+          </EmptyCartView>
+        )}
       </Layout>
     </CheckedCartListProvider>
   );
@@ -41,6 +61,32 @@ const Contents = styled.div`
   justify-content: space-between;
 
   margin-top: 34px;
+`;
+
+const EmptyCartView = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+
+  margin-top: 200px;
+
+  font-weight: 500;
+  font-size: 30px;
+`;
+
+const LinkButton = styled.button`
+  width: 300px;
+  padding: 20px 50px;
+  background-color: #333;
+
+  border: none;
+  border-radius: 15px;
+
+  font-size: 20px;
+  color: white;
+
+  cursor: pointer;
 `;
 
 export default CartPage;
