12e067ca354fdbfb79cc08782bdad52e22c246ff
semnil5202 <semnil5202@gmail.com>
2023-05-18 17:58:12 +0900

    feat: 장바구니에 담은 물품 중 선택한 물품들의 결제예상금액을 표시하는 기능 추가

---
+++ src/components/CartTotalPriceContainer.tsx
@@ -0,0 +1,54 @@
+import { useRecoilValue } from 'recoil';
+import {
+  Typography,
+  Typography as ProductPrice,
+  Typography as ShippingPrice,
+  Typography as TotalPrice,
+} from '../ui/Typography';
+import * as Styled from './styles/CartTotalPriceContainer.styles';
+import { cartState } from '../atoms/CartState';
+import { useMemo } from 'react';
+
+export const CartTotalPriceContainer = () => {
+  const cartList = useRecoilValue(cartState);
+  const shippingPrice = 3000;
+
+  const getTotalProductPrice = useMemo(
+    () =>
+      cartList.reduce(
+        (acc, cartItem) =>
+          acc +
+          (cartItem.checked ? cartItem.quantity * cartItem.product.price : 0),
+        0
+      ),
+    [cartList]
+  );
+  return (
+    <Styled.Wrapper>
+      <div>
+        <Typography size="24px">결제예상금액</Typography>
+      </div>
+      <div>
+        <ProductPrice size="20px">총 상품 가격</ProductPrice>
+        <ProductPrice size="20px">{`${getTotalProductPrice.toLocaleString(
+          'ko-KR'
+        )} 원`}</ProductPrice>
+      </div>
+      <div>
+        <ShippingPrice size="20px">배송비</ShippingPrice>
+        <ShippingPrice size="20px">{`${
+          getTotalProductPrice > 0 ? shippingPrice.toLocaleString('ko-KR') : 0
+        } 원`}</ShippingPrice>
+      </div>
+      <div>
+        <TotalPrice size="20px">총 주문금액</TotalPrice>
+        <TotalPrice size="20px">{`${
+          getTotalProductPrice > 0
+            ? (getTotalProductPrice + shippingPrice).toLocaleString('ko-KR')
+            : 0
+        } 원`}</TotalPrice>
+      </div>
+      <Styled.OrderButton>주문하기</Styled.OrderButton>
+    </Styled.Wrapper>
+  );
+};
---
+++ src/components/styles/CartTotalPriceContainer.styles.ts
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+import { Button } from '../../ui/styles/Button.styles';
+
+export const Wrapper = styled.div`
+  width: 448px;
+  height: 408px;
+  border: 1px solid #aaaaaa;
+  margin-top: 84px;
+  position: sticky;
+
+  > div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 28px 36px;
+  }
+
+  > div:first-child {
+    margin: 0;
+    height: 92px;
+    border-bottom: 1px solid #aaaaaa;
+
+    p {
+      margin-left: 36px;
+    }
+  }
+
+  > div:last-of-type {
+    margin-top: 52px;
+
+    p {
+      font-weight: 900;
+    }
+  }
+`;
+
+export const OrderButton = styled(Button)`
+  width: 388px;
+  height: 72px;
+  background-color: #04c09e;
+  color: #ffffff;
+  margin: 40px auto;
+  font-size: 24px;
+`;
