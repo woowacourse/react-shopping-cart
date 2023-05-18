36d187051512971ecf5dd24818bc54595a740570
d0dam <smallkdb@gmail.com>
2023-05-18 16:38:07 +0900

    refactor: Header 위치를 Root로 변경

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import ToastList from '../components/Common/Toast/ToastList';
+import Header from '../components/Header';
 
 function Root() {
   return (
     <>
+      <Header />
       <ToastList />
       <Outlet />
     </>
