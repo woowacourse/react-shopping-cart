aae39ea4178f1d4d57c37746569d3238862e861b
hae-on <solbi2004@naver.com>
2023-05-18 16:39:09 +0900

    chore: msw 초기 환경 셋팅

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,20 @@ import App from './App';
 import GlobalStyle from './GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
+import { worker } from './mocks/browsers';
+
+const main = async () => {
+  if (window.location.pathname === '/react-shopping-cart') {
+    window.location.pathname = '/react-shopping-cart/';
+    return;
+  }
+
+  await worker.start({
+    serviceWorker: {
+      url: '/react-shopping-cart/mockServiceWorker.js',
+    },
+  });
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -18,3 +32,5 @@ root.render(
     </ThemeProvider>
   </React.StrictMode>
 );
+
+main();
