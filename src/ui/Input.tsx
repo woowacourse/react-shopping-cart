961ed83aceb89e6711badce7a5e0bceb2fe04bd6
semnil5202 <semnil5202@gmail.com>
2023-05-18 11:11:31 +0900

    feat: Input 공통 컴포넌트 정의

---
+++ src/ui/Input.tsx
@@ -0,0 +1,11 @@
+import { InputHTMLAttributes } from 'react';
+import * as Styled from './styles/Input.styles';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  width?: string;
+  height?: string;
+}
+
+export const Input = ({ width = '160px', height = '36px' }: InputProps) => {
+  return <Styled.Input width={width} height={height} />;
+};
---
+++ src/ui/styles/Input.styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Input = styled.input<{ width?: string; height?: string }>`
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
+`;
