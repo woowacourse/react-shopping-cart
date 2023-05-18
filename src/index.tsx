fe0f60bef165e9afafb211b742ce173e363c065b
inyeong-kang <tomatorkd@naver.com>
2023-05-19 08:30:01 +0900

    chore: 이미지 추가 장바구니 상품이 없는 경우 이미지 로드

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import '../public/assets/cart.svg';
 import '../public/assets/logo.svg';
 import '../public/assets/delete.svg';
+import '../public/assets/baemin-empty.png';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
