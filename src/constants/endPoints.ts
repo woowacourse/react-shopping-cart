5d41b3840521af15331f9868addcd4e991e55750
sh981013s <sh981013s@gmail.com>
2023-05-18 15:04:36 +0900

    feat: request endpoint 상수화

---
+++ src/constants/endPoints.ts
@@ -0,0 +1,7 @@
+export const baseURL = 'http://localhost:3000';
+
+export const endPoints = {
+  productList: baseURL + '/product-list',
+  cart: baseURL + '/cart',
+  cartUpdate: baseURL + '/cart/update',
+};
