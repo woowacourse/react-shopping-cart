45d6855f387073f21f5800b15bf4809652a37a21
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-05-18 13:17:43 +0900

    design: 모달 스크롤 방지 css 추가

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -4,6 +4,10 @@ const GlobalStyles = createGlobalStyle`
   body{
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
+  
+  body:has([role='dialog']){
+    overflow: hidden;
+  }
 
   * {
     box-sizing: border-box;
