fee5d0e2cbfcb242b3bae4990cc0f31285580baf
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-05-18 11:14:46 +0900

    fix: storybook msw 적용

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,8 +2,21 @@ import React from 'react';
 import { RecoilRoot } from 'recoil';
 import type { Preview } from '@storybook/react';
 import { withRouter } from 'storybook-addon-react-router-v6';
-
 import GlobalStyles from '../src/GlobalStyles';
+import { worker } from '../src/mocks/browser';
+
+const main = async () => {
+  const url =
+    location.hostname === 'jeonjeunghoon.github.io'
+      ? '/react-shopping-cart/mockServiceWorker.js'
+      : '/mockServiceWorker.js';
+
+  await worker.start({
+    serviceWorker: { url },
+  });
+};
+
+main();
 
 const preview: Preview = {
   parameters: {
