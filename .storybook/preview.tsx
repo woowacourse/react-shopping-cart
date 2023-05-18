053c6cfdca0c39b47cae1e6110dc92cf41973f15
hozzijeong <ghwls0905@naver.com>
2023-05-18 22:53:16 +0900

    chore: msw 기본 세팅 설정

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,10 +1,33 @@
 import type { Preview } from '@storybook/react';
-import { withThemeFromJSXProvider } from '@storybook/addon-styling';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '../src/styles/GlobalStyle';
 import theme from '../src/styles/theme';
 import { RecoilRoot } from 'recoil';
 import React from 'react';
+import { handlers } from '../src/mocks/handlers';
+import { initialize, mswDecorator } from 'msw-storybook-addon';
+
+let options = {};
+if (location.hostname === 'hozzijeong.github.io') {
+  options = {
+    serviceWorker: {
+      url: '/react-shopping-cart/mockServiceWorker.js',
+    },
+  };
+}
+
+// Initialize MSW
+initialize(options);
+
+const customViewports = {
+  Default: {
+    name: 'Default',
+    styles: {
+      width: '992px',
+      height: '700px',
+    },
+  },
+};
 
 export const decorators = [
   (Story) => (
@@ -15,16 +38,23 @@ export const decorators = [
       </ThemeProvider>
     </RecoilRoot>
   ),
+  mswDecorator,
 ];
 
 const preview: Preview = {
   parameters: {
+    msw: handlers,
+    layout: 'centered',
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/,
       },
+    },
+    viewport: {
+      viewports: { ...customViewports },
+      defaultViewport: 'Default',
     },
   },
 };
