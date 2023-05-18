7d74f76a783ac641bf188b0fcbe74eb87b7ceab8
Dahye <06robin11@gmail.com>
2023-05-18 17:14:41 +0900

    feat(orderDetail):결제금액 필드 마크업

---
+++ src/components/CartPage/OrderDetail/OrderDetail.styles.tsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const Section = styled.section`
+  width: 30%;
+  height: 25rem;
+  border: 1px solid darkgray;
+`;
+
+export const Title = styled.h3`
+  height: 20%;
+  font-size: 1.3rem;
+  padding: 2rem;
+  border-bottom: 1px solid darkgray;
+`;
+
+export const PriceContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 80%;
+  font-size: 1.5rem;
+  font-weight: 700;
+
+  padding: 2rem;
+`;
+
+export const PriceWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const TotalPrice = styled(PriceWrapper)`
+  padding-top: 1rem;
+`;
---
+++ src/components/CartPage/OrderDetail/index.tsx
@@ -0,0 +1,30 @@
+import Button from '../../common/Button';
+import Flex from '../../common/Flex';
+import * as S from './OrderDetail.styles';
+
+const OrderDetail = () => {
+  return (
+    <S.Section>
+      <S.Title>결제예상금액</S.Title>
+      <S.PriceContainer>
+        <S.PriceWrapper>
+          <span>총 상품가격</span>
+          <span>price</span>
+        </S.PriceWrapper>
+        <S.PriceWrapper>
+          <span>배송비</span>
+          <span>3,000원</span>
+        </S.PriceWrapper>
+        <S.TotalPrice>
+          <span>총 구매금액</span>
+          <span>price</span>
+        </S.TotalPrice>
+        <Button view="dark" size="M">
+          주문하기
+        </Button>
+      </S.PriceContainer>
+    </S.Section>
+  );
+};
+
+export default OrderDetail;
