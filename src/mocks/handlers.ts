9fa49979f0183caf62c1221c6b0ce434e5cb017d
hae-on <solbi2004@naver.com>
2023-05-18 22:36:10 +0900

    refactor: sleep 함수 내장 함수 delay로 변경

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,24 +2,18 @@ import { rest } from 'msw';
 import products from './data/products.json';
 
 export const handlers = [
-  rest.get('/products', async (req, res, ctx) => {
-    await sleep(200);
+  rest.get('/products', (req, res, ctx) => {
+    ctx.delay(200);
     return res(ctx.status(200), ctx.json(products));
   }),
 
-  rest.get('/products/empty', async (req, res, ctx) => {
-    await sleep(200);
+  rest.get('/products/empty', (req, res, ctx) => {
+    ctx.delay(200);
     return res(ctx.status(200), ctx.json([]));
   }),
 
-  rest.get('/products/error', async (req, res, ctx) => {
-    await sleep(200);
-    return res(ctx.status(200), ctx.json(products));
+  rest.get('/products/error', (req, res, ctx) => {
+    ctx.delay(200);
+    return res(ctx.status(400), ctx.json(products));
   }),
 ];
-
-async function sleep(timeout: number) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, timeout);
-  });
-}
