c032fb0c6bd118467eb72c95b2857d0565faeb90
sh981013s <sh981013s@gmail.com>
2023-05-18 15:00:05 +0900

    chore: eslintrc.cjs code formatting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,31 +1,30 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'react', 'prettier'],
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "prettier"
-  ],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended', 'prettier', 'plugin:storybook/recommended'],
   parserOptions: {
     ecmaVersion: 2021,
     sourceType: 'module',
     ecmaFeatures: {
-      jsx: true,
-    },
+      jsx: true
+    }
   },
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
   rules: {
     'prettier/prettier': 'error',
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    '@typescript-eslint/no-unused-vars': ['error', {
+      argsIgnorePattern: '^_'
+    }],
+    'no-console': ['warn', {
+      allow: ['warn', 'error']
+    }],
     'prefer-const': 'warn',
     eqeqeq: 'error',
     'no-multi-spaces': 'error',
@@ -35,11 +34,14 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     "quotes": ["error", "single"],
     '@typescript-eslint/no-empty-function': 'off',
-    "react/function-component-definition": [2, { "namedComponents": "arrow-function" }]
+    "react/function-component-definition": [2, {
+      "namedComponents": "arrow-function"
+    }],
+    "react-hooks/exhaustive-deps": "off",
   },
   settings: {
     react: {
-      version: 'detect',
-    },
-  },
+      version: 'detect'
+    }
+  }
 };
