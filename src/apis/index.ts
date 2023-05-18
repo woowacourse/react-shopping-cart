4c3a11e501e31f8ec289465c78abad4731dc89ca
Dahye <06robin11@gmail.com>
2023-05-18 23:44:47 +0900

    feat(api):REST 유틸함수 생성

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,4 +1,4 @@
-export const fetcher = async <T>(
+export const getData = async <T>(
   url: string,
   options?: RequestInit
 ): Promise<T> => {
@@ -25,6 +25,30 @@ export const postData = async <T>(url: string, data: T) => {
     const errorMessage = res.statusText;
     throw new Error(`에러가 발생했습니다. ${errorMessage}`);
   }
+};
 
-  return res.json();
+export const patchData = async <T>(url: string, data: T) => {
+  const res = await fetch(url, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (!res.ok) {
+    const errorMessage = res.statusText;
+    throw new Error(`에러가 발생했습니다. ${errorMessage}`);
+  }
+};
+
+export const deleteData = async (url: string) => {
+  const res = await fetch(url, {
+    method: 'DELETE',
+  });
+
+  if (!res.ok) {
+    const errorMessage = res.statusText;
+    throw new Error(`에러가 발생했습니다. ${errorMessage}`);
+  }
 };
