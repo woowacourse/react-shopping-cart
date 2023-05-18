38586aec0d4d51e9100dcce10f8f0dfb09ebd72c
d0dam <smallkdb@gmail.com>
2023-05-18 16:38:23 +0900

    refactor: 불필요한 코드 삭제

--- src/Page/Home/index.tsx
+++ src/Page/Home/index.tsx
@@ -1,25 +1,9 @@
-import { useSetRecoilState } from 'recoil';
 import ProductItemList from '../../components/ProductItemList';
 import styles from './index.module.scss';
-import { $CartIdList } from '../../recoil/atom';
-import useGetQuery from '../../hooks/useGetQuery';
-import { useEffect } from 'react';
-import type { CartItem } from '../../types';
-import Header from '../../components/Header';
 
 function Home() {
-  const { data } = useGetQuery<CartItem[]>('/cart-items');
-  const setCartIdList = useSetRecoilState($CartIdList);
-
-  useEffect(() => {
-    if (data) {
-      setCartIdList(data.map(({ product }) => product.id));
-    }
-  }, [data, setCartIdList]);
-
   return (
     <>
-      <Header />
       <main className={styles['main-container']}>
         <ProductItemList />
       </main>
