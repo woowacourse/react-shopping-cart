1d8a5e9ffd64b9e94a6d85a3f9de08045a8c9885
sh981013s <sh981013s@gmail.com>
2023-05-18 14:53:17 +0900

    feat: msw 초기 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const renderApp = () => {
+  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+};
+
+if (process.env.NODE_ENV === 'development') {
+  import('./mocks/server').then(async ({ worker }) => {
+    await worker.start();
+    renderApp();
+  });
+} else {
+  renderApp();
+}
