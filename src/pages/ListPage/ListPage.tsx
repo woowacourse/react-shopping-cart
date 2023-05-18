9308f263e19d58cc89610bab4422767829971e88
nangkyeonglim <nangkyeonglim@gmail.com>
2023-05-18 15:00:44 +0900

    refactor: mockData 대신 fetch한 productList를 가져오도록 변경

--- src/pages/ListPage/ListPage.tsx
+++ src/pages/ListPage/ListPage.tsx
@@ -1,11 +1,14 @@
 import * as Styled from './ListPage.styles.tsx';
 import ProductItem from '../../components/ProductItem/ProductItem.tsx';
-import mockData from '../../data/productList.json';
+import useFetchData from '../../hooks/useFetchData.ts';
+import type { Product } from '../../types/index.ts';
 
 const ListPage = () => {
+  const { data } = useFetchData<Product[]>('/products');
+
   return (
     <Styled.ProductList>
-      {mockData.map((product) => {
+      {data?.map((product) => {
         return <ProductItem key={product.id} {...product} />;
       })}
     </Styled.ProductList>
