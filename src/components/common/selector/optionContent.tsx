17506765e73ea6d12e562c0033136ffbbaf3ebb9
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-05-18 11:07:42 +0900

    feat : OptionContent 컴포넌트의 커스텀된 element 리턴 구현

--- src/components/common/selector/optionContent.tsx
+++ src/components/common/selector/optionContent.tsx
@@ -1,4 +1,5 @@
-import { PropsWithChildren } from "react";
+import { Children, Fragment, PropsWithChildren } from "react";
+import { createCustomElement, hasCustomChild } from "../../../utils/elements";
 
 interface OptionContentProps {
   asChild?: boolean;
@@ -7,5 +8,14 @@ interface OptionContentProps {
 
 export function OptionContent(props: OptionContentProps) {
   const { children, asChild = false } = props;
+  const customElement = children ? Children.only(children) : null;
+
+  if (customElement && hasCustomChild(asChild, customElement)) {
+    return createCustomElement<OptionContentProps & { "data-testid": string }>(
+      customElement,
+      { ...props, "data-testid": "custom-content" }
+    );
+  }
+
   return <div data-testid="content">{children}</div>;
 }
