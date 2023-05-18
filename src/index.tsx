371c4f68d2238a60c68f01accb5cef84aebcbc14
semnil5202 <semnil5202@gmail.com>
2023-05-18 20:49:55 +0900

    chore: MSW init

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { GlobalStyle } from './styles/GlobalStyles';
 
+if (process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { worker } = require('./mocks/browser');
+  worker.start();
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <GlobalStyle />
