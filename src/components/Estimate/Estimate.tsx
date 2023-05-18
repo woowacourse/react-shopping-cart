7216d2a1224b843694afdd4fb4deacc110320e4a
inyeong-kang <tomatorkd@naver.com>
2023-05-18 13:55:00 +0900

    feat: Estimate 컴포넌트, 스토리 구현

---
+++ src/components/Estimate/Estimate.tsx
@@ -0,0 +1,110 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { SHIPPING_FEE } from '../../constants';
+
+interface EstimateProps {
+  total_price: number;
+}
+
+export default function Estimate({ total_price }: EstimateProps) {
+  return (
+    <Wrapper>
+      <Title>결제 예상 금액</Title>
+      <InfoBox>
+        <Info>
+          <p>총 상품가격</p>
+          <p>{total_price.toLocaleString()}원</p>
+        </Info>
+        <Info>
+          <p>총 배송비</p>
+          <p>{SHIPPING_FEE.toLocaleString()}원</p>
+        </Info>
+        <Total>
+          <p>총 주문금액</p>
+          <p>{(total_price + SHIPPING_FEE).toLocaleString()}원</p>
+        </Total>
+      </InfoBox>
+      <OrderButton>
+        <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>
+          주문하기
+        </Link>
+      </OrderButton>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  width: 360px;
+  height: 400px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  gap: 55px;
+
+  border: 1px solid #dddddd;
+
+  margin-top: 55px;
+
+  @media screen and (max-width: 767px) {
+    width: 320px;
+    height: 360px;
+
+    margin-top: 30px;
+  }
+`;
+
+const Title = styled.div`
+  font-size: 18px;
+  padding: 20px;
+  border-bottom: 3px solid #dddddd;
+`;
+
+const InfoBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  gap: 27px;
+
+  font-size: 17px;
+  font-weight: 600;
+
+  @media screen and (max-width: 767px) {
+    gap: 15px;
+  }
+`;
+
+const Info = styled.div`
+  width: 80%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Total = styled.div`
+  width: 80%;
+  display: flex;
+  justify-content: space-between;
+
+  margin-top: 20px;
+`;
+
+const OrderButton = styled.button`
+  width: 90%;
+  height: 73px;
+
+  padding: 13px;
+  background-color: black;
+  color: white;
+
+  margin: 0px 20px;
+  font-size: 18px;
+  text-decoration: none;
+  cursor: pointer;
+
+  @media screen and (max-width: 767px) {
+    padding: 6px;
+    height: 50px;
+    font-size: 15px;
+  }
+`;
---
+++ src/components/Estimate/stories/Estimate.stories.tsx
@@ -0,0 +1,23 @@
+import type { StoryFn } from '@storybook/react';
+import { MemoryRouter } from 'react-router-dom';
+import Estimate from '../Estimate';
+
+export default {
+  title: 'Estimate',
+  component: Estimate,
+  tags: ['autodocs'],
+  decorators: [
+    (Story: StoryFn) => (
+      <MemoryRouter>
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
+};
+
+const Template: StoryFn<React.ComponentProps<typeof Estimate>> = (props) => <Estimate {...props} />;
+
+export const DefaultEstimate = Template.bind({});
+DefaultEstimate.args = {
+  total_price: 21700,
+};
