7d5b31cc2c92dc6a98b049c882591de5ca54675c
Dahye <06robin11@gmail.com>
2023-05-18 17:36:43 +0900

    styles(cartList):스타일 조정

--- src/components/CartPage/CartList/CartList.styles.tsx
+++ src/components/CartPage/CartList/CartList.styles.tsx
@@ -2,6 +2,22 @@ import styled from 'styled-components';
 
 export const Container = styled.section`
   width: 60%;
+  font-size: 1.5rem;
+`;
+
+export const Title = styled.h3`
+  padding-bottom: 3rem;
 `;
 
 export const CartList = styled.ul``;
+
+export const SelectAll = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const CheckBox = styled.input`
+  width: 2rem;
+  height: 2rem;
+  margin-right: 1rem;
+`;
--- src/components/CartPage/CartList/index.tsx
+++ src/components/CartPage/CartList/index.tsx
@@ -3,12 +3,15 @@ import { useRecoilValue } from 'recoil';
 import { cartState } from '../../../recoil/cart';
 import CartItem from '../CartItem';
 import * as S from './CartList.styles';
+import Button from '../../common/Button';
+import Flex from '../../common/Flex';
 
 const CartList = () => {
   const cartItems = useRecoilValue(cartState);
 
   return (
     <S.Container>
+      <S.Title>든든 배송 상품 ( {cartItems.length}개 )</S.Title>
       <S.CartList>
         {cartItems.map((item) => (
           <li key={item.id}>
@@ -16,6 +19,15 @@ const CartList = () => {
           </li>
         ))}
       </S.CartList>
+      <Flex width="100%" justify="space-between" align="center">
+        <S.SelectAll>
+          <S.CheckBox type="checkbox" />
+          전체 선택
+        </S.SelectAll>
+        <Button size="S" view="light">
+          전체 삭제
+        </Button>
+      </Flex>
     </S.Container>
   );
 };
