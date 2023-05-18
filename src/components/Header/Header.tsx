58d3e9d4258efca7329dedd6d3c23ab2ad7e635d
sh981013s <sh981013s@gmail.com>
2023-05-18 15:23:44 +0900

    feat: 로고 클릭 시 메인화면으로 navigate 기능 구현

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,19 @@
 import Logo from '../../assets/Logo.svg';
 import * as Styled from './Header.styles';
 import ShoppingCartStatus from './ShoppingCartStatus/ShoppingCartStatus.tsx';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
+  const handleLogoClick = () => {
+    navigate('/');
+  };
+
   return (
     <Styled.HeaderContainer>
       <Styled.HeaderWrapper>
-        <Styled.LogoButton data-cy='headerlogo'>
+        <Styled.LogoButton data-cy='headerlogo' onClick={handleLogoClick}>
           <Styled.LogoImage src={Logo} alt='SHOP' />
-          <Styled.LogoTitle>SHOP</Styled.LogoTitle>
         </Styled.LogoButton>
         <Styled.ShoppingCartButton>
           <Styled.ShoppingCartButtonText>장바구니</Styled.ShoppingCartButtonText>
