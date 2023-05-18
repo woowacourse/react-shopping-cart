dd257212ed5288d76f8bffc76db875c8cd54a472
ukkodeveloper <ukkodeveloper@gmail.com>
2023-05-18 17:44:44 +0900

    remove: 사용하지 않는 atom 상태 제거

--- src/recoil/cartListState.ts
+++
@@ -1,9 +0,0 @@
-import { atom } from 'recoil';
-import * as T from '../types/ProductType';
-
-const cartListState = atom<T.CartItem[]>({
-  key: 'cartListState',
-  default: [],
-});
-
-export default cartListState;
