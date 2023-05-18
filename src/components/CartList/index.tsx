41af63ad92b80b9c2ce6a9b00ad21806ce575d67
Patrick <kuyt3122@naver.com>
2023-05-18 16:25:55 +0900

    feat: cartlist 컴포넌트 구현

--- src/components/CartList/index.tsx
+++ src/components/CartList/index.tsx
@@ -1,88 +1,21 @@
-import useControlCart from '@hooks/useControlCart';
 import { styled } from 'styled-components';
-import BucketCounter from '@components/common/BucketCounter';
-import CheckBox from '@components/common/CheckBox';
-import { A_MOCK, CART_ITEM_REMOVE_BUTTON } from '@assets';
+import CartItem from './CartItem';
 
-interface CartItemProps {
-  name: string;
-}
-
-const CartItem = (props: CartItemProps) => {
-  const { removeProductFromCart } = useControlCart();
-
+const CartList = () => {
   return (
-    <CartItemWrapper>
-      <CheckBox />
-      <CartItemImg src={A_MOCK} alt="상품 사진" />
-      <CartItemName>{props.name}</CartItemName>
-      <CardItemInformationWrapper>
-        <RemoveCardItemImg src={CART_ITEM_REMOVE_BUTTON} />
-        <BucketCounter
-          removeProductFromCart={() => removeProductFromCart(1)}
-          kind= "big"
-        />
-        <CartItemMoney>7000</CartItemMoney>
-      </CardItemInformationWrapper>
-    </CartItemWrapper>
+    <CartListWrapper>
+      <CartItem name="든든 볶음밥" />
+      <hr />
+      <CartItem name="은근 볶음밥" />
+      <hr />
+      <CartItem name="히든 볶음밥" />
+    </CartListWrapper>
   );
 };
 
-const CartItemWrapper = styled.div`
-  display: flex;
-  align-items: flex-start;
-
+const CartListWrapper = styled.div`
   width: 740px;
+  border-top: 4px solid #aaaaaa;
 `;
 
-const CartItemImg = styled.img`
-  width: 144px;
-  height: 147px;
-
-  margin-left: 15px;
-
-  background: rgba(0, 0, 0, 0.05);
-`;
-
-const CartItemName = styled.span`
-  font-family: 'Noto Sans KR';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 20px;
-  line-height: 24px;
-
-  margin-left: 20px;
-
-  letter-spacing: 0.5px;
-
-  color: #333333;
-`;
-
-const CardItemInformationWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-  justify-content: space-between;
-
-  flex-grow: 1;
-
-  height: 147px;
-`;
-
-const RemoveCardItemImg = styled.img`
-  width: 24px;
-  height: 24px;
-`;
-
-const CartItemMoney = styled.span`
-  font-family: 'Noto Sans KR';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 16px;
-  line-height: 24px;
-
-  letter-spacing: 0.5px;
-
-  color: #333333;
-`;
-export default CartItem;
+export default CartList;
