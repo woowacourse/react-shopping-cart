c416931d3145641b1bec4261b6af349d620f17e0
regularPark <srk99279@naver.com>
2023-05-18 14:28:58 +0900

    feat: CartList 컴포넌트 생성 및 useHandleCartList 훅 생성

--- src/components/cart/CartList.tsx
+++ src/components/cart/CartList.tsx
@@ -2,34 +2,48 @@ import { useRecoilValue } from 'recoil';
 import { cartState } from '../../store/CartState';
 import CartListItem from './CartListItem';
 import { styled } from 'styled-components';
-import { DELIVERY_FEE } from '../../constants';
-import { totalPriceSelector } from '../../store/CartSelector';
+import Checkbox, { CheckboxStyle } from './Checkbox';
+import PriceWrapper from './PriceWrapper';
+import { useHandleCartList } from '../../hooks/useHandleCartList';
 
 const CartList = () => {
   const cart = useRecoilValue(cartState);
-  const totalPrice = useRecoilValue(totalPriceSelector);
+  const {
+    checkedItems,
+    totalPrice,
+    handleCheckAllItems,
+    handleCheckedItem,
+    handleRemoveCheckedItem,
+  } = useHandleCartList();
 
   return (
     <>
       <S.Title>장바구니</S.Title>
       <S.Wrapper>
-        <S.ItemWrapper>
+        <S.ItemListWrapper>
+          <S.CheckboxAllWrapper>
+            <S.CheckboxAll
+              type="checkbox"
+              id={'all'}
+              onClick={handleCheckAllItems}
+              checked={checkedItems.length === cart.length}
+            />
+            <label htmlFor={'all'}>{`전체 ${cart.length}`}</label>
+            <S.RemoveSelectedButton onClick={handleRemoveCheckedItem}>
+              선택 삭제
+            </S.RemoveSelectedButton>
+          </S.CheckboxAllWrapper>
           {cart.map((cartItem) => (
-            <CartListItem item={cartItem} />
+            <S.ItemWrapper>
+              <Checkbox
+                onChange={handleCheckedItem(cartItem.id)}
+                isChecked={checkedItems.includes(cartItem.id)}
+              />
+              <CartListItem item={cartItem} />
+            </S.ItemWrapper>
           ))}
-        </S.ItemWrapper>
-        <S.PriceWrapper>
-          <S.PriceLabel>결제예상금액</S.PriceLabel>
-          <S.PriceInfo>
-            <p>총 삼품가격</p>
-            <p>{totalPrice.toLocaleString()}원</p>
-            <p>총 배송비</p>
-            <p>{DELIVERY_FEE.toLocaleString()}원</p>
-            <p>총 주문금액</p>
-            <p>{(totalPrice + DELIVERY_FEE).toLocaleString()}원</p>
-          </S.PriceInfo>
-          <S.OrderButton>주문하기</S.OrderButton>
-        </S.PriceWrapper>
+        </S.ItemListWrapper>
+        <PriceWrapper totalPrice={totalPrice} />
       </S.Wrapper>
     </>
   );
@@ -39,63 +53,73 @@ const S = {
   Wrapper: styled.div`
     display: flex;
     flex-direction: row;
-    justify-content: space-between;
+    justify-content: center;
+    position: relative;
+    gap: 100px;
+  `,
+
+  ItemListWrapper: styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+    width: 736px;
+    height: 700px;
+    overflow: scroll;
+
+    & > :first-child {
+      position: absolute;
+      left: 0;
+    }
+
+    & > :nth-child(2) {
+      margin-top: 40px;
+      border-top: 4px solid #aaa;
+    }
+
+    & > :last-child {
+      border-bottom: none;
+    }
+  `,
+
+  CheckboxAllWrapper: styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    height: 40px;
+    gap: 10px;
+    font-size: 24px;
   `,
 
   ItemWrapper: styled.div`
     display: flex;
-    flex-direction: column;
-    align-items: center;
+    flex-direction: row;
+
+    & > :first-child {
+      position: relative;
+      left: 20px;
+      margin-top: 85px;
+      margin-left: 20px;
+    }
+
+    & > :nth-child(even) {
+      border-bottom: 1.5px solid #ccc;
+    }
+  `,
+
+  RemoveSelectedButton: styled.button`
+    border: 1px solid black;
+    padding: 5px;
+    border-radius: 7px;
+    cursor: pointer;
   `,
 
   Title: styled.h1`
+    text-align: center;
     font-size: 32px;
     font-weight: 700;
   `,
 
-  PriceWrapper: styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 450px;
-    height: 410px;
-    padding: 30px;
-    border: 1px solid #ddd;
-
-    & > :nth-child(1) {
-      display: flex;
-      align-items: flex-start;
-    }
-
-    p {
-      display: inline-block;
-      width: 50%;
-      margin: 10px 0px;
-    }
-
-    p:nth-child(2n) {
-      text-align: right;
-    }
-  `,
-
-  PriceLabel: styled.div`
-    width: 100%;
-    font-size: 24px;
-    font-weight: 400;
-  `,
-  PriceInfo: styled.div`
-    font-size: 20px;
-    font-weight: 700;
-  `,
-
-  OrderButton: styled.button`
-    width: 390px;
-    height: 75px;
-    font-size: 24px;
-    font-weight: 400;
-    color: #fff;
-    background-color: #333;
-    cursor: pointer;
-  `,
+  CheckboxAll: styled(CheckboxStyle)``,
 };
 export default CartList;
---
+++ src/hooks/useHandleCartList.ts
@@ -0,0 +1,39 @@
+import { useRecoilCallback, useRecoilValue } from 'recoil';
+import { cartState } from '../store/CartState';
+import { ChangeEvent, useState } from 'react';
+import { removeProductItemFromCartSelector, totalPriceSelector } from '../store/CartSelector';
+
+export const useHandleCartList = () => {
+  const cart = useRecoilValue(cartState);
+  const [checkedItems, setCheckedItems] = useState<number[]>([]);
+  const removeProductItemFromCart = useRecoilCallback(({ set }) => (id: number) => {
+    set(removeProductItemFromCartSelector(id), []);
+  });
+
+  const totalPrice = useRecoilValue(totalPriceSelector(checkedItems));
+
+  const handleCheckedItem = (id: number) => (e: ChangeEvent<HTMLInputElement>) => {
+    checkedItems.includes(id)
+      ? setCheckedItems((prev) => prev.filter((itemId) => itemId !== id))
+      : setCheckedItems((prev) => [...prev, id]);
+  };
+
+  const handleCheckAllItems = () => {
+    checkedItems.length === cart.length
+      ? setCheckedItems([])
+      : setCheckedItems(cart.map((item) => item.id));
+  };
+
+  const handleRemoveCheckedItem = () => {
+    checkedItems.forEach((id) => removeProductItemFromCart(id));
+    setCheckedItems([]);
+  };
+
+  return {
+    checkedItems,
+    totalPrice,
+    handleCheckAllItems,
+    handleCheckedItem,
+    handleRemoveCheckedItem,
+  };
+};
