55242f856e2851ce6792f1bde9a935f6734f1c45
nangkyeonglim <nangkyeonglim@gmail.com>
2023-05-18 13:52:01 +0900

    feat: msw 기본 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { worker } from './mocks/worker';
+
+worker.start();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
