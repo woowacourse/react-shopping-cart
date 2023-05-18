0155f8968d217ece229c4c4cdd3be4ec26bb503c
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-05-18 23:00:46 +0900

    feat: 장바구니 get fetcher 구현
    1. 서버 장바구니 데이터 형식을 프로젝트 데이터 형식으로 파싱하는 로직 구현
    2. 파싱 전 실행하는 커스텀 타입가드 구현

---
+++ src/apis/cart/index.ts
@@ -0,0 +1,30 @@
+import fetcher from 'apis';
+import { CartProducts, Product } from 'types/product';
+
+const GET_URL = '/cart-items';
+
+type ServerCartProduct = {
+  id: Product['id'];
+  quantity: number;
+  product: Product;
+};
+
+const isServerCartProductsType = (data: any): data is ServerCartProduct[] => {
+  if (!Array.isArray(data)) return false;
+
+  return data.every((value) => 'id' in value && 'quantity' in value && 'product' in value);
+};
+
+const cartProductsParser = (data: any): CartProducts => {
+  if (!isServerCartProductsType(data)) throw new Error(`서버 데이터 형식이 serverCartProducts type이 아닙니다.`);
+
+  const parsedCartProducts = data.map(({ id, product, quantity }) => [id, { quantity, product }] as const);
+
+  return new Map(parsedCartProducts);
+};
+
+export const getCartProducts = async (): Promise<CartProducts> => {
+  const cartProducts = await fetcher<ServerCartProduct[]>(GET_URL);
+
+  return cartProductsParser(cartProducts);
+};
