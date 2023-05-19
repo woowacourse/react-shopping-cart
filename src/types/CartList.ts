308ec79ee0d74c80dcbedf9c3941f3beb104d699
sh981013s <sh981013s@gmail.com>
2023-05-19 15:04:56 +0900

    feat: 장바구니 관련 타입 분리

--- src/types/CartList.ts
+++ src/types/CartList.ts
@@ -1,1 +1,24 @@
-export type CartList = { [key: number]: { quantity: number } };
+export type ItemInfo = {
+  id: number;
+  name: string;
+  imageUrl: string;
+};
+
+export type Item = {
+  id: number;
+  quantity: number;
+  itemInfo: ItemInfo;
+};
+
+export type CartList = {
+  items: Item[];
+};
+
+export type Product = {
+  id: number;
+  name: string;
+  price: number;
+  imageUrl: string;
+};
+
+export type ProductList = Product[];
