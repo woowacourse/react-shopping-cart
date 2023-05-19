86264296c79a0f849588d2d8d0de129e17570796
yeopto <gunyeop530@gmail.com>
2023-05-19 17:09:35 +0900

    feat: ShoppingPage Suspense 추가

--- src/components/pages/ShoppingPage.tsx
+++ src/components/pages/ShoppingPage.tsx
@@ -1,10 +1,14 @@
+import { Suspense } from 'react';
 import PageTemplate from '../templates/PageTemplate';
 import ProductList from '../list/ProductList';
+import { Text } from '../common/Text/Text';
 
 const ShoppingPage = () => {
   return (
     <PageTemplate>
-      <ProductList />
+      <Suspense fallback={<Text>로딩 중...</Text>}>
+        <ProductList />
+      </Suspense>
     </PageTemplate>
   );
 };
