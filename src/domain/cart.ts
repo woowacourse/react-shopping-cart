1d81f7382f7c8ab45671f3211a7c3213e9d6d192
ashleysyheo <ashleysyheo@gmail.com>
2023-05-19 15:44:51 +0900

    feat: 카트 관련 도메인 로직 도메인 폴더로 이동

---
+++ src/domain/cart.ts
@@ -0,0 +1,68 @@
+import { CART_LIST_LOCAL_STORAGE_KEY } from '../constants';
+import productListData from '../data/mockData.json';
+import { CartItemData } from '../types';
+import { getFromLocalStorage, saveToLocalStorage } from '../utils/localStorage';
+
+const getCartData = () => {
+  return getFromLocalStorage<CartItemData[]>(CART_LIST_LOCAL_STORAGE_KEY) ?? [];
+};
+
+const setCartData = (newCartList: CartItemData[]) => {
+  saveToLocalStorage(CART_LIST_LOCAL_STORAGE_KEY, newCartList);
+};
+
+const findCartItemIndex = (cartList: CartItemData[], productId: number) => {
+  return cartList.findIndex((cartItem) => cartItem.product.id === productId);
+};
+
+const addCartItemQuantity = (cartList: CartItemData[], productId: number, quantity: number) => {
+  const selectedCartItemIndex = findCartItemIndex(cartList, productId);
+
+  if (selectedCartItemIndex === -1) {
+    const newCartId = Number(new Date());
+    const product = productListData.find((productItem) => productItem.id === productId)!;
+
+    return [...cartList, { id: newCartId, quantity, product }];
+  }
+
+  const updatedCartList = [
+    ...cartList.slice(0, selectedCartItemIndex),
+    {
+      ...cartList[selectedCartItemIndex],
+      quantity: quantity + cartList[selectedCartItemIndex].quantity,
+    },
+    ...cartList.slice(selectedCartItemIndex + 1),
+  ];
+
+  return updatedCartList;
+};
+
+const updateCartItemQuantity = (cartList: CartItemData[], productId: number, quantity: number) => {
+  const selectedCartItemIndex = findCartItemIndex(cartList, productId);
+
+  const updatedCartList = [
+    ...cartList.slice(0, selectedCartItemIndex),
+    {
+      ...cartList[selectedCartItemIndex],
+      quantity,
+    },
+    ...cartList.slice(selectedCartItemIndex + 1),
+  ];
+
+  return updatedCartList;
+};
+
+const removeCartItem = (cartList: CartItemData[], productId: number) => {
+  const selectedCartItemIndex = findCartItemIndex(cartList, productId);
+
+  if (selectedCartItemIndex === -1) return [...cartList];
+
+  const updatedCartList = [
+    ...cartList.slice(0, selectedCartItemIndex),
+    ...cartList.slice(selectedCartItemIndex + 1),
+  ];
+
+  return updatedCartList;
+};
+
+export { getCartData, setCartData, addCartItemQuantity, updateCartItemQuantity, removeCartItem };
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,39 +1,8 @@
 import { rest } from 'msw';
 
-import { CART_LIST_LOCAL_STORAGE_KEY } from '../constants';
 import productListData from '../data/mockData.json';
+import { addCartItemQuantity, getCartData, setCartData } from '../domain/cart';
 import { CartItemData, PostCartItemRequestBody, ProductItemData } from '../types';
-import { getFromLocalStorage, saveToLocalStorage } from '../utils/localStorage';
-
-const getCartData = () => {
-  return getFromLocalStorage<CartItemData[]>(CART_LIST_LOCAL_STORAGE_KEY) ?? [];
-};
-
-const setCartData = (newCartList: CartItemData[]) => {
-  saveToLocalStorage(CART_LIST_LOCAL_STORAGE_KEY, newCartList);
-};
-
-const changeCartItemQuantity = (cartList: CartItemData[], productId: number, quantity: number) => {
-  const selectedCartItemIndex = cartList.findIndex((cartItem) => cartItem.product.id === productId);
-
-  if (selectedCartItemIndex === -1) {
-    const newCartId = Number(new Date());
-    const product = productListData.find((productItem) => productItem.id === productId)!;
-
-    return [...cartList, { id: newCartId, quantity, product }];
-  }
-
-  const updatedCartList = [
-    ...cartList.slice(0, selectedCartItemIndex),
-    {
-      ...cartList[selectedCartItemIndex],
-      quantity: quantity + cartList[selectedCartItemIndex].quantity,
-    },
-    ...cartList.slice(selectedCartItemIndex + 1),
-  ];
-
-  return updatedCartList;
-};
 
 const handlers = [
   rest.get('/api/products', (req, res, ctx) => {
@@ -41,7 +10,7 @@ const handlers = [
   }),
 
   rest.get('/api/carts', (req, res, ctx) => {
-    const cartList = getFromLocalStorage<CartItemData[]>(CART_LIST_LOCAL_STORAGE_KEY);
+    const cartList = getCartData();
 
     return res(ctx.delay(2000), ctx.status(200), ctx.json<CartItemData[]>(cartList));
   }),
@@ -50,10 +19,10 @@ const handlers = [
     const { productId, quantity } = await req.json<PostCartItemRequestBody>();
     const currentCartData = getCartData();
 
-    const newCartList = changeCartItemQuantity(currentCartData, productId, quantity);
+    const newCartList = addCartItemQuantity(currentCartData, productId, quantity);
     setCartData(newCartList);
 
-    return res(ctx.status(200), ctx.json<CartItemData[]>(newCartList));
+    return res(ctx.delay(1000), ctx.status(200), ctx.json<CartItemData[]>(newCartList));
   }),
 ];
 
