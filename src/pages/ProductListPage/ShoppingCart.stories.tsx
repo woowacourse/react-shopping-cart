eba23ae7244a2410c357b72c79525aa3ab837515
chsua <csj1919@naver.com>
2023-05-19 16:05:09 +0900

    test: 라우터 관련 컴포넌트 테스트에 브라우저라우터 추가

--- src/pages/ProductListPage/ShoppingCart.stories.tsx
+++ src/pages/ProductListPage/ShoppingCart.stories.tsx
@@ -1,31 +1,34 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
 import Header from '@Components/Header';
 
 import GlobalStyle, { CommonPageStyle } from '@Styles/GlobalStyle';
 
-import ProductList from '.';
+import ProductListPage from '.';
 
 /**
- * `ProductList`은 상품의 페이지를 나타내는 페이지 컴포넌트입니다.
+ * `ProductListPage`은 상품의 페이지를 나타내는 페이지 컴포넌트입니다.
  */
-const meta: Meta<typeof ProductList> = {
-  title: 'ProductList',
+const meta: Meta<typeof ProductListPage> = {
+  title: 'ProductListPage',
   decorators: [
     (storyFn) => (
-      <RecoilRoot>
-        <GlobalStyle />
-        <Header />
-        <CommonPageStyle>{storyFn()}</CommonPageStyle>
-      </RecoilRoot>
+      <BrowserRouter>
+        <RecoilRoot>
+          <GlobalStyle />
+          <Header />
+          <CommonPageStyle>{storyFn()}</CommonPageStyle>
+        </RecoilRoot>
+      </BrowserRouter>
     ),
   ],
-  component: ProductList,
+  component: ProductListPage,
 };
 
 export default meta;
 
-type Story = StoryObj<typeof ProductList>;
+type Story = StoryObj<typeof ProductListPage>;
 
 export const DefaultProductList: Story = {};
