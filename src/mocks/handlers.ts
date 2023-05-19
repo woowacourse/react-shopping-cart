3b6f8d0a8e7499c1ec5a400af7a875cdc60f0e41
Leejin-Yang <ejin1996@gmail.com>
2023-05-19 11:50:53 +0900

    feat: msw 핸들러 작성

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,26 +1,126 @@
 import { rest } from 'msw';
 
+import { CART_STORAGE_ID } from '../constants/storage';
 import products from './data/products.json';
+import { findTargetProduct } from '../states/cartProducts/util';
+import type { CartProduct } from '../types/product';
 
 export const handlers = [
-  rest.get('/products', async (req, res, ctx) => {
-    await sleep(200);
-    return res(ctx.status(200), ctx.json(products));
+  rest.get('/products', (req, res, ctx) => {
+    return res(ctx.delay(2000), ctx.status(200), ctx.json(products));
   }),
 
-  rest.get('/products/empty', async (req, res, ctx) => {
-    await sleep(200);
-    return res(ctx.status(200), ctx.json([]));
+  rest.get('/products/empty', (req, res, ctx) => {
+    return res(ctx.delay(2000), ctx.status(200), ctx.json([]));
   }),
 
-  rest.get('/products/error', async (req, res, ctx) => {
-    await sleep(200);
-    return res(ctx.status(200), ctx.json(products));
+  rest.get('/products/error', (req, res, ctx) => {
+    return res(ctx.delay(2000), ctx.status(400), ctx.json({ error: 'fail' }));
+  }),
+
+  rest.get('/products/network-error', (req, res, ctx) => {
+    return res.networkError('Failed to Connect');
+  }),
+
+  rest.get('/cart-items', (req, res, ctx) => {
+    return res(
+      ctx.delay(2000),
+      ctx.status(200),
+      ctx.json(JSON.parse(localStorage.getItem(CART_STORAGE_ID) ?? '[]'))
+    );
+  }),
+
+  rest.post<{ productId: number }>('/cart-items', (req, res, ctx) => {
+    const { productId } = req.body;
+
+    const storedCartProducts: CartProduct[] = JSON.parse(
+      localStorage.getItem(CART_STORAGE_ID) ?? '[]'
+    );
+
+    if (findTargetProduct(storedCartProducts, productId)) {
+      return res(ctx.status(304), ctx.json({ message: 'Already in the Cart' }));
+    }
+
+    const product = products.find((product) => product.id === productId);
+
+    localStorage.setItem(
+      CART_STORAGE_ID,
+      JSON.stringify([
+        ...storedCartProducts,
+        { id: productId, quantity: 1, product },
+      ])
+    );
+
+    return res(ctx.status(201), ctx.json({ message: 'Success to Create' }));
+  }),
+
+  rest.patch<{ quantity: number }>(
+    '/cart-items/:cartItemId',
+    (req, res, ctx) => {
+      const { cartItemId } = req.params;
+      const { quantity } = req.body;
+
+      const cartProductId = Number(cartItemId as string);
+
+      const storedCartProducts: CartProduct[] = JSON.parse(
+        localStorage.getItem(CART_STORAGE_ID) ?? '[]'
+      );
+
+      if (
+        !storedCartProducts.find(
+          (cartProduct) => cartProduct.id === cartProductId
+        )
+      ) {
+        return res(ctx.status(304), ctx.json({ message: 'Not in the Cart' }));
+      }
+
+      localStorage.setItem(
+        CART_STORAGE_ID,
+        JSON.stringify(
+          storedCartProducts.map((cartProduct) => {
+            if (cartProduct.id === cartProductId) {
+              return { ...cartProduct, quantity };
+            }
+
+            return cartProduct;
+          })
+        )
+      );
+
+      return res(
+        ctx.delay(2000),
+        ctx.status(200),
+        ctx.json({ message: 'Success to Update' })
+      );
+    }
+  ),
+
+  rest.delete('/cart-items/:cartItemId', (req, res, ctx) => {
+    const { cartItemId } = req.params;
+
+    const cartProductId = Number(cartItemId as string);
+
+    const storedCartProducts: CartProduct[] = JSON.parse(
+      localStorage.getItem(CART_STORAGE_ID) ?? '[]'
+    );
+
+    if (
+      !storedCartProducts.find(
+        (cartProduct) => cartProduct.id === cartProductId
+      )
+    ) {
+      return res(ctx.status(304), ctx.json({ message: 'Not in the Cart' }));
+    }
+
+    localStorage.setItem(
+      CART_STORAGE_ID,
+      JSON.stringify(
+        storedCartProducts.filter(
+          (cartProduct) => cartProduct.id === cartProductId
+        )
+      )
+    );
+
+    return res(ctx.delay(2000), ctx.status(204));
   }),
 ];
-
-async function sleep(timeout: number) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, timeout);
-  });
-}
