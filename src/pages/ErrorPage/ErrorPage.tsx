41d583b8994517010f80277c6ef5418b7b470c62
n0eyes <doll4586633@naver.com>
2023-05-19 10:58:54 +0900

    refactor: 에러 페이지의 내용을 커스텀 에러 메세지로 변경

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,12 +1,15 @@
 import * as S from './ErrorPage.styles';
 import Flex from '../../components/common/Flex';
 import { PAGE_ROUTES } from '../../constants/routes';
+import { ERROR_MESSAGE } from '../../constants/errors';
 
-const ErrorPage: React.FC<{ status?: number }> = ({ status }) => {
+const ErrorPage: React.FC<{ message?: string }> = ({
+  message = ERROR_MESSAGE['UNEXPECTED_ERROR'],
+}) => {
   return (
     <Flex dir="column">
       <S.ErrorMessage>
-        <h1>{status ?? '예상치 못한'} 에러가 발생했습니다.</h1>
+        <h1>{message}</h1>
       </S.ErrorMessage>
       <S.LinkToHome to={PAGE_ROUTES.HOME}>Home으로 돌아가기</S.LinkToHome>
     </Flex>
