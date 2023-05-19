8e87f58fd41a0ab0f6912748499f739fc0e6ac71
semnil5202 <semnil5202@gmail.com>
2023-05-19 17:10:40 +0900

    feat: MSW CartList mock API 기능 추가

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,4 +1,5 @@
 import { setupWorker } from 'msw';
 import { productHandlers } from './handlers/productHandlers';
+import { cartHandlers } from './handlers/cartHandlers';
 
-export const worker = setupWorker(...productHandlers);
+export const worker = setupWorker(...productHandlers, ...cartHandlers);
---
+++ src/mocks/handlers/cartHandlers.ts
@@ -0,0 +1,93 @@
+import { rest } from 'msw';
+import { CartProductList } from '../../types/productType';
+
+const getLocalStorageCartLst = localStorage.getItem('cartList');
+
+const setLocalStorage = (newCartList: Omit<CartProductList, 'checked'>[]) =>
+  localStorage.setItem('cartList', JSON.stringify(newCartList));
+
+const isAlreadyInCartList = (id: number) =>
+  cartList.some(
+    (cartItem: Omit<CartProductList, 'checked'>) => cartItem.id === id
+  );
+
+let cartList = getLocalStorageCartLst ? JSON.parse(getLocalStorageCartLst) : [];
+
+export const cartHandlers = [
+  rest.get('/api/carts', (req, res, ctx) => {
+    return res(ctx.delay(10), ctx.status(200), ctx.json(cartList));
+  }),
+
+  rest.post('api/carts', async (req, res, ctx) => {
+    const productId = Number(await req.text());
+
+    if (isAlreadyInCartList(productId)) {
+      return res(
+        ctx.status(409),
+        ctx.json({ message: '이미 장바구니에 존재하는 상품입니다.' })
+      );
+    }
+
+    const product = await fetch(`api/products/${productId}`).then((res) =>
+      res.json()
+    );
+
+    cartList = [
+      ...cartList,
+      {
+        id: productId,
+        quantity: 1,
+        product: { ...product },
+      },
+    ];
+
+    setLocalStorage(cartList);
+
+    return res(
+      ctx.status(201),
+      ctx.set('Location', `/cart-items/${cartList[cartList.length - 1].id}`)
+    );
+  }),
+
+  rest.patch('/cart-items/:id', async (req, res, ctx) => {
+    const cartItemId = Number(req.params.id);
+
+    if (!isAlreadyInCartList) {
+      return res(
+        ctx.status(404),
+        ctx.json({ message: '해당 상품은 장바구니에 존재하지 않습니다.' })
+      );
+    }
+
+    const newQuantity = Number(await req.text());
+
+    cartList = cartList.map((cartItem: Omit<CartProductList, 'checked'>) =>
+      cartItem.id === cartItemId
+        ? { ...cartItem, quantity: newQuantity }
+        : cartItem
+    );
+
+    setLocalStorage(cartList);
+
+    return res(ctx.status(200));
+  }),
+
+  rest.delete('api-items/:id', (req, res, ctx) => {
+    const cartItemId = Number(req.params.id);
+
+    if (!isAlreadyInCartList) {
+      return res(
+        ctx.status(404),
+        ctx.json({ message: '해당 상품은 장바구니에 존재하지 않습니다.' })
+      );
+    }
+
+    cartList = cartList.filter(
+      (cartItem: Omit<CartProductList, 'checked'>) => cartItem.id !== cartItemId
+    );
+
+    setLocalStorage(cartList);
+
+    return res(ctx.status(204));
+  }),
+];
