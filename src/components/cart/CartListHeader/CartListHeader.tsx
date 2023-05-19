00e9f876983ba56b569a2c2c0878316ba9c1d8ff
ashleysyheo <ashleysyheo@gmail.com>
2023-05-19 22:04:11 +0900

    feat: CartListHeader 컴포넌트에 전체선택 checkbox 토글 기능 추가

--- src/components/cart/CartListHeader/CartListHeader.tsx
+++ src/components/cart/CartListHeader/CartListHeader.tsx
@@ -1,21 +1,43 @@
-import { useRecoilValueLoadable } from 'recoil';
+import { useCallback } from 'react';
+import { useRecoilState, useRecoilValue, useRecoilValueLoadable } from 'recoil';
 
+import { CART_LIST_CHECKBOX_KEY } from '../../../constants/store';
+import { useCart } from '../../../hooks/useCart';
 import { cartListItemCountState } from '../../../store/cart';
+import {
+  checkedItemCountState,
+  checkedListState,
+  isAllCheckedState,
+} from '../../../store/checkbox';
 import Checkbox from '../../common/Checkbox/Checkbox';
 import * as S from './CartListHeader.styles';
 
 const CartListHeader = () => {
   const cartListItemCount = useRecoilValueLoadable(cartListItemCountState);
+  const checkedItemCount = useRecoilValue(checkedItemCountState(CART_LIST_CHECKBOX_KEY));
+  const checkedIdList = useRecoilValue(checkedListState(CART_LIST_CHECKBOX_KEY));
+  const [isAllChecked, setIsAllChecked] = useRecoilState(isAllCheckedState(CART_LIST_CHECKBOX_KEY));
+  const { removeCheckedItems } = useCart();
+
+  const onCheckboxClick = useCallback(() => {
+    setIsAllChecked(!isAllChecked);
+  }, [isAllChecked, setIsAllChecked]);
+
+  const deleteCheckedItems = useCallback(() => {
+    removeCheckedItems(Array.from(checkedIdList));
+  }, [checkedIdList, removeCheckedItems]);
 
   return (
     <S.CartListHeaderContainer>
-      <Checkbox isChecked={true} />
+      <Checkbox isChecked={isAllChecked} onClick={onCheckboxClick} />
       <S.CartItemAllSelectText>
-        전체선택 ({cartListItemCount.state === 'hasValue' ? cartListItemCount.contents : 0}/
+        전체선택 ({checkedItemCount}/
         {cartListItemCount.state === 'hasValue' ? cartListItemCount.contents : 0})
       </S.CartItemAllSelectText>
       <S.VerticalLine />
-      <S.CartItemPartialSelectText>선택삭제</S.CartItemPartialSelectText>
+      <S.CartItemPartialSelectText onClick={deleteCheckedItems}>
+        선택삭제
+      </S.CartItemPartialSelectText>
     </S.CartListHeaderContainer>
   );
 };
---
+++ src/constants/store.ts
@@ -0,0 +1,3 @@
+const CART_LIST_CHECKBOX_KEY = 'cartList';
+
+export { CART_LIST_CHECKBOX_KEY };
