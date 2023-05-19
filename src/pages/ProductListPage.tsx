418683c6683502ecd1fbcddc5546407f02e0c8e1
ashleysyheo <ashleysyheo@gmail.com>
2023-05-19 16:12:15 +0900

    feat: ProductListPage 마운트시 product list 데이터 패치 다시하기

--- src/pages/ProductListPage.tsx
+++ src/pages/ProductListPage.tsx
@@ -1,9 +1,19 @@
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
+import { useRecoilCallback } from 'recoil';
 
 import ProductList from '../components/ProductList/ProductList';
 import ProductListSkeleton from '../components/ProductList/ProductListSkeleton';
+import { productListState } from '../store/product';
 
 const ProductListPage = () => {
+  const refreshProductList = useRecoilCallback(({ refresh }) => () => {
+    refresh(productListState);
+  });
+
+  useEffect(() => {
+    refreshProductList();
+  }, [refreshProductList]);
+
   return (
     <Suspense fallback={<ProductListSkeleton />}>
       <ProductList />
