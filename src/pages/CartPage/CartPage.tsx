1789b571920a552eedd8ee404e173077a953e2e4
ashleysyheo <ashleysyheo@gmail.com>
2023-05-19 21:59:15 +0900

    feat: CartPage 마운트시 checkedCartlist값 업데이트하기

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,12 +1,25 @@
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
+import { useRecoilValueLoadable } from 'recoil';
 
 import CartCheckoutBox from '../../components/cart/CartCheckoutBox/CartCheckoutBox';
 import CartList from '../../components/cart/CartList/CartList';
 import CartListSkeleton from '../../components/cart/CartList/CartListSkeleton';
 import CartListHeader from '../../components/cart/CartListHeader/CartListHeader';
+import { CART_LIST_CHECKBOX_KEY } from '../../constants/store';
+import { useCheckboxList } from '../../hooks/common/useCheckboxList';
+import { cartIdListState } from '../../store/cart';
 import * as S from './CartPage.style';
 
 const CartPage = () => {
+  const cartIdList = useRecoilValueLoadable(cartIdListState);
+  const { setInitialCheckedList } = useCheckboxList(CART_LIST_CHECKBOX_KEY);
+
+  useEffect(() => {
+    if (cartIdList.state === 'hasValue') {
+      setInitialCheckedList(cartIdList.contents);
+    }
+  }, [cartIdList.contents, cartIdList.state, setInitialCheckedList]);
+
   return (
     <>
       <S.CartPageHeading size="small">장바구니</S.CartPageHeading>
