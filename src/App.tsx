26efd7ff9a02e5db323c506099ae0323f334cf24
chsua <csj1919@naver.com>
2023-05-19 23:30:38 +0900

    feat: mock장바구니 데이터 통신하여 적용하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
 import { Outlet } from 'react-router-dom';
+import { useSetRecoilState } from 'recoil';
 
 import Header from '@Components/Header';
 
+import ErrorContainer from '@Pages/ProductListPage/ErrorContainer';
+
 import GlobalStyle, { CommonPageStyle } from '@Styles/GlobalStyle';
 
+import useFetch from '@Hooks/useFetch';
+
+import shoppingCartState from '@Atoms/shoppingCartState';
+
+import { getMockSelectItemApiUrl } from './api/index';
+import { ShoppingCartProduct } from './types';
+
 function App() {
+  const { data, isLoading, currentHttpStatus } = useFetch<ShoppingCartProduct[]>(getMockSelectItemApiUrl('GET'));
+  const setShoppingCart = useSetRecoilState<ShoppingCartProduct[]>(shoppingCartState);
+
+  if (!isLoading && !data) {
+    return (
+      <>
+        <GlobalStyle />
+        <Header />
+        <CommonPageStyle>
+          <ErrorContainer error={currentHttpStatus} />
+        </CommonPageStyle>
+      </>
+    );
+  }
+
+  data && setShoppingCart(data);
+
   return (
     <>
       <GlobalStyle />
