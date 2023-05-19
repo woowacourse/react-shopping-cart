8b96f836836a71d935ad18065bfd038fa289e9c2
woo-jk <201702042@o.cnu.ac.kr>
2023-05-20 00:54:51 +0900

    test: CartItem 컴포넌트 스토리북 테스트 컨텍스트 적용

--- src/components/cart/CartItem/CartItem.stories.tsx
+++ src/components/cart/CartItem/CartItem.stories.tsx
@@ -1,27 +1,34 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import CartItem from './CartItem';
+import CheckedCartListProvider from '../../../provider/CheckedListProvider';
+
+const MockCartItem = () => {
+  const cartItem = {
+    id: '1',
+    quantity: 1,
+    product: {
+      id: 0,
+      name: '순살치킨 1KG',
+      price: 9900,
+      imageSrc:
+        'https://cdn-mart.baemin.com/sellergoods/main/c6f2f083-a8b8-4799-834b-444b5eaeb532.png?h=400&w=400',
+    },
+  };
+
+  return (
+    <CheckedCartListProvider>
+      <CartItem cartItem={cartItem} />
+    </CheckedCartListProvider>
+  );
+};
 
 const meta = {
   title: 'CartItem',
-  component: CartItem,
+  component: MockCartItem,
   tags: ['autodocs'],
 } satisfies Meta<typeof CartItem>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {
-    cartItem: {
-      id: '1',
-      quantity: 1,
-      product: {
-        id: 0,
-        name: '순살치킨 1KG',
-        price: 9900,
-        imageSrc:
-          'https://cdn-mart.baemin.com/sellergoods/main/c6f2f083-a8b8-4799-834b-444b5eaeb532.png?h=400&w=400',
-      },
-    },
-  },
-};
+export const Default: Story = {};
