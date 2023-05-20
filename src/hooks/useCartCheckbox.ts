0e1090d72fd79e8f30286d0deb2ebcb879de8ad7
guridaek <xylitol79@gmail.com>
2023-05-20 12:32:45 +0900

    feat: useCartCheckBox 구현

---
+++ src/hooks/useCartCheckbox.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { CartProductList } from "recoil/selector";
+
+export const useCartCheckbox = () => {
+  const [cartList, setCartList] = useRecoilState(CartProductList);
+  const [isAllchecked, setIsAllChecked] = useState(true);
+  const [checkedCount, setCheckedCount] = useState(cartList.length);
+
+  useEffect(() => {
+    const count = cartList.filter((item) => item.isChecked).length;
+
+    setIsAllChecked(count === cartList.length);
+    setCheckedCount(count);
+  }, [cartList]);
+
+  const setAllCheckbox = (isChecked: boolean) => {
+    setCartList(
+      cartList.map((item) => {
+        return { ...item, isChecked: isChecked };
+      })
+    );
+  };
+
+  return { isAllchecked, checkedCount, setAllCheckbox };
+};
