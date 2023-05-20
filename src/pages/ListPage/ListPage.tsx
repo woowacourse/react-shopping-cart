84f77e43b50c7a537c1b0c7535fa4cf21111a547
nangkyeonglim <nangkyeonglim@gmail.com>
2023-05-21 01:19:36 +0900

    feat: 리스트 페이지 초기에 products와 cartList를 fetch

--- src/pages/ListPage/ListPage.tsx
+++ src/pages/ListPage/ListPage.tsx
@@ -1,14 +1,28 @@
 import * as Styled from './ListPage.styles.tsx';
 import ProductItem from '../../components/ProductItem/ProductItem.tsx';
-import useFetchData from '../../hooks/useFetchData.ts';
-import type { Product } from '../../types/index.ts';
+import type { CartItem, Product } from '../../types/index.ts';
+import useGetData from '../../hooks/useGetData.ts';
+import { useEffect } from 'react';
+import { useSetRecoilState } from 'recoil';
+import { cartListAtom } from '../../stores/cartItemsStore.ts';
 
 const ListPage = () => {
-  const { data } = useFetchData<Product[]>('/products');
+  const { data: products, getData: getProducts } = useGetData<Product[]>('/products');
+  const { data: cartList, getData: getCartList } = useGetData<CartItem[]>('/cart-items');
+  const setCartList = useSetRecoilState(cartListAtom);
+
+  useEffect(() => {
+    getProducts();
+    getCartList();
+  }, [getProducts, getCartList]);
+
+  useEffect(() => {
+    if (cartList) setCartList(cartList);
+  }, [cartList, setCartList]);
 
   return (
     <Styled.ProductList>
-      {data?.map((product) => {
+      {products?.map((product) => {
         return <ProductItem key={product.id} {...product} />;
       })}
     </Styled.ProductList>
