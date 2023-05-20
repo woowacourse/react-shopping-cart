0ae32ffb192ab983d3374ade3b3e0354a1949a38
yogjin <youngjin988@gmail.com>
2023-05-20 20:12:57 +0900

    refactor: 첫 페이지인 `ProductListPage` 에서 `useFetch`를 이용하도록 수정

--- src/pages/ProductListPage.tsx
+++ src/pages/ProductListPage.tsx
@@ -1,20 +1,22 @@
+import { api } from 'apis/products/api';
 import { Loading } from 'components/@common/Loading';
 import ProductCardList from 'components/ProductCardList/ProductCardList';
-import useProductsFetch from 'hooks/useProductsFetch';
+import { useFetch } from 'hooks/useFetch';
+import { Product } from 'types/product';
 
 const ProductListPage = () => {
-  const { products, isLoading, error, fetchProducts } = useProductsFetch();
+  const { data, isLoading, error, fetchData } = useFetch<Product[]>(api.getProducts);
 
   if (error) {
     return (
       <>
         <p>{error.message}</p>
-        <button onClick={() => fetchProducts()}>다시 요청하기</button>
+        <button onClick={() => fetchData()}>다시 요청하기</button>
       </>
     );
   }
 
-  return isLoading ? <Loading /> : <ProductCardList products={products} />;
+  return isLoading ? <Loading /> : <ProductCardList products={data ?? []} />;
 };
 
 export default ProductListPage;
