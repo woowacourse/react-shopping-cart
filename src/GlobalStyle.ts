528e36863a89a431761d61cd8f8dabb5fd6ef461
Jungwoo <evencoding@gmail.com>
2023-05-20 21:17:28 +0900

    feat: skeleton 컴포넌트 구현

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -77,6 +77,18 @@ const GlobalStyle = createGlobalStyle`
     text-decoration: none;
   }
 
+  @keyframes skeleton-animation {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+
   @keyframes drawStepper {
     from {
       width: 30px;
