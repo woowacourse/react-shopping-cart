4d8493a9a3dbe8f2c8bc1356926444c37fa2e6c2
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-05-20 10:00:31 +0900

    chore: eslint 규칙 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,6 +11,8 @@ module.exports = {
   },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': 'warn'
+    'react-refresh/only-export-components': 'warn',
+    'eol-last': 2,
+    'semi': 2,
   }
 };
