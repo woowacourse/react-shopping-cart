d621ee996690a0cab2af3ace949763996cda0983
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-05-20 16:22:24 +0900

    feat: api post 추가

--- src/components/pages/ProductsPage/ProductsPage.tsx
+++ src/components/pages/ProductsPage/ProductsPage.tsx
@@ -1,7 +1,32 @@
 import { StyledProductsPage } from '@pages/ProductsPage/ProductsPage.styled';
 import ProductList from '@components/pages/ProductsPage/ProductList/ProductList';
+import useFetch from '@hooks/useFetch';
+import { useRecoilCallback } from 'recoil';
+import { productIdsSelector, quantityListSelector } from '@recoil/selector';
+import { useEffect } from 'react';
 
 const ProductsPage = () => {
+  const { postData, patchData } = useFetch('/cart-items');
+  const handleProductsPageExit = useRecoilCallback(({ snapshot }) => () => {
+    const productIds = snapshot.getLoadable(productIdsSelector).contents;
+    const quantityList = snapshot.getLoadable(quantityListSelector).contents;
+
+    updateCartApiData(productIds, quantityList);
+  });
+
+  const updateCartApiData = (productId: number[], quantityList: number[]) => {
+    productId.forEach(async (productId, index) => {
+      const url = await postData({ productId: productId });
+      if (url) await patchData({ quantity: quantityList[index] }, url);
+    });
+  };
+
+  useEffect(() => {
+    return () => {
+      handleProductsPageExit();
+    };
+  }, [handleProductsPageExit]);
+
   return (
     <StyledProductsPage>
       <ProductList />
