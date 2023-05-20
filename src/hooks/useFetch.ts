55fd610d5f3288f9e7dfdb75b730317296b9ed0f
gilpop8663 <wolfye@naver.com>
2023-05-21 03:37:24 +0900

    feat: useFetch에서 refreshData라는 데이터를 다시 불러오는 함수 구현

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 export const useFetch = <T>(
   url: string,
@@ -7,32 +7,35 @@ export const useFetch = <T>(
   data: T | null;
   isLoading: boolean;
   error: unknown | null;
+  refreshData: () => void;
 } => {
   const [data, setData] = useState<T | null>(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<unknown | null>(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(url, options);
-        if (!response.ok) {
-          throw new Error(
-            `API 요청에 실패했습니다. status : ${response.status}`
-          );
-        }
-
-        const data = await response.json();
-        setData(data);
-        setIsLoading(false);
-      } catch (error) {
-        setError(error);
-        setIsLoading(true);
-      }
-    };
-
+  const refreshData = () => {
     fetchData();
+  };
+
+  const fetchData = useCallback(async () => {
+    try {
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        throw new Error(`API 요청에 실패했습니다. status : ${response.status}`);
+      }
+
+      const data = await response.json();
+      setData(data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      setIsLoading(true);
+    }
   }, [url, options]);
 
-  return { data, isLoading, error };
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return { data, isLoading, error, refreshData };
 };
