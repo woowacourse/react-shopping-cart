767755667f951665e4b73883d26e4561191c3626
ashleysyheo <ashleysyheo@gmail.com>
2023-05-20 21:28:32 +0900

    feat: 카트 아이템 삭제 여부 재차 물어보는 CartItemDelete 컴포넌트 생성

--- src/components/cart/CartItem/CartItem.tsx
+++ src/components/cart/CartItem/CartItem.tsx
@@ -3,11 +3,14 @@ import { memo, useCallback } from 'react';
 import { CloseIcon } from '../../../assets';
 import { CART_LIST_CHECKBOX_KEY } from '../../../constants/store';
 import { useCheckbox } from '../../../hooks/common/useCheckbox';
+import { useModal } from '../../../hooks/common/useModal';
 import { useCartItem } from '../../../hooks/useCartItem';
 import { ProductItemData } from '../../../types';
 import { priceFormatter } from '../../../utils/formatter';
 import Checkbox from '../../common/Checkbox/Checkbox';
+import Modal from '../../common/Modal/Modal';
 import StepperButton from '../../common/StepperButton/StepperButton';
+import CartItemDelete from '../CartItemDelete/CartItemDelete';
 import * as S from './CartItem.styles';
 
 interface CartItemProps extends ProductItemData {
@@ -17,6 +20,7 @@ interface CartItemProps extends ProductItemData {
 const CartItem = ({ id, quantity, name, price, imageUrl }: CartItemProps) => {
   const { updateQuantity, removeItem } = useCartItem(id);
   const { isChecked, toggleItemCheckbox } = useCheckbox(CART_LIST_CHECKBOX_KEY, id);
+  const { isModalOpen, handleModalOpen, handleModalClose } = useModal();
 
   const handleQuantityDecrement = useCallback(() => {
     updateQuantity(quantity - 1);
@@ -40,9 +44,12 @@ const CartItem = ({ id, quantity, name, price, imageUrl }: CartItemProps) => {
         handleCountChange={updateQuantity}
       />
       <S.CartItemPrice>{priceFormatter(price * quantity)}원</S.CartItemPrice>
-      <S.CartItemDeleteButton variant="textButton" onClick={removeItem}>
+      <S.CartItemDeleteButton variant="textButton" onClick={handleModalOpen}>
         <CloseIcon />
       </S.CartItemDeleteButton>
+      <Modal isOpen={isModalOpen} handleClose={handleModalClose}>
+        <CartItemDelete handleModalClose={handleModalClose} removeItem={removeItem} />
+      </Modal>
     </S.CartItemContainer>
   );
 };
---
+++ src/components/cart/CartItemDelete/CartItemDelete.tsx
@@ -0,0 +1,24 @@
+import * as S from './CartItemDelete.styles';
+
+interface CartItemDeleteProps {
+  removeItem: (variables: void) => void;
+  handleModalClose: () => void;
+}
+
+const CartItemDelete = ({ removeItem, handleModalClose }: CartItemDeleteProps) => {
+  return (
+    <S.CartItemDeleteContainer>
+      <S.CartItemDeleteMessage>해당 상품을 삭제하시겠습니까?</S.CartItemDeleteMessage>
+      <S.CartItemDeleteButtonContainer>
+        <S.CartItemDeleteCancelButton variant="textButton" onClick={handleModalClose}>
+          취소
+        </S.CartItemDeleteCancelButton>
+        <S.CartItemDeleteButton variant="textButton" onClick={() => removeItem()}>
+          삭제
+        </S.CartItemDeleteButton>
+      </S.CartItemDeleteButtonContainer>
+    </S.CartItemDeleteContainer>
+  );
+};
+
+export default CartItemDelete;
