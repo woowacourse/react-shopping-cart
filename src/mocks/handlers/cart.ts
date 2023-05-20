994815af5209cab0563710a6566251bf062fdbda
ashleysyheo <ashleysyheo@gmail.com>
2023-05-20 23:12:37 +0900

    refactor: msw json 데이터 값 넘겨줄 때 에러, 데이터 구분해서 넘겨주도록 변경하고 HTTP 코드 상수로 변경

--- src/mocks/handlers/cart.ts
+++ src/mocks/handlers/cart.ts
@@ -1,6 +1,6 @@
 import { rest } from 'msw';
 
-import { API_ENDPOINT } from '../../constants/api';
+import { API_ENDPOINT, HTTP_STATUS_CODE } from '../../constants/api';
 import {
   addCartItemQuantity,
   getCartData,
@@ -8,14 +8,14 @@ import {
   setCartData,
   updateCartItemQuantity,
 } from '../../domain/cart';
-import { CartItemData, PostCartItemRequestBody } from '../../types';
+import { PostCartItemRequestBody } from '../../types';
 import { PatchCartItemRequestBody } from '../../types/api';
 
 const cartHandlers = [
   rest.get(API_ENDPOINT.CART_GET, (req, res, ctx) => {
     const cartList = getCartData();
 
-    return res(ctx.delay(2000), ctx.status(200), ctx.json<CartItemData[]>(cartList));
+    return res(ctx.delay(2000), ctx.status(HTTP_STATUS_CODE.OK), ctx.json({ data: cartList }));
   }),
 
   rest.post(API_ENDPOINT.CART_POST, async (req, res, ctx) => {
@@ -23,9 +23,17 @@ const cartHandlers = [
     const currentCartData = getCartData();
 
     const newCartList = addCartItemQuantity(currentCartData, productId, quantity);
+
+    if (!newCartList) {
+      return res(
+        ctx.status(HTTP_STATUS_CODE.NOT_FOUND),
+        ctx.json({ errorMessage: '해당하는 상품이 없습니다.' })
+      );
+    }
+
     setCartData(newCartList);
 
-    return res(ctx.delay(300), ctx.status(200), ctx.json<CartItemData[]>(newCartList));
+    return res(ctx.delay(300), ctx.status(HTTP_STATUS_CODE.OK), ctx.json({ data: newCartList }));
   }),
 
   rest.patch(`${API_ENDPOINT.CART_PATCH}/:productId`, async (req, res, ctx) => {
@@ -34,23 +42,38 @@ const cartHandlers = [
     const currentCartData = getCartData();
 
     const newCartList = updateCartItemQuantity(currentCartData, Number(productId), quantity);
+
+    if (!newCartList) {
+      return res(
+        ctx.status(HTTP_STATUS_CODE.NOT_FOUND),
+        ctx.json({ errorMessage: '해당하는 상품이 없습니다.' })
+      );
+    }
+
     setCartData(newCartList);
 
-    return res(ctx.delay(300), ctx.status(200), ctx.json<CartItemData[]>(newCartList));
+    return res(
+      ctx.delay(300),
+      ctx.status(HTTP_STATUS_CODE.NOT_FOUND),
+      ctx.json({ data: newCartList })
+    );
   }),
 
   rest.delete(`${API_ENDPOINT.CART_DELETE}/:productId`, (req, res, ctx) => {
     const { productId } = req.params;
     const currentCartData = getCartData();
+    const newCartList = removeCartItem(currentCartData, Number(productId));
 
-    if (currentCartData.length === 0) {
-      return res(ctx.status(404));
+    if (currentCartData.length === 0 || !newCartList) {
+      return res(
+        ctx.status(HTTP_STATUS_CODE.NOT_FOUND),
+        ctx.json({ errorMessage: '장바구니가 비어있거나 해당하는 상품이 장바구니에 없습니다.' })
+      );
     }
 
-    const newCartList = removeCartItem(currentCartData, Number(productId));
     setCartData(newCartList);
 
-    return res(ctx.delay(300), ctx.status(204));
+    return res(ctx.delay(300), ctx.status(HTTP_STATUS_CODE.NO_CONTENT));
   }),
 ];
 
--- src/mocks/handlers/product.ts
+++ src/mocks/handlers/product.ts
@@ -1,12 +1,15 @@
 import { rest } from 'msw';
 
-import { API_ENDPOINT } from '../../constants/api';
+import { API_ENDPOINT, HTTP_STATUS_CODE } from '../../constants/api';
 import productListData from '../../data/mockData.json';
-import { ProductItemData } from '../../types';
 
 const productHandlers = [
   rest.get(API_ENDPOINT.PRODUCTS_GET, (req, res, ctx) => {
-    return res(ctx.delay(2000), ctx.status(200), ctx.json<ProductItemData[]>(productListData));
+    return res(
+      ctx.delay(2000),
+      ctx.status(HTTP_STATUS_CODE.OK),
+      ctx.json({ data: productListData })
+    );
   }),
 ];
 
