6f1f8c14c697d365945e50be35a9844f906c4424
bassyu <yrkdgus@gmail.com>
2023-05-20 14:22:44 +0900

    refactor(CartItemList): checkedList 변경 함수 수정
    - 체크된 아이템 삭제하는 함수에서 api 요청 추가

--- src/components/cart/CartItemList.tsx
+++ src/components/cart/CartItemList.tsx
@@ -1,34 +1,50 @@
-import { useState } from 'react';
-import styled from 'styled-components';
+import { useEffect } from 'react';
 import { useRecoilState } from 'recoil';
+import styled from 'styled-components';
 
+import * as api from '../../api';
 import CartItem from './CartItem';
-import CheckBox from '../leafs/CheckBox';
-import { cartState } from '../../recoil/state';
+import CheckBox from '../common/CheckBox';
+import { cartState, checkedListState } from '../../recoil/state';
 
 export default function CartItemList() {
   const [cart, setCart] = useRecoilState(cartState);
-  const [checkedList, setCheckedList] = useState<boolean[]>(Array(cart.length).fill(true));
+  const [checkedList, setCheckedList] = useRecoilState(checkedListState);
 
   const checkedCount = checkedList.filter((checked) => checked).length;
   const allChecked = checkedCount === cart.length;
 
   const checkAll = () => {
-    setCheckedList(Array(cart.length).fill(true));
+    setCheckedList(checkedList.map((_checked) => true));
   };
 
   const uncheckAll = () => {
-    setCheckedList(Array(cart.length).fill(false));
+    setCheckedList(checkedList.map((_checked) => false));
   };
 
-  const toggleChecked = (index: number) => () => {
+  const toggleCheckedList = (index: number) => () => {
     setCheckedList(checkedList.toSpliced(index, 1, !checkedList[index]));
   };
 
-  const removeChecked = () => {
+  const deleteCheckedList = (index: number) => () => {
+    setCheckedList(checkedList.toSpliced(index, 1));
+  };
+
+  const removeCheckedCartItem = () => {
+    checkedList.forEach((checked, index) => {
+      if (checked) api.deleteCartItem(cart[index].id);
+    });
+
     setCart(cart.filter((_, index) => checkedList[index] === false));
     setCheckedList(checkedList.filter((checked) => checked === false));
   };
+
+  useEffect(() => {
+    api.getCart().then((cart) => {
+      setCart(cart);
+      setCheckedList(Array(cart.length).fill(true));
+    });
+  }, []);
 
   return (
     <Wrapper>
@@ -38,7 +54,8 @@ export default function CartItemList() {
             <CartItem
               {...cartItem}
               checked={checkedList[index]}
-              toggleChecked={toggleChecked(index)}
+              toggleChecked={toggleCheckedList(index)}
+              deleteChecked={deleteCheckedList(index)}
             />
           </ListItemBox>
         ))}
@@ -48,7 +65,7 @@ export default function CartItemList() {
         <RemoveLabel>
           전체선택 ({checkedCount}/{cart.length})
         </RemoveLabel>
-        <RemoveButton onClick={removeChecked}>선택삭제</RemoveButton>
+        <RemoveButton onClick={removeCheckedCartItem}>선택삭제</RemoveButton>
       </RemoveBox>
     </Wrapper>
   );
@@ -64,7 +81,7 @@ const Wrapper = styled.div`
 
 const List = styled.div`
   min-height: 128px;
-  max-height: 600px;
+  max-height: 612px;
   border-top: 4px solid #aaaaaa;
 
   overflow: scroll;
