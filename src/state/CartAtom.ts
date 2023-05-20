0abb99753a6e3f7a4856dd0f3b26b4e3e0434035
yogjin <youngjin988@gmail.com>
2023-05-20 20:16:28 +0900

    feat: `cartState` atom을 생성 시 실제 서버에 요청한 장바구니 상품으로 초기화 하도록 함
    서버 처럼 이용하던 localStorage를 더 이상 사용하지 않기 때문에 `localStorageEffect` atom Effect 삭제, 수량이 없는 장바구니 상품이 없도록 하는 `zeroQuantityFilterEffect` 는 effect로 filter를 하기 전 quantity가 0인 상품을 서버에 요청을 보낼 수 있으니(cartState가 항상 quantity가 0인 상품을 가지고 있다는 것을 보장할 수 없음) 삭제 (다른 방법을 고민해야함)

--- src/state/CartAtom.ts
+++ src/state/CartAtom.ts
@@ -1,31 +1,15 @@
-import { CART_KEY } from 'constants/storeKey';
-import { AtomEffect, atom, selector, selectorFamily } from 'recoil';
+import { api } from 'apis/products/api';
+import { atom, selector, selectorFamily } from 'recoil';
 import { CartProduct } from 'types/product';
-import store from 'utils/localStorage';
-
-const zeroQuantityFilterEffect: AtomEffect<CartProduct[]> = ({ onSet, setSelf }) => {
-  onSet((newValue) => {
-    setSelf(newValue.filter((v) => v.quantity > 0));
-  });
-};
-
-const localStorageEffect =
-  <T>(key: string, initialValue: T): AtomEffect<T> =>
-  ({ onSet, setSelf, trigger }) => {
-    if (trigger === 'get') {
-      const storageValue = store.getStorage<T>(key) ?? initialValue;
-      setSelf(storageValue);
-    }
-
-    onSet((newValue) => {
-      store.setStorage(key, newValue);
-    });
-  };
 
 export const cartState = atom<CartProduct[]>({
   key: 'cartState',
-  default: [],
-  effects: [zeroQuantityFilterEffect, localStorageEffect<CartProduct[]>(CART_KEY, [])],
+  default: selector({
+    key: 'cartState/Default',
+    get: () => {
+      return api.getCartProducts();
+    },
+  }),
 });
 
 export const filteredCartProductState = selectorFamily({
