0ed0cc3fc819bfa8427c4aadce1913939f0c6767
woo-jk <201702042@o.cnu.ac.kr>
2023-05-20 17:20:04 +0900

    feat: MSW 장바구니 아이템 get 요청 연동

--- src/globalState/atoms/cartState.ts
+++ src/globalState/atoms/cartState.ts
@@ -2,25 +2,25 @@ import { atom } from 'recoil';
 import type { AtomEffect } from 'recoil';
 import type { CartProduct } from '../../types/product';
 
-const localStorageEffect: <T>(key: string) => AtomEffect<T> =
-  (key: string) =>
-  ({ setSelf, onSet }) => {
-    const savedValue = localStorage.getItem(key);
-    if (savedValue !== null) {
-      setSelf(JSON.parse(savedValue));
-    }
+const fetchEffect: AtomEffect<CartProduct[]> = ({ setSelf, trigger }) => {
+  const fetchCartItemList = async () => {
+    const response = await fetch('./cart-items');
 
-    onSet((newValue, _, isReset) => {
-      isReset
-        ? localStorage.removeItem(key)
-        : localStorage.setItem(key, JSON.stringify(newValue));
-    });
+    if (response.status !== 200) throw new Error('서버에 장애가 발생했습니다.');
+
+    const cartItemList = await response.json();
+    setSelf(cartItemList);
   };
+
+  if (trigger === 'get') {
+    fetchCartItemList();
+  }
+};
 
 const cartState = atom<CartProduct[]>({
   key: 'cartState',
   default: [],
-  effects: [localStorageEffect<CartProduct[]>('cart_list')],
+  effects: [fetchEffect],
 });
 
 export default cartState;
