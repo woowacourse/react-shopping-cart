dabd9bec2da7b39eb8fcfc92775214c9ae999bd2
ashleysyheo <ashleysyheo@gmail.com>
2023-05-21 01:24:01 +0900

    feat: useMutationFetch 커스텀 훅 생성

---
+++ src/hooks/common/useMutationFetch.ts
@@ -0,0 +1,48 @@
+import { useCallback, useState } from 'react';
+
+import HTTPError from '../../api/HTTPError';
+
+type MutationFunction<T, V> = (variables: V) => Promise<T>;
+
+interface MutationOptions<T> {
+  onSuccess?: (data: T) => void;
+  onError?: (error: Error | HTTPError) => void;
+}
+
+interface MutationResult<V> {
+  mutate: (variables: V) => void;
+  state: {
+    isLoading: boolean;
+    error: Error | HTTPError | null;
+  };
+}
+
+const useMutationFetch = <T, V = undefined>(
+  mutationFn: MutationFunction<T, V>,
+  { onSuccess, onError }: MutationOptions<T> = {}
+): MutationResult<V> => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<Error | HTTPError | null>(null);
+
+  const mutate = useCallback(
+    (variables: V) => {
+      setIsLoading(true);
+      mutationFn(variables)
+        .then((data) => {
+          setIsLoading(false);
+          setError(null);
+          onSuccess?.(data);
+        })
+        .catch((error) => {
+          setIsLoading(false);
+          setError(error);
+          onError?.(error);
+        });
+    },
+    [mutationFn, onSuccess, onError]
+  );
+
+  return { mutate, state: { isLoading, error } };
+};
+
+export { useMutationFetch };
