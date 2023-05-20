cdbec7fffd80d9dac2217a2ad4d85b35641e7fc7
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-05-20 21:42:40 +0900

    feat: 장바구니 첫 추가 post msw 핸들러 구현

--- src/mocks/handlers/cart.ts
+++ src/mocks/handlers/cart.ts
@@ -1,8 +1,32 @@
 import { rest } from 'msw';
 import cartProducts from 'mocks/fixtures/cartProducts.json';
 
+type ErrorResponse = {
+  message: string;
+};
+
+type PostReqBody = {
+  productId: number;
+};
+
+const authorizationError: ErrorResponse = { message: '인증실패' };
+
 export const cart = [
   rest.get('/cart-items', (req, res, ctx) => {
-    return res(ctx.delay(2000), ctx.status(200), ctx.json(cartProducts));
+    return res(ctx.delay(500), ctx.status(200), ctx.json(cartProducts));
+  }),
+
+  rest.post<PostReqBody>('/cart-items', async (req, res, ctx) => {
+    const { productId } = await req.json<PostReqBody>();
+    const authorization = req.headers.get('Authorization');
+
+    if (authorization !== 'Basic bob:486') {
+      return res(ctx.status(401), ctx.json(authorizationError));
+    }
+
+    return res(ctx.status(201), ctx.set('Location', `/cart-items/${productId}`), ctx.json({}));
+    // 아래 명세상 원래는 productId가 아닌, cartItemId가 반환됨
+    // https://techcourse.woowahan.com/s/zNFZ8xuU/ls/gRaMDVpX
+    // Location: /cart-items/{cartItemId}
   }),
 ];
