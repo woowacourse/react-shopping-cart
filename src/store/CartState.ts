09a5db11fa09c86b5d9b4db9baeb6ab403f3e474
regularPark <srk99279@naver.com>
2023-05-21 02:32:33 +0900

    refactor: AtomEffect 제거

--- src/store/CartState.ts
+++ src/store/CartState.ts
@@ -1,24 +1,8 @@
-import type { AtomEffect } from 'recoil';
 import { atom } from 'recoil';
 import { CartItem } from '../types';
-
-const localStorageEffect: <T>(key: string) => AtomEffect<T> = (key: string) => ({
-  setSelf,
-  onSet,
-}) => {
-  const savedValue = localStorage.getItem(key);
-  if (savedValue !== null) {
-    setSelf(JSON.parse(savedValue));
-  }
-
-  onSet((newValue, _, isReset) => {
-    if (isReset) return localStorage.removeItem(key);
-    return localStorage.setItem(key, JSON.stringify(newValue));
-  });
-};
+import { getLocalStorage } from '../utils/localStorage';
 
 export const cartState = atom<CartItem[]>({
   key: 'cartState',
-  default: [],
-  effects: [localStorageEffect<CartItem[]>('cart')],
+  default: getLocalStorage<CartItem[]>('cart', []),
 });
