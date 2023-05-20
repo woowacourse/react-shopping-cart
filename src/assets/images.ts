190bfd3fda86a37adc518b2e919915c8ebff13cb
Patrick <kuyt3122@naver.com>
2023-05-20 18:42:37 +0900

    fix: 이미지 export 하던 코드 삭제로 에러 해결

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -3,16 +3,3 @@ export { default as BUCKET_BUTTON } from './bucket-button.svg';
 export { default as TOP_ARROW } from './top-arrow.svg';
 export { default as BOTTOM_ARROW } from './bottom-arrow.svg';
 export { default as CART_ITEM_REMOVE_BUTTON } from './cart-remove-button.svg';
-
-export { default as A_MOCK } from './mock/1.png';
-export { default as B_MOCK } from './mock/2.png';
-export { default as C_MOCK } from './mock/3.png';
-export { default as D_MOCK } from './mock/4.png';
-export { default as E_MOCK } from './mock/5.png';
-export { default as F_MOCK } from './mock/6.png';
-export { default as G_MOCK } from './mock/7.png';
-export { default as H_MOCK } from './mock/8.png';
-export { default as I_MOCK } from './mock/9.png';
-export { default as J_MOCK } from './mock/10.png';
-export { default as K_MOCK } from './mock/11.png';
-export { default as L_MOCK } from './mock/12.png';
