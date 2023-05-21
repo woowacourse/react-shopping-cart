0ddeccd8aac97d1b88ecacf227e5dd2a14f01e16
Leejin-Yang <ejin1996@gmail.com>
2023-05-21 21:09:26 +0900

    refactor: 메세지 상수 분리

--- src/components/Common/Message.tsx
+++ src/components/Common/Message.tsx
@@ -1,40 +1,12 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-type MessageType = 'error' | 'empty' | 'notFound' | 'loading' | 'cartEmpty';
+import { MESSAGE, MessageKey } from '../../constants/message';
 
 interface MessageProps {
-  type: MessageType;
+  type: MessageKey;
   link?: boolean;
 }
-
-const message = {
-  error: {
-    title: '에러가 발생했습니다.',
-    description: '새로고침 해주세요.',
-    imageSrc: 'images/error.png',
-  },
-  empty: {
-    title: '상품을 찾을 수 없습니다.',
-    description: '새로고침 해주세요.',
-    imageSrc: 'images/error.png',
-  },
-  notFound: {
-    title: '페이지를 찾을 수 없습니다.',
-    description: '페이지가 존재하지 않거나 삭제되어 찾을 수 없어요.',
-    imageSrc: 'images/error.png',
-  },
-  loading: {
-    title: '로딩중입니다.',
-    description: '잠시만 기다려주세요.',
-    imageSrc: 'images/loading.png',
-  },
-  cartEmpty: {
-    title: '장바구니에 상품이 없습니다.',
-    description: '상품을 추가해보세요.',
-    imageSrc: 'images/lay-down.png',
-  },
-};
 
 const Message = ({ type, link = false }: MessageProps) => {
   return (
@@ -42,11 +14,11 @@ const Message = ({ type, link = false }: MessageProps) => {
       <img
         width={160}
         height={160}
-        src={message[type].imageSrc}
-        alt={message[type].title}
+        src={MESSAGE[type].imageSrc}
+        alt={MESSAGE[type].title}
       />
-      <MessageTitle>{message[type].title}</MessageTitle>
-      <MessageDesc>{message[type].description}</MessageDesc>
+      <MessageTitle>{MESSAGE[type].title}</MessageTitle>
+      <MessageDesc>{MESSAGE[type].description}</MessageDesc>
       {link && <HomeLink to='/'>홈으로 가기</HomeLink>}
     </MessageSection>
   );
