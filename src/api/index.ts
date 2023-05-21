261dd1de1f8e1fd3d62a8aae41ea0be42346afb2
gyeongza <gyeongza@naver.com>
2023-05-21 20:57:42 +0900

    [step2] feat: fetch 에러처리 구현

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,10 @@
-export const fetchApi = async (url: string, options: RequestInit) => {
-  if (!navigator.onLine) {
-    throw new Error('네트워크 연결이 끊어졌습니다.');
-  }
+export const fetchAPI = async (url: string, options: RequestInit) => {
   const response = await fetch(url, options);
 
   if (!response.ok) {
-    throw new Error('데이터를 가져오는데 오류가 발생했습니다.');
+    throw new Error(
+      `${response.status}: ${response.text} 오류가 발생했습니다.`
+    );
   }
 
   return response.json();
