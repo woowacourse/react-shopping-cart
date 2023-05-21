bc042c3493ac8c0a491cd5937cdff35ddf08a7c6
Dain Lee <feb.dain@gmail.com>
2023-05-21 14:32:09 +0900

    design: 체크박스 커스텀 디자인으로 변경

--- src/components/common/CheckboxStyle.tsx
+++ src/components/common/CheckboxStyle.tsx
@@ -1,7 +1,44 @@
-import { styled } from "styled-components";
+import { styled } from 'styled-components';
 
 export const Checkbox = styled.input`
+  all: unset;
+  position: relative;
   width: 28px;
   height: 28px;
   margin-right: 14px;
+  border: 1px solid var(--gray-color-200);
+  cursor: pointer;
+
+  &:checked {
+    background: var(--gray-color-300);
+  }
+
+  &:checked::after {
+    display: block;
+    content: '';
+    width: 17.99px;
+    height: 10.06px;
+    margin: 4px auto;
+    border: 3px solid #ffffff;
+    border-top: 0;
+    border-right: 0;
+
+    transform: rotate(-45deg);
+  }
+
+  @media (max-width: 548px) {
+    min-width: 20px;
+    width: 20px;
+    height: 20px;
+    margin-right: 10px;
+
+    &:checked::after {
+      width: 12.99px;
+      height: 5.06px;
+      margin: 4px auto;
+      border: 3px solid #ffffff;
+      border-top: 0;
+      border-right: 0;
+    }
+  }
 `;
