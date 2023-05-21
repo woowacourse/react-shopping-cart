a3317f1ee2829ddadee392b017928eb36a292164
AhnGeunTaek <dksrmsxor@gmail.com>
2023-05-21 18:11:48 +0900

    refactor: 수량 입력 버튼 스토리북 네이밍 변경

---
+++ src/stories/QuantityInputButton.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import QuantityInputButton from '../components/QuantityInputButton/QuantityInputButton';
+
+/**
+ * 제품의 수량을 입력하는 QuantityInputButton 컴포넌트입니다.
+ */
+
+const meta = {
+  title: 'QuantityInputButton',
+  component: QuantityInputButton,
+} satisfies Meta<typeof QuantityInputButton>;
+
+type Story = StoryObj<typeof meta>;
+
+export const Standard: Story = {
+  args: { productId: 8 },
+};
+
+export default meta;
