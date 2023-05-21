6e30156bde5cb1f6cfe7a61588000ea02cf06993
n0eyes <doll4586633@naver.com>
2023-05-21 20:35:01 +0900

    refactor: msw AddCart handler 로직 수정

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,26 +1,35 @@
-import { getUUID } from './../utils/uuid';
 import { rest } from 'msw';
 import { MockCart } from './fixtures/cart';
 import { MockProducts } from './fixtures/products';
+import { AddCartDataReq } from '../apis/cart';
 
 export const handlers = [
-  rest.post('/cart/:id', (req, res, ctx) => {
-    const { id, quantity } = req.params;
+  rest.post('/cart/:id', async (req, res, ctx) => {
+    const { id, quantity }: AddCartDataReq = await req.json();
 
-    const product = MockProducts.items.find((item) => item.id === Number(id));
+    const itemInCart = MockCart.cart.find(({ product }) => product.id === id);
+    const product = MockProducts.items.find((product) => product.id === id);
 
-    if (product) {
+    if (!product) return res(ctx.status(404), ctx.json({}));
+
+    if (itemInCart) {
       const newCartItem = {
-        id: getUUID(),
-        quantity: Number(quantity),
-        products: product,
+        ...structuredClone(itemInCart),
+        quantity: itemInCart.quantity + quantity,
       };
-      MockCart.cart.push(newCartItem);
 
-      return res(ctx.status(200), ctx.json(MockCart));
+      MockCart.cart.push(newCartItem);
+    } else {
+      const newCartItem = {
+        id,
+        quantity,
+        product,
+      };
+
+      MockCart.cart.push(newCartItem);
     }
 
-    return res(ctx.status(404));
+    return res(ctx.status(200), ctx.json(MockCart));
   }),
 
   rest.get('/products', (req, res, ctx) => {
