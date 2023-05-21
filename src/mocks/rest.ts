8f0ddd96eba30129eb35a569284adf35bb0780e2
solo5star <solo_5@naver.com>
2023-05-22 05:16:21 +0900

    feat: 배포 경로 설정

--- src/mocks/rest.ts
+++ src/mocks/rest.ts
@@ -4,6 +4,8 @@ import type { ExtractPathFromRestAPI, RestAPI } from '../api/rest/RestAPI';
 import type { ShoppingCartRestAPI } from '../api/rest/ShoppingCartRestAPI';
 
 class Rest<TRestAPI extends RestAPI> {
+  constructor(private readonly baseUrl: string) {}
+
   on<
     Method extends TRestAPI['request']['method'],
     Path extends ExtractPathFromRestAPI<TRestAPI, Method>,
@@ -18,10 +20,10 @@ class Rest<TRestAPI extends RestAPI> {
     >,
   ) {
     const [method, path] = endpoint.split(' ');
-    return mswRest[method.toLowerCase() as Lowercase<Method>](path, resolver);
+    return mswRest[method.toLowerCase() as Lowercase<Method>](path + this.baseUrl, resolver);
   }
 }
 
-const rest = new Rest<ShoppingCartRestAPI>();
+const rest = new Rest<ShoppingCartRestAPI>(import.meta.env.BASE_URL);
 
 export default rest;
