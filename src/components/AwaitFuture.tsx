8873722072ab71d290e89a066380676edf882649
solo5star <solo_5@naver.com>
2023-05-22 04:25:50 +0900

    refactor: AwaitFuture로 이름 변경

--- src/components/AwaitFuture.tsx
+++ src/components/AwaitFuture.tsx
@@ -2,35 +2,35 @@ import { Suspense } from 'react';
 import type Future from '../utils/Future';
 import ErrorBoundary from './ErrorBoundary';
 
-type FutureLoaderProps<TData> = {
+type AwaitFutureLoaderProps<TData> = {
   future: Future<TData>;
   children: (data: TData) => React.ReactElement;
 };
 
-const FutureLoader = <TData,>(props: FutureLoaderProps<TData>) => {
+const AwaitFutureLoader = <TData,>(props: AwaitFutureLoaderProps<TData>) => {
   const { future, children } = props;
   const data = future.unwrap();
 
   return children(data);
 };
 
-type FutureSuspenseProps<TData> = {
+type AwaitFutureProps<TData> = {
   future: Future<TData>;
   loadingElement?: React.ReactNode;
   errorElement?: React.ReactNode;
   children: (data: TData) => React.ReactElement;
 };
 
-const FutureSuspense = <TData,>(props: FutureSuspenseProps<TData>) => {
+const AwaitFuture = <TData,>(props: AwaitFutureProps<TData>) => {
   const { future, loadingElement, errorElement, children } = props;
 
   return (
     <ErrorBoundary fallback={errorElement}>
       <Suspense fallback={loadingElement}>
-        <FutureLoader future={future}>{children}</FutureLoader>
+        <AwaitFutureLoader future={future}>{children}</AwaitFutureLoader>
       </Suspense>
     </ErrorBoundary>
   );
 };
 
-export default FutureSuspense;
+export default AwaitFuture;
