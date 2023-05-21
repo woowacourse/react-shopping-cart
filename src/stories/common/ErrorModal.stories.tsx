6c2783e48ee74ea47ff84c05e0e95267fc05e5c9
ashleysyheo <ashleysyheo@gmail.com>
2023-05-22 00:03:03 +0900

    refactor: ErrorModal 컴포넌트 스토리 버튼 없애고 바로 모달 보여주도록 수정

--- src/stories/common/ErrorModal.stories.tsx
+++ src/stories/common/ErrorModal.stories.tsx
@@ -1,7 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { useSetRecoilState } from 'recoil';
 
-import Button from '../../components/common/Button/Button';
 import ErrorModal from '../../components/common/ErrorModal/ErrorModal';
 import { errorModalMessageState } from '../../store/error';
 
@@ -16,17 +15,8 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   render: () => {
     const setErrorMessage = useSetRecoilState(errorModalMessageState);
+    setErrorMessage('일시적인 오류가 발생했습니다.');
 
-    return (
-      <>
-        <Button
-          style={{ width: '250px' }}
-          onClick={() => setErrorMessage('일시적인 오류가 발생했습니다.')}
-        >
-          Click
-        </Button>
-        <ErrorModal />
-      </>
-    );
+    return <ErrorModal />;
   },
 };
