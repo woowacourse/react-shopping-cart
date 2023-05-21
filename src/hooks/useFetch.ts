45b4b23389b675ddccb29886139a3e5131341ad1
sh981013s <sh981013s@gmail.com>
2023-05-22 02:18:46 +0900

    feat: useFetch hook param 받을 수 있도록 추가

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -3,13 +3,13 @@ import { useNavigate } from 'react-router-dom';
 
 type FetchStatus = 'idle' | 'loading' | 'fail' | 'success';
 
-type FetchState<T> = {
+export type FetchState<T> = {
   status: FetchStatus;
   data: T | null;
   error: Error | null;
 };
 
-const useFetch = <T>(url: string, method = 'GET'): [FetchState<T>, (body?: any) => Promise<void>] => {
+const useFetch = <T>(url: string, method = 'GET'): [FetchState<T>, (body?: any, param?: number | string) => Promise<void>] => {
   const [fetchState, setFetchState] = useState<FetchState<T>>({
     status: 'idle',
     data: null,
@@ -18,14 +18,22 @@ const useFetch = <T>(url: string, method = 'GET'): [FetchState<T>, (body?: any) 
   const navigate = useNavigate();
 
   const fetchData = useCallback(
-    async (body?: any) => {
+    async ({ body, param }: { body?: any; param?: string | number }) => {
+      const urlWithParam = param ? `${url}/${param}` : url;
+
       setFetchState({ status: 'loading', data: null, error: null });
 
       try {
-        const response: Response = await fetch(url, { method, body: body ? JSON.stringify(body) : null, headers: { 'Content-Type': 'application/json' } });
+        const response: Response = await fetch(urlWithParam, {
+          method,
+          body: body ? JSON.stringify(body) : null,
+          headers: { 'Content-Type': 'application/json' },
+        });
+
         if (!response.ok) {
           throw new Error(response.statusText);
         }
+
         const data: T = await response.json();
         setFetchState({ status: 'success', data, error: null });
       } catch (error) {
@@ -33,12 +41,12 @@ const useFetch = <T>(url: string, method = 'GET'): [FetchState<T>, (body?: any) 
         navigate('/error', { state: { error: error as Error } });
       }
     },
-    [url, method]
+    [url, method, navigate]
   );
 
   useEffect(() => {
     if (method.toLowerCase() === 'get') {
-      fetchData();
+      fetchData({});
     }
   }, [fetchData, method]);
 
