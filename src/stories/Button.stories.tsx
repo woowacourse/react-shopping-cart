a17d9bc3a1fc9b24f8c36f36c96f8ad636e8da28
Leejin-Yang <ejin1996@gmail.com>
2023-05-22 01:32:24 +0900

    teat: Button 스토리 추가 작성

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -11,6 +11,26 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+const DEFAULT_ARGS = {
+  children: 'Button',
+};
+
 export const Default: Story = {
-  args: { children: 'Button' },
+  args: DEFAULT_ARGS,
+};
+
+export const Primary: Story = {
+  args: { ...DEFAULT_ARGS, primary: true },
+};
+
+export const Secondary: Story = {
+  args: { ...DEFAULT_ARGS, primary: false },
+};
+
+export const SecondaryWithBorder: Story = {
+  args: { ...DEFAULT_ARGS, primary: false, border: true },
+};
+
+export const Disabled: Story = {
+  args: { ...DEFAULT_ARGS, disabled: true },
 };
