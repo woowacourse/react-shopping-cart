4fe09e24f2310fa503e352330fd3ad05d607d5ae
sh981013s <sh981013s@gmail.com>
2023-05-22 02:56:43 +0900

    refactor: production 용 baseURL 변경

--- config.ts
+++ config.ts
@@ -2,6 +2,6 @@
 // @ts-ignore
 const env: any = import.meta.env;
 
-const baseURL: string = env.MODE === 'production' ? 'https://step2--shopping-cart-booung.netlify.app/' : 'http://localhost:3000';
+const baseURL: string = env.MODE === 'production' ? 'https://step2--shopping-cart-booung.netlify.app' : 'http://localhost:3000';
 
 export default baseURL;
