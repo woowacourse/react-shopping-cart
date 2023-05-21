95111576379338784c5f078bc213184cdf716aa1
Suyoung <jsy8492@gmail.com>
2023-05-22 00:06:13 +0900

    feat: api 요청 성공 또는 에러 발생 시 toast 출력 기능 구현

--- src/hooks/useCartService.ts
+++ src/hooks/useCartService.ts
@@ -2,56 +2,68 @@ import { useRecoilState } from 'recoil';
 import { cartState } from '../recoil/atoms';
 import { CART_BASE_URL } from '../constants';
 import type { CartItem, Product } from '../types/product';
+import useToast from '../components/common/Toast/useToast';
 
 const useCartService = () => {
   const [cart, setCart] = useRecoilState(cartState);
 
-  const addProductToCart = (productId: Product['id']) => {
     fetch(CART_BASE_URL, {
+  const { showToast } = useToast();
+
+  const addProductToCart = async (productId: Product['id']) => {
+    const response = await fetch(CART_BASE_URL, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(productId),
-    }).then(() => {
-      fetch(CART_BASE_URL)
-        .then((res) => res.json())
-        .then((data) => setCart(data));
     });
+
+    if (!response.ok) {
+      const { message } = await response.json();
+      showToast('error', message);
+    }
+
+    showToast('success', '장바구니에 추가되었습니다.');
   };
 
-  const updateProductQuantity = (
+  const updateProductQuantity = async (
     targetId: Product['id'],
     quantity: CartItem['quantity'],
   ) => {
-    fetch(`${CART_BASE_URL}/${targetId}`, {
+    const response = await fetch(`${CART_BASE_URL}/${targetId}`, {
       method: 'PATCH',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(quantity),
-    }).then(() => {
-      fetch(CART_BASE_URL)
-        .then((res) => res.json())
-        .then((data) => setCart(data));
     });
+
+    if (!response.ok) {
+      const { message } = await response.json();
+      showToast('error', message);
+    }
+
   };
 
-  const removeProductFromCart = (targetId: Product['id']) => {
-    fetch(`${CART_BASE_URL}/${targetId}`, {
+  const removeProductFromCart = async (targetId: Product['id']) => {
+    const response = await fetch(`${CART_BASE_URL}/${targetId}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-    }).then(() => {
-      fetch(CART_BASE_URL)
-        .then((res) => res.json())
-        .then((data) => setCart(data));
     });
+
+    if (!response.ok) {
+      const { message } = await response.json();
+      showToast('error', message);
+    }
+
   };
 
   const removeAllProductsFromCart = (targetIds: Array<Product['id']>) => {
     targetIds.forEach((id) => removeProductFromCart(id));
+    showToast('success', '장바구니에서 삭제되었습니다.');
   };
 
   return {
