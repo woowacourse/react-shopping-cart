32fdd9f49a899babf6967bb4d9d83a66316274aa
woo-jk <201702042@o.cnu.ac.kr>
2023-05-21 22:30:39 +0900

    refactor: useGet->useFetch 네이밍 변경 및 Suspense 설정 적용(fetch 완료 전까지 Promise throw)

---
+++ src/hooks/api/useFetch.ts
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+
+const useFetch = <T>(url: string) => {
+  const [data, setData] = useState<T | null>(null);
+  const [promise, setPromise] = useState<Promise<void>>();
+
+  const fetchData = async () => {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error('데이터를 불러오는 과정에서 문제가 생겼습니다.');
+    }
+
+    const fetchedData = await response.json();
+    setData(fetchedData);
+  };
+
+  useEffect(() => {
+    setPromise(fetchData());
+  }, []);
+
+  const getData = () => {
+    if (data === null && promise) {
+      throw promise;
+    }
+    return data;
+  };
+
+  return { getData, fetchData };
+};
+
+export default useFetch;
