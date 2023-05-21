cc53fdeb9992493972b14b22d6314c990a7086a4
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-05-21 19:32:01 +0900

    feat: 장바구니 리스트가 비어있을 때 대응하는 페이지 추가

--- src/pages/Cart/Cart.style.ts
+++ src/pages/Cart/Cart.style.ts
@@ -20,3 +20,34 @@ export const CartWrapper = styled.div`
   display: flex;
   gap: 100px;
 `;
+
+export const EmptyCartWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 700px;
+`;
+
+export const EmptyCartTitle = styled.div`
+  font-size: 20rem;
+  font-weight: bold;
+  text-align: center;
+`;
+
+export const EmptyCartDescription = styled.div`
+  font-size: 2rem;
+  text-align: center;
+`;
+
+export const EmptyCartButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 20px 0px 20px 0px;
+`;
+
+export const EmptyCartButton = styled.button`
+  text-align: center;
+  border: 1px gray solid;
+  border-radius: 5px;
+  padding: 5px;
+`;
--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,10 +1,17 @@
-import styled from 'styled-components';
 import CartList from '../../components/CartList';
 import PurchaseBox from '../../components/PurchaseBox';
-import {CartTitle, CartWrapper, FatBorder} from "./Cart.style.ts";
+import {
+  CartTitle,
+  CartWrapper, EmptyCartButton, EmptyCartButtonWrapper,
+  EmptyCartDescription,
+  EmptyCartTitle,
+  EmptyCartWrapper,
+  FatBorder
+} from "./Cart.style.ts";
 import {CartListTitle} from "../../components/CartList/CartList.style.ts";
 import {useRecoilValue} from "recoil";
 import {cartCountSelector} from "../../recoil/cartAtoms.ts";
+import {useNavigate} from "react-router-dom";
 
 /**
  * TODO: 반응형 적용, gap 대신 max-width로 간격 조절하기
@@ -12,16 +19,35 @@ import {cartCountSelector} from "../../recoil/cartAtoms.ts";
 
 function Cart() {
   const cartCount = useRecoilValue(cartCountSelector);
-  
+  const navigate = useNavigate();
+
   return (
     <div>
       <CartTitle>장바구니</CartTitle>
       <FatBorder/>
-      <CartListTitle>든든배송 상품 ({cartCount}개)</CartListTitle>
-      <CartWrapper>
-        <CartList/>
-        <PurchaseBox/>
-      </CartWrapper>
+      {
+        cartCount > 0 ?
+          (
+            <>
+              <CartListTitle>든든배송 상품 ({cartCount}개)</CartListTitle>
+              <CartWrapper>
+                <CartList/>
+                <PurchaseBox/>
+              </CartWrapper>
+            </>
+          ) : (
+            <EmptyCartWrapper>
+              <div>
+                <EmptyCartTitle>텅</EmptyCartTitle>
+                <EmptyCartDescription>장바구니가 비어있어요.</EmptyCartDescription>
+                <EmptyCartButtonWrapper>
+                  <EmptyCartButton onClick={() => navigate('/')}>홈으로 돌아가기</EmptyCartButton>
+                </EmptyCartButtonWrapper>
+              </div>
+            </EmptyCartWrapper>
+          )
+      }
+
     </div>
   );
 }
