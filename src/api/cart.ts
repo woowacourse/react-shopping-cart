346bded0bca88f9319e08e093087187f829929d3
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-05-22 06:07:49 +0900

    feat : 장바구니 서버연결 함수선언

---
+++ src/api/cart.ts
@@ -0,0 +1,51 @@
+import { CartType } from "../type/cart";
+
+export async function getCartData() {
+  const response = await fetch("/carts");
+  if (!response.ok) {
+    throw Error(response.status.toString());
+  }
+  const data = await response.json();
+  return data;
+}
+
+export async function patchProductCount(cartItemId: number, quantity: number) {
+  const response = await fetch(`cart-items/${cartItemId}`, {
+    method: "PATCH",
+    body: JSON.stringify({
+      quantity: quantity,
+    }),
+  });
+
+  if (!response.ok) {
+    throw Error(response.status.toString());
+  }
+}
+
+export async function postCartProduct(postData: CartType) {
+  const response = await fetch(`cart-items`, {
+    method: "POST",
+    body: JSON.stringify({
+      postData: postData,
+    }),
+  });
+
+  if (!response.ok) {
+    throw Error(response.status.toString());
+  }
+
+  return response;
+}
+
+export async function deleteCartProduct(cartItemId: number[]) {
+  const response = await fetch(`cart-items`, {
+    method: "DELETE",
+    body: JSON.stringify({ cartItemId }),
+  });
+
+  if (!response.ok) {
+    throw Error(response.status.toString());
+  }
+
+  return response;
+}
