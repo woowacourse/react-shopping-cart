f574ed4b93dcf7cefb70aafca749b953f7e8278d
n0eyes <doll4586633@naver.com>
2023-05-22 01:44:36 +0900

    refactor: cart 관련 커스텀 훅을 한 파일로 변경

---
+++ src/hooks/cart/cart.tsx
@@ -0,0 +1,54 @@
+import { ChangeEvent, useCallback } from 'react';
+import { useRecoilRefresher_UNSTABLE } from 'recoil';
+import { addToCart, updateCartItem } from '../../apis/cart';
+import { cartState, selectedItemsState } from '../../atoms/cart';
+import { CartItem } from '../../types/cart';
+import { waitForMutation } from '../../utils/waitFor';
+import { useRefreshableRecoilState } from '../common/useRefreshableAtom';
+
+export const useCartSelector = () => {
+  const [cart] = useRefreshableRecoilState(cartState);
+  const [selectedItems, setSelectedItems] =
+    useRefreshableRecoilState(selectedItemsState);
+
+  const selectItem = useCallback((id: CartItem['id']) => {
+    setSelectedItems((prevSelectedItems) => {
+      const updatedSelectedItems = new Set(prevSelectedItems);
+
+      prevSelectedItems.has(id)
+        ? updatedSelectedItems.delete(id)
+        : updatedSelectedItems.add(id);
+
+      return updatedSelectedItems;
+    });
+  }, []);
+
+  const handleSelectDeselectAll = useCallback(
+    ({ target: { checked } }: ChangeEvent<HTMLInputElement>) => {
+      checked
+        ? setSelectedItems(new Set(cart.map(({ id }) => id)))
+        : setSelectedItems(new Set());
+    },
+    []
+  );
+
+  return { selectedItems, selectItem, handleSelectDeselectAll };
+};
+
+export const useMutateCart = () => {
+  const refresh = useRecoilRefresher_UNSTABLE(cartState);
+
+  const addItemToCartMutation = waitForMutation(addToCart, {
+    onSuccess() {
+      refresh();
+    },
+  });
+
+  const updateCartItemMutation = waitForMutation(updateCartItem, {
+    onSuccess() {
+      refresh();
+    },
+  });
+
+  return { addItemToCartMutation, updateCartItemMutation };
+};
