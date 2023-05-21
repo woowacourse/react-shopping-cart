36e41c258938d105681af310fe2612e35878d89b
Patrick <kuyt3122@naver.com>
2023-05-21 19:03:47 +0900

    fix: 배포 build 실패 해결

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import App from 'App';
+import { RouterProvider } from 'react-router-dom';
 import { worker } from 'mocks/browser';
 import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '@styles/globalStyle';
 import { theme } from '@styles/theme';
+import router from './router';
 
 const main = async () => {
   if (window.location.pathname === '/react-shopping-cart') {
@@ -23,12 +24,13 @@ const main = async () => {
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <RecoilRoot>
         <GlobalStyle />
-        <App />
+        <RouterProvider router={router} />
       </RecoilRoot>
     </ThemeProvider>
   </React.StrictMode>
