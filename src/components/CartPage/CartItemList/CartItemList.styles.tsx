5be8b1306e3dc6bbfe2bbfbfef28211f260a24fe
n0eyes <doll4586633@naver.com>
2023-05-22 06:15:13 +0900

    css: CartItem border 추가

--- src/components/CartPage/CartItemList/CartItemList.styles.tsx
+++ src/components/CartPage/CartItemList/CartItemList.styles.tsx
@@ -4,4 +4,12 @@ export const Root = styled.ul`
   width: 100%;
 
   padding: 2rem 0;
+
+  & > li {
+    padding: 2rem 0;
+  }
+
+  & > li:not(:last-child) {
+    border-bottom: 1.5px solid #cccccc;
+  }
 `;
