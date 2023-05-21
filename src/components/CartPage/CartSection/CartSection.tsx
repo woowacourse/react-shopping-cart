07016c056a3570de341793a2e5b64c2e16e3220f
n0eyes <doll4586633@naver.com>
2023-05-21 21:18:27 +0900

    refactor: 현재 선택된 상품 갯수를 동적인 데이터로 변경

--- src/components/CartPage/CartSection/CartSection.tsx
+++ src/components/CartPage/CartSection/CartSection.tsx
@@ -1,11 +1,15 @@
+import { useRecoilValue } from 'recoil';
+import { selectedItemsAmountSelector } from '../../../atoms/cart';
 import CartItemController from '../CartItemController/CartItemController';
 import CartItemList from '../CartItemList/CartItemList';
 import * as S from './CartSection.styles';
 
 const CartSection = () => {
+  const selectedItemsAmount = useRecoilValue(selectedItemsAmountSelector);
+
   return (
     <S.Root>
-      <S.SelectedCount>든든배송 상품 (3개)</S.SelectedCount>
+      <S.SelectedCount>든든배송 상품 ({selectedItemsAmount}개)</S.SelectedCount>
       <CartItemList />
       <CartItemController />
     </S.Root>
