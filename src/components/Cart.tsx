14a93beccc991f76f730da8685ca0dfdc8b760de
Jungwoo <evencoding@gmail.com>
2023-05-21 23:14:36 +0900

    feat: 카트에 아이템이 존재하지 않을 때에 대한 메시지 구현

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { styled } from 'styled-components';
 
 import { useCartSizeValue } from '../recoils/recoilCart';
@@ -14,7 +15,14 @@ export const Cart = () => {
         <span>든든배송 상품 ({cartSize}개)</span>
         <CartActions />
       </Style.CartHeader>
-      <CartItemList />
+      {cartSize > 0 ? (
+        <CartItemList />
+      ) : (
+        <Style.NoExistItemsMessage>
+          <p>장바구니에 등록된 상품이 존재하지 않아요🥲</p>
+          <Link to="/">상품 보러가기🚀</Link>
+        </Style.NoExistItemsMessage>
+      )}
     </Style.Cart>
   );
 };
@@ -34,6 +42,10 @@ const Style = {
 
     padding: 8px 0;
 
+    @media screen and (min-width: 501px) {
+      width: 735px;
+    }
+
     @media screen and (max-width: 500px) {
       width: 100%;
 
@@ -42,6 +54,29 @@ const Style = {
       & > span {
         display: none;
       }
+    }
+  `,
+
+  NoExistItemsMessage: styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    height: 200px;
+
+    color: var(--grey-300);
+
+    & > p {
+      font-size: 20px;
+      margin-bottom: 30px;
+
+      line-height: 25px;
+    }
+
+    & > a {
+      color: var(--grey-400);
+      border-bottom: 3px solid var(--grey-400);
     }
   `,
 };
