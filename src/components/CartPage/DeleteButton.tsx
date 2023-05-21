9edb576fbee9499a1e43f161eac6e76888c02419
2yunseong <dbsdltjd3701@naver.com>
2023-05-21 21:06:16 +0900

    style: formatter

--- src/components/CartPage/DeleteButton.tsx
+++ src/components/CartPage/DeleteButton.tsx
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 import DeleteButtonImage from '../../asset/delete_icon.png';
-import { useSetRecoilState } from 'recoil';
+import { useRecoilState, useSetRecoilState } from 'recoil';
 import { cartRequestAction } from '../../atoms/cartState';
+import { cartSelects } from '../../atoms/cartSelects';
 
 interface DeleteButtonProps {
   cartId: number;
@@ -11,6 +12,7 @@ export default function DeleteButton({ cartId }: DeleteButtonProps) {
   const setCartRequestActions = useSetRecoilState(
     cartRequestAction({ action: 'GET' })
   );
+  const [cartSelectSet, setCartSelects] = useRecoilState(cartSelects);
 
   return (
     <DeleteButtonContainer
@@ -19,6 +21,9 @@ export default function DeleteButton({ cartId }: DeleteButtonProps) {
           action: 'DELETE',
           payload: { cartId: cartId },
         });
+        const newCartSelects = new Set(Array.from(cartSelectSet));
+        newCartSelects.delete(cartId);
+        setCartSelects(newCartSelects);
       }}
     >
       <DeleteButtonIcon src={DeleteButtonImage} />
