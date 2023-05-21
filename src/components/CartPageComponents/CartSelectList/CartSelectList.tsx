7fda7a029fde42b88cdfdd951b52ac366b886d7e
sh981013s <sh981013s@gmail.com>
2023-05-22 02:21:44 +0900

    feat: 장바구니 데이터 바인딩

--- src/components/CartPageComponents/CartSelectList/CartSelectList.tsx
+++ src/components/CartPageComponents/CartSelectList/CartSelectList.tsx
@@ -1,32 +1,29 @@
 import * as Styled from './CartSelectList.styles.tsx';
 import CartItem from './CartItem/CartItem.tsx';
+import useGetCartList from '../../../hooks/requests/useGetCartList.ts';
+import { useRecoilValue } from 'recoil';
+import { cartSelectedItemsSelector } from '../../../stores/cartListStore.ts';
+import useCart from '../../../hooks/useCart.ts';
 
 const CartSelectList = () => {
+  const { data: cartList, refetchCartList } = useGetCartList();
+  const [selectedItemsCount, allItemsCount] = useRecoilValue(cartSelectedItemsSelector);
+  const { selectAllItems } = useCart();
+
   return (
     <Styled.CartSelectListWrapper>
-      <Styled.CartQuantityText>배송 상품 (3)개</Styled.CartQuantityText>
+      <Styled.CartQuantityText>배송 상품 ({cartList?.items.length || 0})개</Styled.CartQuantityText>
       <hr />
       <div>
         <div>
-          <CartItem />
-          <CartItem />
-
-          <CartItem />
-          <CartItem />
-          <CartItem />
-          <CartItem />
-          <CartItem />
-          <CartItem />
-          <CartItem />
-          <CartItem />
-          <CartItem />
-          <CartItem />
-          <CartItem />
-          <CartItem />
-          <CartItem />
-
-          <span>전체선택 (2/3)</span>
-          <button>선택삭제</button>
+          {cartList &&
+            cartList.items.map((cart) => {
+              return <CartItem key={cart.id} cart={cart} refetchCartList={refetchCartList} />;
+            })}
+          <button onClick={selectAllItems}>전체선택</button>
+          <span>
+            ({selectedItemsCount}/{allItemsCount})
+          </span>
         </div>
       </div>
     </Styled.CartSelectListWrapper>
