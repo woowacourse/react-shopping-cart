5b3b372b7342cb50adcefcb7fcf442bef81c440c
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-05-22 06:09:05 +0900

    feat : cartData recoil 상태 추가

--- src/atoms/cartState.ts
+++ src/atoms/cartState.ts
@@ -1,4 +1,5 @@
 import { atom, selector } from "recoil";
+import { getCartData } from "../api/cart";
 
 import type { CartType } from "../type/cart";
 
@@ -7,10 +8,23 @@ export const cartState = atom<CartType[]>({
   default: [],
 });
 
+export const cartData = selector({
+  key: "cartData",
+  get: async () => {
+    const data = await getCartData();
+    return data;
+  },
+});
+
 export const totalCartCount = selector({
   key: "totalCartCount",
   get: ({ get }) => {
-    const cart = get(cartState);
+    const cart = get(cartData);
     return cart.length;
   },
+});
+
+export const productPriceState = atom({
+  key: "productPriceState",
+  default: {},
 });
