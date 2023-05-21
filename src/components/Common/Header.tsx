99cb23245b951bd5ad74cb09eb635b8714a24db6
Leejin-Yang <ejin1996@gmail.com>
2023-05-21 22:51:58 +0900

    feat: Header 반응형 스타일 적용

--- src/components/Common/Header.tsx
+++ src/components/Common/Header.tsx
@@ -30,13 +30,13 @@ const HeaderContainer = styled.header`
 `;
 
 const HeaderContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   max-width: 1250px;
   height: 100%;
   margin: 0 auto;
   padding: 0 30px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
 `;
 
 const LogoContainer = styled(Link)`
@@ -44,35 +44,59 @@ const LogoContainer = styled(Link)`
   align-items: center;
 
   & > svg {
-    margin-right: 18px;
+    margin-right: 12px;
     transform: scaleX(-1);
+  }
+
+  @media (min-width: 640px) {
+    & > svg {
+      margin-right: 18px;
+      transform: scaleX(-1);
+    }
   }
 `;
 
 const Logo = styled.h1`
+  padding: 8px 0 0;
   color: ${({ theme }) => theme.colors.white};
-  font-size: 40px;
+  font-size: 34px;
   font-weight: 900;
   letter-spacing: 0.1em;
-  padding: 10px 0 0;
+
+  @media (min-width: 640px) {
+    padding: 10px 0 0;
+    font-size: 40px;
+    line-height: 40px;
+  }
 `;
 
 const CartPageLink = styled(Link)`
   display: flex;
   color: ${({ theme }) => theme.colors.white};
-  font-size: 24px;
+  font-size: 20px;
   font-weight: 500;
+
+  @media (min-width: 640px) {
+    font-size: 24px;
+  }
 `;
 
-const ProductCountAlert = styled.p`
-  width: 26px;
-  height: 26px;
+const ProductCountAlert = styled.span`
+  display: inline-block;
+  width: 22px;
+  height: 22px;
   margin-left: 6px;
+  font-size: 16px;
   text-align: center;
   border-radius: 50%;
   background-color: ${({ theme }) => theme.colors.primary};
-  line-height: 26px;
-  font-size: 16px;
+  line-height: 24px;
+
+  @media (min-width: 640px) {
+    width: 26px;
+    height: 26px;
+    line-height: 28px;
+  }
 `;
 
 export default Header;
