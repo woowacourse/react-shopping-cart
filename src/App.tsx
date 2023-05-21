6c45653a8be1a9b12b9c13d63a22355d524cd582
sh981013s <sh981013s@gmail.com>
2023-05-22 01:57:24 +0900

    feat: Error handling page 구현

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import ListPage from './pages/ListPage/ListPage.tsx';
 import { RecoilRoot } from 'recoil';
 import UnloadHandler from './components/@common/UnloadHandler.tsx';
 import CartPage from './pages/CartPage/CartPage.tsx';
+import ErrorPage from './pages/ErrorPage/ErrorPage.tsx';
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
           <Routes>
             <Route path='/' element={<ListPage />} />
             <Route path='/cart' element={<CartPage />} />
+            <Route path='/error' element={<ErrorPage />} />
           </Routes>
         </Layout>
       </BrowserRouter>
