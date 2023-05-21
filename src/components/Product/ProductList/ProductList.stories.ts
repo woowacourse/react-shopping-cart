318452fe5e70024e37da0b086dc429644f587108
woo-jk <201702042@o.cnu.ac.kr>
2023-05-21 23:07:31 +0900

    test: Header, ProductItem, ProductList 스토리북 MSW 적용

--- src/components/Product/ProductList/ProductList.stories.ts
+++ src/components/Product/ProductList/ProductList.stories.ts
@@ -1,10 +1,12 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import ProductList from './ProductList';
+import { cartHandler, productHandler } from '../../../mocks/handlers';
 
 const meta = {
   title: 'ProductList',
   component: ProductList,
   tags: ['autodocs'],
+  parameters: { msw: [...productHandler, ...cartHandler] },
 } satisfies Meta<typeof ProductList>;
 
 export default meta;
--- src/components/common/Header/Header.stories.ts
+++ src/components/common/Header/Header.stories.ts
@@ -1,10 +1,12 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import Header from './Header';
+import { cartHandler } from '../../../mocks/handlers';
 
 const meta = {
   title: 'Header',
   component: Header,
   tags: ['autodocs'],
+  parameters: { msw: cartHandler },
 } satisfies Meta<typeof Header>;
 
 export default meta;
