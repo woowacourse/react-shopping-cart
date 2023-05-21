537aceedcccf770a302e9e0e16c1c7b53476b40a
jeongwusi <jeongwusi@gmail.com>
2023-05-21 16:22:57 +0900

    ♻️ refactor: 결제예상금액을 체크된 상품 기준으로 동적으로 바뀔 수 있도록 수정

--- src/components/PaymentAmount/index.tsx
+++ src/components/PaymentAmount/index.tsx
@@ -1,6 +1,13 @@
+import { useRecoilValue } from 'recoil';
 import styles from './index.module.css';
+import { $CheckedCartState } from '../../recoil/atom';
 
 const PaymentAmount = () => {
+  const checkedItems = useRecoilValue($CheckedCartState);
+  const totalPrice = checkedItems.reduce((res, { quantity, product }) => {
+    res += quantity * product.price;
+    return res;
+  }, 0);
   return (
     <section className={styles.container}>
       <header className={styles.title}>
@@ -9,7 +16,7 @@ const PaymentAmount = () => {
       <main className={styles['payment-box']}>
         <div>
           <span className={styles.label}>총 상품가격</span>
-          <span className={styles.value}>21,700</span>
+          <span className={styles.value}>{totalPrice.toLocaleString()}</span>
         </div>
         <div>
           <span className={styles.label}>총 배송비</span>
@@ -17,7 +24,7 @@ const PaymentAmount = () => {
         </div>
         <div>
           <span className={styles.label}>총 주문금액</span>
-          <span className={styles.value}>24,700원</span>
+          <span className={styles.value}>{totalPrice === 0 ? 0 : (totalPrice + 3000).toLocaleString()}</span>
         </div>
       </main>
       <footer>
