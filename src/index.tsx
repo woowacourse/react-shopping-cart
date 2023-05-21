907b43b83b01036772c10b98858aa05598a98fc2
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-05-21 20:43:17 +0900

    chore: 배포 환경 수정

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,18 @@ import CartPage from './pages/CartPage';
 import ProductPage from './pages/ProductPage';
 import GlobalStyle from './styles/GlobalStyle';
 
-if (process.env.NODE_ENV === 'development') worker.start();
+const main = async () => {
+  if (window.location.pathname === '/react-shopping-cart') {
+    window.location.pathname = '/react-shopping-cart/';
+    return;
+  }
+
+  await worker.start({
+    serviceWorker: {
+      url: '/react-shopping-cart/mockServiceWorker.js',
+    },
+  });
+};
 
 const router = createBrowserRouter(
   [
@@ -39,3 +50,5 @@ root.render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
+
+main();
--- src/mocks/handlers/productHandler.ts
+++ src/mocks/handlers/productHandler.ts
@@ -2,7 +2,7 @@ import { rest } from 'msw';
 import { products } from '../../components/data/mockData';
 
 export const productHandler = [
-  rest.get('/products', async (_req, res, ctx) => {
+  rest.get('/products', (_req, res, ctx) => {
     return res(ctx.delay(1000), ctx.json(products));
   }),
 ];
