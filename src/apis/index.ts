9dcce0f5eb236eb404b0e273a0ec1a4f95221909
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-05-21 13:57:40 +0900

    refactor(fetcher): fetch 래핑 함수 고도화
    1. 다양한 에러케이스를 처리할 수 있도록 개선
    2. 에러메세지를 보다 명확하게 제공하도록 개선
    3. 204 상태에 대한 처리

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,13 +1,41 @@
+export type ErrorResponse = {
+  message: string;
+};
+
 const BASE_URL = ``;
 
-const fetcher = async <T>(url: string, options?: RequestInit): Promise<T> => {
+const fetcher = async <T>(url: string, options?: RequestInit): Promise<T | null> => {
   const response = await fetch(`${BASE_URL}${url}`, options);
 
-  if (!response.ok) {
-    throw new Error(`데이터 요청에 실패했습니다. 요청 URL: ${url}`);
+  if (response.status >= 500) {
+    throw new Error(`서버문제로 HTTP 통신에 실패했습니다. 상태 코드:${response.status}`);
   }
 
-  return response.json();
+  if (!response.ok) {
+    let errorMessage;
+
+    try {
+      const errorResponse: ErrorResponse = await response.json();
+      errorMessage = errorResponse.message;
+    } catch {
+      errorMessage = 'error Message를 표시할 수 없습니다. 에러 응답이 json형식이 아닙니다.';
+    }
+
+    throw new Error(`HTTP 통신에 실패했습니다. 상태 코드: ${response.status}
+    요청 URL: ${url} 
+    method: ${options?.method}
+    ${errorMessage}`);
+  }
+
+  if (response.status === 204) {
+    return null;
+  }
+
+  try {
+    return response.json();
+  } catch {
+    throw new Error('응답이 json형식이 아닙니다.');
+  }
 };
 
 export default fetcher;
