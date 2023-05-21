a201c59deb2043b75b46100ec97618742fa04c90
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-05-22 06:14:13 +0900

    feat : 불필요한 로직 제거

--- src/hooks/useAddCart.ts
+++ src/hooks/useAddCart.ts
@@ -6,41 +6,10 @@ import { CartType } from "../type/cart";
 
 export function useAddCart() {
   const [isSelected, setIsSelected] = useState(false);
-  const setCart = useSetRecoilState<CartType[]>(cartState);
 
   const selectProductItem = () => {
-    setIsSelected(true);
+    isSelected ? setIsSelected(false) : setIsSelected(true);
   };
 
-  const createChangedCart = (prevCart: CartType[], cartItem: CartType) => {
-    const index = prevCart.findIndex(
-      (prevItem) => prevItem.productId === cartItem.productId
-    );
-    if (index === -1) return [...prevCart, cartItem];
-
-    return [
-      ...prevCart.slice(0, index),
-      cartItem,
-      ...prevCart.slice(index + 1),
-    ];
-  };
-
-  const addCartProductItem = (
-    id: number,
-    quantityRef: React.RefObject<HTMLInputElement>
-  ) => {
-    setIsSelected(false);
-    const cartItem: CartType = {
-      productId: id,
-      quantity: 0,
-    };
-
-    if (quantityRef.current) {
-      cartItem.quantity = +quantityRef.current.value;
-    }
-
-    setCart((prev) => createChangedCart(prev, cartItem));
-  };
-
-  return { isSelected, selectProductItem, addCartProductItem };
+  return { isSelected, selectProductItem };
 }
