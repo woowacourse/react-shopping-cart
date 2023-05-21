fc14ecb9609c3e7a9b463eddb379d1f5608930b8
hae-on <solbi2004@naver.com>
2023-05-21 14:53:18 +0900

    test: 카트 버튼 클릭 시 장바구니에 카운트 되는지 확인 테스트

--- src/stories/pages/ProductsListPage.stories.tsx
+++ src/stories/pages/ProductsListPage.stories.tsx
@@ -1,4 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react';
+import { userEvent, within, waitFor } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
 
 import ProductsListPage from '../../pages/ProductsListPage';
 
@@ -14,4 +16,27 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {},
+};
+
+export const Interaction: Story = {
+  args: {},
+
+  play: async ({ canvasElement, step }) => {
+    window.localStorage.clear();
+
+    const canvas = within(canvasElement);
+
+    const productList = await canvas.findByRole('list');
+    const productListCanvas = within(productList);
+    const cartButtons = await productListCanvas.findAllByRole('button');
+
+    await step('Click Cart Button', async () => {
+      await userEvent.click(cartButtons[0]);
+      await userEvent.click(cartButtons[1]);
+    });
+
+    await waitFor(() => {
+      expect(canvas.getByText(2)).toBeInTheDocument();
+    });
+  },
 };
