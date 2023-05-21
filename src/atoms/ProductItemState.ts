84a06d3a1175372ab156e8f9aea664746cf0f6db
semnil5202 <semnil5202@gmail.com>
2023-05-21 23:34:01 +0900

    refactor: ProductList Fetch 로직 변경 및 State 관리 변경
    fetch 로직을 각각의 컴포넌트에 뿌리는 것 보다 atom에서 일괄 관리한다.
    또한 atom의 setter가 각각의 컴포넌트에 뿌려져있어 state CRUD확인이 어려우므로 atom에서 일괄 관리한다.

--- src/atoms/ProductItemState.ts
+++ src/atoms/ProductItemState.ts
@@ -1,11 +1,35 @@
-import { atomFamily } from 'recoil';
-import { getMockData } from '../utils/getMockData';
+import { atom, selectorFamily } from 'recoil';
+import { ProductItem } from '../types/productType';
 
-export const productItemStateFamily = atomFamily({
+const getProductListFromMocks =
+  () =>
+  ({ setSelf }: { setSelf: (productList: ProductItem[] | []) => void }) => {
+    const initProductListState = async () => {
+      const response = await fetch('/api/products');
+      const productList = await response.json();
+
+      if (!productList) setSelf([]);
+      setSelf(productList);
+    };
+
+    initProductListState();
+  };
+
+export const productListState = atom({
+  key: 'ProductListState',
+  default: [],
+  effects: [getProductListFromMocks()],
+});
+
+export const productItemStateFamily = selectorFamily({
   key: 'ProductItemState',
-  default: (id: number) => {
-    const productItem = getMockData.filter((product) => product.id === id)[0];
+  get:
+    (id: number) =>
+    ({ get }) => {
+      const productItem = get(productListState).filter(
+        (product) => product.id === id
+      )[0];
 
-    return productItem;
-  },
+      return productItem;
+    },
 });
--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -1,27 +1,14 @@
+import { useRecoilValue } from 'recoil';
 import { ProductItem } from '../components/ProductItem';
 import * as Styled from './styles/ProductList.styles';
-import { useEffect, useState } from 'react';
+import { productListState } from '../atoms/ProductItemState';
 
 export const ProductList = () => {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('/api/products');
-        const data = await response.json();
-        setProducts(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchData();
-  }, []);
+  const productList = useRecoilValue(productListState);
 
   return (
     <Styled.Wrapper>
-      {products.map(({ id, name, price, imageUrl }) => (
+      {productList.map(({ id, name, price, imageUrl }) => (
         <ProductItem
           key={id}
           id={id}
