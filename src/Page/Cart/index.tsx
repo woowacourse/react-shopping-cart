f75a92d8c46d067270f7282a3aadeba3140f9a1a
jeongwusi <jeongwusi@gmail.com>
2023-05-22 08:47:13 +0900

    ✨ feat: 카트에 상품이 없는 경우 상품이 없다는 것을 알 수 있는 문구 추가

--- src/Page/Cart/index.tsx
+++ src/Page/Cart/index.tsx
@@ -1,17 +1,30 @@
 import styles from './index.module.css';
 import CartProductList from '../../components/CartProductList';
 import PaymentAmount from '../../components/PaymentAmount';
+import { useRecoilValue } from 'recoil';
+import { $Cart } from '../../recoil/atom';
+import { useEffect } from 'react';
 
 const Cart = () => {
+  const cart = useRecoilValue($Cart);
+
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
+
   return (
     <section className={styles.container}>
       <header className={styles.title}>
         <h2>장바구니</h2>
       </header>
-      <main className={styles['cart-container']}>
-        <CartProductList />
-        <PaymentAmount />
-      </main>
+      {cart.length === 0 ? (
+        <span>저희가 도와드릴 수 있도록 상품을 선택해주세요.</span>
+      ) : (
+        <main className={styles['cart-container']}>
+          <CartProductList />
+          <PaymentAmount />
+        </main>
+      )}
     </section>
   );
 };
