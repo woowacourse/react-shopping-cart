5cf8916747d61ffbfcae33936e50e9a814c054fe
Suyoung <jsy8492@gmail.com>
2023-05-22 00:34:30 +0900

    fix: Header 컴포넌트 스토리의 BrowserRouter 제거

--- src/components/common/Header/Header.stories.tsx
+++ src/components/common/Header/Header.stories.tsx
@@ -1,18 +1,10 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import Header from './Header';
-import { BrowserRouter } from 'react-router-dom';
 
 const meta = {
   title: 'common/Header',
   component: Header,
   tags: ['autodocs'],
-  decorators: [
-    (Story) => (
-      <BrowserRouter>
-        <Story />
-      </BrowserRouter>
-    ),
-  ],
 } satisfies Meta<typeof Header>;
 
 export default meta;
