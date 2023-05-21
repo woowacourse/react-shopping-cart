167329b4cd23bca4283fa0c898780479e19673ff
JIWON <afds4567@gmail.com>
2023-05-21 23:46:58 +0900

    refactor: 카트 상태 비동기 에러처리 추가

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -1,47 +1,56 @@
-import { useRecoilCallback } from 'recoil';
+import type { CallbackInterface } from 'recoil';
+import { useRecoilCallback, useRecoilValue } from 'recoil';
 import mockServerClient from '../api';
 import cartState from '../recoil/atoms/cartState';
 import type { Product } from '../type';
 
 const useCartProduct = (productId: Product['id']) => {
+  const carts = useRecoilValue(cartState);
   const setQuantity = useRecoilCallback(
-    ({ set }) =>
-      (quantity: number) => {
-        set(cartState, (cart) => {
-          const cartProduct = cart.find((it) => it.productId === productId) ?? null;
-
-          if (cartProduct === null) {
-            mockServerClient.post('/cart-items', { productId });
-
-            const newCartItem = {
-              id: Math.round(Math.random() * 100000),
-              quantity,
-              productId,
-            };
-            return [...cart, newCartItem];
+    ({ set }: CallbackInterface) =>
+      async (quantity: number) => {
+        const cartItem = carts.find((it) => it.productId === productId) ?? null;
+        try {
+          if (cartItem === null) {
+            await mockServerClient.post(`/cart-items`, { productId });
           }
-
           if (quantity === 0) {
-            mockServerClient.delete(`/cart-items`, productId);
+            await mockServerClient.delete(`/cart-items`, productId);
+          } else {
+            await mockServerClient.patch(`/cart-items`, productId, { quantity });
           }
-          if (quantity > 0) mockServerClient.patch(`/cart-items`, productId, { quantity });
 
-          const cartProductIndex = cart.findIndex((it) => it.id === cartProduct.id);
-          const updatedCartItem = {
-            ...cartProduct,
-            quantity,
-          };
+          set(cartState, (cart) => {
+            const cartProduct = cart.find((it) => it.productId === productId) ?? null;
 
-          const newCart = [
-            ...cart.slice(0, cartProductIndex),
-            updatedCartItem,
-            ...cart.slice(cartProductIndex + 1),
-          ].filter((it) => it.quantity > 0);
+            if (cartProduct === null) {
+              const newCartItem = {
+                id: Math.round(Math.random() * 100000),
+                quantity,
+                productId,
+              };
+              return [...cart, newCartItem];
+            }
 
-          return newCart;
-        });
+            const cartProductIndex = cart.findIndex((it) => it.id === cartProduct.id);
+            const updatedCartItem = {
+              ...cartProduct,
+              quantity,
+            };
+
+            const newCart = [
+              ...cart.slice(0, cartProductIndex),
+              updatedCartItem,
+              ...cart.slice(cartProductIndex + 1),
+            ].filter((it) => it.quantity > 0);
+
+            return newCart;
+          });
+        } catch (error) {
+          console.log(error);
+          window.alert('상품을 처리하는 도중 오류가 발생했습니다.');
+        }
       },
-    [productId],
   );
 
   return { setQuantity };
