6244335ef26a5a0b68786a81b47c39cc3d554fcc
Dahye <06robin11@gmail.com>
2023-05-22 14:09:34 +0900

    feat(productlist):로딩시 스켈레톤 페이지 구현

--- src/components/ProductPage/ProductList/index.tsx
+++ src/components/ProductPage/ProductList/index.tsx
@@ -3,18 +3,26 @@ import * as S from './ProductList.styles';
 import { useEffect, useState } from 'react';
 import { Product } from '../../../types/products';
 import { fetchProductData } from '../../../apis/products';
+import ProductListSkeleton from './ProductListSkeleton';
 
 const ProductList = () => {
   const [items, setItems] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const getData = async () => {
+      setIsLoading(true);
+
       const data = await fetchProductData();
       setItems(data);
+
+      setIsLoading(false);
     };
 
     getData();
   }, []);
+
+  if (isLoading) return <ProductListSkeleton />;
 
   return (
     <S.List>
--- src/pages/ProductPage/index.tsx
+++ src/pages/ProductPage/index.tsx
@@ -1,6 +1,4 @@
-import React, { Suspense } from 'react';
 import ProductList from '../../components/ProductPage/ProductList';
-import ProductListSkeleton from '../../components/ProductPage/ProductList/ProductListSkeleton';
 
 const ProductPage = () => {
   return <ProductList />;
