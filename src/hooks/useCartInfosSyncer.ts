57aabc9903a5ab78bf9fea0bcbf24161ac5be5ec
wzrabbit <curious.wzrabbit@gmail.com>
2023-05-22 17:09:17 +0900

    ✨ feat: useCartInfosSyncer 구현
    - 실제 fetch한 데이터를 바탕으로 간단화한 클라이언트 전용 수정 데이터를 atom에 업데이트

---
+++ src/hooks/useCartInfosSyncer.ts
@@ -0,0 +1,53 @@
+import { useEffect, useCallback } from "react";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { cartProductsAtom } from "../store/fetchAtoms";
+import {
+  cartLocalInfosAtom,
+  cartProductsCountSelector,
+  totalPriceSelector,
+  cartIsCheckedAtom,
+} from "../store/cartProductsAtoms";
+
+const useCartInfosSyncer = () => {
+  const cartProducts = useRecoilValue(cartProductsAtom);
+  const setCartInfos = useSetRecoilState(cartLocalInfosAtom);
+  const cartProductsCount = useRecoilValue(cartProductsCountSelector);
+  const totalPrice = useRecoilValue(totalPriceSelector);
+  const setCartIsChecked = useSetRecoilState(cartIsCheckedAtom);
+
+  const convertToCartInfo = useCallback(() => {
+    setCartInfos(() =>
+      cartProducts.map(({ id, quantity, product }) => ({
+        id,
+        quantity,
+        price: product.price,
+      }))
+    );
+  }, [cartProducts, setCartInfos]);
+
+  const initializeCartIsChecked = useCallback(() => {
+    setCartIsChecked((prevCartIsChecked) => {
+      const newCartIsChecked = { ...prevCartIsChecked };
+
+      cartProducts.forEach(({ id }) => {
+        if (!newCartIsChecked.hasOwnProperty(id)) {
+          newCartIsChecked[id] = false;
+        }
+      });
+
+      return newCartIsChecked;
+    });
+  }, [cartProducts, setCartIsChecked]);
+
+  useEffect(() => {
+    convertToCartInfo();
+  }, [convertToCartInfo]);
+
+  useEffect(() => {
+    initializeCartIsChecked();
+  }, [initializeCartIsChecked]);
+
+  return { cartProductsCount, totalPrice };
+};
+
+export { useCartInfosSyncer };
