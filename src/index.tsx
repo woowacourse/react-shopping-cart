a55bc6dfde751c0ff2da2f1b197efd291e88a2e8
turtle601 <turtle601@naver.com>
2023-05-22 14:13:03 +0900

    chore: 배포 개발 환경 셋팅

--- src/index.tsx
+++ src/index.tsx
@@ -3,31 +3,26 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import GlobalStyle from '@/styles/global';
 import { GlobalFontStyles } from '@/styles/font';
-import { worker } from './mocks/worker';
+
+const renderApp = () => {
+	const root = ReactDOM.createRoot(
+		document.getElementById('root') as HTMLElement
+	);
+
+	root.render(
+		<React.StrictMode>
+			<GlobalFontStyles />
+			<GlobalStyle />
+			<App />
+		</React.StrictMode>
+	);
+};
 
 async function main() {
-	if (window.location.pathname === '/') {
-		window.location.pathname = '/';
-		return;
-	}
-
-	await worker.start({
-		serviceWorker: {
-			url: '/mockServiceWorker.js',
-		},
+	import('./mocks/worker').then(async ({ worker }) => {
+		await worker.start();
 	});
+	renderApp();
 }
-
-const root = ReactDOM.createRoot(
-	document.getElementById('root') as HTMLElement
-);
-
-root.render(
-	<React.StrictMode>
-		<GlobalFontStyles />
-		<GlobalStyle />
-		<App />
-	</React.StrictMode>
-);
 
 main();
