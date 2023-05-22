414a72982f3f787fcebc7e91856fd444dee8269a
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-05-22 11:05:46 +0900

    refactor(useFetch): fetch 함수를 제공하도록 개선
    error시 재시도를 해야하는 경우 대비

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,28 +1,29 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 const useFetch = <T>(fetcher: () => Promise<T>) => {
   const [data, setData] = useState<T | null>(null);
-  const [isLoading, isSetLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [errorState, setErrorState] = useState<{ isError: boolean; error: Error } | null>(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      isSetLoading(true);
+  const fetchData = useCallback(async () => {
+    setErrorState(null);
+    setIsLoading(true);
 
-      try {
-        const data = await fetcher();
-        setData(data);
-      } catch (error) {
-        setErrorState({ isError: true, error: error as Error });
-      } finally {
-        isSetLoading(false);
-      }
-    };
-
-    fetchData();
+    try {
+      const data = await fetcher();
+      setData(data);
+    } catch (error) {
+      setErrorState({ isError: true, error: error as Error });
+    } finally {
+      setIsLoading(false);
+    }
   }, [fetcher]);
 
-  return { data, isLoading, errorState };
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return { data, isLoading, errorState, fetchData };
 };
 
 export default useFetch;
