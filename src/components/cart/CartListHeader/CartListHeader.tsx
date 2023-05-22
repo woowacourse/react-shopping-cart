ffa9e87ed7d479f71744d1ef89a149e46851c1fe
ashleysyheo <ashleysyheo@gmail.com>
2023-05-22 19:39:32 +0900

    refactor: CartListHeader 체크박스에 id 값 추가

--- src/components/cart/CartListHeader/CartListHeader.tsx
+++ src/components/cart/CartListHeader/CartListHeader.tsx
@@ -34,7 +34,7 @@ const CartListHeader = () => {
 
   return (
     <S.CartListHeaderContainer>
-      <Checkbox id="total-checkbox" checked={isAllChecked} onClick={onCheckboxClick} />
+      <Checkbox id="전체 선택 체크박스" checked={isAllChecked} onClick={onCheckboxClick} />
       <S.CartItemAllSelectText>
         전체선택 ({checkedItemCount}/
         {cartListItemCount.state === 'hasValue' ? cartListItemCount.contents : 0})
