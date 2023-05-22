2f697855202f798aa24a88bf604d6c7409220dd7
semnil5202 <semnil5202@gmail.com>
2023-05-22 15:46:07 +0900

    refactor(NotFound): style 변경 및 메인페이지로 이동할 수 있는 기능 추가

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,3 +1,17 @@
+import * as Styled from './styles/NotFound.styles';
+import { Button as GoToHomeButton } from '../ui/Button';
+
 export const NotFound = () => {
-  return <div>404</div>;
+  return (
+    <Styled.Wrapper>
+      <h2>404 ERROR 🧐</h2>
+      <p>요청하신 페이지를 찾을 수 없습니다.</p>
+      <p>페이지 주소를 다시 한 번 확인해주세요.</p>
+      <Styled.GoToHomeButtonWrapper to={'/'}>
+        <GoToHomeButton width="148px" height="36px" backgroundColor="#04c09e">
+          상품페이지로 이동
+        </GoToHomeButton>
+      </Styled.GoToHomeButtonWrapper>
+    </Styled.Wrapper>
+  );
 };
