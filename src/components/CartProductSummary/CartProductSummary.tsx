a5d70b3835a8e05cdebfcdfd68f8e668ae8e7bd3
nangkyeonglim <nangkyeonglim@gmail.com>
2023-05-22 14:58:11 +0900

    feat: cartProductSummary 체크박스 및 추가 삭제 기능 구현

--- src/components/CartProductSummary/CartProductSummary.tsx
+++ src/components/CartProductSummary/CartProductSummary.tsx
@@ -1,31 +1,106 @@
 import { styled } from 'styled-components';
 import CartProductItem from '../CartProductItem/CartProductItem';
-import mockData from '../../data/productList.json';
 import checkIcon from '../../assets/check.svg';
-import { useRecoilValue } from 'recoil';
-import { cartListAtom } from '../../stores/cartItemsStore';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import { cartListAtom, itemSelectionAtom } from '../../stores/cartItemsStore';
+import { useEffect, useState } from 'react';
+import useUpdateCartItems from '../../hooks/useUpdateCartItems';
+import useGetData from '../../hooks/useGetData';
+import { CartItem } from '../../types';
 
 const CartProductSummary = () => {
+  const { data: cartListData, getData: getCartList } = useGetData<CartItem[]>('/cart-items');
+  const { updateCartItems } = useUpdateCartItems();
+  const [itemSelection, setItemSelection] = useRecoilState(itemSelectionAtom);
+  const setCartList = useSetRecoilState(cartListAtom);
   const cartList = useRecoilValue(cartListAtom);
+  const [initialLoad, setInitialLoad] = useState(false);
+
+  const selectAll = () => {
+    setItemSelection(itemSelection.map(() => true));
+  };
+
+  const cancelSelectAll = () => {
+    setItemSelection(itemSelection.map(() => false));
+  };
+
+  const toggleItemSelection = (index: number) => () => {
+    setItemSelection((prev) => prev.map((value, currentIndex) => (currentIndex === index ? !value : value)));
+  };
+
+  const deleteItemSelection = (index: number) => async () => {
+    await updateCartItems({ itemId: cartList[index].id, itemCount: 0 });
+    await getCartList();
+
+    setItemSelection((prev) => prev.filter((_, idx) => index !== idx));
+  };
+
+  const deleteCartItem = () => {
+    const checkItemIndex: number[] = [];
+
+    itemSelection.forEach(async (checkedItem, index) => {
+      if (checkedItem) {
+        checkItemIndex.push(index);
+        await updateCartItems({ itemId: cartList[index].id, itemCount: 0 });
+        await getCartList();
+      }
+    });
+    setItemSelection((prev) => prev.filter((_, index) => !checkItemIndex.includes(index)));
+  };
+
+  useEffect(() => {
+    getCartList();
+  }, []);
+
+  useEffect(() => {
+    if (cartListData) {
+      setCartList(cartListData);
+      if (!initialLoad) {
+        setItemSelection(Array.from({ length: cartListData.length }, () => true));
+        setInitialLoad(true);
+      }
+    }
+  }, [cartListData, initialLoad]);
 
   return (
-    <Wrapper>
-      <ProductCountWrapper>
-        <ProductCount>든든배송 상품 (3개)</ProductCount>
-      </ProductCountWrapper>
-      <ProductList role='list'>
-        {cartList.map((cartItem) => {
-          return <CartProductItem key={cartItem.id} {...cartItem} />;
-        })}
-      </ProductList>
-      <ProductSelectWrapper>
-        <Label>
-          <Input type='checkbox' icon={checkIcon} />
-          <LabelContent>전체선택 (?/{mockData.length})</LabelContent>
-        </Label>
-        <ProductsDeleteButton>선택삭제</ProductsDeleteButton>
-      </ProductSelectWrapper>
-    </Wrapper>
+    <>
+      {cartList.length !== 0 ? (
+        <Wrapper>
+          <ProductCountWrapper>
+            <ProductCount>든든배송 상품 ({cartList.length}개)</ProductCount>
+          </ProductCountWrapper>
+          <ProductList role='list'>
+            {cartList.map((cartItem, index) => {
+              return (
+                <CartProductItem
+                  key={cartItem.id}
+                  check={itemSelection[index] ?? true}
+                  {...cartItem}
+                  toggleItemSelection={toggleItemSelection(index)}
+                  deleteItemSelection={deleteItemSelection(index)}
+                />
+              );
+            })}
+          </ProductList>
+          <ProductSelectWrapper>
+            <Label>
+              <Input
+                type='checkbox'
+                icon={checkIcon}
+                checked={itemSelection.every((item) => item === true)}
+                onChange={itemSelection.every((item) => item === true) ? cancelSelectAll : selectAll}
+              />
+              <LabelContent>
+                전체선택 ({itemSelection.filter((item) => item === true).length}/{cartList.length})
+              </LabelContent>
+            </Label>
+            <ProductsDeleteButton onClick={deleteCartItem}>선택삭제</ProductsDeleteButton>
+          </ProductSelectWrapper>
+        </Wrapper>
+      ) : (
+        <EmptyProductWrapper>장바구니가 비어있습니다!</EmptyProductWrapper>
+      )}
+    </>
   );
 };
 
@@ -105,6 +180,17 @@ const ProductSelectWrapper = styled.div`
   display: flex;
   align-items: center;
   gap: 16px;
+`;
+
+const EmptyProductWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
+  line-height: 20px;
+  letter-spacing: 0.5px;
+  color: #333333;
 `;
 
 export default CartProductSummary;
