e2ad9a17d34b31ea1d187d8da55fff86226cf987
SangwonKang <72087183+ksone02@users.noreply.github.com>
2023-05-22 17:36:58 +0900

    feat: UserCartInfo 컴포넌트 장바구니 카운트 로직 변경

--- src/components/common/header/UserCartInfo.tsx
+++ src/components/common/header/UserCartInfo.tsx
@@ -1,22 +1,60 @@
 import styled from '@emotion/styled';
-import { useRecoilValue } from 'recoil';
 import { Text } from '../Text/Text';
-import { cartItemTotalQuantityState } from '../../../service/atom';
+import { useEffect, useRef, useState } from 'react';
+import { gsap } from 'gsap';
+import { Link } from 'react-router-dom';
+import { useCart } from '../../../hooks/useCart';
 
 const UserCartInfo = () => {
-  const cartTotalQuantity = useRecoilValue(cartItemTotalQuantityState);
+  const { data } = useCart();
+
+  const cartTotalQuantity = data && data.length;
+
+  const [isShown, setIsShown] = useState(false);
+
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if (cartTotalQuantity) {
+      if (cartTotalQuantity > 0) {
+        setIsShown(true);
+        return;
+      }
+      setTimeout(() => {
+        setIsShown(false);
+      }, 200);
+    }
+  }, [cartTotalQuantity]);
+  useEffect(() => {
+    if (cartTotalQuantity && cartTotalQuantity > 0 && !isShown) {
+      gsap.fromTo(
+        ref.current,
+        { opacity: 0, transform: 'translateX(-10px)', delay: 0.3, ease: 'ease' },
+        { opacity: 1, transform: 'translateX(0)' },
+      );
+    }
+    if (cartTotalQuantity === 0) {
+      gsap.fromTo(
+        ref.current,
+        { opacity: 1, transform: 'translateX(0)', delay: 0.3, ease: 'ease' },
+        { opacity: 0, transform: 'translateX(-10px)' },
+      );
+    }
+  }, [cartTotalQuantity, isShown]);
 
   return (
-    <CardCounterWrapper>
-      <Text color="#ffffff" size="large" lineHeight="12px">
-        장바구니
-      </Text>
-      {cartTotalQuantity > 0 && (
-        <CartCounter>
-          <Text size="smallest" color="#ffffff">
-            {cartTotalQuantity > 99 ? 99 : cartTotalQuantity}
+    <CardCounterWrapper ref={ref} to="/cart">
+      {isShown && (
+        <>
+          <Text color="#ffffff" size="large" lineHeight="12px">
+            장바구니
           </Text>
-        </CartCounter>
+          <CartCounter>
+            <Text size="smallest" color="#ffffff">
+              {cartTotalQuantity && cartTotalQuantity > 99 ? 99 : cartTotalQuantity}
+            </Text>
+          </CartCounter>
+        </>
       )}
     </CardCounterWrapper>
   );
@@ -24,7 +62,7 @@ const UserCartInfo = () => {
 
 export default UserCartInfo;
 
-const CardCounterWrapper = styled.div`
+const CardCounterWrapper = styled(Link)`
   display: flex;
   flex-direction: row;
   align-items: center;
