eadca35ca38fed9f77f5e6e3e3f265e9e6358d63
wzrabbit <curious.wzrabbit@gmail.com>
2023-05-22 17:22:55 +0900

    ✅ test: 여러 요청에 대한 모킹 응답 코드 작성

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,18 +1,81 @@
 import { rest } from "msw";
-import products from "./mockData/products.json";
-import cartItems from "./mockData/cartItems.json";
+import productsJson from "./mockData/products.json";
+import cartItemsJson from "./mockData/cartItems.json";
+import { ProductsWithId, CartProducts } from "../types";
+
+let products: ProductsWithId = productsJson;
+let cartItems: CartProducts = cartItemsJson;
 
 const handlers = [
   rest.get("/products", (_, res, ctx) => {
     return new Promise((resolve) => {
       setTimeout(() => {
         resolve(res(ctx.status(200), ctx.json(products)));
-      }, 3000);
+      }, 500);
     });
   }),
 
   rest.get("/cart-items", (_, res, ctx) => {
-    return res(ctx.status(200), ctx.json(cartItems));
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(res(ctx.status(200), ctx.json(cartItems)));
+      }, 500);
+    });
+  }),
+
+  rest.post("/cart-items", (req, res, ctx) => {
+    return new Promise((resolve) => {
+      setTimeout(async () => {
+        const json = await req.json();
+
+        const searchedProduct = products.find(
+          (product) => product.id === json.productId
+        );
+
+        const isExist = !!cartItems.find(
+          (product) => product.id === json.productId
+        );
+
+        if (searchedProduct && !isExist) {
+          cartItems.push({
+            id: json.productId,
+            quantity: 1,
+            product: searchedProduct,
+          });
+        }
+
+        resolve(res(ctx.status(201), ctx.json({ success: true })));
+      }, 0);
+    });
+  }),
+
+  rest.patch("/cart-items/:cartItemId", (req, res, ctx) => {
+    return new Promise((resolve) => {
+      setTimeout(async () => {
+        const json = await req.json();
+        const { cartItemId } = req.params;
+
+        cartItems = cartItems.map((cartItem) => {
+          if (cartItem.id === Number(cartItemId)) {
+            return { ...cartItem, quantity: json.quantity };
+          }
+
+          return cartItem;
+        });
+        resolve(res(ctx.status(200), ctx.json({ success: true })));
+      }, 0);
+    });
+  }),
+
+  rest.delete("/cart-items/:cartItemId", (req, res, ctx) => {
+    return new Promise((resolve) => {
+      setTimeout(async () => {
+        const { cartItemId } = req.params;
+
+        cartItems = cartItems.filter(({ id }) => id !== Number(cartItemId));
+        resolve(res(ctx.status(200), ctx.json({ success: true })));
+      }, 0);
+    });
   }),
 ];
 
