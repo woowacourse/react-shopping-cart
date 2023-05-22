0a0d4a8dad7857017242e080fafd82a8f74d652d
hae-on <solbi2004@naver.com>
2023-05-22 15:36:07 +0900

    refactor: 반환 타입 명시

--- src/domain/cartProductHandler.ts
+++ src/domain/cartProductHandler.ts
@@ -1,7 +1,10 @@
-import { CartProduct } from '../types/product';
+import type { CartProduct } from '../types/product';
 
 export const findTargetProduct = (cartProducts: CartProduct[], id: number) =>
   cartProducts.find((cartProduct) => id === cartProduct.product.id);
 
-export const deleteProduct = (cartProducts: CartProduct[], id: number) =>
+export const deleteProduct = (
+  cartProducts: CartProduct[],
+  id: number
+): CartProduct[] =>
   cartProducts.filter((cartProduct) => cartProduct.product.id !== id);
