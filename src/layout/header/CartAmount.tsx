f659eb7ebd3e24e4d2e6ae08bf68e8479067d7ea
unknown <tkdrb15@gmail.com>
2023-05-22 14:31:32 +0900

    style: 장바구니 품목 수 애니메이션 추가

--- src/layout/header/CartAmount.tsx
+++ src/layout/header/CartAmount.tsx
@@ -1,14 +1,20 @@
-import styled from "styled-components";
+import styled, { Keyframes, keyframes } from "styled-components";
 import { useRecoilValue } from "recoil";
 import { cartListState } from "../../atoms";
 
 export const CartAmount = () => {
   const cartAmount = useRecoilValue(cartListState).length;
-  return <Style.CartAmount>{cartAmount}</Style.CartAmount>;
+  if (cartAmount === 0) return <Style.Empty />;
+  return <Style.CartAmount $appear={Style.$appear}>{cartAmount}</Style.CartAmount>;
 };
 
 const Style = {
-  CartAmount: styled.div`
+  Empty: styled.div`
+    width: 26px;
+    height: 26px;
+  `,
+
+  CartAmount: styled.div<{ $appear?: Keyframes }>`
     display: flex;
     justify-content: center;
     align-items: center;
@@ -22,5 +28,19 @@ const Style = {
     background-color: #04c09e;
     color: white;
     font-size: 16px;
+
+    animation: ${(props) => props.$appear} 0.7s ease-in;
+  `,
+
+  $appear: keyframes`
+    0%{
+      transform: scale(0);
+    }
+    80%{
+      transform: scale(1.1);
+    }
+    100%{
+      transform: scale(1);
+    }
   `,
 };
