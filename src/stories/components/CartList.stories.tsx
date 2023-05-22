5ae64c72e000213ee8a717b53254f8caa89c7b19
regularPark <srk99279@naver.com>
2023-05-22 10:14:12 +0900

    test: `CartList`, `ProductList`에 대한 interaction 작성

--- src/stories/components/CartList.stories.tsx
+++ src/stories/components/CartList.stories.tsx
@@ -3,6 +3,44 @@ import CartList from '../../components/cart/CartList';
 import { handlers } from '../../mocks/handlers';
 import { CartListWrapper } from '../../style/ContentLayout';
 import { styled } from 'styled-components';
+import Header from '../../components/@common/Header';
+import { within } from '@testing-library/react';
+import { setDataInLocalStorage } from '../../utils/localStorage';
+import { expect } from '@storybook/jest';
+import userEvent from '@testing-library/user-event';
+
+const mockData = [
+  {
+    id: 1,
+    quantity: 1,
+    product: {
+      id: 1,
+      name: 'PET보틀-정사각(420ml)',
+      price: 43400,
+      imageUrl: '/assets/product1.svg',
+    },
+  },
+  {
+    id: 2,
+    quantity: 2,
+    product: {
+      id: 2,
+      name: 'PET보틀-밀크티(370ml)',
+      price: 73400,
+      imageUrl: '/assets/product2.svg',
+    },
+  },
+  {
+    id: 3,
+    quantity: 3,
+    product: {
+      id: 3,
+      name: 'PET보틀-정사각(370ml)',
+      price: 41000,
+      imageUrl: '/assets/product3.svg',
+    },
+  },
+];
 
 const meta = {
   title: 'Pages/cart/CartList',
@@ -27,8 +65,59 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
 
+export const Interaction: Story = {
+  decorators: [
+    (Story) => {
+      return (
+        <>
+          <HeaderWrapper>
+            <Header title="CART" />
+          </HeaderWrapper>
+
+          <Story />
+        </>
+      );
+    },
+  ],
+  play: async ({ canvasElement }) => {
+    window.localStorage.clear();
+
+    setDataInLocalStorage('cart', mockData);
+
+    const canvas = within(canvasElement);
+
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    
+    const badge = document.querySelector('#cart-badge');
+    expect(badge).toContainHTML('3')
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    
+    const totalPrice = document.querySelector('#total-price')!;
+    expect(totalPrice).toHaveTextContent('총 주문금액316,200원')
+    
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    const checkedAllItems = document.querySelector('#check-all-items')!;
+    userEvent.click(checkedAllItems);
+    expect(totalPrice).toHaveTextContent('총 주문금액3,000원')
+    
+    // const removeSelectedItems = document.querySelector('#remove-checked-items')!;
+    // await userEvent.click(removeSelectedItems);
+    // await userEvent.click( screen.getByRole('button', { name: '확인' }));
+    // expect(badge).toContainHTML('0')
+
+  },
+};
+
 const S = {
   Wrapper: styled(CartListWrapper)`
     width: calc(100vw - 5vw);
   `,
 };
+
+const HeaderWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
--- src/stories/components/ProductList.stories.tsx
+++ src/stories/components/ProductList.stories.tsx
@@ -1,6 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import ProductList from '../../components/main/ProductList';
 import { handlers } from '../../mocks/handlers';
+import { within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { expect } from '@storybook/jest';
+import Header from '../../components/@common/Header';
+import { styled } from 'styled-components';
 
 const meta = {
   title: 'Pages/product/ProductList',
@@ -9,10 +14,83 @@ const meta = {
   parameters: {
     msw: handlers,
   },
-  decorators: [(Story) => <Story />],
+  decorators: [
+    (Story) => {
+      return <Story />;
+    },
+  ],
 } satisfies Meta<typeof ProductList>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
+
+export const Interaction: Story = {
+  decorators: [
+    (Story) => {
+      return (
+        <>
+          <HeaderWrapper>
+            <Header title="STORE" />
+          </HeaderWrapper>
+          <Wrapper>
+            <Story />
+          </Wrapper>
+        </>
+      );
+    },
+  ],
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    window.localStorage.clear();
+
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
+    const badge = document.querySelector('#cart-badge');
+    expect(badge).toContainHTML('0')
+
+    const addCartButton = document.querySelectorAll('svg')[1];
+    await userEvent.click(addCartButton);
+    expect(canvas.getAllByLabelText('increase'));
+    expect(badge).toContainHTML('1')
+
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    const increaseButton = document.querySelector('button[aria-label="increase"]')!;
+    await userEvent.click(increaseButton);
+    expect(canvas.getByDisplayValue(2));
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    const quantityInput = document.getElementById('product1')!;
+    await userEvent.type(quantityInput, '0', { delay: 200 });
+    expect(canvas.getByDisplayValue(20));
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    await userEvent.type(quantityInput, '{backspace}{backspace}', { delay: 200 });
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    
+    const decreaseButton = document.querySelector('button[aria-label="decrease"]')!;
+    await userEvent.click(decreaseButton);
+
+    await new Promise((resolve) => setTimeout(resolve, 100));
+
+    expect(badge).toContainHTML('0')
+
+  },
+};
+
+const HeaderWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const Wrapper = styled.div`
+  margin-top: 120px;
+`;
