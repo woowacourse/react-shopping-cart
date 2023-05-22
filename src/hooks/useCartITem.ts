5244088aeed0a66cb95d1beb9498f849c328c60e
unknown <tkdrb15@gmail.com>
2023-05-22 17:33:32 +0900

    git 오류 수정

--- src/hooks/useCartITem.ts
+++
@@ -1,5 +0,0 @@
-    if (!isInCart()) setCartList((current) => [...current, { id, quantity: 1 }]);
-    setCartList((current) => current.filter((item) => item.id !== id));
-  const changeCartItemQuantity = (quantity: number) => {
-    quantity,
-    changeCartItemQuantity,
