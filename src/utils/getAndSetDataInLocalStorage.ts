37ee41c1b0a77308fa540eee9260380c8d03857e
Dain Lee <feb.dain@gmail.com>
2023-05-22 13:08:03 +0900

    refactor: `getDataFromLocalStorage`에도 타입 지정

--- src/utils/getAndSetDataInLocalStorage.ts
+++ src/utils/getAndSetDataInLocalStorage.ts
@@ -2,6 +2,9 @@ export const setDataInLocalStorage = <T>(key: string, data: T) => {
   localStorage.setItem(key, JSON.stringify(data));
 };
 
-export const getDataFromLocalStorage = (key: string) => {
-  return localStorage.getItem(key);
+export const getDataFromLocalStorage = (key: string): string => {
+  const data = localStorage.getItem(key);
+
+  if (!data) return '[]';
+  return data;
 };
