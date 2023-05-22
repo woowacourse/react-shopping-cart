a79af1f06b5617f8513706770fd36bdf8693551f
wzrabbit <curious.wzrabbit@gmail.com>
2023-05-22 17:36:31 +0900

    ♻️ refactor: 항상 모킹 서버가 실행되도록 변경(배포와 관계없이)

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,15 @@ import { RecoilRoot } from "recoil";
 import GlobalStyle from "./GlobalStyle";
 import { HashRouter } from "react-router-dom";
 
+/**
+ * 본래 mocking 서버는 배포 상태에서는 실행되어서는 안 되지만,
+ * 백엔드 서버가 현재 존재하지 않고, 테스트 목적이므로 mocking 서버로 동작하도록 배포합니다.
+ *
+ * "false" 부분의 조건문에는 실제로 아래의 조건이 들어가야 합니다:
+ * process.env.NODE_ENV !== "development"
+ */
 const startMSW = async () => {
-  if (process.env.NODE_ENV !== "development") {
+  if (false) {
     return;
   }
 
