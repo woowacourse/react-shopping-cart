f559484f10cde142ced7035b1e704b77d10faa1e
ashleysyheo <ashleysyheo@gmail.com>
2023-05-22 19:34:10 +0900

    refactor: CartItemDelete 컴포넌트 버튼들 aria-label 삭제

--- src/components/cart/CartItemDelete/CartItemDelete.tsx
+++ src/components/cart/CartItemDelete/CartItemDelete.tsx
@@ -12,18 +12,10 @@ const CartItemDelete = ({ removeItem, handleModalClose }: CartItemDeleteProps) =
         해당 상품을 삭제하시겠습니까?
       </S.CartItemDeleteMessage>
       <S.CartItemDeleteButtonContainer>
-        <S.CartItemDeleteCancelButton
-          aria-label="close modal"
-          variant="textButton"
-          onClick={handleModalClose}
-        >
+        <S.CartItemDeleteCancelButton variant="textButton" onClick={handleModalClose}>
           취소
         </S.CartItemDeleteCancelButton>
-        <S.CartItemDeleteButton
-          aria-label="delete cart item"
-          variant="textButton"
-          onClick={() => removeItem()}
-        >
+        <S.CartItemDeleteButton variant="textButton" onClick={() => removeItem()}>
           삭제
         </S.CartItemDeleteButton>
       </S.CartItemDeleteButtonContainer>
