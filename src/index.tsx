4575842f9d2b107e51dbc11894565fb6ed750ab4
SangwonKang <72087183+ksone02@users.noreply.github.com>
2023-05-22 17:31:56 +0900

    feat: queryClient 추가

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 import App from './components/App';
 import { ResetStyle } from './styles/ResetStyle';
+import { worker } from './mock/browsers';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+worker.start();
 root.render(
   <React.StrictMode>
-    <RecoilRoot>
-      <ResetStyle />
-      <App />
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <ResetStyle />
+        <App />
+      </RecoilRoot>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
