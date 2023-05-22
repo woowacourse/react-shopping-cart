812f61c5b81b344cf61bbd8ca659a877646cdcd6
AhnGeunTaek <dksrmsxor@gmail.com>
2023-05-22 17:54:28 +0900

    fix: 에러 파일 임시 제거

--- src/stories/CartNotificationButton.stories.tsx
+++ src/stories/CartNotificationButton.stories.tsx
@@ -1,26 +1,26 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import CartNotificationButton from "../components/CartNotificationButton/CartNotificationButton";
+// import type { Meta, StoryObj } from "@storybook/react";
+// import CartNotificationButton from "../components/CartNotificationButton/CartNotificationButton";
 
-/**
- * 장바구니에 제품이 추가되면 카운트하고 보여주는 역할을 하는 컴포넌트입니다.
- */
+// /**
+//  * 장바구니에 제품이 추가되면 카운트하고 보여주는 역할을 하는 컴포넌트입니다.
+//  */
 
-const meta = {
-  title: "CartNotificationButton",
-  component: CartNotificationButton,
-  decorators: [
-    (Story) => (
-      <div style={{ backgroundColor: "black" }}>
-        <Story />
-      </div>
-    ),
-  ],
-} satisfies Meta<typeof CartNotificationButton>;
+// const meta = {
+//   title: "CartNotificationButton",
+//   component: CartNotificationButton,
+//   decorators: [
+//     (Story) => (
+//       <div style={{ backgroundColor: "black" }}>
+//         <Story />
+//       </div>
+//     ),
+//   ],
+// } satisfies Meta<typeof CartNotificationButton>;
 
-type Story = StoryObj<typeof meta>;
+// type Story = StoryObj<typeof meta>;
 
-export const Standard: Story = {
-  args: { productCount: 8 },
-};
+// export const Standard: Story = {
+//   args: { productCount: 8 },
+// };
 
-export default meta;
+// export default meta;
--- src/stories/ProductCard.stories.tsx
+++ src/stories/ProductCard.stories.tsx
@@ -1,25 +1,25 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import ProductCard from "../components/ProductCard/ProductCard";
+// import type { Meta, StoryObj } from "@storybook/react";
+// import ProductCard from "../components/ProductCard/ProductCard";
 
-/** 
-  각각의 제품 정보를 나타내는 ProductCard 컴포넌트입니다.
+// /**
+//   각각의 제품 정보를 나타내는 ProductCard 컴포넌트입니다.
 
-*/
-const meta = {
-  title: "ProductCard",
-  component: ProductCard,
-} satisfies Meta<typeof ProductCard>;
+// */
+// const meta = {
+//   title: "ProductCard",
+//   component: ProductCard,
+// } satisfies Meta<typeof ProductCard>;
 
-type Story = StoryObj<typeof meta>;
+// type Story = StoryObj<typeof meta>;
 
-export const Standard: Story = {
-  args: {
-    productId: 1,
-    productImage:
-      "https://user-images.githubusercontent.com/87642422/237034964-aae42f44-a957-4083-b2ed-d1a65b91f7f7.png",
-    productName: "소고기",
-    productPrice: 43400,
-  },
-};
+// export const Standard: Story = {
+//   args: {
+//     productId: 1,
+//     productImage:
+//       "https://user-images.githubusercontent.com/87642422/237034964-aae42f44-a957-4083-b2ed-d1a65b91f7f7.png",
+//     productName: "소고기",
+//     productPrice: 43400,
+//   },
+// };
 
-export default meta;
+// export default meta;
