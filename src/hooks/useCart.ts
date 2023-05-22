d000434eddaccf723822d3f6cb9b9c723ddfaac9
inyeong-kang <tomatorkd@naver.com>
2023-05-22 14:40:59 +0900

    refactor: API로 장바구니 데이터 업데이트

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -1,25 +1,33 @@
 import type { CartItemType } from '../types';
 import { useRecoilState } from 'recoil';
 import { cartState, selectedCartState } from '../recoil/state';
+import { createCartItem, deleteCartItem, updateCartItemQuantity } from '../api/cart';
 
 export default function useCart() {
   const [cart, setCart] = useRecoilState(cartState);
   const [selectedCart, setSelectedCart] = useRecoilState(selectedCartState);
 
-  const addCartItem = (cartItem: CartItemType) => {
-    setCart([...cart, cartItem]);
+  const addCartItem = async (productId: number) => {
+    const newCartItem = await createCartItem(productId);
+    setCart([...cart, newCartItem]);
   };
 
   const removeCartItem = (productId: number) => {
-    setSelectedCart(selectedCart.filter((id) => id !== productId));
-    setCart(cart.filter((item) => item.product.id !== productId));
+    const cartItem = cart.find((item) => item.product.id === productId);
+    if (cartItem) {
+      deleteCartItem(cartItem.id);
+
+      setSelectedCart(selectedCart.filter((id) => id !== productId));
+      setCart(cart.filter((item) => item.id !== cartItem.id));
+    }
   };
 
   const updateQuantity = (productId: number, quantity: number) => {
     const orderIndex = cart.findIndex((order) => order.product.id === productId);
+    updateCartItemQuantity(cart[orderIndex].id, quantity);
+
     const newCart = [...cart];
     newCart.splice(orderIndex, 1, { ...cart[orderIndex], quantity });
-
     setCart(newCart);
   };
 
