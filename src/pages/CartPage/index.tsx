8e6f86f7d88424446b06bab760fbb71532b9e785
Gilpop8663 <wolfye@naver.com>
2023-05-22 15:46:24 +0900

    chore: 사용하지 않는 import 코드 삭제

--- src/pages/CartPage/index.tsx
+++ src/pages/CartPage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { styled } from 'styled-components';
 import useCartList from '@hooks/useCartList';
 import useControlCart from '@hooks/useControlCart';
