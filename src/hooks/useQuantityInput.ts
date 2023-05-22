480e651a2043d037983e8e617c4921506b181c68
bassyu <yrkdgus@gmail.com>
2023-05-22 16:30:11 +0900

    feat(useQuantityInput): 장바구니 수량 변경에서 API 에러 대응 추가

--- src/hooks/useQuantityInput.ts
+++ src/hooks/useQuantityInput.ts
@@ -3,16 +3,47 @@ import { useSetRecoilState } from 'recoil';
 
 import * as api from '../api';
 import { cartState } from '../recoil/state';
+import { API_ERROR_MESSAGE } from '../constants';
 
 const useQuantityInput = (cartItemId: number) => {
   const [quantityInput, setQuantityInput] = useState('');
   const setCart = useSetRecoilState(cartState);
 
-  const setQuantityInputProxy = (quantity: number) => {
+  const getCart = async () => {
+    try {
+      const cart = await api.getCart();
+      setCart(cart);
+    } catch {
+      alert(API_ERROR_MESSAGE.getCart);
+    }
+  };
+
+  const deleteCartItem = async () => {
+    try {
+      await api.deleteCartItem(cartItemId);
+    } catch {
+      alert(API_ERROR_MESSAGE.deleteCartItem);
+      return;
+    }
+
+    getCart();
+  };
+
+  const patchCartItemQuantity = async (quantity: number) => {
+    try {
+      await api.patchCartItemQuantity(cartItemId, quantity);
+    } catch {
+      alert(API_ERROR_MESSAGE.postCartItem);
+    }
+
+    getCart();
+  };
+
+  const setQuantityInputProxy = async (quantity: number) => {
     if (quantity === 0) {
-      api.deleteCartItem(cartItemId).then(api.getCart).then(setCart);
+      deleteCartItem();
     } else {
-      api.patchCartItemQuantity(cartItemId, quantity).then(api.getCart).then(setCart);
+      patchCartItemQuantity(quantity);
     }
 
     setQuantityInput(quantity.toString());
