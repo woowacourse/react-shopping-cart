62576b9611c1c8dfd5a4eee5660cf7e0fa321cd8
hafnium1923 <hafnium1923@gmail.com>
2023-05-22 15:47:08 +0900

    refactor: 에러메시지 상수화

--- src/abstract/constants.ts
+++ src/abstract/constants.ts
@@ -16,4 +16,10 @@ const FETCH = {
   DELETE: "DELETE",
 } as const;
 
-export { MONEY, ZERO, API, FETCH };
+const ERROR_MESSAGE = {
+  PRODUCT: "상품 목록을 불러오지 못했어요!",
+  SHOPPING_LIST: "장바구니 목록을 불러오지 못했어요!",
+  ADD_TO_CART: "장바구니에 물건이 추가되지 않았어요!",
+  DELETE_TO_CART: "장바구니 물건이 삭제되지 않았어요!",
+} as const;
+export { MONEY, ZERO, API, FETCH, ERROR_MESSAGE };
