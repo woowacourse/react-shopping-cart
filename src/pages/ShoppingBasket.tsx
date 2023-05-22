9de99701720b63fb7a90f36bb8509c483e4fb567
hozzijeong <ghwls0905@naver.com>
2023-05-22 16:15:01 +0900

    refactor: 장바구니 불러오는 함수 로직 분리

--- src/pages/ShoppingBasket.tsx
+++ src/pages/ShoppingBasket.tsx
@@ -1,33 +1,16 @@
-import { useEffect } from 'react';
-import { useSetRecoilState } from 'recoil';
 import ContentLayout from 'src/components/@common/ContentLayout';
 import CartList from 'src/components/Cart/CartList';
 import OrderInfo from 'src/components/Cart/OrderInfo';
 import Header from 'src/components/@common/Header';
-import { useGetFetch } from 'src/hooks/useFetch';
-import useToast from 'src/hooks/useToast';
-import { cartListAtom } from 'src/recoil/cartList';
-import { CartItem } from 'src/types';
 import { styled } from 'styled-components';
+import useGetCartList from 'src/hooks/useGetCartList';
+import { Suspense } from 'react';
 
 const ShoppingBasket = () => {
-  const { toast } = useToast();
-  const { data, error, loading } = useGetFetch<CartItem[]>(
-    '/api/cart-items',
-    []
-  );
-  const setCartItem = useSetRecoilState(cartListAtom); // 로그인 했을 경우에는 데이터를 받아오고, 그게 아닌 경우에는 로컬스토리지로 저장할 것.
-
-  useEffect(() => {
-    if (loading) return;
-    if (error.isError) {
-      toast.error(error.message);
-    }
-    setCartItem(data);
-  }, [loading]);
+  useGetCartList();
 
   return (
-    <>
+    <Suspense fallback={<div>loading</div>}>
       <Header />
       <ContentLayout>
         <BasketTitle>장바구니</BasketTitle>
@@ -38,7 +21,7 @@ const ShoppingBasket = () => {
           </OrderInfoWrapper>
         </BasketContent>
       </ContentLayout>
-    </>
+    </Suspense>
   );
 };
 
