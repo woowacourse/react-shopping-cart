c107fdd6469df6d8f9598f9d7e99dc608f23cbe4
cruelladevil <dev.timetravel@gmail.com>
2023-05-22 12:17:04 +0900

    feat: cart atom 추가

--- src/atoms/cart.ts
+++ src/atoms/cart.ts
@@ -1,4 +1,4 @@
-import { AtomEffect, atom, selector } from 'recoil';
+import { AtomEffect, DefaultValue, atom, selector } from 'recoil';
 import { Cart } from '../types/cart';
 
 const localStorageEffect: <T>(key: string) => AtomEffect<T> =
@@ -26,6 +26,54 @@ export const cartState = atom<Cart[]>({
 export const totalAmountState = selector({
   key: 'TotalAmountState',
   get: ({ get }) => {
-    return get(cartState).length;
+    const cart = get(cartState);
+
+    const totalItems = cart.length;
+    const totalPrice = cart.reduce((total, { isSelected, product, quantity }) => {
+      if (!isSelected) return total;
+
+      return total + product.price * quantity;
+    }, 0);
+
+    return {
+      totalItems,
+      totalPrice,
+    };
+  },
+});
+
+export const cartSelectedState = selector({
+  key: 'CartSelectedState',
+  get: ({ get }) => {
+    const cart = get(cartState);
+
+    return cart.map(({ isSelected }) => isSelected);
+  },
+  set: ({ get, set }, newValue) => {
+    const cart = get(cartState);
+    const newCart =
+      newValue instanceof DefaultValue
+        ? newValue
+        : cart.map((prev, i) => ({
+            ...prev,
+            isSelected: newValue[i],
+          }));
+
+    set(cartState, newCart);
+  },
+});
+
+export const cartStatus = selector({
+  key: 'CartStatus',
+  get: ({ get }) => {
+    const selectedList = get(cartSelectedState);
+
+    const selectedAmount = selectedList.filter((isSelected) => isSelected === true).length;
+    const allSelected = selectedList.every((isSelected) => isSelected === true);
+
+    return {
+      selectedAmount,
+      allSelected,
+    };
   },
 });
