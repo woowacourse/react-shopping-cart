ad98a7f761bbb5a669720bb0feff2b1787aea4e9
turtle601 <turtle601@naver.com>
2023-05-22 12:24:38 +0900

    chore: 배포 환경 설정 변경

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,24 @@ import GlobalStyle from '@/styles/global';
 import { GlobalFontStyles } from '@/styles/font';
 import { worker } from './mocks/worker';
 
-worker.start();
+async function main() {
+	await worker.start({
+		serviceWorker: {
+			url: '/mockServiceWorker.js',
+		},
+	});
 
-const root = ReactDOM.createRoot(
-	document.getElementById('root') as HTMLElement
-);
+	const root = ReactDOM.createRoot(
+		document.getElementById('root') as HTMLElement
+	);
 
-root.render(
-	<React.StrictMode>
-		<GlobalFontStyles />
-		<GlobalStyle />
-		<App />
-	</React.StrictMode>
-);
+	root.render(
+		<React.StrictMode>
+			<GlobalFontStyles />
+			<GlobalStyle />
+			<App />
+		</React.StrictMode>
+	);
+}
+
+main();
