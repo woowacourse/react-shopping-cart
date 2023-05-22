462fc32fc0a8bbbcc1a0b243e60a2eb41f4b13c4
wzrabbit <curious.wzrabbit@gmail.com>
2023-05-22 17:25:04 +0900

    ♻️ refactor: 스토리북의 컴포넌트 배경을 검정색으로 변경

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -12,6 +12,15 @@ const preview: Preview = {
         date: /Date$/,
       },
     },
+    backgrounds: {
+      default: "dark",
+      values: [
+        {
+          name: "dark",
+          value: "#000",
+        },
+      ],
+    },
   },
   decorators: [
     (Story) => (
