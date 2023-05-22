19459d1dc58c07fecf308c78863b6c5e0ee03acc
hozzijeong <ghwls0905@naver.com>
2023-05-22 16:13:35 +0900

    refactor: 데이터 수정시에 localStorage에 접근해서 변경하는 방식으로 수정

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,44 +1,34 @@
 import { rest } from 'msw';
 import productJson from './data/productList.json';
-import {
-  CartItem,
-  CartItemPatchBody,
-  CartItemPostBody,
-  Product,
-} from 'src/types';
+import { CartItem, CartItemPatchBody, CartItemPostBody } from 'src/types';
+import { getLocalStorage, setLocalStorage } from 'src/utils/storage';
 // 초기 상품 목록
 
-const initialData: {
-  products: Product[];
-  cartLists: CartItem[];
-} = {
-  products: productJson.choonsikProducts as Product[],
-  cartLists: [] as CartItem[],
-};
+const products = productJson.choonsikProducts;
 
 export const handlers = [
   // 상품 목록 조회 API
   rest.get('/api/products', (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(initialData.products));
+    return res(ctx.status(200), ctx.json(products));
   }),
 
   // 장바구니 조회 API
   rest.get('/api/cart-items', async (req, res, ctx) => {
-    const cartItem = localStorage.getItem('cartItem');
+    const { data: cartList, error } = getLocalStorage<CartItem[]>(
+      'cartItem',
+      []
+    );
 
-    if (cartItem == null) return res(ctx.status(200), ctx.json([]));
+    if (error.isError) return res(ctx.status(200), ctx.json([]));
 
-    const currentCartItems = (await JSON.parse(cartItem)) as CartItem[];
-    initialData.cartLists = currentCartItems;
-
-    return res(ctx.status(200), ctx.json(currentCartItems));
+    return res(ctx.status(200), ctx.json(cartList));
   }),
 
   // 장바구니 추가 API
   rest.post('/api/cart-items', async (req, res, ctx) => {
     const { productId } = (await req.json()) as CartItemPostBody;
 
-    const product = initialData.products.find(({ id }) => id === productId);
+    const product = products.find(({ id }) => id === productId);
 
     if (!product) {
       return res(
@@ -47,12 +37,17 @@ export const handlers = [
       );
     }
 
-    initialData.cartLists.push({
-      id: productId,
-      quantity: 1,
-      product,
-      isSelected: true,
-    });
+    const { data: cartList } = getLocalStorage<CartItem[]>('cartItem', []);
+
+    setLocalStorage('cartItem', [
+      ...cartList,
+      {
+        id: productId,
+        quantity: 1,
+        product,
+        isSelected: true,
+      },
+    ]);
 
     return res(ctx.status(201), ctx.json({}));
   }),
@@ -60,9 +55,12 @@ export const handlers = [
   // 장바구니 삭제 API
   rest.delete('/api/cart-items/:cartItemId', (req, res, ctx) => {
     const { cartItemId } = req.params;
-    const { cartLists } = initialData;
+    const { data: cartList, error } = getLocalStorage<CartItem[]>(
+      'cartItem',
+      []
+    );
 
-    const selectedCartIndex = cartLists.findIndex(
+    const selectedCartIndex = cartList.findIndex(
       ({ id }) => id === Number(cartItemId)
     );
 
@@ -73,9 +71,9 @@ export const handlers = [
       );
     }
 
-    const deletedList = cartLists.filter(({ id }) => id !== Number(cartItemId));
+    const deletedList = cartList.filter(({ id }) => id !== Number(cartItemId));
 
-    initialData.cartLists = deletedList;
+    setLocalStorage('cartItem', deletedList);
 
     return res(ctx.status(204));
   }),
@@ -86,7 +84,12 @@ export const handlers = [
     const { cartItemId } = req.params;
     const { quantity } = (await req.json()) as CartItemPatchBody;
 
-    const cartItemIndex = initialData.cartLists.findIndex(
+    const { data: cartList, error } = getLocalStorage<CartItem[]>(
+      'cartItem',
+      []
+    );
+
+    const cartItemIndex = cartList.findIndex(
       ({ id }) => id === Number(cartItemId)
     );
 
@@ -97,10 +100,11 @@ export const handlers = [
       );
     }
 
-    initialData.cartLists[cartItemIndex] = {
-      ...initialData.cartLists[cartItemIndex],
-      quantity,
-    };
+    const updated = [...cartList].map((item, idx) =>
+      idx === cartItemIndex ? { ...item, quantity } : item
+    );
+
+    setLocalStorage('cartItem', updated);
 
     return res(ctx.status(200), ctx.json({}));
   }),
