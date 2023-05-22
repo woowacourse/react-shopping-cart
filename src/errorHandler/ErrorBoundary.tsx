239610ca1ea632014c897ad2c7d88e63a66df571
woo-jk <201702042@o.cnu.ac.kr>
2023-05-22 14:32:46 +0900

    feat: ErrorBoundary 컴포넌트 생성

---
+++ src/errorHandler/ErrorBoundary.tsx
@@ -0,0 +1,36 @@
+import React, { ErrorInfo, ReactNode } from 'react';
+
+interface ErrorBoundaryProps {
+  children?: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  state: ErrorBoundaryState = {
+    hasError: false,
+  };
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error('Uncaught error:', error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
