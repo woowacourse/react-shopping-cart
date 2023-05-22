448da3e78cb981805978f174eb9e517f807eb981
wzrabbit <curious.wzrabbit@gmail.com>
2023-05-22 17:10:39 +0900

    ✨ feat: useCartInfosUpdater 구현
    - 장바구니의 수량을 변경할 때 사용

---
+++ src/hooks/useCartInfosUpdater.ts
@@ -0,0 +1,42 @@
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { productsAtom } from "../store/fetchAtoms";
+import { cartLocalInfosAtom } from "../store/cartProductsAtoms";
+import { IdQuantity } from "../types";
+
+const useCartQuantityUpdater = () => {
+  const products = useRecoilValue(productsAtom);
+  const setCartInfos = useSetRecoilState(cartLocalInfosAtom);
+
+  const getPriceById = (id: number) => {
+    const searchedProduct = products.find((product) => product.id === id);
+
+    if (!searchedProduct) {
+      alert("상품을 찾지 못했습니다. 사이트를 새로고침 해 주세요.");
+      throw Error();
+    }
+
+    return searchedProduct.price;
+  };
+
+  const updateCartQuantity = ({ id, quantity }: IdQuantity) => {
+    const price = getPriceById(id);
+
+    setCartInfos((prevCartInfos) => {
+      if (prevCartInfos.every((prevCartInfo) => prevCartInfo.id !== id)) {
+        return [...prevCartInfos, { id, quantity, price: price }];
+      }
+
+      return prevCartInfos.map((prevCartInfo) => {
+        if (prevCartInfo.id === id) {
+          return { id, quantity, price };
+        }
+
+        return prevCartInfo;
+      });
+    });
+  };
+
+  return { updateCartQuantity };
+};
+
+export { useCartQuantityUpdater };
