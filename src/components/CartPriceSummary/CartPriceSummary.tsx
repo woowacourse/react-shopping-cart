eab3e1d7e8ff580d329903c6b38b0c4b6e6cd5c7
nangkyeonglim <nangkyeonglim@gmail.com>
2023-05-22 14:57:31 +0900

    feat: 결제 금액 구현 및 cart에 담긴 물건 없을 때 뷰 렌더링 안하도록 구현

--- src/components/CartPriceSummary/CartPriceSummary.tsx
+++ src/components/CartPriceSummary/CartPriceSummary.tsx
@@ -1,27 +1,37 @@
+import { useRecoilValue } from 'recoil';
 import { styled } from 'styled-components';
+import { cartListAtom, cartTotalPriceSelector } from '../../stores/cartItemsStore';
 
 const CartPriceSummary = () => {
+  const cartList = useRecoilValue(cartListAtom);
+  const cartPrice = useRecoilValue(cartTotalPriceSelector);
+  const shippingPrice = cartPrice === 0 ? 0 : 3000;
+
   return (
-    <Wrapper>
-      <Title>결제예상금액</Title>
-      <Content>
-        <PriceInfoWrapper>
-          <PriceInfo>
-            <span>총 상품가격</span>
-            <span>24,700원</span>
-          </PriceInfo>
-          <PriceInfo>
-            <span>총 배송비</span>
-            <span>3,000원</span>
-          </PriceInfo>
-        </PriceInfoWrapper>
-        <PriceInfo>
-          <span>총 주문금액</span>
-          <span>27,700원</span>
-        </PriceInfo>
-        <OrderButton type='button'>주문하기</OrderButton>
-      </Content>
-    </Wrapper>
+    <>
+      {cartList.length !== 0 && (
+        <Wrapper>
+          <Title>결제예상금액</Title>
+          <Content>
+            <PriceInfoWrapper>
+              <PriceInfo>
+                <span>총 상품가격</span>
+                <span>{cartPrice.toLocaleString()}원</span>
+              </PriceInfo>
+              <PriceInfo>
+                <span>총 배송비</span>
+                <span>{shippingPrice.toLocaleString()}원</span>
+              </PriceInfo>
+            </PriceInfoWrapper>
+            <PriceInfo>
+              <span>총 주문금액</span>
+              <span>{(cartPrice + shippingPrice).toLocaleString()}원</span>
+            </PriceInfo>
+            <OrderButton type='button'>주문하기</OrderButton>
+          </Content>
+        </Wrapper>
+      )}
+    </>
   );
 };
 
@@ -42,7 +52,7 @@ const Title = styled.h3`
   border-bottom: 3px solid #ddd;
   font-style: normal;
   font-weight: 400;
-  font-size: 24px;
+  font-size: 20px;
   line-height: 33px;
 
   letter-spacing: 0.5px;
@@ -64,7 +74,7 @@ const OrderButton = styled.button`
   background-color: #333;
   color: #fff;
   font-weight: 400;
-  font-size: 24px;
+  font-size: 20px;
   line-height: 21px;
   cursor: pointer;
 `;
@@ -83,7 +93,7 @@ const PriceInfo = styled.div`
   padding: 0 6px;
   justify-content: space-between;
   font-weight: 700;
-  font-size: 20px;
+  font-size: 16px;
   line-height: 27px;
   letter-spacing: 0.5px;
   color: #333333;
