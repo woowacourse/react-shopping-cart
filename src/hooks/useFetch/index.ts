00260f658c8846029c54331cfccb24df52a49f20
nlom0218 <nlom0218@naver.com>
2023-05-22 17:38:27 +0900

    refactor: HTTP 상태에 따른 예외 처리
    400, 401, 403, 404, 500 상태 코드에 따른 예외 처리 및 response가 안되었을 경우 에러 처리

--- src/hooks/useFetch/index.ts
+++ src/hooks/useFetch/index.ts
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 
+import { ERROR_MESSAGE } from '@Constants/index';
+
 const useFetch = <T>(url: string) => {
   const [data, setData] = useState<T>();
   const [status, setStatus] = useState<'loading' | 'error' | 'success'>('loading');
@@ -10,13 +12,14 @@ const useFetch = <T>(url: string) => {
       try {
         const res = await fetch(url);
 
-        if (res.status === 404) throw new Error('요청받은 페이지를 찾을 수 없습니다. 주소가 정확한지 확인해주세요.');
+        if (res.status === 400) throw new Error(ERROR_MESSAGE[400]);
+        if (res.status === 401) throw new Error(ERROR_MESSAGE[401]);
+        if (res.status === 403) throw new Error(ERROR_MESSAGE[403]);
+        if (res.status === 404) throw new Error(ERROR_MESSAGE[404]);
 
-        if (!res.ok) {
-          throw new Error(
-            '서버 내부에서 오류가 발생했습니다. 해당 오류가 지속적으로 발생한다면 관리자에게 문의해주세요.',
-          );
-        }
+        if (res.status === 500) throw new Error(ERROR_MESSAGE[500]);
+
+        if (!res.ok) throw new Error(ERROR_MESSAGE.default);
 
         const data = await res.json();
         setData(data);
