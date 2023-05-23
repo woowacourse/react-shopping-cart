affd177478271eb8e331fbf1a7a42abe946ad4f7
chsua <csj1919@naver.com>
2023-05-23 20:45:34 +0900

    refactor: msw에서 하기 위해 기존 로컬스토리지 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
-import { LocalData } from '@Utils/localData';
 import { Outlet } from 'react-router-dom';
-import { useSetRecoilState } from 'recoil';
 
 import Header from '@Components/Header';
 
 import GlobalStyle, { CommonPageStyle } from '@Styles/GlobalStyle';
 
-import { ShoppingCartProduct } from '@Types/index';
-
-import shoppingCartState from '@Atoms/shoppingCartState';
-
-import { LOCAL_STORAGE_KEYWORD } from '@Constants/index';
-
 function App() {
-  const setShoppingCart = useSetRecoilState<ShoppingCartProduct[]>(shoppingCartState);
-  setShoppingCart(LocalData.getData(LOCAL_STORAGE_KEYWORD.SHOPPING_CART) ?? []);
-
   return (
     <>
       <GlobalStyle />
