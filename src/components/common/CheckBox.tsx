a62eefd906db3754444be759bc09c886583cf763
gyeongza <gyeongza@naver.com>
2023-05-24 00:11:44 +0900

    [step2] refactor: input을 self-closing 태그 형태로 변경

--- src/components/common/CheckBox.tsx
+++ src/components/common/CheckBox.tsx
@@ -12,7 +12,7 @@ export const CheckBox = ({ isChecked, onChange }: CheckBoxProps) => {
       type="checkbox"
       checked={isChecked}
       onChange={onChange}
-    ></StyledCheckBoxInput>
+    />
   );
 };
 
