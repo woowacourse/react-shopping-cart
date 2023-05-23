b7e8a3aa5bef2b89a07a8c605214dcaed490d8ce
ashleysyheo <ashleysyheo@gmail.com>
2023-05-23 09:47:09 +0900

    fix: Error 컴포넌트 스토리 args 변경된 props에 맞춰서 수정

--- src/stories/common/Error.stories.tsx
+++ src/stories/common/Error.stories.tsx
@@ -1,14 +1,12 @@
 import { Meta, StoryObj } from '@storybook/react';
 
 import Error from '../../components/common/Error/Error';
+import { HTTP_ERROR_MESSAGE, HTTP_STATUS_CODE } from '../../constants/api';
 import { StoryContainerWrapper } from '../styles';
 
 const meta = {
   title: 'ShoppingCart/Common/Error',
   component: Error,
-  argTypes: {
-    statusCode: { control: 'select', options: [400, 404, 500, undefined] },
-  },
   decorators: [
     (Story) => (
       <StoryContainerWrapper>
@@ -30,20 +28,20 @@ export const Default: Story = {
 export const BadRequest: Story = {
   args: {
     message: '잘못된 요청입니다.',
-    statusCode: 400,
+    information: HTTP_ERROR_MESSAGE[HTTP_STATUS_CODE.BAD_REQUEST],
   },
 };
 
 export const NotFound: Story = {
   args: {
     message: '페이지를 찾을 수 없습니다.',
-    statusCode: 404,
+    information: HTTP_ERROR_MESSAGE[HTTP_STATUS_CODE.NOT_FOUND],
   },
 };
 
 export const InternalServer: Story = {
   args: {
     message: '현재 페이지를 표시할 수 없습니다.',
-    statusCode: 500,
+    information: HTTP_ERROR_MESSAGE[HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR],
   },
 };
