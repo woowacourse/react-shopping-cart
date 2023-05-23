d1c221fe0f7219a6bd215ee263efb15d478afede
hozzijeong <ghwls0905@naver.com>
2023-05-23 11:34:49 +0900

    refactor: productItem 로직 변경에 따른 변수 및 메서드 변경

--- src/components/ProductItem/index.tsx
+++ src/components/ProductItem/index.tsx
@@ -4,29 +4,37 @@ import Svg from '../@common/Svg';
 import Counter from '../@common/Counter';
 import * as S from './ProductItem.styles';
 import { convertKORWon } from 'src/utils';
+import { memo, useMemo } from 'react';
 
 interface ProductItemProps {
   product: Product;
 }
 
 const ProductItem = ({ product }: ProductItemProps) => {
-  const { currentCartItem, patchCartItem, addCartItem } =
-    useCartUpdate(product);
-
-  const productSelect = currentCartItem ? (
-    <Counter
-      count={currentCartItem.quantity}
-      productCountMethod={patchCartItem}
-    />
-  ) : (
-    <Svg
-      type="cart-icon"
-      width={25}
-      height={22}
-      onClick={addCartItem}
-      cursor={'pointer'}
-    />
+  const { currentCartItem, patchCartItem, addCartItem } = useCartUpdate(
+    product.id
   );
+
+  const onClick = () => addCartItem(product);
+
+  const itemCount = patchCartItem(currentCartItem);
+
+  const productSelect = useMemo(() => {
+    return currentCartItem ? (
+      <Counter
+        count={currentCartItem.quantity}
+        productCountMethod={itemCount}
+      />
+    ) : (
+      <Svg
+        type="cart-icon"
+        width={25}
+        height={22}
+        onClick={onClick}
+        cursor={'pointer'}
+      />
+    );
+  }, [currentCartItem]);
 
   return (
     <S.ItemWrapper>
@@ -42,4 +50,4 @@ const ProductItem = ({ product }: ProductItemProps) => {
   );
 };
 
-export default ProductItem;
+export default memo(ProductItem);
