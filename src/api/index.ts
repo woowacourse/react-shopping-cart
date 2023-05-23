3001bcab2633df0bf978ede7133da148df6cb016
chsua <csj1919@naver.com>
2023-05-23 20:41:06 +0900

    refactor: fetch 훅을 일반 함수로 수정

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,21 +1,69 @@
-type ShoppingItemApiState = 'GET' | 'POST' | 'PUT' | 'DELETE';
+import { CART_API_URL, PRODUCT_API_URL } from '@Constants/index';
 
-export const getMockShoppingItemApiUrl = (state: ShoppingItemApiState, id?: number) => {
-  switch (state) {
-    case 'GET':
-      return `${state}/products`;
-    default:
-      return `${state}/products/${id}`;
+import { isHttpStatusError } from '@Utils/isHttpStatusError';
+
+export const getFetchProductList = async <T>() => {
+  try {
+    const response = await fetch(PRODUCT_API_URL);
+    isHttpStatusError(response.status);
+    const resultData = (await response.json()) as T;
+    return resultData;
+  } catch {
+    alert('현재 문제가 발생하였습니다. 잠시후 다시 시도해주세요');
+    return [];
   }
 };
 
-type SelectItemApiState = 'GET' | 'POST' | 'DELETE';
+export const getFetchCartList = async <T>() => {
+  try {
+    const response = await fetch(CART_API_URL);
+    isHttpStatusError(response.status);
+    const resultData = (await response.json()) as T;
+    return resultData;
+  } catch {
+    alert('현재 문제가 발생하였습니다. 잠시후 다시 시도해주세요');
+    return [];
+  }
+};
 
-export const getMockSelectItemApiUrl = (state: SelectItemApiState, id?: number) => {
-  switch (state) {
-    case 'GET':
-      return `${state}/cart-items`;
-    default:
-      return `${state}/cart-items/${id}`;
+type PostOrDeleteData = { id: number };
+type PutData = { quantity: number };
+
+export const postFetchCartItem = async <T>(data: PostOrDeleteData) => {
+  try {
+    const response = await fetch(CART_API_URL, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    });
+    isHttpStatusError(response.status);
+  } catch {
+    alert('현재 문제가 발생하였습니다. 잠시후 다시 시도해주세요');
+  }
+};
+
+export const putFetchCartItem = async <T>(data: PutData) => {
+  try {
+    const response = await fetch(CART_API_URL, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    });
+    isHttpStatusError(response.status);
+  } catch {
+    alert('현재 문제가 발생하였습니다. 잠시후 다시 시도해주세요');
+  }
+};
+
+export const deleteFetchCartItem = async <T>(data: PostOrDeleteData) => {
+  try {
+    const response = await fetch(CART_API_URL, {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    });
+    isHttpStatusError(response.status);
+  } catch {
+    alert('현재 문제가 발생하였습니다. 잠시후 다시 시도해주세요');
   }
 };
