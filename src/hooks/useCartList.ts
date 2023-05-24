1a371a3f21a63d00f57fbd71c392c479d2fc24b2
yeopto <gunyeop530@gmail.com>
2023-05-24 21:54:18 +0900

    refactor: async/await으로 로직 변경

--- src/hooks/useCartList.ts
+++ src/hooks/useCartList.ts
@@ -6,66 +6,63 @@ import { CART_ITEMS_BASE_URL } from '../constant';
 const useCartList = () => {
   const [cartList, setCartList] = useRecoilState(cartListState);
 
-  const addProductToCartList = (productId: Product['id']) => {
-    fetch(CART_ITEMS_BASE_URL, {
+  const addProductToCartList = async (productId: Product['id']) => {
+    const response = await fetch(CART_ITEMS_BASE_URL, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(productId),
-    })
-      .then((response) => {
-        if (!response.ok) throw new Error(response.status.toString());
-      })
-      .then(() => {
-        fetch(CART_ITEMS_BASE_URL)
-          .then((response) => {
-            if (!response.ok) throw new Error(response.status.toString());
-            return response.json();
-          })
-          .then((data) => setCartList(data));
-      });
+    });
+
+    if (!response.ok) throw new Error(response.status.toString());
+
+    const newResponse = await fetch(CART_ITEMS_BASE_URL);
+
+    if (!newResponse.ok) throw new Error(newResponse.status.toString());
+
+    const newCartList = await newResponse.json();
+
+    setCartList(newCartList);
   };
 
-  const updateProductQuantity = (targetId: Product['id'], quantity: CartItem['quantity']) => {
-    fetch(`${CART_ITEMS_BASE_URL}/${targetId}`, {
+  const updateProductQuantity = async (targetId: Product['id'], quantity: CartItem['quantity']) => {
+    const response = await fetch(`${CART_ITEMS_BASE_URL}/${targetId}`, {
       method: 'PATCH',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(quantity),
-    })
-      .then((response) => {
-        if (!response.ok) throw new Error(response.status.toString());
-      })
-      .then(() => {
-        fetch(CART_ITEMS_BASE_URL)
-          .then((response) => {
-            if (!response.ok) throw new Error(response.status.toString());
-            return response.json();
-          })
-          .then((data) => setCartList(data));
-      });
+    });
+
+    if (!response.ok) throw new Error(response.status.toString());
+
+    const newResponse = await fetch(CART_ITEMS_BASE_URL);
+
+    if (!newResponse.ok) throw new Error(newResponse.status.toString());
+
+    const newCartList = await newResponse.json();
+
+    setCartList(newCartList);
   };
 
-  const removeProductInCartList = (targetId: Product['id']) => {
-    fetch(`${CART_ITEMS_BASE_URL}/${targetId}`, {
+  const removeProductInCartList = async (targetId: Product['id']) => {
+    const response = await fetch(`${CART_ITEMS_BASE_URL}/${targetId}`, {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json',
       },
-    })
-      .then((response) => {
-        if (!response.ok) throw new Error(response.status.toString());
-      })
-      .then(() => {
-        fetch(CART_ITEMS_BASE_URL)
-          .then((response) => {
-            if (!response.ok) throw new Error(response.status.toString());
-            return response.json();
-          })
-          .then((data) => setCartList(data));
-      });
+    });
+
+    if (!response.ok) throw new Error(response.status.toString());
+
+    const newResponse = await fetch(CART_ITEMS_BASE_URL);
+
+    if (!newResponse.ok) throw new Error(newResponse.status.toString());
+
+    const newCartList = await newResponse.json();
+
+    setCartList(newCartList);
   };
 
   return { cartList, addProductToCartList, updateProductQuantity, removeProductInCartList };
