f693f46e1bdd11c6160b07e42beb268595cec149
2yunseong <dbsdltjd3701@naver.com>
2023-05-25 20:30:46 +0900

    refactor: 의미없는 코드 제거 후 인라인

--- src/atoms/productState.ts
+++ src/atoms/productState.ts
@@ -4,15 +4,13 @@ import { productQuery, productsQuery } from '../api/api';
 export const products = selector({
   key: 'productsSelector',
   get: async () => {
-    const response = productsQuery();
-    return response;
+    return productsQuery();
   },
 });
 
 export const product = selectorFamily({
   key: 'productSelector',
   get: (id: number) => async () => {
-    const response = productQuery(id);
-    return response;
+    return productQuery(id);
   },
 });
