d6b43ace27882bc746267523f870349499baa4de
Jungwoo <evencoding@gmail.com>
2023-05-25 16:27:48 +0900

    feat: 서버 URL 스위칭을 위한 전역 상태 정의

--- src/recoils/recoilApiBaseUrl.ts
+++ src/recoils/recoilApiBaseUrl.ts
@@ -1,12 +1,22 @@
-import { atom, useRecoilValue, useSetRecoilState } from 'recoil';
+import { atom, selector, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 
 import { RECOIL_KEY } from '../constants';
 
 export const ApiBaseUrlState = atom({
   key: RECOIL_KEY.API_BASE_URL_STATE,
-  default: 'http://3.34.190.40:8080',
+  default: process.env.REACT_APP_API_DEFAULT,
 });
 
-export const useApiBaseUrlValue = () => useRecoilValue(ApiBaseUrlState);
+export const ApiUrlSelector = selector({
+  key: 'apiUrlSelector',
+  get: ({ get }) => {
+    const urlState = get(ApiBaseUrlState);
+    if (urlState === '이리내') return process.env.REACT_APP_API_IRINAE;
 
-export const useSetApiBaseUrl = () => useSetRecoilState(ApiBaseUrlState);
+    if (urlState === '채채') return process.env.REACT_APP_API_CHACHA;
+  },
+});
+
+export const useApiBaseUrlState = () => useRecoilState(ApiBaseUrlState);
+
+export const useApiBaseUrlValue = () => useRecoilValue(ApiUrlSelector);
