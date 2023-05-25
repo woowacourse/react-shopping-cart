8ce344127244705c3f5a1f192a093de490a9305c
hafnium1923 <hafnium1923@gmail.com>
2023-05-25 11:46:09 +0900

    refactor: mock 데이터 이미지 경로 변경

--- src/mocks/data.ts
+++ src/mocks/data.ts
@@ -6,7 +6,7 @@ export const products: Product[] = [
     name: "엄청 긴 상품명일 수도 있잖아요 그래서 한번 해보았습니다.엄청 긴 상품명일 수도 있잖아요 그래서 한번 해보았습니다.엄청 긴 상품명일 수도 있잖아요 그래서 한번 해보았습니다.엄청 긴 상품명일 수도 있잖아요 그래서 한번 해보았습니다.엄청 긴 상품명일 수도 있잖아요 그래서 한번 해보았습니다.엄청 긴 상품명일 수도 있잖아요 그래서 한번 해보았습니다.엄청 긴 상품명일 수도 있잖아요 그래서 한번 해보았습니다.",
     price: 10000,
     imageUrl:
-      "https://s3-alpha-sig.figma.com/img/05ef/e578/d81445480aff1872344a6b1b35323488?Expires=1684713600&Signature=gvkVAezZeNJ6-pOzrsQBqsdfN9PGcEmV7HraRhfgfmXTZD0rCy-OEhFh-uP4y7AxUs5FC6ILRG-RNHslTX8x8q9HHcLyVrFq6I1uzwKaZyyWOCwAYgglD99UQlYPlVCU3E-8eIUfX~TRBM~dSPsojsKv-cNJU1gGPOE7G9C3Z1CKoDjIFZ8G2J4TG16VvSzil0RHsGGPi~06jOwn3Dtq~oBZQ1yhoRgfYDrGLlsexYockI38hGjBcBx1gJWg41~kBGNhueXs-tmuLfqRoPc0ReK~jWKLiGZi2JVPaIP-tPOfSAUEt99Po0KzVG-agBflDRhhpybhrR1bmozDvb~9eg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      "https://s3-alpha-sig.figma.com/img/05ef/e578/d81445480aff1872344a6b1b35323488?Expires=1685923200&Signature=mMQsQt3cnprJKKf4NdATsA6iVkLSEiqO~DyKZmtFviE6h~v8BRvX6bvWdOCgPk0OXnPmR5-88b5Ff1KkRPdFODWdOlbtZVzjFerGjRnphHrn85B9M2ne1oamQjDRpb9T151oMS729cSZd~kV4Cpp~b-iXPaasWO1Yh6n8WBhRDoWIT6VlMilwtkEhY5Wgvdp1JdL7JlcYgiZVcRxWXiN~wuMUkZ6NsdSJ4Lgye5ReS0-eN1g8jVv~HnOc3YrgLZK-BPqXhqJh1YtOLk31ryHLYFw1hi3yXY7o~YdG8Oa82Pg6TKdrfjoZrkc7BrrsUb6j8NAwHDO~LFTHw6ufqr-dA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
   },
   {
     id: 2,
@@ -16,72 +16,72 @@ export const products: Product[] = [
   },
   {
     id: 3,
-    name: "PET보틀-원형(350ml)",
-    price: 8000,
+    name: "PET보틀-납작(450ml)",
+    price: 39900,
     imageUrl:
-      "https://s3-alpha-sig.figma.com/img/c7b8/6249/bb062378afa996a8fe0e2f25e099aa55?Expires=1684713600&Signature=TaTYvPZbnPGVzHZeahNvJJ328E5C2mjEgvfLw4spgQ8a~k9tHslk8qx44x8dr6t4-rJnQDOC6H6OJErrhhHIPodgNLapqFaBMQCYWeeoq9Jhka-A6jxgBeQa0l99W33-WCDutoV~d9UHbpBscd2-ZzOD2DTIKp6JuHdj4CityiwOnPgJ4jpx6lwEqU5593P7jkveAGDh7yEcqrmtmmVDLlgaU-~4YO2wmD3cHX4mmTMCN4wu4woBshXCkQXwy4iGUCCdb3nNg0hrSGXRlCfe2SNdFz8IzpOlRpgNLuUwSByJ6TPiajuQ7vS-mD2KqC402ewrZMm7tnRbI1h62e08bA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      "https://s3-alpha-sig.figma.com/img/c91b/8c58/919fc1af3ddd4c1bba09101cf51013cb?Expires=1685923200&Signature=IrwaaMlu1pEwov8-9svP0z~Q8tEmfdSQwMx~iPb1RLcgYrl4rY7pWa-jAhoo0Eh2Krt2q1A4dNUSc747Jff9HXDY1GehS1ugR3-fzTrev9vqLHhOLXPXG~7bpW-OIylTVftrIa7FEjkfITimiz1GRNUyHJwjqmirOXUIIY4MNlGof6CNUvX4bC1HkASJVyrJRVeBezxlEH6i83LNJIquQmLGanuXlIGw5CLpQfT1k~cgDHl7XRI01zgIfQBA2dJYNY6PQjUdD2aglJLV~JeviDLNeyP~IHg9mvzaI2dR16DBnw3gMbiCimzFpFCh2CV-ur1emRNJAOOQT4ukQl8oXA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
   },
   {
     id: 4,
-    name: "PET보틀-원형(500ml)",
-    price: 10000,
+    name: "PET보틀-밀크티(370ml)",
+    price: 73400,
     imageUrl:
-      "https://s3-alpha-sig.figma.com/img/2410/7f6a/cd0400dbcf6d4dd8f586002d7afcd499?Expires=1684713600&Signature=N99STQ8Tf8-XXI-V9v3UJq4bpcsDWlpOn5ZNjCTHE8rc6SMR4Z5DxRR9zwspzQ-Q6F-h5JXwg0OVC-WwjSJMUcAJjWSLoBJ7lQC32UALwNBtHLjcWkNWgZ6DBWNClF~dfHCHEMFrcmj0Bdb3zFPjrtDlNSRdqksqVVZBiAeSmbhKWOkoHnKt23335HJyrHy6xoYJ6hygjpkZnwgAJFTBS8FHAJtwH26gALPGNqI-mDvdPGV2IklIv7VO-hp9kZ6gwqXuRJfhcPFke9g836-Aop9EzYSWxk03fDZezWBv8cx5A7R5NLGSm0fF4HSp~Q5sfL2t-nXxoQ7HR6yhHSh11Q__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      "https://s3-alpha-sig.figma.com/img/c7b8/6249/bb062378afa996a8fe0e2f25e099aa55?Expires=1685923200&Signature=f5ZQbo0P~FBsR673MtawS~dTjpWJq9ljBRWinQgIiMvnSWWu5w9--0n~pPQn2sXVbPf~GNJE1tQrjavgDboMUIxOSeEu2BZOU5gUlG3YS3JtkQVaOLW5G8yZNKpehbrlTwB~SdVkEheqV4VgqwLGkOanZE1NbFA83TursF-DAwa~gmVE96XB-GnKHHrVQ1gcEw5LBeALGOhvSmZIFYBTHAduvOhQti87opA~i47vk4bmbfBQ4bbKLAVnuUrbui1cUCtOY8~kakXwJ2Mn2LpEVuk36FSWDu2bCGWDqQ-Kwu2nydMlmfvx2vHr-7TbQA57i1xqFXPBe92D8A-EdMB2Mw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
   },
   {
     id: 5,
-    name: "유리병-프랑스산(500ml)",
-    price: 20000,
+    name: "PET보틀-단지(480ml)",
+    price: 41000,
     imageUrl:
-      "https://s3-alpha-sig.figma.com/img/4c00/d3d5/bc0865e11d604709bb0369910ab0a961?Expires=1684713600&Signature=SGUp~eIZh2ekd61K1vxNPcr1khVeasz7mDhGb0O0vZ5tjz9nQUx9lfy3iGJgteyIO1VTFIKJbb6Ka3lUnPDK6d6tF6Exzf~AFxvCi73BBjMm45cT3rH4cN4YjPNbnhGELK2o1unlnD7BlCYPjkmJADEjnrpmK8p37UJ9kmdAZakpyk8f95~d~ERD~y-tD572F6f-rx93HZRA3znLibXV749usZPbCNMMs8WLdkyuKJi0ZonhpDST7tHUVnQniRYYFJCMU5gvkJz-v82bvAmKNEyS5DJlOSBtpFauf5Wy0jmWMVjanw7M35F52NHKKofser0C3QUEWX2dHkx7mjX7qA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      "https://s3-alpha-sig.figma.com/img/4c00/d3d5/bc0865e11d604709bb0369910ab0a961?Expires=1685923200&Signature=KC1By2yxxrNaJMFgu~y0ce8c5sLGU3CBM3p1KY7zk1O-hLVFzxzuZL8pAzjb8Ny3Bq946gpbZEiI-Zq9DJAnpb9m3UCffVh5Gn49PayXc1hkt-JYSwSCUL6UrjM9tQgZYYO~3tYHSAhT~NWSh1o1aMV0KPn1iewnd1KcNyHrygyVT0OeeoWIGzTJqcLR5GyAvy5av8UqvTW0RapsYRTM5xLLr~zfDJU0WQ~vOtG59LzcYMd575X8ZXOOiYWE3en1rBWnGfBmLs9wuEtCWc~yQuIA1jcwZ-~NUJxN4kTeJ3sE8Lch1-euK8eJFrluO2LSCTsS2TCmf57vK8OW5A0xUA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
   },
   {
     id: 6,
-    name: "유리병-프랑스산(750ml)",
-    price: 25000,
+    name: "PET보틀-납작(260ml)",
+    price: 61800,
     imageUrl:
-      "https://s3-alpha-sig.figma.com/img/33d6/e526/45207e8000c3bd7f731f561a4b2cf8f4?Expires=1684713600&Signature=YPMEjbMIB3Q0vYA3FVGHAp4xZPz~jsySqhChdrBiIibYgqP3z4kgMc~qXPRV4Sd1JEwJQKV0Ti6VevRAUDfGBZeLwkqEhKWYfui77SWRFFWCZieOLi~22uEKK8Dd~xRpRMSX~nEYyjvz0GF6spvpkaUBOubetjY6Bnclpqzv04-hCuJXmihd0o2DrzwW~SPVrXzab1C87U-4n885oEsgTYM8Pq0aDd5gELQtDomW6jBawC4deA1B8p3PQVkj-i0VnedtxyPbTYNCqnpo1Ii1JGR~CC4QCi4LaG-2tv5MlV8C5DTOusy5cULMtx5XgvpRhdcM2s0gu7VrrOf3v1GwSQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      "https://s3-alpha-sig.figma.com/img/33d6/e526/45207e8000c3bd7f731f561a4b2cf8f4?Expires=1685923200&Signature=Ifn7QjAZJFLdNcznPcCrMpKR4vKahQml1wj44c6zi281Nq2TeU7EejDde~aFS24-fKiCHSyp1749iN7vjtVNS21ipKaUj5F2AgM~gTCsrDd1ijgtH~Y29f4bKqK3RjWVVY9cSK9aT1g~d2kk8A9~UKLYaEjHhMnlRfdiLgZp8qRFd~83pV-~7J5GRDavHlsBNdGBcKlS4kXD~e4ZCgzw7Tjd8BUaKho8ND7nak3TFHZ60mLp5VA~6ekQYtgittqPS6JJuJ2zZxvVLe7RR0HIiVsoJyDezDxBJA5ZVT5NaARM0N~5eYHAzLeEZMpnh7zat87eNIogatQ0f3H9NSfzkQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
   },
   {
     id: 7,
-    name: "알루미늄병-순도100%(500ml)",
-    price: 15000,
+    name: "PET보틀-밀크티(370ml)",
+    price: 73400,
     imageUrl:
-      "https://s3-alpha-sig.figma.com/img/1389/e15f/2846ef65e20433b5f756e170bdb3b8d3?Expires=1684713600&Signature=G~3sHx~Xf4g5ND38fMnGUVwF4c024gHyzNMY~DPSU7CVPin-ZRY5eC1Z5x5BCwAZ0R9vplWrnG~jaDSJANqueEqiGjGkbo1qUcPWAjT3fH2xJsW1N2FuT4v83EU5SR6ygrhFf8~NyC0pp8Kapqj0VQXZIcOPhp1jxLlfBDSEW~nqIE5zMentTuqma7sr1vSiqfiN2bO2F31hJRgslQFX4eXsByZ0Any4hieu73tA8-ekloj~w8Y5z~3jecfFg6ekc4Dupu4OvP7PFeIM5OXj~lbOjPAdfk3gq0sdfvo~6rlN4pcqG9Nw-n8RjJ9MWpHxrvg0pnwhgXUkw0Z7tUJJwA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      "https://s3-alpha-sig.figma.com/img/33d6/e526/45207e8000c3bd7f731f561a4b2cf8f4?Expires=1685923200&Signature=Ifn7QjAZJFLdNcznPcCrMpKR4vKahQml1wj44c6zi281Nq2TeU7EejDde~aFS24-fKiCHSyp1749iN7vjtVNS21ipKaUj5F2AgM~gTCsrDd1ijgtH~Y29f4bKqK3RjWVVY9cSK9aT1g~d2kk8A9~UKLYaEjHhMnlRfdiLgZp8qRFd~83pV-~7J5GRDavHlsBNdGBcKlS4kXD~e4ZCgzw7Tjd8BUaKho8ND7nak3TFHZ60mLp5VA~6ekQYtgittqPS6JJuJ2zZxvVLe7RR0HIiVsoJyDezDxBJA5ZVT5NaARM0N~5eYHAzLeEZMpnh7zat87eNIogatQ0f3H9NSfzkQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
   },
   {
     id: 8,
-    name: "알루미늄병-순도100%(750ml)",
-    price: 18000,
+    name: "PET보틀-밀크티(370ml)",
+    price: 73400,
     imageUrl:
-      "https://s3-alpha-sig.figma.com/img/7327/60a4/509ecdc810638f6ee5664b30ee4db2c4?Expires=1684713600&Signature=FB1OZ-jbx8XN7Q-UrkuE28no9DmHhZyT8A4TG7lrP1ccwhSEMXTKikQhTWruro3rPPvh9hbNY7s3SCxuNcQNWEV4GEpo8PVIy~zAPBkzdOAUNbGeTFAMxa7WEZJN7vAw-Hg7biRAgDw02e2OVkcgOAqevcqaEzZOblsLzkr7DsI8Ex7KErP-EgjLEcGzGuX0O-fgzz3Kjgd1ojfj5d3Q4Yy5AVP-Cosh4zJq8XgipBCPzo5pKxHc5TsdOmMicvA3Z-x-TjW5DPOEkcJLSHu9Hx9ZZYVHWdFHV6ImlhAHT4gNSi8xTmQVoSJL5OxiXiygjb0IjAIxlIrhhS8w6ndhRQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      "https://s3-alpha-sig.figma.com/img/c7b8/6249/bb062378afa996a8fe0e2f25e099aa55?Expires=1685923200&Signature=f5ZQbo0P~FBsR673MtawS~dTjpWJq9ljBRWinQgIiMvnSWWu5w9--0n~pPQn2sXVbPf~GNJE1tQrjavgDboMUIxOSeEu2BZOU5gUlG3YS3JtkQVaOLW5G8yZNKpehbrlTwB~SdVkEheqV4VgqwLGkOanZE1NbFA83TursF-DAwa~gmVE96XB-GnKHHrVQ1gcEw5LBeALGOhvSmZIFYBTHAduvOhQti87opA~i47vk4bmbfBQ4bbKLAVnuUrbui1cUCtOY8~kakXwJ2Mn2LpEVuk36FSWDu2bCGWDqQ-Kwu2nydMlmfvx2vHr-7TbQA57i1xqFXPBe92D8A-EdMB2Mw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
   },
   {
     id: 9,
-    name: "스테인리스병-한국제작(500ml)",
-    price: 18000,
+    name: "PET보틀-원형(500ml)",
+    price: 42200,
     imageUrl:
-      "https://s3-alpha-sig.figma.com/img/05ef/e578/d81445480aff1872344a6b1b35323488?Expires=1684713600&Signature=gvkVAezZeNJ6-pOzrsQBqsdfN9PGcEmV7HraRhfgfmXTZD0rCy-OEhFh-uP4y7AxUs5FC6ILRG-RNHslTX8x8q9HHcLyVrFq6I1uzwKaZyyWOCwAYgglD99UQlYPlVCU3E-8eIUfX~TRBM~dSPsojsKv-cNJU1gGPOE7G9C3Z1CKoDjIFZ8G2J4TG16VvSzil0RHsGGPi~06jOwn3Dtq~oBZQ1yhoRgfYDrGLlsexYockI38hGjBcBx1gJWg41~kBGNhueXs-tmuLfqRoPc0ReK~jWKLiGZi2JVPaIP-tPOfSAUEt99Po0KzVG-agBflDRhhpybhrR1bmozDvb~9eg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      "https://s3-alpha-sig.figma.com/img/1389/e15f/2846ef65e20433b5f756e170bdb3b8d3?Expires=1685923200&Signature=FplCMZhCLLFLH5BiAWaZF0DYCjLI~d56XJE8VDYEkv4NTaEF~K5vs8Pqk4jY3BWB6m4LTivrtqUOquvBAAHZo4M8dDytdC~8RsWXuJgYE2isW13G1eff2p8FQ~twHzqWnkdSltQBRXVpTISNDrmy2VAF4Uf3tKkvjLGmeE2Y0mlNcgrg~LjmmtS5QahhxwrFvKZV52KxDaGAk~Qxgn~wx1gmtTepxGbIxug3xI6NvDxmE~JepZ0hK1wWPc-WOb-kwKSTu3MIB4iRxNySbjkus5x-Kvq7T7197LRyUceGTfuCC1vDWk8cElAh5YW1UNf84t9lSatV-dktdGKf3PoFXQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
   },
   {
     id: 10,
-    name: "스테인리스병-한국제작(750ml)",
-    price: 22000,
+    name: "PET보틀-납작(450ml)",
+    price: 39900,
     imageUrl:
-      "https://s3-alpha-sig.figma.com/img/c91b/8c58/919fc1af3ddd4c1bba09101cf51013cb?Expires=1684713600&Signature=gJXoDh3Kff5YuZh~dwngjm~n7o37JjnnftCnctPE5Y-QYKTWJAa6~laE1rxwy1s1FhgaJmU1IKg9OAHq0iwUob-hH9TEy3RUYgz-HyXqetKOgEw8Wp4937mBA~2cWyMJjp56bjQgLLbBqM8up2ddQQJLPsyfVdDHr-~jZFL-JEDT72NnlNfyO2qG~W6clycdMfrtkB~YxPPiwHSai1bTEKx6b7vaR79k7OtXO3F3kq9TzQH9vNXM6X9GQtRsw9zjc35JDS2CDoFsAh5u~xThB1udtnHfmTQMdm-lqB0NpwewqYB0GF8qov4mbSCPh20HtPIpLkM5-V1QPEXvyiUbiA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      "https://s3-alpha-sig.figma.com/img/c91b/8c58/919fc1af3ddd4c1bba09101cf51013cb?Expires=1685923200&Signature=IrwaaMlu1pEwov8-9svP0z~Q8tEmfdSQwMx~iPb1RLcgYrl4rY7pWa-jAhoo0Eh2Krt2q1A4dNUSc747Jff9HXDY1GehS1ugR3-fzTrev9vqLHhOLXPXG~7bpW-OIylTVftrIa7FEjkfITimiz1GRNUyHJwjqmirOXUIIY4MNlGof6CNUvX4bC1HkASJVyrJRVeBezxlEH6i83LNJIquQmLGanuXlIGw5CLpQfT1k~cgDHl7XRI01zgIfQBA2dJYNY6PQjUdD2aglJLV~JeviDLNeyP~IHg9mvzaI2dR16DBnw3gMbiCimzFpFCh2CV-ur1emRNJAOOQT4ukQl8oXA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
   },
   {
     id: 11,
-    name: "지퍼백-스몰사이즈(300ml)",
-    price: 5000,
+    name: "PET보틀-납작(450ml)",
+    price: 39900,
     imageUrl:
-      "https://s3-alpha-sig.figma.com/img/c7b8/6249/bb062378afa996a8fe0e2f25e099aa55?Expires=1684713600&Signature=TaTYvPZbnPGVzHZeahNvJJ328E5C2mjEgvfLw4spgQ8a~k9tHslk8qx44x8dr6t4-rJnQDOC6H6OJErrhhHIPodgNLapqFaBMQCYWeeoq9Jhka-A6jxgBeQa0l99W33-WCDutoV~d9UHbpBscd2-ZzOD2DTIKp6JuHdj4CityiwOnPgJ4jpx6lwEqU5593P7jkveAGDh7yEcqrmtmmVDLlgaU-~4YO2wmD3cHX4mmTMCN4wu4woBshXCkQXwy4iGUCCdb3nNg0hrSGXRlCfe2SNdFz8IzpOlRpgNLuUwSByJ6TPiajuQ7vS-mD2KqC402ewrZMm7tnRbI1h62e08bA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      "https://s3-alpha-sig.figma.com/img/c91b/8c58/919fc1af3ddd4c1bba09101cf51013cb?Expires=1685923200&Signature=IrwaaMlu1pEwov8-9svP0z~Q8tEmfdSQwMx~iPb1RLcgYrl4rY7pWa-jAhoo0Eh2Krt2q1A4dNUSc747Jff9HXDY1GehS1ugR3-fzTrev9vqLHhOLXPXG~7bpW-OIylTVftrIa7FEjkfITimiz1GRNUyHJwjqmirOXUIIY4MNlGof6CNUvX4bC1HkASJVyrJRVeBezxlEH6i83LNJIquQmLGanuXlIGw5CLpQfT1k~cgDHl7XRI01zgIfQBA2dJYNY6PQjUdD2aglJLV~JeviDLNeyP~IHg9mvzaI2dR16DBnw3gMbiCimzFpFCh2CV-ur1emRNJAOOQT4ukQl8oXA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
   },
   {
     id: 12,
-    name: "지퍼백-라지사이즈(1000ml)",
-    price: 10000,
+    name: "PET보틀-납작(450ml)",
+    price: 39900,
     imageUrl:
-      "https://s3-alpha-sig.figma.com/img/2410/7f6a/cd0400dbcf6d4dd8f586002d7afcd499?Expires=1684713600&Signature=N99STQ8Tf8-XXI-V9v3UJq4bpcsDWlpOn5ZNjCTHE8rc6SMR4Z5DxRR9zwspzQ-Q6F-h5JXwg0OVC-WwjSJMUcAJjWSLoBJ7lQC32UALwNBtHLjcWkNWgZ6DBWNClF~dfHCHEMFrcmj0Bdb3zFPjrtDlNSRdqksqVVZBiAeSmbhKWOkoHnKt23335HJyrHy6xoYJ6hygjpkZnwgAJFTBS8FHAJtwH26gALPGNqI-mDvdPGV2IklIv7VO-hp9kZ6gwqXuRJfhcPFke9g836-Aop9EzYSWxk03fDZezWBv8cx5A7R5NLGSm0fF4HSp~Q5sfL2t-nXxoQ7HR6yhHSh11Q__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      "https://s3-alpha-sig.figma.com/img/7327/60a4/509ecdc810638f6ee5664b30ee4db2c4?Expires=1685923200&Signature=o1a8RIussHyFQTVv2OeELfOsIFIFj9Rsg40pW9IJuSqbIExImMON4oABS2Ftmf13T~IcuxpOkbKrFllw0S83tBYwBmj9-VEN-K-WrNOCzZ4art-5~oisa-ph9L4orLjGm3Gt7E6wBsr2-vHcsdPq4qsfC5rOrUL79faUyS--ehV5XBAmzFBSe-nyumy8K8pfHzgl1v3gzlTQVvhLKN~qCrC1hZpvUHPraItWhPxZj83Rc8f~0PXb2hIUc8Xjk8LP3tcPI3vDOmQIN77W2lIM4QLT4zjBGT34Jnqich6K7ZAchWMlfzIUmQTxS5RM5Zk73n9lKySNFb4WHtK7wooZpA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
   },
 ];
