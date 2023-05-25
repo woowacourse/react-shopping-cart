674b866a42f68d49394db773b7d542e9445594bd
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-05-25 23:56:35 +0900

    refactor: checkbox style 수정

--- src/components/commons/Checkbox/Checkbox.styled.ts
+++ src/components/commons/Checkbox/Checkbox.styled.ts
@@ -5,12 +5,12 @@ import { CheckboxProps } from './Checkbox';
 export const StyledOuterCheckbox = styled.div<CheckboxProps>`
   position: relative;
 
-  width: ${props => (props.width ? `${Number(props.width)}px` : '28px')};
-  height: ${props => (props.height ? `${Number(props.height)}px` : '28px')};
+  width: ${props => (props.width ? `${props.width}px` : '28px')};
+  height: ${props => (props.height ? `${props.height}px` : '28px')};
 
   border: ${props => props.border ?? '1px solid #22A6A2'};
   border-radius: ${props =>
-    props.borderRadius ? `${Number(props.borderRadius)}px` : '4px'};
+    props.borderRadius ? `${props.borderRadius}px` : '4px'};
 
   background-color: ${props => props.backgroundColor ?? 'white'};
 
@@ -27,12 +27,12 @@ export const StyledInnerCheckbox = styled.div<CheckboxProps>`
   left: 50%;
   transform: translate(-50%, -50%);
 
-  width: ${props => (props.width ? `${Number(props.width) - 2}px` : `27px`)};
-  height: ${props => (props.height ? `${Number(props.height) - 2}px` : `27px`)};
+  width: ${props => (props.width ? `${props.width}px` : `27px`)};
+  height: ${props => (props.height ? `${props.height}px` : `27px`)};
 
   border: ${props => props.border ?? '1px solid #3288FF'};
   border-radius: ${props =>
-    props.borderRadius ? `${Number(props.borderRadius)}px` : '4px'};
+    props.borderRadius ? `${props.borderRadius}px` : '4px'};
 
   background-color: ${props => props.backgroundColor ?? '#333333'};
 
