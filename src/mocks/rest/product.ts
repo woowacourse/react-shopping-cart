0a51d44ce695ff1b821695efeef7cb5b82ff7894
2yunseong <dbsdltjd3701@naver.com>
2023-05-25 21:25:54 +0900

    refactor: 로직 재사용화

--- src/mocks/rest/product.ts
+++ src/mocks/rest/product.ts
@@ -1,32 +1,32 @@
 import { rest } from 'msw';
 import { products } from '../products';
 
+const handleNonExistData = (assertData: any, responseData: any) => {
+  if (!assertData) {
+    return { statusCode: 400, responseData: {} };
+  }
+  return { statusCode: 200, responseData };
+};
+
 export const getProduct = rest.get('/products/:productId', (req, res, ctx) => {
-  const product = products.find(
+  const productResponseData = products.find(
     (product) => product.id === +req.params.productId
   );
-  // TODO: 에러처리 스펙에 맞게 변경
-  const responseData = product ?? {
-    errorMsg: 'no exist data',
-  };
-  return res(ctx.status(200), ctx.json(responseData));
+
+  const { statusCode, responseData } = handleNonExistData(
+    productResponseData,
+    productResponseData
+  );
+
+  return res(ctx.status(statusCode), ctx.json(responseData));
 });
 
 export const putProduct = rest.put('/products/:productId', (req, res, ctx) => {
   let matchProduct = products.find(
     (product) => product.id === +req.params.productId
   );
-
-  if (!matchProduct) {
-    return res(
-      ctx.status(400),
-      ctx.json({
-        errorMsg: 'no exist data',
-      })
-    );
-  }
-  // TODO: 성공일 때 구체화 하기
-  return res(ctx.status(200));
+  const { statusCode, responseData } = handleNonExistData(matchProduct, {});
+  return res(ctx.status(statusCode), ctx.json(responseData));
 });
 
 export const getProducts = rest.get('/products', (_, res, ctx) => {
