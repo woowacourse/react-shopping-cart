af408ae038eca38c991aa199f129f5b49e830239
Patrick <kuyt3122@naver.com>
2023-05-26 13:27:46 +0900

    refactor: fetch 함수에 try-catch문 삽입

--- src/utils/fetchApi.ts
+++ src/utils/fetchApi.ts
@@ -1,9 +1,15 @@
-const fetchApi = async (url: string, option: RequestInit) => {
-  const response = await fetch(url, option);
-  if (!response.ok)
-    throw new Error(`api 요청을 실패했습니다! status: ${response.status}`);
+const fetchApi = async (url: string, option: any) => {
+  try {
+    const response = await fetch(url, option);
 
-  return response
+    if (!response.ok)
+      throw new Error(`api 요청을 실패했습니다! status: ${response.status}`);
+
+    return response;
+  } catch (error) {
+    if (error instanceof Error)
+      throw new Error(`api 요청을 실패했습니다! error: ${error.message}`);
+  }
 };
 
 export default fetchApi;
