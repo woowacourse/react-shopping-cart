dac4153de836da2e3056082bceca0e8096e0332e
regularPark <srk99279@naver.com>
2023-05-27 10:46:50 +0900

    style: 404 페이지 내부 디자인 변경 및 GlobalStyle 색상 추가

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,34 +1,48 @@
-import { styled } from 'styled-components';
+import styled from 'styled-components';
 
 const NotFound = () => {
   return (
-    <S.Wrapper>
-      <S.ImgWrapper
-        src="https://img.insight.co.kr/static/2020/11/10/700/img_20201110143156_pym2xdy6.webp"
-        alt="wrong-way"
-      />
-      <h1>존재하지 않는 페이지입니다.</h1>
-      <br />
-      <span>
-        <a href="https://regularpark.github.io/react-shopping-cart/">홈페이지</a>로 돌아가기
-      </span>
-    </S.Wrapper>
+    <Container>
+      <h2>해당 페이지를 찾지 못했습니다.</h2>
+      <Heading>404</Heading>
+      <p>주소가 잘못되었거나 더 이상 제공되지 않는 페이지입니다.</p>
+      <Link href="/react-shopping-shop/">홈으로 돌아가기</Link>
+    </Container>
   );
 };
 
-const S = {
-  Wrapper: styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    font-size: 32px;
-    height: 100vh;
-  `,
-
-  ImgWrapper: styled.img`
-    border-radius: 10px;
-  `,
-};
-
 export default NotFound;
+
+const Container = styled.div`
+  width: 100%;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  text-align: center;
+  color: var(--text-color);
+`;
+
+const Heading = styled.h1`
+  font-size: 160px;
+  margin-bottom: 6px;
+  font-weight: 900;
+  letter-spacing: 20px;
+  color: var(--mint-color);
+`;
+
+const Link = styled.a`
+  text-decoration: none;
+  background: var(--mint-color);
+  color: var(--white-color);
+  padding: 12px 24px;
+  display: inline-block;
+  border-radius: 25px;
+  font-size: 14px;
+  text-transform: uppercase;
+  transition: 0.4s;
+  margin-top: 18px;
+
+  &:hover {
+    background: var(--mint-color-hover);
+  }
+`;
