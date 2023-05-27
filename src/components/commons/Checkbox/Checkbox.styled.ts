7bcc70b6321cb9d4ac9657e0924abdb27831f5ad
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-05-27 20:55:53 +0900

    refactor: checkbox onCheck 핸들러 받게 수정

--- src/components/commons/Checkbox/Checkbox.styled.ts
+++ src/components/commons/Checkbox/Checkbox.styled.ts
@@ -2,11 +2,18 @@ import styled from 'styled-components';
 
 import { CheckboxProps } from './Checkbox';
 
-export const StyledOuterCheckbox = styled.div<CheckboxProps>`
+export const StyledCheckbox = styled.div<CheckboxProps>`
   position: relative;
 
   width: ${props => (props.width ? `${props.width}px` : '28px')};
   height: ${props => (props.height ? `${props.height}px` : '28px')};
+`;
+
+export const StyledOuterCheckbox = styled.div<CheckboxProps>`
+  position: absolute;
+
+  width: 100%;
+  height: 100%;
 
   border: ${props => props.border ?? '1px solid #22A6A2'};
   border-radius: ${props =>
@@ -27,8 +34,8 @@ export const StyledInnerCheckbox = styled.div<CheckboxProps>`
   left: 50%;
   transform: translate(-50%, -50%);
 
-  width: ${props => (props.width ? `${props.width}px` : `27px`)};
-  height: ${props => (props.height ? `${props.height}px` : `27px`)};
+  width: 100%;
+  height: 100%;
 
   border: ${props => props.border ?? '1px solid #3288FF'};
   border-radius: ${props =>
--- src/components/commons/Checkbox/Checkbox.tsx
+++ src/components/commons/Checkbox/Checkbox.tsx
@@ -2,29 +2,52 @@ import { InputHTMLAttributes, useState } from 'react';
 
 import { Check } from '@assets/index';
 
-import { StyledOuterCheckbox, StyledInnerCheckbox } from './Checkbox.styled';
+import {
+  StyledOuterCheckbox,
+  StyledInnerCheckbox,
+  StyledCheckbox,
+} from './Checkbox.styled';
 
 export interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
+  handleCheck?: () => void;
+  handleUnCheck?: () => void;
   border?: string | null;
   borderRadius?: string | number | null;
   backgroundColor?: string | null;
 }
 
 export const Checkbox = (props: CheckboxProps) => {
+  const { handleCheck, handleUnCheck } = props;
   const [isChecked, setIsChecked] = useState(false);
 
+  const handleOuterCheckboxClick = () => {
+    setIsChecked(true);
+
+    if (handleCheck) handleCheck();
+  };
+
+  const handleInnerCheckboxClick = () => {
+    setIsChecked(false);
+
+    if (handleUnCheck) handleUnCheck();
+  };
+
   return (
-    <StyledOuterCheckbox
-      {...props}
-      onClick={() => {
-        setIsChecked(!isChecked);
-      }}
-    >
-      {isChecked && (
-        <StyledInnerCheckbox {...props} aria-label="checked">
+    <StyledCheckbox>
+      {!isChecked ? (
+        <StyledOuterCheckbox
+          {...props}
+          onClick={handleOuterCheckboxClick}
+        ></StyledOuterCheckbox>
+      ) : (
+        <StyledInnerCheckbox
+          {...props}
+          aria-label="checked"
+          onClick={handleInnerCheckboxClick}
+        >
           <Check />
         </StyledInnerCheckbox>
       )}
-    </StyledOuterCheckbox>
+    </StyledCheckbox>
   );
 };
