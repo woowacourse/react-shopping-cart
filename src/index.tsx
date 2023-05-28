01e48caaa192511dd68c526bc3d04db3e3113536
hafnium1923 <hafnium1923@gmail.com>
2023-05-28 14:12:55 +0900

    refactor: 사용하지 않는 주석 제거

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, { lazy } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
