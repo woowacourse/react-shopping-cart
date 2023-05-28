5fff4657f2934102c3622419054aaf8018c2d26e
semnil5202 <semnil5202@gmail.com>
2023-05-28 23:51:57 +0900

    refactor: 에러메세지 상수화

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,10 @@
+import { ERROR_MESSAGE } from '../constants';
+
 export const getRequest = async <T>(path: string) => {
-  console.log(`/api/${path}`);
   const response = await fetch(`/api/${path}`);
 
   if (response.status >= 400) {
-    throw new Error('상품 정보를 가져오는데 실패했습니다.');
+    throw new Error(ERROR_MESSAGE.getInfo);
   }
 
   const data: T = await response.json();
@@ -21,7 +22,7 @@ export const postCartItem = async (productId: number) => {
   });
 
   if (response.status >= 400) {
-    throw new Error('장바구니에 물품을 담는데 실패했습니다.');
+    throw new Error(ERROR_MESSAGE.postToCart);
   }
 };
 
@@ -29,7 +30,7 @@ export const patchCartItemQuantity = async (
   productId: number,
   quantity: number
 ) => {
-  const response = await fetch(`/cart-items/${productId}`, {
+  const response = await fetch(`/cart-iㄴtems/${productId}`, {
     method: 'PATCH',
     headers: { 'content-Type': 'application/json' },
     body: JSON.stringify({
@@ -38,16 +39,16 @@ export const patchCartItemQuantity = async (
   });
 
   if (response.status >= 400) {
-    throw new Error('해당 상품의 수량을 수정할 수 없습니다.');
+    throw new Error(ERROR_MESSAGE.patchCartItem);
   }
 };
 
 export const deleteCartItem = async (productId: number) => {
-  const response = await fetch(`/cart-items/${productId}`, {
+  const response = await fetch(`/cart-iㄴtems/${productId}`, {
     method: 'DELETE',
   });
 
   if (response.status >= 400) {
-    throw new Error('장바구니에 없는 품목을 삭제할 수 없습니다.');
+    throw new Error(ERROR_MESSAGE.deleteCartItem);
   }
 };
---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+export const ERROR_MESSAGE = {
+  getInfo: '상품 정보를 가져오는데 실패했습니다.',
+  postToCart: '장바구니에 물품을 담는데 실패했습니다.',
+  patchCartItem: '해당 상품의 수량을 수정할 수 없습니다.',
+  deleteCartItem: '선택한 품목을 삭제할 수 없습니다.',
+};
