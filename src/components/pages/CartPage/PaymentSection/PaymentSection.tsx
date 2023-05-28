7cda8f3672b39cffee200a268354d084b37b7649
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-05-28 14:58:58 +0900

    feat: 결제 예상 금액 기능 구현

--- src/components/pages/CartPage/PaymentSection/PaymentSection.tsx
+++ src/components/pages/CartPage/PaymentSection/PaymentSection.tsx
@@ -1,3 +1,5 @@
+import { useGenerateTotalPrice } from './useGenerateTotalPrice';
+
 import {
   StyledPaymentInfoBox,
   StyledPaymentInfo,
@@ -9,6 +11,9 @@ import { Button } from '@components/commons/Button/Button';
 import * as Text from '@commons/Text/Text';
 
 const PaymentSection = () => {
+  const { totalCartPrice, totalDeliveryPrice, totalOrderPrice } =
+    useGenerateTotalPrice();
+
   return (
     <StyledPaymentSection>
       <StyledPaymentInfoBox>
@@ -17,16 +22,16 @@ const PaymentSection = () => {
           <StyledFlexBox>
             <div>
               <Text.Paragraph>총 상품가격</Text.Paragraph>
-              <Text.Paragraph>1000</Text.Paragraph>
+              <Text.Paragraph>{totalCartPrice}</Text.Paragraph>
             </div>
             <div>
               <Text.Paragraph>총 배송비</Text.Paragraph>
-              <Text.Paragraph>2000</Text.Paragraph>
+              <Text.Paragraph>{totalDeliveryPrice}</Text.Paragraph>
             </div>
           </StyledFlexBox>
           <StyledPaymentResult>
             <Text.Paragraph>총 주문금액</Text.Paragraph>
-            <Text.Paragraph>3000</Text.Paragraph>
+            <Text.Paragraph>{totalOrderPrice}</Text.Paragraph>
           </StyledPaymentResult>
           <Button width="100%" height="60px" backgroundColor="#333333">
             <Text.Description color="white">주문하기</Text.Description>
