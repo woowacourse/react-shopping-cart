70bab337163f990ff90e3bcd3f03b112a5589738
semnil5202 <semnil5202@gmail.com>
2023-05-28 23:52:25 +0900

    feat: 에러 발생시 동작 정지 및 alert 기능 추가

--- src/components/hooks/useCartState.ts
+++ src/components/hooks/useCartState.ts
@@ -8,6 +8,7 @@ import { useCallback } from 'react';
 import { parseToCartFormat } from '../../services/parseToCartFormat';
 import { checkboxesState } from '../../atoms/CheckboxState';
 import { deleteCartItem, patchCartItemQuantity, postCartItem } from '../../api';
+import { ERROR_MESSAGE } from '../../constants';
 
 export const useCartState = (id: number) => {
   const productItem = useRecoilValue(productItemStateFamily(id));
@@ -15,62 +16,78 @@ export const useCartState = (id: number) => {
   const setCartStates = useSetRecoilState(cartState);
   const setCheckboxes = useSetRecoilState(checkboxesState);
 
-  const handleAddCartState = useCallback(() => {
-    postCartItem(id);
+  const handleAddCartState = useCallback(async () => {
+    try {
+      await postCartItem(id);
 
-    setCartStates((prevCartStates) => [
-      ...prevCartStates,
-      parseToCartFormat(id, productItem),
-    ]);
+      setCartStates((prevCartStates) => [
+        ...prevCartStates,
+        parseToCartFormat(id, productItem),
+      ]);
+    } catch {
+      alert(ERROR_MESSAGE.postToCart);
+    }
   }, []);
 
-  const handleDeleteCartState = useCallback(() => {
-    deleteCartItem(id);
+  const handleDeleteCartState = useCallback(async () => {
+    try {
+      await deleteCartItem(id);
 
-    setCartStates((prevCartStates) =>
-      prevCartStates.filter((product) => product.id !== id)
-    );
-    setCheckboxes((prevState) =>
-      prevState.filter((checkbox) => checkbox.id !== id)
-    );
+      setCartStates((prevCartStates) =>
+        prevCartStates.filter((product) => product.id !== id)
+      );
+      setCheckboxes((prevState) =>
+        prevState.filter((checkbox) => checkbox.id !== id)
+      );
+    } catch {
+      alert(ERROR_MESSAGE.deleteCartItem);
+    }
   }, []);
 
-  const increaseProductCount = useCallback(() => {
-    patchCartItemQuantity(id, quantity + 1);
+  const increaseProductCount = useCallback(async () => {
+    try {
+      await patchCartItemQuantity(id, quantity + 1);
 
-    setCartStates((prevCartStates) =>
-      prevCartStates.map((product) =>
-        product.id === id
-          ? { ...product, quantity: product.quantity + 1 }
-          : product
-      )
-    );
-    setCheckboxes((prevState) =>
-      prevState.map((checkbox) =>
-        checkbox.id === id
-          ? { ...checkbox, quantity: checkbox.quantity + 1 }
-          : checkbox
-      )
-    );
+      setCartStates((prevCartStates) =>
+        prevCartStates.map((product) =>
+          product.id === id
+            ? { ...product, quantity: product.quantity + 1 }
+            : product
+        )
+      );
+      setCheckboxes((prevState) =>
+        prevState.map((checkbox) =>
+          checkbox.id === id
+            ? { ...checkbox, quantity: checkbox.quantity + 1 }
+            : checkbox
+        )
+      );
+    } catch {
+      alert(ERROR_MESSAGE.patchCartItem);
+    }
   }, [quantity]);
 
-  const decreaseProductCount = useCallback(() => {
-    patchCartItemQuantity(id, quantity - 1);
+  const decreaseProductCount = useCallback(async () => {
+    try {
+      await patchCartItemQuantity(id, quantity - 1);
 
-    setCartStates((prevCartStates) =>
-      prevCartStates.map((product) =>
-        product.id === id
-          ? { ...product, quantity: product.quantity - 1 }
-          : product
-      )
-    );
-    setCheckboxes((prevState) =>
-      prevState.map((checkbox) =>
-        checkbox.id === id
-          ? { ...checkbox, quantity: checkbox.quantity - 1 }
-          : checkbox
-      )
-    );
+      setCartStates((prevCartStates) =>
+        prevCartStates.map((product) =>
+          product.id === id
+            ? { ...product, quantity: product.quantity - 1 }
+            : product
+        )
+      );
+      setCheckboxes((prevState) =>
+        prevState.map((checkbox) =>
+          checkbox.id === id
+            ? { ...checkbox, quantity: checkbox.quantity - 1 }
+            : checkbox
+        )
+      );
+    } catch {
+      alert(ERROR_MESSAGE.patchCartItem);
+    }
   }, [quantity]);
 
   return {
