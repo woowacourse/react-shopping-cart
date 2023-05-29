772a77ffcb4d7a54aafb4441d2e2c708c67b1421
semnil5202 <semnil5202@gmail.com>
2023-05-29 14:37:58 +0900

    chore: storybook 배포 오류 설정

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,6 +3,30 @@ import type { Preview } from '@storybook/react';
 import { GlobalStyle } from '../src/styles/GlobalStyles';
 import { RecoilRoot } from 'recoil';
 import { MemoryRouter } from 'react-router';
+import { initialize, mswDecorator } from 'msw-storybook-addon';
+
+let options = {};
+if (location.hostname === 'semnil5202.github.io') {
+  options = {
+    serviceWorker: {
+      url: '/react-shopping-cart/mockServiceWorker.js',
+    },
+  };
+}
+
+initialize(options);
+
+export const decorators = [
+  mswDecorator,
+  (Story) => (
+    <MemoryRouter>
+      <GlobalStyle />
+      <RecoilRoot>
+        <Story />
+      </RecoilRoot>
+    </MemoryRouter>
+  ),
+];
 
 const preview: Preview = {
   parameters: {
@@ -14,16 +38,6 @@ const preview: Preview = {
       },
     },
   },
-  decorators: [
-    (Story) => (
-      <MemoryRouter>
-        <GlobalStyle />
-        <RecoilRoot>
-          <Story />
-        </RecoilRoot>
-      </MemoryRouter>
-    ),
-  ],
 };
 
 export default preview;
