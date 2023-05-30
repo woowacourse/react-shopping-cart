783001de1336f7e34bcb6ad40d2f56448396e62a
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-05-30 14:44:59 +0900

    refactor : 배열에 맞게 변수명 수정

--- src/api/cart.ts
+++ src/api/cart.ts
@@ -20,8 +20,8 @@ export async function postCartProduct(postData: CartType) {
   });
 }
 
-export async function deleteCartProduct(cartItemId: number[]) {
+export async function deleteCartProduct(cartItemIds: number[]) {
   return client.delete<Promise<Response>>("cart-items", {
-    body: JSON.stringify({ cartItemId }),
+    body: JSON.stringify({ cartItemIds }),
   });
 }
