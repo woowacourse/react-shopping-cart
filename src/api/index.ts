f460fa7d7dc6f9a22fe2ac01905a744b0468e744
00kang <kdb070970@gmail.com>
2024-05-14 16:44:00 +0900

    feat : 사용자의 장바구니 목록 조회 API 메서드에 파라미터 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,12 +6,18 @@ const USER_ID = import.meta.env.VITE_USER_ID;
 const USER_PASSWORD = import.meta.env.VITE_USER_PASSWORD;
 
 // GET : /cart-items 사용자의 장바구니 목록 조회
-export async function getCartItems(): Promise<CartItem[]> {
+export async function getCartItems(
+  page: number = 0,
+  size: number = 20
+): Promise<CartItem[]> {
   const token = generateBasicToken(USER_ID, USER_PASSWORD);
-  const response = await fetch(`${API_URL}/cart-items`, {
-    method: "GET",
-    headers: { Authorization: token },
-  });
+  const response = await fetch(
+    `${API_URL}/cart-items?page=${page}&size=${size}`,
+    {
+      method: "GET",
+      headers: { Authorization: token },
+    }
+  );
 
   if (!response.ok) {
     throw new Error("Failed to fetch cart items");
