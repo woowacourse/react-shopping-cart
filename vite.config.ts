c877efbc5962177dca2459ad59ace3276a8e53ef
rbgksqkr <rbgks1937@naver.com>
2024-05-14 16:02:02 +0900

    chore: emotion 설치 및 적용
    Co-authored-by: largopie <jae990320@kakao.com>

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,11 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+    }),
+  ],
   resolve: {
     alias: [
       { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
