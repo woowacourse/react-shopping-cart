62f9f5a8e7f571b3e1d69c788c6e5c2ee78951b8
skiende74 <skiende74@gmail.com>
2024-05-14 20:50:37 +0900

    feat(Button): 체크버튼, 취소 버튼등에 쓰이는 Button 구현
    Co-authored-by: chlwlstlf <jinsil378900@gmail.com>

---
+++ src/components/default/Button.tsx
@@ -0,0 +1,31 @@
+import React, { ReactNode } from "react";
+import { css } from "@emotion/css";
+
+type ButtonVariant = "primary" | "secondary" | "text";
+
+interface ButtonProps extends React.ComponentPropsWithRef<"button"> {
+  children?: ReactNode;
+  variant?: ButtonVariant;
+}
+
+export const Button = ({ children, variant = "text", ...rest }: ButtonProps) => {
+  return (
+    <button className={getButtonClassName(variant)} {...rest}>
+      {children}
+    </button>
+  );
+};
+
+const getButtonClassName = (variant: ButtonVariant) => {
+  return css`
+    width: ${variant === "text" ? "fit-content" : "24px"};
+    height: 24px;
+    border-radius: ${variant === "text" ? "4px" : "8px"};
+    border: 1px solid #0000001a;
+    outline: none;
+    background-color: ${variant === "primary" ? "#000000" : "#ffffff"};
+    color: ${variant === "primary" ? "#ffffff" : "#212529"};
+    cursor: pointer;
+    padding: ${variant === "text" ? "0 8px" : "0"};
+  `;
+};
