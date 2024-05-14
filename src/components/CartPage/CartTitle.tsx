c30ae8f8e70d87bf9218b375ce621774e538dd5e
skiende74 <skiende74@gmail.com>
2024-05-14 20:50:14 +0900

    feat(CartTitle): CartTitle 기능 초기구현
    Co-authored-by: chlwlstlf <jinsil378900@gmail.com>

---
+++ src/components/CartPage/CartTitle.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { css } from "@emotion/css";
+
+const CartTitle = () => {
+  return (
+    <div className={cartTitleCSS}>
+      <div className={cartTitleTextCSS}>장바구니</div>
+      <div className={cartTitleSubTextCSS}>
+        <p>현재 2종류의 상품이 담겨있습니다.</p>
+      </div>
+    </div>
+  );
+};
+
+export default CartTitle;
+
+const cartTitleCSS = css`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+
+const cartTitleTextCSS = css`
+  font-family: Noto Sans KR;
+  font-size: 24px;
+  font-weight: 700;
+  line-height: 34.75px;
+  text-align: left;
+`;
+
+const cartTitleSubTextCSS = css`
+  font-family: Noto Sans;
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 15px;
+  text-align: left;
+`;
