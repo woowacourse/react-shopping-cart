e2e2bb0adcbf4ba6120f6ff2ddee23d7c3415713
00kang <kdb070970@gmail.com>
2024-05-14 16:38:44 +0900

    refactor : API 호출 메서드명 METHOD로 시작하도록 수정

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,7 +6,7 @@ const USER_ID = import.meta.env.VITE_USER_ID;
 const USER_PASSWORD = import.meta.env.VITE_USER_PASSWORD;
 
 // GET : /cart-items 사용자의 장바구니 목록 조회
-export async function fetchCartItems(): Promise<CartItem[]> {
+export async function getCartItems(): Promise<CartItem[]> {
   const token = generateBasicToken(USER_ID, USER_PASSWORD);
   const response = await fetch(`${API_URL}/cart-items`, {
     method: "GET",
@@ -22,7 +22,7 @@ export async function fetchCartItems(): Promise<CartItem[]> {
 }
 
 // GET : /cart-items/counts 장바구니 아이템 수량 조회
-export async function fetchCartItemCounts(): Promise<CartItemCounts> {
+export async function getCartItemCounts(): Promise<CartItemCounts> {
   const token = generateBasicToken(USER_ID, USER_PASSWORD);
   const response = await fetch(`${API_URL}/cart-items/counts`, {
     method: "GET",
@@ -40,7 +40,7 @@ export async function fetchCartItemCounts(): Promise<CartItemCounts> {
 }
 
 // PATCH : /cart-items/{id} 장바구니 아이템 수량 변경
-export async function fetchCartItemQuantityChange(
+export async function patchCartItemQuantityChange(
   cartItemId: number,
   quantity: number
 ): Promise<void> {
