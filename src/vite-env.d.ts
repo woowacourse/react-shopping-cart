9ba323cf158de521a77d3009d0681e6f7397041c
김진호 <rlawlsgh1227@gmail.com>
2024-05-14 14:52:02 +0900

    chore: env 설정
    Co-authored-by:ImxYJL<allensain14@gmail.com>

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_BASE_URL: string;
+  readonly VITE_USER_ID: string;
+  readonly VITE_PASSWORD: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
