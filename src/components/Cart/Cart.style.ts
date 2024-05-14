1c8e8ab596ca80bff9a569bde0852150195feca9
Soyeon Choe <soy2302ten@gmail.com>
2024-05-14 21:31:23 +0900

    design: 장바구니가 비워져있을 시 띄워지는 문구 스타일 EmptyCart 추가
    Co-Authored-By: soosoo22 <80167893+soosoo22@users.noreply.github.com>

--- src/components/Cart/Cart.style.ts
+++ src/components/Cart/Cart.style.ts
@@ -2,7 +2,22 @@ import styled from 'styled-components';
 
 export const CartStyle = styled.section`
   padding: 36px 24px;
-  height: 100vh;
+  height: 100%;
   position: relative;
   top: 64px;
+  min-height: 90vh;
+  position: relative;
+`;
+
+export const EmptyCart = styled.p`
+  color: #0a0d13;
+  font-size: 16px;
+  line-height: 16px;
+  text-align: center;
+  font-weight: 400;
+  margin: 0 auto;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 `;
