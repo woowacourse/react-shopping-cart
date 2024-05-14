4c387f83ababf8390b5a7f448ebb13eed81b19a4
Hyelim Choi <HyelimChoi01@gmail.com>
2024-05-14 21:12:46 +0900

    feat: cart page 작업
    Co-Authored-By: 헤인 <157036488+hain-tain@users.noreply.github.com>

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,12 +1,44 @@
+import CartList from '@/components/Cart/CartList';
+import CartRecipe from '@/components/Cart/CartRecipe';
+import CartTitle from '@/components/Cart/CartTitle';
 import FullWidthButton from '@/components/Button/FullWidthButton';
 import Header from '@/components/Header.tsx';
+import styled from '@emotion/styled';
 
 const Cart = () => {
   return (
     <>
-      <Header />
-      <FullWidthButton onClick={() => {}}>주문 확인</FullWidthButton>
+      <StyledFixedTop>
+        <Header />
+      </StyledFixedTop>
+      <StyledScrollBox>
+        <CartTitle />
+        <CartList />
+      </StyledScrollBox>
+      <StyledFixedBottom>
+        <CartRecipe />
+        <FullWidthButton onClick={() => {}}>주문 확인</FullWidthButton>
+      </StyledFixedBottom>
     </>
   );
 };
 export default Cart;
+
+const StyledFixedTop = styled.div`
+  width: 430px;
+  position: fixed;
+  top: 0;
+`;
+
+const StyledScrollBox = styled.div`
+  margin-top: 64px;
+  overflow-y: scroll;
+  height: calc(100vh - 230px);
+  width: 430px;
+`;
+
+const StyledFixedBottom = styled.div`
+  width: 430px;
+  position: fixed;
+  bottom: 0;
+`;
