dd6de7d888b72b8587ed36916e97fbfa13c97d04
vi-wolhwa <cerezo00@naver.com>
2024-05-14 15:55:59 +0900

    chore: 코드포맷팅 설정
    Co-authored-by: pp449 <mma7710@naver.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,40 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended"],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+
+    "import/extensions": ["error", { js: "ignorePackages" }],
+    "import/order": [
+      "error",
+      {
+        groups: ["builtin", "external", "internal", ["parent", "sibling"], "index", "object", "type", "unknown"],
+        pathGroups: [
+          {
+            pattern: "next",
+            group: "builtin",
+            position: "before",
+          },
+          {
+            pattern: "@/core/**",
+            group: "unknown",
+          },
+          {
+            pattern: "**/*.css.ts",
+            group: "unknown",
+            position: "after",
+          },
+        ],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
     ],
   },
-}
+};
