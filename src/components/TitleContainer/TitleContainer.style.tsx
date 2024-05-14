b18a08e85f28daaf76009fec7f71b0126ea3043d
greetings1012 <ahnqwerty@gmail.com>
2024-05-14 20:34:40 +0900

    feat(TitleContainer): TitleContainer 구현
    Co-authored-by: YUNAH <anttiey@users.noreply.github.com>

---
+++ src/components/TitleContainer/TitleContainer.style.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 12px;
+`;
+
+export const Title = styled.p`
+  font-size: 24px;
+  font-weight: 700;
+`;
+
+export const SubTitle = styled.p`
+  font-size: 12px;
+`;
