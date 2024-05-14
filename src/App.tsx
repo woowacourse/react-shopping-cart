3733b314343e7c34f9148607c7d99fa878d85e2b
skiende74 <skiende74@gmail.com>
2024-05-14 17:28:49 +0900

    feat(App): Router 추가
    Co-authored-by: chlwlstlf <jinsil378900@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,15 @@
-import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CartPage from "./pages/CartPage";
 import "./App.css";
-import { fetchCartItems } from "./api/CartItem";
-import { Product } from "./types";
 
 function App() {
-  const [cardItems, setCartItmes] = useState<Product[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<Error | null>(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      setError(null);
-
-      try {
-        const data = await fetchCartItems();
-        setCartItmes(data);
-      } catch (error) {
-        setError(error as Error);
-      }
-
-      setLoading(false);
-    };
-
-    fetchData();
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-
   return (
     <>
-      <h1>react-shopping-cart</h1>
-      <div>
-        {cardItems.map((item, index) => (
-          <div key={index}>
-            {item.id} {item.quantity}
-          </div>
-        ))}
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<CartPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
