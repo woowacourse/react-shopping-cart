f3966c65aaeb28f03bac2a03cb6f958e2065ec10
badahertz52 <badahertz52@gmail.com>
2024-05-14 21:28:06 +0900

    feat: 라우터 생성 및 라우터 관련 상수 처리
    Co-Authored-By: jinyoung <87177577+jinyoung234@users.noreply.github.com>

---
+++ src/routes/route.constant.ts
@@ -0,0 +1,4 @@
+export const ROUTE_PATHS = {
+  root: '/',
+  confirm: '/confirm',
+} as const;
--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,13 +1,19 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import { OrderPage } from '@pages/index';
+import AppLayout from '@components/layout/AppLayout/AppLayout';
 
 const router = createBrowserRouter([
   {
     path: '/',
+    element: <AppLayout />,
     children: [
       {
         path: '/',
+        element: <OrderPage />,
+      },
+      {
+        path: '/confirm',
         element: <OrderPage />,
       },
     ],
