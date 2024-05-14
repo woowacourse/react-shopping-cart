a2af046d2b7393704835a78fafea1934786affa4
badahertz52 <badahertz52@gmail.com>
2024-05-14 21:26:03 +0900

    docs :svg 파일 추가
    Co-Authored-By: jinyoung <87177577+jinyoung234@users.noreply.github.com>

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+import BackArrow from './backArrow.svg?react';
+import CheckIcon from './check.svg?react';
+import UpsideDownExclamation from './upsideDownExclamation.svg?react';
+import Plus from './plus.svg?react';
+import Minus from './minus.svg?react';
+
+export { BackArrow, CheckIcon, Minus, Plus, UpsideDownExclamation };
