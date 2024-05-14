2c0e6baba62937515a02a7a97084d61dd68a9e0c
greetings1012 <ahnqwerty@gmail.com>
2024-05-14 17:07:56 +0900

    chore: 필요한 이미지 에셋 import 및 export
    Co-authored-by: YUNAH <anttiey@users.noreply.github.com>

---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+export { default as InfoIcon } from './images/InfoIcon.svg';
+export { default as CheckedIcon } from './images/CheckIcon/CheckedIcon.svg';
+export { default as UncheckedIcon } from './images/CheckIcon/UncheckedIcon.svg';
+export { default as PlusIcon } from './images/PlusIcon.svg';
+export { default as MinusIcon } from './images/MinusIcon.svg';
