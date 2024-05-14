d5d397408a2e1830ebedec58f1e236ab99950672
GimGayeon <kky1373@naver.com>
2024-05-14 21:16:49 +0900

    design(ShoppingCart): 장바구니 컨테이너 스타일 지정
    Co-authored-by: simorimi <simorimi@users.noreply.github.com>

--- src/components/pages/ShoppingCart/ShoppingCart.tsx
+++ src/components/pages/ShoppingCart/ShoppingCart.tsx
@@ -1,5 +1,8 @@
 import Header from '../../Header/Header';
+import ItemList from '../../ItemList/ItemList';
+import OrderButton from '../../OrderButton/OrderButton';
 import Title from '../../Title/Title';
+import TotalPaymentInfo from '../../TotalPaymentInfo/TotalPaymentInfo';
 
 import * as Styled from './style';
 
@@ -7,13 +10,20 @@ const ShoppingCart = () => {
   return (
     <Styled.ShoppingCart>
       <Header title="SHOP" />
-      <Title
-        title="장바구니"
-        caption={`현재 ${'2'}종류의 상품이 담겨있습니다.`}
-      ></Title>
-      {/* <ItemList></ItemList>
-      <ItemTotal></ItemTotal> */}
-      Order
+
+      <Styled.Container>
+        <Title
+          title="장바구니"
+          caption={`현재 ${'2'}종류의 상품이 담겨있습니다.`}
+        ></Title>
+        <ItemList></ItemList>
+        <TotalPaymentInfo></TotalPaymentInfo>
+      </Styled.Container>
+      <OrderButton
+        onClick={() => console.log('s')}
+        label="확인"
+        isOrderable={false}
+      />
     </Styled.ShoppingCart>
   );
 };
--- src/components/pages/ShoppingCart/style.tsx
+++ src/components/pages/ShoppingCart/style.tsx
@@ -3,6 +3,15 @@ import styled from 'styled-components';
 export const ShoppingCart = styled.section`
   width: 26.875rem;
   height: 58.5rem;
+  position: relative;
 
   background-color: ${(props) => props.theme.color.white};
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2.8125rem;
+
+  padding: 2.25rem 1.5rem;
 `;
