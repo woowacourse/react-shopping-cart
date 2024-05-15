6198211dc9df096ba157d7a80c2a05ff289ed3de
river <jenn0.6n@gmail.com>
2024-05-15 17:09:50 +0900

    feat: Recoil Suspense 컴포넌트 추가
    Co-Authored-By: KIM DAEUN <midekuna@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import './App.css';
-import CartList from './components/CartList';
+import { useRecoilValueLoadable } from "recoil";
+import "./App.css";
+import CartList from "./components/CartList";
+import RecoilSuspense from "./components/common/RecoilSuspense";
+import { cartListState } from "./recoil/selectors";
 
 function App() {
-  return <CartList />;
+  const cartList = useRecoilValueLoadable(cartListState);
+
+  return (
+    <>
+      <RecoilSuspense loadable={cartList} fallback={<div>안쪽 로딩 중...</div>}>
+        <CartList items={cartList.contents} />
+        {/* <CheckoutSummary /> */}
+      </RecoilSuspense>
+    </>
+  );
 }
 
 export default App;
