ccd9f0063a4cf040182bb703c0a39f4f7f01d208
greetings1012 <ahnqwerty@gmail.com>
2024-05-15 12:50:14 +0900

    feat: 뒤로가기 아이콘 선언
    Co-authored-by: YUNAH <anttiey@users.noreply.github.com>

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,3 +4,4 @@ export { default as UncheckedIcon } from './images/CheckIcon/UncheckedIcon.svg';
 export { default as PlusIcon } from './images/PlusIcon.svg';
 export { default as MinusIcon } from './images/MinusIcon.svg';
 export { default as MainLogo } from './images/MainLogo.svg';
+export { default as LeftArrow } from './images/LeftArrow.svg';
