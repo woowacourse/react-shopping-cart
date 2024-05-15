8a4824e05935af7c92d82e2fffc23bdad87466ae
이태훈 <rhymint@gmail.com>
2024-05-16 03:23:17 +0900

    fix: 전체 결제 금액 계산 로직 오류 수정
    Co-authored-by: pakxe <pigkill40@naver.com>

--- src/recoil/price/priceSelector.ts
+++ src/recoil/price/priceSelector.ts
@@ -20,7 +20,7 @@ export const priceSelector = selector({
     }, 0);
 
     const deliveryFee = orderedPrice <= 0 || orderedPrice >= DELIVERY_FEE_THRESHOLD ? 0 : DELIVERY_FEE;
-    const totalPrice = orderedPrice - deliveryFee;
+    const totalPrice = orderedPrice + deliveryFee;
 
     return { orderedPrice, deliveryFee, totalPrice };
   },
