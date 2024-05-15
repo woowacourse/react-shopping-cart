cb467e3434707753fb54eaab721f7224a0b9a4cf
bmj0053 <bmj05378@gmail.com>
2024-05-15 10:51:27 +0900

    chore: 라우팅 적용
    Co-authored-by: jaeml06 <jaeml06@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Cart from './pages/Cart.tsx';
+import Completed from './pages/Completed.tsx';
 import App from './App.tsx';
 import './index.css';
-import { RecoilRoot } from 'recoil';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/cart',
+    element: <Cart />,
+  },
+  {
+    path: '/completed',
+    element: <Completed />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RecoilRoot>
-      <App />
+      <RouterProvider router={router} />
     </RecoilRoot>
   </React.StrictMode>,
 );
