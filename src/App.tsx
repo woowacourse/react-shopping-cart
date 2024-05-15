f6cbe8067447889780e26447bf3da49775203c8a
Seongjin Hong <hongseongjin.to@gmail.com>
2024-05-15 10:46:51 +0900

    feat: App.tsx에 현재까지 구현된 컴포넌트 시각화
    Co-authored-by: ss0526100 <ss0526100@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,33 @@
 import './App.css';
 import './reset.css';
 
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import CartItemContainer from './components/CartItemContainer/CartItemContainer';
+import FooterButton from './components/FooterButton/FooterButton';
+import Header from './components/Header/Header';
+import OrderAmount from './components/OrderAmount/OrderAmount';
+import TitleContainer from './components/TitleContainer/TitleContainer';
+import styled from '@emotion/styled';
 
-import ConfirmOrderPage from './page/ConfirmOrderPage/ConfirmOrderPage';
-import ENDPOINTS from './constants/endpoints';
-import ShoppingCartPage from './page/ShoppingCartPage';
-import { Suspense } from 'react';
+const Content = styled.section({
+  padding: '36px 24px',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '36px',
+});
 
-const router = createBrowserRouter([
-  {
-    path: ENDPOINTS.shoppingCart,
-    element: <ShoppingCartPage />,
-  },
-  {
-    path: ENDPOINTS.confirmOrder,
-    element: (
-      <Suspense>
-        <ConfirmOrderPage />
-      </Suspense>
-    ),
-  },
-]);
+function App() {
+  return (
+    <>
+      <Header />
+      <Content>
+        <TitleContainer title="장바구니" description="현재 2종류의 상품이 담겨있습니다." />
 
-export default function App() {
-  return <RouterProvider router={router} />;
+        <CartItemContainer />
+        <OrderAmount />
+      </Content>
+      <FooterButton buttonText="주문 확인" />
+    </>
+  );
 }
+
+export default App;
