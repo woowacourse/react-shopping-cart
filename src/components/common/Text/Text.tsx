088e9407bc2fbe298e624b538eef5ecd690e8d5a
이태훈 <rhymint@gmail.com>
2024-05-16 03:47:35 +0900

    fix: Text style 파일 분리
    Co-authored-by: pakxe <pigkill40@naver.com>

--- src/components/common/Text/Text.tsx
+++ src/components/common/Text/Text.tsx
@@ -1,45 +1,18 @@
 import { ReactNode } from 'react';
-import styled from 'styled-components';
+import * as S from './Text.style'
 
-type TextProps = {
+export type TextProps = {
   size?: 's' | 'm' | 'l' | number;
-  weight?: 's' | 'm' | 'l' | number;
+  weight?: 's' | 'm' | 'l';
   children: ReactNode;
 };
 
-const StyledText = styled.p<TextProps>`
-  font-size: ${({ size }) => {
-    switch (size) {
-      case 's':
-        return '12';
-      case 'm':
-        return '16';
-      case 'l':
-        return '24';
-      default:
-        return size;
-    }
-  }}px;
-
-  font-weight: ${({ weight }) => {
-    switch (weight) {
-      case 's':
-        return '300';
-      case 'm':
-        return '500';
-      case 'l':
-        return '700';
-      default:
-        return weight;
-    }
-  }};
-`;
 
 const Text = ({ children, size = 'm', weight = 'l' }: TextProps) => {
   return (
-    <StyledText size={size} weight={weight}>
+    <S.Text size={size} weight={weight}>
       {children}
-    </StyledText>
+    </S.Text>
   );
 };
 
