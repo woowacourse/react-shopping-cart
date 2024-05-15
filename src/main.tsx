8f64e174d74524c5cf7045bedcb67cac44df8f6b
pakxe <pigkill40@naver.com>
2024-05-15 18:07:29 +0900

    feat: RecoilRoot 추가
    Co-authored-by: Todari <rhymint@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import GlobalStyles from './GlobalStyles.tsx';
+import { RecoilRoot } from 'recoil';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <RecoilRoot>
+      <GlobalStyles />
+      <App />
+    </RecoilRoot>
   </React.StrictMode>,
 );
