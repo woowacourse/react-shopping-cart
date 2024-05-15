a7598bc8ee74bcfadfce080eca6b59dca50636c7
이태훈 <rhymint@gmail.com>
2024-05-16 03:47:44 +0900

    storybook: Text storybook 작성
    Co-authored-by: pakxe <pigkill40@naver.com>

---
+++ src/components/common/Text/Text.stories.tsx
@@ -0,0 +1,48 @@
+import { type Meta, type StoryObj } from '@storybook/react';
+import Text from './Text';
+
+const meta = {
+  title: 'Components/Text',
+  component: Text,
+  tags: ['autodocs'],
+  argTypes: {
+    size: {
+      description: '',
+      control: { type: 'radio' },
+      options: ['s', 'm', 'l', 'custom'],
+    },
+    customSize: {
+      if: { arg: 'size', eq: 'custom' },
+      description: '',
+      control: { type: 'number' },
+    },
+    weight: {
+      description: '',
+      control: { type: 'radio' },
+      options: ['s', 'm', 'l', 'custom'],
+    },
+    children: {
+      description: '',
+      control: { type: 'text' },
+    }
+  },
+  args: {
+    size: 'm',
+    customSize: 0,
+    weight: 'm',
+    children: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+  }
+} satisfies Meta<typeof Text>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Playground: Story = {
+  render: ({ ...args }) => {
+    if (args.customSize) {
+      args.size = args.customSize;
+    }
+    return <Text {...args}>{args.children}</Text>;
+  },
+};
