d7fdeb5c7509ced18fa6138eebf4f48578769bbe
pakxe <pigkill40@naver.com>
2024-05-16 02:36:09 +0900

    feat: 헤더의 뒤로가기 버튼 로직 추가
    Co-authored-by: Todari <rhymint@gmail.com>

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import * as S from './Header.style';
 
 import BACK from '../../assets/chevron-back.svg';
+import { useNavigate } from 'react-router-dom';
 
 type HeaderType = 'logo' | 'back';
 
@@ -9,9 +10,13 @@ interface HeaderProps {
 }
 
 const Header = ({ type = 'logo' }: HeaderProps) => {
+  const navigate = useNavigate();
+
   return (
     <S.Header>
-      <S.Prefix>{type === 'logo' ? <S.Logo>SHOP</S.Logo> : <S.SvgContainer src={BACK} />}</S.Prefix>
+      <S.Prefix>
+        {type === 'logo' ? <S.Logo>SHOP</S.Logo> : <S.SvgContainer src={BACK} onClick={() => navigate(-1)} />}
+      </S.Prefix>
       <S.Suffix></S.Suffix>
     </S.Header>
   );
