73d2f13823968408778920a15f5e66699ce48879
lurgi <wahoo9040@naver.com>
2024-05-15 11:07:48 +0900

    feat: CartTitle UI 작성
    Co-authored-by: Fe <chysis@users.noreply.github.com>

--- src/components/Main/Cart/CartTitle/CartTitle.tsx
+++ src/components/Main/Cart/CartTitle/CartTitle.tsx
@@ -1,8 +1,13 @@
+/** @jsxImportSource @emotion/react */
+import { CartItemTypesCountStyle, CartTitleStyle } from "./CartTitle.style";
+
 const CartTitle = () => {
+  const count = 3;
+
   return (
     <div>
-      <h1>장바구니</h1>
-      <div>현재 오만가지 종류가 있슴</div>
+      <h1 css={CartTitleStyle}>장바구니</h1>
+      <div css={CartItemTypesCountStyle}>현재 {count}종류의 상품이 담겨 있습니다.</div>
     </div>
   );
 };
