c5b3201b16e5ef22ea7a2962a6ec3126ca1f6517
GimGayeon <kky1373@naver.com>
2024-05-15 17:19:30 +0900

    feat(shoppingCart): remove, adjustQuantity 핸들러 구현
    Co-authored-by: simorimi <simorimi@users.noreply.github.com>

--- src/components/pages/ShoppingCart/ShoppingCart.tsx
+++ src/components/pages/ShoppingCart/ShoppingCart.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 
 import * as Styled from './style';
 
-import { fetchCartItem } from '../../../api';
 import Header from '../../Header/Header';
 import ItemList from '../../ItemList/ItemList';
 import OrderButton from '../../OrderButton/OrderButton';
@@ -10,16 +9,45 @@ import Title from '../../Title/Title';
 import TotalPaymentInfo from '../../TotalPaymentInfo/TotalPaymentInfo';
 
 import { CartItem } from '../../../type';
+import {
+  adjustCartItemQuantity,
+  fetchCartItems,
+  removeCartItem,
+} from '../../../api/shoppingCart';
 
 const ShoppingCart = () => {
   const [item, setItem] = useState<CartItem[]>();
+
   useEffect(() => {
     const fetchItems = async () => {
-      const items = await fetchCartItem();
+      const items = await fetchCartItems();
       setItem(items);
     };
     fetchItems();
   }, []);
+
+  const handleRemoveCartItem = async (cartItemId: number) => {
+    try {
+      await removeCartItem(cartItemId);
+      const updatedCartItems = await fetchCartItems();
+      setItem(updatedCartItems);
+    } catch (error) {
+      console.error('Failed to remove cart item:', error);
+    }
+  };
+
+  const handleAdjustCartItemQuantity = async (
+    cartItemId: number,
+    quantity: number,
+  ) => {
+    try {
+      await adjustCartItemQuantity(cartItemId, quantity);
+      const updatedCartItems = await fetchCartItems();
+      setItem(updatedCartItems);
+    } catch (error) {
+      console.error('수량변경 실패', error);
+    }
+  };
 
   const isOrderable = !!item?.length;
 
@@ -33,7 +61,11 @@ const ShoppingCart = () => {
             title="장바구니"
             caption={`현재 ${item.length}종류의 상품이 담겨있습니다.`}
           />
-          <ItemList cartItems={item} />
+          <ItemList
+            cartItems={item}
+            onRemoveItem={handleRemoveCartItem}
+            onAdjustItemQuantity={handleAdjustCartItemQuantity}
+          />
           <TotalPaymentInfo></TotalPaymentInfo>
         </Styled.Container>
       )}
