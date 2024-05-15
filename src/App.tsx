d3134ffe5b134acbcc64ef3f81348e0d64ff21e0
김진호 <rlawlsgh1227@gmail.com>
2024-05-15 11:44:53 +0900

    design: App style 설정
    Co-authored-by: ImxYJL <allensain14@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import Checkbox from './components/Checkbox';
+import getItems from './api/get/getItems';
+import useFetch from './hooks/useFetch';
+import { AppStyle } from './App.styled';
+import Header from './components/Header/Header';
+import ShoppingCartOverview from './components/ShoppingCartOverview/ShoppingCartOverview';
+import FloatingButton from './components/FloatingButton/FloatingButton';
 
 function App() {
+  const { data, isLoading, errorMessage } = useFetch(getItems);
+  console.log(data, isLoading, errorMessage);
+
   return (
-    <>
-      <h1>react-shopping-cart</h1>
-      <Checkbox isChecked={true} id={1}></Checkbox>
-    </>
+    <AppStyle>
+      <Header></Header>
+      <ShoppingCartOverview></ShoppingCartOverview>
+      <FloatingButton label={'주문 확인'}></FloatingButton>
+    </AppStyle>
   );
 }
 
