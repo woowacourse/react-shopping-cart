ebc08492c3d89e887d5f2fcb21b602212eb68458
lurgi <wahoo9040@naver.com>
2024-05-15 11:09:24 +0900

    feat: CartItemContainer UI 작성
    Co-authored-by: Fe <chysis@users.noreply.github.com>

---
+++ src/components/Main/Cart/CartItemContainer/CartItemContainer.style.tsx
@@ -0,0 +1,9 @@
+import { css } from "@emotion/react";
+
+export const CartItemContainerStyle = css({
+  display: "flex",
+  flexDirection: "column",
+
+  padding: "12px",
+  gap: "20px",
+});
--- src/components/Main/Cart/CartItemContainer/CartItemContainer.tsx
+++ src/components/Main/Cart/CartItemContainer/CartItemContainer.tsx
@@ -1,10 +1,14 @@
+/** @jsxImportSource @emotion/react */
+import Divider from "../../../Divider/Divider";
+import { CartItemContainerStyle } from "./CartItemContainer.style";
 import CartItemControls from "./CartItemControls/CartItemControls";
 import CartItemList from "./CartItemList/CartItemList";
 
 const CartItemContainer = () => {
   return (
-    <div>
+    <div css={CartItemContainerStyle}>
       <CartItemControls></CartItemControls>
+      <Divider />
       <CartItemList></CartItemList>
     </div>
   );
