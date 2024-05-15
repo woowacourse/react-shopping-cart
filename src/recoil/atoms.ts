63922ec85aa85809aeeef9ba065a6b265e4f2292
vi-wolhwa <cerezo00@naver.com>
2024-05-15 18:57:53 +0900

    feat: 상품의 선택 상태를 로컬스토리지로 관리

--- src/recoil/atoms.ts
+++ src/recoil/atoms.ts
@@ -1,13 +1,17 @@
 import { atom, atomFamily } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 export const cartSelectedState = atom<number[]>({
   key: "cartSelectedState",
   default: [],
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const cartQuantityState = atomFamily<number, number>({
   key: "cartQuantityState",
-  default: 0,
+  default: 1,
 });
 
 export const cartPriceState = atomFamily<number, number>({
