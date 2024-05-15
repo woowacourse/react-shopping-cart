85acbb6b88951a5154b619c57f355be31422d485
lurgi <wahoo9040@naver.com>
2024-05-15 11:11:03 +0900

    feat: Main UI 작성
    Co-authored-by: Fe <chysis@users.noreply.github.com>

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,10 +1,13 @@
+/** @jsxImportSource @emotion/react */
 import CartItemContainer from "./Cart/CartItemContainer/CartItemContainer";
 import CartResults from "./Cart/CartResults/CartResults";
 import CartTitle from "./Cart/CartTitle/CartTitle";
 
+import { MainStyle } from "./Main.style";
+
 const Main = () => {
   return (
-    <main>
+    <main css={MainStyle}>
       <CartTitle></CartTitle>
       <CartItemContainer></CartItemContainer>
       <CartResults></CartResults>
