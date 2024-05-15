fc085da580cbbea0b63d80e7ad1d7813c7d371bf
lurgi <wahoo9040@naver.com>
2024-05-15 11:10:44 +0900

    feat: CartResults UI 작성
    Co-authored-by: Fe <chysis@users.noreply.github.com>

---
+++ src/components/Main/Cart/CartResults/CartResults.style.ts
@@ -0,0 +1,19 @@
+import { css } from "@emotion/react";
+
+export const CartResultGuideContainerStyle = css({
+  display: "flex",
+  alignItems: "center",
+
+  gap: "4px",
+});
+
+export const CartResultGuideStyle = css({
+  fontSize: "12px",
+  fontWeight: "500",
+});
+
+export const CartResultsContainerStyle = css({
+  display: "flex",
+  flexDirection: "column",
+  gap: "12px",
+});
--- src/components/Main/Cart/CartResults/CartResults.tsx
+++ src/components/Main/Cart/CartResults/CartResults.tsx
@@ -1,12 +1,22 @@
+/** @jsxImportSource @emotion/react */
+import { IoIosInformationCircleOutline } from "react-icons/io";
+import { CartResultGuideStyle, CartResultGuideContainerStyle, CartResultsContainerStyle } from "./CartResults.style";
+import PaymentDetail from "../../../PaymentDetail/PaymentDetail";
+import Divider from "../../../Divider/Divider";
+
 const CartResults = () => {
   return (
-    <div>
-      <div>총 주문 금액이 십만 이상일 경우 무료 배송됩니다.</div>
-      <div>
-        <div>주문 금액</div>
-        <div>배송비</div>
+    <div css={CartResultsContainerStyle}>
+      <div css={CartResultGuideContainerStyle}>
+        <IoIosInformationCircleOutline size={15} />
+        <div css={CartResultGuideStyle}>총 주문 금액이 십만 이상일 경우 무료 배송됩니다.</div>
       </div>
-      <div>총 결제 금액</div>
+
+      <Divider />
+      <PaymentDetail title="주문 금액" amount={60_000} />
+      <PaymentDetail title="배송비" amount={60_000} />
+      <Divider />
+      <PaymentDetail title="총 결제 금액" amount={60_000} />
     </div>
   );
 };
