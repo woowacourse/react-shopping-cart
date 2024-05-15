ef79367f28fde0a8116162863db2d42b5405d2d4
vi-wolhwa <cerezo00@naver.com>
2024-05-15 17:07:13 +0900

    feat: Button 컴포넌트 구현
    Co-authored-by: pp449 <mma7710@naver.com>

---
+++ src/components/Button/CheckboxButton/index.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import IMAGES from "../../../assets/images/Images";
+
+interface CheckboxButtonProps {
+  id: string;
+  onClick: () => void;
+  isChecked: boolean;
+}
+
+const CheckboxButton = ({ id, onClick, isChecked }: CheckboxButtonProps) => {
+  return (
+    <Button id={id} type="button" onClick={onClick}>
+      <Image alt="checkbox" src={isChecked ? IMAGES.checkedButton : IMAGES.uncheckedButton}></Image>
+    </Button>
+  );
+};
+
+export default CheckboxButton;
+
+const Button = styled.button`
+  height: 24px;
+  padding: 0;
+  margin: 0;
+  background: transparent;
+  border: none;
+  outline: none;
+
+  &:focus {
+    border: none;
+    outline: none;
+  }
+`;
+
+const Image = styled.img`
+  width: 24px;
+  height: 24px;
+  border: 1px solid;
+`;
---
+++ src/components/Button/CountingButton/index.tsx
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+import IMAGES from "../../../assets/images/Images";
+
+const BUTTON_TYPE = {
+  increase: "increase",
+  decrease: "decrease",
+} as const;
+
+const imageSrc = {
+  [BUTTON_TYPE.increase]: IMAGES.plusButton,
+  [BUTTON_TYPE.decrease]: IMAGES.minusButton,
+};
+
+type ButtonType = keyof typeof BUTTON_TYPE;
+
+interface CountingButton {
+  type: ButtonType;
+  onClick: () => void;
+}
+
+const CountingButton = ({ type, onClick }: CountingButton) => {
+  return (
+    <Button onClick={onClick}>
+      <Image alt="countingButton" src={imageSrc[type]}></Image>
+    </Button>
+  );
+};
+
+export default CountingButton;
+
+const Button = styled.button`
+  height: 24px;
+  padding: 0;
+  margin: 0;
+  background: transparent;
+  border: none;
+  outline: none;
+
+  &:active {
+    transform: scale(1.1);
+  }
+`;
+
+const Image = styled.img`
+  width: 24px;
+  height: 24px;
+`;
