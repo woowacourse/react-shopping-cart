1189fcfe1bc5fcdfee4bcb7f25863ed5568bb305
vi-wolhwa <cerezo00@naver.com>
2024-05-15 18:59:49 +0900

    feat: 상품 수량 변경, 장바구니에서 제거 API 추가

--- src/api/cartItem.ts
+++ src/api/cartItem.ts
@@ -16,3 +16,33 @@ export async function fetchCartItems(): Promise<CartItemResponse[]> {
   const data = await response.json();
   return data.content;
 }
+
+export async function patchCartItemQuantity(itemId: number, quantity: number): Promise<void> {
+  const token = generateBasicToken(SERVER_URL.userId, SERVER_URL.userPassword);
+  const bodyData = {
+    quantity,
+  };
+
+  const response = await fetch(`${SERVER_URL.apiUrl}/cart-items/${itemId}`, {
+    method: "PATCH",
+    headers: { Authorization: token, "Content-Type": "application/json" },
+    body: JSON.stringify(bodyData),
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch cart items");
+  }
+}
+
+export async function deleteCartItem(itemId: number): Promise<void> {
+  const token = generateBasicToken(SERVER_URL.userId, SERVER_URL.userPassword);
+
+  const response = await fetch(`${SERVER_URL.apiUrl}/cart-items/${itemId}`, {
+    method: "DELETE",
+    headers: { Authorization: token, "Content-Type": "application/json" },
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch cart items");
+  }
+}
