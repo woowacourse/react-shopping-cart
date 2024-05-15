fb22f39fb7719a7e727de69a07d0a6089de437ae
GimGayeon <kky1373@naver.com>
2024-05-15 17:17:23 +0900

    feat(index): 환경변수 정보들 상수 선언

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,22 +1,3 @@
-import { CartItem } from '../type';
-import { generateBasicToken } from './auth';
-
-const BASE_URL = import.meta.env.VITE_API_URL;
-const USER_ID = import.meta.env.VITE_USER_ID;
-const USER_PASSWORD = import.meta.env.VITE_USER_PASSWORD;
-
-export const fetchCartItem = async (): Promise<CartItem[]> => {
-  const token = generateBasicToken(USER_ID, USER_PASSWORD);
-
-  const response = await fetch(`${BASE_URL}/cart-items`, {
-    method: 'GET',
-    headers: { Authorization: token },
-  });
-
-  if (!response.ok) {
-    throw new Error('장바구니 정보 불러오기를 실패했습니다.');
-  }
-
-  const data = await response.json();
-  return data.content;
-};
+export const BASE_URL = import.meta.env.VITE_API_URL;
+export const USER_ID = import.meta.env.VITE_USER_ID;
+export const USER_PASSWORD = import.meta.env.VITE_USER_PASSWORD;
