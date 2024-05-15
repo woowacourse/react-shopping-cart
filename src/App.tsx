fb080961492e0940048d814db322807b393e6ba9
pakxe <pigkill40@naver.com>
2024-05-16 02:36:52 +0900

    feat: 라우터에 최종 결제 확인 페이지 추가
    Co-authored-by: Todari <rhymint@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MobileLayout from './components/common/MobileLayout/MobileLayout';
 import CartPage from './pages/CartPage';
+import ConfirmPurchasePage from './pages/ConfirmPurchasePage';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <MobileLayout>
         <Routes>
           <Route path="/" element={<CartPage />} />
+          <Route path="/confirm-purchase" element={<ConfirmPurchasePage />} />
         </Routes>
       </MobileLayout>
     </BrowserRouter>
