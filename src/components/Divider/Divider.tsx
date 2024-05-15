ed598459000df7b99b481f064ea7302c9651f787
lurgi <wahoo9040@naver.com>
2024-05-15 11:12:17 +0900

    feat: Divider Component 작성
    Co-authored-by: Fe <chysis@users.noreply.github.com>

---
+++ src/components/Divider/Divider.tsx
@@ -0,0 +1,9 @@
+/** @jsxImportSource @emotion/react */
+
+import { DividerStyle } from "./Divider.style";
+
+const Divider = () => {
+  return <hr css={DividerStyle} />;
+};
+
+export default Divider;
