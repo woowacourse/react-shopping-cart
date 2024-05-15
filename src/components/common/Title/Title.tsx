d782094a0d0e49233ecfe49c4a8414ba4ef204bd
이태훈 <rhymint@gmail.com>
2024-05-16 04:04:24 +0900

    fix: Title style file 분리
    Co-authored-by: pakxe <pigkill40@naver.com>

--- src/components/common/Title/Title.tsx
+++ src/components/common/Title/Title.tsx
@@ -1,29 +1,21 @@
-import styled from 'styled-components';
 import Text from '../Text/Text';
+import * as S from './Title.style'
 
 type TitleProps = {
   title: string;
   description?: string;
 };
 
-const TitleContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  row-gap: 12px;
-
-  width: 100%;
-`;
-
 const Title = ({ title, description }: TitleProps) => {
   return (
-    <TitleContainer>
+    <S.TitleContainer>
       <Text size="l" weight="l">
         {title}
       </Text>
       <Text size="s" weight="m">
         {description}
       </Text>
-    </TitleContainer>
+    </S.TitleContainer>
   );
 };
 
