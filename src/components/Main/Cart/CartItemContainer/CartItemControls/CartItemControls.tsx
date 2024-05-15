4fdde5e2cef43edc5d2cf1510a13ef9e381e722a
lurgi <wahoo9040@naver.com>
2024-05-15 11:08:12 +0900

    feat: CartItemControls UI 작성
    Co-authored-by: Fe <chysis@users.noreply.github.com>

--- src/components/Main/Cart/CartItemContainer/CartItemControls/CartItemControls.tsx
+++ src/components/Main/Cart/CartItemContainer/CartItemControls/CartItemControls.tsx
@@ -1,5 +1,15 @@
+/** @jsxImportSource @emotion/react */
+import Checkbox from "../../../../Button/Checkbox/Checkbox";
+import { ControlStyle } from "./CartItemControls.style";
+
 const CartItemControls = () => {
-  return <div>전체 선택 할 수 있는 요소</div>;
+  return (
+    <div css={ControlStyle}>
+      <Checkbox />
+
+      <div>전체 선택</div>
+    </div>
+  );
 };
 
 export default CartItemControls;
