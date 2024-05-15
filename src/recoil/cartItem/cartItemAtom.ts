b47eb616180c79d45d371c32de19c43ea6537623
이태훈 <rhymint@gmail.com>
2024-05-16 04:43:53 +0900

    style: 사용하지 않는 주석 제거
    Co-authored-by: pakxe <pigkill40@naver.com>

--- src/recoil/cartItem/cartItemAtom.ts
+++ src/recoil/cartItem/cartItemAtom.ts
@@ -4,12 +4,10 @@ const localStorageEffect =
   (key: string) =>
   ({ setSelf, onSet }: any) => {
     const savedValue = localStorage.getItem(key);
-    // setSelf -> Callbacks to set or reset the value of the atom.
     if (savedValue != null) {
       setSelf(JSON.parse(savedValue));
     }
 
-    // onSet -> Subscribe to changes in the atom value.
     onSet((newValue: any, _: any, isReset: boolean) => {
       isReset ? localStorage.removeItem(key) : localStorage.setItem(key, JSON.stringify(newValue));
     });
