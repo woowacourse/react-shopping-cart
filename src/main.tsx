dc91470d88881ea225d8b29ecc8175c29f2e2319
00kang <kdb070970@gmail.com>
2024-05-15 16:50:56 +0900

    fix : App.css import
    Co-Authored-By: Yuseon Kim(썬데이) <74897720+useon@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
+import "./App.css";
 import router from "./router.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
