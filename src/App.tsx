d524b548e14ae83102383b87b75c146745394101
lurgi <wahoo9040@naver.com>
2024-05-15 11:15:25 +0900

    feat: App에 Footer 적용
    Co-authored-by: Fe <chysis@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import "./App.css";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   );
 }
