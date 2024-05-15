c176bfbd136a9d4bd3c3df753319e95df834165b
Seongjin Hong <hongseongjin.to@gmail.com>
2024-05-15 10:46:51 +0900

    feat: App.tsx에 현재까지 구현된 컴포넌트 시각화
    Co-authored-by: ss0526100 <ss0526100@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-import "./App.css";
+import styled from '@emotion/styled';
+
+import Header from './components/Header/Header';
+import FooterButton from './components/FooterButton/FooterButton';
+import TitleContainer from './components/TitleContainer/TitleContainer';
+import OrderAmount from './components/OrderAmount/OrderAmount';
+
+import './App.css';
+import './reset.css';
+import CartItemContainer from './components/CartItemContainer/CartItemContainer';
+
+const Content = styled.section({
+  padding: '36px 24px',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '36px',
+});
 
 function App() {
   return (
     <>
-      <h1>react-shopping-cart</h1>
+      <Header />
+      <Content>
+        <TitleContainer title="장바구니" description="현재 2종류의 상품이 담겨있습니다." />
+
+        <CartItemContainer />
+        <OrderAmount />
+      </Content>
+      <FooterButton buttonText="주문 확인" />
     </>
   );
 }
