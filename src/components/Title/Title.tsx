55c69b918dc2fa9094b665b09ab0117861dfa128
GimGayeon <kky1373@naver.com>
2024-05-15 15:50:39 +0900

    refactor(Title): caption prop optional로 수정

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -2,14 +2,14 @@ import * as Styled from './style';
 
 interface TitleProp {
   title: string;
-  caption: string;
+  caption?: string;
 }
 
 const Title = ({ title, caption }: TitleProp) => {
   return (
     <Styled.TitleContainer>
       <Styled.Title>{title}</Styled.Title>
-      <Styled.Caption>{caption}</Styled.Caption>
+      {caption && <Styled.Caption>{caption}</Styled.Caption>}
     </Styled.TitleContainer>
   );
 };
