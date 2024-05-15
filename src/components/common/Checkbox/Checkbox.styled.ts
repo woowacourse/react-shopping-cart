623a9a8f2e81a71e3954fac3513ab3289c808d78
손진영 <sm099@naver.com>
2024-05-15 10:51:18 +0900

    feat: Checkbox 컴포넌트 마크업
    Co-authored-by: badahertz52 <badahertz52@gmail.com>

--- src/components/common/Checkbox/Checkbox.styled.ts
+++ src/components/common/Checkbox/Checkbox.styled.ts
@@ -1,50 +1,24 @@
 import styled from 'styled-components';
 
-export const CheckboxContainer = styled.div`
-  display: flex;
-  vertical-align: middle;
-  align-items: center;
-`;
+import CheckIcon from '@assets/check.svg';
+import NoneCheckIcon from '@assets/noneCheck.svg';
 
-export const HiddenCheckBox = styled.input.attrs((props) => ({
-  type: 'checkbox',
-  checked: props.checked,
-  onChange: props.onChange,
-}))`
-  border: 0;
-  clip: rect(0 0 0 0);
-  clippath: inset(50%);
-  height: 1px;
-  margin: -1px;
-  overflow: hidden;
-  padding: 0;
-  position: absolute;
-  white-space: nowrap;
-  width: 1px;
-`;
+import { COLOR } from '@styles/style.constant';
 
-export const Icon = styled.svg`
-  fill: none;
-  stroke: white;
-  stroke-width: 2px;
-`;
+export const CheckBoxInput = styled.input`
+  appearance: none;
+  width: 24px;
+  height: 24px;
+  border-radius: 8px;
+  background-repeat: no-repeat;
+  background-size: 70% 70%;
+  background-position: 50%;
+  background-image: url(${NoneCheckIcon});
+  background-color: ${COLOR.white};
+  border: 1px solid ${COLOR.borderColor};
 
-export const SaveIDCheckBox = styled.div<{ checked: boolean }>`
-  display: inline-block;
-  width: 18px;
-  height: 18px;
-  background: ${(props) => (props.checked ? `black` : `yellow`)};
-  opacity: ${(props) => !props.checked && 0.7};
-  border-radius: 3px;
-  transition: all 100ms;
-  margin-right: 8px;
-
-  &:hover {
-    opacity: 1;
-    background: 'black';
-  }
-
-  ${Icon} {
-    visibility: ${(props) => (props.checked ? 'visible' : 'hidden')};
+  &:checked {
+    background-image: url(${CheckIcon});
+    background-color: ${COLOR.black};
   }
 `;
--- src/components/common/Checkbox/Checkbox.tsx
+++ src/components/common/Checkbox/Checkbox.tsx
@@ -1,18 +1,11 @@
-import { COLOR } from '@styles/style.constant';
-import styled from 'styled-components';
+import * as Styled from './Checkbox.styled';
 
 interface CheckBoxProps {
   checked: boolean;
 }
-const CheckBoxInput = styled.input`
-  background-color: ${COLOR.black};
-  width: 24px;
-  height: 24px;
-  border-radius: 8px;
-`;
 
 const CheckBox: React.FC<React.InputHTMLAttributes<HTMLInputElement> & CheckBoxProps> = ({ checked, ...props }) => {
-  return <CheckBoxInput type="checkbox" checked={checked} onChange={props.onChange} />;
+  return <Styled.CheckBoxInput type="checkbox" checked={checked} {...props} />;
 };
 
 export default CheckBox;
