33af0020172618617ddd398aee123a76e0e5d157
pakxe <pigkill40@naver.com>
2024-05-15 18:03:26 +0900

    feat: 모바일 환경을을 연출하기 위한 레이아웃 컴포넌트 구현
    Co-authored-by: Todari <rhymint@gmail.com>

---
+++ src/components/common/MobileLayout/MobileLayout.tsx
@@ -0,0 +1,36 @@
+import { ReactNode } from 'react';
+import styled from 'styled-components';
+
+const Background = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+
+  background-color: lightGray;
+`;
+
+const LimitedWidthContainer = styled.div`
+  position: relative;
+  width: 390px;
+  height: 100vh;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  background-color: white;
+`;
+
+type MobileLayoutProps = {
+  children: ReactNode;
+};
+
+const MobileLayout = ({ children, ...rest }: MobileLayoutProps) => {
+  return (
+    <Background>
+      <LimitedWidthContainer {...rest}>{children}</LimitedWidthContainer>
+    </Background>
+  );
+};
+
+export default MobileLayout;
