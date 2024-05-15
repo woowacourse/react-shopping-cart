da69d612b53c153731aa98e7b31dfcdb57b2ffb4
vi-wolhwa <cerezo00@naver.com>
2024-05-15 17:06:06 +0900

    chore: 이미지 Src 맵핑 상수 선언

---
+++ src/assets/images/Images.ts
@@ -0,0 +1,13 @@
+import checkedButton from "./checkedButton.png";
+import uncheckedButton from "./uncheckedButton.png";
+import plusButton from "./plusButton.png";
+import minusButton from "./minusButton.png";
+
+const IMAGES = {
+  checkedButton,
+  uncheckedButton,
+  plusButton,
+  minusButton,
+};
+
+export default IMAGES;
