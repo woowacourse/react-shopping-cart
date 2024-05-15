9e9b339662ec09e6ae00ad03f5c39d240c42330b
bmj0053 <bmj05378@gmail.com>
2024-05-15 13:31:16 +0900

    feat: NotificationIcon 이미지 추가 및 CheckedBox 이미지 수정
    Co-authored-by: jaeml06 <jaeml06@users.noreply.github.com>

--- src/asset/index.ts
+++ src/asset/index.ts
@@ -1,2 +1,3 @@
 export { default as NoneCheckedBox } from './NoneCheckedBox.svg';
 export { default as CheckedBox } from './CheckedBox.svg';
+export { default as NotificationIcon } from './NotificationIcon.svg';
