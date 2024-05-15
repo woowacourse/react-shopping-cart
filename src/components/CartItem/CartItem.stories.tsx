7b131ef08b84bc16e9023e4233ade131fd701ed9
이태훈 <rhymint@gmail.com>
2024-05-16 04:03:27 +0900

    storybook: CarItem storybook 작성
    Co-authored-by: pakxe <pigkill40@naver.com>

--- src/components/CartItem/CartItem.stories.tsx
+++ src/components/CartItem/CartItem.stories.tsx
@@ -1,22 +1,50 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import CartItem from './CartItem';
+import CartItem, { CartItemProps } from './CartItem';
+import { RecoilRoot } from 'recoil';
 
 const meta = {
   title: 'Components/CartItem',
   component: CartItem,
   tags: ['autodocs'],
+  argTypes: {
+    product: {
+      description: '',
+      control: { type: 'object' },
+    },
+    quantity: {
+      description: '',
+      control: { type: 'number', min: 1 },
+    },
+    cartItemId: {
+      description: '',
+    }
+  },
+  args: {
+    product: {
+      "id": 3,
+      "name": "아디다스",
+      "price": 2000,
+      "imageUrl": "https://sitem.ssgcdn.com/74/25/04/item/1000373042574_i1_750.jpg",
+      "category": "fashion"
+    },
+    quantity: 1,
+    cartItemId: 1,
+  }
 } satisfies Meta<typeof CartItem>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
+// TODO : decorator 사용하여 동작할 수 있도록 current: recoil로 인해 작동 x
 export const Playground: Story = {
-  render: ({ ...args }) => {
+  render: ({ ...args }: CartItemProps) => {
     return (
-      <div style={{ width: '380px' }}>
-        <CartItem />
-      </div>
+      <RecoilRoot>
+        <div style={{ width: '430px' }}>
+          <CartItem {...args} />
+        </div>
+      </RecoilRoot>
     )
   }
 };
