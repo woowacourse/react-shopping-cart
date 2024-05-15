589747907d824bc89ff113b4c0a458fb80d8e09f
lurgi <wahoo9040@naver.com>
2024-05-15 11:11:19 +0900

    feat: Footer UI 작성
    Co-authored-by: Fe <chysis@users.noreply.github.com>

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+/** @jsxImportSource @emotion/react */
+import { FooterStyle } from "./Footer.style";
+
+const Footer = () => {
+  return <button css={FooterStyle}>주문 확인</button>;
+};
+
+export default Footer;
