e28ba8cdc4c4fffca3319b07681f0ff26df877cd
손진영 <sm099@naver.com>
2024-05-15 19:11:36 +0900

    fix: A component suspended while responding to synchronous input. 문제 해결
    App 컴포넌트에 Suspense wrapping 하는 형태로 해결
    
    Co-authored-by: badahertz52 <badahertz52@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import router from '@routes/route';
 import GlobalStyle from '@styles/globalStyle';
+import { Suspense } from 'react';
 import { RouterProvider } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
 function App() {
   return (
-    <>
+    <RecoilRoot>
       <GlobalStyle />
-      <RouterProvider router={router} />
-    </>
+      <Suspense fallback="loading...">
+        <RouterProvider router={router} />
+      </Suspense>
+    </RecoilRoot>
   );
 }
 
