8010f4e1033f1a30504b6df63b2a829cf674490a
GimGayeon <kky1373@naver.com>
2024-05-15 17:18:25 +0900

    feat(shoppingCart): 장바구니 fetch, remove, adjustQuantity 기능 구현
    Co-authored-by: simorimi <simorimi@users.noreply.github.com>

---
+++ src/api/shoppingCart.ts
@@ -0,0 +1,53 @@
+import { BASE_URL, USER_ID, USER_PASSWORD } from '.';
+import { CartItem } from '../type';
+import { generateBasicToken } from './auth';
+
+export const fetchCartItems = async (): Promise<CartItem[]> => {
+  const token = generateBasicToken(USER_ID, USER_PASSWORD);
+
+  const response = await fetch(`${BASE_URL}/cart-items`, {
+    method: 'GET',
+    headers: { Authorization: token },
+  });
+
+  if (!response.ok) {
+    throw new Error('장바구니 정보 불러오기를 실패했습니다.');
+  }
+
+  const data = await response.json();
+  return data.content;
+};
+
+export const removeCartItem = async (cartItemId: number): Promise<void> => {
+  const token = generateBasicToken(USER_ID, USER_PASSWORD);
+
+  const response = await fetch(`${BASE_URL}/cart-items/${cartItemId}`, {
+    method: 'DELETE',
+    headers: { Authorization: token },
+  });
+
+  if (!response.ok) {
+    throw new Error('장바구니 아이템 삭제를 실패했습니다.');
+  }
+};
+
+export const adjustCartItemQuantity = async (
+  cartItemId: number,
+  quantity: number,
+): Promise<void> => {
+  const token = generateBasicToken(USER_ID, USER_PASSWORD);
+
+  const response = await fetch(`${BASE_URL}/cart-items/${cartItemId}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: token,
+    },
+
+    body: JSON.stringify({ quantity }),
+  });
+
+  if (!response.ok) {
+    throw new Error('장바구니 아이템 수령 변경을 실패했습니다.');
+  }
+};
