fc73afe690461076954aead73e6bbe3c4804a659
Soyeon Choe <soy2302ten@gmail.com>
2024-05-16 12:02:10 +0900

    feat: 페이지 router
    Co-Authored-By: soosoo22 <80167893+soosoo22@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
-import App from './App.tsx';
+import CartPage from './pages/CartPage';
+import OrderConfirmPage from './pages/OrderConfirmPage';
+
 import './index.css';
+
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      Component: CartPage,
+    },
+    {
+      path: '/order-confirm',
+      Component: OrderConfirmPage,
+    },
+  ],
+  { basename: import.meta.env.BASE_URL },
+);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RecoilRoot>
-      <App />
+      <RouterProvider router={router} />
     </RecoilRoot>
   </React.StrictMode>,
 );
