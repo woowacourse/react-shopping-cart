7f7a82c60438f50bf78019942287ed23868979b4
greetings1012 <ahnqwerty@gmail.com>
2024-05-16 11:22:44 +0900

    feat(selectors): 장바구니 아이템 선택 토글 기능 selector로 구현
    Co-authored-by: YUNAH <anttiey@users.noreply.github.com>

--- src/recoil/selectors/selectors.ts
+++ src/recoil/selectors/selectors.ts
@@ -1,18 +1,43 @@
-import { selector } from 'recoil';
-import { selectedCartItemState } from '../atoms/atoms';
+import { selector, selectorFamily } from 'recoil';
+import { selectedCartItemListState } from '../atoms/atoms';
+import { TCartItem } from '../../types/CartItem.type';
 
-export const selectedCartItemTotalPriceState = selector<number>({
-  key: 'selectedCartItemTotalPriceState',
+export const selectedCartItemListSelector = selectorFamily<boolean, TCartItem>({
+  key: 'selectedCartItemListSelector',
+  get:
+    (newItem: TCartItem) =>
+    ({ get }) => {
+      const selectedCartItemList = get(selectedCartItemListState);
+      return selectedCartItemList.some((item) => item.id === newItem.id);
+    },
+  set:
+    (newItem: TCartItem) =>
+    ({ set, get }) => {
+      const selectedCartItemList = get(selectedCartItemListState);
+
+      if (!selectedCartItemList.some((item) => item.id === newItem.id)) {
+        set(selectedCartItemListState, [...selectedCartItemList, newItem]);
+      } else {
+        set(
+          selectedCartItemListState,
+          selectedCartItemList.filter((item) => item.id !== newItem.id),
+        );
+      }
+    },
+});
+
+export const selectedCartItemListTotalPriceSelector = selector<number>({
+  key: 'selectedCartItemListTotalPriceSelector',
   get: ({ get }) => {
-    const selectedCardItem = get(selectedCartItemState);
-    return selectedCardItem.reduce((acc, cur) => acc + cur.quantity * cur.product.price, 0);
+    const selectedCardItemList = get(selectedCartItemListState);
+    return selectedCardItemList.reduce((acc, cur) => acc + cur.quantity * cur.product.price, 0);
   },
 });
 
-export const selectedCartItemTotalCountState = selector<number>({
-  key: 'selectedCartItemTotalCountState',
+export const selectedCartItemListTotalCountSelector = selector<number>({
+  key: 'selectedCartItemListTotalCountSelector',
   get: ({ get }) => {
-    const selectedCardItem = get(selectedCartItemState);
-    return selectedCardItem.reduce((acc, cur) => acc + cur.quantity, 0);
+    const selectedCardItemList = get(selectedCartItemListState);
+    return selectedCardItemList.reduce((acc, cur) => acc + cur.quantity, 0);
   },
 });
