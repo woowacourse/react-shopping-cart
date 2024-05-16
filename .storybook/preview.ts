28b06566f03451e6696dfb36601aeaf9c2298608
vi-wolhwa <cerezo00@naver.com>
2024-05-17 02:42:59 +0900

    chore: init Storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,16 @@
+import type { Preview } from '@storybook/react';
+import '/src/f_shared/styles/reset.css';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    layout: 'fullscreen',
+  },
+};
+
+export default preview;
