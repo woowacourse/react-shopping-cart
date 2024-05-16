a62466b435dbcfe6cee9dbb0fcbbc36591da7e74
hwinkr <dnddl8280@naver.com>
2024-05-16 16:39:12 +0900

    config(type): 카드 아이템 타입 설정
    Co-authored-by: ooherin <555ohr@naver.com>

--- src/types/cart.ts
+++ src/types/cart.ts
@@ -1,13 +1,13 @@
+export interface CartItem {
+  id: number;
+  quantity: number;
+  product: Product;
+}
+
 export interface Product {
   id: number;
   name: string;
   price: number;
   imageUrl: string;
   category: string;
-}
-
-export interface CartItem {
-  id: number;
-  product: Product;
-  quantity: number;
 }
