c53496539b4b5e9d577221c7b8c9803dc7229446
hwinkr <dnddl8280@naver.com>
2024-05-16 16:59:35 +0900

    feat(CartEmpty): 장바구니에 데이터 없는 경우 보여줄 컴포넌트 구현
    Co-authored-by: Hyerin <ooherin@users.noreply.github.com>

---
+++ src/components/CartEmpty/CartEmpty.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import { FlexCenter } from "@/styles/common";
+import Caption from "../_common/Caption/Caption";
+import OrderConfirmButton from "../OrderConfirmButton/OrderConfirmButton";
+
+const CartEmpty = () => {
+  return (
+    <>
+      <Wrapper>
+        <Caption text="장바구니에 담은 상품이 없습니다."></Caption>
+        <OrderConfirmButton disabled={true} />
+      </Wrapper>
+    </>
+  );
+};
+
+export default CartEmpty;
+
+const Wrapper = styled.div`
+  ${FlexCenter}
+  height:calc(100% - 128px);
+`;
