92c205fa0ab04a1a7e610c80474cbbd8ba988068
vi-wolhwa <cerezo00@naver.com>
2024-05-16 12:01:11 +0900

    chore: 이미지 추가
    Co-authored-by: Lee sang Yeop <mma7710@naver.com>

--- src/assets/images/Images.ts
+++ src/assets/images/Images.ts
@@ -2,12 +2,16 @@ import checkedButton from "./checkedButton.png";
 import uncheckedButton from "./uncheckedButton.png";
 import plusButton from "./plusButton.png";
 import minusButton from "./minusButton.png";
+import infoOutline from "./infoOutline.svg";
+import thunk from "./thunk.png";
 
 const IMAGES = {
   checkedButton,
   uncheckedButton,
   plusButton,
   minusButton,
+  infoOutline,
+  thunk,
 };
 
 export default IMAGES;
