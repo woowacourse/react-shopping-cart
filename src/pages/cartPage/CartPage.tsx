6d9a9b29d95045ee5a39972a53397b9074204b59
useon <yoosk4986@naver.com>
2024-05-16 18:45:07 +0900

    fix: useEffect 의존성 배열 제거

--- src/pages/cartPage/CartPage.tsx
+++ src/pages/cartPage/CartPage.tsx
@@ -1,16 +1,16 @@
-import { useEffect } from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { getCartItems } from "../../api";
-import { ConfirmButton } from "../../components/button/confirmButton/ConfirmButton";
-import { CartContentSection } from "../../components/cartContentSection/CartContentSection";
-import { CartHeader } from "../../components/cartHeader/CartHeader";
-import Header from "../../components/header/Header";
-import { cartItemsState } from "../../recoil/atoms/atoms";
-import { categoryCountState } from "../../recoil/selector/selector";
-import { StyledCartPage } from "./CartPage.styled";
+import { useEffect } from 'react';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { getCartItems } from '../../api';
+import { ConfirmButton } from '../../components/button/confirmButton/ConfirmButton';
+import { CartContentSection } from '../../components/cartContentSection/CartContentSection';
+import { CartHeader } from '../../components/cartHeader/CartHeader';
+import Header from '../../components/header/Header';
+import { cartItemsState } from '../../recoil/atoms/atoms';
+import { categoryCountState } from '../../recoil/selector/selector';
+import { StyledCartPage } from './CartPage.styled';
 
 export const CartPage: React.FC = () => {
-  const [cartItems, setCartItems] = useRecoilState(cartItemsState);
+  const setCartItems = useSetRecoilState(cartItemsState);
   const categoryCount = useRecoilValue(categoryCountState);
 
   useEffect(() => {
@@ -19,24 +19,24 @@ export const CartPage: React.FC = () => {
         const items = await getCartItems();
         setCartItems(items);
       } catch (error) {
-        console.error("Failed to fetch cart items:", error);
+        console.error('Failed to fetch cart items:', error);
       }
     };
 
     fetchCartItems();
-  }, [cartItems]);
+  }, []);
 
   const buttonBackgroundColor =
-    categoryCount === 0 ? "rgba(190, 190, 190, 1)" : "rgba(0, 0, 0, 1)";
+    categoryCount === 0 ? 'rgba(190, 190, 190, 1)' : 'rgba(0, 0, 0, 1)';
 
   return (
     <>
-      <Header type="shop" />
+      <Header type='shop' />
       <StyledCartPage>
         <CartHeader categoryCount={categoryCount} />
         <CartContentSection categoryCount={categoryCount} />
       </StyledCartPage>
-      <ConfirmButton text="주문 확인" backgroundColor={buttonBackgroundColor} />
+      <ConfirmButton text='주문 확인' backgroundColor={buttonBackgroundColor} />
     </>
   );
 };
