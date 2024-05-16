be095edbda07aba754767ab208b9be8665c46d89
GimGayeon <kky1373@naver.com>
2024-05-16 16:16:07 +0900

    feat(main): Error, Loading 상태 설정
    Co-authored-by: simorimi <simorimi@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './GlobalStyle.ts';
 import theme from './theme.ts';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <RecoilRoot>
         <GlobalStyle />
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </RecoilRoot>
     </ThemeProvider>
   </React.StrictMode>,
