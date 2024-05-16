741a4391206025765ff3157fd067b7f523275fcf
00kang <kdb070970@gmail.com>
2024-05-16 17:17:34 +0900

    feat : baseURL 추가
    Co-Authored-By: Yuseon Kim(썬데이) <74897720+useon@users.noreply.github.com>

--- src/router.tsx
+++ src/router.tsx
@@ -3,21 +3,24 @@ import App from "./App";
 import { CartPage } from "./pages/cartPage/CartPage";
 import { OrderConfirmationPage } from "./pages/orderConfirmationPage/OrderConfirmationPage";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <CartPage />,
-      },
-      {
-        path: "/order-confirmation",
-        element: <OrderConfirmationPage />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          index: true,
+          element: <CartPage />,
+        },
+        {
+          path: "/order-confirmation",
+          element: <OrderConfirmationPage />,
+        },
+      ],
+    },
+  ],
+  { basename: "/react-shopping-cart/" }
+);
 
 export default router;
