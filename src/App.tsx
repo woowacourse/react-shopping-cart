705245d13d70c52075ab223cc2cf025f419e0890
pakxe <pigkill40@naver.com>
2024-05-16 13:16:03 +0900

    fix: app component에 errorboundary, suspense 적용
    Co-authored-by: Todari <rhymint@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,35 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MobileLayout from './components/common/MobileLayout/MobileLayout';
 import CartPage from './pages/CartPage';
 import ConfirmPurchasePage from './pages/ConfirmPurchasePage';
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorFallback from './components/ErrorFallback/ErrorFallback';
+import { Suspense } from 'react';
 
 function App() {
   return (
     <BrowserRouter>
       <MobileLayout>
         <Routes>
-          <Route path="/" element={<CartPage />} />
-          <Route path="/confirm-purchase" element={<ConfirmPurchasePage />} />
+          <Route
+            path="/"
+            element={
+              <ErrorBoundary FallbackComponent={ErrorFallback}>
+                <Suspense fallback={<div>suspense</div>}>
+                  <CartPage />
+                </Suspense>
+              </ErrorBoundary>
+            }
+          />
+          <Route
+            path="/confirm-purchase"
+            element={
+              <ErrorBoundary FallbackComponent={ErrorFallback}>
+                <Suspense fallback={<div>suspense</div>}>
+                  <ConfirmPurchasePage />
+                </Suspense>
+              </ErrorBoundary>
+            }
+          />
         </Routes>
       </MobileLayout>
     </BrowserRouter>
