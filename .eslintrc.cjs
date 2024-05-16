fd8ee9339847d3e9cdeb876befbae57fb231b944
vi-wolhwa <cerezo00@naver.com>
2024-05-17 02:42:41 +0900

    style(.eslintrc)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,7 @@ const { allowedNodeEnvironmentFlags } = require('process');
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:import/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:import/recommended', 'plugin:storybook/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh', 'import'],
