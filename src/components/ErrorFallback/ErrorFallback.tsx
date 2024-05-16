5b3e96cd0e088e273f64c702f830ff9c4c8cb870
pakxe <pigkill40@naver.com>
2024-05-16 13:12:45 +0900

    fix: error fallback error message 받도록 수정
    Co-authored-by: Todari <rhymint@gmail.com>

--- src/components/ErrorFallback/ErrorFallback.tsx
+++ src/components/ErrorFallback/ErrorFallback.tsx
@@ -1,13 +1,11 @@
-import * as S from './ErrorFallback.style'
+import * as S from './ErrorFallback.style';
 
-
-//TODO : Errorcode 받아서 반환하는게 나을듯
-const ErrorFallback = () => {
-  return (
-    <S.FallbackContainer>
-      장바구니에 담은 상품이 없습니다.
-    </S.FallbackContainer >
-  )
+interface ErrorFallbackProps {
+  error?: Error;
 }
+//TODO : Errorcode 받아서 반환하는게 나을듯
+const ErrorFallback = ({ error }: ErrorFallbackProps) => {
+  return <S.FallbackContainer>{error ? `${error.message}` : '장바구니에 담은 상품이 없습니다.'}</S.FallbackContainer>;
+};
 
-export default ErrorFallback
+export default ErrorFallback;
