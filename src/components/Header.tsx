f92a5e0494bd3e9f7d9a02ec2508b090aac1597b
hanyoung <phanyoung@naver.com>
2024-05-16 15:56:47 +0900

    feat(Header): 공통 컴포넌트 header 추가
    Co-authored-by: cys4585 <dev.youngsu@gmail.com>

---
+++ src/components/Header.tsx
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { ReactComponent as BackIcon } from "../assets/back-icon.svg";
+
+export interface IHeaderProps {
+  hasBackButton: boolean;
+  title?: string;
+}
+
+export default function Header({ hasBackButton, title }: IHeaderProps) {
+  const navigate = useNavigate();
+
+  const handleBackButtonClick = () => {
+    navigate(-1);
+  };
+
+  return (
+    <S.HeaderContainer>
+      {hasBackButton ? (
+        <S.BackButton onClick={handleBackButtonClick} />
+      ) : (
+        <S.Title>{title}</S.Title>
+      )}
+    </S.HeaderContainer>
+  );
+}
+
+const S = {
+  HeaderContainer: styled.header`
+    height: 64px;
+    background-color: black;
+    display: flex;
+    align-items: center;
+    padding-left: 24px;
+  `,
+
+  BackButton: styled(BackIcon)`
+    cursor: pointer;
+    width: 32px;
+    height: 32px;
+  `,
+
+  Title: styled.div`
+    font-size: 20px;
+    font-weight: 800;
+    color: white;
+    width: fit-content;
+  `,
+};
