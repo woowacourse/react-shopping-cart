39d5dd701be7041519a77ac628d29b0f335da998
00kang <kdb070970@gmail.com>
2024-05-16 16:06:39 +0900

    feat : mockData를 활용한 장바구니 데이터 로딩 테스트
    Co-Authored-By: Yuseon Kim(썬데이) <74897720+useon@users.noreply.github.com>

--- src/recoil/atoms/atoms.test.ts
+++ src/recoil/atoms/atoms.test.ts
@@ -1,6 +1,6 @@
-import "@testing-library/jest-dom";
-import { renderHook } from "@testing-library/react";
+import { act, renderHook } from "@testing-library/react";
 import { RecoilRoot, useRecoilState } from "recoil";
+import { mockCartItems } from "../../mocks/mockData";
 import { cartItemsCountState, cartItemsState } from "./atoms";
 
 describe("초기값 테스트", () => {
@@ -18,5 +18,17 @@ describe("초기값 테스트", () => {
     });
 
     expect(result.current[0]).toBe(0);
+  });
+});
+
+describe("mockData를 이용한 테스트", () => {
+  it("장바구니 데이터 로딩 ", () => {
+    const { result } = renderHook(() => useRecoilState(cartItemsState), {
+      wrapper: RecoilRoot,
+    });
+
+    act(() => result.current[1](mockCartItems.content));
+
+    expect(result.current[0].length).toBe(3);
   });
 });
