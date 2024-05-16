bb82741a50707c731abfa52904c9743959442323
vi-wolhwa <cerezo00@naver.com>
2024-05-16 23:22:31 +0900

    chore: lint 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,5 @@
+const { allowedNodeEnvironmentFlags } = require('process');
+
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
@@ -5,14 +7,42 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'import'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'import/newline-after-import': ['error', { count: 1 }],
+    'import/order': [
+      'error',
+      {
+        groups: [['builtin', 'external'], 'internal', 'parent', 'sibling', 'index', 'type'],
+        'newlines-between': 'always',
+        warnOnUnassignedImports: true,
+        distinctGroup: false,
+        pathGroups: [
+          {
+            pattern: '*react',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '.**/*.css',
+            group: 'type',
+            position: 'after',
+          },
+        ],
+        alphabetize: {
+          order: 'asc',
+        },
+      },
     ],
   },
-}
+};
