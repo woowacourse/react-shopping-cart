96ed9a5e5e34bbbb7eb9b127646cc39e48a39487
김진호 <rlawlsgh1227@gmail.com>
2024-05-16 13:19:05 +0900

    fix: 가격이 0원일 때 배송비가 없도록 수정
    Co-authored-by: ImxYJL <allensain14@gmail.com>

--- src/recoil/selectors.ts
+++ src/recoil/selectors.ts
@@ -19,7 +19,8 @@ export const priceInfoStore = selector<PriceInfo>({
   get: ({ get }) => {
     const selected = get(selectedCartItems);
     const price = selected.reduce((acc, cur) => (acc += cur.price * cur.quantity), 0);
-    const shipping = price < ORDER.SHIPPING_FREE_PRICE ? ORDER.SHIPPING_FEE : 0;
+    const isShippingFree = price >= ORDER.SHIPPING_FREE_PRICE || price === 0;
+    const shipping = isShippingFree ? 0 : ORDER.SHIPPING_FEE;
     return {
       order: price,
       shipping,
