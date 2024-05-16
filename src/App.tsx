0d247dd7627149216c4ece8b3030dae1f38309ce
hanyoung <phanyoung@naver.com>
2024-05-16 16:30:21 +0900

    feat(App): ErrorBoundary 추가
    Co-authored-by: cys4585 <dev.youngsu@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CartPage from "./pages/CartPage";
 import { PATH } from "./constants/path";
 import OrderSummaryPage from "./pages/OrderSummaryPage";
+import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
   return (
     <BrowserRouter>
       <RecoilRoot>
-        <Routes>
-          <Route path={PATH.cart} element={<CartPage />} />
-          <Route path={PATH.orderSummary} element={<OrderSummaryPage />} />
-        </Routes>
+        <ErrorBoundary fallbackRender={({ error }) => error.message}>
+          <Routes>
+            <Route path={PATH.cart} element={<CartPage />} />
+            <Route path={PATH.orderSummary} element={<OrderSummaryPage />} />
+          </Routes>
+        </ErrorBoundary>
       </RecoilRoot>
     </BrowserRouter>
   );
