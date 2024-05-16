4b6c0068506f6c36ecf0b94153a72685a911a3d1
chysis <chysiss@naver.com>
2024-05-16 19:15:17 +0900

    chore: addButton 주석 추가

--- src/components/AddButton/AddButton.tsx
+++ src/components/AddButton/AddButton.tsx
@@ -13,7 +13,7 @@ const AddButton = () => {
             method: "POST",
             headers: { "Content-Type": "application/json", Authorization: API_TOKEN },
             body: JSON.stringify({
-              productId: 10,
+              productId: 10, // 2, 3, 10, 11, 12, 21, 34로 설정해서 상품을 장바구니에 추가할 수 있습니다.
             }),
           });
         };
