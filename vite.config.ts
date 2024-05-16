4247950d71e5531cbc90d44c820fb9bf9ad19ae1
pakxe <pigkill40@naver.com>
2024-05-16 17:16:23 +0900

    feat: process.env로 사용할 수 있도록 수정
    Co-authored-by: Todari <rhymint@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    define: {
+      'process.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL),
+      'process.env.VITE_API_USER_ID': JSON.stringify(env.VITE_API_USER_ID),
+      'process.env.VITE_API_USER_PASSWORD': JSON.stringify(env.VITE_API_USER_PASSWORD),
+    },
+  };
+});
