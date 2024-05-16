baeb40561130085a606656ff2f9e55c0559a7a0e
pakxe <pigkill40@naver.com>
2024-05-16 13:15:09 +0900

    fix: cartpage fallback 로직 변경
    Co-authored-by: Todari <rhymint@gmail.com>

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -5,7 +5,7 @@ import CartItemList from '../components/CartItemList/CartItemList';
 import PriceTable from '../components/PriceTable/PriceTable';
 import { cartItemListQuery, cartItemListState } from '../recoil/cartItemList/cartItemListSelector';
 import { useRecoilValue, useRecoilValueLoadable } from 'recoil';
-import { Suspense, useEffect } from 'react';
+import { useEffect } from 'react';
 import useCartListItem from '../recoil/cartItemList/useCartItemList';
 import Button from '../components/common/Button/Button';
 import { cartItemSelectedIdListAtom } from '../recoil/cartItem/cartItemAtom';
@@ -18,16 +18,6 @@ const CartPageContainer = styled.main`
   width: 100%;
   padding: 100px 20px 80px 20px;
 `;
-
-const EmptyPage = styled.div`
-  width: 100%;
-  height: 100%;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
 const CartPage = () => {
   const { updateCartItemList } = useCartListItem();
 
@@ -41,40 +31,41 @@ const CartPage = () => {
 
   const navigate = useNavigate();
 
-  // TODO: atom은 초기값이 0이라서 새로고침하면 장바구니에 담은 상품이 없다고 잠깐 깜빡이는 문제가 있음
-  // 따라서 이를 list를 가져오는 query의 loading state 값을 사용해 분기 처리가 필요함
+  const ItemList = () => {
+    if (isPending.state === 'loading' || cartItemList === null) {
+      return (
+        <>
+          <Title title="장바구니" />
+          <LoadingFallback />
+        </>
+      );
+    }
 
-  // 몇가지 문제가 있음
-  // 1.Suspense를 쓰기엔, CartItemList 내부에 비동기로 요청을 보내는 부분이 없음
-  // 2.useRecoilValueLoadable은 사실상 cartItemListQuery를 불러올 수 있냐는 건데
-  //   이미 위에 updateCartItemList()에서 cartItemListQuery를 불러옴 사실상 로딩을 2번 하는중
-  // * 일단 작성해 놓은 로직에서, suspendingFallback이 제대로 보이게는 처리해놓음
+    if (cartItemList.length === 0) {
+      return (
+        <>
+          <Title title="장바구니" />
+          <ErrorFallback />
+        </>
+      );
+    }
+
+    return (
+      <>
+        <Title title="장바구니" description={`현재 ${cartItemList.length}종류의 상품이 담겨있습니다.`} />
+        <Spacer height={36} />
+        <CartItemList itemList={cartItemList} />
+        <Spacer height={52} />
+        <PriceTable />
+      </>
+    );
+  };
+
   return (
     <>
       <Header />
       <CartPageContainer>
-        {isPending.state === 'loading' ?
-          <>
-            <Title title="장바구니" />
-            <LoadingFallback />
-          </>
-          :
-          cartItemList.length !== 0 ? (
-            <>
-              <Title title="장바구니" description={`현재 ${cartItemList.length}종류의 상품이 담겨있습니다.`} />
-              <Spacer height={36} />
-              <CartItemList itemList={cartItemList} />
-              <Spacer height={52} />
-              <PriceTable />
-            </>
-          ) : (
-            <>
-              <Title title="장바구니" />
-              <ErrorFallback />
-            </>
-          )
-        }
-
+        <ItemList />
       </CartPageContainer>
       <Button
         color="primary"
@@ -82,7 +73,7 @@ const CartPage = () => {
         radius={0}
         size="l"
         style={{ position: 'fixed', bottom: '0', width: 'inherit' }}
-        isDisabled={selectedItemList.length === 0 || cartItemList.length === 0}
+        isDisabled={selectedItemList.length === 0 || (cartItemList !== null && cartItemList.length === 0)}
         onClick={() => navigate('/confirm-purchase')}
       >
         주문 확인
