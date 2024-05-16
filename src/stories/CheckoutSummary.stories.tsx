a90fe38a16260dc451fec1a72a3dc35c75e0d479
river <jenn0.6n@gmail.com>
2024-05-16 10:49:04 +0900

    test: 장바구니 주문 정보 컴포넌트 storybook 추가
    Co-Authored-By: KIM DAEUN <midekuna@gmail.com>

---
+++ src/stories/CheckoutSummary.stories.tsx
@@ -0,0 +1,46 @@
+import { ThemeProvider } from "@emotion/react";
+import theme from "../styles/theme";
+
+import type { Meta, StoryObj } from "@storybook/react";
+import CheckoutSummary from "../components/CartList/CheckoutSummary";
+
+const meta: Meta<typeof CheckoutSummary> = {
+  title: "ShoppingCart/CheckoutSummary",
+  component: CheckoutSummary,
+  parameters: {
+    docs: {
+      description: {
+        component: "결제 요약 컴포넌트입니다.",
+      },
+    },
+  },
+
+  tags: ["autodocs"],
+
+  argTypes: {
+    totalPrice: {
+      description: "상품의 총 금액입니다.",
+    },
+    shippingFee: {
+      description: "배송비입니다.",
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Summary: Story = {
+  args: {
+    totalPrice: 10000,
+    shippingFee: 3000,
+  },
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={theme}>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+};
