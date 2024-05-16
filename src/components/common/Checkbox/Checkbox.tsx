4917fcdc8709ed9eec1844e63318776f73d09ae5
pakxe <pigkill40@naver.com>
2024-05-16 17:13:55 +0900

    test: CheckBox의 테스트 작성
    Co-authored-by: Todari <rhymint@gmail.com>

--- src/components/common/Checkbox/Checkbox.tsx
+++ src/components/common/Checkbox/Checkbox.tsx
@@ -3,13 +3,14 @@ import * as S from './Checkbox.style';
 import CHECK_TRUE from '../../../assets/check-true.svg';
 import CHECK_FALSE from '../../../assets/check-false.svg';
 
-interface CheckboxProp {
+export interface CheckboxProps {
   state: boolean;
   handleClick: () => void;
+  alt: string;
 }
 
-const Checkbox = ({ state, handleClick }: CheckboxProp) => {
-  return <S.Checkbox src={state ? CHECK_TRUE : CHECK_FALSE} onClick={handleClick} />;
+const Checkbox = ({ state, handleClick, alt = 'Checkbox' }: CheckboxProps) => {
+  return <S.Checkbox src={state ? CHECK_TRUE : CHECK_FALSE} onClick={handleClick} alt={alt} />;
 };
 
 export default Checkbox;
