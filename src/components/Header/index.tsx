b75738e13d42020fb3d87dedb1b0463fd6d33c1e
vi-wolhwa <cerezo00@naver.com>
2024-05-16 15:48:35 +0900

    refactor: Header Type 생성

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,29 @@
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import IMAGES from "../../assets/images/Images";
 
-const Header = () => {
-  return (
-    <HeaderContainer>
-      <HeaderTitle>SHOP</HeaderTitle>
-    </HeaderContainer>
-  );
+type HeaderType = "logo" | "goBack";
+
+const Header = ({ type = "logo" }: { type?: HeaderType }) => {
+  const router = useNavigate();
+
+  if (type === "logo") {
+    return (
+      <HeaderContainer>
+        <HeaderTitle>SHOP</HeaderTitle>
+      </HeaderContainer>
+    );
+  }
+
+  if (type === "goBack") {
+    return (
+      <HeaderContainer>
+        <GoBackButton onClick={() => router(-1)}>
+          <Img src={IMAGES.goBack} />
+        </GoBackButton>
+      </HeaderContainer>
+    );
+  }
 };
 
 export default Header;
@@ -25,4 +43,16 @@ const HeaderTitle = styled.h2`
   font-size: 20px;
   font-weight: 800;
   line-height: 16px;
+  text-align: left;
+`;
+
+const GoBackButton = styled.button`
+  background: transparent;
+  color: white;
+  padding: 0;
+`;
+
+const Img = styled.img`
+  width: 21px;
+  height: 21px;
 `;
