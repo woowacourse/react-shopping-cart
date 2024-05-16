941ff7becb5ff9926883684e5e6eae67631dc7ad
vi-wolhwa <cerezo00@naver.com>
2024-05-17 06:11:12 +0900

    feat(imgMap): Add Images and 'imgMap'

---
+++ src/f_shared/assets/imgMap.ts
@@ -0,0 +1,11 @@
+import goBackBlack from './goBack/goBackBlack.png';
+import goBackWhite from './goBack/goBackWhite.png';
+import logoBlack from './logo/logoBlack.png';
+import logoWhite from './logo/logoWhite.png';
+
+export const imgMap = {
+  logoBlack,
+  logoWhite,
+  goBackBlack,
+  goBackWhite,
+};
