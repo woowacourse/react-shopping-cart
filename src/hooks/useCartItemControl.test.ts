58f6ad78372d433f90a3590af131a7ec6a955d24
hanyoung <phanyoung@naver.com>
2024-05-16 16:56:51 +0900

    test(useCartItemControl): 미동작 테스트 케이스 삭제

--- src/hooks/useCartItemControl.test.ts
+++ src/hooks/useCartItemControl.test.ts
@@ -1,7 +1,7 @@
 import { renderHook } from "@testing-library/react";
 import { RecoilRoot, useRecoilState } from "recoil";
 import { act } from "react";
-import { removeCartItem, updateCartItemQuantity } from "../api/cartItems";
+import { updateCartItemQuantity } from "../api/cartItems";
 import { useCartItemControl } from "./useCartItemControl";
 
 jest.mock("recoil", () => ({
@@ -12,28 +12,6 @@ jest.mock("../api/cartItems");
 jest.mock("../utils/sessionStorage");
 
 describe("useCartItemControl", () => {
-  // describe("remove", () => {
-  //   const mockCartItems = [{ id: 1 }, { id: 2 }, { id: 3 }];
-  //   const mockRemovingCartItemId = 1;
-  //   const mockRemovedCartItems = [{ id: 2 }, { id: 3 }];
-
-  //   it("1. delete api 요청 2. 상태 변경", async () => {
-  //     const mockDeleteCartItemRequest = removeCartItem as jest.Mock;
-  //     const setRawCartItems = jest.fn();
-
-  //     (useRecoilState as jest.Mock).mockImplementation(() => [mockCartItems, setRawCartItems]);
-
-  //     const { result } = renderHook(() => useCartItemControl(), {
-  //       wrapper: RecoilRoot,
-  //     });
-
-  //     await act(async () => result.current.remove(mockRemovingCartItemId));
-
-  //     expect(mockDeleteCartItemRequest).toHaveBeenCalledWith(mockRemovingCartItemId);
-  //     expect(setRawCartItems).toHaveBeenCalledWith(mockRemovedCartItems);
-  //   });
-  // });
-
   describe("updateQuantity", () => {
     const mockUpdatingCartItemId = 1;
     const updatingQuantity = 2;
@@ -59,29 +37,4 @@ describe("useCartItemControl", () => {
       expect(setRawCartItems).toHaveBeenCalledWith([{ id: 1, quantity: 2 }]);
     });
   });
-
-  /**
-   * TODO: 상태 변경에 대한 테스트 케이스 작성 요망 (모킹한 setSelectedCartItemIds의 내부 callback에서 문제 발생)
-  describe("toggleSelection", () => {
-    const mockCartItemId = 1;
-    const mockSelectedCartItemIds = [1, 2];
-    const mockPutInSelectedCartItemIds = putInSelectedCartItemIds as jest.Mock;
-    const mockSetSelectedCartItemIds = jest.fn();
-
-    it("1. 스토리지 동기화 2. 상태 변경", () => {
-      (useRecoilState as jest.Mock).mockImplementation(() => [
-        mockSelectedCartItemIds,
-        mockSetSelectedCartItemIds,
-      ]);
-
-      const { result } = renderHook(() => useCartItemControl(), {
-        wrapper: RecoilRoot,
-      });
-
-      act(() => result.current.toggleSelection(mockCartItemId));
-
-      expect(mockPutInSelectedCartItemIds).toHaveBeenCalledWith([2]);
-    });
-  });
-   */
 });
