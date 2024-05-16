bf1ff4958fb1b6aa2a04bce3966cfedc382a9ccb
손진영 <sm099@naver.com>
2024-05-16 16:56:46 +0900

    test: 기능 테스트 구현
    상품 선택, 금액 계산, 수량 변경상품 제거 관련 기능 테스트 구현
    
    Co-authored-by: badahertz52 <badahertz52@gmail.com>

---
+++ __tests__/changeQuantity.test.tsx
@@ -0,0 +1,176 @@
+import { useUpdateCartItemCount } from '@hooks/shoppingCart';
+import { cartItemsSelector, selectedIdsAtom } from '@recoil/shoppingCart';
+import { renderHook, waitFor } from '@testing-library/react';
+import { RecoilRoot, useRecoilValue } from 'recoil';
+
+import { INITIAL_ITEMS, QUANTITY_TEST_ITEMS } from './constants/cartItems';
+
+describe('수량 변경 테스트', () => {
+  it('- 버튼을 누를 경우, 수량이 1 감소한다.', async () => {
+    const QUANTITY = INITIAL_ITEMS[0].quantity;
+
+    const { result } = renderHook(
+      () => {
+        const cartItems = useRecoilValue(cartItemsSelector);
+        const { updateCartItems, getNewQuantity } = useUpdateCartItemCount(cartItems[0]);
+
+        return { cartItems, updateCartItems, getNewQuantity };
+      },
+      {
+        wrapper: ({ children }) => (
+          <RecoilRoot
+            initializeState={({ set }) => {
+              set(cartItemsSelector, INITIAL_ITEMS);
+              set(
+                selectedIdsAtom,
+                INITIAL_ITEMS.map((item) => item.id),
+              );
+            }}
+          >
+            {children}
+          </RecoilRoot>
+        ),
+      },
+    );
+
+    await waitFor(() => {
+      return result.current !== undefined;
+    });
+
+    const newQuantity = result.current.getNewQuantity('minus');
+
+    await waitFor(() => {
+      result.current.updateCartItems(newQuantity);
+    });
+
+    expect(newQuantity).toBe(QUANTITY - 1);
+    expect(result.current.cartItems[0].quantity).toBe(newQuantity);
+  });
+
+  it('+ 버튼을 누를 경우, 수량이 1 증가한다.', async () => {
+    const QUANTITY = INITIAL_ITEMS[0].quantity;
+
+    const { result } = renderHook(
+      () => {
+        const cartItems = useRecoilValue(cartItemsSelector);
+        const { updateCartItems, getNewQuantity } = useUpdateCartItemCount(cartItems[0]);
+
+        return { cartItems, updateCartItems, getNewQuantity };
+      },
+      {
+        wrapper: ({ children }) => (
+          <RecoilRoot
+            initializeState={({ set }) => {
+              set(cartItemsSelector, INITIAL_ITEMS);
+              set(
+                selectedIdsAtom,
+                INITIAL_ITEMS.map((item) => item.id),
+              );
+            }}
+          >
+            {children}
+          </RecoilRoot>
+        ),
+      },
+    );
+
+    await waitFor(() => {
+      return result.current !== undefined;
+    });
+
+    const newQuantity = result.current.getNewQuantity('plus');
+
+    await waitFor(() => {
+      result.current.updateCartItems(newQuantity);
+    });
+
+    expect(newQuantity).toBe(QUANTITY + 1);
+    expect(result.current.cartItems[0].quantity).toBe(newQuantity);
+  });
+
+  describe('최저 수량, 최대 수량 테스트', () => {
+    it('수량이 1일때 - 버튼을 누를 경우, 수량이 변경되지 않는다.', async () => {
+      const QUANTITY = INITIAL_ITEMS[1].quantity;
+
+      const { result } = renderHook(
+        () => {
+          const cartItems = useRecoilValue(cartItemsSelector);
+
+          const { updateCartItems, getNewQuantity } = useUpdateCartItemCount(cartItems[1]);
+
+          return { cartItems, updateCartItems, getNewQuantity };
+        },
+        {
+          wrapper: ({ children }) => (
+            <RecoilRoot
+              initializeState={({ set }) => {
+                set(cartItemsSelector, INITIAL_ITEMS);
+                set(
+                  selectedIdsAtom,
+                  INITIAL_ITEMS.map((item) => item.id),
+                );
+              }}
+            >
+              {children}
+            </RecoilRoot>
+          ),
+        },
+      );
+
+      await waitFor(() => {
+        return result.current !== undefined;
+      });
+
+      const newQuantity = result.current.getNewQuantity('minus');
+
+      await waitFor(() => {
+        result.current.updateCartItems(newQuantity);
+      });
+
+      expect(newQuantity).toBe(QUANTITY);
+      expect(result.current.cartItems[1].quantity).toBe(newQuantity);
+    });
+
+    it('수량이 100개 일때 + 버튼을 누를 경우, 수량이 변경되지 않는다.', async () => {
+      const QUANTITY = QUANTITY_TEST_ITEMS[0].quantity;
+
+      const { result } = renderHook(
+        () => {
+          const cartItems = useRecoilValue(cartItemsSelector);
+
+          const { updateCartItems, getNewQuantity } = useUpdateCartItemCount(cartItems[0]);
+
+          return { cartItems, updateCartItems, getNewQuantity };
+        },
+        {
+          wrapper: ({ children }) => (
+            <RecoilRoot
+              initializeState={({ set }) => {
+                set(cartItemsSelector, QUANTITY_TEST_ITEMS);
+                set(
+                  selectedIdsAtom,
+                  QUANTITY_TEST_ITEMS.map((item) => item.id),
+                );
+              }}
+            >
+              {children}
+            </RecoilRoot>
+          ),
+        },
+      );
+
+      await waitFor(() => {
+        return result.current !== undefined;
+      });
+
+      const newQuantity = result.current.getNewQuantity('plus');
+
+      await waitFor(() => {
+        result.current.updateCartItems(newQuantity);
+      });
+
+      expect(newQuantity).toBe(QUANTITY);
+      expect(result.current.cartItems[0].quantity).toBe(newQuantity);
+    });
+  });
+});
