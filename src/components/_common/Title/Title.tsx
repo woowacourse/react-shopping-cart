67b3048ad2f4593529989e408fdd784962f774dc
hwinkr <dnddl8280@naver.com>
2024-05-16 16:47:38 +0900

    feat(Title): 재사용할 수 있는 페이지 제목 컴포넌트 구현
    Co-authored-by: Hyerin <ooherin@users.noreply.github.com>

---
+++ src/components/_common/Title/Title.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+interface TitleProps {
+  text: string;
+}
+
+const Title = ({ text }: TitleProps) => {
+  return <TitleText>{text}</TitleText>;
+};
+
+export default Title;
+
+const TitleText = styled.span`
+  ${({ theme }) => theme.TEXT.Title}
+`;
