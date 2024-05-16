3820f8951282d2bc7dfd840bfd5a908e8e787925
pakxe <pigkill40@naver.com>
2024-05-16 11:31:17 +0900

    feat: 아이템을 삭제하면 로컬 스토리지의 선택 아이템 리스트에서도 해당 아이템을 삭제하도록 함
    Co-authored-by: Todari <rhymint@gmail.com>

--- src/recoil/cartItemList/useCartItemList.ts
+++ src/recoil/cartItemList/useCartItemList.ts
@@ -1,10 +1,12 @@
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { cartItemListState, cartItemListQuery } from './cartItemListSelector';
 import { requestCartItemList, requestDeleteCartItem } from '../../apis/cartItemList/cartItemList';
+import { useCartItemSelectedIdList } from '../cartItem/useCartItemSelectedIdList';
 
 const useCartItemList = () => {
   const cartItemList = useRecoilValue(cartItemListState);
   const setCartItemList = useSetRecoilState(cartItemListQuery);
+  const { removeSelectedId } = useCartItemSelectedIdList();
 
   const updateCartItemList = async () => {
     const cartItemList = await requestCartItemList();
@@ -16,6 +18,7 @@ const useCartItemList = () => {
     await requestDeleteCartItem(cartItemId);
 
     setCartItemList(cartItemList.filter((item) => item.cartItemId !== cartItemId));
+    removeSelectedId(cartItemId);
   };
 
   return {
