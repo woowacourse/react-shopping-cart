d8f46623f9296af2299a7580c0e17121746e8204
Hyelim Choi <HyelimChoi01@gmail.com>
2024-05-16 15:45:51 +0900

    test: recipe selector test 추가
    Co-Authored-By: 헤인 <157036488+hain-tain@users.noreply.github.com>

---
+++ src/constants/_mock/mockFilteredCartList.ts
@@ -0,0 +1,109 @@
+// 배송비 유무 확인 목 데이터
+export const MOCK_FILTERED_CART_LIST_FREE_SHIPPING_FEE = [
+  {
+    id: 624,
+    quantity: 5,
+    isSelected: true,
+    price: 20000,
+  },
+  {
+    id: 674,
+    quantity: 3,
+    isSelected: true,
+    price: 20000,
+  },
+  {
+    id: 675,
+    quantity: 7,
+    isSelected: true,
+    price: 1000,
+  },
+  {
+    id: 676,
+    quantity: 1,
+    isSelected: true,
+    price: 2000,
+  },
+];
+
+export const MOCK_FILTERED_CART_LIST_NEED_SHIPPING_FEE = [
+  {
+    id: 624,
+    quantity: 5,
+    isSelected: false,
+    price: 20000,
+  },
+  {
+    id: 674,
+    quantity: 3,
+    isSelected: false,
+    price: 20000,
+  },
+  {
+    id: 675,
+    quantity: 7,
+    isSelected: true,
+    price: 1000,
+  },
+  {
+    id: 676,
+    quantity: 1,
+    isSelected: false,
+    price: 2000,
+  },
+];
+
+// 전체 선택 확인 목 데이터
+export const MOCK_FILTERED_CART_LIST_ALL_SELECTED = [
+  {
+    id: 624,
+    quantity: 5,
+    isSelected: true,
+    price: 20000,
+  },
+  {
+    id: 674,
+    quantity: 3,
+    isSelected: true,
+    price: 20000,
+  },
+  {
+    id: 675,
+    quantity: 7,
+    isSelected: true,
+    price: 1000,
+  },
+  {
+    id: 676,
+    quantity: 1,
+    isSelected: true,
+    price: 2000,
+  },
+];
+
+export const MOCK_FILTERED_CART_LIST_PARTLY_SELECTED = [
+  {
+    id: 624,
+    quantity: 5,
+    isSelected: false,
+    price: 20000,
+  },
+  {
+    id: 674,
+    quantity: 3,
+    isSelected: false,
+    price: 20000,
+  },
+  {
+    id: 675,
+    quantity: 7,
+    isSelected: true,
+    price: 1000,
+  },
+  {
+    id: 676,
+    quantity: 1,
+    isSelected: false,
+    price: 2000,
+  },
+];
