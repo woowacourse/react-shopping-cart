1b39561e7a2c4ea4df829d2b9dd18c3aa13c78bf
hwinkr <dnddl8280@naver.com>
2024-05-16 16:58:10 +0900

    feat(PriceInfoBox): 가격 정보 컴포넌트 구현
    Co-authored-by: Hyerin <ooherin@users.noreply.github.com>

---
+++ src/components/PriceInfoBox/PriceInfoBox.tsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+interface PriceInfoBoxProps {
+  priceLabel: string;
+  price: number;
+}
+
+const PriceInfoBox = ({ priceLabel, price }: PriceInfoBoxProps) => {
+  return (
+    <Wrapper>
+      <PriceLabel>{priceLabel}</PriceLabel>
+      <Price>{price.toLocaleString() + "원"}</Price>
+    </Wrapper>
+  );
+};
+
+export default PriceInfoBox;
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 42px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const PriceLabel = styled.span`
+  height: 16px;
+  font-size: 16px;
+  line-height: 16px;
+  font-weight: 700;
+`;
+
+const Price = styled.span`
+  height: 34px;
+  line-height: 34px;
+  font-size: 24px;
+  font-weight: 700;
+`;
