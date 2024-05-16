0666baa23ab3f440539a165a18c7d8566fe0538c
GimGayeon <kky1373@naver.com>
2024-05-16 16:20:28 +0900

    design(ShoppingCart): 장바구니 비었을 때 UI 구현

--- src/components/pages/ShoppingCart/ShoppingCart.tsx
+++ src/components/pages/ShoppingCart/ShoppingCart.tsx
@@ -8,9 +8,12 @@ import TotalPaymentInfo from '../../TotalPaymentInfo/TotalPaymentInfo';
 
 import { useRecoilValue } from 'recoil';
 import { CartItemsSelector } from '../../../recoil/cartItems';
-import { SelectedSomeCardItemsSelector } from '../../../recoil/selectedAllCardItems';
+import { SelectedSomeCardItemsSelector } from '../../../recoil/selectedCardItems';
+import { useNavigate } from 'react-router-dom';
 
 const ShoppingCart = () => {
+  const navigator = useNavigate();
+
   const cartItems = useRecoilValue(CartItemsSelector);
 
   const hasSomeCartItem = !!cartItems.length;
@@ -21,22 +24,31 @@ const ShoppingCart = () => {
   return (
     <Styled.ShoppingCart>
       <Header title="SHOP" />
+      <Styled.Container>
+        {hasSomeCartItem && (
+          <>
+            <Title
+              title="장바구니"
+              caption={`현재 ${cartItems.length}종류의 상품이 담겨있습니다.`}
+            />
+            <ItemList />
+            <TotalPaymentInfo />
+          </>
+        )}
 
-      {hasSomeCartItem && (
-        <Styled.Container>
-          <Title
-            title="장바구니"
-            caption={`현재 ${cartItems.length}종류의 상품이 담겨있습니다.`}
-          />
-          <ItemList />
-          <TotalPaymentInfo />
-        </Styled.Container>
-      )}
-
-      {!hasSomeCartItem && <Title title="장바구니" />}
-
+        {!hasSomeCartItem && (
+          <>
+            <Title title="장바구니" />
+            <Styled.Content>
+              <Styled.EmptyCartMessage>
+                장바구니에 담은 상품이 없습니다.
+              </Styled.EmptyCartMessage>
+            </Styled.Content>
+          </>
+        )}
+      </Styled.Container>
       <OrderButton
-        onClick={() => console.log('s')}
+        onClick={() => navigator('/orderConfirmation')}
         label="주문 확인"
         isOrderable={isOrderable}
       />
--- src/components/pages/ShoppingCart/style.tsx
+++ src/components/pages/ShoppingCart/style.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const ShoppingCart = styled.section`
   width: 26.875rem;
-  min-height: 58.5rem;
+  min-height: 100vh;
   position: relative;
 
   background-color: ${(props) => props.theme.color.white};
@@ -15,4 +15,20 @@ export const Container = styled.div`
 
   padding: 2.25rem 1.5rem;
   padding-bottom: 8rem;
+`;
+
+export const Content = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  width: 100%;
+
+  text-align: center;
+`;
+
+export const EmptyCartMessage = styled.p`
+  ${(props) => props.theme.typography.content};
+  color: ${(props) => props.theme.color.captionBlack};
 `;
