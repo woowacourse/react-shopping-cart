73d2b83d780927a9fb9731cad363c12b101740f3
greetings1012 <ahnqwerty@gmail.com>
2024-05-16 16:07:37 +0900

    refactor: 에러 페이지 문구 수정

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -9,12 +9,14 @@ const ErrorPage = () => {
       <Header />
       <S.Main>
         <S.ErrorTextContainer>
-          <h1>404</h1>
-          <h2>죄송합니다. 현재 찾을 수 없는 페이지를 요청하셨습니다.</h2>
+          <h1>ERROR</h1>
+          <h2>죄송합니다. 현재 요청을 처리할 수 없습니다.</h2>
           <p>
             페이지의 주소가 잘못 입력되었거나,
             <br />
-            주소가 변경 혹은 삭제되어 요청하신 페이지를 찾을 수 없습니다.
+            주소가 변경 혹은 삭제되어 요청하신 페이지를 찾을 수 없거나,
+            <br />
+            서버와의 연결 상태가 좋지 않습니다.
           </p>
         </S.ErrorTextContainer>
         <Link to={PATHS.ROOT}>
