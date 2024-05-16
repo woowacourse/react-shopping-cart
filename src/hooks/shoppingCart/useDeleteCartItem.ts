9e2ea21532f38ae7758d30fbe1fcfca433e31f00
손진영 <sm099@naver.com>
2024-05-16 13:48:57 +0900

    refactor: useDeleteCartItem 내 핸들러 함수 레이어 분리
    Co-authored-by: badahertz52 <badahertz52@gmail.com>

--- src/hooks/shoppingCart/useDeleteCartItem.ts
+++ src/hooks/shoppingCart/useDeleteCartItem.ts
@@ -6,14 +6,22 @@ const useDeleteCartItem = (id: number) => {
   const setCartItems = useSetRecoilState(cartItemsSelector);
   const setSelectedCartItemIds = useSetRecoilState(selectedIdsAtom);
 
-  const onDeleteItem = async () => {
-    await fetchDeleteCartItem(id);
-
+  const updateCartItems = () => {
     setCartItems((prevCartItems) => prevCartItems.filter((prevCartItem) => prevCartItem.id !== id));
+  };
+
+  const updateSelectedCartItemIds = () => {
     setSelectedCartItemIds((prevSelectedIds) => prevSelectedIds.filter((prevSelectedId) => prevSelectedId !== id));
   };
 
-  return { onDeleteItem };
+  const onDeleteItem = async () => {
+    await fetchDeleteCartItem(id);
+
+    updateCartItems();
+    updateSelectedCartItemIds();
+  };
+
+  return { updateCartItems, updateSelectedCartItemIds, onDeleteItem };
 };
 
 export default useDeleteCartItem;
