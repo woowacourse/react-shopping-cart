08de52770d9e594c1feb2849ffa16bcba25e1a88
Yoonkyoung Kim <dev.yoonkyoung@gmail.com>
2024-05-16 15:25:14 +0900

    feat: Cart layout 구현

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,14 @@ import CartDescription from "./CartDescription";
 import CartItemList from "./CartItemList";
 import Header from "./Header";
 import Footer from "./Footer";
-import Layout from "./Layout";
 import CartPrice from "./CartPrice";
+import OrderConfirm from "./OrderConfirm";
 
-export { CartDescription, CartItemList, Header, Footer, Layout, CartPrice };
+export {
+  CartDescription,
+  CartItemList,
+  Header,
+  Footer,
+  CartPrice,
+  OrderConfirm,
+};
---
+++ src/layouts/Cart/style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  min-height: calc(100vh - 176px);
+  padding: 24px;
+`;
+
+export const Title = styled.div`
+  font-size: var(--font-size-xl);
+  font-weight: var(--font-weight-bold);
+  line-height: 35px;
+`;
