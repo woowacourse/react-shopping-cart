3251209a13f5e3ebe4a521e7ec13472d63e1285c
GimGayeon <kky1373@naver.com>
2024-05-16 16:25:24 +0900

    feat(App): ErrorBoundary, Suspense, Routes 추가
    Co-authored-by: simorimi <simorimi@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import "./App.css";
+import { Suspense } from 'react';
+
+import ShoppingCart from './components/pages/ShoppingCart/ShoppingCart';
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorFallback from './components/ErrorFallback/ErrorFallback';
+
+import * as Styled from './components/LoadingFallback/style';
+import { Route, Routes } from 'react-router-dom';
+import OrderConfirmation from './components/pages/OrderConfirmation/OrderConfirmation';
 
 function App() {
   return (
-    <>
-      <h1>react-shopping-cart</h1>
-    </>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <Suspense
+        fallback={<Styled.LoadingMessage>Loading...</Styled.LoadingMessage>}
+      >
+        <Routes>
+          <Route path="/" element={<ShoppingCart />} />
+          <Route path="/orderConfirmation" element={<OrderConfirmation />} />
+        </Routes>
+      </Suspense>
+    </ErrorBoundary>
   );
 }
 
