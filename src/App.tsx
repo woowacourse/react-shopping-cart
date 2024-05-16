9b76b1022a3130ad32505979167cfa68bfa73f23
손진영 <sm099@naver.com>
2024-05-16 17:18:39 +0900

    refactor: App 내 suspense 제거
    Co-authored-by: badahertz52 <badahertz52@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import router from '@routes/route';
 import GlobalStyle from '@styles/globalStyle';
-import { Suspense } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
-
 function App() {
   return (
     <RecoilRoot>
       <GlobalStyle />
-      <Suspense fallback="loading...">
-        <RouterProvider router={router} />
-      </Suspense>
+      <RouterProvider router={router} />
     </RecoilRoot>
   );
 }
