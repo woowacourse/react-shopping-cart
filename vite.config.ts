37bb5c3ff8441eec0cbea0460f85a4d0d0510bb7
vi-wolhwa <cerezo00@naver.com>
2024-05-16 21:32:48 +0900

    chore: Path Alias, css 컨벤션 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+  css: {
+    modules: {
+      generateScopedName: '[name]__[local]__[hash:8]',
+      localsConvention: 'camelCaseOnly',
+    },
+  },
+});
