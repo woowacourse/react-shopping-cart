eed63b790a607560892b25d1b55ced1a7a89ba63
Seongjin Hong <hongseongjin.to@gmail.com>
2024-05-16 13:16:41 +0900

    feat: 하단 버튼 기능 구현
    Co-authored-by: ss0526100 <ss0526100@users.noreply.github.com>

--- src/components/ConfirmButton/ConfirmButton.tsx
+++ src/components/ConfirmButton/ConfirmButton.tsx
@@ -1,23 +1,8 @@
-import ENDPOINTS from '../../constants/endpoints';
 import FooterButton from '../FooterButton/FooterButton';
-import { checkedItemsState } from '../../recoil/selectors';
-import { useNavigate } from 'react-router-dom';
+import { hasCheckedItemsState } from '../../recoil/selectors';
 import { useRecoilValue } from 'recoil';
 
-export default function ConfirmButton({ ...props }) {
-  const navigate = useNavigate();
-  const hasCheckedItems = useRecoilValue(checkedItemsState).length > 0;
-
-  const handleClickConfirmButton = () => {
-    navigate(ENDPOINTS.confirmOrder, { state: { isSubmitted: true } });
-  };
-
-  return (
-    <FooterButton
-      {...props}
-      buttonText="주문 확인"
-      disabled={!hasCheckedItems}
-      onClick={handleClickConfirmButton}
-    />
-  );
+export default function ConfirmButton() {
+  const hasCheckedItems = useRecoilValue(hasCheckedItemsState);
+  return <FooterButton buttonText="주문 확인" disabled={!hasCheckedItems} />;
 }
