54da1046b11a6ca0e245a87ddd0abfd875191d2e
pakxe <pigkill40@naver.com>
2024-05-18 02:44:25 +0900

    refactor: 리코일에서만 쓰이는 이펙트 함수를 리코일 폴더 내부로 이전

--- src/recoil/effects/localStorageEffect.ts
+++ src/recoil/effects/localStorageEffect.ts
@@ -4,6 +4,8 @@ export const localStorageEffect =
     const savedValue = localStorage.getItem(key);
     if (savedValue != null) {
       setSelf(JSON.parse(savedValue));
+    } else {
+      setSelf(JSON.parse('[]')); // TODO: effect있을 수 있음
     }
 
     onSet((newValue: any, _: any, isReset: boolean) => {
