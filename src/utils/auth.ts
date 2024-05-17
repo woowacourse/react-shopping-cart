c46b32e0dff95132e3f169382b0cfff37ff180ee
greetings1012 <ahnqwerty@gmail.com>
2024-05-17 12:19:17 +0900

    refactor: 주석 추가, export문 형식 변경

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,4 +1,7 @@
-export function generateBasicToken(userId: string, userPassword: string): string {
+function generateBasicToken(userId: string, userPassword: string): string {
   const token = btoa(`${userId}:${userPassword}`);
   return `Basic ${token}`;
 }
+// 서버 통신 인증에 필요한 ID와 PASSWORD를 Basic 인증 조건에 맞게 토큰 생성
+
+export default generateBasicToken;
