f63d68e5622279d41baa91ce977d728296f16416
greetings1012 <ahnqwerty@gmail.com>
2024-05-17 15:24:44 +0900

    feat: UNKNOWN 메시지 추가

--- src/constants/MESSAGES.ts
+++ src/constants/MESSAGES.ts
@@ -3,4 +3,5 @@ export const CART_ITEM_ERROR_MESSAGE = {
   ADD: '상품을 장바구니에 추가하는 중 문제가 발생했습니다.',
   REMOVE: '상품을 장바구니에서 제거하는 중 문제가 발생했습니다.',
   UPDATE: '상품 개수를 업데이트하는 중 문제가 발생했습니다.',
+  UNKNOWN: '알 수 없는 오류가 발생했습니다.',
 };
