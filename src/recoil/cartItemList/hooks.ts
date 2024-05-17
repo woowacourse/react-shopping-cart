2a160952732d8009ff1a8fcb288fdd7bdf48274d
pakxe <pigkill40@naver.com>
2024-05-18 03:30:57 +0900

    feat: 장바구니 아이템을 삭제하면 선택 목록에서도 제거되도록 구현

--- src/recoil/cartItemList/hooks.ts
+++ src/recoil/cartItemList/hooks.ts
@@ -1,10 +1,12 @@
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { requestCartItemList, requestDeleteCartItem } from '../../apis/cartItemList/cartItemList';
 import { cartItemListAtom, cartItemListQuery } from './states';
+import { selectedCartItemIdListAtom } from '../selectedCartItemIdList/states';
 
-const useCartItemList = () => {
+export const useCartItemList = () => {
   const setCartItemList = useSetRecoilState(cartItemListQuery);
   const cartItemList = useRecoilValue(cartItemListAtom);
+  const setSelectedCartItemIdList = useSetRecoilState(selectedCartItemIdListAtom);
 
   const updateCartItemList = async () => {
     const updatedCartItemList = await requestCartItemList();
@@ -16,10 +18,10 @@ const useCartItemList = () => {
     await requestDeleteCartItem(cartItemId);
 
     const newCartItemList = cartItemList.filter((cartItem) => cartItem.cartItemId !== cartItemId);
+
+    setSelectedCartItemIdList((prev) => prev.filter((id) => id !== cartItemId)); // 아이템을 삭제하면 선택 목록에서도 삭제되는 사이드 이펙트
     setCartItemList(newCartItemList);
   };
 
   return { updateCartItemList, deleteCartItem };
 };
-
-export default useCartItemList;
