4f1f13945234fd54bc455d788769865a394e82de
pakxe <pigkill40@naver.com>
2024-05-18 03:06:53 +0900

    chore: 아톰의 key 변경

--- src/recoil/selectedCartItemIdList/states.ts
+++ src/recoil/selectedCartItemIdList/states.ts
@@ -4,7 +4,7 @@ import { localStorageEffect } from '../effects/localStorageEffect';
 const SELECTED_CART_ITEM_ID_LOCAL_STORAGE_KEY = 'selectedCartItemIdList';
 
 export const selectedCartItemIdListAtom = atom<number[]>({
-  key: 'selectedCartItemIdList',
+  key: 'selectedCartItemIdListAtom',
   default: [],
   effects: [localStorageEffect(SELECTED_CART_ITEM_ID_LOCAL_STORAGE_KEY)],
   // TODO: 값이 없으면 기존 모든 항목들을 전체선택하도록(아니면 페이지 로딩됐을 때 내가 전체선택 로직을 수행하던가)
