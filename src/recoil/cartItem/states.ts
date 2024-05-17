a1f4517b177ba1c8ed4a49f18105ba80ae089971
pakxe <pigkill40@naver.com>
2024-05-18 02:54:03 +0900

    feat: 현재 장바구니 아이템의 총 수량을 위한 selector 구현

--- src/recoil/cartItem/states.ts
+++ src/recoil/cartItem/states.ts
@@ -1,6 +1,22 @@
-import { atomFamily } from 'recoil';
+import { atomFamily, selector } from 'recoil';
+import { selectedCartItemIdListAtom } from '../selectedCartItemIdList/states';
 
-export const cartItemQuantityAtom = atomFamily<number, number>({
-  key: 'cartItemQuantity',
+export const cartItemQuantityAtomFamily = atomFamily<number, number>({
+  key: 'cartItemQuantityAtomFamily',
   default: 0,
+});
+
+export const totalCartItemQuantitySelector = selector<number>({
+  key: 'totalCartItemQuantitySelector',
+  get: ({ get }) => {
+    const selectedCartItemIdList = get(selectedCartItemIdListAtom);
+
+    const totalQuantity = selectedCartItemIdList.reduce((totalQuantity, selectedCartItemId) => {
+      const quantity = get(cartItemQuantityAtomFamily(selectedCartItemId));
+
+      return totalQuantity + quantity;
+    }, 0);
+
+    return totalQuantity;
+  },
 });
