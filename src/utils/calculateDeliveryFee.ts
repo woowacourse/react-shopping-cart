fc6f5448bef50d243e56870cbc4bee43883000b4
anttiey <hanuuny@gmail.com>
2024-05-17 14:44:10 +0900

    refactor(calculateDeliveryFee): 과도한 삼항 연산자 제거

--- src/utils/calculateDeliveryFee.ts
+++ src/utils/calculateDeliveryFee.ts
@@ -2,5 +2,6 @@ const DELIVERY_FEE = 3000;
 export const DELIVERY_FEE_DISCOUNT_THRESHOLD = 100000;
 
 export const calculateDeliveryFee = (cartItemTotalPrice: number) => {
-  return cartItemTotalPrice === 0 || cartItemTotalPrice >= DELIVERY_FEE_DISCOUNT_THRESHOLD ? 0 : DELIVERY_FEE;
+  if (cartItemTotalPrice === 0) return 0;
+  return cartItemTotalPrice >= DELIVERY_FEE_DISCOUNT_THRESHOLD ? 0 : DELIVERY_FEE;
 };
