10b3335802b49c527dadad136f743aa19b7b2e40
badahertz52 <badahertz52@gmail.com>
2024-05-17 16:58:32 +0900

    fix : 아이템 삭제 시 404 오류 해결
     - RouterProvider를 Suspense로 감싸는 것으로 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { LoadingSpinner } from '@components/common';
 import router from '@routes/route';
 import GlobalStyle from '@styles/globalStyle';
+import { Suspense } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 function App() {
   return (
     <RecoilRoot>
       <GlobalStyle />
-      <RouterProvider router={router} />
+      <Suspense fallback={<LoadingSpinner $width="100vw" $height="100vh" />}>
+        <RouterProvider router={router} />
+      </Suspense>
     </RecoilRoot>
   );
 }
