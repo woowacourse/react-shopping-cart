8192c8c7caa8652e42c564f8da81506de88adc64
ooherin <555ohr@naver.com>
2024-05-18 10:41:07 +0900

    chore: 배포 재실행

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import * as S from "./MainLayout.style";
 import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
@@ -12,29 +12,3 @@ const MainLayout = () => {
 };
 
 export default MainLayout;
-
-const Wrapper = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #888;
-`;
-
-const LayoutWrapper = styled.div`
-  width: 430px;
-  height: calc(100vh - 64px);
-  min-height: 100%;
-  margin-top: 64px;
-  position: relative;
-
-  overflow-y: scroll;
-
-  background-color: white;
-  color: black;
-`;
-
-const S = {
-  Wrapper,
-  LayoutWrapper,
-};
