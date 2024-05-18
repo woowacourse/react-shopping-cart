53b972acabc51540cac604da5de3b0d9ecf020e2
Kim Da Eun <midekuna@gmail.com>
2024-05-18 22:39:22 +0900

    chore: 불필요한 타입 선언 제거

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,29 +1,29 @@
-import { CartItemType } from "../types";
+import { CartItemType } from '../types';
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 const USERNAME = import.meta.env.VITE_USERNAME;
 const PASSWORD = import.meta.env.VITE_PASSWORD;
 
-function generateBasicToken(userId: string, userPassword: string): string {
+function generateBasicToken(userId: string, userPassword: string) {
   const token = btoa(`${userId}:${userPassword}`);
   return `Basic ${token}`;
 }
 
 const HEADERS = {
   Authorization: generateBasicToken(USERNAME, PASSWORD),
-  "Content-Type": "application/json",
+  'Content-Type': 'application/json',
 };
 
 const CART_ITEMS_API_URL = `${BASE_URL}/cart-items`;
 
 export const fetchCartItems = async (): Promise<CartItemType[]> => {
   const response = await fetch(`${CART_ITEMS_API_URL}`, {
-    method: "GET",
+    method: 'GET',
     headers: HEADERS,
   });
 
   if (!response.ok) {
-    throw new Error("Failed to fetch cart items");
+    throw new Error('Failed to fetch cart items');
   }
 
   const data = await response.json();
@@ -31,27 +31,27 @@ export const fetchCartItems = async (): Promise<CartItemType[]> => {
 };
 
 export const updateCartItemQuantity = async (
-  id: CartItemType["id"],
-  quantity: CartItemType["quantity"]
+  id: CartItemType['id'],
+  quantity: CartItemType['quantity']
 ) => {
   const response = await fetch(`${BASE_URL}/cart-items/${id}`, {
-    method: "PATCH",
+    method: 'PATCH',
     headers: HEADERS,
     body: JSON.stringify({ quantity }),
   });
 
   if (!response.ok) {
-    throw new Error("Failed to update cart item quantity");
+    throw new Error('Failed to update cart item quantity');
   }
 };
 
-export const deleteCartItem = async (id: CartItemType["id"]) => {
+export const deleteCartItem = async (id: CartItemType['id']) => {
   const response = await fetch(`${BASE_URL}/cart-items/${id}`, {
-    method: "DELETE",
+    method: 'DELETE',
     headers: HEADERS,
   });
 
   if (!response.ok) {
-    throw new Error("Failed to delete cart item");
+    throw new Error('Failed to delete cart item');
   }
 };
