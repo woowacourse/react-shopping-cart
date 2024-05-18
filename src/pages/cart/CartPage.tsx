02a2d193e7bac680c19f6cd078be0aa0e66057cd
brgndy <brgndy96@gmail.com>
2024-05-18 18:25:25 +0900

    refactor: CartPage 컴포넌트 내부 컴포넌트 분리

--- src/pages/cart/CartPage.tsx
+++ src/pages/cart/CartPage.tsx
@@ -1,42 +1,32 @@
-import Header from '../../components/Header/Header';
-import Button from '../../components/common/Button';
 import { useRecoilValue } from 'recoil';
 import CartList from './components/CartList';
 import CartTitle from './components/CartTitle';
 import CartTotals from './components/CartTotals';
-import useCartNavigate from '../../hooks/useCartNavigate';
-import useCheckoutNavigate from '../../hooks/useCheckoutNavigate';
+import CartPageHeader from './components/CartPageHeader';
+import { allCartItemStates } from '@/store/atoms';
+import OrderButton from './components/OrderButton';
 
 import styles from './Cart.module.css';
-import { productsState } from '../../store/atoms';
 
 export default function CartPage() {
-  const products = useRecoilValue(productsState);
-  const { handleFooterButtonClick } = useCheckoutNavigate();
-  const { handleHomeButtonClick } = useCartNavigate();
+  const cartItems = useRecoilValue(allCartItemStates);
+  const isExistingCartItem = cartItems.length > 0;
+
+  console.log(cartItems);
 
   return (
     <>
-      <Header>
-        <Button variant="header" onClick={handleHomeButtonClick}>
-          SHOP
-        </Button>
-      </Header>
-
-      {products.length === 0 ? (
-        <div className={styles.noneProductContainer}>장바구니에 담은 상품이 없습니다.</div>
+      <CartPageHeader />
+      {isExistingCartItem ? (
+        <div className={styles.cartBodyWrapper}>
+          <CartTitle cartItemsCount={cartItems.length} />
+          <CartList cartItems={cartItems} />
+          <CartTotals />
+        </div>
       ) : (
-        <>
-          <div className={styles.cartBodyWrapper}>
-            <CartTitle productsCount={products.length} />
-            <CartList products={products} />
-            <CartTotals />
-          </div>
-        </>
+        <div className={styles.noneProductContainer}>장바구니에 담은 상품이 없습니다.</div>
       )}
-      <Button onClick={handleFooterButtonClick} variant="footer" disabled={products.length === 0}>
-        주문 하기
-      </Button>
+      <OrderButton />
     </>
   );
 }
