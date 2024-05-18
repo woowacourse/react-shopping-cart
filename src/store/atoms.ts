e79f393eabe7261315e16df4b5f0d9619312b445
brgndy <brgndy96@gmail.com>
2024-05-18 17:50:24 +0900

    refactor: 베이스 아톰 재설정

--- src/store/atoms.ts
+++ src/store/atoms.ts
@@ -1,21 +1,15 @@
-import { atom, atomFamily, selector } from 'recoil';
+import formatCartItems from '@/services/formatCartItem';
+import { atom, selector } from 'recoil';
 import { fetchCartItems } from '../api';
 
-export const productsState = atom({
-  key: 'productsState',
+export const allCartItemStates = atom({
+  key: 'allCartItemStates',
   default: selector({
-    key: 'productsState/Default',
+    key: 'allCartItemStates/Default',
     get: async () => {
-      const products = await fetchCartItems();
-      return products;
+      const cartItems = await fetchCartItems();
+      const formattedProducts = formatCartItems(cartItems);
+      return formattedProducts;
     },
   }),
-});
-
-export const isCheckedState = atomFamily<boolean, number>({
-  key: 'isCheckedState',
-  default: (id: number) => {
-    const item = window.localStorage.getItem(JSON.stringify(id));
-    return item ? JSON.parse(item) : true;
-  },
 });
