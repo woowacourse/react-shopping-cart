bcea6def711f19f54cfe4be6d0b4ec3adc614f83
brgndy <brgndy96@gmail.com>
2024-05-18 18:13:10 +0900

    refactor: 각 페이지 헤더 컴포넌트 wrapping

---
+++ src/pages/checkout/components/CheckoutPageHeader.tsx
@@ -0,0 +1,21 @@
+import Button from '@/components/common/Button';
+import Header from '@/components/Header/Header';
+import ROUTES from '@/constants/routes';
+import { useNavigate } from 'react-router-dom';
+import BackIcon from '../../../asset/back.png';
+
+export default function CheckoutPageHeader() {
+  const navigate = useNavigate();
+
+  const handleBackButtonClick = () => {
+    navigate(ROUTES.CART);
+  };
+
+  return (
+    <Header>
+      <Button onClick={handleBackButtonClick}>
+        <img src={BackIcon} width={32} height={32} alt="back-icon" />
+      </Button>
+    </Header>
+  );
+}
