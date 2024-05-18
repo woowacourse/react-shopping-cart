5b28bae6a10139441d46f8292afc1c0c7643596b
김진호 <rlawlsgh1227@gmail.com>
2024-05-18 14:32:11 +0900

    refactor: pagination page, size 옵션으로 바깥에서 넣어줄 수 있도록 변경

--- src/api/get/getCartItems.ts
+++ src/api/get/getCartItems.ts
@@ -3,12 +3,14 @@ import axiosInstance from '../../utils/axios';
 import { URLS } from '../../constants/constants';
 import { CartItem } from '../../types/cartItem';
 
-const getCartItems = async () => {
+interface PaginationParams {
+  page: number;
+  size: number;
+}
+
+const getCartItems = async (options: PaginationParams = { page: 0, size: 20 }) => {
   const response = await axiosInstance.get<Pageable<CartItem>>(URLS.CART_ITEMS, {
-    params: {
-      page: 0,
-      size: 20,
-    },
+    params: options,
   });
 
   return response.data.content;
