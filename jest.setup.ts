9d3ad6c568b7571b2ba65fa8d6491dfa58ba75bd
ooherin <555ohr@naver.com>
2024-05-18 09:52:10 +0900

    style: 불필요한 코드 삭제

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,8 +1,1 @@
 import "@testing-library/jest-dom";
-// import * as dotenv from "dotenv";
-
-// dotenv.config({ path: "<rootDir>/jest.setup.ts" });
-
-// process.env.VITE_API_URL = "http://localhost:3000";
-// process.env.VITE_USER_ID = "test-user";
-// process.env.VITE_USER_PASSWORD = "test-password";
