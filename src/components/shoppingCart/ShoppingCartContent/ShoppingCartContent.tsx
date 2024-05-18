f7effd3297ca25fd4303379c3fdd56f9bc12783f
손진영 <sm099@naver.com>
2024-05-18 16:32:18 +0900

    refactor(ShoppingCartContent): 무료 배송 가격 상수화
    하드 코딩 된 무료 배송 가격 상수화

--- src/components/shoppingCart/ShoppingCartContent/ShoppingCartContent.tsx
+++ src/components/shoppingCart/ShoppingCartContent/ShoppingCartContent.tsx
@@ -1,6 +1,8 @@
 import { UpsideDownExclamation } from '@assets/index';
 import { CartItemCount, CartList, OrderPrice } from '@components/shoppingCart';
+import { PRICE } from '@constants/shippingCart';
 import { cartItemsSelector } from '@recoil/shoppingCart';
+import { formatKoreanCurrency } from '@utils/currency';
 import { useRecoilValue } from 'recoil';
 
 import * as Styled from './ShoppingCardContent.styled';
@@ -22,7 +24,9 @@ const ShoppingCartContent = () => {
           </div>
           <Styled.CartInfoBanner>
             <UpsideDownExclamation />
-            <Styled.CartInfoBannerText>총 주문 금액이 100,000원 이상일 경우 무료 배송됩니다.</Styled.CartInfoBannerText>
+            <Styled.CartInfoBannerText>
+              총 주문 금액이 {formatKoreanCurrency(PRICE.freeShippingMinAmount)} 이상일 경우 무료 배송됩니다.
+            </Styled.CartInfoBannerText>
           </Styled.CartInfoBanner>
           <OrderPrice />
         </>
