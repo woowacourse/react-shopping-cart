d7d7727f4c8a6e1073a2920b62aae5890dc5c9b2
김진호 <rlawlsgh1227@gmail.com>
2024-05-18 15:20:43 +0900

    fix: kindCount가 0 초과일 때 보이도록 설정

--- src/components/ShoppingCartDescription/ShoppingCartDescription.tsx
+++ src/components/ShoppingCartDescription/ShoppingCartDescription.tsx
@@ -8,7 +8,7 @@ const ShoppingCartDescription = ({ kindCount }: ShoppingCartDescriptionProps) =>
   return (
     <S.Container>
       <S.Title>장바구니</S.Title>
-      {kindCount !== 0 && (
+      {kindCount > 0 && (
         <S.Description>{`현재 ${kindCount}종류의 상품이 담겨있습니다.`}</S.Description>
       )}
     </S.Container>
