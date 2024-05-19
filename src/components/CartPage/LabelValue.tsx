0c5e7f836aac21b4e1467c79fc2f19b77585b69a
jinsil <jinsil378900@gmail.com>
2024-05-19 16:51:49 +0900

    refactor(LabelValue): 불필요한 코드 삭제

--- src/components/CartPage/LabelValue.tsx
+++ src/components/CartPage/LabelValue.tsx
@@ -1,6 +1,4 @@
 import { css } from "@emotion/css";
-import { useRecoilState } from "recoil";
-import { cartItemsAtom } from "../../recoil/atom/atom";
 
 interface LabelValueProps {
   label: string;
@@ -8,15 +6,10 @@ interface LabelValueProps {
 }
 
 const LabelValue = ({ label, value }: LabelValueProps) => {
-  useRecoilState(cartItemsAtom);
   return (
     <div className={containerCSS}>
-      <div>
-        <p className={labelCSS}>{label}</p>
-      </div>
-      <div>
-        <p className={valueCSS}>{value}</p>
-      </div>
+      <p className={labelCSS}>{label}</p>
+      <p className={valueCSS}>{value}</p>
     </div>
   );
 };
@@ -25,19 +18,17 @@ export default LabelValue;
 
 const containerCSS = css`
   display: flex;
-  width: 100%;
-  height: 42px;
   justify-content: space-between;
   align-items: center;
+  width: 100%;
+  height: 42px;
 `;
 
 const labelCSS = css`
   font: var(--cart-subtitle);
   color: var(--grey-400);
-  text-align: left;
 `;
 
 const valueCSS = css`
   font: var(--cart-title);
-  text-align: right;
 `;
