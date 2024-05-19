4667d279de633e13511bd7d8a5361830e0e8b820
pakxe <pigkill40@naver.com>
2024-05-19 21:45:42 +0900

    feat: CheckBox를 img태그가 아닌 input 태그를 사용하도록 수정

--- src/components/common/Checkbox/Checkbox.style.ts
+++ src/components/common/Checkbox/Checkbox.style.ts
@@ -1,6 +1,21 @@
 import styled from 'styled-components';
+import CHECKED from '../../../assets/check-true.svg';
+import NOT_CHECKED from '../../../assets/check-false.svg';
 
-export const Checkbox = styled.img`
+export const Checkbox = styled.input`
   height: 24px;
   width: 24px;
+  background-size: contain;
+  background-repeat: no-repeat;
+  cursor: pointer;
+  appearance: none;
+  margin: 0;
+
+  &:checked {
+    background-image: url(${CHECKED});
+  }
+
+  &:not(:checked) {
+    background-image: url(${NOT_CHECKED});
+  }
 `;
--- src/components/common/Checkbox/Checkbox.tsx
+++ src/components/common/Checkbox/Checkbox.tsx
@@ -1,16 +1,13 @@
 import * as S from './Checkbox.style';
 
-import CHECK_TRUE from '../../../assets/check-true.svg';
-import CHECK_FALSE from '../../../assets/check-false.svg';
-
 export interface CheckboxProps {
-  state: boolean;
+  checked: boolean;
   handleClick: () => void;
   alt?: string;
 }
 
-const Checkbox = ({ state, handleClick, alt = 'Checkbox' }: CheckboxProps) => {
-  return <S.Checkbox src={state ? CHECK_TRUE : CHECK_FALSE} onClick={handleClick} alt={alt} />;
+const Checkbox = ({ checked, handleClick, alt = 'Checkbox' }: CheckboxProps) => {
+  return <S.Checkbox checked={checked} onClick={handleClick} alt={alt} type="checkbox" />;
 };
 
 export default Checkbox;
