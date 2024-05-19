d8664ad450b8e5440ca7478f52f5da1649fcc078
hwinkr <dnddl8280@naver.com>
2024-05-19 10:31:15 +0900

    chore: S -> Styled 스타일 객체 네이밍 수정, import 순서 수정

---
+++ src/components/ProductList/ProductList.style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const ListWrapper = styled.div`
+  margin-bottom: 52px;
+`;
+
+const Styled = {
+  ListWrapper,
+};
+
+export default Styled;
--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,28 +1,21 @@
-import styled from "styled-components";
-
 import { useRecoilValue } from "recoil";
+import { cartItems } from "@/recoil/cartItems";
 
 import ProductItem from "../ProductItem/ProductItem";
-import { cartItems } from "@/recoil/cartItems";
+
+import Styled from "./ProductList.style";
 
 const ProductList = () => {
   const cartItemList = useRecoilValue(cartItems);
 
   return (
-    <S.ListWrapper>
-      {cartItemList.map((item) => (
-        <ProductItem key={item.product.id} item={item} />
-      ))}
-    </S.ListWrapper>
+    <Styled.ListWrapper>
+      {cartItemList &&
+        cartItemList.map((item) => (
+          <ProductItem key={item.product.id} item={item} />
+        ))}
+    </Styled.ListWrapper>
   );
 };
 
 export default ProductList;
-
-const ListWrapper = styled.div`
-  margin-bottom: 52px;
-`;
-
-const S = {
-  ListWrapper,
-};
