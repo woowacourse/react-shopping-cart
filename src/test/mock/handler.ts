b311aa17af855ad1dbb7bb5689f18d217afff860
lurgi <wahoo9040@naver.com>
2024-05-19 14:36:46 +0900

    chore: 더미 파일 수정

--- src/test/mock/handler.ts
+++ src/test/mock/handler.ts
@@ -1,8 +1,8 @@
 import { HttpResponse, http } from "msw";
-import { dummy } from "./dummy";
+import { freeShippingDummy } from "./dummy";
 
 export const handlers = [
   http.get(`${import.meta.env.VITE_API_BASE_URL}/cart-items`, () => {
-    return HttpResponse.json(dummy);
+    return HttpResponse.json(freeShippingDummy);
   }),
 ];
