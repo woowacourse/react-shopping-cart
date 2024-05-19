2456c37a53c54a0eb786661ba65f2362312d4190
hwinkr <dnddl8280@naver.com>
2024-05-19 10:34:01 +0900

    chore: S -> Styled 스타일 객체 네이밍 수정, import 순서 수정, 함수 네이밍 수정

--- src/pages/CartPage/CartPage.style.ts
+++ src/pages/CartPage/CartPage.style.ts
@@ -1,33 +1,54 @@
 import styled from "styled-components";
 
-export const CartHeaderTitle = styled.span`
+const CartHeaderTitle = styled.span`
   ${({ theme }) => theme.TEXT.Title};
 `;
 
-export const CartPageLayout = styled.div`
-  padding: 36px 24px 0px 24px;
+const CartPageLayout = styled.div`
   height: calc(100vh - 128px);
+  padding: 36px 24px 0px 24px;
 
   overflow-y: scroll;
 `;
 
-export const CheckBoxWrapper = styled.div`
+const CheckBoxWrapper = styled.div`
   display: flex;
   align-items: center;
+
   gap: 8px;
+  margin-top: 36px;
   border-bottom: 1px;
 `;
 
-export const ButtonText = styled.span`
+const ButtonText = styled.span`
   font-size: 16px;
   line-height: 16px;
   font-weight: 700;
 `;
 
-export const OrderConfirmButton = styled.div`
+const OrderConfirmButton = styled.div`
   position: fixed;
   bottom: 0;
   width: 100%;
   max-width: 430px;
   margin: 0;
 `;
+
+const EmptyCartWrapper = styled.div`
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Styled = {
+  CartHeaderTitle,
+  CartPageLayout,
+  CheckBoxWrapper,
+  ButtonText,
+  OrderConfirmButton,
+  EmptyCartWrapper,
+};
+
+export default Styled;
--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,59 +1,61 @@
 import React, { Suspense } from "react";
 import { useNavigate } from "react-router-dom";
+
 import { useRecoilValue } from "recoil";
 import { totalItemLengthSelector } from "@/recoil/orderInformation";
 
 import useSelectAll from "@/hooks/useSelectAll";
 
+import Header from "@/components/_common/Header/Header";
 import Caption from "@/components/_common/Caption/Caption";
 import CheckBox from "@/components/_common/CheckBox/CheckBox";
 import TitleSet from "@/components/_common/TitleSet/TitleSet";
-import Button from "@/components/_common/Button/Button";
 
-import Header from "@/components/layout/Header/Header";
-
+import OrderConfirmButton from "@/components/OrderConfirmButton/OrderConfirmButton";
 import PriceSection from "@/components/PriceSection/PriceSection";
+
 import MoreInfo from "@/assets/more-info.svg?react";
-import CartEmpty from "@/components/CartEmpty/CartEmpty";
 
 import { PAGE_URL } from "@/constants/url";
 
-import * as S from "./CartPage.style";
+import Styled from "./CartPage.style";
 
 const ProductList = React.lazy(
   () => import("../../components/ProductList/ProductList")
 );
 
 const CartPage = () => {
+  const navigate = useNavigate();
+  const routerToOrderConfirmPage = () => {
+    navigate(PAGE_URL.orderConfirm);
+  };
+
   const totalItemLength = useRecoilValue(totalItemLengthSelector);
   const { isAllItemSelected, selectAllItem, unselectAllItem } = useSelectAll();
 
-  const navigate = useNavigate();
-
-  const onMoveOrderConfirmPage = () => {
-    navigate(PAGE_URL.orderConfirm);
-  };
+  const isEmptyCart = totalItemLength === 0;
 
   return (
     <>
       <Header>
-        <S.CartHeaderTitle>SHOP</S.CartHeaderTitle>
+        <Styled.CartHeaderTitle>SHOP</Styled.CartHeaderTitle>
       </Header>
-      {totalItemLength ? (
-        <>
-          <S.CartPageLayout>
+
+      <Styled.CartPageLayout>
+        {!isEmptyCart ? (
+          <>
             <TitleSet
               title="장바구니"
               subTitle={`현재 ${totalItemLength}종류의 상품이 담겨있습니다.`}
             />
 
-            <S.CheckBoxWrapper>
+            <Styled.CheckBoxWrapper>
               <CheckBox
                 isChecked={isAllItemSelected}
                 onClick={isAllItemSelected ? unselectAllItem : selectAllItem}
               />
               <Caption text="전체선택" />
-            </S.CheckBoxWrapper>
+            </Styled.CheckBoxWrapper>
 
             <Suspense fallback={<div>loading....</div>}>
               <ProductList />
@@ -63,23 +65,22 @@ const CartPage = () => {
               asset={() => <MoreInfo />}
               text="총 주문 금액이 100,00원 이상일 경우 무료 배송됩니다."
             />
-
             <PriceSection />
-          </S.CartPageLayout>
-          <S.OrderConfirmButton onClick={onMoveOrderConfirmPage}>
-            <Button width="full" size="xLarge" theme="dark">
-              <S.ButtonText>주문 확인</S.ButtonText>
-            </Button>
-          </S.OrderConfirmButton>
-        </>
-      ) : (
-        <>
-          <S.CartPageLayout>
+          </>
+        ) : (
+          <>
             <TitleSet title="장바구니" />
-            <CartEmpty />
-          </S.CartPageLayout>
-        </>
-      )}
+            <Styled.EmptyCartWrapper>
+              <Caption text="장바구니에 담은 상품이 없습니다."></Caption>
+            </Styled.EmptyCartWrapper>
+          </>
+        )}
+      </Styled.CartPageLayout>
+
+      <OrderConfirmButton
+        onClick={routerToOrderConfirmPage}
+        disabled={isEmptyCart}
+      />
     </>
   );
 };
