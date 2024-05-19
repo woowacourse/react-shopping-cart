f94c6dd28f7b383bbca45c0f21b4453bd7b936af
GimGayeon <kky1373@naver.com>
2024-05-19 21:28:35 +0900

    feat: 라우팅 상수화

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import { Suspense } from 'react';
-
 import ShoppingCart from './components/pages/ShoppingCart/ShoppingCart';
 import { ErrorBoundary } from 'react-error-boundary';
 import ErrorFallback from './components/ErrorFallback/ErrorFallback';
 
-import * as Styled from './components/LoadingFallback/style';
 import { Route, Routes } from 'react-router-dom';
 import OrderConfirmation from './components/pages/OrderConfirmation/OrderConfirmation';
+import { ROUTE } from './constant/route';
 
 function App() {
   return (
     <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <Suspense
-        fallback={<Styled.LoadingMessage>Loading...</Styled.LoadingMessage>}
-      >
-        <Routes>
-          <Route path="/" element={<ShoppingCart />} />
-          <Route path="/orderConfirmation" element={<OrderConfirmation />} />
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path={ROUTE.cart.path} element={<ShoppingCart />} />
+        <Route
+          path={ROUTE.orderConfirmation.path}
+          element={<OrderConfirmation />}
+        />
+      </Routes>
     </ErrorBoundary>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,23 @@
+import { useLocation } from 'react-router-dom';
 import * as Styled from './style';
 
 interface HeaderProps {
-  title: string;
   onClick?: () => void;
 }
 
-const Header = ({ title, onClick }: HeaderProps) => {
+const Header = ({ onClick }: HeaderProps) => {
+  const location = useLocation();
+
+  const getHeaderTitle = (path: any) => {
+    if (path === '/') return 'SHOP';
+    if (path === '/orderConfirmation') return '⬅';
+    return 'none';
+  };
+
   return (
     <Styled.Header>
       <Styled.AppTitle onClick={() => onClick && onClick()}>
-        {title}
+        {getHeaderTitle(location.pathname)}
       </Styled.AppTitle>
     </Styled.Header>
   );
