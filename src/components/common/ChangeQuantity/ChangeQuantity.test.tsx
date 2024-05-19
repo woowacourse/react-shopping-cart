e3423fde0f90383f25ba4498bfa4f654297901bf
이태훈 <rhymint@gmail.com>
2024-05-20 04:13:44 +0900

    test: ChangeQuantity.test 코드 수정

--- src/components/common/ChangeQuantity/ChangeQuantity.test.tsx
+++ src/components/common/ChangeQuantity/ChangeQuantity.test.tsx
@@ -7,24 +7,7 @@ jest.mock('../../../assets/plus.svg', () => 'plus.svg');
 jest.mock('../../../assets/minus.svg', () => 'minus.svg');
 
 describe('ChangeQuantity 컴포넌트', () => {
-  test('초기 수량을 올바르게 렌더링한다', () => {
-    render(<ChangeQuantity quantity={5} decreaseQuantity={jest.fn()} increaseQuantity={jest.fn()} />);
-    const quantityElement = screen.getByText('5');
-    expect(quantityElement).toBeInTheDocument();
-  });
-
-  test('수량 감소 버튼이 제대로 작동하고 비활성화 상태를 확인한다', () => {
-    const decreaseQuantity = jest.fn();
-    render(<ChangeQuantity quantity={1} decreaseQuantity={decreaseQuantity} increaseQuantity={jest.fn()} />);
-
-    const decreaseButton = screen.getByLabelText('minus');
-    expect(decreaseButton).toBeDisabled();
-
-    fireEvent.click(decreaseButton);
-    expect(decreaseQuantity).not.toHaveBeenCalled();
-  });
-
-  test('수량 증가 버튼이 제대로 작동한다', () => {
+  test('수량 증가 버튼을 눌렀을 때, 수량이 1 증가해야 한다', () => {
     const increaseQuantity = jest.fn();
     render(<ChangeQuantity quantity={5} decreaseQuantity={jest.fn()} increaseQuantity={increaseQuantity} />);
 
@@ -35,7 +18,7 @@ describe('ChangeQuantity 컴포넌트', () => {
     expect(increaseQuantity).toHaveBeenCalledTimes(1);
   });
 
-  test('수량 감소 버튼이 제대로 작동한다', () => {
+  test('수량 감소 버튼을 눌렀을 때, 수량이 1 감소해야 한다', () => {
     const decreaseQuantity = jest.fn();
     render(<ChangeQuantity quantity={5} decreaseQuantity={decreaseQuantity} increaseQuantity={jest.fn()} />);
 
@@ -46,14 +29,14 @@ describe('ChangeQuantity 컴포넌트', () => {
     expect(decreaseQuantity).toHaveBeenCalledTimes(1);
   });
 
-  test('수량이 1일 때 수량 감소 버튼이 비활성화 상태임을 확인한다', () => {
+  test('수량이 1일 때 수량 감소 버튼이 비활성화 상태여야 한다', () => {
     render(<ChangeQuantity quantity={1} decreaseQuantity={jest.fn()} increaseQuantity={jest.fn()} />);
 
     const decreaseButton = screen.getByLabelText('minus');
     expect(decreaseButton).toBeDisabled();
   });
 
-  test('수량이 1보다 클 때 수량 감소 버튼이 활성화 상태임을 확인한다', () => {
+  test('수량이 1보다 클 때 수량 감소 버튼이 활성화 상태여야 한다', () => {
     render(<ChangeQuantity quantity={2} decreaseQuantity={jest.fn()} increaseQuantity={jest.fn()} />);
 
     const decreaseButton = screen.getByLabelText('minus');
