f72bc439a74ddbd280d66158cb25cce583eadc9c
hwinkr <dnddl8280@naver.com>
2024-05-19 10:33:18 +0900

    feat(useUpdateQuantity): 상품 수량 변경을 책임지는 커스텀 훅 리팩터링
    - 로딩 상태 추가, 수량 변경 요청에 대한 서버의 응답이 올 때까지 로딩 UI 를 구성하기 위함

--- src/hooks/useUpdateItemQuantity.ts
+++ src/hooks/useUpdateItemQuantity.ts
@@ -1,34 +1,46 @@
+import { useState } from "react";
+
 import { useRecoilState } from "recoil";
+import { cartItemQuantity } from "@/recoil/cartItemQuantity";
 
 import { patchCartItemQuantity } from "@/apis";
-import { cartItemQuantity } from "@/recoil/cartItemQuantity";
 
 const useUpdateItemQuantity = (id: number) => {
   const [quantity, setQuantity] = useRecoilState(cartItemQuantity(id));
+  const [isUpdateLoading, setIsUpdateLoading] = useState(false);
 
   const handleIncreaseQuantity = async () => {
+    setIsUpdateLoading(true);
+
     const canUpdateCardItemQuantity = await patchCartItemQuantity(
       id,
       quantity + 1
     );
 
+    setIsUpdateLoading(false);
     if (canUpdateCardItemQuantity) {
       setQuantity((prevQuantity) => prevQuantity + 1);
     }
   };
 
   const handleDecreaseQuantity = async () => {
+    if (quantity === 1) return;
+
+    setIsUpdateLoading(true);
+
     const canUpdateCardItemQuantity = await patchCartItemQuantity(
       id,
       quantity - 1
     );
 
+    setIsUpdateLoading(false);
     if (canUpdateCardItemQuantity) {
-      setQuantity((prevQuantity) => Math.max(1, prevQuantity - 1));
+      setQuantity((prevQuantity) => prevQuantity - 1);
     }
   };
 
   return {
+    isUpdateLoading,
     quantity,
     handleIncreaseQuantity,
     handleDecreaseQuantity,
