a3aa14c27acf4463653eb503ada554912e9da296
vi-wolhwa <cerezo00@naver.com>
2024-05-20 02:48:06 +0900

    refactor: API 함수 리팩토링

--- src/api/cartItem.ts
+++ src/api/cartItem.ts
@@ -2,47 +2,33 @@ import SERVER_URL from "../config/serverUrl";
 import { CartItemResponse } from "../types/ShoppingCart";
 import { generateBasicToken } from "../utils/auth";
 
-export async function fetchCartItems(): Promise<CartItemResponse[]> {
+async function requestServer<T = void>(path: string, method: string, bodyData?: any): Promise<T> {
   const token = generateBasicToken(SERVER_URL.userId, SERVER_URL.userPassword);
-  const response = await fetch(`${SERVER_URL.apiUrl}/cart-items`, {
-    method: "GET",
-    headers: { Authorization: token },
-  });
+  const headers = {
+    Authorization: token,
+    "Content-Type": "application/json",
+  };
+
+  const options: RequestInit = { method, headers, ...(bodyData && { body: JSON.stringify(bodyData) }) };
+
+  const response = await fetch(`${SERVER_URL.apiUrl}${path}`, options);
 
   if (!response.ok) {
-    throw new Error("Failed to fetch cart items");
+    throw new Error("Failed to fetch data");
   }
 
-  const data = await response.json();
+  return method !== "DELETE" ? response.json() : null;
+}
+
+export async function fetchCartItems(): Promise<CartItemResponse[]> {
+  const data = await requestServer<{ content: CartItemResponse[] }>("/cart-items", "GET");
   return data.content;
 }
 
 export async function patchCartItemQuantity(itemId: number, quantity: number): Promise<void> {
-  const token = generateBasicToken(SERVER_URL.userId, SERVER_URL.userPassword);
-  const bodyData = {
-    quantity,
-  };
-
-  const response = await fetch(`${SERVER_URL.apiUrl}/cart-items/${itemId}`, {
-    method: "PATCH",
-    headers: { Authorization: token, "Content-Type": "application/json" },
-    body: JSON.stringify(bodyData),
-  });
-
-  if (!response.ok) {
-    throw new Error("Failed to fetch cart items");
-  }
+  await requestServer(`/cart-items/${itemId}`, "PATCH", { quantity });
 }
 
 export async function deleteCartItem(itemId: number): Promise<void> {
-  const token = generateBasicToken(SERVER_URL.userId, SERVER_URL.userPassword);
-
-  const response = await fetch(`${SERVER_URL.apiUrl}/cart-items/${itemId}`, {
-    method: "DELETE",
-    headers: { Authorization: token, "Content-Type": "application/json" },
-  });
-
-  if (!response.ok) {
-    throw new Error("Failed to fetch cart items");
-  }
+  await requestServer(`/cart-items/${itemId}`, "DELETE");
 }
