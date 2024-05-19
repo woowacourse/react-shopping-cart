6d3272bd9229d3a1e934f140f34aa12e8cf6f715
00kang <kdb070970@gmail.com>
2024-05-19 22:29:09 +0900

    feat : 수정된 selector 에 맞는 테스트 케이스 작성

--- src/recoil/selector/selector.test.ts
+++ src/recoil/selector/selector.test.ts
@@ -1,19 +1,13 @@
 import { act, renderHook } from "@testing-library/react";
 import { RecoilRoot, useRecoilValue, useSetRecoilState } from "recoil";
+import { DELIVERY } from "../../constants";
 import { mockCartItemsData } from "../../mocks/mockCartItemsData";
 import { cartItemsState } from "../atoms/atoms";
-import { uniqueItemCountState, deliveryPriceState, orderPriceState } from "./selector";
+import { cartSummarySelectorState, uniqueItemCountState } from "./selector";
 
 describe("초기값 테스트", () => {
-  it("상품 종류 수량 초기값은 0이다.", () => {
+  it("uniqueItemCountState(상품 종류 수량)의 초기값은 0이야 합니다.", () => {
     const { result } = renderHook(() => useRecoilValue(uniqueItemCountState), {
-      wrapper: RecoilRoot,
-    });
-
-    expect(result.current).toBe(0);
-  });
-  it("주문 금액 초기값은 0이다.", () => {
-    const { result } = renderHook(() => useRecoilValue(orderPriceState), {
       wrapper: RecoilRoot,
     });
 
@@ -22,12 +16,12 @@ describe("초기값 테스트", () => {
 });
 
 describe("mockData를 이용한 테스트", () => {
-  it("주문 금액이 올바르게 계산되는지 확인한다.", () => {
+  it("cartSummarySelectorState에 mockData를 로드하고, 금액이 올바르게 계산되는지 확인한다.", () => {
     const { result } = renderHook(
       () => {
         const setCartItems = useSetRecoilState(cartItemsState);
-        const orderPrice = useRecoilValue(orderPriceState);
-        return { setCartItems, orderPrice };
+        const cartSummary = useRecoilValue(cartSummarySelectorState);
+        return { setCartItems, cartSummary };
       },
       {
         wrapper: RecoilRoot,
@@ -35,63 +29,15 @@ describe("mockData를 이용한 테스트", () => {
     );
 
     act(() => {
-      result.current.setCartItems(
-        mockCartItemsData.content.map((item) => ({
-          id: item.id,
-          product: {
-            id: item.product.id,
-            name: item.product.name,
-            price: item.product.price,
-            imageUrl: item.product.imageUrl,
-            category: item.product.category,
-          },
-          quantity: item.quantity,
-        }))
-      );
+      result.current.setCartItems(mockCartItemsData.content);
     });
 
-    const expectedOrderPrice = mockCartItemsData.content.reduce((total, item) => {
-      return total + item.product.price * item.quantity;
-    }, 0);
+    const expectedOrderPrice = 23000;
+    const expectedDeliveryPrice = DELIVERY.STANDARD;
+    const expectedTotalPrice = expectedOrderPrice + expectedDeliveryPrice;
 
-    expect(expectedOrderPrice).toBe(23000);
-  });
-
-  it("주문 금액이 10만원 미만이면 배송비 3천원이 올바르게 계산되는지 확인한다.", () => {
-    const { result } = renderHook(
-      () => {
-        const setCartItems = useSetRecoilState(cartItemsState);
-        const orderPrice = useRecoilValue(orderPriceState);
-        const deliveryPrcie = useRecoilValue(deliveryPriceState);
-        return { setCartItems, orderPrice, deliveryPrcie };
-      },
-      {
-        wrapper: RecoilRoot,
-      }
-    );
-
-    act(() => {
-      result.current.setCartItems(
-        mockCartItemsData.content.map((item) => ({
-          id: item.id,
-          product: {
-            id: item.product.id,
-            name: item.product.name,
-            price: item.product.price,
-            imageUrl: item.product.imageUrl,
-            category: item.product.category,
-          },
-          quantity: item.quantity,
-        }))
-      );
-    });
-
-    const expectedOrderPrice = mockCartItemsData.content.reduce((total, item) => {
-      return total + item.product.price * item.quantity;
-    }, 0);
-
-    const expectedDeliveryPrice = expectedOrderPrice > 100000 ? 0 : 3000;
-
-    expect(expectedDeliveryPrice).toBe(3000);
+    expect(result.current.cartSummary.orderPrice).toBe(expectedOrderPrice);
+    expect(result.current.cartSummary.deliveryPrice).toBe(expectedDeliveryPrice);
+    expect(result.current.cartSummary.totalPrice).toBe(expectedTotalPrice);
   });
 });
