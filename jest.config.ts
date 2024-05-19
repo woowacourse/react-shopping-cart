419ebe95810384f6de16abfb7bce6b6b1adfc4c5
Largopie <106071687+Largopie@users.noreply.github.com>
2024-05-20 02:03:22 +0900

    chore: jest 설정 관련 라이브러리 설치

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,29 @@
 export default {
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        diagnostics: {
+          ignoreCodes: [1343],
+        },
+        astTransformers: {
+          before: [
+            {
+              path: 'node_modules/ts-jest-mock-import-meta',
+              options: {
+                metaObjectReplacement: {
+                  env: { VITE_API_URL: 'empty', VITE_USERNAME: 'empty', VITE_PASSWORD: 'empty' },
+                },
+              },
+            },
+          ],
+        },
+      },
+    ],
+    '^.+\\.svg$': 'jest-transformer-svg',
   },
   moduleNameMapper: {
-    '^.+\\.svg$': 'jest-svg-transformer',
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
     '@/(.*)': '<rootDir>/src/$1',
     '@components/(.*)': '<rootDir>/src/components/$1',
