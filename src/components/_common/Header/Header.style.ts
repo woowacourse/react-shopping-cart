4d3a4a93738dda2a3ad79d6f815a8c0c3cb87f6c
hwinkr <dnddl8280@naver.com>
2024-05-19 10:24:57 +0900

    chore: S -> Styled 스타일 객체 네이밍 수정

---
+++ src/components/_common/Header/Header.style.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+const Header = styled.header`
+  width: 100%;
+  height: 64px;
+  max-width: 430px;
+  position: fixed;
+  top: 0;
+
+  padding: 24px;
+  display: flex;
+  align-items: center;
+
+  background-color: #000000;
+  color: #ffffff;
+
+  box-sizing: border-box;
+`;
+
+const Styled = {
+  Header,
+};
+
+export default Styled;
