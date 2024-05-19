3db6823f72cc93f9ff5f9c3efe5ea98960f1cf2f
pakxe <pigkill40@naver.com>
2024-05-19 20:43:26 +0900

    storybook: 스토리북 세팅 파일 작성

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,14 +8,10 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
-    '@storybook/addon-themes',
   ],
   framework: {
     name: '@storybook/react-vite',
     options: {},
-  },
-  docs: {
-    autodocs: 'tag',
   },
 };
 export default config;
