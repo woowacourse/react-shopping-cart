c43b87bf9f4d9542f598548a779e18a9f5d0d687
김진호 <rlawlsgh1227@gmail.com>
2024-05-19 14:19:49 +0900

    fix: 체크박스 스토리북 props 변경으로 인한 수정

--- src/components/Checkbox/Checkbox.stories.tsx
+++ src/components/Checkbox/Checkbox.stories.tsx
@@ -12,14 +12,16 @@ type Story = StoryObj<typeof meta>;
 
 export const 기본_체크박스: Story = {
   args: {
+    id: 1,
     isChecked: false,
-    onClick: () => console.log('sdf'),
+    onChange: () => console.log('sdf'),
   },
 };
 
 export const 선택된_체크박스: Story = {
   args: {
+    id: 2,
     isChecked: true,
-    onClick: () => console.log('sdf'),
+    onChange: () => console.log('sdf'),
   },
 };
