38260d85b5e7ece753a4734c1b1fa6ff9d474f8f
badahertz52 <badahertz52@gmail.com>
2024-05-19 11:16:40 +0900

    feat: ErrorFallback에 새로고침 버튼 추가

--- src/components/common/ErrorFallback/ErrorFallback.tsx
+++ src/components/common/ErrorFallback/ErrorFallback.tsx
@@ -1,11 +1,21 @@
+import { BasicButton } from '@components/common';
+import { useNavigate } from 'react-router-dom';
+
 import * as Styled from './ErrorFallback.styled';
 
 interface ErrorFallbackProps {
   error: Error;
   $height: string;
+  reload?: boolean;
 }
 
-function ErrorFallback({ error, $height }: ErrorFallbackProps) {
+function ErrorFallback({ error, $height, reload }: ErrorFallbackProps) {
+  const navigate = useNavigate();
+
+  const handleRefresh = () => {
+    navigate(0);
+  };
+
   return (
     <Styled.ErrorPageContents $height={$height}>
       <Styled.ErrorPageHeader>⚠️ 오류</Styled.ErrorPageHeader>
@@ -13,6 +23,11 @@ function ErrorFallback({ error, $height }: ErrorFallbackProps) {
       <Styled.ErrorPageText>
         <i>오류: {error.message}</i>
       </Styled.ErrorPageText>
+      {reload && (
+        <BasicButton onClick={handleRefresh} style={{ marginTop: '16px' }}>
+          새로고침
+        </BasicButton>
+      )}
     </Styled.ErrorPageContents>
   );
 }
