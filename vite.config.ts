69ed0f3a0d88aa1f6d82442eae2c364f249fa242
river <jenn0.6n@gmail.com>
2024-05-19 19:02:40 +0900

    refactor: web3 alias 제거

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,4 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      web3: "web3/dist/web3.min.js",
-    },
-  },
 });
