d448f5cbfe30a1dc553fc00146f407db14a46b75
00kang <kdb070970@gmail.com>
2024-05-20 20:15:21 +0900

    refactor : CheckedButtonIcon과 UncheckedButtonIcon 이미지 해상도 높이기

--- src/components/button/checkboxButton/CheckboxButton.styled.ts
+++ src/components/button/checkboxButton/CheckboxButton.styled.ts
@@ -3,6 +3,8 @@ import styled from "styled-components";
 export const StyledCheckboxButton = styled.button`
   background-color: transparent;
   border: none;
+  padding: 0;
+  margin: 0;
 
   cursor: pointer;
 
