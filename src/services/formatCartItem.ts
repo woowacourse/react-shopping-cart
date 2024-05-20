51d135d3a64a3e06ce157f127452e334313a6b8d
brgndy <brgndy96@gmail.com>
2024-05-20 20:43:11 +0900

    refactor: 로컬스토리지 객체 명 수정

--- src/services/formatCartItem.ts
+++ src/services/formatCartItem.ts
@@ -1,8 +1,8 @@
 import { CartItemData } from '@/types';
-import LocalStorage from './LocalStorage';
+import CheckedCartItemStorage from './CheckedProductStorage';
 
 const formatCartItems = (allCartItems: CartItemData[]): CartItemData[] => {
-  const checkedProductIds = new Set(LocalStorage.getCheckedProductIds());
+  const checkedProductIds = new Set(CheckedCartItemStorage.getCheckedProductIds());
 
   return allCartItems.map((cartItem) => {
     const isChecked = checkedProductIds.has(cartItem.id);
