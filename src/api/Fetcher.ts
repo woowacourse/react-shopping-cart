50965934f757fe68ce0a730a199a8ae8aa820ba3
brgndy <brgndy96@gmail.com>
2024-05-20 20:42:24 +0900

    refactor: 비동기 Fetcher 객체 수정 및 api 콜 수정

--- src/api/Fetcher.ts
+++ src/api/Fetcher.ts
@@ -3,20 +3,16 @@ import HttpError from '@/error/HttpError';
 
 type FetchOption = {
   method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
-  body: BodyInit | null;
-  headers: HeadersInit;
+  body?: BodyInit | null;
+  headers?: HeadersInit;
 };
 
 const Fetcher = {
-  sendRequest: async <T>(
-    url: string,
-    { method = 'GET', body = null, headers = {} }: Partial<FetchOption>,
-  ): Promise<T> => {
+  get: async <T>(url: string, { headers = {} }: Partial<FetchOption> = {}): Promise<T> => {
     try {
       const res = await fetch(url, {
-        method: method,
-        body,
-        headers: headers,
+        method: 'GET',
+        headers,
       });
 
       if (!res.ok) {
@@ -24,13 +20,75 @@ const Fetcher = {
         throw error;
       }
 
-      const text = await res.text();
-      if (!text) {
-        return {} as T;
-      }
-
-      const data: T = JSON.parse(text);
+      const data = await res.json();
       return data;
+    } catch (err) {
+      throw err;
+    }
+  },
+
+  post: async (url: string, { body, headers = {} }: Partial<FetchOption>) => {
+    try {
+      const res = await fetch(url, {
+        method: 'POST',
+        body,
+        headers,
+      });
+
+      if (!res.ok) {
+        const error = new HttpError(ERROR_MESSAGE.FAIL_FETCH, res.status);
+        throw error;
+      }
+    } catch (err) {
+      throw err;
+    }
+  },
+
+  put: async (url: string, { body, headers = {} }: Partial<FetchOption>) => {
+    try {
+      const res = await fetch(url, {
+        method: 'PUT',
+        body,
+        headers,
+      });
+
+      if (!res.ok) {
+        const error = new HttpError(ERROR_MESSAGE.FAIL_FETCH, res.status);
+        throw error;
+      }
+    } catch (err) {
+      throw err;
+    }
+  },
+
+  patch: async (url: string, { body, headers = {} }: Partial<FetchOption>) => {
+    try {
+      const res = await fetch(url, {
+        method: 'PATCH',
+        body,
+        headers,
+      });
+
+      if (!res.ok) {
+        const error = new HttpError(ERROR_MESSAGE.FAIL_FETCH, res.status);
+        throw error;
+      }
+    } catch (err) {
+      throw err;
+    }
+  },
+
+  delete: async (url: string, { headers = {} }: Partial<FetchOption> = {}) => {
+    try {
+      const res = await fetch(url, {
+        method: 'DELETE',
+        headers,
+      });
+
+      if (!res.ok) {
+        const error = new HttpError(ERROR_MESSAGE.FAIL_FETCH, res.status);
+        throw error;
+      }
     } catch (err) {
       throw err;
     }
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,17 @@
 import { generateBasicToken } from './utils/auth';
 import { CartItemData } from '@/types';
 import Fetcher from './Fetcher';
+import { API_ROUTES } from '@/constants/routes';
+import { ERROR_MESSAGE } from '@/constants/messages';
+import HttpError from '@/error/HttpError';
 
-const API_URL = import.meta.env.VITE_API_URL;
-const USER_ID = import.meta.env.VITE_USER_ID;
-const USER_PASSWORD = import.meta.env.VITE_USER_PASSWORD;
+// const API_URL = import.meta.env.VITE_API_URL;
+// const USER_ID = import.meta.env.VITE_USER_ID;
+// const USER_PASSWORD = import.meta.env.VITE_USER_PASSWORD;
+
+const API_URL = process.env.API_URL;
+const USER_ID = process.env.USER_ID;
+const USER_PASSWORD = process.env.USER_PASSWORD;
 
 interface CartItemsResponse {
   content: CartItemData[];
@@ -13,8 +20,7 @@ interface CartItemsResponse {
 export async function fetchCartItems(): Promise<CartItemData[]> {
   const token = generateBasicToken(USER_ID, USER_PASSWORD);
 
-  const data = await Fetcher.sendRequest<CartItemsResponse>(`${API_URL}/cart-items`, {
-    method: 'GET',
+  const data = await Fetcher.get<CartItemsResponse>(`${API_URL}${API_ROUTES.CART}`, {
     headers: { Authorization: token },
   });
 
@@ -24,24 +30,34 @@ export async function fetchCartItems(): Promise<CartItemData[]> {
 export async function updateCartItemQuantity(id: number, newQuantity: number) {
   const token = generateBasicToken(USER_ID, USER_PASSWORD);
 
-  await Fetcher.sendRequest(`${API_URL}/cart-items/${id}`, {
-    method: 'PATCH',
-    headers: { Authorization: token, 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      quantity: newQuantity,
-    }),
-  });
-
-  return { success: true };
+  try {
+    await Fetcher.patch(`${API_URL}${API_ROUTES.CART}/${id}`, {
+      headers: { Authorization: token, 'Content-Type': 'application/json' },
+      body: JSON.stringify({ quantity: newQuantity }),
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new HttpError(`${ERROR_MESSAGE.FAIL_UPDATE_QUANTITY} ${error.message}`, 400);
+    }
+  }
 }
 
 export async function deleteCartItem(id: number) {
   const token = generateBasicToken(USER_ID, USER_PASSWORD);
 
-  await Fetcher.sendRequest(`${API_URL}/cart-items/${id}`, {
-    method: 'DELETE',
-    headers: { Authorization: token, 'Content-Type': 'application/json' },
-  });
+  try {
+    await Fetcher.delete(`${API_URL}${API_ROUTES.CART}/${id}`, {
+      headers: { Authorization: token, 'Content-Type': 'application/json' },
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new HttpError(`${ERROR_MESSAGE.FAIL_DELETE_ITEM}${error.message}`, 400);
+    }
+  }
+}
 
-  return { success: true };
+export async function getAllCoupons() {
+  const data = Fetcher.get(`${API_URL}/coupons`);
+
+  return data;
 }
