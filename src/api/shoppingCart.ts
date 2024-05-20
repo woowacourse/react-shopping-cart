01d577c0462db26fc0c98f8b41905ca4d2f14f2f
simhorim <hori5020@gmail.com>
2024-05-20 19:31:48 +0900

    refactor(shoppingCart): 불 필요한 API 제거

--- src/api/shoppingCart.ts
+++ src/api/shoppingCart.ts
@@ -17,37 +17,3 @@ export const fetchCartItems = async (): Promise<CartItem[]> => {
   const data = await response.json();
   return data.content;
 };
-
-export const removeCartItem = async (cartItemId: number): Promise<void> => {
-  const token = generateBasicToken(USER_ID, USER_PASSWORD);
-
-  const response = await fetch(`${BASE_URL}/cart-items/${cartItemId}`, {
-    method: 'DELETE',
-    headers: { Authorization: token },
-  });
-
-  if (!response.ok) {
-    throw new Error('장바구니 아이템 삭제를 실패했습니다.');
-  }
-};
-
-export const adjustCartItemQuantity = async (
-  cartItemId: number,
-  quantity: number,
-): Promise<void> => {
-  const token = generateBasicToken(USER_ID, USER_PASSWORD);
-
-  const response = await fetch(`${BASE_URL}/cart-items/${cartItemId}`, {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: token,
-    },
-
-    body: JSON.stringify({ quantity }),
-  });
-
-  if (!response.ok) {
-    throw new Error('장바구니 아이템 수령 변경을 실패했습니다.');
-  }
-};
