1c78e097a207406bae43acf8cb296554e1cb4262
Soyeon Choe <soy2302ten@gmail.com>
2024-05-20 16:02:46 +0900

    deploy: vite.config.ts의 base 추가

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+  plugins: [react()],
+  base: '/react-shopping-cart/dist',
 });
