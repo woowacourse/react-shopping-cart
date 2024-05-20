86be41fa981de16aa30383aad35778ca070253f6
Largopie <106071687+Largopie@users.noreply.github.com>
2024-05-20 16:37:56 +0900

    test: act 및 초기화 로직 추가

--- src/recoil/cartItems/cartItems.test.tsx
+++ src/recoil/cartItems/cartItems.test.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom';
-import { render, waitFor, screen, fireEvent } from '@testing-library/react';
-import { Suspense } from 'react';
+import { render, waitFor, screen, fireEvent, cleanup } from '@testing-library/react';
+import { Suspense, act } from 'react';
 import { RecoilRoot } from 'recoil';
 
 import { TOTAL_PRICE_OVER_100000_DATA, TOTAL_PRICE_UNDER_100000_DATA } from '@/constants/mock';
@@ -13,35 +13,47 @@ jest.mock('@apis/cartItem');
 const mockFetchCartItems = fetchCartItems as jest.Mock;
 
 describe('cartItems 동작 테스트', () => {
-  it('cartItem 리스트는 "나이키", "퓨마"가 렌더링된다.', async () => {
-    mockFetchCartItems.mockReturnValueOnce(TOTAL_PRICE_UNDER_100000_DATA);
+  beforeEach(() => {
+    mockFetchCartItems.mockReset();
+  });
 
-    render(
-      <RecoilRoot>
-        <Suspense fallback={<Loading />}>
-          <CartProducts />
-        </Suspense>
-      </RecoilRoot>,
-    );
+  afterEach(() => {
+    cleanup();
+  });
+
+  it('cartItem 리스트는 "나이키", "아디다스"가 렌더링된다.', async () => {
+    mockFetchCartItems.mockResolvedValueOnce(TOTAL_PRICE_UNDER_100000_DATA);
+
+    act(() => {
+      render(
+        <RecoilRoot>
+          <Suspense fallback={<Loading />}>
+            <CartProducts />
+          </Suspense>
+        </RecoilRoot>,
+      );
+    });
 
     await waitFor(() => {
       expect(screen.getByText('나이키')).toBeInTheDocument();
-      expect(screen.getByText('퓨마')).toBeInTheDocument();
+      expect(screen.getByText('아디다스')).toBeInTheDocument();
     });
   });
 
   it('"퓨마" 상품의 체크박스를 클릭했을 때, 체크가 해제된다.', async () => {
-    mockFetchCartItems.mockReturnValueOnce(TOTAL_PRICE_OVER_100000_DATA);
-
     const PUMA = '퓨마';
 
-    render(
-      <RecoilRoot>
-        <Suspense fallback={<Loading />}>
-          <CartProducts />
-        </Suspense>
-      </RecoilRoot>,
-    );
+    act(() => {
+      mockFetchCartItems.mockResolvedValueOnce(TOTAL_PRICE_OVER_100000_DATA);
+
+      render(
+        <RecoilRoot>
+          <Suspense fallback={<Loading />}>
+            <CartProducts />
+          </Suspense>
+        </RecoilRoot>,
+      );
+    });
 
     await waitFor(() => {
       const checkbox = screen.getByLabelText(PUMA + 'checkbox', { selector: 'input' });
@@ -53,57 +65,63 @@ describe('cartItems 동작 테스트', () => {
   });
 
   it('"퓨마" 상품의 +버튼을 클릭했을 때, 수량이 1개 늘어난다.', async () => {
-    mockFetchCartItems.mockReturnValueOnce(TOTAL_PRICE_OVER_100000_DATA);
+    mockFetchCartItems.mockResolvedValueOnce(TOTAL_PRICE_OVER_100000_DATA);
 
     const PUMA = '퓨마';
 
-    render(
-      <RecoilRoot>
-        <Suspense fallback={<Loading />}>
-          <CartProducts />
-        </Suspense>
-      </RecoilRoot>,
-    );
+    act(() => {
+      render(
+        <RecoilRoot>
+          <Suspense fallback={<Loading />}>
+            <CartProducts />
+          </Suspense>
+        </RecoilRoot>,
+      );
+    });
 
     await waitFor(() => {
       fireEvent.click(screen.getByLabelText(PUMA + 'plus-button', { selector: 'button' }));
 
-      expect(screen.getByText('11')).toBeInTheDocument();
+      expect(screen.getByTestId(PUMA + 'quantity')).toHaveTextContent('11');
     });
   });
 
   it('"퓨마" 상품의 -버튼을 클릭했을 때, 수량이 1개 줄어든다.', async () => {
-    mockFetchCartItems.mockReturnValueOnce(TOTAL_PRICE_OVER_100000_DATA);
+    mockFetchCartItems.mockResolvedValueOnce(TOTAL_PRICE_OVER_100000_DATA);
 
     const PUMA = '퓨마';
 
-    render(
-      <RecoilRoot>
-        <Suspense fallback={<Loading />}>
-          <CartProducts />
-        </Suspense>
-      </RecoilRoot>,
-    );
+    act(() => {
+      render(
+        <RecoilRoot>
+          <Suspense fallback={<Loading />}>
+            <CartProducts />
+          </Suspense>
+        </RecoilRoot>,
+      );
+    });
 
     await waitFor(() => {
       fireEvent.click(screen.getByLabelText(PUMA + 'minus-button', { selector: 'button' }));
 
-      expect(screen.getByText('9')).toBeInTheDocument();
+      expect(screen.getByTestId(PUMA + 'quantity')).toHaveTextContent('8');
     });
   });
 
   it('"퓨마" 상품의 delete버튼을 클릭했을 때, 상품이 삭제된다.', async () => {
-    mockFetchCartItems.mockReturnValueOnce(TOTAL_PRICE_OVER_100000_DATA);
+    mockFetchCartItems.mockResolvedValueOnce(TOTAL_PRICE_OVER_100000_DATA);
 
     const PUMA = '퓨마';
 
-    render(
-      <RecoilRoot>
-        <Suspense fallback={<Loading />}>
-          <CartProducts />
-        </Suspense>
-      </RecoilRoot>,
-    );
+    act(() => {
+      render(
+        <RecoilRoot>
+          <Suspense fallback={<Loading />}>
+            <CartProducts />
+          </Suspense>
+        </RecoilRoot>,
+      );
+    });
 
     await waitFor(() => {
       fireEvent.click(screen.getByLabelText(PUMA + 'delete-button', { selector: 'button' }));
