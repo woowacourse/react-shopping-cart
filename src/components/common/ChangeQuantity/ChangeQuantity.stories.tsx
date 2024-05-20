0e776d5fae29868ab145ac8568cdcfb3209c4cd4
pakxe <pigkill40@naver.com>
2024-05-21 02:55:19 +0900

    storybook: 오류나는 스토리북 코드 수정

--- src/components/common/ChangeQuantity/ChangeQuantity.stories.tsx
+++ src/components/common/ChangeQuantity/ChangeQuantity.stories.tsx
@@ -2,41 +2,40 @@ import type { Meta, StoryObj } from '@storybook/react';
 import ChangeQuantity from './ChangeQuantity';
 import { useState } from 'react';
 
-const meta = {
+const meta: Meta<typeof ChangeQuantity> = {
   title: 'Components/ChangeQuantity',
   component: ChangeQuantity,
   tags: ['autodocs'],
   argTypes: {
     quantity: {
-      description: '',
+      description: 'The current quantity',
       control: { type: 'number' },
     },
     decreaseQuantity: {
-      description: '',
-      // control: { type: 'number' }
+      description: 'Function to decrease the quantity',
     },
     increaseQuantity: {
-      description: '',
-      // control: { type: 'number' }
+      description: 'Function to increase the quantity',
     },
   },
-} satisfies Meta<typeof ChangeQuantity>;
+};
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
+const PlaygroundComponent = () => {
+  const [value, setValue] = useState(0);
+
+  return (
+    <ChangeQuantity
+      quantity={value}
+      decreaseQuantity={() => setValue((prev) => Math.max(prev - 1, 0))}
+      increaseQuantity={() => setValue((prev) => prev + 1)}
+    />
+  );
+};
+
 export const Playground: Story = {
-  decorators: [
-    () => {
-      const [value, setValue] = useState(0);
-      return (
-        <ChangeQuantity
-          quantity={value}
-          decreaseQuantity={() => setValue((prev) => Math.max(prev - 1, 0))}
-          increaseQuantity={() => setValue((prev) => prev + 1)}
-        />
-      );
-    },
-  ],
+  render: () => <PlaygroundComponent />,
 };
