9fc66a815b1ff0f9a292b0ae2527de962a1f8df8
Largopie <106071687+Largopie@users.noreply.github.com>
2024-05-20 20:26:49 +0900

    fix: 미사용 로직 제거

--- src/components/common/Error.tsx
+++ src/components/common/Error.tsx
@@ -1,19 +1,13 @@
 import { css } from '@emotion/react';
-// import { useNavigate } from 'react-router-dom';
 
 import { WARNING } from '@assets/images';
 import FooterButton from '@components/common/FooterButton';
-// import Button from '@components/common/Button';
 
 interface ErrorProps {
   errorMessage: string;
 }
 
 export default function Error({ errorMessage }: ErrorProps) {
-  // const navigate = useNavigate();
-
-  // const goHome = () => navigate('/');
-
   return (
     <>
       <main css={errorMain}>
@@ -28,10 +22,6 @@ export default function Error({ errorMessage }: ErrorProps) {
           <span></span>
           <span css={errorText}>({errorMessage})</span>
         </div>
-        {/* 해당 페이지에서 오류가 발생했기 때문에 페이지 이동이 안됨 페이지 이동이 안되는 현상 */}
-        {/* <Button onClick={goHome} css={cartButton} id="cart-button">
-          장바구니로 돌아가기
-        </Button> */}
       </main>
       <FooterButton id="error-footer" isDisabled={true} />
     </>
@@ -68,25 +58,4 @@ const errorInfoText = css`
   font-size: 18px;
   font-weight: 600;
   text-align: center;
-`;
-
-const cartButton = css`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 200px;
-
-  padding: 12px 8px;
-
-  color: #fff;
-  border-radius: 8px;
-
-  background-color: #000;
-
-  font-size: 20px;
-  font-weight: 700;
-
-  &:hover {
-    opacity: 0.7;
-  }
 `;
