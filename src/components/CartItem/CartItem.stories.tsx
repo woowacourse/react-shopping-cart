4ebc496ede1d7eb2ce00f240f810e1a0d99f7d3f
pakxe <pigkill40@naver.com>
2024-05-21 02:59:26 +0900

    storybook: 프로퍼티명 수정

--- src/components/CartItem/CartItem.stories.tsx
+++ src/components/CartItem/CartItem.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import CartItem, { CartItemProps } from './CartItem';
+import CartItem from './CartItem';
 import { RecoilRoot } from 'recoil';
 
 const meta = {
@@ -17,34 +17,33 @@ const meta = {
     },
     cartItemId: {
       description: '',
-    }
+    },
   },
   args: {
     product: {
-      "productId": 3,
-      "name": "아디다스",
-      "price": 2000,
-      "imageUrl": "https://sitem.ssgcdn.com/74/25/04/item/1000373042574_i1_750.jpg",
-      "category": "fashion"
+      id: 3,
+      name: '아디다스',
+      price: 2000,
+      imageUrl: 'https://sitem.ssgcdn.com/74/25/04/item/1000373042574_i1_750.jpg',
+      category: 'fashion',
     },
     quantity: 1,
     cartItemId: 1,
-  }
+  },
 } satisfies Meta<typeof CartItem>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-// TODO : decorator 사용하여 동작할 수 있도록 current: recoil로 인해 작동 x
 export const Playground: Story = {
-  render: ({ ...args }: CartItemProps) => {
+  render: ({ ...args }: CartItem) => {
     return (
       <RecoilRoot>
         <div style={{ width: '430px' }}>
           <CartItem {...args} />
         </div>
       </RecoilRoot>
-    )
-  }
+    );
+  },
 };
