269c34581352b16607a2f3978c3a866cfb610da7
simhorim <hori5020@gmail.com>
2024-05-21 00:57:08 +0900

    feat(shoppingCart): remove, adjust fetch 기능 구현

--- src/api/shoppingCart.ts
+++ src/api/shoppingCart.ts
@@ -1,4 +1,5 @@
 import { BASE_URL, USER_ID, USER_PASSWORD } from '.';
+import MESSAGE from '../constants/Message';
 import { CartItemType } from '../type';
 import { generateBasicToken } from './auth';
 
@@ -11,9 +12,44 @@ export const fetchCartItems = async (): Promise<CartItemType[]> => {
   });
 
   if (!response.ok) {
-    throw new Error('장바구니 정보 불러오기를 실패했습니다.');
+    throw new Error(MESSAGE.fetchError);
   }
 
   const data = await response.json();
   return data.content;
+};
+
+export const fetchRemoveCartItem = async (
+  cartItemId: number,
+): Promise<void> => {
+  const token = generateBasicToken(USER_ID, USER_PASSWORD);
+
+  const response = await fetch(`${BASE_URL}/cart-items/${cartItemId}`, {
+    method: 'DELETE',
+    headers: { Authorization: token },
+  });
+
+  if (!response.ok) {
+    throw new Error(MESSAGE.removalError);
+  }
+};
+
+export const fetchAdjustCartItemQuantity = async (
+  cartItemId: number,
+  quantity: number,
+): Promise<void> => {
+  const token = generateBasicToken(USER_ID, USER_PASSWORD);
+
+  const response = await fetch(`${BASE_URL}/cart-items/${cartItemId}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: token,
+    },
+    body: JSON.stringify({ quantity }),
+  });
+
+  if (!response.ok) {
+    throw new Error(MESSAGE.quantityAdjustmentError);
+  }
 };
