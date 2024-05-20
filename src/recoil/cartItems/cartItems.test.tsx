46f62ae26f5f5627ea59da6494b5901463135b29
Largopie <106071687+Largopie@users.noreply.github.com>
2024-05-20 17:16:32 +0900

    test: RecoilRoot initializeState 세팅

--- src/recoil/cartItems/cartItems.test.tsx
+++ src/recoil/cartItems/cartItems.test.tsx
@@ -1,32 +1,27 @@
 import '@testing-library/jest-dom';
-import { render, waitFor, screen, fireEvent, cleanup } from '@testing-library/react';
+import { render, waitFor, screen, fireEvent } from '@testing-library/react';
 import { Suspense, act } from 'react';
 import { RecoilRoot } from 'recoil';
 
+import { cartItemsState } from './atoms';
+
 import { TOTAL_PRICE_OVER_100000_DATA, TOTAL_PRICE_UNDER_100000_DATA } from '@/constants/mock';
-import { fetchCartItems } from '@apis/cartItem';
 import CartProducts from '@components/Cart/CartProducts';
 import Loading from '@components/common/Loading';
 
-jest.mock('@apis/cartItem');
-
-const mockFetchCartItems = fetchCartItems as jest.Mock;
+jest.mock('@apis/cartItem', () => ({
+  fetchCartItems: jest.fn(),
+  updateItemQuantity: jest.fn(),
+  deleteItem: jest.fn(),
+}));
 
 describe('cartItems 동작 테스트', () => {
-  beforeEach(() => {
-    mockFetchCartItems.mockReset();
-  });
-
-  afterEach(() => {
-    cleanup();
-  });
-
   it('cartItem 리스트는 "나이키", "아디다스"가 렌더링된다.', async () => {
-    mockFetchCartItems.mockResolvedValueOnce(TOTAL_PRICE_UNDER_100000_DATA);
-
     act(() => {
       render(
-        <RecoilRoot>
+        <RecoilRoot
+          initializeState={({ set }) => set(cartItemsState, TOTAL_PRICE_UNDER_100000_DATA)}
+        >
           <Suspense fallback={<Loading />}>
             <CartProducts />
           </Suspense>
@@ -44,10 +39,10 @@ describe('cartItems 동작 테스트', () => {
     const PUMA = '퓨마';
 
     act(() => {
-      mockFetchCartItems.mockResolvedValueOnce(TOTAL_PRICE_OVER_100000_DATA);
-
       render(
-        <RecoilRoot>
+        <RecoilRoot
+          initializeState={({ set }) => set(cartItemsState, TOTAL_PRICE_OVER_100000_DATA)}
+        >
           <Suspense fallback={<Loading />}>
             <CartProducts />
           </Suspense>
@@ -65,13 +60,13 @@ describe('cartItems 동작 테스트', () => {
   });
 
   it('"퓨마" 상품의 +버튼을 클릭했을 때, 수량이 1개 늘어난다.', async () => {
-    mockFetchCartItems.mockResolvedValueOnce(TOTAL_PRICE_OVER_100000_DATA);
-
     const PUMA = '퓨마';
 
     act(() => {
       render(
-        <RecoilRoot>
+        <RecoilRoot
+          initializeState={({ set }) => set(cartItemsState, TOTAL_PRICE_OVER_100000_DATA)}
+        >
           <Suspense fallback={<Loading />}>
             <CartProducts />
           </Suspense>
@@ -87,13 +82,13 @@ describe('cartItems 동작 테스트', () => {
   });
 
   it('"퓨마" 상품의 -버튼을 클릭했을 때, 수량이 1개 줄어든다.', async () => {
-    mockFetchCartItems.mockResolvedValueOnce(TOTAL_PRICE_OVER_100000_DATA);
-
     const PUMA = '퓨마';
 
     act(() => {
       render(
-        <RecoilRoot>
+        <RecoilRoot
+          initializeState={({ set }) => set(cartItemsState, TOTAL_PRICE_OVER_100000_DATA)}
+        >
           <Suspense fallback={<Loading />}>
             <CartProducts />
           </Suspense>
@@ -104,18 +99,18 @@ describe('cartItems 동작 테스트', () => {
     await waitFor(() => {
       fireEvent.click(screen.getByLabelText(PUMA + 'minus-button', { selector: 'button' }));
 
-      expect(screen.getByTestId(PUMA + 'quantity')).toHaveTextContent('8');
+      expect(screen.getByTestId(PUMA + 'quantity')).toHaveTextContent('9');
     });
   });
 
   it('"퓨마" 상품의 delete버튼을 클릭했을 때, 상품이 삭제된다.', async () => {
-    mockFetchCartItems.mockResolvedValueOnce(TOTAL_PRICE_OVER_100000_DATA);
-
     const PUMA = '퓨마';
 
     act(() => {
       render(
-        <RecoilRoot>
+        <RecoilRoot
+          initializeState={({ set }) => set(cartItemsState, TOTAL_PRICE_OVER_100000_DATA)}
+        >
           <Suspense fallback={<Loading />}>
             <CartProducts />
           </Suspense>
