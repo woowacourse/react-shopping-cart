aef5d66318401df2656923c5dcc161d547d30ec9
hwinkr <dnddl8280@naver.com>
2024-05-20 11:05:42 +0900

    refactor(BackButton): 버튼 컴포넌트 래핑 추가

--- src/components/_common/BackButton/BackButton.tsx
+++ src/components/_common/BackButton/BackButton.tsx
@@ -1,11 +1,16 @@
 import { useNavigate } from "react-router-dom";
 
 import BackButtonIcon from "@/assets/back-button.svg?react";
+import Button from "../Button/Button";
 
 const BackButton = () => {
   const navigate = useNavigate();
 
-  return <BackButtonIcon onClick={() => navigate(-1)} />;
+  return (
+    <Button theme="transparent" width="fit">
+      <BackButtonIcon onClick={() => navigate(-1)} />
+    </Button>
+  );
 };
 
 export default BackButton;
