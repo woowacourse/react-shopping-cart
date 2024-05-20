b1f34ea07680049ce29885b3992bf5c9d10485cb
soosoo22 <lllllh00@naver.com>
2024-05-20 19:21:48 +0900

    refactor: 선택된 상품이 없을 경우 FloatingButton 클릭하면 페이지 전환 막기

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -2,14 +2,12 @@ import Header from '../components/Header/Header';
 import Cart from '../components/Cart/Cart';
 import { FloatingButton } from '../components/Button';
 import { useNavigate } from 'react-router-dom';
-import { cartQuantity } from '../recoil/atoms/atoms';
 import { useRecoilValue } from 'recoil';
 import { checkedCartItems } from '../recoil/selectors/selectors';
 
 export default function CartPage() {
   const navigate = useNavigate();
 
-  const cartTotalCount = useRecoilValue(cartQuantity);
   const orderProduct = useRecoilValue(checkedCartItems);
 
   return (
@@ -25,7 +23,7 @@ export default function CartPage() {
         text="주문 확인"
         isDisable={orderProduct.length ? false : true}
         onClick={() => {
-          cartTotalCount && navigate('/order-confirm');
+          orderProduct.length && navigate('/order-confirm');
         }}
       />
     </div>
