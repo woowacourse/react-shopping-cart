ed4d6d78e592aef97150f93de863baaf548215f1
hwinkr <dnddl8280@naver.com>
2024-05-20 11:05:10 +0900

    refactor(api): api 요청 함수 리팩터링
    - 예외 처리, 데이터 반환하는 로직 공통화 추가

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -10,12 +10,16 @@ type HttpMethod = "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
 
 interface APIOptions {
   method: HttpMethod;
+  exceptionText: string;
   headers?: HeadersInit;
   body?: object;
 }
 
-const cartAPIClient = async (endPoint: string, apiOptions: APIOptions) => {
-  const { method, headers, body } = apiOptions;
+const cartAPIClient = async <T>(
+  endPoint: string,
+  apiOptions: APIOptions
+): Promise<T> => {
+  const { method, exceptionText, headers, body } = apiOptions;
 
   const token = generateBasicToken(USER_ID, USER_PASSWORD);
   const response = await fetch(`${API_URL}${endPoint}`, {
@@ -24,49 +28,44 @@ const cartAPIClient = async (endPoint: string, apiOptions: APIOptions) => {
     body: body ? JSON.stringify(body) : null,
   });
 
-  return response;
+  if (!response.ok) {
+    throw new Error(exceptionText);
+  }
+
+  const isJson = response.headers
+    .get("content-type")
+    ?.includes("application/json");
+
+  return isJson ? response.json() : ({} as T);
 };
 
 export const getCartItems = async (): Promise<CartItem[]> => {
-  const response = await cartAPIClient("/cart-items", { method: "GET" });
+  const response = await cartAPIClient<{ content: CartItem[] }>("/cart-items", {
+    method: "GET",
+    exceptionText:
+      "장바구니 데이터를 불러올 수 없습니다. 나중에 다시 시도해 주세요.",
+  });
 
-  if (!response.ok) {
-    throw new Error(
-      "장바구니 데이터를 불러올 수 없습니다. 나중에 다시 시도해 주세요."
-    );
-  }
-
-  const data = await response.json();
-  return data.content;
+  return response.content;
 };
 
 export const patchCartItemQuantity = async (
   cartItemId: number,
   quantity: number
-): Promise<boolean> => {
-  const response = await cartAPIClient(`/cart-items/${cartItemId}`, {
+): Promise<void> => {
+  await cartAPIClient<void>(`/cart-items/${cartItemId}`, {
     method: "PATCH",
+    exceptionText: "상품 수량 변경에 실패했습니다. 나중에 다시 시도해 주세요.",
     headers: {
       "Content-Type": "application/json",
     },
     body: { quantity },
   });
-
-  if (!response.ok) {
-    throw new Error("상품 수량 변경에 실패했습니다. 나중에 다시 시도해 주세요");
-  }
-
-  return response.ok;
 };
 
-export const removeCartItem = async (cartItemId: number): Promise<boolean> => {
-  const response = await cartAPIClient(`/cart-items/${cartItemId}`, {
+export const removeCartItem = async (cartItemId: number): Promise<void> => {
+  await cartAPIClient<void>(`/cart-items/${cartItemId}`, {
     method: "DELETE",
+    exceptionText: "아이템을 삭제할 수 없습니다. 나중에 다시 시도해 주세요.",
   });
-
-  if (!response.ok) {
-    throw new Error("아이템을 삭제할 수 없습니다. 나중에 다시 시도해 주세요.");
-  }
-
-  return response.ok;
 };
