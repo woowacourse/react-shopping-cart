83b789675cc17e7c1fba1d9f6cb5a54433d503c1
vi-wolhwa <cerezo00@naver.com>
2024-05-21 12:40:39 +0900

    chore(vite.config): Remove CSS config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
-  },
-  css: {
-    modules: {
-      generateScopedName: '[name]__[local]__[hash:8]',
-      localsConvention: 'camelCaseOnly',
-    },
   },
 });
