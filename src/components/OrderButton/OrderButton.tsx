d8ecae368e764985cacffddefc154107eea94060
simhorim <hori5020@gmail.com>
2024-05-21 17:23:56 +0900

    refactor(OrderButton): props 수정 및 path에 따른 label 값 반환하는 함수 구현

--- src/components/OrderButton/OrderButton.tsx
+++ src/components/OrderButton/OrderButton.tsx
@@ -1,18 +1,42 @@
 import * as Styled from './style';
+
+import { useRecoilValue } from 'recoil';
+import { cartItemsCountState } from '../../recoil/cartItems';
+import { isSomeCartItemSelectedState } from '../../recoil/selectedCardItems';
+import { useLocation } from 'react-router-dom';
+import MESSAGE from '../../constants/Message';
+
 interface OrderButtonProp {
-  isOrderable: boolean;
-  children: React.ReactNode;
   onClick: () => void;
 }
 
-const OrderButton = ({ children, isOrderable, onClick }: OrderButtonProp) => {
+const OrderButton = ({ onClick }: OrderButtonProp) => {
+  const cartItemsCount = useRecoilValue(cartItemsCountState);
+  const isSomeCartItemSelected = useRecoilValue(isSomeCartItemSelectedState);
+
+  const hasSomeCartItem = !!cartItemsCount;
+  const isOrderable = hasSomeCartItem && isSomeCartItemSelected;
+
+  const location = useLocation();
+
+  const label = () => {
+    switch (location.pathname) {
+      case '/':
+        return MESSAGE.orderConfirmation;
+      case '/orderConfirmation':
+        return MESSAGE.pay;
+      default:
+        return '';
+    }
+  };
+
   return (
     <Styled.OrderButton
       onClick={() => onClick()}
       $isOrderable={isOrderable}
       disabled={!isOrderable}
     >
-      {children}
+      {label()}
     </Styled.OrderButton>
   );
 };
