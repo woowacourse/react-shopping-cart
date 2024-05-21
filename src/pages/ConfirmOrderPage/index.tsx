730b0fa6c01f083d5386aae96ae3d536d52dfa57
river <jenn0.6n@gmail.com>
2024-05-21 19:59:06 +0900

    feat: PageHeader 컴포넌트 구현

--- src/pages/ConfirmOrderPage/index.tsx
+++ src/pages/ConfirmOrderPage/index.tsx
@@ -1,6 +1,7 @@
 import { PropsWithChildren } from "react";
 import { useRecoilValue, useRecoilValueLoadable } from "recoil";
 import Header from "../../components/Header";
+import PageHeader from "../../components/PageHeader";
 import BottomButton from "../../components/common/BottomButton";
 import RecoilSuspense from "../../components/common/RecoilSuspense";
 import Layout from "../../layout";
@@ -30,11 +31,12 @@ const ConfirmOrderPage: React.FC<PropsWithChildren> = () => {
     >
       <RecoilSuspense loadable={cartList} fallback={<div>안쪽 로딩 중...</div>}>
         <ConfirmOrderContainer>
-          <Title>주문 확인</Title>
-          <OrderSummary>
-            <p>{`총 ${cartList.contents.length}종류의 상품 ${totalQuantity}개를 주문합니다.`}</p>
-            <p>최종 결제 금액을 확인해주세요.</p>
-          </OrderSummary>
+          <PageHeader title="주문 확인">
+            <OrderSummary>
+              <span>{`총 ${cartList.contents.length}종류의 상품 ${totalQuantity}개를 주문합니다.`}</span>
+              <span>최종 결제 금액을 확인해주세요.</span>
+            </OrderSummary>
+          </PageHeader>
 
           <TotalPrice>
             <SubTitle>총 결제 금액</SubTitle>
