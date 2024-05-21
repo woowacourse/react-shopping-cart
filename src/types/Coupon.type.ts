2f88f0aa4fa351c4918c58eb59a14a503fbc60f4
greetings1012 <ahnqwerty@gmail.com>
2024-05-21 13:59:10 +0900

    feat: 쿠폰 데이터 인터페이스 선언

---
+++ src/types/Coupon.type.ts
@@ -0,0 +1,9 @@
+export type TCoupon =  {
+    "id": number,
+    "code": string,
+    "description": string,
+    "expirationDate": string,
+    "discount": number,
+    "minimumAmount": number,
+    "discountType": string,
+}
