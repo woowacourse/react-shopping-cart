101c634c054d37b692bd6e9fcffe9ea5217d7f4c
brgndy <brgndy96@gmail.com>
2024-05-21 14:41:15 +0900

    refactor: 라우터 로직 외부로 변경

--- src/pages/cart/components/OrderButton.tsx
+++ src/pages/cart/components/OrderButton.tsx
@@ -1,15 +1,41 @@
 import Button from '@/components/common/Button';
-import useCheckoutNavigate from '@/hooks/useCheckoutNavigate';
-import { orderAmount } from '@/store/selectors';
+import { PAGE_ROUTES } from '@/constants/routes';
+import {
+  orderAmountSelector,
+  totalCategoryCountSelector,
+  totalOrderAmountSelector,
+  totalOrderQuantitySelector,
+  totalCartItemsSelector,
+} from '@/store/selectors';
+import { useNavigate } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 
+const MIN_ORDER_QUANTITY = 1;
+
 export default function OrderButton() {
-  const { handleOrderButton } = useCheckoutNavigate();
-  const orderAmountValue = useRecoilValue(orderAmount);
+  const navigate = useNavigate();
+  const totalOrderQuantity = useRecoilValue(totalOrderQuantitySelector);
+  const totalCategoryCount = useRecoilValue(totalCategoryCountSelector);
+  const totalOrderAmount = useRecoilValue(totalOrderAmountSelector);
+  const orderAmount = useRecoilValue(orderAmountSelector);
+  const totalCartItems = useRecoilValue(totalCartItemsSelector);
+
+  const handleOrderButton = () => {
+    if (totalOrderQuantity > MIN_ORDER_QUANTITY) {
+      navigate(PAGE_ROUTES.ORDER_CONFIRM, {
+        state: {
+          totalCategoryCount,
+          totalOrderQuantity,
+          totalOrderAmount,
+          totalCartItems,
+        },
+      });
+    }
+  };
 
   return (
-    <Button onClick={handleOrderButton} variant="footer" disabled={!orderAmountValue}>
-      주문 하기
+    <Button onClick={handleOrderButton} variant="footer" disabled={!orderAmount}>
+      주문 확인
     </Button>
   );
 }
