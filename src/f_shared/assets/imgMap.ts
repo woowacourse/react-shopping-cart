5bf95ee1b4727acdfdab9bb45138b0de3948ca75
vi-wolhwa <cerezo00@naver.com>
2024-05-21 19:56:55 +0900

    Add image 'information'

--- src/f_shared/assets/imgMap.ts
+++ src/f_shared/assets/imgMap.ts
@@ -1,11 +1,13 @@
 import goBack from './goBack/goBack.png';
 import goBack_dark from './goBack/goBack_dark.png';
+import information from './information/information.png';
 import logo from './logo/logo.png';
 import logo_dark from './logo/logo_dark.png';
 
 export const imgMap = {
-  logo,
-  logo_dark,
   goBack,
   goBack_dark,
+  information,
+  logo,
+  logo_dark,
 };
