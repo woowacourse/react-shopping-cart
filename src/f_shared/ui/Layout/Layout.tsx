65ff97d1593b0f6b3738820ffe27cd04e5bcd9c2
vi-wolhwa <cerezo00@naver.com>
2024-05-21 21:29:34 +0900

    refactor(Layout): Add gap between contents

--- src/f_shared/ui/Layout/Layout.tsx
+++ src/f_shared/ui/Layout/Layout.tsx
@@ -5,12 +5,20 @@ import css from './Layout.module.css';
 
 const cn = classNames.bind(css);
 
+interface Gap {
+  top?: number;
+  bottom?: number;
+}
+
 interface LayoutProps {
   headerSlot?: ReactNode;
   contentHeaderSlot?: ReactNode;
   contentBodySlot: ReactNode;
   contentFooterSlot?: ReactNode;
   footerSlot?: ReactNode;
+
+  gap?: Gap;
+  bgColor?: string;
 }
 
 export const Layout = ({
@@ -19,13 +27,18 @@ export const Layout = ({
   contentBodySlot,
   contentFooterSlot,
   footerSlot,
+
+  gap,
+  bgColor = 'white',
 }: LayoutProps) => {
   return (
-    <div className={cn('root')}>
+    <div style={{ backgroundColor: bgColor }} className={cn('root')}>
       {headerSlot && <header className={cn('header')}>{headerSlot}</header>}
       <div className={cn('content')}>
         {contentHeaderSlot && <header className={cn('contentHeader')}>{contentHeaderSlot}</header>}
-        <div className={cn('contentBody')}>{contentBodySlot}</div>
+        <div style={{ margin: `${gap?.top ?? 0}px 0 ${gap?.bottom ?? 0}px 0` }} className={cn('contentBody')}>
+          {contentBodySlot}
+        </div>
         {contentFooterSlot && <footer className={cn('contentFooter')}>{contentFooterSlot}</footer>}
       </div>
       {footerSlot && <footer className={cn('footer')}>{footerSlot}</footer>}
