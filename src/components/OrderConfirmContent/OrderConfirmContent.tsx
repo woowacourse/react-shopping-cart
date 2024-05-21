6d071d4704ea7f8d1a9700bd843fc3a690822b98
jaeml06 <jaeml0630@gmail.com>
2024-05-21 17:02:20 +0900

    feat : 선택된 상품 리스트 보여주는 기능 구현

---
+++ src/components/OrderConfirmContent/OrderConfirmContent.tsx
@@ -0,0 +1,42 @@
+import { useRecoilValue } from 'recoil';
+import TotalAmount from '../TotalAmount/TotalAmount';
+import Title from '../Title/Title';
+import styled from 'styled-components';
+import { MESSAGES } from '../../constants/Messages';
+import {
+  checkedItemsSelector,
+  totalCountSelector,
+} from '../../recoil/selectors';
+import { CartItems } from '../../types/Item';
+import ConfirmItemCard from '../ItemCard/ConfirmItemCard';
+
+export const NoCartItemContainer = styled.p`
+  width: 100%;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 1.6rem;
+  font-weight: 400;
+  line-height: 1.6rem;
+  text-align: center;
+`;
+
+function OrderConfirmContent() {
+  const checkedItem = useRecoilValue(checkedItemsSelector);
+  const { totalItemTypeCount, totalCount } = useRecoilValue(totalCountSelector);
+  return (
+    <>
+      <Title
+        title={MESSAGES.confirm}
+        subTitle={`총 ${totalItemTypeCount}종류의 상품 ${totalCount}개를 주문합니다.\n 최종 결제 금액을 확인해 주세요.`}
+      />
+      {checkedItem.map((product: CartItems) => {
+        return <ConfirmItemCard key={product.id} item={product} />;
+      })}
+      <TotalAmount />
+    </>
+  );
+}
+
+export default OrderConfirmContent;
