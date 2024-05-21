ac8dd3d97b9aac3e64b7809019be419b2bae1b77
badahertz52 <badahertz52@gmail.com>
2024-05-21 19:43:47 +0900

    test: 사용 가능한 상품 목록에 대한 테스트 추가

---
+++ __tests__/availableCoupon.test.tsx
@@ -0,0 +1,251 @@
+import { CartItem } from '@appTypes/shoppingCart';
+import { useAvailableCoupons } from '@hooks/index';
+import { act } from '@testing-library/react';
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
+
+import { COUPONS } from './mockData/coupon';
+import { renderCouponHookWithRecoilRoot } from './utils/recoilTestUtils';
+
+const renderAvailableCoupon = (initialItems: CartItem[], selectedIds: number[]) =>
+  renderCouponHookWithRecoilRoot(() => useAvailableCoupons(), initialItems, selectedIds);
+
+describe('사용 가능한 쿠폰 테스트', () => {
+  const [FIXED_5000, BOGO, FREE_SHIPPING, MIRACLE_SALE] = COUPONS;
+
+  beforeEach(() => {
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
+  describe('5000원 할인 쿠폰 테스트', () => {
+    const ITEMS: CartItem[] = [
+      {
+        id: 106,
+        quantity: 1,
+        product: {
+          id: 2,
+          name: '나이키',
+          price: 100000,
+          imageUrl: '',
+          category: 'fashion',
+        },
+      },
+    ];
+
+    it('주문 금액이 최소 주문 금액(100000원) 미만이면, 쿠폰을 사용할 수 없다', () => {
+      const INVALID_ITEMS = [{ ...ITEMS[0], product: { ...ITEMS[0].product, price: ITEMS[0].product.price - 100 } }];
+
+      vi.setSystemTime(new Date(FIXED_5000.expirationDate));
+
+      const { result } = renderAvailableCoupon(INVALID_ITEMS, [INVALID_ITEMS[0].id]);
+      const { getAvailableCoupons } = result.current;
+
+      act(() => {
+        const coupons = getAvailableCoupons();
+
+        expect(coupons.includes(FIXED_5000)).toBeFalsy();
+      });
+    });
+
+    it.each([
+      { date: new Date('2024-12-1'), isValid: false },
+      { date: new Date('2024-11-30'), isValid: true },
+      { date: new Date('2024-11-29'), isValid: true },
+    ])('쿠폰은 만료 기간안에 사용해야 한다. (사용 기간:%s)', ({ date, isValid }) => {
+      vi.setSystemTime(date);
+
+      const { result } = renderAvailableCoupon(ITEMS, [ITEMS[0].id]);
+      const { getAvailableCoupons } = result.current;
+
+      act(() => {
+        const coupons = getAvailableCoupons();
+        expect(coupons.includes(FIXED_5000)).toBe(isValid);
+      });
+    });
+
+    it('주문 금액이 최소 주문 금액 이상이고 만료일 이전이라면, 쿠폰을 사용할 수 있다.', () => {
+      vi.setSystemTime(new Date(FIXED_5000.expirationDate));
+
+      const { result } = renderAvailableCoupon(ITEMS, [ITEMS[0].id]);
+      const { getAvailableCoupons } = result.current;
+
+      act(() => {
+        const coupons = getAvailableCoupons();
+        expect(coupons.includes(FIXED_5000)).toBeTruthy();
+      });
+    });
+  });
+
+  describe('2+1 쿠폰 테스트', () => {
+    const ITEMS: CartItem[] = [
+      {
+        id: 106,
+        quantity: 3,
+        product: {
+          id: 2,
+          name: '나이키',
+          price: 1000,
+          imageUrl: '',
+          category: 'fashion',
+        },
+      },
+    ];
+
+    it.each([
+      { date: new Date('2024-5-31'), isValid: false },
+      { date: new Date('2024-5-30'), isValid: true },
+      { date: new Date('2024-5-29'), isValid: true },
+    ])('쿠폰은 만료 기간안에 사용해야 한다. (사용 기간:%s)', ({ date, isValid }) => {
+      vi.setSystemTime(date);
+
+      const { result } = renderAvailableCoupon(ITEMS, [ITEMS[0].id]);
+      const { getAvailableCoupons } = result.current;
+
+      act(() => {
+        const coupons = getAvailableCoupons();
+        expect(coupons.includes(BOGO)).toBe(isValid);
+      });
+    });
+
+    it.each([
+      { quantity: 1, isValid: false },
+      { quantity: 2, isValid: true },
+      { quantity: 3, isValid: true },
+    ])('수량이 2개 이상인 상품이 선택되어야 쿠폰을 사용할 수 있다.(수량: %s)', ({ quantity, isValid }) => {
+      const testItems = [{ ...ITEMS[0], quantity }];
+
+      vi.setSystemTime(new Date(BOGO.expirationDate));
+
+      const { result } = renderAvailableCoupon(testItems, [testItems[0].id]);
+      const { getAvailableCoupons } = result.current;
+
+      act(() => {
+        const coupons = getAvailableCoupons();
+        expect(coupons.includes(BOGO)).toBe(isValid);
+      });
+    });
+
+    it('수량이 2개 이상인 상품이 존재하고 만료일 이전이라면 쿠폰을 사용할 수 있다.', () => {
+      vi.setSystemTime(new Date(BOGO.expirationDate));
+
+      const { result } = renderAvailableCoupon(ITEMS, [ITEMS[0].id]);
+      const { getAvailableCoupons } = result.current;
+
+      act(() => {
+        const coupons = getAvailableCoupons();
+
+        expect(coupons.includes(BOGO)).toBeTruthy();
+      });
+    });
+  });
+  describe('5만원 이상 구매 시 무료 배송 쿠폰 테스트', () => {
+    const ITEMS: CartItem[] = [
+      {
+        id: 106,
+        quantity: 1,
+        product: {
+          id: 2,
+          name: '나이키',
+          price: 50000,
+          imageUrl: '',
+          category: 'fashion',
+        },
+      },
+    ];
+
+    it('주문 금액이 최소 주문 금액(5만원) 미만이면, 쿠폰을 사용할 수 없다', () => {
+      const INVALID_ITEMS = [{ ...ITEMS[0], product: { ...ITEMS[0].product, price: ITEMS[0].product.price - 100 } }];
+
+      vi.setSystemTime(new Date(FREE_SHIPPING.expirationDate));
+
+      const { result } = renderAvailableCoupon(INVALID_ITEMS, [INVALID_ITEMS[0].id]);
+      const { getAvailableCoupons } = result.current;
+
+      act(() => {
+        const coupons = getAvailableCoupons();
+
+        expect(coupons.includes(FREE_SHIPPING)).toBeFalsy();
+      });
+    });
+
+    it.each([
+      { date: new Date('2024-09-01'), isValid: false },
+      { date: new Date('2024-08-31'), isValid: true },
+      { date: new Date('2024-08-30'), isValid: true },
+    ])('쿠폰은 만료 기간안에 사용해야 한다. (사용 기간:%s)', ({ date, isValid }) => {
+      vi.setSystemTime(date);
+
+      const { result } = renderAvailableCoupon(ITEMS, [ITEMS[0].id]);
+      const { getAvailableCoupons } = result.current;
+
+      act(() => {
+        const coupons = getAvailableCoupons();
+        expect(coupons.includes(FREE_SHIPPING)).toBe(isValid);
+      });
+    });
+
+    it('주문 금액이 최소 주문 금액 이상이고 만료일 이전이라면, 쿠폰을 사용할 수 있다.', () => {
+      vi.setSystemTime(new Date(FREE_SHIPPING.expirationDate));
+
+      const { result } = renderAvailableCoupon(ITEMS, [ITEMS[0].id]);
+      const { getAvailableCoupons } = result.current;
+
+      act(() => {
+        const coupons = getAvailableCoupons();
+        expect(coupons.includes(FREE_SHIPPING)).toBeTruthy();
+      });
+    });
+  });
+
+  describe('미라클모닝 30% 할인 쿠폰 테스트', () => {
+    const ITEMS: CartItem[] = [
+      {
+        id: 106,
+        quantity: 3,
+        product: {
+          id: 2,
+          name: '나이키',
+          price: 50000,
+          imageUrl: '',
+          category: 'fashion',
+        },
+      },
+    ];
+
+    it.each([
+      { date: new Date('2024-08-01'), isValid: false },
+      { date: new Date('2024-07-31T04:00:01'), isValid: true },
+      { date: new Date('2024-07-30T04:00:01'), isValid: true },
+    ])('쿠폰은 만료 기간안에 사용해야 한다. (사용 기간:%s)', ({ date, isValid }) => {
+      vi.setSystemTime(date);
+
+      const { result } = renderAvailableCoupon(ITEMS, [ITEMS[0].id]);
+      const { getAvailableCoupons } = result.current;
+
+      act(() => {
+        const coupons = getAvailableCoupons();
+        expect(coupons.includes(MIRACLE_SALE)).toBe(isValid);
+      });
+    });
+
+    it.each([
+      { date: new Date('2024-05-21T03:59:59'), isValid: false },
+      { date: new Date('2024-05-21T07:00:01'), isValid: false },
+      { date: new Date('2024-05-21T04:00:01'), isValid: true },
+      { date: new Date('2024-05-21T06:59:59'), isValid: true },
+    ])('쿠폰은 오전4~7시에 사용할 수 있다,(사용 시간: %s)', ({ date, isValid }) => {
+      vi.setSystemTime(date);
+
+      const { result } = renderAvailableCoupon(ITEMS, [ITEMS[0].id]);
+      const { getAvailableCoupons } = result.current;
+
+      act(() => {
+        const coupons = getAvailableCoupons();
+        expect(coupons.includes(MIRACLE_SALE)).toBe(isValid);
+      });
+    });
+  });
+});
--- __tests__/utils/recoilTestUtils.tsx
+++ __tests__/utils/recoilTestUtils.tsx
@@ -1,9 +1,10 @@
 import { CartItem } from '@appTypes/shoppingCart';
-import { cartItemsAtom, selectedIdsAtom } from '@recoil/shoppingCart';
+import { cartItemsAtom, couponListAtom, selectedIdsAtom } from '@recoil/shoppingCart';
 import { renderHook } from '@testing-library/react';
 import { RecoilRoot } from 'recoil';
 
 import { INITIAL_ITEMS } from '../mockData/cartItems';
+import { COUPON_LIST } from '../mockData/coupon';
 
 export const renderHookWithRecoilRoot = <T,>(
   hook: () => T,
@@ -16,6 +17,26 @@ export const renderHookWithRecoilRoot = <T,>(
         initializeState={({ set }) => {
           set(cartItemsAtom, initialItems || INITIAL_ITEMS);
           set(selectedIdsAtom, initialSelectedIds || []);
+        }}
+      >
+        {children}
+      </RecoilRoot>
+    ),
+  });
+};
+
+export const renderCouponHookWithRecoilRoot = <T,>(
+  hook: () => T,
+  initialItems: CartItem[],
+  initialSelectedIds: number[],
+) => {
+  return renderHook(hook, {
+    wrapper: ({ children }) => (
+      <RecoilRoot
+        initializeState={({ set }) => {
+          set(cartItemsAtom, initialItems);
+          set(selectedIdsAtom, initialSelectedIds);
+          set(couponListAtom, COUPON_LIST);
         }}
       >
         {children}
