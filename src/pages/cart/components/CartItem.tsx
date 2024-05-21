abb45d75f5e815ab5318b2e00a16ba20b6ba5133
brgndy <brgndy96@gmail.com>
2024-05-22 08:17:21 +0900

    refactor: 재사용을 위해 아이템 컴포넌트 분리

--- src/pages/cart/components/CartItem.tsx
+++ src/pages/cart/components/CartItem.tsx
@@ -1,33 +1,6 @@
 import styles from '../Cart.module.css';
-import Button from '@/components/common/Button';
-import formatKoreanCurrency from '@/utils/formatKoreanCurrency';
-import useDeleteProduct from '@/hooks/useDeleteProduct';
-import CartItemCheckBox from './CartItemCheckBox';
-import { FormattedProduct } from '@/types';
-import CountButton from './CountButton';
+import { PropsWithChildren } from 'react';
 
-export default function CartItem({ id, price, imageUrl, name }: FormattedProduct) {
-  const { handleDeleteButton } = useDeleteProduct(id);
-
-  return (
-    <li className={styles.cartItemContainer}>
-      <div className={styles.cartItemInputButtonContainer}>
-        <CartItemCheckBox id={id} />
-        <Button className={styles.deleteButton} onClick={handleDeleteButton}>
-          삭제
-        </Button>
-      </div>
-      <div className={styles.itemImageAndInfoContainer}>
-        <div>
-          <img className={styles.itemImage} src={imageUrl} width={100} height={100} alt={name} />
-        </div>
-
-        <div className={styles.itemInfoContainer}>
-          <span className={styles.name}> {name}</span>
-          <span className={styles.titleText}> {formatKoreanCurrency(price)}원</span>
-          <CountButton id={id} />
-        </div>
-      </div>
-    </li>
-  );
+export default function CartItem({ children }: PropsWithChildren) {
+  return <li className={styles.cartItemContainer}>{children}</li>;
 }
--- src/pages/cart/components/CartList.tsx
+++ src/pages/cart/components/CartList.tsx
@@ -1,6 +1,10 @@
 import CartItem from './CartItem';
 import AllSelectCheckBox from './AllSelectCheckBox';
 import { useCartContext } from '../context/CartContext';
+import CheckAndDelete from './CheckAndDelete';
+import CartItemImageAndInfo from './CartItemImageAndInfo';
+import CountButton from './CountButton';
+import styles from './cartItemImageAndInfo.module.css';
 
 export default function CartList() {
   const { allCartItems } = useCartContext();
@@ -11,15 +15,17 @@ export default function CartList() {
       <ul>
         {allCartItems.map((cartItem) => {
           return (
-            <CartItem
-              key={cartItem.id}
-              id={cartItem.id}
-              name={cartItem.product.name}
-              price={cartItem.product.price}
-              category={cartItem.product.category}
-              imageUrl={cartItem.product.imageUrl}
-              isChecked={cartItem.product.isChecked}
-            />
+            <CartItem key={cartItem.id}>
+              <CheckAndDelete id={cartItem.id} />
+              <CartItemImageAndInfo
+                className={styles.itemImageAndInfoContainer}
+                imageUrl={cartItem.product.imageUrl}
+                name={cartItem.product.name}
+                price={cartItem.product.price}
+              >
+                <CountButton id={cartItem.id} />
+              </CartItemImageAndInfo>
+            </CartItem>
           );
         })}
       </ul>
