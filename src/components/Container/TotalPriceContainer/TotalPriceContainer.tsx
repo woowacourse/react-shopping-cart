357fa9f7835a6ac3ff2d2b65772fb6aaf00a2a6f
greetings1012 <ahnqwerty@gmail.com>
2024-05-21 17:46:31 +0900

    refactor: NotificationContainer 컴포넌트 분리

--- src/components/Container/TotalPriceContainer/TotalPriceContainer.tsx
+++ src/components/Container/TotalPriceContainer/TotalPriceContainer.tsx
@@ -1,8 +1,8 @@
 import { useRecoilValue } from 'recoil';
+import NotificationContainer from '../../NotificationContainer/NotificationContainer';
 import PriceContainer from '../PriceContainer/PriceContainer';
 import { selectedCartItemListTotalPriceSelector } from '../../../pages/ShoppingCartPage/recoil/selector/selectedCartItemListTotalPriceSelector';
 import { DELIVERY_FEE_DISCOUNT_THRESHOLD, calculateDeliveryFee } from '../../../utils/calculateDeliveryFee';
-import { InfoIcon } from '../../../assets';
 import * as S from './TotalPriceContainer.style';
 
 function TotalPriceContainer() {
@@ -13,10 +13,9 @@ function TotalPriceContainer() {
 
   return (
     <S.Layout>
-      <S.NotificationContainer>
-        <S.InfoIcon src={InfoIcon} />
-        <p>총 주문 금액이 {DELIVERY_FEE_DISCOUNT_THRESHOLD.toLocaleString()}원 이상일 경우 무료 배송됩니다.</p>
-      </S.NotificationContainer>
+      <NotificationContainer
+        content={`총 주문 금액이 ${DELIVERY_FEE_DISCOUNT_THRESHOLD.toLocaleString()}원 이상일 경우 무료 배송됩니다.`}
+      />
       <S.PriceDetailContainer>
         <PriceContainer title="주문 금액" value={selectedCartItemTotalPrice} />
         <PriceContainer title="배송비" value={deliveryFee} />
