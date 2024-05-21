f7387fb97772a2e711739badd1e9c6c275af3091
rbgksqkr <rbgks1937@naver.com>
2024-05-21 21:03:30 +0900

    feat: 주문 확인 페이지 퍼블리싱

--- src/components/CartConfirmMainSection.tsx
+++ src/components/CartConfirmMainSection.tsx
@@ -1,31 +1,69 @@
 import { css } from '@emotion/react';
+import { ChangeEvent, useState } from 'react';
 import { useRecoilValue } from 'recoil';
 
-import PurchaseButton from '@components/common/PurchaseButton';
-import { orderResultState, productTypesCountState } from '@recoil/cartItems/selectors';
+import CartFooterSection from './common/CartFooter';
+import CartItem from './common/CartItem';
+
+import { CHECKED, UNCHECKED } from '@/assets/images';
+import { THEME } from '@/constants/theme';
+import {
+  checkedCartItemsState,
+  orderResultState,
+  productTypesCountState,
+} from '@recoil/cartItems/selectors';
 
 const CartConfirmMainSection = () => {
-  const { totalQuantity, totalPurchasePrice } = useRecoilValue(orderResultState);
+  const { totalQuantity } = useRecoilValue(orderResultState);
   const productTypesCount = useRecoilValue(productTypesCountState);
+  const checkedCartItems = useRecoilValue(checkedCartItemsState);
+
+  const [isAdditionalDelivery, setIsAdditionalDelivery] = useState(false);
+
+  const handleClickAdditionalDelivery = (e: ChangeEvent<HTMLInputElement>) => {
+    setIsAdditionalDelivery(e.target.checked);
+  };
 
   return (
-    <>
-      <div css={container}>
-        <div css={titleWrapper}>
-          <h2 css={title}>주문 확인</h2>
+    <div css={container}>
+      <section css={cartHeaderSection}>
+        <div css={cartTitleWrapper}>
+          <h2 css={cartTitle}>주문 확인</h2>
         </div>
-        <div css={orderInfoContainer}>
-          총 {productTypesCount}종류의 상품 {totalQuantity}개를 주문합니다. <br />
-          최종 결제 금액을 확인해주세요.
+        <div css={descriptionWrapper}>
+          <span css={description}>
+            총 {productTypesCount}종류의 상품 {totalQuantity}개를 주문합니다. <br />
+            최종 결제 금액을 확인해주세요.
+          </span>
         </div>
+      </section>
 
-        <div css={orderResultContainer}>
-          <span css={orderResultText}>총 결제 금액</span>
-          <span css={orderResult}>{totalPurchasePrice.toLocaleString('ko-KR')}원</span>
+      <div>
+        {checkedCartItems.map((checkedCartItem) => (
+          <CartItem key={checkedCartItem.id} item={checkedCartItem} type="ORDER" />
+        ))}
+      </div>
+      {/* TODO: 쿠폰 할인 적용 값 recoil로 관리 */}
+      <button css={couponButton}>쿠폰 적용</button>
+      <div style={{ margin: '16px 0' }}>
+        <span>배송 정보</span>
+        <div>
+          {/* TODO: 체크 박스 공통 컴포넌트로 분리 */}
+          <input
+            id="far-location"
+            type="checkbox"
+            checked={isAdditionalDelivery}
+            css={screenReaderOnly}
+            onChange={handleClickAdditionalDelivery}
+          />
+          <label css={label} htmlFor="far-location">
+            <img src={isAdditionalDelivery ? CHECKED : UNCHECKED} css={checkIcon} />
+            <span css={labelText}>제주도 및 도서 산간 지역</span>
+          </label>
         </div>
       </div>
-      <PurchaseButton />
-    </>
+      <CartFooterSection type="ORDER" />
+    </div>
   );
 };
 
@@ -34,48 +72,88 @@ export default CartConfirmMainSection;
 const container = css`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
   flex: 1;
-  gap: 24px;
+  gap: 12px;
+
+  padding: 0 24px;
+
+  overflow-y: scroll;
 `;
 
-const titleWrapper = css`
-  display: flex;
-  align-items: center;
-  height: 35px;
-  gap: 24px;
-`;
-
-const title = css`
-  font-size: 24px;
-  font-weight: 700;
-`;
-
-const orderInfoContainer = css`
-  display: flex;
-  align-items: center;
-
-  font-size: 12px;
-  font-weight: 500;
-  text-align: center;
-  line-height: 18px;
-`;
-
-const orderResultContainer = css`
+const cartHeaderSection = css`
   display: flex;
   flex-direction: column;
   gap: 12px;
+
+  width: 100%;
+
+  padding: 36px 0;
 `;
 
-const orderResultText = css`
-  font-size: 16px;
-  font-weight: 700;
-  text-align: center;
-  color: #0a0d13;
+const cartTitleWrapper = css`
+  display: flex;
+  align-items: center;
+
+  height: 35px;
 `;
 
-const orderResult = css`
+const cartTitle = css`
   font-size: 24px;
   font-weight: 700;
+`;
+
+const descriptionWrapper = css`
+  display: flex;
+  align-items: center;
+  width: 100%;
+`;
+
+const description = css`
+  color: #0a0d13;
+
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 18px;
+`;
+
+const couponButton = css`
+  border: 1px solid #33333340;
+  border-radius: 10px;
+  padding: 16px 0;
+
+  background-color: ${THEME.WHITE};
+
+  font-weight: 700;
+  font-size: 15px;
+  color: #333333bf;
+`;
+
+const screenReaderOnly = css`
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  border: 0;
+
+  overflow: hidden;
+  clip-path: inset(50%);
+  clip: rect(0 0 0 0);
+`;
+
+const label = css`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+const labelText = css`
+  font-size: 12px;
+  font-weight: 500;
+`;
+
+const checkIcon = css`
+  width: 24px;
+  height: 24px;
+
+  cursor: pointer;
 `;
