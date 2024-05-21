cd287ff5d8ba0c3abbb6b61fc4a71958be5ff6d3
greetings1012 <ahnqwerty@gmail.com>
2024-05-21 15:06:33 +0900

    refactor: OrderConfirmPage에 주문 목록 추가

--- src/pages/OrderConfirmPage/OrderConfirmPage.style.tsx
+++ src/pages/OrderConfirmPage/OrderConfirmPage.style.tsx
@@ -3,31 +3,13 @@ import styled from 'styled-components';
 export const Layout = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  row-gap: 24px;
-  width: 100%;
-  height: calc(100vh - ${({ theme }) => theme.boxHeight.md} * 2);
-  text-align: center;
+  row-gap: 36px;
+  min-height: calc(100vh - ${({ theme }) => theme.boxHeight.md} * 3);
+  margin-bottom: ${({ theme }) => theme.boxHeight.md};
+  padding: 36px 24px;
 `;
 
 export const OrderDetailText = styled.p`
   font-size: ${({ theme }) => theme.fontSize.sm};
   line-height: 1.5;
-`;
-
-export const TotalPriceContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  row-gap: 12px;
-`;
-
-export const TotalPriceTitle = styled.p`
-  font-size: ${({ theme }) => theme.fontSize.md};
-  font-weight: 700;
-`;
-
-export const TotalPriceValue = styled.p`
-  font-size: ${({ theme }) => theme.fontSize.lg};
-  font-weight: 700;
 `;
--- src/pages/OrderConfirmPage/OrderConfirmPage.tsx
+++ src/pages/OrderConfirmPage/OrderConfirmPage.tsx
@@ -4,38 +4,40 @@ import Header from '../../components/Header/Header';
 import TitleContainer from '../../components/Container/TitleContainer/TitleContainer';
 import SubmitButton from '../../components/Button/SubmitButton/SubmitButton';
 import { selectedCartItemListState } from '../ShoppingCartPage/recoil/atom/selectedCartItemListState';
-import { selectedCartItemListTotalPriceSelector } from '../ShoppingCartPage/recoil/selector/selectedCartItemListTotalPriceSelector';
 import { selectedCartItemListTotalCountSelector } from '../ShoppingCartPage/recoil/selector/selectedCartItemListTotalCountSelector';
-import { calculateDeliveryFee } from '../../utils/calculateDeliveryFee';
 import { PATHS } from '../../constants/PATHS';
 import * as S from './OrderConfirmPage.style';
+import TotalPriceContainer from '../../components/Container/TotalPriceContainer/TotalPriceContainer';
+import CartItemContainer from '../../components/CartItem/CartItemContainer';
 
 function OrderConfirmPage() {
   const selectedItemList = useRecoilValue(selectedCartItemListState);
 
-  const selectedCartItemTotalPrice = useRecoilValue(selectedCartItemListTotalPriceSelector);
   const selectedCartItemTotalCount = useRecoilValue(selectedCartItemListTotalCountSelector);
 
   if (selectedItemList.length === 0) {
     return <Navigate to={PATHS.ERROR} />;
   }
 
-  const totalPrice = selectedCartItemTotalPrice + calculateDeliveryFee(selectedCartItemTotalPrice);
+  const renderSelectedItemListSection = () => (
+    <>
+      {selectedItemList.map((el) => (
+        <CartItemContainer key={el.id} item={el} />
+      ))}
+
+      <TotalPriceContainer />
+    </>
+  );
 
   return (
     <div>
       <Header />
       <S.Layout>
-        <TitleContainer title="주문 확인" />
-        <S.OrderDetailText>
-          총 {selectedItemList.length}종류의 상품 {selectedCartItemTotalCount}개를 주문합니다.
-          <br />
-          최종 결제 금액을 확인해주세요.
-        </S.OrderDetailText>
-        <S.TotalPriceContainer>
-          <S.TotalPriceTitle>총 결제 금액</S.TotalPriceTitle>
-          <S.TotalPriceValue>{totalPrice.toLocaleString()}원</S.TotalPriceValue>
-        </S.TotalPriceContainer>
+        <TitleContainer
+          title="주문 확인"
+          subTitle={`총 ${selectedItemList.length}종류의 상품 ${selectedCartItemTotalCount}개를 주문합니다. 최종 결제 금액을 확인해주세요.`}
+        />
+        {renderSelectedItemListSection()}
       </S.Layout>
       <SubmitButton isActive={false} content="결제하기" />
     </div>
