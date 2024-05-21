88493227a7ff8366578f0484af649248f1e40002
00kang <kdb070970@gmail.com>
2024-05-21 15:16:01 +0900

    refactor : useFetchCartItems 커스텀 훅으로 분리

--- src/pages/cartPage/CartPage.tsx
+++ src/pages/cartPage/CartPage.tsx
@@ -1,31 +1,16 @@
-import { useEffect } from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { getCartItems } from "../../api";
+import { useRecoilValue } from "recoil";
 import { ConfirmButton } from "../../components/button";
 import { CartContentSection } from "../../components/cartContentSection/CartContentSection";
 import { CartHeader } from "../../components/cartHeader/CartHeader";
 import Header from "../../components/header/Header";
-import { BUTTON_COLORS, CART, ERROR_MESSAGES, HEADER_TYPES } from "../../constants";
-import { cartItemsState } from "../../recoil/atoms/atoms";
+import { BUTTON_COLORS, CART, HEADER_TYPES } from "../../constants";
 import { cartSummarySelectorState } from "../../recoil/selector/selector";
 import { StyledCartPage } from "./CartPage.styled";
+import { useFetchCartItems } from "../../hooks/useFetchCartItems";
 
 export const CartPage: React.FC = () => {
-  const [cartItems, setCartItems] = useRecoilState(cartItemsState);
+  const { cartItems } = useFetchCartItems();
   const { uniqueItemCount } = useRecoilValue(cartSummarySelectorState);
-
-  useEffect(() => {
-    const fetchCartItems = async () => {
-      try {
-        const items = await getCartItems();
-        setCartItems(items);
-      } catch (error) {
-        console.error(ERROR_MESSAGES.FETCH_CART_ITEMS, error);
-      }
-    };
-
-    fetchCartItems();
-  }, []);
 
   const buttonMode =
     uniqueItemCount === CART.EMPTY_THRESHOLD ? BUTTON_COLORS.LIGHT : BUTTON_COLORS.DARK;
