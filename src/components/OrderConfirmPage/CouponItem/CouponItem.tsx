9bc52997804c9ec00578916005ca8c0863064d2b
anttiey <hanuuny@gmail.com>
2024-05-22 18:43:16 +0900

    refactor(CouponModal): 쿠폰 정보 문자열 포맷팅 적용

--- src/components/OrderConfirmPage/CouponItem/CouponItem.tsx
+++ src/components/OrderConfirmPage/CouponItem/CouponItem.tsx
@@ -1,6 +1,7 @@
 import useCouponApplicable from '../../../hooks/useCouponApplicable';
 import CheckBox from '../../common/CheckBox/CheckBox';
 import type { Coupon } from '../../../types/Coupon.type';
+import { formatExpirationDate, formatAvailableTime } from '../../../utils/format';
 
 import * as S from './CouponItems.style';
 
@@ -34,11 +35,12 @@ function CouponItem({ coupon, isSelected, isMaxLength, handleSelectedCoupons }: 
         <p>{description}</p>
       </S.CouponTitle>
       <S.CouponDetail>
-        <p>만료일: {expirationDate}</p>
-        {minimumAmount && <p>최소 주문 금액: {minimumAmount}</p>}
+        <p>만료일: {formatExpirationDate(expirationDate)}</p>
+        {minimumAmount && <p>최소 주문 금액: {minimumAmount.toLocaleString()}원</p>}
         {availableTime && (
           <p>
-            사용 가능 시간: {availableTime?.start}부터 {availableTime?.end}까지
+            사용 가능 시간: {formatAvailableTime(availableTime?.start)}부터 {formatAvailableTime(availableTime?.end)}
+            까지
           </p>
         )}
       </S.CouponDetail>
