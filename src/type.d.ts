68ba14132af4f5bfc59102176479dfc393b2032f
ss0526100 <ss0526100@gmail.com>
2024-05-22 20:00:07 +0900

    feat: 쿠폰 목데이터 생성

--- src/type.d.ts
+++ src/type.d.ts
@@ -11,3 +11,40 @@ export interface CartItem {
   quantity: number;
   product: Product;
 }
+
+interface CouponBase {
+  id: number;
+  code: string;
+  description: string;
+  expirationDate: string;
+  discountType: string;
+}
+
+export interface FixedDiscountCoupon extends CouponBase {
+  discountType: 'fixed';
+  discount: number;
+  minimumAmount: number;
+}
+
+export interface BuyXGetYCoupon extends CouponBase {
+  discountType: 'buyXgetY';
+  buyQuantity: number;
+  getQuantity: number;
+}
+
+export interface FreeShippingCoupon extends CouponBase {
+  discountType: 'freeShipping';
+  minimumAmount: number;
+}
+
+export interface PercentageDiscountCoupon extends CouponBase {
+  discountType: 'percentage';
+  discount: number;
+  availableTime: { start: string; end: string };
+}
+
+export type Coupon =
+  | FixedDiscountCoupon
+  | BuyXGetYCoupon
+  | FreeShippingCoupon
+  | PercentageDiscountCoupon;
