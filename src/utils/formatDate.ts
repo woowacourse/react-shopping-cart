25e5e853c944260105ebd9e9aca36065c5f45222
00kang <kdb070970@gmail.com>
2024-05-22 21:31:10 +0900

    refactor : 포맷팅하는 formatDate와 formatTime 메서드 구현

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,8 @@
+export const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  return `${year}년 ${month}월 ${day}일`;
+};
