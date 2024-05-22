6f5cadc4901fde6eb52dc2b3638003042b1823ea
river <jenn0.6n@gmail.com>
2024-05-22 17:09:07 +0900

    feat: CartItem 컴포넌트 props 추가
    - widthHeader, widthCounter props를 추가하여 재활용성 높임

--- src/components/CartItem/index.tsx
+++ src/components/CartItem/index.tsx
@@ -16,30 +16,45 @@ import BorderButton from "../common/BorderButton";
 import CheckBox from "../common/CheckBox";
 import CartItemQuantity from "./CartItemQuantity";
 
+import { useRecoilValue } from "recoil";
 import useDeleteCartItem from "../../hooks/useDeleteCartItem";
 import useSelectCartItem from "../../hooks/useSelectCartItem";
+import { cartItemQuantity } from "../../recoil/atoms";
 import formatPriceToKoreanWon from "../../util/formatPriceToKoreanWon";
 
 interface CartItemProps {
   cartItem: CartItemType;
+  widthHeader?: boolean;
+  widthCounter?: boolean;
 }
 
-export default function CartItem({ cartItem: { id, product } }: CartItemProps) {
+export default function CartItem({
+  cartItem: { id, product },
+  widthHeader = false,
+  widthCounter = false,
+}: CartItemProps) {
   const { handleDelete } = useDeleteCartItem(id);
   const { isSelected, toggleSelected } = useSelectCartItem(id);
+  const quantity = useRecoilValue(cartItemQuantity(id));
 
   return (
     <CartItemContainer key={id}>
-      <CartItemHeader>
-        <CheckBox
-          isSelected={isSelected}
-          toggleSelected={toggleSelected}
-          id={`checkbox_${id}`}
-        />
-        <BorderButton className="deleteBtn" onClick={handleDelete} size="large">
-          삭제
-        </BorderButton>
-      </CartItemHeader>
+      {widthHeader && (
+        <CartItemHeader>
+          <CheckBox
+            isSelected={isSelected}
+            toggleSelected={toggleSelected}
+            id={`checkbox_${id}`}
+          />
+          <BorderButton
+            className="deleteBtn"
+            onClick={handleDelete}
+            size="large"
+          >
+            삭제
+          </BorderButton>
+        </CartItemHeader>
+      )}
 
       <CartItemContent>
         <ItemImage src={product.imageUrl} />
@@ -48,7 +63,11 @@ export default function CartItem({ cartItem: { id, product } }: CartItemProps) {
             <ProductName>{product.name}</ProductName>
             <Price>{formatPriceToKoreanWon(product.price)}</Price>
           </CartItemInfo>
-          <CartItemQuantity itemId={id} />
+          {widthCounter ? (
+            <CartItemQuantity itemId={id} />
+          ) : (
+            <div>{quantity}개</div>
+          )}
         </CartItemDetails>
       </CartItemContent>
     </CartItemContainer>
