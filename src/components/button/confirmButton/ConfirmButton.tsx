1f8705ddb2a2e346c73074e7fd41242565311dfa
00kang <kdb070970@gmail.com>
2024-05-22 13:37:22 +0900

    refactor : ConfirmButton의 클릭 이벤트 수정

--- src/components/button/confirmButton/ConfirmButton.tsx
+++ src/components/button/confirmButton/ConfirmButton.tsx
@@ -1,24 +1,19 @@
-import { useNavigate } from "react-router-dom";
-import { BUTTON_COLORS, PATHS } from "../../../constants";
+import { BUTTON_COLORS } from "../../../constants";
 import { StyledConfirmButton } from "./ConfirmButton.styled";
 
 export interface ConfirmButtonProps {
   text: string;
   mode?: keyof typeof BUTTON_COLORS;
+  onClick: () => void;
 }
 
 export const ConfirmButton: React.FC<ConfirmButtonProps> = ({
   text,
   mode = BUTTON_COLORS.DARK,
+  onClick = () => {},
 }) => {
-  const navigate = useNavigate();
-
-  const navigateToPaymentsConfirmationPage = () => {
-    navigate(PATHS.PAYMENTS_CONFIRMATION);
-  };
-
   return (
-    <StyledConfirmButton onClick={navigateToPaymentsConfirmationPage} mode={mode}>
+    <StyledConfirmButton onClick={onClick} mode={mode}>
       {text}
     </StyledConfirmButton>
   );
