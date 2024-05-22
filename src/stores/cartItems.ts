35613ed88cbc8dc08fc1b554f991d65e0da8b771
Yoonkyoung Kim <dev.yoonkyoung@gmail.com>
2024-05-22 14:02:48 +0900

    refactor: cartItemsState를 atom과 selector로 분리하여 상태 관리 구조 개선

--- src/stores/cartItems.ts
+++ src/stores/cartItems.ts
@@ -2,10 +2,16 @@ import { atom, selector } from "recoil";
 import { getCartItems } from "@/apis/cart";
 import { CartItem } from "@/types/cart";
 
-export const cartItemsState = selector<CartItem[]>({
-  key: "cartItemsState",
+export const cartItemsStateResponse = selector<CartItem[]>({
+  key: "cartItemsStateResponse",
   get: async () => {
     const cartItems = await getCartItems();
+
     return cartItems;
   },
+});
+
+export const cartItemsState = atom<CartItem[]>({
+  key: "cartItemsState",
+  default: cartItemsStateResponse,
 });
