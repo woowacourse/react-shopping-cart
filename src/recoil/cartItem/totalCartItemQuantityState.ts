2c63285575b749156d399f74ba37391c14d25a68
pakxe <pigkill40@naver.com>
2024-05-23 01:26:35 +0900

    feat: 선택된 상품 목록을 저장하는 상태를 사용해 파생 상태를 계산하도록 수정

--- src/recoil/cartItem/totalCartItemQuantityState.ts
+++ src/recoil/cartItem/totalCartItemQuantityState.ts
@@ -1,17 +1,12 @@
 import { selector } from 'recoil';
-import { cartItemQuantityFamilyState } from './cartItemQuantityFamilyState';
-import { selectedCartItemIdListState } from '../selectedCartItemIdList/selectedCartItemIdListState';
+import { selectedCartItemListState } from '../selectedCartItemList/selectedCartItemList';
 
 export const totalCartItemQuantitySelector = selector<number>({
   key: 'totalCartItemQuantitySelector',
   get: ({ get }) => {
-    const selectedCartItemIdList = get(selectedCartItemIdListState);
+    const selectedCartItemList = get(selectedCartItemListState);
 
-    const totalQuantity = selectedCartItemIdList.reduce((totalQuantity, selectedCartItemId) => {
-      const quantity = get(cartItemQuantityFamilyState(selectedCartItemId));
-
-      return totalQuantity + quantity;
-    }, 0);
+    const totalQuantity = selectedCartItemList.reduce((totalQuantity, { quantity }) => totalQuantity + quantity, 0);
 
     return totalQuantity;
   },
