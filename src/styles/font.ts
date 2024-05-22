76b561a7011a93d042b6701ba6ef42cd306d97c8
river <jenn0.6n@gmail.com>
2024-05-22 16:57:44 +0900

    feat: font style 추가

---
+++ src/styles/font.ts
@@ -0,0 +1,16 @@
+import { css } from "@emotion/react";
+
+export const title = css`
+  font-size: 24px;
+  font-weight: 700;
+`;
+
+export const subtitle = css`
+  font-size: 16px;
+  font-weight: 700;
+`;
+
+export const caption = css`
+  font-size: 12px;
+  font-weight: 500;
+`;
