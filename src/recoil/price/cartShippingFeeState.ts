31c3052e035a23739c5e989b16b6fd0ab23aa575
pakxe <pigkill40@naver.com>
2024-05-23 01:23:11 +0900

    feat: 배송비를 계산할 때 쿠폰에 의한 배송비 할인도 적용하도록 수정

--- src/recoil/price/cartShippingFeeState.ts
+++ src/recoil/price/cartShippingFeeState.ts
@@ -1,13 +1,17 @@
 import { selector } from 'recoil';
 import { totalCartPriceState } from './totalCartPriceState';
 import { SHIPPING_FEE } from '../../constants/price';
+import { discountPriceByCouponListState } from './discountPriceByCouponListState';
 
 export const cartShippingFeeState = selector<number>({
   key: 'cartShippingFeeState',
   get: ({ get }) => {
     const totalCartPrice = get(totalCartPriceState);
+    const { shippingFee } = get(discountPriceByCouponListState);
 
     if (totalCartPrice >= SHIPPING_FEE.FREE_THRESHOLD || totalCartPrice <= 0) return 0;
-    else return SHIPPING_FEE.DEFAULT;
+    if (shippingFee === 'free') return 0;
+
+    return SHIPPING_FEE.DEFAULT;
   },
 });
