b776cfa94e669f07036ae67c5a668513842c18e0
pakxe <pigkill40@naver.com>
2024-05-23 02:06:28 +0900

    feat: 조작 기능이 없는 CartItem 컴포넌트 구현

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -1,24 +1,15 @@
 import { useEffect } from 'react';
 
 import * as S from './CartItem.style';
-
-import Button from '../common/Button/Button';
-import ChangeQuantity from '../common/ChangeQuantity/ChangeQuantity';
-import Checkbox from '../common/Checkbox/Checkbox';
+import { useCartItemQuantity } from '../../recoil/cartItem/useCartItemQuantity';
 import Divider from '../common/Divider/Divider';
 import ImageBox from '../common/ImageBox/ImageBox';
 import Text from '../common/Text/Text';
-
 import type { CartItem } from '../../types/cartItem.type';
-import { useCartItemQuantity } from '../../recoil/cartItem/useCartItemQuantity';
-import { useSelectedCartItemIdList } from '../../recoil/selectedCartItemList/useSelectedCartItemIdList';
-import { useCartItemList } from '../../recoil/cartItemList/useCartItemList';
 
 const CartItem = ({ product, quantity: initialQuantity, cartItemId }: CartItem) => {
   const { name, price, imageUrl } = product;
   const { quantity, setQuantity, increaseQuantity, decreaseQuantity } = useCartItemQuantity(cartItemId);
-  const { getIsSelected, addSelectedItemId, deleteSelectedItemId } = useSelectedCartItemIdList();
-  const { deleteCartItem } = useCartItemList();
 
   useEffect(() => {
     setQuantity(initialQuantity);
@@ -27,18 +18,7 @@ const CartItem = ({ product, quantity: initialQuantity, cartItemId }: CartItem) 
   return (
     <S.CartItem>
       <Divider />
-      <S.ItemHeader>
-        <Checkbox
-          checked={getIsSelected(cartItemId)}
-          handleClick={
-            getIsSelected(cartItemId) ? () => deleteSelectedItemId(cartItemId) : () => addSelectedItemId(cartItemId)
-          }
-          alt="Checkbox"
-        />
-        <Button size="s" radius="s" onClick={() => deleteCartItem(cartItemId)}>
-          삭제
-        </Button>
-      </S.ItemHeader>
+
       <S.ItemBody>
         <ImageBox width={112} height={112} radius="m" border="lightGray" src={imageUrl} />
         <S.ItemDetail>
@@ -50,7 +30,9 @@ const CartItem = ({ product, quantity: initialQuantity, cartItemId }: CartItem) 
               {`${price.toLocaleString('ko-KR')}원`}
             </Text>
           </S.ItemNameAndCost>
-          <ChangeQuantity quantity={quantity} increaseQuantity={increaseQuantity} decreaseQuantity={decreaseQuantity} />
+          <Text size="s" weight="s">
+            {quantity}개
+          </Text>
         </S.ItemDetail>
       </S.ItemBody>
     </S.CartItem>
