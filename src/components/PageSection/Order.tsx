c701777f81dccaf3b610ab9bb41af25f49db5c53
Soyeon Choe <soy2302ten@gmail.com>
2024-05-22 17:33:52 +0900

    chore: OrderList을 OrderProductList로 이름 변경 반영

--- src/components/PageSection/Order.tsx
+++ src/components/PageSection/Order.tsx
@@ -2,7 +2,7 @@ import { Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 
 import OrderHeader from '../PageHeader/OrderHeader';
-import OrderList from '../ProductList/OrderList';
+import OrderProductList from '../ProductList/OrderProductList';
 import ErrorFallback from '../ErrorFallback/ErrorFallback';
 
 import * as O from './PageSection.style';
@@ -10,10 +10,11 @@ import * as O from './PageSection.style';
 export default function Order() {
   return (
     <O.PageSectionStyle>
+      {/* category 수량과 총 주문 수량 데이터 받아서 뿌리기 */}
       <OrderHeader category={1} count={1} />
       <ErrorBoundary FallbackComponent={ErrorFallback}>
         <Suspense fallback={<O.Loading>로딩중!</O.Loading>}>
-          <OrderList />
+          <OrderProductList />
         </Suspense>
       </ErrorBoundary>
     </O.PageSectionStyle>
