14a6f3c5addbe89a0365547e28b115ed0fa1556c
pakxe <pigkill40@naver.com>
2024-05-23 01:41:16 +0900

    feat: 도서 산간 지역인 경우 배송비를 추가하도록 로직 구현

--- src/recoil/price/cartShippingFeeState.ts
+++ src/recoil/price/cartShippingFeeState.ts
@@ -2,16 +2,20 @@ import { selector } from 'recoil';
 import { totalCartPriceState } from './totalCartPriceState';
 import { SHIPPING_FEE } from '../../constants/price';
 import { discountPriceByCouponListState } from './discountPriceByCouponListState';
+import { isMountainIslandAreaState } from './isMountainIslandAreaState';
 
 export const cartShippingFeeState = selector<number>({
   key: 'cartShippingFeeState',
   get: ({ get }) => {
     const totalCartPrice = get(totalCartPriceState);
-    const { shippingFee } = get(discountPriceByCouponListState);
+    const totalDiscount = get(discountPriceByCouponListState);
+    const isMountainIslandArea = get(isMountainIslandAreaState);
 
-    if (totalCartPrice >= SHIPPING_FEE.FREE_THRESHOLD || totalCartPrice <= 0) return 0;
-    if (shippingFee === 'free') return 0;
+    const shippingFee = SHIPPING_FEE.FREE_THRESHOLD + (isMountainIslandArea ? SHIPPING_FEE.MOUNTAIN_ISLAND_AREA : 0);
 
-    return SHIPPING_FEE.DEFAULT;
+    if (totalCartPrice >= shippingFee || totalCartPrice <= 0) return 0;
+    if (totalDiscount.shippingFee === 'free') return 0;
+
+    return shippingFee;
   },
 });
