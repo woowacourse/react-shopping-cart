deb2a09fbe1a80bfcfd7bcade103ff8012093045
pakxe <pigkill40@naver.com>
2024-05-23 01:26:16 +0900

    feat: 쿠폰 목록을 atom으로 저장하지 않도록 수정

--- src/recoil/couponList/couponListState.ts
+++ src/recoil/couponList/couponListState.ts
@@ -1,19 +1,11 @@
-import { atom, selector } from 'recoil';
+import { selector } from 'recoil';
 import type { Coupon } from '../../types/coupon.type';
 import { requestCouponList } from '../../apis/requests/couponList';
-
-export const couponListState = atom<Coupon[]>({
-  key: 'couponListState',
-  default: [],
-});
 
 export const couponListStateQuery = selector<Coupon[]>({
   key: 'couponListStateQuery',
   get: async () => {
     const couponList = await requestCouponList();
     return couponList;
-  },
-  set: ({ set }, couponList) => {
-    set(couponListState, couponList);
   },
 });
