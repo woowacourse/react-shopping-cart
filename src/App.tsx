7e9689e55c320ba26e3973ed226086ef9c33144a
simhorim <hori5020@gmail.com>
2024-05-23 15:39:48 +0900

    refactor(App): 경로 상수화에 따른 수정

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,22 @@ import ErrorFallback from './components/ErrorFallback/ErrorFallback';
 
 import { Route, Routes } from 'react-router-dom';
 import PaymentConfirmation from './components/pages/PaymentConfirmation/PaymentConfirmation';
+import OrderConfirmation from './components/pages/OrderConfirmation/OrderConfirmation';
+import CONDITION from './constants/Condition';
 
 function App() {
   return (
     <ErrorBoundary FallbackComponent={ErrorFallback}>
       <Routes>
-        <Route path="/" element={<ShoppingCart />} />
-        <Route path="/paymentConfirmation" element={<PaymentConfirmation />} />
+        <Route path={CONDITION.shoppingCartPage} element={<ShoppingCart />} />
+        <Route
+          path={CONDITION.orderConfirmationPage}
+          element={<OrderConfirmation />}
+        />
+        <Route
+          path={CONDITION.paymentConfirmationPage}
+          element={<PaymentConfirmation />}
+        />
       </Routes>
     </ErrorBoundary>
   );
