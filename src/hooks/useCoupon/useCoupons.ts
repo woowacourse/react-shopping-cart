23132a0e3ed566dcd5cd1985faccf08c516ebb0f
Largopie <106071687+Largopie@users.noreply.github.com>
2024-05-23 11:05:56 +0900

    remove: 불필요한 로직 삭제

--- src/hooks/useCoupon/useCoupons.ts
+++
@@ -1,9 +0,0 @@
-import { useRecoilValue } from 'recoil';
-
-import { couponsState } from '@recoil/coupon/atom';
-
-export const useCoupons = () => {
-  const coupons = useRecoilValue(couponsState);
-
-  return { coupons };
-};
