c1a1f1cbb9979afff63cb3318cdcc6f2add90bff
Largopie <106071687+Largopie@users.noreply.github.com>
2024-05-23 13:28:59 +0900

    refactor: SeparateLine 컴포넌트 분리

--- src/components/Cart/CartOrderInfo/index.tsx
+++ src/components/Cart/CartOrderInfo/index.tsx
@@ -3,6 +3,7 @@ import { useRecoilValue } from 'recoil';
 
 import Receipt from './Receipt';
 
+import SeparateLine from '@/components/common/SeparateLine';
 import { MINIMUM_FREE_SHIPPING_AMOUNT } from '@/constants/cart';
 import InformationText from '@components/common/InformationText';
 import { orderTotalPriceState, deliveryPriceState } from '@recoil/cartItems/selectors';
@@ -17,11 +18,14 @@ export default function CartOrderInfo() {
         총 주문 금액이 {MINIMUM_FREE_SHIPPING_AMOUNT.toLocaleString('ko-KR')}원 이상일 경우 무료
         배송됩니다.
       </InformationText>
-      <div css={borderTopWrapper}>
+
+      <SeparateLine />
+      <div css={beforeCalculateWrapper}>
         <Receipt description="주문 금액" price={totalPrice} />
         <Receipt description="배송비" price={deliveryPrice} />
-        <Receipt description="총 결제 금액" price={totalPrice + deliveryPrice} />
       </div>
+      <SeparateLine />
+      <Receipt description="총 결제 금액" price={totalPrice + deliveryPrice} />
     </div>
   );
 }
@@ -29,9 +33,11 @@ export default function CartOrderInfo() {
 const cartOrderInfoContainer = css`
   display: flex;
   flex-direction: column;
-  gap: 12px;
 `;
 
-const borderTopWrapper = css`
-  border-top: 1px solid #0000001a;
+const beforeCalculateWrapper = css`
+  display: flex;
+  flex-direction: column;
+
+  gap: 8px;
 `;
