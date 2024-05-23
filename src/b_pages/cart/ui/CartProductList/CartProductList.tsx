39e375ee7e7390c7314517b4972b4724aa1d5bec
vi-wolhwa <cerezo00@naver.com>
2024-05-23 16:56:31 +0900

    feat(CartProductList): Add 'CheckAllButton'

--- src/b_pages/cart/ui/CartProductList/CartProductList.tsx
+++ src/b_pages/cart/ui/CartProductList/CartProductList.tsx
@@ -1,6 +1,9 @@
 import { BaseProductList } from '@/c_widgets/BaseProductList';
 import { CheckCartButton, DeleteCartButton, CartQuantityAdjuster } from '@/d_features/cart';
+import { CheckAllCartButton } from '@/d_features/cart/ui/CheckAllCartButton/CheckAllCartButton';
 import { Cart } from '@/e_entities/cart';
+
+import css from './CartProductList.module.css';
 
 interface CartProductListProps {
   carts: Cart[];
@@ -8,11 +11,14 @@ interface CartProductListProps {
 
 export const CartProductList = ({ carts }: CartProductListProps) => {
   return (
-    <BaseProductList
-      carts={carts}
-      cardLeftActionSlot={(cartId) => <CheckCartButton cartId={cartId} />}
-      cardRightActionSlot={(cartId) => <DeleteCartButton cartId={cartId} />}
-      cardCounterSlot={(cartId, quantity) => <CartQuantityAdjuster cartId={cartId} quantity={quantity} />}
-    />
+    <div className={css.root}>
+      <CheckAllCartButton />
+      <BaseProductList
+        carts={carts}
+        cardLeftActionSlot={(cartId) => <CheckCartButton cartId={cartId} />}
+        cardRightActionSlot={(cartId) => <DeleteCartButton cartId={cartId} />}
+        cardCounterSlot={(cartId, quantity) => <CartQuantityAdjuster cartId={cartId} quantity={quantity} />}
+      />
+    </div>
   );
 };
