a6b3ff32479d3a096be8b838f1165b3a7656e913
skiende74 <skiende74@gmail.com>
2024-05-24 01:52:00 +0900

    feat(Coupon): 쿠폰 적용조건 구현 및 최소금액 조건 테스트 추가

--- src/recoil/coupon/couponState.ts
+++ src/recoil/coupon/couponState.ts
@@ -1,15 +1,7 @@
 import { selector } from "recoil";
 import { fetchCouponList } from "../../api/coupon";
-import { mockCouponList } from "../mocks";
 
 export const fetchCouponListSelector = selector({
   key: "fetchCouponListSelector",
   get: fetchCouponList,
-});
-
-jest.mock("../api/coupon", () => ({
-  fetchCouponList: () => mockCouponList,
-}));
-describe("couponState 테스트", () => {
-  it("");
 });
