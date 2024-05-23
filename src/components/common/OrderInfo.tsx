e46e0630d896d8752e68948fce647d037b082898
rbgksqkr <rbgks1937@naver.com>
2024-05-23 19:45:21 +0900

    feat: 먼 지역 배송 체크 시 10만원 이상이면 체크해도 무료 배송

--- src/components/common/OrderInfo.tsx
+++ src/components/common/OrderInfo.tsx
@@ -7,7 +7,11 @@ import { calculateShippingPrice } from '../Cart/utils';
 
 import { totalDiscountPriceState } from '@/recoil/coupons/atoms';
 import { CONFIG } from '@constants/config';
-import { orderResultState, shippingPriceState } from '@recoil/cartItems/selectors';
+import {
+  orderResultState,
+  shippingPriceState,
+  totalPurchasePriceState,
+} from '@recoil/cartItems/selectors';
 
 interface CartFooterProps {
   type: 'CART' | 'ORDER';
@@ -16,12 +20,14 @@ interface CartFooterProps {
 const OrderInfo = ({ type }: CartFooterProps) => {
   const totalDiscountPrice = useRecoilValue(totalDiscountPriceState);
   const { totalOrderPrice } = useRecoilValue(orderResultState);
+  const totalPurchasePrice = useRecoilValue(totalPurchasePriceState);
 
   const shippingPriceWithCoupon = useRecoilValue(shippingPriceState);
   const shippingPrice = calculateShippingPrice(totalOrderPrice);
   const finalShippingPrice = type === 'ORDER' ? shippingPriceWithCoupon : shippingPrice;
 
-  const totalPurchasePrice = totalOrderPrice + finalShippingPrice;
+  const finalPurchasePrice =
+    type === 'ORDER' ? totalPurchasePrice : totalOrderPrice + finalShippingPrice;
 
   return (
     <section css={cartFooterSection}>
@@ -31,11 +37,16 @@ const OrderInfo = ({ type }: CartFooterProps) => {
       <div>
         <div css={borderTopWrapper}>
           <Receipt title="주문 금액" price={totalOrderPrice} />
-          {type === 'ORDER' && <Receipt title="쿠폰 할인 금액" price={totalDiscountPrice} />}
+          {type === 'ORDER' && (
+            <Receipt
+              title="쿠폰 할인 금액"
+              price={totalDiscountPrice > 0 ? totalDiscountPrice * -1 : 0}
+            />
+          )}
           <Receipt title="배송비" price={finalShippingPrice} />
         </div>
         <div css={borderTopWrapper}>
-          <Receipt title="총 결제 금액" price={totalPurchasePrice} />
+          <Receipt title="총 결제 금액" price={finalPurchasePrice} />
         </div>
       </div>
     </section>
