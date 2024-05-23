ce927f79d818a90a6342497f16a368747bccf1ea
Soyeon Choe <soy2302ten@gmail.com>
2024-05-23 09:28:30 +0900

    feat: OrderHeader의 props에 서버로 받은 주문한 제품의 데이터를 넣기

--- src/components/PageSection/Order.tsx
+++ src/components/PageSection/Order.tsx
@@ -6,12 +6,18 @@ import OrderProductList from '../ProductList/OrderProductList';
 import ErrorFallback from '../ErrorFallback/ErrorFallback';
 
 import * as O from './PageSection.style';
+import { useRecoilValue } from 'recoil';
+import { checkedCartItems } from '../../recoil/selectors';
 
 export default function Order() {
+  const order = useRecoilValue(checkedCartItems);
+  const orderTotalQuantity = order.reduce((acc, item) => {
+    return acc + item.quantity;
+  }, 0);
+
   return (
     <O.PageSectionStyle>
-      {/* TODO: category 수량과 총 주문 수량 데이터 받아서 뿌리기 */}
-      <OrderHeader category={1} count={1} />
+      <OrderHeader category={order.length} count={orderTotalQuantity} />
       <ErrorBoundary FallbackComponent={ErrorFallback}>
         <Suspense fallback={<O.Loading>로딩중!</O.Loading>}>
           <OrderProductList />
