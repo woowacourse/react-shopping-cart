35093c242ba443213425a5ba2067a56e4d96305a
김진호 <rlawlsgh1227@gmail.com>
2024-05-23 13:20:39 +0900

    feat: 유효기간을 검사하는 훅 구현

---
+++ src/hooks/coupon/useExpirationDate.ts
@@ -0,0 +1,14 @@
+import dayjs from '../../utils/dayjs';
+
+const useExpirationDate = () => {
+  const isExpired = (dateString: string) => {
+    const targetTime = dayjs(dateString);
+    const now = dayjs();
+
+    return now.isAfter(targetTime);
+  };
+
+  return { isExpired };
+};
+
+export default useExpirationDate;
