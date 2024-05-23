7d1fe557ef6ab09f0f1b2dc8b099e19a6eca12ff
badahertz52 <badahertz52@gmail.com>
2024-05-23 14:25:23 +0900

    feat: 주문 확인 페이지의 가격 정보 컴포넌트(OrderAmountsList) 추가

--- src/components/orderConfirm/index.tsx
+++ src/components/orderConfirm/index.tsx
@@ -1,4 +1,5 @@
 export { default as CouponModal } from './CouponModal/CouponModal';
 export { default as CouponModalButton } from './CouponModalButton/CouponModalButton';
+export { default as OrderAmountsList } from './OrderAmountsList/OrderAmountsList';
 export { default as ShippingInfo } from './ShippingInfo/ShippingInfo';
 export { default as SelectedItemList } from './SelectedItemList/SelectedItemList';
