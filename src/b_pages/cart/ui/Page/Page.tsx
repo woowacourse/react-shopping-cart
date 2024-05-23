e8a7ce4eb2b7a8554d3bfe884dbb0e3fb44a6833
vi-wolhwa <cerezo00@naver.com>
2024-05-23 14:48:40 +0900

    feat(CartPage)

---
+++ src/b_pages/cart/ui/Page/Page.tsx
@@ -0,0 +1,28 @@
+import { ContentHeader } from '@/c_widgets/ContentHeader';
+import { LayoutHeader } from '@/c_widgets/LayoutHeader';
+import { OrderCartButton } from '@/d_features/cart';
+import { Layout } from '@/f_shared';
+
+import { mockCarts } from '../../../../../mocks/index';
+import { CartProductList } from '../CartProductList/CartProductList';
+import { CartSummary } from '../CartSummary/CartSummary';
+
+export const CartPage = () => {
+  // TODO: Connect to state
+  const cartItemCount = 2;
+  // const cartItemCount = useRecoilValue(CartItemCountState);
+  const carts = mockCarts;
+
+  return (
+    <Layout
+      headerSlot={<LayoutHeader middleSlotType='logo' />}
+      contentHeaderSlot={
+        <ContentHeader title={'장바구니'} desc={`현재 ${cartItemCount}종류의 상품이 담겨있습니다.`}></ContentHeader>
+      }
+      contentBodySlot={<CartProductList carts={carts} />}
+      contentFooterSlot={<CartSummary />}
+      footerSlot={<OrderCartButton />}
+      gap={{ top: 36, bottom: 52 }}
+    />
+  );
+};
