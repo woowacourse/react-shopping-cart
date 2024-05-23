65118cc5f290d49b0cf458667f3d30e0160d494a
chysis <chysiss@naver.com>
2024-05-23 21:34:57 +0900

    chore: 주문 페이지 컴포넌트에서 모달 분리

--- src/components/Order/OrderContainer/OrderContainer.style.ts
+++ src/components/Order/OrderContainer/OrderContainer.style.ts
@@ -29,21 +29,3 @@ export const ShippingOptionCheckboxStyle = css({
     fontWeight: "500",
   },
 });
-
-export const CouponModalContainerStyle = css({
-  maxHeight: "70vh",
-  overflow: "auto",
-});
-
-export const CouponGuideContainerStyle = css({
-  display: "flex",
-  alignItems: "center",
-
-  gap: "4px",
-  margin: "17px 0",
-});
-
-export const CouponGuideStyle = css({
-  fontSize: "12px",
-  fontWeight: "500",
-});
