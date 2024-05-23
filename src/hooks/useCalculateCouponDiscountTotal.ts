5a7039453581fe8e8ee1e10076f00067384661ff
greetings1012 <ahnqwerty@gmail.com>
2024-05-23 11:40:46 +0900

    feat(useCalculateCouponDiscountTotal): 각 쿠폰의 할인을 계산하는 훅 구현

---
+++ src/hooks/useCalculateCouponDiscountTotal.ts
@@ -0,0 +1,36 @@
+import { useRecoilValue } from 'recoil';
+import { selectedCartItemListState } from '../recoil/CartItem/atoms/selectedCartItemListState';
+import { deliveryFeeState } from '../recoil/DeliveryFee/atoms/deliveryFeeState';
+import { Coupon } from '../types/Coupon.type';
+import { selectedCartItemTotalPriceState } from '../recoil/CartItem/atoms/selectedCartItemTotalPriceState';
+
+export function useCalculateCouponDiscount() {
+  const deliveryFee = useRecoilValue(deliveryFeeState);
+  const selectedCartItemList = useRecoilValue(selectedCartItemListState);
+  const selectedCartItemTotalPrice = useRecoilValue(selectedCartItemTotalPriceState);
+
+  const calculateCouponDiscount = (coupon: Coupon) => {
+    switch (coupon.discountType) {
+      case 'fixed': {
+        return coupon.discount!;
+      }
+      case 'buyXgetY': {
+        const x = coupon.buyQuantity!;
+        const y = coupon.getQuantity!;
+        const eligibleItems = selectedCartItemList.filter((item) => item.quantity >= x + y);
+        const mostExpensiveItem = eligibleItems.reduce(
+          (maxItem, item) => (item.product.price > maxItem.product.price ? item : maxItem),
+          eligibleItems[0],
+        );
+        return mostExpensiveItem.product.price * y;
+      }
+      case 'freeShipping': {
+        return deliveryFee;
+      }
+      case 'percentage': {
+        return Math.round(selectedCartItemTotalPrice * (coupon.discount! / 100));
+      }
+    }
+  };
+  return { calculateCouponDiscount };
+}
