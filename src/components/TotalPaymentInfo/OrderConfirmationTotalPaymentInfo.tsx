ab65750d7f8ef65cd5d87f0604ae8c9dbcdd47ef
simhorim <hori5020@gmail.com>
2024-05-23 19:07:05 +0900

    design(OrderConfirmationTotalPaymentInfo): UI 구현

---
+++ src/components/TotalPaymentInfo/OrderConfirmationTotalPaymentInfo.tsx
@@ -0,0 +1,40 @@
+import * as Styled from './style';
+
+import PaymentInfo from './PaymentInfo';
+import CaptionEmoji from '../assets/CaptionEmoji.svg';
+
+import { useRecoilValue } from 'recoil';
+
+import {
+  shippingFeeSelector,
+  totalOrderAmountSelector,
+  totalPaymentAmountSelector,
+} from '../../recoil/selectors';
+
+import MESSAGE from '../../constants/Message';
+
+const OrderConfirmationTotalPaymentInfo = () => {
+  const totalOrderAmount = useRecoilValue(totalOrderAmountSelector);
+  const shippingFee = useRecoilValue(shippingFeeSelector);
+  const totalPaymentAmount = useRecoilValue(totalPaymentAmountSelector);
+
+  return (
+    <Styled.TotalPaymentInfo>
+      <Styled.PaymentCaptionContainer>
+        <img src={CaptionEmoji} />
+        <Styled.PaymentCaption>{MESSAGE.paymentCaption}</Styled.PaymentCaption>
+      </Styled.PaymentCaptionContainer>
+      <Styled.Divider />
+      <PaymentInfo label={MESSAGE.paymentAmount} price={totalOrderAmount} />
+      <PaymentInfo label={MESSAGE.couponDiscountAmount} price={-6000} />
+      <PaymentInfo label={MESSAGE.shippingFee} price={shippingFee} />
+      <Styled.Divider />
+      <PaymentInfo
+        label={MESSAGE.totalPaymentAmount}
+        price={totalPaymentAmount}
+      />
+    </Styled.TotalPaymentInfo>
+  );
+};
+
+export default OrderConfirmationTotalPaymentInfo;
