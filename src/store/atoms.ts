2893b110a5ca41ec7f3d5175481f23896e7fd23f
brgndy <brgndy96@gmail.com>
2024-05-23 09:27:17 +0900

    feat: 아톰 재생성 및 테스트 코드 작성

--- src/store/atoms.ts
+++ src/store/atoms.ts
@@ -1,15 +1,36 @@
+import ORDER_CONDITION from '@/constants/order';
 import formatCartItems from '@/services/formatCartItem';
 import { atom, selector } from 'recoil';
 import { fetchCartItems } from '../api';
+import { orderAmountSelector } from './selectors';
+import { FormattedCoupon } from '@/types';
 
 export const allCartItemStates = atom({
   key: 'allCartItemStates',
   default: selector({
-    key: 'allCartItemStates/Default',
+    key: 'cartItemsSelector',
     get: async () => {
       const cartItems = await fetchCartItems();
-      const formattedProducts = formatCartItems(cartItems);
-      return formattedProducts;
+      return formatCartItems(cartItems);
     },
   }),
+});
+
+export const isFarShippingLocationState = atom({
+  key: 'isFarShippingLocationState',
+  default: selector({
+    key: 'isFarShippingLocationDefault',
+    get: ({ get }) => {
+      const orderAmount = get(orderAmountSelector);
+      return {
+        isAvailable: orderAmount < ORDER_CONDITION.FREE_SHIPPING_PRICE,
+        isChecked: false,
+      };
+    },
+  }),
+});
+
+export const allCouponStates = atom<FormattedCoupon[] | []>({
+  key: 'allCoupons',
+  default: [],
 });
