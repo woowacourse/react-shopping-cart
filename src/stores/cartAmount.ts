65dab455c40c87123e83348637207554be9aff6f
Yoonkyoung Kim <dev.yoonkyoung@gmail.com>
2024-05-23 15:50:14 +0900

    feat: 베송 지역 상태를 관리하는 atom shippingAreaState 정의

--- src/stores/cartAmount.ts
+++ src/stores/cartAmount.ts
@@ -1,27 +1,41 @@
-import { selector } from "recoil";
+import { atom, selector } from "recoil";
 import { cartItemsState } from "./cartItems";
 import { isCartItemsSelectedState } from "./cartItemSelections";
 
 import { CART_PRICE } from "@/constants/cart";
 
-const calculateDeliveryFee = (orderPrice: number) => {
-  if (orderPrice === 0) return 0;
-  return orderPrice >= CART_PRICE.minOrderAmount ? 0 : CART_PRICE.deliveryFee;
+type ShippingArea = "standard" | "remote";
+
+export const shippingAreaState = atom<ShippingArea>({
+  key: "shoppingAreaState",
+  default: "standard",
+});
+
+const calculateShippingFee = (
+  orderAmount: number,
+  shippingArea: ShippingArea
+) => {
+  if (orderAmount === 0) return 0;
+
+  const shippingFee = CART_PRICE.shippingFees[shippingArea];
+
+  return orderAmount >= CART_PRICE.minOrderAmount ? 0 : shippingFee;
 };
 
-export const cartPriceState = selector({
-  key: "cartPriceState",
+export const cartAmountState = selector({
+  key: "cartAmountState",
   get: ({ get }) => {
     const cartItems = get(cartItemsState);
+    const shippingArea = get(shippingAreaState);
 
-    const orderPrice = cartItems.reduce((acc, item) => {
+    const orderAmount = cartItems.reduce((acc, item) => {
       const isSelected = get(isCartItemsSelectedState(item.id));
       return isSelected ? acc + item.product.price * item.quantity : acc;
     }, 0);
 
-    const deliveryFee = calculateDeliveryFee(orderPrice);
-    const totalPrice = orderPrice + deliveryFee;
+    const shippingFee = calculateShippingFee(orderAmount, shippingArea);
+    const totalAmount = orderAmount + shippingFee;
 
-    return { orderPrice, deliveryFee, totalPrice };
+    return { orderAmount, shippingFee, totalAmount };
   },
 });
