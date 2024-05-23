94f2d5ef2f21863c310e58be6902d942f4b8b526
skiende74 <skiende74@gmail.com>
2024-05-24 01:50:56 +0900

    types(Coupon): 쿠폰의 타입 정의. 쿠폰베이스, 적용가능조건, 할인방식을 각각 정의후, 쿠폰 = 쿠폰베이스&적용가능조건&할인방식. 으로 정의

--- src/types/types.ts
+++ src/types/types.ts
@@ -16,13 +16,28 @@ export interface CouponBase {
   code: string;
   description: string;
   expirationDate: string;
-  discountType: DiscountType;
 }
 
-export type DiscountType = "fixed" | "BuyXGetY" | "freeShipping" | "percentage";
+export type Coupon = CouponBase & DiscountType & DiscountCondition;
+
+export type DiscountType = FixedDiscount | BuyXGetYDiscount | freeShippingDiscount | PercentageDiscount;
+export interface FixedDiscount {
+  discountType: "fixed";
+  discount: number;
+}
+
+export interface BuyXGetYDiscount {
+  discountType: "buyXgetY";
+}
+export interface freeShippingDiscount {
+  discountType: "freeShipping";
+}
+export interface PercentageDiscount {
+  discountType: "percentage";
+  discount: number;
+}
 
 export type DiscountCondition = BuyXGetYCondition | MinimumAmountCondition | AvailableTimeCondition;
-export type DiscountConditionType = "";
 
 export interface BuyXGetYCondition {
   buyQuantity: number;
