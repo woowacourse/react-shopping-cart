29fb0855f9500d48073628504a783b0e9a602a4d
greetings1012 <ahnqwerty@gmail.com>
2024-05-23 11:12:46 +0900

    feat(TotalPriceContainer): 선택된 쿠폰이 있을 경우  쿠폰 할인 금액란이 등장하도록 구현

--- src/components/Container/TotalPriceContainer/TotalPriceContainer.tsx
+++ src/components/Container/TotalPriceContainer/TotalPriceContainer.tsx
@@ -5,10 +5,12 @@ import { selectedCartItemListTotalPriceSelector } from '../../../recoil/CartItem
 import * as S from './TotalPriceContainer.style';
 import { useCalculateDeliveryFee } from '../../../hooks/useCalculateDeliveryFee';
 import { DELIVERY_FEE_DISCOUNT_THRESHOLD } from '../../../constants/DELIVERY_INFOS';
+import { selectedCouponListState } from '../../../recoil/Coupon/atoms/selectedCouponListState';
 
 function TotalPriceContainer() {
   const selectedCartItemTotalPrice = useRecoilValue(selectedCartItemListTotalPriceSelector);
   const { deliveryFee, calculateDeliveryFee } = useCalculateDeliveryFee();
+  const selectedCouponList = useRecoilValue(selectedCouponListState);
   calculateDeliveryFee();
 
   const totalPrice = selectedCartItemTotalPrice + deliveryFee;
@@ -20,7 +22,7 @@ function TotalPriceContainer() {
       />
       <S.PriceDetailContainer>
         <PriceContainer title="주문 금액" value={selectedCartItemTotalPrice} />
-        <PriceContainer title="쿠폰 할인 금액" value={-5000} />
+        {selectedCouponList.length !== 0 && <PriceContainer title="쿠폰 할인 금액" value={-5000} />}
         <PriceContainer title="배송비" value={deliveryFee} />
       </S.PriceDetailContainer>
       <PriceContainer title="총 결제 금액" value={totalPrice} />
