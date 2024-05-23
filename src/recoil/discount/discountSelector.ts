1df25bdefb61dc6a3cd6de88a738003f91087707
이태훈 <rhymint@gmail.com>
2024-05-23 21:50:43 +0900

    feat: fixed discount amount 계산 기능 추가

--- src/recoil/discount/discountSelector.ts
+++ src/recoil/discount/discountSelector.ts
@@ -1,27 +1,51 @@
 import { selector } from 'recoil';
 import { orderedPriceSelector } from '../price/priceSelector';
 import { selectedCouponListAtom } from '../coupon/couponListAtom';
+import { validateCouponAvailable } from '../../utils/validateCouponAvailable';
 
-export const percentageDiscount = selector<number>({
+export const percentageDiscountSelector = selector<number>({
   key: 'percentageDiscount',
   get: ({ get }) => {
     const orderedPrice = get(orderedPriceSelector);
     const selectedCouponList = get(selectedCouponListAtom);
 
-    const discountedAmount = selectedCouponList
+    const discountedPrice = selectedCouponList
       .filter((coupon) => coupon.discountType === 'percentage')
-      .reduce((result, { discount = 0 }) => result * (100 - discount), 0);
+      .reduce((result, { discount }) => {
+        return result * (1 - (discount ?? 0) / 100);
+      }, 1);
 
-    return orderedPrice - discountedAmount;
+    return orderedPrice * (1 - discountedPrice);
   },
 });
 
-export const totalDiscount = selector<number>({
+export const fixedDiscountSelector = selector<number>({
+  key: 'fixedDiscount',
+  get: ({ get }) => {
+    const orderedPrice = get(orderedPriceSelector);
+    const selectedCouponList = get(selectedCouponListAtom);
+
+    const discountAmount = selectedCouponList
+      .filter(
+        (coupon) =>
+          coupon.discountType === 'fixed' &&
+          orderedPrice >= (coupon.minimumAmount ?? 0),
+      )
+      .reduce((result, { discount }) => {
+        return result + (discount ?? 0);
+      }, 0);
+
+    selectedCouponList.forEach((coupon) => validateCouponAvailable(coupon));
+    return discountAmount;
+  },
+});
+
+export const totalDiscountSelector = selector<number>({
   key: 'totalDiscount',
   get: ({ get }) => {
-    const percentage = get(percentageDiscount);
-    // const fixed = get(fixedDiscount)
+    const percentageDiscount = get(percentageDiscountSelector);
+    const fixedDiscount = get(fixedDiscountSelector);
 
-    return percentage;
+    return percentageDiscount + fixedDiscount;
   },
 });
