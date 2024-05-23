f4e82089adc667b402beb15cd7abfb1e9681df94
simhorim <hori5020@gmail.com>
2024-05-23 19:30:19 +0900

    design(ApplyingCouponButton): UI 컴포넌트 구현

---
+++ src/components/ApplyingCouponButton/ApplyingCouponButton.tsx
@@ -0,0 +1,7 @@
+import * as Styled from './style';
+
+const ApplyingCouponButton = () => {
+  return <Styled.ApplyingCouponButton>쿠폰 적용</Styled.ApplyingCouponButton>;
+};
+
+export default ApplyingCouponButton;
