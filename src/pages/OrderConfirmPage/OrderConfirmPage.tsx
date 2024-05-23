8d3c5374d4170006bb588f12cedd4fd88e3075d2
badahertz52 <badahertz52@gmail.com>
2024-05-23 14:27:45 +0900

    refactor: 주문 확인 페이지에 하위 컴포넌트 추가

--- src/pages/OrderConfirmPage/OrderConfirmPage.tsx
+++ src/pages/OrderConfirmPage/OrderConfirmPage.tsx
@@ -1,13 +1,29 @@
+import { PageExplanation } from '@components/common';
+import { OrderAmountsList, SelectedItemList, ShippingInfo } from '@components/orderConfirm';
+import CouponModalContainer from '@components/orderConfirm/CouponModalContainer/CouponModalContainer';
 import { PurchaseProcessLayout } from '@components/shoppingCart';
+import { useAvailableCoupons } from '@hooks/coupon';
+import { useSelectedCartItems } from '@hooks/shoppingCart';
+import { availableCouponsAtom } from '@recoil/shoppingCart';
 import { ROUTE_PATHS } from '@routes/route.constant';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useSetRecoilState } from 'recoil';
 
 const OrderConfirmPage: React.FC = () => {
+  const setAvailableCoupons = useSetRecoilState(availableCouponsAtom);
+  const { getAvailableCoupons } = useAvailableCoupons();
+
   const navigate = useNavigate();
+  const { totalSelectedItemLength, selectedTotalQuantity, selectedItems } = useSelectedCartItems();
 
   const handleClickBottomButton = () => {
     navigate(ROUTE_PATHS.purchaseConfirm);
   };
+
+  useEffect(() => {
+    setAvailableCoupons(getAvailableCoupons());
+  }, []);
 
   return (
     <PurchaseProcessLayout
@@ -15,7 +31,17 @@ const OrderConfirmPage: React.FC = () => {
       handleBottomBtnClick={handleClickBottomButton}
       bottomButtonText="결제하기"
     >
-      <>{/*주문 확인*/}</>
+      <PageExplanation>
+        <PageExplanation.Row>
+          총 {totalSelectedItemLength}종류의 상품 {selectedTotalQuantity}개를 주문합니다.
+        </PageExplanation.Row>
+        <PageExplanation.Row>최종 결제 금액을 확인해주세요.</PageExplanation.Row>
+      </PageExplanation>
+      <SelectedItemList selectedItems={selectedItems} />
+      {/*쿠폰 모달 -> 쿠폰 모달 적용 시, maxDiscount atom 상태 변경*/}
+      <CouponModalContainer />
+      <ShippingInfo />
+      <OrderAmountsList />
     </PurchaseProcessLayout>
   );
 };
--- src/pages/OrderPage/OrderPage.tsx
+++ src/pages/OrderPage/OrderPage.tsx
@@ -1,25 +1,21 @@
 import { PurchaseProcessLayout } from '@components/shoppingCart';
-import { useAvailableCoupons } from '@hooks/coupon';
-import { availableCouponsAtom, cartItemsAtom, selectedIdsAtom } from '@recoil/shoppingCart';
+import { cartItemsAtom, selectedIdsAtom } from '@recoil/shoppingCart';
 import { ROUTE_PATHS } from '@routes/route.constant';
 import { lazy } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { useRecoilValue } from 'recoil';
 
 const OrderPageContents = lazy(() => import('@components/shoppingCart/ShoppingCartContent/ShoppingCartContent'));
 
 const OrderPage: React.FC = () => {
   const cartItems = useRecoilValue(cartItemsAtom);
   const selectedIds = useRecoilValue(selectedIdsAtom);
-  const setAvailableCoupons = useSetRecoilState(availableCouponsAtom);
-  const { getAvailableCoupons } = useAvailableCoupons();
 
   const isButtonDisabled = cartItems.length === 0 || selectedIds.length === 0;
 
   const navigate = useNavigate();
 
   const handleClickConfirmOderButton = () => {
-    setAvailableCoupons(getAvailableCoupons());
     navigate(ROUTE_PATHS.orderConfirm);
   };
 
