afcfc529b4040eb5defabd1ec15b55f3f59086aa
skiende74 <skiende74@gmail.com>
2024-05-23 15:43:02 +0900

    feat(apiClient): non-200응답이 올 경우 에러를 던지고, 에러를 rethrow하게 하여 사용처에서 잡을 수 있게 함.

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -4,8 +4,13 @@ const API_URL = import.meta.env.VITE_API_URL as string;
 const USER_ID = import.meta.env.VITE_USER_ID as string;
 const USER_PASSWORD = import.meta.env.VITE_USER_PASSWORD as string;
 
-type Method = "GET" | "POST" | "PATCH" | "DELETE";
+export class FetchError extends Error {
+  constructor(public name: "NETWORK" | "RESPONSE_CODE" | "NON_JSON_UNEXPECTED" | "UNKNOWN", message: string) {
+    super(message);
+  }
+}
 
+type Method = "GET" | "POST" | "PATCH" | "DELETE";
 const apiClient = {
   get(endpoint: string, headers: object) {
     return this.request("GET", endpoint, headers, null);
@@ -28,28 +33,35 @@ const apiClient = {
       body: body ? JSON.stringify(body) : null,
     };
 
-    return this.fetchWithErrorHandling(endpoint, requestInit);
+    return apiClient._fetchWithRethrow(endpoint, requestInit);
   },
-
-  async fetchWithErrorHandling(endpoint: string, requestInit: RequestInit) {
+  async _fetchWithRethrow(endpoint: string, requestInit: RequestInit): Promise<object> {
     try {
-      const response = await fetch(`${API_URL}${endpoint}`, requestInit);
-
-      if (!response.ok) {
-        throw new Error(`Failed to ${endpoint}`);
-      }
-
-      const text = await response.text();
-      if (text === "") {
-        return;
-      }
-
-      const data = await JSON.parse(text);
-      return data;
+      return await apiClient._fetchWithThrow(endpoint, requestInit);
     } catch (error) {
-      console.error(`Failed to fetch ${endpoint}:`, error);
-      return error;
+      if (error instanceof TypeError) {
+        throw new FetchError("NETWORK", `네트워크 에러가 발생하였습니다. :\n ${error.message}`);
+      }
+      if (error instanceof SyntaxError) {
+        throw new FetchError(
+          "NON_JSON_UNEXPECTED",
+          `서버가 JSON이 아닌 응답을 반환하였습니다. (빈 문자열일 수도 있습니다.) : \n ${error.message}`
+        );
+      }
+      if (!(error instanceof FetchError)) {
+        throw new FetchError("UNKNOWN", `미리 규정하지않은 fetch Error가 발생했습니다. :\n ${error.message}`);
+      }
+      throw error;
     }
+  },
+  async _fetchWithThrow(endpoint: string, requestInit: RequestInit) {
+    const response = await fetch(`${API_URL}${endpoint}`, requestInit);
+
+    if (!response.ok) {
+      throw new FetchError("RESPONSE_CODE", "200-299 이외의 응답코드가 도착했습니다.");
+    }
+    const data = await response.json();
+    return data;
   },
 };
 
