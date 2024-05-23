0ec3f9ed2c081b928c463c76baf5859eee5bcc20
hanyoung <phanyoung@naver.com>
2024-05-24 01:57:51 +0900

    refactor(PaymentTitle): useSelectedCartItemCounts를 사용하는 방식으로 수정

--- src/components/PaymentPage/PaymentTitle.tsx
+++ src/components/PaymentPage/PaymentTitle.tsx
@@ -1,20 +1,14 @@
 import styled from "styled-components";
-import { useRecoilValue } from "recoil";
-
-import { cartItemsState } from "../../recoil/cartItems";
-import { calculateSelectedCartItemsCount } from "../../utils/domain/calculateSelectedCartItemsCount";
+import { useSelectedCartItemCounts } from "../../hooks/useCartItemCounts";
 
 export default function PaymentTitle() {
-  const cartItems = useRecoilValue(cartItemsState);
-  const selectedCartItems = cartItems.filter(({ isSelected }) => isSelected);
-  const cartItemsCount = calculateSelectedCartItemsCount(cartItems);
-
+  const { selectedCartItemsCount, selectedUniqueCartItemsCount } = useSelectedCartItemCounts();
   return (
-    <S.Container className="PaymentTitle">
+    <S.Container>
       <S.Title>주문 확인</S.Title>
       <S.InfoWrapper>
         <S.Info>
-          총 {selectedCartItems.length}종류의 상품 {cartItemsCount}개를 주문합니다.
+          총 {selectedUniqueCartItemsCount}종류의 상품 {selectedCartItemsCount}개를 주문합니다.
         </S.Info>
         <S.Info>최종 결제 금액을 확인해 주세요.</S.Info>
       </S.InfoWrapper>
