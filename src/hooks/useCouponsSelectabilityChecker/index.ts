9afb88cbe3c8cd10d08d713f0b7ae618e5c755b0
hanyoung <phanyoung@naver.com>
2024-05-23 17:11:09 +0900

    feat(useCouponsSelectabilityChecker): 쿠폰 선택 가능 여부 체크를 위한 커스텀 훅 추가

---
+++ src/hooks/useCouponsSelectabilityChecker/index.ts
@@ -0,0 +1,16 @@
+import { cartItemsState } from "./../../recoil/cartItems";
+import { useRecoilValue } from "recoil";
+import { cartAmountState } from "../../recoil/cartAmount";
+import { RawCoupon } from "../../types/rawCoupon";
+import { isCouponSelectable } from "./isCouponSelectable";
+
+type CouponSelectabilityChecker = (coupon: RawCoupon) => boolean;
+
+export const useCouponSelectabilityChecker = (): CouponSelectabilityChecker => {
+  const cartItems = useRecoilValue(cartItemsState);
+  const { orderAmount, shippingCost } = useRecoilValue(cartAmountState);
+
+  return (coupon: RawCoupon) => {
+    return isCouponSelectable(coupon, cartItems, { orderAmount, shippingCost });
+  };
+};
