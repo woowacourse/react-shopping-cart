f35263e070212f4347ecb19dbfe55f0c22b4e515
simhorim <hori5020@gmail.com>
2024-05-23 15:31:36 +0900

    refactor(page): 공통된 Page 스타일 하나로 통일

--- src/components/pages/ShoppingCart/style.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const ShoppingCart = styled.section`
-  width: 26.875rem;
-  min-height: 100vh;
-  position: relative;
-
-  background-color: ${(props) => props.theme.color.white};
-`;
