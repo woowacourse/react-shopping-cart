bec095ab683f41f8c93bc94ee17dbd36aab1644d
Hyelim Choi <HyelimChoi01@gmail.com>
2024-05-23 10:58:05 +0900

    test: 쿠폰이 하나만 선택되었을 때 테스트 케이스 추가

--- src/hooks/useTotalCouponDiscount.test.ts
+++ src/hooks/useTotalCouponDiscount.test.ts
@@ -19,6 +19,60 @@ jest.mock('../api/config', () => ({
 
 describe('useTotalCouponDiscount 훅을 사용하여 적용된 모든 쿠폰들에 대한 최종 할인 가격을 가져올 수 있다.', () => {
   const date = new Date();
+  it('쿠폰을 하나만 사용시 하나의 할인 금액만 보여준다.', () => {
+    const { result } = renderHook(
+      () => {
+        const setCartList = useSetRecoilState(cartListState);
+        setCartList(MOCK_CART_LIST);
+
+        const MOCK_FILTERED_CART_LIST = [
+          {
+            id: 624,
+            quantity: 5,
+            isSelected: true,
+            price: 20000,
+          },
+        ];
+
+        MOCK_FILTERED_CART_LIST.forEach((item) => {
+          const setFilteredCartList = useSetRecoilState(
+            filteredCartItemState(item.id)
+          );
+          setFilteredCartList(item);
+        });
+
+        const setCouponList = useSetRecoilState(couponListState);
+        setCouponList(MOCK_COUPON_LIST);
+
+        const coupons: Coupon[] = [
+          {
+            id: 1,
+            code: 'FIXED5000',
+            description: '5,000원 할인 쿠폰',
+            expirationDate: '2024-11-30',
+            discount: 5000,
+            minimumAmount: 100000,
+            discountType: 'fixed',
+          },
+        ];
+
+        const AM4 = new Date(2024, 4, 22, 4, 0, 0, 0);
+        const totalDiscountedPrice = useTotalCouponDiscount({
+          coupons,
+          date: AM4,
+        });
+
+        return { totalDiscountedPrice };
+      },
+      {
+        wrapper: RecoilRoot,
+      }
+    );
+
+    const totalAmount = 5000;
+    expect(result.current.totalDiscountedPrice).toBe(totalAmount);
+  });
+
   it('[5000원 할인 + 미라클 모닝 퍼센트 할인 쿠폰].', () => {
     const { result } = renderHook(
       () => {
