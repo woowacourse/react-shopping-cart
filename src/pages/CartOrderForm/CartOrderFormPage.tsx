f7ccc7c2a4a47404b787b4c0fd8084d0493600b1
pakxe <pigkill40@naver.com>
2024-05-25 05:24:16 +0900

    feat: 도서산간 여부를 선택하는 폼을 컴포넌트로 분리

--- src/pages/CartOrderForm/CartOrderFormPage.tsx
+++ src/pages/CartOrderForm/CartOrderFormPage.tsx
@@ -1,9 +1,7 @@
 import Header from '../../components/Header/Header';
-import Text from '../../components/common/Text/Text';
 import Button from '../../components/common/Button/Button';
 import { useRecoilValue } from 'recoil';
 import { cartItemListState } from '../../recoil/cartItemList/cartItemListState';
-import CartOrderFormPageLoader from './CartOrderFormPageLoader';
 import { selectedCartItemIdListState } from '../../recoil/selectedCartItemList/selectedCartItemIdListState';
 import styled from 'styled-components';
 import Title from '../../components/common/Title/Title';
@@ -12,35 +10,35 @@ import { totalCartPriceState } from '../../recoil/price/totalCartPriceState';
 import { cartShippingFeeState } from '../../recoil/price/cartShippingFeeState';
 import { finalCartPriceState } from '../../recoil/price/finalCartPriceState';
 import { discountPriceByCouponListState } from '../../recoil/price/discountPriceByCouponListState';
-import { totalCartItemQuantitySelector } from '../../recoil/cartItem/totalCartItemQuantityState';
+import { totalCartItemQuantityState } from '../../recoil/cartItem/totalCartItemQuantityState';
 import CartItem from '../../components/CartItem/CartItem';
-import Checkbox from '../../components/common/Checkbox/Checkbox';
-import { Modal } from '@pakxe/react-simple-modal';
 import { useState } from 'react';
-import CouponListModal from '../../components/Modal/CouponListModal';
+import CouponListModal from '../../components/CouponListModal/CouponListModal';
+import { useNavigate } from 'react-router-dom';
+import { ROUTES } from '../../constants/routes';
+import useCouponList from '../../recoil/couponList/useCouponList';
+import ListContainer from '../../components/common/ListContainer/ListContainer';
+import ShippingAreaForm from '../../components/ShippingAreaForm/ShippingAreaForm';
+import { requestOrders } from '../../apis/requests/order';
+import { useErrorBoundary } from 'react-error-boundary';
+import { useSelectedCartItemIdList } from '../../recoil/selectedCartItemList/useSelectedCartItemIdList';
 
 // TODO: cartOrders 의 컴포넌트와 동일. 복붙으로 재사용하니 수정요망
 const CartPageContainer = styled.main`
   width: 100%;
   height: 100%;
   padding: 50px 20px 80px 20px;
+  display: flex;
+  flex-direction: column;
+  row-gap: 25px;
 `;
-
-const ShippingAreaForm = () => {
-  return (
-    <>
-      <Text>배송 정보</Text>
-      <Checkbox description="제주도 및 도서 산간 지역" />
-    </>
-  );
-};
 
 const CartOrderFormPage = () => {
   const cartItemList = useRecoilValue(cartItemListState);
   const selectedCartItemIdList = useRecoilValue(selectedCartItemIdListState);
 
   const selectedCartItemList = cartItemList.filter(({ cartItemId }) => selectedCartItemIdList.includes(cartItemId));
-  const totalQuantity = useRecoilValue(totalCartItemQuantitySelector);
+  const totalQuantity = useRecoilValue(totalCartItemQuantityState);
 
   const totalCartPrice = useRecoilValue(totalCartPriceState);
   const discountPrice = useRecoilValue(discountPriceByCouponListState);
@@ -81,6 +79,7 @@ const CartOrderFormPage = () => {
           title="주문 확인"
           description={`총 ${selectedCartItemList.length}종류의 상품 ${totalQuantity}개를 주문합니다. \n최종 결제 금액을 확인해주세요.`}
         />
+        <ListContainer direction="column" gap="20px">
           {selectedCartItemList.map((cartItem) => (
             <CartItem {...cartItem} />
           ))}
@@ -92,11 +91,12 @@ const CartOrderFormPage = () => {
         >
           쿠폰 적용
         </Button>
+
         <ShippingAreaForm />
         <PriceTable>
           <PriceTable.Row name="주문 금액" price={totalCartPrice} />
-          <PriceTable.Row name="쿠폰 할인 금액" price={discountPrice.coupon} />
-          <PriceTable.Row name="배송비" price={shippingFee} />
+          <PriceTable.Row name="쿠폰 할인 금액" price={discountPrice.price === 0 ? 0 : -discountPrice.price} />
+          <PriceTable.Row name="배송비" price={discountPrice.shippingFee === 'free' ? '무료 배송' : shippingFee} />
           <PriceTable.Row name="총 결제 금액" price={finalCartPrice} upperDivider />
         </PriceTable>
       </CartPageContainer>
