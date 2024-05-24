913c2ed3b5699ff320f8844b2fc801ca3eae833b
jayming66 <mjw8941@gmail.com>
2024-05-24 20:32:34 +0900

    refactor(Divider, SubtitleSpaceBetween): 반복되는 마크업 컴포넌트화

--- src/pages/cart/components/CartTotals.tsx
+++ src/pages/cart/components/CartTotals.tsx
@@ -1,33 +1,32 @@
 import { useRecoilValue } from 'recoil';
 import { totalOrderAmountState } from '../../../store/selectors';
 import NoticeLabel from '../../../components/common/NoticeLabel/NoticeLabel';
-import { NOTICE_MESSAGE } from '../../../constants/messages';
-import formatKoreanCurrency from '../../../utils/formatKoreanCurrency';
+import SubtitleSpaceBetween from '../../../components/common/SubtitleSpaceBetween/SubtitleSpaceBetween';
+import Divider from '../../../components/common/Divider/Divider';
 
-import common from '../../../styles/common.module.css';
+import formatKoreanCurrency from '../../../utils/formatKoreanCurrency';
+import { NOTICE_MESSAGE } from '../../../constants/messages';
 import styles from '../Cart.module.css';
 
 export default function CartTotals() {
   const { orderAmount, deliveryCharge, totalAmount } = useRecoilValue(totalOrderAmountState);
   return (
-    <div className={styles.cartContentWrapper}>
+    <div className={styles.cartTotalsContainer}>
       <NoticeLabel>{NOTICE_MESSAGE.shipping}</NoticeLabel>
-      <div className={styles.cartTotalsWrapper}>
-        <div className={styles.cartToTalsTextWrapper}>
-          <span className={common.subtitleText}>주문 금액</span>
-          <span className={common.titleText}>{formatKoreanCurrency(orderAmount)}원</span>
-        </div>
-        <div className={styles.cartToTalsTextWrapper}>
-          <span className={common.subtitleText}>배송비</span>
-          <span className={common.titleText}>{formatKoreanCurrency(deliveryCharge)}원</span>
-        </div>
-      </div>
-      <div className={styles.cartTotalsWrapper}>
-        <div className={styles.cartToTalsTextWrapper}>
-          <span className={common.subtitleText}>총 결제 금액</span>
-          <span className={common.titleText}>{formatKoreanCurrency(totalAmount)}원</span>
-        </div>
-      </div>
+      <Divider />
+      <SubtitleSpaceBetween
+        subtitle="주문 금액"
+        content={`${formatKoreanCurrency(orderAmount)}원`}
+      />
+      <SubtitleSpaceBetween
+        subtitle="배송비"
+        content={`${formatKoreanCurrency(deliveryCharge)}원`}
+      />
+      <Divider />
+      <SubtitleSpaceBetween
+        subtitle="총 결제 금액"
+        content={`${formatKoreanCurrency(totalAmount)}원`}
+      />
     </div>
   );
 }
