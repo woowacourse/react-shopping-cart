5372a7881737d4c041a74d48556d449efcd6ab8f
이태훈 <rhymint@gmail.com>
2024-05-24 19:03:07 +0900

    feat: SelectCouponModal에서 button 내부 text에 총 할인 금액 적용

--- src/components/SelectCouponModal/SelectCouponModal.tsx
+++ src/components/SelectCouponModal/SelectCouponModal.tsx
@@ -1,5 +1,7 @@
 import { CustomModal } from 'woowacourse-todari-components';
 import SelectCouponModalSection from '../SelectCouponModalSection/SelectCouponModalSection';
+import { useRecoilValue } from 'recoil';
+import { totalDiscountSelector } from '../../recoil/discount/discountSelector';
 
 interface SelectCouponModalProps {
   modalOpened: boolean;
@@ -10,13 +12,16 @@ const SelectCouponModal = ({
   modalOpened,
   onClose,
 }: SelectCouponModalProps) => {
+  const totalDiscount = useRecoilValue(totalDiscountSelector);
   return (
     <CustomModal
       title="쿠폰을 선택해 주세요"
       size="medium"
       isOpened={modalOpened}
       onClose={onClose}
-      primaryButton={{ text: '총 6,000원 할인 쿠폰 사용하기' }}
+      primaryButton={{
+        text: `총 ${totalDiscount.toLocaleString('ko-kr')}원 할인 쿠폰 사용하기`,
+      }}
       showCloseButton={true}
     >
       <SelectCouponModalSection />
