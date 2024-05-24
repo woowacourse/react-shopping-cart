7e278798b5c00b94541b0e9a9cf8e70ae258fa60
pakxe <pigkill40@naver.com>
2024-05-24 11:14:39 +0900

    feat: 라우팅 경로 상수화

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MobileLayout from './components/common/MobileLayout/MobileLayout';
-import CartPage from './pages/CartPage/CartPage';
-import ConfirmPurchasePage from './pages/ConfirmPurchasePage/ConfirmPurchasePage';
+import CartPageContainer from './pages/CartOrders/CartPageContainer';
+import StartPage from './pages/StartPage';
+import { ROUTES } from './constants/routes';
+import CartOrderFormPageContainer from './pages/CartOrderForm/CartOrderFormPageContainer';
 
 function App() {
   return (
     <BrowserRouter>
       <MobileLayout>
         <Routes>
-          <Route path="/" element={<CartPage />} />
-          <Route path="/confirm-purchase" element={<ConfirmPurchasePage />} />
+          <Route path="/" element={<StartPage />} />
+          <Route path={ROUTES.CART_ORDERS} element={<CartPageContainer />} />
+          <Route path={ROUTES.CART_ORDER_FORM} element={<CartOrderFormPageContainer />} />
         </Routes>
       </MobileLayout>
     </BrowserRouter>
---
+++ src/constants/routes.ts
@@ -0,0 +1,4 @@
+export const ROUTES = {
+  CART_ORDERS: '/cart/orders',
+  CART_ORDER_FORM: '/cart/order-form',
+};
