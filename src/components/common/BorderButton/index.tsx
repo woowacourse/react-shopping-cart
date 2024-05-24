186f7d3e45f3024a8e02c33fc0a5ce8b82d6045d
river <jenn0.6n@gmail.com>
2024-05-24 11:11:00 +0900

    feat: borderButton style prop 수정

--- src/components/common/BorderButton/index.tsx
+++ src/components/common/BorderButton/index.tsx
@@ -2,12 +2,20 @@ import { PropsWithChildren } from "react";
 import { BorderButtonContainer, SizeType } from "./style";
 
 const BorderButton: React.FC<
-  PropsWithChildren<{ onClick: () => void; size: SizeType; className?: string }>
-> = ({ children, onClick, size, className }) => {
+  PropsWithChildren<{
+    onClick?: () => void;
+    size: SizeType;
+    bgColor?: string;
+    color?: string;
+    className?: string;
+  }>
+> = ({ children, onClick, size, bgColor, color = "#fff", className }) => {
   return (
     <BorderButtonContainer
-      type="button"
       className={className}
+      type="button"
+      bgColor={bgColor}
+      color={color}
       onClick={onClick}
       size={size}
     >
--- src/components/common/BorderButton/style.ts
+++ src/components/common/BorderButton/style.ts
@@ -1,25 +1,62 @@
 import styled from "@emotion/styled";
 
-export type SizeType = "small" | "large";
+export type SizeType = "small" | "large" | "full";
+
+const getWidthSize = (size: SizeType) => {
+  switch (size) {
+    case "small": {
+      return "24px";
+    }
+    case "large": {
+      return "40px";
+    }
+    case "full": {
+      return "100%";
+    }
+    default: {
+      return "24px";
+    }
+  }
+};
+
+const getPadding = (size: SizeType) => {
+  switch (size) {
+    case "small": {
+      return "0";
+    }
+    case "large": {
+      return "0";
+    }
+    case "full": {
+      return "12px";
+    }
+    default: {
+      return "0";
+    }
+  }
+};
 
 interface BorderButtonContainerProps {
   size?: SizeType;
+  bgColor?: string;
+  color: string;
 }
 
 export const BorderButtonContainer = styled.button<BorderButtonContainerProps>(
-  ({ theme, size = "large" }) => ({
-    background: "none",
-    border: `1px solid ${theme.colors.border}`,
+  ({ theme, size = "large", bgColor = "none", color }) => ({
+    background: `${bgColor}`,
+    border: bgColor !== "none" ? `1px solid ${theme.colors.border}` : "none",
     borderRadius: `${theme.borderRadius.medium}`,
 
-    width: size === "small" ? "24px" : "40px",
+    color: color,
+    width: getWidthSize(size),
     height: size === "small" ? "24px" : "",
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
     cursor: "pointer",
     fontSize: "12px",
-    padding: 0,
+    padding: getPadding(size),
 
     img: {
       width: size === "small" ? "12px" : "16px",
