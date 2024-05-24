9c4bc5d00cc1c4bdb6a494e258c866d5e7b62321
skiende74 <skiende74@gmail.com>
2024-05-24 15:22:00 +0900

    feat(recoil/couponState): couponIdSetSelector추가. isSelectedCoupon을 atomFamily로 구현하고, 이게 true인 것만 Set으로 관리하는 selectedCouponSetSelector 추가.

--- src/recoil/coupon/couponState.test.ts
+++ src/recoil/coupon/couponState.test.ts
@@ -1,6 +1,11 @@
 import { snapshot_UNSTABLE } from "recoil";
 import { mockCouponList } from "../mocks";
-import { couponSelectorFamily } from "./couponState";
+import {
+  couponIdSetSelector,
+  couponSelectorFamily,
+  isSelectedCouponAtomFamily,
+  selectedCouponIdSetSelector,
+} from "./couponState";
 
 jest.mock("../../api/coupon", () => ({ fetchCouponList: () => mockCouponList }));
 describe("couponState 테스트", () => {
@@ -16,6 +21,36 @@ describe("couponState 테스트", () => {
       const snapshot = snapshot_UNSTABLE();
 
       expect(snapshot.getLoadable(couponSelectorFamily(NON_EXISTING_ID)).contents).toEqual(undefined);
+    });
+  });
+  describe("couponIdSelector", () => {
+    it("존재하는 쿠폰의 id를 Set으로 반환한다.", () => {
+      const snapshot = snapshot_UNSTABLE();
+
+      expect(snapshot.getLoadable(couponIdSetSelector).contents).toEqual(new Set([1, 2, 3, 4]));
+    });
+  });
+
+  describe("isSelectedAtomSelectorFamily", () => {
+    it("기본값은 false를 반환한다.", () => {
+      const snapshot = snapshot_UNSTABLE();
+
+      expect(snapshot.getLoadable(isSelectedCouponAtomFamily(1)).contents).toBe(false);
+    });
+  });
+
+  describe("selectedCouponIdSetSelector", () => {
+    it("아무것도 선택되지 않았을 때, 빈 Set을 반환한다.", () => {
+      const snapshot = snapshot_UNSTABLE();
+
+      expect(snapshot.getLoadable(selectedCouponIdSetSelector).contents).toEqual(new Set());
+    });
+    describe("하나의 쿠폰만 선택된 상태일 때, 해당 id만 담은 Set을 반환한다.", () => {
+      it.each([1, 2, 3, 4])("$s번 쿠폰만 선택하였을 때, 숫자 %s만 담은 Set을 반환한다.", (SELECTED_COUPON_ID) => {
+        const snapshot = snapshot_UNSTABLE(({ set }) => set(isSelectedCouponAtomFamily(SELECTED_COUPON_ID), true));
+
+        expect(snapshot.getLoadable(selectedCouponIdSetSelector).contents).toEqual(new Set([SELECTED_COUPON_ID]));
+      });
     });
   });
 });
--- src/recoil/coupon/couponState.ts
+++ src/recoil/coupon/couponState.ts
@@ -1,4 +1,4 @@
-import { selector, selectorFamily } from "recoil";
+import { DefaultValue, atomFamily, selector, selectorFamily } from "recoil";
 import { fetchCouponList } from "../../api/coupon";
 import { Coupon } from "../../types/Coupon";
 
@@ -7,10 +7,24 @@ export const fetchCouponListSelector = selector({
   get: fetchCouponList,
 });
 
+export const couponIdSetSelector = selector({
+  key: "couponIdSetSelector",
+  get: ({ get }) => new Set(get(fetchCouponListSelector).map((coupon) => coupon.id)),
+});
+
 export const couponSelectorFamily = selectorFamily<Coupon, number>({
-  key: "",
+  key: "couponSelectorFamily",
   get:
     (id) =>
     ({ get }) =>
       get(fetchCouponListSelector).find((coupon) => coupon.id === id),
+});
+
+export const isSelectedCouponAtomFamily = atomFamily<boolean, number>({
+  key: "isSelectedAtomFamily",
+  default: false,
+});
+export const selectedCouponIdSetSelector = selector<Set<number>>({
+  key: "selectedCouponSetSelector",
+  get: ({ get }) => new Set([...get(couponIdSetSelector)].filter((id) => get(isSelectedCouponAtomFamily(id)))),
 });
