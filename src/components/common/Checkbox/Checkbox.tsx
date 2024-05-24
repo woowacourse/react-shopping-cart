2f579ae8d32ce19235b03a180863d11d40ab3c66
pakxe <pigkill40@naver.com>
2024-05-24 11:11:09 +0900

    feat: 체크박스에 선택적으로 설명 추가할 수 있도록 구현

--- src/components/common/Checkbox/Checkbox.tsx
+++ src/components/common/Checkbox/Checkbox.tsx
@@ -1,13 +1,20 @@
+import { ReactNode } from 'react';
 import * as S from './Checkbox.style';
 
 export interface CheckboxProps {
   checked: boolean;
   handleClick: () => void;
   alt?: string;
+  description?: ReactNode; // Text 컴포넌트를 넣을 수 있도록
 }
 
-const Checkbox = ({ checked, handleClick, alt = 'Checkbox' }: CheckboxProps) => {
-  return <S.Checkbox checked={checked} onClick={handleClick} alt={alt} type="checkbox" />;
+const Checkbox = ({ checked, handleClick, alt = 'Checkbox', description }: CheckboxProps) => {
+  return (
+    <>
+      <S.Checkbox checked={checked} onChange={handleClick} alt={alt} type="checkbox" />
+      {description}
+    </>
+  );
 };
 
 export default Checkbox;
