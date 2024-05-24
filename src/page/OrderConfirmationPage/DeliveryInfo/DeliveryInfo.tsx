09c604d0a4ada60f2e7cb4b027d33bf0d84f9d61
ss0526100 <ss0526100@gmail.com>
2024-05-24 21:06:21 +0900

    feat(DeliveryInfo): 로직 적용

--- src/page/OrderConfirmationPage/DeliveryInfo/DeliveryInfo.tsx
+++ src/page/OrderConfirmationPage/DeliveryInfo/DeliveryInfo.tsx
@@ -1,14 +1,18 @@
 import * as S from './style';
 
 import CheckBox from '../../../components/CheckBox/CheckBox';
+import { isIslandOrMountain } from '../../../recoil/atoms';
+import { useRecoilState } from 'recoil';
 
 export default function DeliveryInfo() {
+  const [isChecked, setIsChecked] = useRecoilState(isIslandOrMountain);
+  const checkHandler = () => setIsChecked(!isChecked);
   return (
     <S.Container>
       <S.Title>배송정보</S.Title>
 
       <S.CheckItem>
-        <CheckBox isChecked={false} />
+        <CheckBox isChecked={isChecked} onClick={checkHandler} />
         <p>제주도 및 도서 산간 지역</p>
       </S.CheckItem>
     </S.Container>
