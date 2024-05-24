6464f70e274cdeaf6ea7651a685c1b8410a66b50
jaeml06 <jaeml0630@gmail.com>
2024-05-25 01:46:08 +0900

    feat : items의 개수에 따라 버튼 disabled 변경

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,14 +1,11 @@
 import { Suspense } from 'react';
 import Header from '../components/Header/Header';
 import styled from 'styled-components';
-import Footer from '../components/Footer/Footer';
 import ErrorFallback from '../components/ErrorFallback';
 import { ErrorBoundary } from 'react-error-boundary';
 import CartContent, {
   NoCartItemContainer,
 } from '../components/CartContent/CartContent';
-import { URL_PATH } from '../constants/UrlPath';
-import { MESSAGES } from '../constants/Messages';
 
 const CartContainer = styled.div`
   display: flex;
@@ -17,32 +14,17 @@ const CartContainer = styled.div`
   height: 100%;
 `;
 
-const ContentWrapper = styled.section`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  padding: 3.6rem 2.4rem 10.4rem 2.4rem;
-  box-sizing: border-box;
-  height: 100%;
-`;
-
 function Cart() {
   return (
     <CartContainer>
       <ErrorBoundary FallbackComponent={ErrorFallback}>
         <Header headerIconType="home" />
-        <ContentWrapper>
-          <Suspense
-            fallback={<NoCartItemContainer>Loading...</NoCartItemContainer>}
-          >
-            <CartContent />
-          </Suspense>
-        </ContentWrapper>
-        <Footer
-          value={MESSAGES.confirm}
-          isDisabled={false}
-          url={URL_PATH.orderConfirm}
-        />
+
+        <Suspense
+          fallback={<NoCartItemContainer>Loading...</NoCartItemContainer>}
+        >
+          <CartContent />
+        </Suspense>
       </ErrorBoundary>
     </CartContainer>
   );
