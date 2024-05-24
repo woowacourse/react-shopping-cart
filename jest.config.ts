c8c46949762b5b4c86d2f0c59260ee763769ee2e
김진호 <rlawlsgh1227@gmail.com>
2024-05-24 19:59:30 +0900

    chore: 절대경로 설정으로 jest 설정파일 수정

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,17 @@ export default {
   },
   moduleNameMapper: {
     '^.+\\.svg$': 'jest-svg-transformer',
+    '^@api/(.*)$': '<rootDir>/src/api/$1',
+    '^@assets/(.*)$': '<rootDir>/src/assets/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@constants/(.*)$': '<rootDir>/src/constants/$1',
+    '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '^@mocks/(.*)$': '<rootDir>/src/mocks/$1',
+    '^@pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^@recoil/(.*)$': '<rootDir>/src/recoil/$1',
+    '^@styles/(.*)$': '<rootDir>/src/styles/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^@type/(.*)$': '<rootDir>/src/type/$1',
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
   },
 };
