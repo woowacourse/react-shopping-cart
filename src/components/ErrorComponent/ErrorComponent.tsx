e34bcf86d64b08d830f10bea9d1960213f088547
pakxe <pigkill40@naver.com>
2024-05-24 11:12:18 +0900

    feat: 에러를 보여주기 위한 컴포넌트 구현

---
+++ src/components/ErrorComponent/ErrorComponent.tsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+import Text from '../common/Text/Text';
+import Button from '../common/Button/Button';
+
+const ErrorComponentContainer = styled.div`
+  height: 100%;
+  display: flex;
+
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  row-gap: 14px;
+`;
+
+type ErrorComponentProps = {
+  error: Error;
+};
+
+const ErrorComponent = ({ error }: ErrorComponentProps) => {
+  return (
+    <ErrorComponentContainer>
+      <Text size="l" weight="l">
+        요청사항을 처리하는데 실패했습니다.
+      </Text>
+      <Text size="s" weight="s">
+        {error.message}
+      </Text>
+      <Button color="primary">다시 시도</Button>
+    </ErrorComponentContainer>
+  );
+};
+
+export default ErrorComponent;
