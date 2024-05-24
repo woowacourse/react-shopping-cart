62a60b35491e36d9857f9c4a7e3561bd8f8023a8
rbgksqkr <rbgks1937@naver.com>
2024-05-25 01:38:02 +0900

    feat: 에러 컴포넌트에서 홈화면 이동 버튼 추가

--- src/components/common/ErrorComponent.tsx
+++ src/components/common/ErrorComponent.tsx
@@ -1,14 +1,26 @@
 import { css } from '@emotion/react';
+import { useNavigate } from 'react-router-dom';
+
+import { THEME } from '@/constants/theme';
 
 interface ErrorComponentProps {
   error: Error;
 }
 
 const ErrorComponent = ({ error }: ErrorComponentProps) => {
+  const navigate = useNavigate();
+
+  const handleClickHomeButton = () => {
+    navigate('/');
+  };
+
   return (
     <div css={container}>
       <div>에러가 발생했습니다.</div>
       <div>에러 : {error.message}</div>
+      <button css={homeButton} onClick={handleClickHomeButton}>
+        홈화면으로 가기
+      </button>
     </div>
   );
 };
@@ -22,4 +34,14 @@ const container = css`
   align-items: center;
   flex: 1;
   gap: 24px;
+`;
+
+const homeButton = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  padding: 8px 16px;
+
+  background-color: ${THEME.BLACK};
 `;
