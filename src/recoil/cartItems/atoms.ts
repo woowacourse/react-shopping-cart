b6941892b9e8930f68df5c7d3589854a76a0b753
Largopie <106071687+Largopie@users.noreply.github.com>
2024-05-24 23:37:52 +0900

    refactor: cartItemsState fetch 로직 수정

--- src/recoil/cartItems/atoms.ts
+++ src/recoil/cartItems/atoms.ts
@@ -1,21 +1,12 @@
-import { atom, atomFamily, selector } from 'recoil';
+import { atom, atomFamily } from 'recoil';
 
 import LocalStorage, { CART_ITEM } from '@/Storage';
 import { CartItemProps } from '@/types/cartItem';
 import { fetchCartItems } from '@apis/cartItem';
 
-const cartItemSelector = selector({
-  key: 'getCartItems',
-  get: async () => {
-    const cartItems = await fetchCartItems();
-
-    return cartItems;
-  },
-});
-
 export const cartItemsState = atom<CartItemProps[]>({
   key: 'cartItemsState',
-  default: cartItemSelector,
+  default: fetchCartItems(),
 });
 
 export const checkedItemsState = atomFamily<boolean, number>({
