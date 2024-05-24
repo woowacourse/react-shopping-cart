ff3376448dff7a58dcaf0b54556491a026ff52d7
river <jenn0.6n@gmail.com>
2024-05-24 11:17:21 +0900

    feat: 쿠폰 리스트 컴포넌트 구현

--- src/components/CouponList/index.tsx
+++ src/components/CouponList/index.tsx
@@ -1,55 +1,16 @@
-/** @jsxImportSource @emotion/react */
-import { title } from "process";
-import { PropsWithChildren } from "react";
-import { mockCoupons } from "../../mocks";
-import { caption } from "../../styles/font";
-import formatAvailableTime from "../../util/formatAvailableTime";
-import formatExpirationDate from "../../util/formatExpirationDate";
-import formatPriceToKoreanWon from "../../util/formatPriceToKoreanWon";
-import { Divider } from "../CartList/CheckoutSummary/style";
-import CheckBox from "../common/CheckBox";
-import {
-  CouponListDescription,
-  CouponListSection,
-  CouponListTitle,
-} from "./style";
+import { Coupon } from "../../types";
+import CouponItem from "../CouponItem";
 
-const CouponList: React.FC<PropsWithChildren> = () => {
+interface CouponListProps {
+  coupons: Coupon[];
+}
+
+const CouponList: React.FC<CouponListProps> = ({ coupons }) => {
   return (
     <>
-      {mockCoupons.map(
-        ({ id, description, expirationDate, minimumAmount, availableTime }) => (
-          <>
-            <Divider />
-            <CouponListSection>
-              <CouponListTitle>
-                <CheckBox
-                  isSelected={false}
-                  id={id.toString()}
-                  toggleSelected={() => {}}
-                />
-                <h3 css={title}>{description}</h3>
-              </CouponListTitle>
-              <CouponListDescription>
-                <p css={caption}>
-                  만료일 : {formatExpirationDate(expirationDate)}
-                </p>
-                {minimumAmount && (
-                  <p css={caption}>
-                    최소 주문 금액 : {formatPriceToKoreanWon(minimumAmount)}
-                  </p>
-                )}
-
-                {availableTime && (
-                  <p css={caption}>
-                    사용 가능 시간 : {formatAvailableTime(availableTime)}
-                  </p>
-                )}
-              </CouponListDescription>
-            </CouponListSection>
-          </>
-        )
-      )}
+      {coupons.map((coupon: Coupon) => (
+        <CouponItem coupon={coupon} key={coupon.id} />
+      ))}
     </>
   );
 };
--- src/components/CouponList/style.ts
+++ src/components/CouponList/style.ts
@@ -5,6 +5,10 @@ export const CouponListTitle = styled.div`
   gap: 5px;
 `;
 
+interface CouponListSectionProps {
+  isApplicable: boolean;
+}
+
 export const CouponListDescription = styled.div`
   display: flex;
   flex-direction: column;
@@ -13,9 +17,12 @@ export const CouponListDescription = styled.div`
   p {
     padding: 0;
     margin: 0;
+    })}
   }
 `;
 
-export const CouponListSection = styled.section`
+export const CouponListSection = styled.section<CouponListSectionProps>`
   height: 110px;
+  color: ${({ theme, isApplicable }) =>
+    isApplicable ? theme.colors.black : theme.colors.gray};
 `;
