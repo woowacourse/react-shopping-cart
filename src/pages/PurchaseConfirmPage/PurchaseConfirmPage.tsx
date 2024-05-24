330729c835fc3e161844ea347d96ace05a2df9ba
badahertz52 <badahertz52@gmail.com>
2024-05-24 10:55:59 +0900

    feat: 결제 완료 페이지-> 장바구니 페이지 이동 시, 선택된 상품에 대한 상태,로컬 스토리지 초기화

--- src/pages/PurchaseConfirmPage/PurchaseConfirmPage.tsx
+++ src/pages/PurchaseConfirmPage/PurchaseConfirmPage.tsx
@@ -1,19 +1,24 @@
 import { BottomButton } from '@components/common';
 import { useSelectedCartItems } from '@hooks/index';
-import { totalPriceSelector } from '@recoil/shoppingCart';
+import { selectedIdsAtom, totalPriceSelector } from '@recoil/shoppingCart';
 import { ROUTE_PATHS } from '@routes/route.constant';
 import { formatKoreanCurrency } from '@utils/currency';
 import { useNavigate } from 'react-router-dom';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useResetRecoilState } from 'recoil';
 
 import * as Styled from './PurchaseConfirmPage.styled';
 
 const PurchaseConfirmPage = () => {
   const totalPrice = useRecoilValue(totalPriceSelector);
+  const resetSelectedIds = useResetRecoilState(selectedIdsAtom);
+
   const { totalSelectedItemLength, selectedTotalQuantity } = useSelectedCartItems();
   const navigate = useNavigate();
 
   const handleClickBottomButton = () => {
+    // 선택된 상품 목록 reset
+    resetSelectedIds();
+    // 장바구니로 이동
     navigate(ROUTE_PATHS.root);
   };
 
