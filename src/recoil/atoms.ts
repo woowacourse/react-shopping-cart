497126838957d3c9d599ef32e1e1e40a6be98d5e
Lee sang Yeop <mma7710@naver.com>
2024-05-24 14:56:33 +0900

    feat(atom): 전역 상태 추가 - 현재 선택된 쿠폰 상태 - 시골에 사는지에 대한 상태

--- src/recoil/atoms.ts
+++ src/recoil/atoms.ts
@@ -1,5 +1,6 @@
 import { atom, atomFamily } from "recoil";
 import { recoilPersist } from "recoil-persist";
+import { CouponInstances } from "../domain/coupons/AbstractCoupon";
 
 const { persistAtom } = recoilPersist();
 
@@ -7,6 +8,16 @@ export const checkedCartItemsState = atom<number[]>({
   key: "checkedCartItemsState",
   default: [],
   effects_UNSTABLE: [persistAtom],
+});
+
+export const userLiveInSigolStates = atom<boolean>({
+  key: "userLiveInSigolStates",
+  default: false,
+});
+
+export const selectedCoupons = atom<CouponInstances[]>({
+  key: "selectedCoupons",
+  default: [],
 });
 
 export const cartItemQuantityStates = atomFamily<number, number>({
