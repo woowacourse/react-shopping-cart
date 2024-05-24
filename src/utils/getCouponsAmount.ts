1998c215f44d5806927aec8f51ccd015ddc94a3e
ss0526100 <ss0526100@gmail.com>
2024-05-24 21:04:55 +0900

    refactor(getCouponAmount): 분리된 쿠폰 유효성 검증 로직 적용

--- src/utils/getCouponsAmount.ts
+++ src/utils/getCouponsAmount.ts
@@ -1,18 +1,7 @@
-import {
-  BuyXGetYCoupon,
-  CartItem,
-  Coupon,
-  FixedDiscountCoupon,
-  FreeShippingCoupon,
-  PercentageDiscountCoupon,
-} from '../type';
-import { getHHColonMMtoMinutes, iso8601ToDate } from './translateFormat';
+import { BuyXGetYCoupon, CartItem, Coupon, PercentageDiscountCoupon } from '../type';
 
-import getLastTimeDate from './getLastTimeDate';
+import checkIsAvailableCoupon from './checkIsAvailableCoupon';
 
-const getFixedCouponDiscount = (fixedCoupon: FixedDiscountCoupon, itemsAmount: number) => {
-  return itemsAmount >= fixedCoupon.minimumAmount ? fixedCoupon.discount : 0;
-};
 const getBuyXgetYCouponDiscount = (buyXgetYCoupon: BuyXGetYCoupon, items: CartItem[]) => {
   const overXQuantityItems = [...items].filter(
     (item) => item.quantity >= buyXgetYCoupon.buyQuantity,
@@ -23,38 +12,20 @@ const getBuyXgetYCouponDiscount = (buyXgetYCoupon: BuyXGetYCoupon, items: CartIt
   return biggestItemPrice * buyXgetYCoupon.getQuantity;
 };
 
-const getFreeShippingCouponDiscount = (
-  freeShippingCoupon: FreeShippingCoupon,
-  deliveryFee: number,
-  itemsAmount: number,
-) => {
-  return freeShippingCoupon.minimumAmount >= itemsAmount ? deliveryFee : 0;
-};
-
 const getPercentageCouponDiscount = (
   percentageCoupon: PercentageDiscountCoupon,
   itemsAmount: number,
 ) => {
-  const nowDate = new Date();
-  const nowMinutes = nowDate.getHours() * 60 + nowDate.getMinutes();
-  const couponStartMinutes = getHHColonMMtoMinutes(
-    percentageCoupon.availableTime.start.slice(0, 5),
-  );
-  const couponEndMinutes = getHHColonMMtoMinutes(percentageCoupon.availableTime.end.slice(0, 5));
-  return couponStartMinutes <= nowMinutes && nowMinutes <= couponEndMinutes
-    ? Math.floor((itemsAmount * percentageCoupon.discount) / 100)
-    : 0;
+  return Math.floor((itemsAmount * percentageCoupon.discount) / 100);
 };
 
 const getCouponDiscount = (coupon: Coupon, items: CartItem[], deliveryFee: number) => {
-  const expirationDate = getLastTimeDate(iso8601ToDate(coupon.expirationDate));
-  if (expirationDate < new Date()) return 0;
+  if (!checkIsAvailableCoupon(coupon, items, deliveryFee)) return 0;
 
   const itemsAmount = items.reduce((acc, item) => acc + item.quantity * item.product.price, 0);
-  if (coupon.discountType === 'fixed') return getFixedCouponDiscount(coupon, itemsAmount);
+  if (coupon.discountType === 'fixed') return coupon.discount;
   if (coupon.discountType === 'buyXgetY') return getBuyXgetYCouponDiscount(coupon, items);
-  if (coupon.discountType === 'freeShipping')
-    return getFreeShippingCouponDiscount(coupon, deliveryFee, itemsAmount);
+  if (coupon.discountType === 'freeShipping') return deliveryFee;
   if (coupon.discountType === 'percentage') return getPercentageCouponDiscount(coupon, itemsAmount);
   return 0;
 };
