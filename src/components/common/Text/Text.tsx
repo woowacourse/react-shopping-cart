c9b85672d7f723cefd077d63c0e63df3b05d514d
pakxe <pigkill40@naver.com>
2024-05-24 11:11:41 +0900

    feat: \n이 있는 경우 <br/>로 줄넘김 하는 로직 추가

--- src/components/common/Text/Text.tsx
+++ src/components/common/Text/Text.tsx
@@ -1,5 +1,5 @@
-import { ReactNode } from 'react';
-import * as S from './Text.style'
+import React, { ReactNode } from 'react';
+import * as S from './Text.style';
 
 export type TextProps = {
   size?: 's' | 'm' | 'l' | number;
@@ -7,11 +7,22 @@ export type TextProps = {
   children: ReactNode;
 };
 
-
 const Text = ({ children, size = 'm', weight = 'l' }: TextProps) => {
+  const makeNewLine = (text: ReactNode) => {
+    if (typeof text === 'string') {
+      return text.split('\n').map((line, index) => (
+        <React.Fragment key={index}>
+          {line}
+          <br />
+        </React.Fragment>
+      ));
+    }
+    return text;
+  };
+
   return (
     <S.Text size={size} weight={weight}>
-      {children}
+      {makeNewLine(children)}
     </S.Text>
   );
 };
