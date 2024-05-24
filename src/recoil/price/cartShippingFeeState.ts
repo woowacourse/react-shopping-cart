f07c242daef0fd495436a0177b89c5f289a5051c
pakxe <pigkill40@naver.com>
2024-05-24 11:13:07 +0900

    fix: 배송비 계산 오류 수정

--- src/recoil/price/cartShippingFeeState.ts
+++ src/recoil/price/cartShippingFeeState.ts
@@ -11,9 +11,11 @@ export const cartShippingFeeState = selector<number>({
     const totalDiscount = get(discountPriceByCouponListState);
     const isMountainIslandArea = get(isMountainIslandAreaState);
 
-    const shippingFee = SHIPPING_FEE.FREE_THRESHOLD + (isMountainIslandArea ? SHIPPING_FEE.MOUNTAIN_ISLAND_AREA : 0);
+    if (totalCartPrice >= SHIPPING_FEE.FREE_THRESHOLD) return 0;
 
-    if (totalCartPrice >= shippingFee || totalCartPrice <= 0) return 0;
+    const shippingFee = SHIPPING_FEE.DEFAULT + (isMountainIslandArea ? SHIPPING_FEE.MOUNTAIN_ISLAND_AREA : 0);
+
+    if (totalCartPrice <= shippingFee || totalCartPrice <= 0) return 0;
     if (totalDiscount.shippingFee === 'free') return 0;
 
     return shippingFee;
