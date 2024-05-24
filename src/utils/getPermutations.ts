290864f51d480e9ae1e9aa379dbd3ed7413967b1
Seongjin Hong <hongseongjin.to@gmail.com>
2024-05-24 16:20:40 +0900

    feat: 쿠폰 조합별 최대 할인금액 계산을 위한 순열 함수 작성

---
+++ src/utils/getPermutations.ts
@@ -0,0 +1,19 @@
+export default function getPermutations<T>(array: T[]): T[][] {
+  return array.reduce<T[][]>((result, _, index) => {
+    if (index === 0) {
+      return array.map((item) => [item]);
+    }
+
+    const permutations: T[][] = [];
+
+    result.forEach((currentNumber) => {
+      array.forEach((item) => {
+        if (!currentNumber.includes(item)) {
+          permutations.push([...currentNumber, item]);
+        }
+      });
+    });
+
+    return permutations;
+  }, []);
+}
