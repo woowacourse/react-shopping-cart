0215011df8d794efeb18c2822253f0531f77066b
GimGayeon <kky1373@naver.com>
2024-05-26 01:04:22 +0900

    feat(useDiscountType): 쿠폰 할인 타입에 따른 적용 방법 기능 구현

---
+++ src/hooks/useDiscountType.ts
@@ -0,0 +1,101 @@
+import { shippingFeeDiscountState } from './../recoil/coupons';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import {
+  selectedCartItemsSelector,
+  shippingFeeSelector,
+  totalOrderAmountSelector,
+} from './../recoil/cartItems';
+import { useEffect, useState } from 'react';
+import { CouponType, DiscountType } from '../components/type';
+
+const useDiscountType = () => {
+  const totalOrderAmount = useRecoilValue(totalOrderAmountSelector);
+  const [discountAmount, setDiscountAmount] = useState(totalOrderAmount);
+  const cartItemsBuyQuantity = useRecoilValue(selectedCartItemsSelector);
+  const [shippingFeeDiscount, setShippingFeeDiscount] = useRecoilState(
+    shippingFeeDiscountState,
+  );
+
+  const discountTypePriority = {
+    fixed: 3,
+    buyXgetY: 2,
+    freeShipping: 4,
+    percentage: 1,
+  };
+
+  const fixedCouponApply = (discount: number) => {
+    return (orderAmount: number) => orderAmount - discount;
+  };
+
+  const percentageCouponApply = (discount: number) => {
+    return (orderAmount: number) => orderAmount * (100 - discount) * 0.01;
+  };
+
+  /**
+   *
+   * buyQuantity : 구매 수량
+   * getQuantity : 무료로 받을 수 있는 수량
+   * 만약 buyQuantity+getQuantity 개 구매한 제품이 있다면 그 중 가장 비싼 제품의 * getQuantity 만큼 할인받는다.
+   */
+  const buyXgetYCouponApply = (buyQuantity: number, getQuantity: number) => {
+    const bulkCartItems = cartItemsBuyQuantity.filter((cartItem) => {
+      const quantity = cartItem.quantity;
+      return quantity >= buyQuantity + getQuantity;
+    });
+
+    return (orderAmount: number) => {
+      const bulkCartItemsPrice = bulkCartItems.map(
+        (cartItem) => cartItem.product.price,
+      );
+      const discountPrice = bulkCartItemsPrice.sort().pop();
+      return orderAmount - (discountPrice || 0) * getQuantity;
+    };
+  };
+
+  const freeShipping = () => {
+    return (orderAmount: number) => {
+      return orderAmount;
+    };
+  };
+
+  const selectDiscountType = (coupon: CouponType) => {
+    switch (coupon.discountType) {
+      case 'percentage':
+        return percentageCouponApply(coupon.discount!);
+      case 'buyXgetY':
+        return buyXgetYCouponApply(coupon.buyQuantity!, coupon.getQuantity!);
+      case 'fixed':
+        return fixedCouponApply(coupon.discount!);
+      case 'freeShipping':
+        return freeShipping();
+    }
+  };
+
+  const applyCoupon = (coupons: CouponType[]) => {
+    // 쿠폰 우선순위로 정렬
+    coupons.sort(
+      (a, b) =>
+        discountTypePriority[a.discountType] -
+        discountTypePriority[b.discountType],
+    );
+    let discount = totalOrderAmount;
+    let isFreeShipping = false;
+
+    coupons.forEach((coupon) => {
+      const applyDiscount = selectDiscountType(coupon);
+      discount = applyDiscount(discount);
+
+      isFreeShipping = coupon.discountType === 'freeShipping' ? true : false;
+      //TODO: 이 부분 리팩
+    });
+
+    setDiscountAmount(discount);
+    setShippingFeeDiscount(isFreeShipping);
+  };
+
+  const getDiscountAmount = totalOrderAmount - discountAmount;
+
+  return { applyCoupon, getDiscountAmount };
+};
+
+export default useDiscountType;
