f646411f2c44dc88aad0c128f0cb84ef6a94b9a8
hwinkr <dnddl8280@naver.com>
2024-05-25 16:32:52 +0900

    feat: 장바구니 아이템 쿠폰 적용 모달 컴포넌트 구현
    - 적용하기 버튼을 누를 경우에만 atom에 반영되도록 설정

---
+++ src/components/CartItemCouponModal/CartItemCouponModal.tsx
@@ -0,0 +1,70 @@
+import { useRecoilValue } from "recoil";
+import { couponsState } from "@/recoil/coupon";
+import useCouponSimulator from "@/hooks/useCouponSimulator";
+
+import { Modal } from "harrysimodal";
+
+import Caption from "../_common/Caption/Caption";
+
+import MoreInfo from "@/assets/more-info.svg?react";
+import CartItemCoupon from "../CartItemCoupon/CartItemCoupon";
+
+interface CartItemCouponModalProps {
+  isModalOpen: boolean;
+  closeModal: () => void;
+}
+
+const CartItemCouponModal = ({
+  isModalOpen,
+  closeModal,
+}: CartItemCouponModalProps) => {
+  const coupons = useRecoilValue(couponsState);
+
+  const {
+    checkSelectedCoupon,
+    handleAddCoupon,
+    handleRemoveCoupon,
+    onTemporaryCouponsSubmit,
+    discountAmount,
+    hasTemporaryCoupons,
+  } = useCouponSimulator();
+
+  const onButtonClick = () => {
+    onTemporaryCouponsSubmit();
+    closeModal();
+  };
+
+  return (
+    <Modal isOpen={isModalOpen} onClose={closeModal} position="center">
+      <Modal.ModalContent size="small">
+        <Modal.ModalHeader>
+          <Modal.ModalTitle text="쿠폰을 선택해 주세요" />
+          <Modal.ModalCloseButton onCloseButtonClick={closeModal} />
+        </Modal.ModalHeader>
+
+        <Caption
+          asset={() => <MoreInfo />}
+          text="쿠폰은 최대 2개까지 사용할 수 있습니다."
+        />
+        {coupons.map((coupon) => (
+          <CartItemCoupon
+            coupon={coupon}
+            checkSelectedCoupon={checkSelectedCoupon}
+            handleAddCoupon={handleAddCoupon}
+            handleRemoveCoupon={handleRemoveCoupon}
+          />
+        ))}
+
+        <Modal.ModalFooter direction="row" justify="between">
+          <Modal.ModalButton theme="dark" onClick={onButtonClick}>
+            {!hasTemporaryCoupons
+              ? "쿠폰 취소하기"
+              : `총 ${discountAmount}원 할인 쿠폰 사용하기`}
+          </Modal.ModalButton>
+        </Modal.ModalFooter>
+      </Modal.ModalContent>
+    </Modal>
+  );
+};
+
+export default CartItemCouponModal;
