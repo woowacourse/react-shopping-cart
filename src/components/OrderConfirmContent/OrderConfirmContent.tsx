09f121ec703122add75c1ca669bd8cf9c4a1a1a7
jaeml06 <jaeml0630@gmail.com>
2024-05-26 00:46:07 +0900

    refactor : handleFooterClick 추가

--- src/components/OrderConfirmContent/OrderConfirmContent.tsx
+++ src/components/OrderConfirmContent/OrderConfirmContent.tsx
@@ -1,5 +1,5 @@
 import { useRecoilValue } from 'recoil';
-import TotalAmount from '../TotalAmount/TotalAmount';
+import CouponTotalAmount from '../TotalAmount/CouponTotalAmount';
 import Title from '../Title/Title';
 import styled from 'styled-components';
 import { MESSAGES } from '../../constants/Messages';
@@ -11,7 +11,10 @@ import {
 import { CartItems } from '../../types/Item';
 import ConfirmItemCard from '../ItemCard/ConfirmItemCard';
 import CouponButton from '../CouponModal/CouponModal';
-import ShippingInformation from '../ShippingInformation';
+import ShippingInformation from '../ShippingInformation/ShippingInformation';
+import Footer from '../Footer/Footer';
+import { useNavigate } from 'react-router-dom';
+import { URL_PATH } from '../../constants/UrlPath';
 
 export const NoCartItemContainer = styled.p`
   width: 100%;
@@ -25,30 +28,51 @@ export const NoCartItemContainer = styled.p`
   text-align: center;
 `;
 
+const ContentWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+  padding: 3.6rem 2.4rem 10.4rem 2.4rem;
+  box-sizing: border-box;
+  height: 100%;
+`;
+
 function OrderConfirmContent() {
   const checkedItem = useRecoilValue(checkedItemsSelector);
+  const checkedItemId = checkedItem.map((item) => item.id);
   const { totalItemTypeCount, totalCount } = useRecoilValue(totalCountSelector);
-  const { totalAmount, deliveryFee, totalDiscount, calculatedTotalAmount } =
-    useRecoilValue(totalPriceSelector('Discount'));
+  const { calculatedTotalAmount } = useRecoilValue(
+    totalPriceSelector('Discount'),
+  );
+  const navigate = useNavigate();
+  const handleFooterClick = () => {
+    navigate(URL_PATH.completed, {
+      state: {
+        totalItemTypeCount,
+        totalCount,
+        calculatedTotalAmount,
+        checkedItemId,
+      },
+    });
+  };
   return (
     <>
-      <Title
-        title={MESSAGES.confirm}
-        subTitle={`총 ${totalItemTypeCount}종류의 상품 ${totalCount}개를 주문합니다.\n 최종 결제 금액을 확인해 주세요.`}
-      />
-      {checkedItem.map((product: CartItems) => {
-        return <ConfirmItemCard key={product.id} item={product} />;
-      })}
-      <CouponButton />
-      <ShippingInformation />
-      <TotalAmount
-        type={'discount'}
-        price={{
-          totalAmount,
-          deliveryFee,
-          totalDiscount,
-          calculatedTotalAmount,
-        }}
+      <ContentWrapper>
+        <Title
+          title={MESSAGES.confirm}
+          subTitle={`총 ${totalItemTypeCount}종류의 상품 ${totalCount}개를 주문합니다.\n 최종 결제 금액을 확인해 주세요.`}
+        />
+        {checkedItem.map((product: CartItems) => {
+          return <ConfirmItemCard key={product.id} item={product} />;
+        })}
+        <CouponButton />
+        <ShippingInformation />
+        <CouponTotalAmount />
+      </ContentWrapper>
+      <Footer
+        value={MESSAGES.payment}
+        isDisabled={false}
+        onClick={handleFooterClick}
       />
     </>
   );
