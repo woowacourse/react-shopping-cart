a23483815b11feddb0c9e900eb5aa7e99fd0804b
hwinkr <dnddl8280@naver.com>
2024-05-25 16:52:34 +0900

    configt: 쿠폰 타입 설정

--- src/types/cart.ts
+++ src/types/cart.ts
@@ -11,3 +11,23 @@ export interface Product {
   imageUrl: string;
   category: string;
 }
+
+export interface AvailableTime {
+  start: string;
+  end: string;
+}
+
+export interface Coupon {
+  id: number;
+  code: string;
+  description: string;
+  expirationDate: string;
+  discountType: "fixed" | "percentage" | "buyXgetY" | "freeShipping";
+  discount?: number;
+  minimumAmount?: number;
+  buyQuantity?: number;
+  getQuantity?: number;
+  availableTime?: AvailableTime;
+}
+
+export type SelectedCartItemQuantity = Pick<CartItem, "id" | "quantity">;
