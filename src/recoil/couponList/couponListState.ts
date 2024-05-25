9b63d48869b61d708841d5beb3b72e4e6b82cfa8
pakxe <pigkill40@naver.com>
2024-05-25 16:28:06 +0900

    feat: 쿠폰 목록의 상태를 selector -> atom으로 관리하도록 수정

--- src/recoil/couponList/couponListState.ts
+++ src/recoil/couponList/couponListState.ts
@@ -1,11 +1,8 @@
-import { selector } from 'recoil';
+import { atom } from 'recoil';
 import type { Coupon } from '../../types/coupon.type';
 import { requestCouponList } from '../../apis/requests/couponList';
 
-export const couponListStateQuery = selector<Coupon[]>({
-  key: 'couponListStateQuery',
-  get: async () => {
-    const couponList = await requestCouponList();
-    return couponList;
-  },
+export const couponListState = atom<Coupon[]>({
+  key: 'couponListState',
+  default: requestCouponList(),
 });
