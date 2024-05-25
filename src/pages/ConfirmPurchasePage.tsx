e192f3a42f51e014452d0923e38b4f0e946e7f86
이태훈 <rhymint@gmail.com>
2024-05-26 03:16:21 +0900

    feat: 결제하기 버튼 클릭 시 주문 완료 기능 추가

--- src/pages/ConfirmPurchasePage.tsx
+++ src/pages/ConfirmPurchasePage.tsx
@@ -1,16 +1,18 @@
 import { Suspense } from 'react';
 
+import { useNavigate } from 'react-router-dom';
 import { ErrorBoundary } from 'react-error-boundary';
 
+import useOrder from '../hooks/order/useOrders';
 import Header from '../components/Header/Header';
 import { Button } from '../components/common/Button/Button.style';
 import ErrorFallback from '../components/ErrorFallback/ErrorFallback';
 import LoadingFallback from '../components/LoadingFallback/LoadingFallback';
 import ConfirmPurchaseSection from '../components/ConfirmPurchaseSection/ConfirmPurchaseSection';
-import { useNavigate } from 'react-router-dom';
 
 const ConfirmPurchasePage = () => {
   const navigate = useNavigate();
+  const { orderSelectedCartItems } = useOrder();
   return (
     <>
       <Header type="back" />
@@ -26,10 +28,11 @@ const ConfirmPurchasePage = () => {
         size="l"
         style={{ position: 'fixed', maxWidth: '768px', bottom: '0' }}
         onClick={() => {
+          orderSelectedCartItems();
           navigate('/complete-purchase');
         }}
       >
-        결제 확인
+        결제하기
       </Button>
     </>
   );
