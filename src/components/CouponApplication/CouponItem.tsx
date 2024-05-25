a0e718bece1318199edc46445cfa18d2e5b7e0bb
GimGayeon <kky1373@naver.com>
2024-05-26 01:06:07 +0900

    feat(CouponItem): 쿠폰아이템 로직 기능 구현

---
+++ src/components/CouponApplication/CouponItem.tsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import CheckButton from '../common/CheckButton/CheckButton';
+import * as Styled from './style';
+import { CouponCode, AvailableType } from '../type';
+import { convertToTimeFormat } from '../util/convertToTimeFormat';
+import { koMoneyFormat } from '../util/koMoneyFormat';
+import { convertToDateFormat } from '../util/convertToDateFormat';
+
+interface CouponItemProp {
+  couponCode: CouponCode;
+  expirationDate: string;
+  description: string;
+  selectCoupon: (coupon: CouponCode) => void;
+  disable: boolean;
+  minimumAmount?: number;
+  availableTime?: AvailableType;
+}
+
+const CouponItem = ({
+  selectCoupon,
+  couponCode,
+  description,
+  expirationDate,
+  disable,
+  minimumAmount,
+  availableTime,
+}: CouponItemProp) => {
+  const [couponApply, setCouponApply] = useState(false);
+
+  const handleClickCoupon = () => {
+    setCouponApply((prop) => !prop);
+    selectCoupon(couponCode);
+  };
+  return (
+    <Styled.CouponItem disabled={disable}>
+      <Styled.CouponItemTitle>
+        <CheckButton
+          isSelected={couponApply}
+          setIsSelected={handleClickCoupon}
+        />
+        <p>{description}</p>
+      </Styled.CouponItemTitle>
+      <div>만료일: {convertToDateFormat(expirationDate)}</div>
+      {minimumAmount && (
+        <div>{`최소 주문 금액: ${koMoneyFormat(minimumAmount)}`}</div>
+      )}
+      {availableTime && (
+        <div>{`사용 가능 시간: ${convertToTimeFormat(availableTime.start)} 부터 ${convertToTimeFormat(availableTime.end)} 까지`}</div>
+      )}
+    </Styled.CouponItem>
+  );
+};
+
+export default CouponItem;
--- src/components/OrderConfirmationContainer/OrderConfirmationContainer.tsx
+++ src/components/OrderConfirmationContainer/OrderConfirmationContainer.tsx
@@ -1,23 +1,21 @@
-import { useRecoilValue } from 'recoil';
 import Title from '../common/Title/Title';
 import TotalPaymentInfo from '../common/TotalPaymentInfo/TotalPaymentInfo';
 import * as Styled from './style';
-import { selectedSomeCartItemsSelector } from '../../recoil/selectedCardItems';
 import OrderButton from '../common/OrderButton/OrderButton';
 import { useNavigate } from 'react-router-dom';
-import { fetchedCartItemsSelector } from '../../recoil/fetch';
 import { ROUTE } from '../../constant/route';
 import PaymentInfo from '../common/TotalPaymentInfo/PaymentInfo';
 import ReadOnlyCartItemList from '../CartItemList/ReadOnlyCartItemList';
 import CouponApplication from '../CouponApplication/CouponApplication';
+import { useRecoilValue } from 'recoil';
+import { totaltotalSelector } from '../../recoil/coupons';
+import { koMoneyFormat } from '../util/koMoneyFormat';
+import DeliveryInfo from '../DeliveryInfo/DeliveryInfo';
 
 const OrderConfirmationContainer = () => {
   const navigator = useNavigate();
-  const cartItems = useRecoilValue(fetchedCartItemsSelector);
-
-  const hasSomeCartItem = !!cartItems.length;
-  const isSomeCartItemSelected = useRecoilValue(selectedSomeCartItemsSelector);
-  const isOrderable = hasSomeCartItem && isSomeCartItemSelected;
+  const discount = useRecoilValue(totaltotalSelector);
+  //TODO: 네이밍 변경
 
   return (
     <>
@@ -29,16 +27,21 @@ const OrderConfirmationContainer = () => {
         />
         <ReadOnlyCartItemList />
         <CouponApplication />
+        <DeliveryInfo />
         <TotalPaymentInfo>
-          {<PaymentInfo label="쿠폰 할인 금액" price={'-2000원'} />}
+          {
+            <PaymentInfo
+              label="쿠폰 할인 금액"
+              price={`${discount === 0 ? '' : '-'}${koMoneyFormat(discount)}`}
+            />
+          }
         </TotalPaymentInfo>
       </Styled.Container>
-
       <OrderButton
         onClick={() => {
           navigator(ROUTE.buyItem.path);
         }}
-        isOrderable={isOrderable}
+        isOrderable={true}
       >
         결제하기
       </OrderButton>
