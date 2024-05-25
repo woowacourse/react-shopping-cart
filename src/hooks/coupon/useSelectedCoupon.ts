f118af519d956491676f10dc113682fd8bbbf01b
이태훈 <rhymint@gmail.com>
2024-05-25 22:30:53 +0900

    fix: useSelectedCoupon 내부의 메소드 명 변경

--- src/hooks/coupon/useSelectedCoupon.ts
+++ src/hooks/coupon/useSelectedCoupon.ts
@@ -3,26 +3,26 @@ import { useRecoilState } from 'recoil';
 import { selectedCouponListState } from '../../recoil/coupon/atom';
 
 const useSelectedCoupon = () => {
-  const [selectedIdList, setSelectedIdList] = useRecoilState(
+  const [selectedCouponList, setSelectedCouponList] = useRecoilState(
     selectedCouponListState,
   );
 
-  const addSelectedCoupon = async (coupon: Coupon) => {
-    setSelectedIdList((prev) => [...prev, coupon]);
+  const selectCoupon = async (coupon: Coupon) => {
+    setSelectedCouponList((prev) => [...prev, coupon]);
   };
 
-  const removeSelectedCoupon = async (coupon: Coupon) => {
-    setSelectedIdList((prev) => [...prev].filter(({ id }) => id !== coupon.id));
+  const unselectCoupon = async (coupon: Coupon) => {
+    setSelectedCouponList((prev) =>
+      [...prev].filter(({ id }) => id !== coupon.id),
+    );
   };
 
   const toggleSelectedCoupon = (coupon: Coupon) => {
-    isSelected(coupon.id)
-      ? removeSelectedCoupon(coupon)
-      : addSelectedCoupon(coupon);
+    isSelected(coupon.id) ? unselectCoupon(coupon) : selectCoupon(coupon);
   };
 
   const isSelected = (couponId: number) => {
-    return selectedIdList.some(({ id }) => id === couponId);
+    return selectedCouponList.some(({ id }) => id === couponId);
   };
 
   return {
