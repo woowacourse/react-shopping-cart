4ad5250e987649bf4dd205cb277f69a3ed5ea957
pakxe <pigkill40@naver.com>
2024-05-25 17:12:52 +0900

    feat: 결제 완료 페이지 라우트 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MobileLayout from './components/common/MobileLayout/MobileLayout';
-import CartPageContainer from './pages/CartOrders/CartPageContainer';
+import CartOrdersPageContainer from './pages/CartOrders/CartOrdersPageContainer';
 import StartPage from './pages/StartPage';
 import { ROUTES } from './constants/routes';
 import CartOrderFormPageContainer from './pages/CartOrderForm/CartOrderFormPageContainer';
+import CartOrderCompletePage from './pages/CartOrderComplete/CartOrderCompletePage';
 
 function App() {
   return (
@@ -11,8 +12,9 @@ function App() {
       <MobileLayout>
         <Routes>
           <Route path="/" element={<StartPage />} />
-          <Route path={ROUTES.CART_ORDERS} element={<CartPageContainer />} />
+          <Route path={ROUTES.CART_ORDERS} element={<CartOrdersPageContainer />} />
           <Route path={ROUTES.CART_ORDER_FORM} element={<CartOrderFormPageContainer />} />
+          <Route path={ROUTES.CART_ORDER_COMPLETE} element={<CartOrderCompletePage />} />
         </Routes>
       </MobileLayout>
     </BrowserRouter>
