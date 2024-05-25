549e587fc973cbfb8b44b25cdd374ccc1ab4d2b9
vi-wolhwa <cerezo00@naver.com>
2024-05-26 00:22:27 +0900

    Add 'RecoilRoot' on 'main'

--- src/a_app/main.tsx
+++ src/a_app/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
 
-import App from './App.tsx';
+// eslint-disable-next-line import/order
+import '@/f_shared/styles/reset.css';
+import '@/f_shared/styles/color.css';
+import '@/f_shared/styles/index.css';
+
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </React.StrictMode>
 );
