ff9b8897bf4e67617124af4e036fe26be5286623
novice0840 <magnolia0840@gmail.com>
2024-05-26 01:19:26 +0900

    test: 테스트 코드 수정

--- src/test/coupon.test.tsx
+++ src/test/coupon.test.tsx
@@ -30,21 +30,18 @@ describe("쿠폰 유효성 테스트", () => {
 
   it("주문 가격이 10만원인 경우 '5000원 할인 쿠폰(최소 주문 10만원)' 사용 가능", async () => {
     const couponId = 1;
-    const { result } = renderHook(
-      () => useRecoilValue(possibleCouponListState),
-      {
-        wrapper: ({ children }) => (
-          <RecoilRoot
-            initializeState={({ set }) => {
-              set(cartItemsState, [makeCartItem(1234, 100000, 1)]);
-              set(selectedListState, [1234]);
-            }}
-          >
-            <Suspense>{children}</Suspense>
-          </RecoilRoot>
-        ),
-      }
-    );
+    const { result } = renderHook(() => useRecoilValue(possibleCouponListState), {
+      wrapper: ({ children }) => (
+        <RecoilRoot
+          initializeState={({ set }) => {
+            set(cartItemsState, [makeCartItem(1234, 100000, 1)]);
+            set(selectedListState, [1234]);
+          }}
+        >
+          <Suspense>{children}</Suspense>
+        </RecoilRoot>
+      ),
+    });
 
     await waitFor(() => {
       expect(result.current.includes(couponId)).toBeTruthy();
@@ -53,21 +50,18 @@ describe("쿠폰 유효성 테스트", () => {
 
   it("주문 가격이 9만원인 경우 '5000원 할인 쿠폰(최소 주문 10만원)' 사용 불가", async () => {
     const couponId = 1;
-    const { result } = renderHook(
-      () => useRecoilValue(possibleCouponListState),
-      {
-        wrapper: ({ children }) => (
-          <RecoilRoot
-            initializeState={({ set }) => {
-              set(cartItemsState, [makeCartItem(1234, 90000, 1)]);
-              set(selectedListState, [1234]);
-            }}
-          >
-            <Suspense>{children}</Suspense>
-          </RecoilRoot>
-        ),
-      }
-    );
+    const { result } = renderHook(() => useRecoilValue(possibleCouponListState), {
+      wrapper: ({ children }) => (
+        <RecoilRoot
+          initializeState={({ set }) => {
+            set(cartItemsState, [makeCartItem(1234, 90000, 1)]);
+            set(selectedListState, [1234]);
+          }}
+        >
+          <Suspense>{children}</Suspense>
+        </RecoilRoot>
+      ),
+    });
 
     await waitFor(() => {
       expect(result.current.includes(couponId)).toBeFalsy();
@@ -103,21 +97,18 @@ describe("쿠폰 유효성 테스트", () => {
   });
   it("3개 이상 담은 품목이 존재하지 않는 경우 '2개 구매 시 1개 무료 쿠폰' 사용 불가 ", async () => {
     const couponId = 2;
-    const { result } = renderHook(
-      () => useRecoilValue(possibleCouponListState),
-      {
-        wrapper: ({ children }) => (
-          <RecoilRoot
-            initializeState={({ set }) => {
-              set(cartItemsState, [makeCartItem(1234, 90000, 2)]);
-              set(selectedListState, [1234]);
-            }}
-          >
-            <Suspense>{children}</Suspense>
-          </RecoilRoot>
-        ),
-      }
-    );
+    const { result } = renderHook(() => useRecoilValue(possibleCouponListState), {
+      wrapper: ({ children }) => (
+        <RecoilRoot
+          initializeState={({ set }) => {
+            set(cartItemsState, [makeCartItem(1234, 90000, 2)]);
+            set(selectedListState, [1234]);
+          }}
+        >
+          <Suspense>{children}</Suspense>
+        </RecoilRoot>
+      ),
+    });
 
     await waitFor(() => {
       expect(result.current.includes(couponId)).toBeFalsy();
@@ -125,21 +116,18 @@ describe("쿠폰 유효성 테스트", () => {
   });
   it("주문 가격이 5만원인 경우 '2개 구매 시 5만원 이상 구매 시 무료 배송 쿠폰' 사용 가능 ", async () => {
     const couponId = 3;
-    const { result } = renderHook(
-      () => useRecoilValue(possibleCouponListState),
-      {
-        wrapper: ({ children }) => (
-          <RecoilRoot
-            initializeState={({ set }) => {
-              set(cartItemsState, [makeCartItem(1234, 50000, 1)]);
-              set(selectedListState, [1234]);
-            }}
-          >
-            <Suspense>{children}</Suspense>
-          </RecoilRoot>
-        ),
-      }
-    );
+    const { result } = renderHook(() => useRecoilValue(possibleCouponListState), {
+      wrapper: ({ children }) => (
+        <RecoilRoot
+          initializeState={({ set }) => {
+            set(cartItemsState, [makeCartItem(1234, 50000, 1)]);
+            set(selectedListState, [1234]);
+          }}
+        >
+          <Suspense>{children}</Suspense>
+        </RecoilRoot>
+      ),
+    });
 
     await waitFor(() => {
       expect(result.current.includes(couponId)).toBeTruthy();
@@ -147,28 +135,25 @@ describe("쿠폰 유효성 테스트", () => {
   });
   it("주문 가격이 4만원인 경우 '2개 구매 시 5만원 이상 구매 시 무료 배송 쿠폰' 사용 불가 ", async () => {
     const couponId = 3;
-    const { result } = renderHook(
-      () => useRecoilValue(possibleCouponListState),
-      {
-        wrapper: ({ children }) => (
-          <RecoilRoot
-            initializeState={({ set }) => {
-              set(cartItemsState, [makeCartItem(1234, 40000, 1)]);
-              set(selectedListState, [1234]);
-            }}
-          >
-            <Suspense>{children}</Suspense>
-          </RecoilRoot>
-        ),
-      }
-    );
+    const { result } = renderHook(() => useRecoilValue(possibleCouponListState), {
+      wrapper: ({ children }) => (
+        <RecoilRoot
+          initializeState={({ set }) => {
+            set(cartItemsState, [makeCartItem(1234, 40000, 1)]);
+            set(selectedListState, [1234]);
+          }}
+        >
+          <Suspense>{children}</Suspense>
+        </RecoilRoot>
+      ),
+    });
 
     await waitFor(() => {
       expect(result.current.includes(couponId)).toBeFalsy();
     });
   });
 
-  it.only("오전 4시에는 '미라클모닝 30% 할인 쿠폰' 사용 가능 ", async () => {
+  it("오전 4시에는 '미라클모닝 30% 할인 쿠폰' 사용 가능 ", async () => {
     jest.setSystemTime(new Date("2023-05-20T06:00:00"));
 
     const couponId = 4;
