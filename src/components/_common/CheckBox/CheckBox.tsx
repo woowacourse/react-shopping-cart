1ebe97cdc9f30dc83a877211b0b6aa4ae641488e
hwinkr <dnddl8280@naver.com>
2024-05-25 16:16:39 +0900

    refactor(CheckkBox): 체크박스 컴포넌트 리팩터링
    - svg를 Button 컴포넌트 내부로 이동
    - CheckBox 비활성화 추가

--- src/components/_common/CheckBox/CheckBox.tsx
+++ src/components/_common/CheckBox/CheckBox.tsx
@@ -1,17 +1,37 @@
+import styled from "styled-components";
+
 import Checked from "@/assets/checked.svg?react";
 import NotChecked from "@/assets/not-checked.svg?react";
 
+import { disabledClickStyles } from "../QuantityButton/QuantityButton.styles";
+import Button from "../Button/Button";
+
 interface CheckBoxProps {
   isChecked: boolean;
+  disabled?: boolean;
   onClick: () => void;
 }
 
-const CheckBox = ({ isChecked, onClick }: CheckBoxProps) => {
+const CheckBox = ({ isChecked, disabled, onClick }: CheckBoxProps) => {
   return (
-    <button onClick={onClick} role="checkbox">
-      {isChecked ? <Checked /> : <NotChecked />}
-    </button>
+    <Button
+      onClick={onClick}
+      disabled={disabled}
+      role="checkbox"
+      width="fit"
+      theme="transparent"
+    >
+      {disabled && <DisabledCheckbox />}
+      {!disabled && (isChecked ? <Checked /> : <NotChecked />)}
+    </Button>
   );
 };
 
 export default CheckBox;
+
+export const DisabledCheckbox = styled(Checked)`
+  width: 25px;
+
+  height: 25px;
+  ${disabledClickStyles}
+`;
