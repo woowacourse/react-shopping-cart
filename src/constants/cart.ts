f3a38ec1887e0cbee7fd89104eeae857ce156026
hwinkr <dnddl8280@naver.com>
2024-05-25 16:58:39 +0900

    config: 상수 설정

--- src/constants/cart.ts
+++ src/constants/cart.ts
@@ -1,4 +1,5 @@
 export const CART_FEE = {
   shippingFeeThreshold: 100_000,
-  shippingFee: 3000,
-};
+  shippingFee: 3_000,
+  remoteAreaShippingFee: 3_000,
+} as const;
---
+++ src/constants/path.ts
@@ -0,0 +1,7 @@
+const CLIENT_PATH = {
+  home: "/",
+  orderConfirm: "/order-confirm",
+  paymentConfirm: "/payment-confirm",
+} as const;
+
+export default CLIENT_PATH;
