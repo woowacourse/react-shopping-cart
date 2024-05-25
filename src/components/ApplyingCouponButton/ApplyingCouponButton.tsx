b3fa044b74467c92c883cb99ff8993fd07050c20
simhorim <hori5020@gmail.com>
2024-05-25 16:34:22 +0900

    feat(ApplyingCouponButton): 쿠폰 적용 모달 오픈 핸들러 추가

--- src/components/ApplyingCouponButton/ApplyingCouponButton.tsx
+++ src/components/ApplyingCouponButton/ApplyingCouponButton.tsx
@@ -1,7 +1,19 @@
+import { useSetRecoilState } from 'recoil';
 import * as Styled from './style';
+import { isApplyingCouponModalOpenState } from '../../recoil/atoms';
 
 const ApplyingCouponButton = () => {
-  return <Styled.ApplyingCouponButton>쿠폰 적용</Styled.ApplyingCouponButton>;
+  const setIsApplyingCouponModalOpen = useSetRecoilState(
+    isApplyingCouponModalOpenState,
+  );
+
+  const handleOnClick = () => setIsApplyingCouponModalOpen(true);
+
+  return (
+    <Styled.ApplyingCouponButton onClick={handleOnClick}>
+      쿠폰 적용
+    </Styled.ApplyingCouponButton>
+  );
 };
 
 export default ApplyingCouponButton;
