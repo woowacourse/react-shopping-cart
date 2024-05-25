ebdd9c6a89bf0c49b023a2cd0073d0975978d551
jaeml06 <jaeml0630@gmail.com>
2024-05-26 00:48:21 +0900

    refactr : Footer 컴포넌트 OrderConfirmContent안으로 이동

--- src/pages/OrderConfirm.tsx
+++ src/pages/OrderConfirm.tsx
@@ -1,13 +1,10 @@
 import { Suspense } from 'react';
 import Header from '../components/Header/Header';
 import styled from 'styled-components';
-import Footer from '../components/Footer/Footer';
 import ErrorFallback from '../components/ErrorFallback';
 import { ErrorBoundary } from 'react-error-boundary';
-import { URL_PATH } from '../constants/UrlPath';
 import OrderConfirmContent from '../components/OrderConfirmContent/OrderConfirmContent';
 import { NoCartItemContainer } from '../components/CartContent/CartContent';
-import { MESSAGES } from '../constants/Messages';
 
 const CartContainer = styled.div`
   display: flex;
@@ -16,32 +13,16 @@ const CartContainer = styled.div`
   height: 100%;
 `;
 
-const ContentWrapper = styled.section`
-  display: flex;
-  flex-direction: column;
-  gap: 1.5rem;
-  padding: 3.6rem 2.4rem 10.4rem 2.4rem;
-  box-sizing: border-box;
-  height: 100%;
-`;
-
 function OrderConfirm() {
   return (
     <CartContainer>
       <ErrorBoundary FallbackComponent={ErrorFallback}>
         <Header headerIconType="home" />
-        <ContentWrapper>
-          <Suspense
-            fallback={<NoCartItemContainer>Loading...</NoCartItemContainer>}
-          >
-            <OrderConfirmContent />
-          </Suspense>
-        </ContentWrapper>
-        <Footer
-          value={MESSAGES.payment}
-          isDisabled={false}
-          url={URL_PATH.completed}
-        />
+        <Suspense
+          fallback={<NoCartItemContainer>Loading...</NoCartItemContainer>}
+        >
+          <OrderConfirmContent />
+        </Suspense>
       </ErrorBoundary>
     </CartContainer>
   );
