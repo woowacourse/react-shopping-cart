dc4cf2d95ec135af2674eade19c57b0682ed1b94
jaeml06 <jaeml0630@gmail.com>
2024-05-26 00:08:48 +0900

    refactor : TotalAmount을 CouponTotalAmount, CartTotalAmount로 분리 구현

---
+++ src/components/TotalAmount/TotalAmount.styled.ts
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+
+export const TotalAmountContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.2rem;
+
+  font-size: 1.6rem;
+  font-weight: 700;
+  line-height: 1.6rem;
+`;
+
+export const NotificationIconImg = styled.img`
+  width: 1.2rem;
+  height: 1.2rem;
+`;
+
+export const InformationMsg = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 1.2rem;
+  gap: 0.4rem;
+`;
+
+export const TotalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.2rem;
+`;
+
+export const Border = styled.div`
+  border: 1px solid rgba(0, 0, 0, 0.1);
+`;
+
+export const TotalInfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+`;
+
+export const TotalInfoBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const TotalInfoLabel = styled.p`
+  font-size: 1.6rem;
+  line-height: 1.6rem;
+`;
+
+export const TotalInfoAmount = styled.p`
+  font-size: 2.4rem;
+  line-height: 3.475rem;
+  text-align: right;
+`;
