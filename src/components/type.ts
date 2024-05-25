025e540e500295ac36a4d41754a18e68e62252fa
GimGayeon <kky1373@naver.com>
2024-05-26 00:32:56 +0900

    refactor(type): 카드 및 쿠폰 데이터 타입 정의

---
+++ src/components/type.ts
@@ -0,0 +1,19 @@
+export type DiscountType = 'fixed' | 'buyXgetY' | 'freeShipping' | 'percentage';
+export type CouponCode = 'FIXED5000' | 'BOGO' | 'FREESHIPPING' | 'MIRACLESALE';
+
+export type AvailableType = {
+  start: string;
+  end: string;
+};
+export interface CouponType {
+  code: CouponCode;
+  description: string;
+  discount: number;
+  discountType: DiscountType;
+  expirationDate: string;
+  id: number;
+  minimumAmount?: number;
+  availableTime?: AvailableType;
+  buyQuantity?: number;
+  getQuantity?: number;
+}
