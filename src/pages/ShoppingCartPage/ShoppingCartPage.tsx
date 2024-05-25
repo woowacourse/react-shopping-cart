532c376af3650b666e132e6aa8b61cc0e527b3c3
손진영 <sm099@naver.com>
2024-05-26 05:09:57 +0900

    fix: 장바구니 페이지 돌아올 때 산간 지역 상태 reset
    장바구니 페이지로 돌아오면 해당 상태가 남아있어 배송비가 6000원이 보여지는 문제 해결

--- src/pages/ShoppingCartPage/ShoppingCartPage.tsx
+++ src/pages/ShoppingCartPage/ShoppingCartPage.tsx
@@ -1,11 +1,12 @@
 import { BottomButton, LoadingSpinner } from '@components/common';
 import APIErrorBoundary from '@components/common/ErrorBoundary/APIErrorBoundary';
 import ErrorFallback from '@components/common/ErrorBoundary/ErrorFallback/ErrorFallback';
+import { isInaccessibleAreaAtom } from '@recoil/orderConfirm';
 import { cartItemsAtom, selectedIdsAtom } from '@recoil/shoppingCart';
 import { ROUTE_PATHS } from '@routes/route.constant';
-import { lazy, Suspense } from 'react';
+import { lazy, Suspense, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useResetRecoilState } from 'recoil';
 
 import * as Styled from './ShoppingCartPage.styled';
 
@@ -16,6 +17,12 @@ const OrderPage: React.FC = () => {
   const selectedIds = useRecoilValue(selectedIdsAtom);
 
   const isButtonDisabled = cartItems.length === 0 || selectedIds.size === 0;
+
+  const resetIsInaccessibleArea = useResetRecoilState(isInaccessibleAreaAtom);
+
+  useEffect(() => {
+    resetIsInaccessibleArea();
+  }, [resetIsInaccessibleArea]);
 
   const navigate = useNavigate();
 
