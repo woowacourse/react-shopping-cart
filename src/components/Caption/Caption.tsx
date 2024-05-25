f07bb95500a4c23c2112e7e474631e78288c9c9c
simhorim <hori5020@gmail.com>
2024-05-26 04:20:38 +0900

    design(Caption): 적용가능하지 않은 경우 색상 연하게 보이도록 추가

--- src/components/Caption/Caption.tsx
+++ src/components/Caption/Caption.tsx
@@ -3,11 +3,12 @@ import * as Styled from './style';
 import { ReactNode } from 'react';
 
 interface CationProps {
+  isValid?: boolean;
   children: ReactNode;
 }
 
-const Caption = ({ children }: CationProps) => {
-  return <Styled.Caption>{children}</Styled.Caption>;
+const Caption = ({ isValid = true, children }: CationProps) => {
+  return <Styled.Caption $isValid={isValid}>{children}</Styled.Caption>;
 };
 
 export default Caption;
--- src/components/Caption/style.tsx
+++ src/components/Caption/style.tsx
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
 
-export const Caption = styled.p`
+export const Caption = styled.p<{ $isValid?: boolean }>`
   display: flex;
   align-items: center;
   gap: 0.25rem;
 
   ${(props) => props.theme.typography.caption}
-  color: ${(props) => props.theme.color.captionBlack};
+  color: ${(props) =>
+    props.$isValid
+      ? props.theme.color.captionBlack
+      : props.theme.color.borderGray};
 `;
