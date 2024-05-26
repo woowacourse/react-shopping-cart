b66a26e2921040cbaf415aeaaafb30fbfec9228d
chysis <chysiss@naver.com>
2024-05-26 14:13:29 +0900

    chore: 테스트용 AddButton이 아이템 추가 이후 새로고침하도록 변경

--- src/components/AddButton/AddButton.tsx
+++ src/components/AddButton/AddButton.tsx
@@ -1,9 +1,12 @@
 /** @jsxImportSource @emotion/react */
 
+import { useNavigate } from "react-router-dom";
 import { API_TOKEN } from "../../store/utils";
 import { AddButtonStyle } from "./AddButton.style";
 
 const AddButton = () => {
+  const navigate = useNavigate();
+
   return (
     <div
       css={AddButtonStyle}
@@ -13,11 +16,12 @@ const AddButton = () => {
             method: "POST",
             headers: { "Content-Type": "application/json", Authorization: API_TOKEN },
             body: JSON.stringify({
-              productId: 10, // 2, 3, 10, 11, 12, 21, 34로 설정해서 상품을 장바구니에 추가할 수 있습니다.
+              productId: 11, // 2, 3, 10, 11, 12, 21, 34로 설정해서 상품을 장바구니에 추가할 수 있습니다.
             }),
           });
         };
         fn();
+        navigate(0);
       }}
     >
       ADD
