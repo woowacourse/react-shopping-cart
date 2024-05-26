134b4d336a6175f073bdfcb3f1febd9966fb456e
simhorim <hori5020@gmail.com>
2024-05-26 17:13:28 +0900

    feat(utils): buttonLabel, navigarotPath, headerTitle generate 함수 기능 구현

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,6 @@
+import CONDITION from '../constants/Condition';
+import MESSAGE from '../constants/Message';
+
 export const formatExpirationDate = (expirationDate: string) => {
   const date = new Date(expirationDate);
   const year = date.getFullYear();
@@ -20,4 +23,54 @@ export const formatAvailableTime = (start: string, end: string) => {
     return `오전 ${startHour}시부터 오후 ${endHour}시까지`;
   if (startHour < 12 && endHour < 12)
     return `오전 ${startHour}시부터 ${endHour}시까지`;
+};
+
+export const generateButtonLabel = (page: string) => {
+  switch (page) {
+    case CONDITION.shoppingCartPage:
+      return MESSAGE.orderConfirmation;
+    case CONDITION.orderConfirmationPage:
+      return MESSAGE.pay;
+    case CONDITION.paymentConfirmationPage:
+      return MESSAGE.returningToShoppingCart;
+    default:
+      return '';
+  }
+};
+
+export const generateNextPageNavigatorPath = (page: string) => {
+  switch (page) {
+    case CONDITION.shoppingCartPage:
+      return CONDITION.orderConfirmationPage;
+    case CONDITION.orderConfirmationPage:
+      return CONDITION.paymentConfirmationPage;
+    case CONDITION.paymentConfirmationPage:
+      return CONDITION.shoppingCartPage;
+    default:
+      return '';
+  }
+};
+
+export const generatePrevPageNavigatorPath = (page: string) => {
+  switch (page) {
+    case CONDITION.shoppingCartPage:
+      return CONDITION.shoppingCartPage;
+    case CONDITION.orderConfirmationPage:
+      return CONDITION.shoppingCartPage;
+    case CONDITION.paymentConfirmationPage:
+      return '';
+    default:
+      return '';
+  }
+};
+
+export const generateHeaderTitle = (page: string) => {
+  switch (page) {
+    case CONDITION.shoppingCartPage:
+      return MESSAGE.shop;
+    case CONDITION.orderConfirmationPage:
+      return MESSAGE.backSpace;
+    default:
+      return '';
+  }
 };
