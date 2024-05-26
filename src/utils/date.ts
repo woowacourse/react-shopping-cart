31a17fe4949b21d96672d195856a16a1c06336de
badahertz52 <badahertz52@gmail.com>
2024-05-26 13:47:30 +0900

    docs: 불필요한 주석 삭제

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -13,7 +13,7 @@ export const getHoursFromServerData = (text: string) => {
 export const formatDateToString = (text: string) => {
   const date = new Date(text);
   const year = date.getFullYear();
-  const month = date.getMonth() + 1; // 월은 0부터 시작하므로 1을 더해줍니다.
+  const month = date.getMonth() + 1;
   const day = date.getDate();
 
   return `${year}년 ${month}월 ${day}일`;
