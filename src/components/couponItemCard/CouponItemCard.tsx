e33b2023f7df8d760f3009fc11c017e1a8e5fb23
00kang <kdb070970@gmail.com>
2024-05-26 20:15:31 +0900

    feat : CouponItemCard에서 useCouponApplicabilityChecker 사용

--- src/components/couponItemCard/CouponItemCard.tsx
+++ src/components/couponItemCard/CouponItemCard.tsx
@@ -1,3 +1,6 @@
+import { useRecoilValue } from "recoil";
+import { useCouponApplicabilityChecker } from "../../hooks/useCouponApplicabilityChecker";
+import { cartSummarySelectorState } from "../../recoil/selector/selector";
 import { Coupon } from "../../types";
 import { couponValidator } from "../../utils/couponValidator";
 import { formatDate } from "../../utils/formatDate";
@@ -23,13 +26,31 @@ export const CouponItemCard: React.FC<Coupon> = ({
   availableTime,
 }) => {
   const { isCouponValid } = couponValidator();
+  const { isCouponApplicable } = useCouponApplicabilityChecker();
+  const { orderPrice } = useRecoilValue(cartSummarySelectorState);
+
   const isValid = isCouponValid({ id, code, description, expirationDate, discountType });
+
+  const isApplicable = isCouponApplicable(
+    {
+      id,
+      code,
+      description,
+      expirationDate,
+      discountType,
+      minimumAmount,
+      buyQuantity,
+      getQuantity,
+      availableTime,
+    },
+    orderPrice
+  );
 
   const isChecked = true;
   const onCheck = () => {};
 
   return (
-    <StyledCouponItemCard disabled={!isValid}>
+    <StyledCouponItemCard disabled={!isValid || !isApplicable}>
       <StyledCouponItemCardHeader>
         <CheckboxButton isChecked={isChecked} onCheck={onCheck} />
         <StyledCouponItemCardTitle>{description}</StyledCouponItemCardTitle>
