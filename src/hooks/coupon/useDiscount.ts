dc71332a469acc182aa8574b7b3c20b06bc30ca6
김진호 <rlawlsgh1227@gmail.com>
2024-05-26 20:01:47 +0900

    refactor: discount 함수들 훅 외부로 분리

--- src/hooks/coupon/useDiscount.ts
+++ src/hooks/coupon/useDiscount.ts
@@ -4,42 +4,42 @@ import { BuyXGetYCoupon, Coupon, FixedCoupon, PercentageCoupon } from '@type/cou
 import { CartItem } from '@type/cartItem';
 import { useEffect, useState } from 'react';
 import usePriceInfo from '@hooks/usePriceInfo';
+import { IPriceInfo } from '@type/priceInfo';
+
+const discountByFixed = (coupon: FixedCoupon) => {
+  return coupon.discount as number;
+};
+
+const discountByBuyXGetY = (selectedItems: CartItem[], coupon: BuyXGetYCoupon) => {
+  // 2개 이상인 상품을 선별해서
+  const targetItems = selectedItems.filter(item => item.quantity >= (coupon.buyQuantity as number));
+
+  // 가장 가격이 비싼 상품을 고른다.
+  const targetItemsPrice = targetItems.map(item => item.product.price);
+  const maxPrice = Math.max(...targetItemsPrice);
+
+  return maxPrice;
+};
+
+const discountByFreeShipping = (priceInfo: IPriceInfo, isolatedRegion: boolean) => {
+  return priceInfo.shipping === 0 ? 0 : isolatedRegion ? 6000 : 3000;
+};
+
+const discountByPercentage = (cartItemsPrice: number, coupon: PercentageCoupon) => {
+  return (cartItemsPrice * (coupon.discount as number)) / 100;
+};
 
 const useDiscount = (applyingCoupons: Coupon[], isolatedRegion: boolean) => {
   const [discount, setDiscount] = useState(0);
   const selectedItems = useRecoilValue(selectedCartItems);
   const priceInfo = usePriceInfo(isolatedRegion);
 
-  const discountByFixed = (coupon: FixedCoupon) => {
-    return coupon.discount as number;
-  };
-
-  const discountByBuyXGetY = (selectedItems: CartItem[], coupon: BuyXGetYCoupon) => {
-    // 2개 이상인 상품을 선별해서
-    const targetItems = selectedItems.filter(
-      item => item.quantity >= (coupon.buyQuantity as number),
-    );
-
-    // 가장 가격이 비싼 상품을 고른다.
-    const targetItemsPrice = targetItems.map(item => item.product.price);
-    const maxPrice = Math.max(...targetItemsPrice);
-
-    return maxPrice;
-  };
-
-  const discountByFreeShipping = () => {
-    return priceInfo.shipping === 0 ? 0 : isolatedRegion ? 6000 : 3000;
-  };
-
-  const discountByPercentage = (cartItemsPrice: number, coupon: PercentageCoupon) => {
-    return (cartItemsPrice * (coupon.discount as number)) / 100;
-  };
-
   const discountByCoupon = (selectedItems: CartItem[], coupon: Coupon) => {
     if (coupon.discountType === 'fixed') return discountByFixed(coupon as FixedCoupon);
     if (coupon.discountType === 'buyXgetY')
       return discountByBuyXGetY(selectedItems, coupon as BuyXGetYCoupon);
-    if (coupon.discountType === 'freeShipping') return discountByFreeShipping();
+    if (coupon.discountType === 'freeShipping')
+      return discountByFreeShipping(priceInfo, isolatedRegion);
     if (coupon.discountType === 'percentage')
       return discountByPercentage(
         selectedItems.reduce((acc, cur) => acc + cur.quantity * cur.product.price, 0),
