67fcff77b3781692907d71663d37a415adee9971
simhorim <hori5020@gmail.com>
2024-05-26 17:14:07 +0900

    refactor(Header): generate 함수 유틸화에 따른 수정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,38 +1,22 @@
-import { useLocation, useNavigate } from 'react-router-dom';
 import * as Styled from './style';
-import MESSAGE from '../../constants/Message';
-import CONDITION from '../../constants/Condition';
+
+import { useLocation, useNavigate } from 'react-router-dom';
+
+import {
+  generateHeaderTitle,
+  generatePrevPageNavigatorPath,
+} from '../../utils/utils';
 
 const Header = () => {
-  const location = useLocation();
+  const page = useLocation().pathname;
   const navigator = useNavigate();
-
-  const title = () => {
-    switch (location.pathname) {
-      case CONDITION.shoppingCartPage:
-        return MESSAGE.shop;
-      case CONDITION.orderConfirmationPage:
-        return MESSAGE.backSpace;
-      default:
-        return '';
-    }
-  };
-
-  const handleOnClick = () => {
-    switch (location.pathname) {
-      case CONDITION.shoppingCartPage:
-        return navigator(CONDITION.shoppingCartPage);
-      case CONDITION.orderConfirmationPage:
-        return navigator(CONDITION.shoppingCartPage);
-      default:
-        return '';
-    }
-  };
 
   return (
     <Styled.Header>
-      <Styled.AppTitle onClick={() => handleOnClick()}>
-        {title()}
+      <Styled.AppTitle
+        onClick={() => navigator(generatePrevPageNavigatorPath(page))}
+      >
+        {generateHeaderTitle(page)}
       </Styled.AppTitle>
     </Styled.Header>
   );
