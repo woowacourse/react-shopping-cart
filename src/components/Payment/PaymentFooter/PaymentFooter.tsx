dde355446ab48ab29e7b1cf2214e25d77b746f74
chysis <chysiss@naver.com>
2024-05-26 14:24:46 +0900

    feat: 장바구니로 돌아가기 버튼 클릭 시 장바구니 초기화 로직

--- src/components/Payment/PaymentFooter/PaymentFooter.tsx
+++ src/components/Payment/PaymentFooter/PaymentFooter.tsx
@@ -2,18 +2,24 @@
 
 import { useNavigate } from "react-router-dom";
 import { PaymentFooterStyle } from "./PaymentFooter.style";
+import { useSetRecoilState } from "recoil";
+import { cartState } from "../../../store/atom/atoms";
+import { fetchProducts } from "../../../store/api";
 
 const PaymentFooter = () => {
   const navigate = useNavigate();
 
+  const setCartState = useSetRecoilState(cartState);
+
+  const handleClickReturnCart = async () => {
+    const { content }: { content: CartItemInfo[] } = await fetchProducts();
+    setCartState(content);
+
+    navigate("/");
+  };
+
   return (
-    <button
-      disabled={false}
-      css={PaymentFooterStyle}
-      onClick={() => {
-        navigate("/");
-      }}
-    >
+    <button disabled={false} css={PaymentFooterStyle} onClick={handleClickReturnCart}>
       장바구니로 돌아가기
     </button>
   );
