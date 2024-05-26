54edcf0c00174af2ae661a90b500e51fab1aae19
Yoonkyoung Kim <dev.yoonkyoung@gmail.com>
2024-05-26 22:44:01 +0900

    feat: 공통 컴포넌트 CouponApplyButton 구현

---
+++ src/components/button/CouponApplyButton/styled.ts
@@ -0,0 +1,11 @@
+import Button from "../../_common/Button";
+import styled from "styled-components";
+
+export const StyledButton = styled(Button)`
+  width: 100%;
+  height: 48px;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  font-size: var(--font-size-base);
+  font-weight: var(--font-weight-bold);
+  line-height: 16px;
+`;
