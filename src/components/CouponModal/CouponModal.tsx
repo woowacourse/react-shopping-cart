f8b0317d21ddd1931c45fb4351bd241d386e0aaa
ImxYJL <allensain14@gmail.com>
2024-05-26 14:39:02 +0900

    refactor: CouponModal 리팩토링
    1. 스타일 수정
    2. 쿠폰 선택에 따른 할인 금액 수정 로직 적용

--- src/components/CouponModal/CouponModal.tsx
+++ src/components/CouponModal/CouponModal.tsx
@@ -1,6 +1,11 @@
+import { COUPON } from '../../constants/constants';
+import info from '../../assets/info.svg';
+import close from '../../assets/close.svg';
 import CouponList from './CouponList/CouponList';
 import { Modal } from 'ollie-modal-components';
-import info from '../../assets/info.svg';
+
+import { useRecoilState } from 'recoil';
+import { modalDiscountState, orderDiscountState } from './../../recoil/atoms';
 import * as S from './styled';
 
 export interface CouponModalProps {
@@ -9,8 +14,16 @@ export interface CouponModalProps {
 }
 
 const CouponModal = ({ isOpen, toggleModal }: CouponModalProps) => {
+  const [modalDiscountAmount, setModalDiscountAmount] = useRecoilState(modalDiscountState);
+  const [orderDiscountAmount, setOrderDiscountAmount] = useRecoilState(orderDiscountState);
+
+  const onClickModalButton = () => {
+    setOrderDiscountAmount(modalDiscountAmount);
+    toggleModal();
+  };
+
   return (
-    <Modal isOpen={isOpen} onClose={toggleModal}>
+    <Modal isOpen={isOpen} onClose={toggleModal} style={{ borderRadius: '8px' }}>
       <Modal.ModalHeader style={{ margin: '3.2rem' }}>
         <Modal.ModalTitle
           style={{
@@ -23,13 +36,13 @@ const CouponModal = ({ isOpen, toggleModal }: CouponModalProps) => {
           쿠폰을 선택해 주세요
         </Modal.ModalTitle>
         <Modal.ModalCloseButton onClick={toggleModal} style={{ cursor: 'pointer' }}>
-          X
+          <img src={close} alt="" />
         </Modal.ModalCloseButton>
       </Modal.ModalHeader>
       <Modal.ModalContent style={{ margin: '3.2rem' }}>
         <S.Info>
           <img src={info} alt="" />
-          <S.InfoText>쿠폰은 최대 2개까지 사용할 수 있습니다.</S.InfoText>
+          <S.InfoText>{`쿠폰은 최대 ${COUPON.MAX_APPLICABLE_COUNT}개까지 사용할 수 있습니다.`}</S.InfoText>
         </S.Info>
         <CouponList></CouponList>
       </Modal.ModalContent>
@@ -37,14 +50,16 @@ const CouponModal = ({ isOpen, toggleModal }: CouponModalProps) => {
       <Modal.ModalFooter align={'center'} style={{ margin: '0' }}>
         <Modal.ModalButton
           size={'L'}
-          onClick={toggleModal}
+          onClick={onClickModalButton}
           style={{
             borderRadius: '5px',
+            width: '35rem',
             margin: '0 3.2rem 2.4rem 3.2rem',
             padding: '1.1rem 6.2rem',
+            cursor: 'pointer',
           }}
         >
-          총 6,000원 할인 쿠폰 사용하기
+          {`총 ${modalDiscountAmount.toLocaleString()}원 할인 쿠폰 사용하기`}
         </Modal.ModalButton>
       </Modal.ModalFooter>
     </Modal>
