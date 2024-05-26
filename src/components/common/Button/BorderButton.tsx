8ad2694936b6c80aab0229fd4636d95a08166b63
hain-tain <hain7ce@gmail.com>
2024-05-27 01:18:03 +0900

    feat: BorderButton에 widthType 추가

--- src/components/common/Button/BorderButton.tsx
+++ src/components/common/Button/BorderButton.tsx
@@ -1,21 +1,28 @@
 import styled from "@emotion/styled";
 
+type WidthType = "fit" | "full";
 interface Props {
   onClick: () => void;
+  widthType?: WidthType;
 }
 
 const BorderButton = ({
   children,
+  widthType = "fit",
   onClick,
 }: React.PropsWithChildren<Props>) => {
-  return <StyledButton onClick={onClick}>{children}</StyledButton>;
+  return (
+    <StyledButton onClick={onClick} withType={widthType}>
+      {children}
+    </StyledButton>
+  );
 };
 
 export default BorderButton;
 
-const StyledButton = styled.button`
-  width: fit-content;
-  padding: 6px 8px;
+const StyledButton = styled.button<{ withType: WidthType }>`
+  width: ${(props) => (props.withType === "fit" ? "fit-content" : "100%")};
+  padding: ${(props) => (props.withType === "fit" ? "6px 8px" : "12px")};
   border-radius: 4px;
   border: 1px solid rgba(0, 0, 0, 0.1);
   background-color: #ffffff;
