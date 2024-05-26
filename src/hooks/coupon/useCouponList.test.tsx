7efd05ea73fd218c268e007c51a693432e4d48f6
이태훈 <rhymint@gmail.com>
2024-05-27 04:27:27 +0900

    test: useCouponList 비동기 처리 관련 테스트코드 추가

--- src/hooks/coupon/useCouponList.test.tsx
+++ src/hooks/coupon/useCouponList.test.tsx
@@ -5,7 +5,9 @@ import { RecoilRoot } from 'recoil';
 import { renderHook } from '@testing-library/react';
 
 import useCouponList from './useCouponList';
+import useApiErrorState from '../error/useApiErrorState';
 import { couponListState } from '../../recoil/coupon/atom';
+import { FailedFetchCouponListError } from '../../error/customError';
 
 jest.mock('../../apis/couponList', () => ({
   requestCouponList: jest.fn(),
@@ -64,6 +66,55 @@ describe('useCouponList', () => {
     await act(async () => await result.current.fetchCouponList());
     const { requestCouponList } = require('../../apis/couponList');
     expect(requestCouponList).toHaveBeenCalled();
+  });
+
+  it('fetchCouponList는 couponList가 비어있지 않을때 실행되지 않는다.', async () => {
+    const { result } = renderHook(() => useCouponList(), {
+      wrapper: ({ children }) => (
+        <RecoilRoot
+          initializeState={({ set }) => set(couponListState, [MOCK_COUPONS[0]])}
+        >
+          {children}
+        </RecoilRoot>
+      ),
+    });
+    await act(async () => await result.current.fetchCouponList());
+    const { requestCouponList } = require('../../apis/couponList');
+    expect(requestCouponList).not.toHaveBeenCalled();
+    expect(result.current.couponList).toStrictEqual([MOCK_COUPONS[0]]);
+  });
+
+  it('fetchCouponList가 실패할 경우 apiError 상태를 FailedFetchCouponListError로 설정해야 한다.', async () => {
+    const useCustomHook = () => {
+      return { ...useCouponList(), ...useApiErrorState() };
+    };
+    const { result } = renderHook(() => useCustomHook(), {
+      wrapper: ({ children }) => <RecoilRoot>{children}</RecoilRoot>,
+    });
+
+    const { requestCouponList } = require('../../apis/couponList');
+    requestCouponList.mockRejectedValueOnce(new Error('API Error'));
+
+    await act(async () => {
+      await result.current.fetchCouponList();
+    });
+
+    expect(result.current.apiError).toBeInstanceOf(FailedFetchCouponListError);
+  });
+
+  it('fetchCouponList가 성공할 경우 apiError 상태는 null로 유지되어야 한다.', async () => {
+    const useCustomHook = () => {
+      return { ...useCouponList(), ...useApiErrorState() };
+    };
+    const { result } = renderHook(() => useCustomHook(), {
+      wrapper: ({ children }) => <RecoilRoot>{children}</RecoilRoot>,
+    });
+
+    await act(async () => {
+      await result.current.fetchCouponList();
+    });
+
+    expect(result.current.apiError).toBe(null);
   });
 
   it('couponList는 저장된 "쿠폰 목록" 상태와 같은 값을 불러와야 한다.', async () => {
