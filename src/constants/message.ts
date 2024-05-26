83cef3f1f43dd52e60153e9ef8bf9b0445f90a4f
hain-tain <hain7ce@gmail.com>
2024-05-27 01:14:37 +0900

    feat: 쿠폰 및 주문관련 메세지 및 에러 메세지 추가

--- src/constants/message.ts
+++ src/constants/message.ts
@@ -15,3 +15,29 @@ export const ORDER_CONFIRM_MESSAGE = {
     `총 ${itemCount}종류의 상품 ${totalQuantity}개를 주문합니다.`,
   confirmPrice: `최종 결제 금액을 확인해 주세요.`,
 };
+
+export const COUPON_MESSAGE = {
+  maxApplicableCoupon: (number: number) =>
+    `쿠폰은 최대 ${number}개까지 사용할 수 있습니다.`,
+  chooseCoupon: "쿠폰을 선택해 주세요",
+  applyCoupon: (amount: number) =>
+    `총 ${amount.toLocaleString("ko-KR")}원 할인 쿠폰 사용하기`,
+
+  expirationDate: (date: string) => {
+    const [year, month, day] = date.split("-");
+    return `${year}년 ${Number(month)}월 ${Number(day)}일`;
+  },
+
+  minimumAmount: (amount: number) => `${amount.toLocaleString("ko-KR")}원`,
+
+  availableTime: ({ start, end }: { start: string; end: string }) => {
+    const startTime = Number(start.slice(0, 2));
+    const endTime = Number(end.slice(0, 2));
+    const startSlot = startTime < 12 ? "오전" : "오후";
+    const endSlot = endTime < 12 ? "오전" : "오후";
+
+    return `${startSlot} ${startTime}시부터 ${
+      startSlot === endSlot ? "" : endSlot
+    } ${endTime}까지`;
+  },
+};
