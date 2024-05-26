a26ef8902c03f6b624b0d126551a91f42755bce6
00kang <kdb070970@gmail.com>
2024-05-26 14:03:59 +0900

    feat : useCouponApplicabilityChecker 커스텀 훅에 사용 가능 시간 조건 확인 추가

--- src/hooks/useCouponApplicabilityChecker.ts
+++ src/hooks/useCouponApplicabilityChecker.ts
@@ -6,12 +6,42 @@ export const useCouponApplicabilityChecker = () => {
   const { findCouponByCode } = useCouponFinder();
   const { isCouponValid } = useCouponValidator();
 
-  const isCouponApplicable = (coupon: Coupon, totalAmount: number) => {
+  const isCouponApplicable = (coupon: Coupon, totalAmount: number, now: Date = new Date()) => {
     const targetCoupon = findCouponByCode(coupon.code);
     if (!targetCoupon || !isCouponValid(targetCoupon)) return false;
 
     if (targetCoupon.minimumAmount && totalAmount < targetCoupon.minimumAmount) {
       return false;
+    }
+
+    if (targetCoupon.availableTime) {
+      const [startHour, startMinute, startSecond] = targetCoupon.availableTime.start
+        .split(":")
+        .map(Number);
+
+      const [endHour, endMinute, endSecond] = targetCoupon.availableTime.end.split(":").map(Number);
+
+      const startTime = new Date(
+        now.getFullYear(),
+        now.getMonth(),
+        now.getDate(),
+        startHour,
+        startMinute,
+        startSecond
+      );
+
+      const endTime = new Date(
+        now.getFullYear(),
+        now.getMonth(),
+        now.getDate(),
+        endHour,
+        endMinute,
+        endSecond
+      );
+
+      if (now < startTime || now > endTime) {
+        return false;
+      }
     }
 
     return true;
