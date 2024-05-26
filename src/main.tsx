2f5c7e3cbe3bc79264014061052345c19f1433a2
ImxYJL <allensain14@gmail.com>
2024-05-26 13:43:47 +0900

    chore: strictmode 제거

--- src/main.tsx
+++ src/main.tsx
@@ -8,12 +8,12 @@ import { RouterProvider } from 'react-router-dom';
 import router from './router.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <RecoilRoot>
         <RouterProvider router={router} />
       </RecoilRoot>
     </ThemeProvider>
-  </React.StrictMode>,
+  </>,
 );
