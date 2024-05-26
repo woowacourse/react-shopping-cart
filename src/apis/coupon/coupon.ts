06a0b8df5c0ed22b1a695eade6d5a1652265e42f
Seongjin Hong <hongseongjin.to@gmail.com>
2024-05-26 15:55:44 +0900

    fix: 쿠폰 목록 API fetch 함수 수정

--- src/apis/coupon/coupon.ts
+++ src/apis/coupon/coupon.ts
@@ -1,12 +1,9 @@
-import { generateBasicToken } from '../../utils/auth';
-
 import { fetchData } from '../';
-import { USER_ID, USER_PASSWORD, API_ROUTE } from '../constants/apiSetting';
+import { API_ROUTE } from '../constants/apiSetting';
 import { Coupon } from '../../type';
 
 export async function fetchCoupons(): Promise<Coupon[]> {
-  const token = generateBasicToken(USER_ID, USER_PASSWORD);
-  const response = await fetchData({ url: API_ROUTE.COUPONS, method: 'GET', token });
+  const response = await fetchData({ url: API_ROUTE.COUPONS, method: 'GET' });
   const couponsData = await response.json();
-  return couponsData.content;
+  return couponsData;
 }
