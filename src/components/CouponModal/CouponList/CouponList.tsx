06d69ac45ab2af174f27f2e1bbd76867346389dc
ImxYJL <allensain14@gmail.com>
2024-05-26 14:36:06 +0900

    feat: CouponList 컴포넌트 구현
    1. 쿠폰 목록을 불러오는 기능 구현
    2. 선택된 쿠폰 목록을 다루는 로직 구현
    3. 할인 금액을 계산하는 로직 적용

--- src/components/CouponModal/CouponList/CouponList.tsx
+++ src/components/CouponModal/CouponList/CouponList.tsx
@@ -1,12 +1,45 @@
+import { COUPON_DISCOUNT_TYPE } from '../../../constants/constants';
 import CouponItem from '../CouponItem/CouponItem';
+import getCoupons from '../../../api/get/getCoupons';
+import useFetch from '../../../hooks/useFetch';
+import { selectedCoupons, modalDiscountState, shippingFeeState } from '../../../recoil/atoms';
+import { priceInfoStore } from '../../../recoil/selectors';
+import useCouponDiscountCalculator from '../../../hooks/coupons/useCouponDiscountCalculator';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { useEffect } from 'react';
 
 const CouponList = () => {
-  return (
-    <>
-      <CouponItem></CouponItem>
-      <CouponItem></CouponItem>
-    </>
-  );
+  const { data } = useFetch(getCoupons);
+  const [selected, setSelected] = useRecoilState(selectedCoupons);
+  const [modalDiscountAmount, setModalDiscountAmount] = useRecoilState(modalDiscountState);
+  const [shippingFeeInfo, setShippingFeeInfo] = useRecoilState(shippingFeeState);
+  const { calculateBestDiscount } = useCouponDiscountCalculator(selected);
+  const priceInfo = useRecoilValue(priceInfoStore);
+
+  const isAppliedFreeShipping = () => {
+    return selected.find(selectedItem => {
+      return selectedItem.discountType === COUPON_DISCOUNT_TYPE.FREE_SHIPPING;
+    });
+  };
+
+  useEffect(() => {
+    if (isAppliedFreeShipping()) {
+      setShippingFeeInfo({
+        isFree: true,
+        shipping: 0,
+      });
+    } else {
+      setShippingFeeInfo({
+        ...shippingFeeInfo,
+        isFree: false,
+      });
+    }
+
+    const bestDiscountAmount = calculateBestDiscount(priceInfo.order);
+    setModalDiscountAmount(bestDiscountAmount);
+  }, [selected]);
+
+  return <>{data?.map(coupon => <CouponItem key={coupon.id} coupon={coupon} />)}</>;
 };
 
 export default CouponList;
