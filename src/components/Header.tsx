4e9dcc64dbc39b154517306acc7fb6e0680de531
hain-tain <hain7ce@gmail.com>
2024-05-27 01:16:30 +0900

    fix: Header Link to 를 navigate로 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,45 @@
 import ArrowBack from "@/assets/Arrow-Back.svg";
-import { Link } from "react-router-dom";
 import Logo from "@/assets/Logo.svg";
 import styled from "@emotion/styled";
+import { useNavigate } from "react-router-dom";
 
-type HeaderType = "Logo" | "ArrowBack";
+type HeaderType = "Logo" | "ArrowBack" | "none";
 
 interface Props {
   type?: HeaderType;
 }
 
 const Header = ({ type = "Logo" }: Props) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    if (type === "ArrowBack") {
+      navigate(-1);
+      return;
+    }
+    navigate("/");
+  };
+
   return (
     <StyledHeader>
-      <Link to="/">
-        <img src={type === "Logo" ? Logo : ArrowBack} alt="" />
-      </Link>
+      {type !== "none" && (
+        <img src={getHeaderButtonImg(type)} alt="" onClick={handleClick} />
+      )}
     </StyledHeader>
   );
 };
 export default Header;
+
+const getHeaderButtonImg = (type: HeaderType) => {
+  switch (type) {
+    case "Logo":
+      return Logo;
+    case "ArrowBack":
+      return ArrowBack;
+    default:
+      return "";
+  }
+};
 
 const StyledHeader = styled.header`
   width: 100%;
