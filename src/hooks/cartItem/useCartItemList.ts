29e1d01015a8ef427c3549785c53773dd5e695f6
이태훈 <rhymint@gmail.com>
2024-05-26 12:16:37 +0900

    feat: useCartItemList에서 비동기 요청 실패 시 에러를 반환하도록 수정

--- src/hooks/cartItem/useCartItemList.ts
+++ src/hooks/cartItem/useCartItemList.ts
@@ -1,26 +1,39 @@
 import { useRecoilState } from 'recoil';
 
+import useApiErrorState from '../error/useApiErrorState';
 import { cartItemListState } from '../../recoil/cartItem/atom';
+import { useSelectedCartItemId } from './useSelectedCartItemId';
 import {
   requestCartItemList,
   requestDeleteCartItem,
 } from '../../apis/cartItemList';
-import { useSelectedCartItemId } from './useSelectedCartItemId';
 
 const useCartItemList = () => {
+  const { setApiError, resetApiError } = useApiErrorState();
   const [cartItemList, setCartItemList] = useRecoilState(cartItemListState);
   const { unselectCartItem } = useSelectedCartItemId();
 
   const deleteCartItem = async (cartItemId: number) => {
-    await requestDeleteCartItem(cartItemId);
+    try {
+      await requestDeleteCartItem(cartItemId);
+      setCartItemList(cartItemList.filter((item) => item.id !== cartItemId));
+      unselectCartItem(cartItemId);
 
-    setCartItemList(cartItemList.filter((item) => item.id !== cartItemId));
-    unselectCartItem(cartItemId);
+      resetApiError();
+    } catch (error) {
+      setApiError(new Error('네트워크 오류로 인해 삭제에 실패했어요.'));
+    }
   };
 
   const fetchCartItemList = async () => {
-    const result = await requestCartItemList();
-    setCartItemList(result);
+    try {
+      const result = await requestCartItemList();
+      setCartItemList(result);
+    } catch (error) {
+      setApiError(
+        new Error('네트워크 오류로 인해 장바구니를 불러올 수 없어요.'),
+      );
+    }
   };
 
   return {
