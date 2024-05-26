2eec6509a1edfa38e1c3d905987b5b166d5e6d4e
hain-tain <hain7ce@gmail.com>
2024-05-27 01:23:59 +0900

    feat: 모든 상품 추가 버튼 추가

--- src/components/EmptyCart.tsx
+++ src/components/EmptyCart.tsx
@@ -1,15 +1,41 @@
-import { FlexCenter, WhiteSpace } from "@/style/common.style";
+import { FlexCenter, FlexColumn, WhiteSpace } from "@/style/common.style";
+import { getCartList, postCartItem } from "@/api/cartItem";
 
+import { ALL_PRODUCTS_IDS } from "@/constants/system";
+import BorderButton from "./common/Button/BorderButton";
 import { CART_MESSAGE } from "@/constants/message";
 import FullWidthButton from "@/components/common/Button/FullWidthButton";
+import { cartListState } from "@/store/atoms/atoms";
 import styled from "@emotion/styled";
+import { useSetRecoilState } from "recoil";
 
 const EmptyCart = () => {
+  const setCartList = useSetRecoilState(cartListState);
+  const refreshCartList = async () => {
+    const newList = await getCartList();
+    setCartList(newList);
+  };
+
+  const addAllProducts = () => {
+    const postData = async (productId: number) => {
+      await postCartItem(productId);
+    };
+
+    ALL_PRODUCTS_IDS.forEach((id) => {
+      postData(id);
+    });
+
+    refreshCartList();
+  };
+
   return (
     <>
       <StyledEmptyWrapper>
         <StyledTitle>장바구니</StyledTitle>
-        <StyledCenterBox>{CART_MESSAGE.emptyCart}</StyledCenterBox>
+        <StyledCenterBox>
+          {CART_MESSAGE.emptyCart}
+          <BorderButton onClick={addAllProducts}>모든 상품 담기</BorderButton>
+        </StyledCenterBox>
       </StyledEmptyWrapper>
       <StyledFixedBottom>
         <FullWidthButton onClick={() => {}} disabled>
@@ -39,6 +65,8 @@ const StyledCenterBox = styled.div`
   position: fixed;
   translate: calc(-50%, -50%);
   ${FlexCenter}
+  ${FlexColumn}
+  gap:16px;
 `;
 
 const StyledFixedBottom = styled.div`
--- src/constants/system.ts
+++ src/constants/system.ts
@@ -3,3 +3,5 @@ export const ADDITIONAL_SHIPPING_FEE = 3000;
 export const FREE_SHIPPING_CONDITION = 100000;
 
 export const MAX_APPLICABLE_COUPON = 2;
+
+export const ALL_PRODUCTS_IDS = [2, 3, 10, 11, 12, 21, 34];
