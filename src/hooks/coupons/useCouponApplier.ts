6a2fe8a225f907ad14271ee2db654800efaf0a4e
ImxYJL <allensain14@gmail.com>
2024-05-26 14:29:34 +0900

    feat: 선택한 쿠폰을 적용시키는 커스텀 훅 구현

---
+++ src/hooks/coupons/useCouponApplier.ts
@@ -0,0 +1,67 @@
+import { COUPON, COUPON_DISCOUNT_TYPE } from '../../constants/constants';
+import { Coupon } from '../../api/get/getCoupons';
+import { ORDER } from '../../constants/constants';
+import { selectedCartItems } from '../../recoil/atoms';
+import { priceInfoStore } from '../../recoil/selectors';
+import { useRecoilValue, useRecoilState } from 'recoil';
+import useCouponValidator from './useCouponValidator';
+
+const useCouponApplier = () => {
+  const [selectedItems, setSelectedItems] = useRecoilState(selectedCartItems);
+  const priceInfo = useRecoilValue(priceInfoStore);
+  const { isValidCoupon } = useCouponValidator();
+
+  const isCouponUsable = (coupon: Coupon, orderPrice: number, now: Date = new Date()) => {
+    if (!isValidCoupon(coupon)) return false;
+
+    if (coupon.minimumAmount && orderPrice < coupon.minimumAmount) return false;
+
+    if (coupon.discountType === COUPON_DISCOUNT_TYPE.BUY_X_GET_Y) {
+      return selectedItems.some(selectedItem => {
+        return selectedItem.quantity >= COUPON.BOGO_BUY_QUANTITY;
+      });
+    }
+
+    if (coupon.discountType === COUPON_DISCOUNT_TYPE.FREE_SHIPPING) {
+      return !(priceInfo.order >= ORDER.SHIPPING_FREE_PRICE);
+    }
+
+    if (coupon.availableTime) {
+      const [startHour, startMinute, startSecond] = coupon.availableTime.start
+        .split(':')
+        .map(Number);
+
+      const [endHour, endMinute, endSecond] = coupon.availableTime.end.split(':').map(Number);
+
+      const startTime = new Date(
+        now.getFullYear(),
+        now.getMonth(),
+        now.getDate(),
+        startHour,
+        startMinute,
+        startSecond,
+      );
+
+      const endTime = new Date(
+        now.getFullYear(),
+        now.getMonth(),
+        now.getDate(),
+        endHour,
+        endMinute,
+        endSecond,
+      );
+
+      if (now < startTime || now > endTime) {
+        return false;
+      }
+    }
+
+    return true;
+  };
+
+  return {
+    isCouponUsable,
+  };
+};
+
+export default useCouponApplier;
