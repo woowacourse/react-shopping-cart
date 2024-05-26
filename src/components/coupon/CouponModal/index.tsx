5387eb92e79aff48381157885fcda1d30cfdf5f0
Yoonkyoung Kim <dev.yoonkyoung@gmail.com>
2024-05-26 22:43:33 +0900

    feat: CouponModal 컴포넌트 구현

---
+++ src/components/coupon/CouponModal/index.tsx
@@ -0,0 +1,60 @@
+import Button from "@/components/_common/Button";
+import { Modal } from "woowacourse-6th-react-modal-component";
+
+import useDiscountCalculator from "@/hooks/coupons/useDiscountCalculator";
+import useCouponSelections from "@/hooks/coupons/useCouponSelections";
+
+import NotificationText from "@/components/_common/NotificationText";
+import CouponList from "../CouponList";
+import { COUPON_SELECTION_RULES } from "@/constants/coupon";
+
+import * as S from "./styled";
+
+interface CouponModalProps {
+  isModalOpen: boolean;
+  closeModal: () => void;
+}
+
+const CouponModal = ({ isModalOpen, closeModal }: CouponModalProps) => {
+  const { clearSelectedCoupons } = useCouponSelections();
+  const { calculateTotalDiscount } = useDiscountCalculator();
+  const totalDiscountPrice = calculateTotalDiscount();
+
+  if (!isModalOpen) return null;
+
+  const handleCloseModal = () => {
+    clearSelectedCoupons();
+    closeModal();
+  };
+
+  return (
+    <Modal onCloseModal={handleCloseModal} $size="small">
+      <S.ModalContent>
+        <Modal.Header
+          title="쿠폰을 선택해 주세요"
+          isCloseIcon={true}
+          onCloseModal={handleCloseModal}
+        />
+        <Modal.Content>
+          <NotificationText
+            text={`쿠폰은 최대 ${COUPON_SELECTION_RULES.maxSelectCount}개까지 사용할 수 있습니다.`}
+          />
+          <CouponList />
+        </Modal.Content>
+        <Modal.Footer>
+          <Button
+            type="button"
+            onClick={closeModal}
+            $theme="black"
+            $width="100%"
+            $height="44px"
+          >
+            {totalDiscountPrice}원 할인 쿠폰 사용하기
+          </Button>
+        </Modal.Footer>
+      </S.ModalContent>
+    </Modal>
+  );
+};
+
+export default CouponModal;
