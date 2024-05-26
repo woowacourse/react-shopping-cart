47278c527116a1f19c0d16cd17a4e39eca367857
simhorim <hori5020@gmail.com>
2024-05-26 21:59:05 +0900

    refactor(validators): coupon mock data 변경에 따른 수정

--- src/test/validators.test.ts
+++ src/test/validators.test.ts
@@ -14,14 +14,22 @@ describe('couponValidator', () => {
 
   describe('isCouponValid', () => {
     it('만료일이 지난 쿠폰은 유효하지 않다', () => {
-      const EXPIRED_COUPON = mockCoupons[0];
+      const EXPIRED_COUPON = {
+        id: 0,
+        code: 'FIXED5000',
+        description: '5,000원 할인 쿠폰',
+        discount: 5000,
+        discountType: 'fixed' as 'fixed',
+        minimumAmount: 100000,
+        expirationDate: '2024-04-30',
+      };
       const { validateCoupon } = couponValidator();
 
       expect(validateCoupon(EXPIRED_COUPON)).toBe(false);
     });
 
     it('만료일이 지나지 않은 쿠폰은 유효하다', () => {
-      const VALID_COUPON = mockCoupons[1];
+      const VALID_COUPON = mockCoupons[0];
       const { validateCoupon } = couponValidator();
 
       expect(validateCoupon(VALID_COUPON)).toBe(true);
@@ -31,7 +39,7 @@ describe('couponValidator', () => {
   describe('couponApplicabilityValidator', () => {
     describe('FIXED', () => {
       it('주문 금액이 최소 주문 금액 미만이면 쿠폰 적용 불가하다', () => {
-        const TEST_COUPON = mockCoupons[1];
+        const TEST_COUPON = mockCoupons[0];
         const INAPPLICABLE_TOTAL_AMOUNT = 90000;
 
         const { validateCouponApplicability } = couponApplicabilityValidator();
@@ -42,7 +50,7 @@ describe('couponValidator', () => {
       });
 
       it('주문 금액이 최소 주문 금액 이상이면 쿠폰 적용 가능하다.', () => {
-        const TEST_COUPON = mockCoupons[1];
+        const TEST_COUPON = mockCoupons[0];
         const APPLICABLE_TOTAL_AMOUNT = 100000;
 
         const { validateCouponApplicability } = couponApplicabilityValidator();
@@ -56,7 +64,7 @@ describe('couponValidator', () => {
     describe('MIRACLE_SALE', () => {
       it('사용 가능 시간 외에는 쿠폰 적용이 불가하다', () => {
         const INAPPLICABLE_TIME = new Date('2023-05-01T08:00:00');
-        const TEST_COUPON = mockCoupons[4];
+        const TEST_COUPON = mockCoupons[3];
         const TEST_TOTAL_AMOUNT = 100000;
 
         const { validateCouponApplicability } = couponApplicabilityValidator();
@@ -72,7 +80,7 @@ describe('couponValidator', () => {
 
       it('사용 가능 시간 내에는 쿠폰 적용이 가능하다.', () => {
         const APPLICABLE_TIME = new Date('2023-05-01T06:00:00');
-        const TEST_COUPON = mockCoupons[4];
+        const TEST_COUPON = mockCoupons[3];
         const TEST_TOTAL_AMOUNT = 100000;
 
         const { validateCouponApplicability } = couponApplicabilityValidator();
@@ -89,7 +97,7 @@ describe('couponValidator', () => {
 
     describe('FREE_SHIPPING', () => {
       it('배송비 쿠폰의 경우 100,000원이 넘으면 쿠폰 적용이 불가하다.', () => {
-        const TEST_COUPON = mockCoupons[3];
+        const TEST_COUPON = mockCoupons[2];
         const INAPPLICABLE_TOTAL_AMOUNT = 100000;
 
         const { validateCouponApplicability } = couponApplicabilityValidator();
@@ -100,7 +108,7 @@ describe('couponValidator', () => {
       });
 
       it('배송비 쿠폰의 경우 최소 주문 금액 이상 100,000원 미만이면 쿠폰 적용이 가능하다.', () => {
-        const TEST_COUPON = mockCoupons[3];
+        const TEST_COUPON = mockCoupons[2];
         const APPLICABLE_TOTAL_AMOUNT = 90000;
 
         const { validateCouponApplicability } = couponApplicabilityValidator();
