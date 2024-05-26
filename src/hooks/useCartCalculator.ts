3a07a8c4eb9078b172929b8ae7bac07e4387a6ab
chysis <chysiss@naver.com>
2024-05-26 16:17:04 +0900

    chore: 변수명을 직관적으로 변경

--- src/hooks/useCartCalculator.ts
+++ src/hooks/useCartCalculator.ts
@@ -4,13 +4,13 @@ import { selectedCouponsState } from "../store/atom/atoms";
 import { checkedCartItemsSelector, shippingFeeSelector } from "../store/selector/selectors";
 
 export const useCartCalculator = () => {
-  const cartItems = useRecoilValue(checkedCartItemsSelector);
+  const checkedCartItems = useRecoilValue(checkedCartItemsSelector);
   const selectedCoupon = useRecoilValue(selectedCouponsState);
   const shippingFee = useRecoilValue(shippingFeeSelector);
   const { calculateTotalDiscountAmount } = useDiscountCalculator();
 
   const calculateCartTotal = () => {
-    return cartItems.reduce((total, item) => total + item.product.price * item.quantity, 0);
+    return checkedCartItems.reduce((total, item) => total + item.product.price * item.quantity, 0);
   };
 
   const calculateTotalWithCoupon = () => {
