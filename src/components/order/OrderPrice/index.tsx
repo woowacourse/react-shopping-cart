63bb94f4380ce03db29561ae4d6b746b005d55af
Yoonkyoung Kim <dev.yoonkyoung@gmail.com>
2024-05-26 22:24:49 +0900

    feat: OrderPrice 컴포넌트 구현

---
+++ src/components/order/OrderPrice/index.tsx
@@ -0,0 +1,34 @@
+import { useRecoilValue } from "recoil";
+import { cartPriceState } from "@/stores/cartPrice";
+
+import PriceInfo from "@/components/_common/PriceInfo";
+import useDiscountCalculator from "@/hooks/coupons/useDiscountCalculator";
+
+const OrderPrice = () => {
+  const { orderPrice, shippingFee, totalPrice } =
+    useRecoilValue(cartPriceState);
+  const { calculateTotalDiscount } = useDiscountCalculator();
+  const totalDiscountPrice = calculateTotalDiscount();
+
+  return (
+    <>
+      <PriceInfo>
+        <PriceInfo.Row title="주문 금액" price={orderPrice} />
+        <PriceInfo.Row
+          title="쿠폰 할인 금액"
+          price={totalDiscountPrice}
+          isDiscount={true}
+        />
+        <PriceInfo.Row title="배송비" price={shippingFee} />
+      </PriceInfo>
+      <PriceInfo>
+        <PriceInfo.Row
+          title="총 결제 금액"
+          price={totalPrice - totalDiscountPrice}
+        />
+      </PriceInfo>
+    </>
+  );
+};
+
+export default OrderPrice;
