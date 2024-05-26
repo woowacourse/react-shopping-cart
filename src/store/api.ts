84b27f77a00ea0d584b500e423c0bd5f0e7a3be1
chysis <chysiss@naver.com>
2024-05-26 15:59:10 +0900

    refactor: 재사용 가능한 공통 request 로직 분리

--- src/store/api.ts
+++ src/store/api.ts
@@ -2,28 +2,51 @@ import { API_TOKEN } from "./utils";
 
 const BASE_URL = process.env.VITE_API_BASE_URL;
 
-export const fetchProducts = async () => {
-  try {
-    const token = API_TOKEN;
-    const url = BASE_URL + "/cart-items";
-    const response = await fetch(url, {
-      method: "GET",
-      headers: { Authorization: token },
-    });
+interface RequestProps {
+  endpoint: string;
+  method: "GET" | "POST" | "PATCH" | "DELETE";
+  headers?: HeadersInit;
+  body?: object;
+}
 
-    const data = await response.json();
-    return data;
+export const request = async ({ endpoint, method, headers = {}, body }: RequestProps) => {
+  try {
+    const url = `${BASE_URL}${endpoint}`;
+    const options: RequestInit = {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: API_TOKEN,
+        ...headers,
+      },
+    };
+
+    if (body) {
+      options.body = JSON.stringify(body);
+    }
+
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      throw new Error(`Failed to request, status code: ${response.status}`);
+    }
+
+    if (method === "GET") {
+      return await response.json();
+    }
+
+    return response;
   } catch (error) {
-    console.error("Failed to fetch products:", error);
-    return error;
+    console.error("Failed to request", error);
+    throw error;
   }
 };
 
+export const fetchProducts = async () => {
+  return await request({ endpoint: "/cart-items", method: "GET" });
+};
+
 export const deleteProduct = async (cartId: number) => {
-  await fetch(BASE_URL + `/cart-items/${cartId}`, {
-    method: "DELETE",
-    headers: { "Content-Type": "application/json", Authorization: API_TOKEN },
-  });
+  return await request({ endpoint: `/cart-items/${cartId}`, method: "DELETE" });
 };
 
 interface ChangeProductAmountProps {
@@ -32,40 +55,19 @@ interface ChangeProductAmountProps {
 }
 
 export const changeProductAmount = async ({ quantity, id }: ChangeProductAmountProps) => {
-  await fetch(BASE_URL + `/cart-items/${id}`, {
-    method: "PATCH",
-    headers: { "Content-Type": "application/json", Authorization: API_TOKEN },
-    body: JSON.stringify({
-      quantity,
-    }),
-  });
+  return request({ endpoint: `/cart-items/${id}`, method: "PATCH", body: { quantity } });
 };
 
 export const fetchCartItemsCounts = async () => {
-  const response = await fetch(BASE_URL + "/cart-items/counts", {
-    method: "GET",
-    headers: { "Content-Type": "application/json", Authorization: API_TOKEN },
-  });
-  const data = await response.json();
+  const data: { quantity: number } = await request({ endpoint: "/cart-items/counts", method: "GET" });
   return data.quantity;
 };
 
 export const fetchCoupons = async () => {
-  const response = await fetch(BASE_URL + "/coupons", {
-    method: "GET",
-    headers: { "Content-Type": "application/json", Authorization: API_TOKEN },
-  });
-  const data = await response.json();
-  return data;
+  return await request({ endpoint: "/coupons", method: "GET" });
 };
 
 export const generateOrder = async (cartItemIds: number[]) => {
-  const response = await fetch(BASE_URL + "/orders", {
-    method: "POST",
-    headers: { "Content-Type": "application/json", Authorization: API_TOKEN },
-    body: JSON.stringify({
-      cartItemIds,
-    }),
-  });
+  const response: Response = await request({ endpoint: "/orders", method: "POST", body: { cartItemIds } });
   return response.status;
 };
