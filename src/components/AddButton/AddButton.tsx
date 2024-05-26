939287e7f42b5eb9c6f56518b3591f6f859a23e3
chysis <chysiss@naver.com>
2024-05-26 16:12:32 +0900

    refactor: 공통 request 로직 적용

--- src/components/AddButton/AddButton.tsx
+++ src/components/AddButton/AddButton.tsx
@@ -1,8 +1,8 @@
 /** @jsxImportSource @emotion/react */
 
 import { useNavigate } from "react-router-dom";
-import { API_TOKEN } from "../../store/utils";
 import { AddButtonStyle } from "./AddButton.style";
+import { request } from "../../store/api";
 
 const AddButton = () => {
   const navigate = useNavigate();
@@ -10,17 +10,9 @@ const AddButton = () => {
   return (
     <div
       css={AddButtonStyle}
-      onClick={() => {
-        const fn = async () => {
-          await fetch(process.env.VITE_API_BASE_URL + "/cart-items", {
-            method: "POST",
-            headers: { "Content-Type": "application/json", Authorization: API_TOKEN },
-            body: JSON.stringify({
-              productId: 11, // 2, 3, 10, 11, 12, 21, 34로 설정해서 상품을 장바구니에 추가할 수 있습니다.
-            }),
-          });
-        };
-        fn();
+      onClick={async () => {
+        // productId로 2, 3, 10, 11, 12, 21, 34를 설정해서 상품을 장바구니에 추가할 수 있습니다.
+        await request({ endpoint: "/cart-items", method: "POST", body: { productId: 3 } });
         navigate(0);
       }}
     >
