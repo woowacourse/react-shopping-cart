72ab54acd0e91ceff8105d3d8f6a9a0447d97557
useon <yoosk4986@naver.com>
2024-05-27 15:46:34 +0900

    refactor: 확인 버튼 로직 개선

--- src/components/confirmButton/ConfirmButton.tsx
+++ src/components/confirmButton/ConfirmButton.tsx
@@ -1,26 +1,18 @@
-import { useNavigate } from 'react-router-dom';
 import { StyledConfirmButton } from './ConfirmButton.styled';
 
 export interface ConfirmButtonProps {
   text: string;
   disabled: boolean;
+  onClick: () => void;
 }
 
 export const ConfirmButton: React.FC<ConfirmButtonProps> = ({
   text,
   disabled,
+  onClick,
 }) => {
-  const navigate = useNavigate();
-
-  const navigateToOrderConfirmationPage = () => {
-    navigate('/order-confirmation');
-  };
-
   return (
-    <StyledConfirmButton
-      onClick={navigateToOrderConfirmationPage}
-      disabled={disabled}
-    >
+    <StyledConfirmButton onClick={onClick} disabled={disabled}>
       {text}
     </StyledConfirmButton>
   );
