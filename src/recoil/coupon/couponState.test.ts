6658bbd3198d3fea03c128efe6c85c89541b6c17
skiende74 <skiende74@gmail.com>
2024-05-27 15:16:10 +0900

    test(couponState): 쿠폰 할인 상태 테스트 추가

--- src/recoil/coupon/couponState.test.ts
+++ src/recoil/coupon/couponState.test.ts
@@ -1,12 +1,16 @@
 import { snapshot_UNSTABLE } from "recoil";
-import { mockCouponList } from "../mocks";
+import { mockCartItems, mockCouponList } from "../mocks";
 import {
+  couponDiscountPriceSelectorFamily,
   couponIdSetSelector,
   couponSelectorFamily,
   isSelectedCouponAtomFamily,
   selectedCouponSetSelector,
 } from "./couponState";
+import { Coupon, FixedDiscount } from "src/types/Coupon";
+import { checkedIdSetSelector } from "../cart/checkedState";
 
+jest.mock("../../api/cartItem", () => ({ fetchCartItems: () => mockCartItems }));
 jest.mock("../../api/coupon", () => ({ fetchCouponList: () => mockCouponList }));
 describe("couponState 테스트", () => {
   describe("couponSelectorFamily", () => {
@@ -51,6 +55,45 @@ describe("couponState 테스트", () => {
         const COUPON_SET_SELECTED = new Set([mockCouponList[SELECTED_COUPON_ID - 1]]);
 
         expect(snapshot.getLoadable(selectedCouponSetSelector).contents).toEqual(COUPON_SET_SELECTED);
+      });
+    });
+  });
+
+  describe("couponDiscountPriceSelectorFamily", () => {
+    describe("쿠폰이 인자로 들어왔을 때, 그에맞게 할인금액을 반환한다.", () => {
+      it("고정금액 할인쿠폰이 인자로 들어왔을 때, 해당 고정금액 5000원을 반환한다", () => {
+        const snapshot = snapshot_UNSTABLE();
+        const FIXED_COUPON_INDEX = 0;
+        const coupon = mockCouponList[FIXED_COUPON_INDEX] as Coupon & FixedDiscount;
+
+        expect(snapshot.getLoadable(couponDiscountPriceSelectorFamily(coupon.id)).getValue()).toBe(coupon.discount);
+      });
+      it("BuyXGetY 할인쿠폰이 인자로 들어왔을 때, 해당 하는 할인 금액을 반환한다", () => {
+        const snapshot = snapshot_UNSTABLE();
+        const BOGO_COUPON_INDEX = 1;
+        const EXPECTED_DISCOUNT = 25000;
+        const coupon = mockCouponList[BOGO_COUPON_INDEX] as Coupon & FixedDiscount;
+
+        expect(snapshot.getLoadable(couponDiscountPriceSelectorFamily(coupon.id)).getValue()).toBe(EXPECTED_DISCOUNT);
+      });
+      it("무료배송 할인쿠폰이 인자로 들어왔을 때, 해당 고정금액 5000원을 반환한다", () => {
+        const snapshot = snapshot_UNSTABLE();
+        const BOGO_COUPON_INDEX = 2;
+        const EXPECTED_DISCOUNT = 3000;
+        const coupon = mockCouponList[BOGO_COUPON_INDEX] as Coupon & FixedDiscount;
+
+        expect(snapshot.getLoadable(couponDiscountPriceSelectorFamily(coupon.id)).getValue()).toBe(EXPECTED_DISCOUNT);
+      });
+      it("MIRACLESALE 할인쿠폰이 인자로 들어왔을 때, 해당 고정금액 5000원을 반환한다", () => {
+        const snapshot = snapshot_UNSTABLE(({ set }) => {
+          set(checkedIdSetSelector, new Set([1, 2]));
+        });
+        const MIRACLESALE_COUPON_INDEX = 3;
+        const EXPECTED_ORDER_PRICE = 145000;
+        const EXPECTED_DISCOUNT = EXPECTED_ORDER_PRICE * 0.3;
+        const coupon = mockCouponList[MIRACLESALE_COUPON_INDEX] as Coupon & FixedDiscount;
+
+        expect(snapshot.getLoadable(couponDiscountPriceSelectorFamily(coupon.id)).getValue()).toBe(EXPECTED_DISCOUNT);
       });
     });
   });
