8f508ff604a51f448a839af9d97be748b1f30e7f
greetings1012 <ahnqwerty@gmail.com>
2024-05-27 23:08:46 +0900

    fix: 타입을 명시적으로 선언

--- src/constants/DELIVERY_INFOS.ts
+++ src/constants/DELIVERY_INFOS.ts
@@ -3,6 +3,6 @@ import type { DeliveryInfo } from '../types/DeliveryInfo';
 export const DEFAULT_DELIVERY_FEE = 3000;
 export const DELIVERY_FEE_DISCOUNT_THRESHOLD = 100000;
 
-export const DELIVERY_INFOS = {
-  ISLANDS_AND_MOUNTAINS: { title: '제주도 및 도서 산간 지역', surcharge: 3000 } as DeliveryInfo,
+export const DELIVERY_INFOS: Record<string, DeliveryInfo> = {
+  ISLANDS_AND_MOUNTAINS: { title: '제주도 및 도서 산간 지역', surcharge: 3000 },
 };
