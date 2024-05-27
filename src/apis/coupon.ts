55266607f10d8d8ea9d8e6759066463a74b97e04
rbgksqkr <rbgks1937@naver.com>
2024-05-27 20:36:53 +0900

    chore: fetch API 반환 타입 지정

--- src/apis/coupon.ts
+++ src/apis/coupon.ts
@@ -1,8 +1,9 @@
 import fetcher from './fetcher';
 
+import { Coupon } from '@/types/coupon';
 import { REQUEST_URL } from '@constants/url';
 
-export const fetchCouponList = async () => {
+export const fetchCouponList = async (): Promise<Coupon[]> => {
   const res = await fetcher.get({ requestUrl: REQUEST_URL.coupons });
   const result = await res.json();
 
--- src/apis/order.ts
+++ src/apis/order.ts
@@ -2,7 +2,12 @@ import fetcher from './fetcher';
 
 import { REQUEST_URL } from '@constants/url';
 
-export const orderProductList = async (cartItemIds: number[]) => {
+interface FetchResponse {
+  type: 'PATCH' | 'DELETE' | 'POST';
+  status: number;
+}
+
+export const orderProductList = async (cartItemIds: number[]): Promise<FetchResponse> => {
   const res = await fetcher.post({ requestUrl: REQUEST_URL.orders, body: { cartItemIds } });
 
   return { type: 'POST', status: res.status };
