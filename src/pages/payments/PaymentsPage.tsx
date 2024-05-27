cd4c3f35b27f8a43d405f2890002d5fea1abb2ba
jayming66 <mjw8941@gmail.com>
2024-05-27 10:36:03 +0900

    feat(PaymentsPage): 결제 페이지 추가

--- src/pages/payments/PaymentsPage.tsx
+++ src/pages/payments/PaymentsPage.tsx
@@ -1,29 +1,21 @@
-import { useNavigate, useLocation } from 'react-router-dom';
 import ROUTES from '../../constants/routes';
 import Button from '../../components/common/Button';
 import Header from '../../components/Header/Header';
-import BackIcon from '../../asset/back.png';
 import formatKoreanCurrency from '../../utils/formatKoreanCurrency';
 import styles from './PaymentsPage.module.css';
+import { useRecoilValue } from 'recoil';
+import { totalAmountState, totalProductQuantityState } from '../../store/selectors';
+import useNavigatePage from '../../hooks/useNavigatePage';
 
 export default function PaymentsPage() {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const {
-    state: { totalCount, totalQuantity, totalAmount },
-  } = location;
+  const { totalCount, totalQuantity } = useRecoilValue(totalProductQuantityState);
+  const totalAmount = useRecoilValue(totalAmountState);
 
-  const handleBackButtonClick = () => {
-    navigate(ROUTES.CART);
-  };
+  const navigateCartPage = useNavigatePage(ROUTES.CART);
 
   return (
     <>
-      <Header>
-        <Button variant="image" onClick={handleBackButtonClick}>
-          <img src={BackIcon} width={32} height={32} alt="back-icon" />
-        </Button>
-      </Header>
+      <Header />
       <div className={styles.container}>
         <h2 className={styles.mainText}>주문 확인</h2>
         <div className={styles.orderInfoContainer}>
@@ -34,13 +26,12 @@ export default function PaymentsPage() {
         </div>
         <div className={styles.totalAmount}>
           <h2 className={styles.totalAmountText}>총 결제 금액</h2>
-          <h2 className={styles.totalAmountNumber}>{formatKoreanCurrency(totalAmount)}원</h2>
+          <h2 className={styles.totalAmountNumber}>{formatKoreanCurrency(totalAmount)}</h2>
         </div>
       </div>
 
-      {/* 다음 미션에서 기능 추가 예정 */}
-      <Button variant="footer" disabled={true}>
-        결제하기
+      <Button variant="footer" onClick={navigateCartPage}>
+        장바구니로 돌아가기
       </Button>
     </>
   );
