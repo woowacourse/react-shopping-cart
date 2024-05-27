70bb10fb2140fcca0fa05f1fc089c60a700a529f
ImxYJL <allensain14@gmail.com>
2024-05-27 18:14:33 +0900

    refactor: 중복되는 도서산간 상태 제거 - 전역 상태만 사용

--- src/components/ShippingInfo/ShippingInfo.tsx
+++ src/components/ShippingInfo/ShippingInfo.tsx
@@ -1,22 +1,18 @@
 import { ORDER } from '../../constants/constants';
 import Checkbox from '../Checkbox/Checkbox';
-
 import { shippingFeeState, orderDiscountState, specialAreaState } from '../../recoil/atoms';
-
 import { isOverShippingFeeFreeStore } from '../../recoil/selectors';
-import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
-import { useState } from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import * as S from './styled';
 
 const ShippingInfo = () => {
-  const [isSpecialAreaChecked, setIsSpecialAreaChecked] = useState(false);
   const [orderDiscountAmount, setOrderDiscountAmount] = useRecoilState(orderDiscountState);
-  const setIsSpecialArea = useSetRecoilState(specialAreaState);
+  const [isSpecialArea, setIsSpecialArea] = useRecoilState(specialAreaState);
   const [shippingFeeInfo, setShippingFeeInfo] = useRecoilState(shippingFeeState);
   const isOverShippingFeeFree = useRecoilValue(isOverShippingFeeFreeStore);
 
   const onClickCheckbox = () => {
-    const newIsSpecialAreaChecked = !isSpecialAreaChecked;
+    const newIsSpecialArea = !isSpecialArea;
 
     let newShipping;
     if (isOverShippingFeeFree) {
@@ -25,7 +21,7 @@ const ShippingInfo = () => {
     } else {
       // NOTE: 무료배송 금액을 넘지 않은 경우의 배송비 설정 (도서산간 여부 반영)
       // (배송비 무료 쿠폰 유무에 상관없이 배송비는 정해짐)
-      newShipping = newIsSpecialAreaChecked
+      newShipping = newIsSpecialArea
         ? ORDER.BASIC_SHIPPING_FEE + ORDER.SPECIAL_AREA_ADDITIONAL_SHIPPING_FEE
         : ORDER.BASIC_SHIPPING_FEE;
     }
@@ -37,26 +33,21 @@ const ShippingInfo = () => {
 
     // NOTE: 배송비 무료 쿠폰을 적용한 상태에서 도서산간을 체크 혹은 해제한 경우의 혜택 금액 업데이트
     if (shippingFeeInfo.isFree) {
-      const newDiscount = newIsSpecialAreaChecked
+      const newDiscount = newIsSpecialArea
         ? orderDiscountAmount + ORDER.SPECIAL_AREA_ADDITIONAL_SHIPPING_FEE
         : orderDiscountAmount - ORDER.SPECIAL_AREA_ADDITIONAL_SHIPPING_FEE;
 
       setOrderDiscountAmount(newDiscount);
     }
 
-    setIsSpecialAreaChecked(newIsSpecialAreaChecked);
-    setIsSpecialArea(newIsSpecialAreaChecked);
+    setIsSpecialArea(newIsSpecialArea);
   };
 
   return (
     <S.Container>
       <S.ShippingTitle>배송 정보</S.ShippingTitle>
       <S.ShippingCheckContainer>
-        <Checkbox
-          id="shipping-info-checkbox"
-          isChecked={isSpecialAreaChecked}
-          onClick={onClickCheckbox}
-        />
+        <Checkbox id="shipping-info-checkbox" isChecked={isSpecialArea} onClick={onClickCheckbox} />
         <S.ShippingDescription>제주도 및 도서 산간 지역</S.ShippingDescription>
       </S.ShippingCheckContainer>
     </S.Container>
