72543d79b1d33502550f83e0b1f8b7821e67bf99
hanyoung <phanyoung@naver.com>
2024-05-28 23:47:41 +0900

    chore(discountCalculators): 각 할인 정책 로직을 간략히 설명하는 주석 추가

--- src/hooks/useCouponListDiscountCalculator/discountCalculators/index.ts
+++ src/hooks/useCouponListDiscountCalculator/discountCalculators/index.ts
@@ -9,10 +9,12 @@ import { getMaxNumberFromList } from "../../../utils/math/getMaxFromNumberList";
 import { roundDownToTens } from "../../../utils/math/roundDownToTens";
 
 export const calculateFixedDiscountAmount = (coupon: FixedDiscountCouponResponse): number => {
+  // 고정 할인 쿠폰 - 쿠폰의 할인 금액을 반환
   return coupon.discount;
 };
 
 export const calculatePercentageDiscountAmount = (
+  // 퍼센트 할인 쿠폰 - 쿠폰의 할인 비율을 적용한 할인 금액을 반환
   coupon: PercentageDiscountCouponResponse,
   totalAmount: number
 ): number => {
@@ -23,6 +25,7 @@ export const calculatePercentageDiscountAmount = (
 };
 
 export const calculateBuyXGetYDiscountAmount = (
+  // X + Y 증정 쿠폰 - 특정 상품을 X + Y개를 구매할 때 Y개만큼의 할인 금액을 반환 (여러 종류의 할인 가능 상품이 있을 경우 최대 할인 금액을 반환)
   coupon: BuyXGetYCouponResponse,
   cartItems: CartItem[]
 ): number => {
@@ -36,5 +39,6 @@ export const calculateBuyXGetYDiscountAmount = (
 };
 
 export const calculateFreeShippingDiscountAmount = (shippingCost: number): number => {
+  // 무료 배송 쿠폰 - 배송비를 반환
   return shippingCost;
 };
