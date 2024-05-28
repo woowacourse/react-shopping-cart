f14651d9409fa2c8c402eafc0e878206c8d0008f
pakxe <pigkill40@naver.com>
2024-05-29 01:32:34 +0900

    feat: alt -> area-label로 변경해 요소를 설명하도록 수정

--- src/components/common/Checkbox/Checkbox.tsx
+++ src/components/common/Checkbox/Checkbox.tsx
@@ -4,15 +4,15 @@ import * as S from './Checkbox.style';
 export interface CheckboxProps {
   checked: boolean;
   handleClick: () => void;
-  alt?: string;
+  areaLabel?: string;
   description?: ReactNode; // Text 컴포넌트를 넣을 수 있도록
   disabled?: boolean;
 }
 
-const Checkbox = ({ checked, handleClick, alt = 'Checkbox', description, disabled }: CheckboxProps) => {
+const Checkbox = ({ checked, handleClick, areaLabel = '선택', description, disabled }: CheckboxProps) => {
   return (
     <S.CheckboxWrapper>
-      <S.Checkbox checked={checked} onChange={handleClick} alt={alt} type="checkbox" disabled={disabled} />
+      <S.Checkbox checked={checked} onChange={handleClick} area-label={areaLabel} type="checkbox" disabled={disabled} />
       {description}
     </S.CheckboxWrapper>
   );
