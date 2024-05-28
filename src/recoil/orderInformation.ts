956003ba697b29579382cce8dc74d253042f76f0
ooherin <555ohr@naver.com>
2024-05-29 08:14:36 +0900

    refactor(selectedItems): 선택된 id로 items을 반환하는 selector 만들어서 리팩토링

--- src/recoil/orderInformation.ts
+++ src/recoil/orderInformation.ts
@@ -1,21 +1,14 @@
 import { atom, selector } from "recoil";
-
-import { cartItemsState } from "./cartItems";
-
-import { cartItemQuantityState } from "./cartItemQuantity";
-import { selectedCartItemsIdState } from "./selectedCardItems";
+import { selectedCartItemList } from "./selectedCardItems";
 
 export const totalItemsPriceSelector = selector({
   key: "totalItemsPriceSelector",
   get: ({ get }) => {
-    const cartItemList = get(cartItemsState);
-    const selectedItemsId = get(selectedCartItemsIdState);
-    if (!selectedItemsId.length) return 0;
+    const selectedItems = get(selectedCartItemList);
+    if (!selectedItems.length) return 0;
 
-    const totalPrice = selectedItemsId.reduce((acc, productId) => {
-      const productInfo = cartItemList.find((item) => item.id == productId);
-      const quantity = get(cartItemQuantityState(productId));
-      acc += productInfo!.product.price * quantity;
+    const totalPrice = selectedItems.reduce((acc, item) => {
+      acc += item.product.price * item.quantity;
       return acc;
     }, 0);
 
@@ -27,12 +20,11 @@ export const totalItemOrderCountSelector = selector({
   key: "totalItemOrderCountSelector",
 
   get: ({ get }) => {
-    const selectedItemsId = get(selectedCartItemsIdState);
-    if (!selectedItemsId.length) return 0;
+    const selectedItems = get(selectedCartItemList);
+    if (!selectedItems.length) return 0;
 
-    const totalItemOrderCount = selectedItemsId?.reduce((acc, id) => {
-      const itemQuantity = get(cartItemQuantityState(id));
-      acc += itemQuantity;
+    const totalItemOrderCount = selectedItems.reduce((acc, item) => {
+      acc += item.product.price;
       return acc;
     }, 0);
 
