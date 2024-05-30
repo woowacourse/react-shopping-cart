df0b982ca356a2c463a79ec1dab328665a05cd2e
jinsil <jinsil378900@gmail.com>
2024-05-30 19:34:10 +0900

    refactor: useCouponValidation 코드 리팩토링

--- src/hooks/useCouponValidation/useCouponValidation.ts
+++ src/hooks/useCouponValidation/useCouponValidation.ts
@@ -15,16 +15,15 @@ const useCouponValidation = () => {
       return false;
     }
 
-    if (coupon.minimumAmount) {
-      if (orderPrice < coupon.minimumAmount) {
-        return false;
-      }
+    if (coupon.minimumAmount && orderPrice < coupon.minimumAmount) {
+      return false;
     }
 
     if (coupon.buyQuantity && coupon.getQuantity) {
-      if (checkedCartItems.every((item) => item.quantity < coupon.buyQuantity + coupon.getQuantity)) {
-        return false;
-      }
+      const totalRequiredQuantity = coupon.buyQuantity + coupon.getQuantity;
+      const hasEnoughQuantity = checkedCartItems.some((item) => item.quantity >= totalRequiredQuantity);
+
+      if (!hasEnoughQuantity) return false;
     }
 
     if (coupon.availableTime) {
@@ -36,21 +35,15 @@ const useCouponValidation = () => {
       const startTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), startHour, startMinute, startSecond);
       const endTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), endHour, endMinute, endSecond);
 
-      if (now < startTime || now > endTime) {
-        return false;
-      }
+      if (now < startTime || now > endTime) return false;
     }
 
-    if (coupon.code === "FREESHIPPING") {
-      if (shippingFee === 0) {
-        return false;
-      }
+    if (coupon.code === "FREESHIPPING" && shippingFee === 0) {
+      return false;
     }
 
-    if (checkedCoupons.length === MAX_CHECKED_COUPONS_LENGTH) {
-      if (!checkedCoupons.includes(coupon)) {
-        return false;
-      }
+    if (checkedCoupons.length === MAX_CHECKED_COUPONS_LENGTH && !checkedCoupons.includes(coupon)) {
+      return false;
     }
 
     return true;
