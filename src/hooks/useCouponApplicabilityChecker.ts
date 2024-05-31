036403cd9d933ef806004a281ddb440d26aa1f15
chysis <chysiss@naver.com>
2024-05-31 16:21:39 +0900

    refactor: 쿠폰 유효성 검사에서 quantity, time 관련 판별 로직 분리

--- src/hooks/useCouponApplicabilityChecker.ts
+++ src/hooks/useCouponApplicabilityChecker.ts
@@ -1,31 +1,24 @@
-import { useRecoilValue } from "recoil";
-import { useCouponFinder } from "./useCouponFinder";
-import { useCouponValidator } from "./useCouponValidator";
-import { checkedCartItemsSelector } from "../store/selector/selectors";
+import { useRecoilValue } from 'recoil';
+import { useCouponFinder } from './useCouponFinder';
+import { useCouponValidator } from './useCouponValidator';
+import { checkedCartItemsSelector } from '../store/selector/selectors';
 
 export const useCouponApplicabilityChecker = () => {
   const checkedItem = useRecoilValue(checkedCartItemsSelector);
   const { findCouponByCode } = useCouponFinder();
   const { isCouponValid } = useCouponValidator();
 
-  const isCouponApplicable = (coupon: Coupon, totalAmount: number, now: Date = new Date()) => {
-    const targetCoupon = findCouponByCode(coupon.code);
-    if (!targetCoupon || !isCouponValid(targetCoupon)) return false;
-
-    if (targetCoupon.minimumAmount && totalAmount < targetCoupon.minimumAmount) {
-      return false;
+  const isQuantityAvailable = (coupon: Coupon) => {
+    if (coupon.buyQuantity && coupon.getQuantity) {
+      return checkedItem.map((item) => item.quantity >= coupon.buyQuantity! + coupon.getQuantity!).some(Boolean);
     }
+  };
 
-    if (targetCoupon.buyQuantity && targetCoupon.getQuantity) {
-      return checkedItem
-        .map((item) => item.quantity >= targetCoupon.buyQuantity! + targetCoupon.getQuantity!)
-        .some(Boolean);
-    }
+  const isTimeAvailable = (coupon: Coupon, now: Date) => {
+    if (coupon.availableTime) {
+      const [startHour, startMinute, startSecond] = coupon.availableTime.start.split(':').map(Number);
 
-    if (targetCoupon.availableTime) {
-      const [startHour, startMinute, startSecond] = targetCoupon.availableTime.start.split(":").map(Number);
-
-      const [endHour, endMinute, endSecond] = targetCoupon.availableTime.end.split(":").map(Number);
+      const [endHour, endMinute, endSecond] = coupon.availableTime.end.split(':').map(Number);
 
       const startTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), startHour, startMinute, startSecond);
 
@@ -35,6 +28,19 @@ export const useCouponApplicabilityChecker = () => {
         return false;
       }
     }
+  };
+
+  const isCouponApplicable = (coupon: Coupon, totalAmount: number, now: Date = new Date('2024-02-22 10:00:00')) => {
+    const targetCoupon = findCouponByCode(coupon.code);
+    if (!targetCoupon || !isCouponValid(targetCoupon)) return false;
+
+    if (targetCoupon.minimumAmount && totalAmount < targetCoupon.minimumAmount) {
+      return false;
+    }
+
+    if (!isQuantityAvailable(targetCoupon)) return false;
+
+    if (!isTimeAvailable(targetCoupon, now)) return false;
 
     return true;
   };
