299b62a28dc0db5a04cd33b3dfc327aa5481d223
GimGayeon <kky1373@naver.com>
2024-06-01 20:51:34 +0900

    test: 쿠폰 사용 가능 상태 확인 테스트

--- src/hooks/test/useCouponAvailable.test.tsx
+++ src/hooks/test/useCouponAvailable.test.tsx
@@ -1,87 +1,293 @@
-import { renderHook } from '@testing-library/react';
+import { renderHook, waitFor } from '@testing-library/react';
 import { RecoilRoot } from 'recoil';
 import useCouponAvailable from '../useCouponAvailable';
 import { mockCoupons } from './coupons.testData';
 import { cartItemQuantityState, cartItemsState } from '../../recoil/cartItems';
 import mockCartItems from './mockCartItems.testData';
-import mockIsCartItemSelecteds from './selectedCartItems.testData';
-import { selectedCartItemsState } from '../../recoil/selectedCardItems';
-import cartQuantity from './cartQuantity.testData';
+import { cartQuantityFail, cartQuantityPass } from './cartQuantity.testData';
 
 describe('useCouponAvailable', () => {
-  it('주문 금액이 최소 주문 금액 미만이면 쿠폰 적용 불가', () => {
-    const { result } = renderHook(() => useCouponAvailable(), {
-      wrapper: ({ children }) => (
-        <RecoilRoot
-          initializeState={({ set }) => {
-            set(cartItemsState, mockCartItems);
-            mockIsCartItemSelecteds.forEach((isCartItemSelected) =>
-              set(
-                selectedCartItemsState(isCartItemSelected.id),
-                isCartItemSelected.boolean,
-              ),
-            );
-            cartQuantity.forEach((isCartItemSelected) =>
-              set(
-                cartItemQuantityState(isCartItemSelected.id),
-                isCartItemSelected.quantity,
-              ),
-            );
-          }}
-        >
-          {children}
-        </RecoilRoot>
-      ),
+  describe('최소 주문금액이 100,000원 인 쿠폰 테스트 ', () => {
+    it('주문 금액이 최소 주문 금액(100,000) 이상일 경우 쿠폰을 사용할 수 있다.', async () => {
+      const { result } = renderHook(() => useCouponAvailable(), {
+        wrapper: ({ children }) => (
+          <RecoilRoot
+            initializeState={({ set }) => {
+              set(cartItemsState, mockCartItems);
+              cartQuantityPass.forEach((isCartItemQuantity) =>
+                set(
+                  cartItemQuantityState(isCartItemQuantity.id),
+                  isCartItemQuantity.quantity,
+                ),
+              );
+            }}
+          >
+            {children}
+          </RecoilRoot>
+        ),
+      });
+
+      await waitFor(() => {
+        expect(result.current.isCouponAvailable(mockCoupons[0])).toBe(true);
+      });
     });
-    console.log(result.current);
-    expect(result.current.isCouponAvailable(mockCoupons[0])).toBe(false);
+    it('주문 금액이 최소 주문 금액(10,000) 이하일 경우 쿠폰을 사용할 수 없다.', async () => {
+      const { result } = renderHook(() => useCouponAvailable(), {
+        wrapper: ({ children }) => (
+          <RecoilRoot
+            initializeState={({ set }) => {
+              set(cartItemsState, mockCartItems);
+              cartQuantityFail.forEach((isCartItemQuantity) =>
+                set(
+                  cartItemQuantityState(isCartItemQuantity.id),
+                  isCartItemQuantity.quantity,
+                ),
+              );
+            }}
+          >
+            {children}
+          </RecoilRoot>
+        ),
+      });
+
+      await waitFor(() => {
+        expect(result.current.isCouponAvailable(mockCoupons[0])).toBe(false);
+      });
+    });
   });
 
-  //   it('주문 금액이 최소 주문 금액 이상이면 쿠폰 적용 가능', () => {
-  //     const { result } = renderHook(() => useCouponApplicabilityChecker(), {
-  //       wrapper: ({ children }) => (
-  //         <RecoilRoot
-  //           initializeState={({ set }) => set(couponsState, mockCoupons)}
-  //         >
-  //           {children}
-  //         </RecoilRoot>
-  //       ),
-  //     });
-  //     expect(result.current.isCouponApplicable(mockCoupons[2], 60000)).toBe(true);
-  //   });
+  describe('BOGO 쿠폰 Available 테스트', () => {
+    it('주문 수량이 3개 이상인 제품이 있다면 사용 가능하다.', async () => {
+      const { result } = renderHook(() => useCouponAvailable(), {
+        wrapper: ({ children }) => (
+          <RecoilRoot
+            initializeState={({ set }) => {
+              set(cartItemsState, mockCartItems);
+              cartQuantityPass.forEach((isCartItemQuantity) =>
+                set(
+                  cartItemQuantityState(isCartItemQuantity.id),
+                  isCartItemQuantity.quantity,
+                ),
+              );
+            }}
+          >
+            {children}
+          </RecoilRoot>
+        ),
+      });
+
+      await waitFor(() => {
+        expect(result.current.isCouponAvailable(mockCoupons[1])).toBe(true);
+      });
+    });
+    it('주문 수량이 3개 이상인 제품이 없다면 사용할 수 없다.', async () => {
+      const { result } = renderHook(() => useCouponAvailable(), {
+        wrapper: ({ children }) => (
+          <RecoilRoot
+            initializeState={({ set }) => {
+              set(cartItemsState, mockCartItems);
+              cartQuantityFail.forEach((isCartItemQuantity) =>
+                set(
+                  cartItemQuantityState(isCartItemQuantity.id),
+                  isCartItemQuantity.quantity,
+                ),
+              );
+            }}
+          >
+            {children}
+          </RecoilRoot>
+        ),
+      });
+
+      await waitFor(() => {
+        expect(result.current.isCouponAvailable(mockCoupons[1])).toBe(false);
+      });
+    });
+  });
+
+  describe('MIRACLESALE 쿠폰 available 테스트', () => {
+    beforeAll(() => {
+      jest.useFakeTimers();
+    });
+    afterAll(() => {
+      jest.useRealTimers();
+    });
+    it('시간이 04~07 일 경우 쿠폰 사용이 가능하다.', async () => {
+      const testTime = new Date('2025-07-10T05:00:00');
+      jest.setSystemTime(testTime);
+
+      const { result } = renderHook(() => useCouponAvailable(), {
+        wrapper: ({ children }) => (
+          <RecoilRoot
+            initializeState={({ set }) => {
+              set(cartItemsState, mockCartItems);
+
+              cartQuantityPass.forEach((isCartItemQuantity) =>
+                set(
+                  cartItemQuantityState(isCartItemQuantity.id),
+                  isCartItemQuantity.quantity,
+                ),
+              );
+            }}
+          >
+            {children}
+          </RecoilRoot>
+        ),
+      });
+
+      await waitFor(() => {
+        expect(result.current.isCouponAvailable(mockCoupons[3])).toBe(true);
+      });
+    });
+    it('시간이 04~07 가 아닐 경우 쿠폰 사용이 불가능하다.', async () => {
+      const testTime = new Date('2025-07-10T15:00:00');
+      jest.setSystemTime(testTime);
+
+      const { result } = renderHook(() => useCouponAvailable(), {
+        wrapper: ({ children }) => (
+          <RecoilRoot
+            initializeState={({ set }) => {
+              set(cartItemsState, mockCartItems);
+
+              cartQuantityFail.forEach((isCartItemQuantity) =>
+                set(
+                  cartItemQuantityState(isCartItemQuantity.id),
+                  isCartItemQuantity.quantity,
+                ),
+              );
+            }}
+          >
+            {children}
+          </RecoilRoot>
+        ),
+      });
+
+      await waitFor(() => {
+        expect(result.current.isCouponAvailable(mockCoupons[3])).toBe(false);
+      });
+    });
+  });
+
+  describe('BOGO 쿠폰 Available 테스트', () => {
+    it('주문 수량이 3개 이상인 제품이 있다면 사용 가능하다.', async () => {
+      const { result } = renderHook(() => useCouponAvailable(), {
+        wrapper: ({ children }) => (
+          <RecoilRoot
+            initializeState={({ set }) => {
+              set(cartItemsState, mockCartItems);
+
+              cartQuantityPass.forEach((isCartItemQuantity) =>
+                set(
+                  cartItemQuantityState(isCartItemQuantity.id),
+                  isCartItemQuantity.quantity,
+                ),
+              );
+            }}
+          >
+            {children}
+          </RecoilRoot>
+        ),
+      });
+
+      await waitFor(() => {
+        expect(result.current.isCouponAvailable(mockCoupons[1])).toBe(true);
+      });
+    });
+    it('주문 수량이 3개 이상인 제품이 없다면 사용할 수 없다.', async () => {
+      const { result } = renderHook(() => useCouponAvailable(), {
+        wrapper: ({ children }) => (
+          <RecoilRoot
+            initializeState={({ set }) => {
+              set(cartItemsState, mockCartItems);
+
+              cartQuantityFail.forEach((isCartItemQuantity) =>
+                set(
+                  cartItemQuantityState(isCartItemQuantity.id),
+                  isCartItemQuantity.quantity,
+                ),
+              );
+            }}
+          >
+            {children}
+          </RecoilRoot>
+        ),
+      });
+
+      await waitFor(() => {
+        expect(result.current.isCouponAvailable(mockCoupons[1])).toBe(false);
+      });
+    });
+  });
+
+  describe('쿠폰 유효기간 테스트', () => {
+    beforeAll(() => {
+      jest.useFakeTimers();
+    });
+    afterAll(() => {
+      jest.useRealTimers();
+    });
+
+    it('쿠폰 유효기간이 지났을 경우 쿠폰 사용이 불가하다.', async () => {
+      /**
+       * 현재 시간을 2026년 7월로 설정
+       * 쿠폰 유효기간은 2025-11-30 이다.
+       */
+      const testTime = new Date('2026-07-10T15:00:00');
+      jest.setSystemTime(testTime);
+
+      const { result } = renderHook(() => useCouponAvailable(), {
+        wrapper: ({ children }) => (
+          <RecoilRoot
+            initializeState={({ set }) => {
+              set(cartItemsState, mockCartItems);
+
+              cartQuantityPass.forEach((isCartItemQuantity) =>
+                set(
+                  cartItemQuantityState(isCartItemQuantity.id),
+                  isCartItemQuantity.quantity,
+                ),
+              );
+            }}
+          >
+            {children}
+          </RecoilRoot>
+        ),
+      });
+
+      await waitFor(() => {
+        expect(result.current.isCouponAvailable(mockCoupons[0])).toBe(false);
+      });
+    });
+    it('쿠폰 유효기간이 지나지 않았을 경우 쿠폰 사용이 가능하다.', async () => {
+      /**
+       * 현재 시간을 2025년 7월로 설정
+       * 쿠폰 유효기간은 2025-11-30 이다.
+       */
+      const testTime = new Date('2025-07-10T05:00:00');
+      jest.setSystemTime(testTime);
+
+      const { result } = renderHook(() => useCouponAvailable(), {
+        wrapper: ({ children }) => (
+          <RecoilRoot
+            initializeState={({ set }) => {
+              set(cartItemsState, mockCartItems);
+
+              cartQuantityPass.forEach((isCartItemQuantity) =>
+                set(
+                  cartItemQuantityState(isCartItemQuantity.id),
+                  isCartItemQuantity.quantity,
+                ),
+              );
+            }}
+          >
+            {children}
+          </RecoilRoot>
+        ),
+      });
+
+      await waitFor(() => {
+        expect(result.current.isCouponAvailable(mockCoupons[0])).toBe(true);
+      });
+    });
+  });
 });
-
-// describe('쿠폰 사용 가능 시간 확인', () => {
-//   it('사용 가능 시간 외에는 쿠폰 적용 불가', () => {
-//     const testTime = new Date('2023-05-01T08:00:00');
-
-//     const { result } = renderHook(() => useCouponApplicabilityChecker(), {
-//       wrapper: ({ children }) => (
-//         <RecoilRoot
-//           initializeState={({ set }) => set(couponsState, mockCoupons)}
-//         >
-//           {children}
-//         </RecoilRoot>
-//       ),
-//     });
-//     expect(
-//       result.current.isCouponApplicable(mockCoupons[3], 100000, testTime),
-//     ).toBe(false);
-//   });
-
-//   it('사용 가능 시간 내에는 쿠폰 적용 가능', () => {
-//     const testTime = new Date('2023-05-01T07:00:00');
-//     const { result } = renderHook(() => useCouponApplicabilityChecker(), {
-//       wrapper: ({ children }) => (
-//         <RecoilRoot
-//           initializeState={({ set }) => set(couponsState, mockCoupons)}
-//         >
-//           {children}
-//         </RecoilRoot>
-//       ),
-//     });
-//     expect(
-//       result.current.isCouponApplicable(mockCoupons[3], 100000, testTime),
-//     ).toBe(true);
-//   });
-// });
