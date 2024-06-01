d78a5725e5f1ec0979181f3294e0f104a8061c4a
soosoo22 <lllllh00@naver.com>
2024-06-01 13:26:04 +0900

    fix: 결제하기 버튼 클릭 시 장바구니 데이터가 비워지지 않는 문제 해결

--- src/pages/OrderConfirmPage/OrderConfirmPage.tsx
+++ src/pages/OrderConfirmPage/OrderConfirmPage.tsx
@@ -3,16 +3,34 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import { FloatingButton } from '../../components/Button';
 import OrderConfirmCart from '../../components/OrderConfirmCart/OrderConfirmCart';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { calculateOrderPrice } from '../../recoil/selectors/selectors';
 import { orderCartItems } from '../../api';
 import { Cart } from '../../types/cart';
+import { cartData, cartQuantity } from '../../recoil/atoms/atoms';
 
 export default function OrderConfirmPage() {
   const navigate = useNavigate();
   const location = useLocation();
   const { cartItems } = location.state || {};
   const { totalPrice } = useRecoilValue(calculateOrderPrice);
+  const setCartTotalCount = useSetRecoilState(cartQuantity);
+  const setCartData = useSetRecoilState(cartData);
+
+  const handleOrder = async () => {
+    try {
+      await orderCartItems(cartItems.map((item: Cart) => item.id));
+
+      setCartData((prevCartData) =>
+        prevCartData.filter((item) => !cartItems.some((orderedItem: Cart) => orderedItem.id === item.id)),
+      );
+
+      setCartTotalCount((prevCount) => prevCount - cartItems.length);
+      navigate('/payment-confirm', { state: { totalPrice } });
+    } catch (error) {
+      console.error('Error processing order:', error);
+    }
+  };
 
   return (
     <div id="orderConfirmPage">
@@ -23,14 +41,7 @@ export default function OrderConfirmPage() {
         }}
       />
       <OrderConfirmCart cartItems={cartItems} />
-      <FloatingButton
-        text="결제하기"
-        isDisable={false}
-        onClick={() => {
-          navigate('/payment-confirm', { state: { totalPrice } });
-          orderCartItems(cartItems.map((item: Cart) => item.id));
-        }}
-      />
+      <FloatingButton text="결제하기" isDisable={false} onClick={handleOrder} />
     </div>
   );
 }
