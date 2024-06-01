1b7e7edf3102ec72c064861b9ce5fd8037ead508
GimGayeon <kky1373@naver.com>
2024-06-01 20:51:44 +0900

    test: 쿠폰 할인 적용 테스트

--- src/hooks/test/useDisCountType.test.tsx
+++ src/hooks/test/useDisCountType.test.tsx
@@ -1,90 +1,122 @@
-import '@testing-library/jest-dom';
-import { fireEvent, render, screen } from '@testing-library/react';
-import { OrderConfirmation } from '../../components/pages/OrderConfirmationPage/style';
-import 'jest-styled-components';
+import { renderHook, waitFor } from '@testing-library/react';
+import { RecoilRoot } from 'recoil';
+import {
+  buy1get1,
+  couponPriority,
+  fixed1000,
+  percentageDiscount,
+} from './coupons.testData';
+import { cartItemQuantityState, cartItemsState } from '../../recoil/cartItems';
+import mockCartItems from './mockCartItems.testData';
+import { cartQuantityPass } from './cartQuantity.testData';
+import useDiscountType from '../useDiscountType';
+
 describe('useDiscountType', () => {
-  it('2개 구매 시 1개 무료 쿠폰 클릭 시 ', () => {
-    render(<OrderConfirmation />);
-    const button = screen.getByLabelText('제주도 및 도서 산간 지역');
+  describe('쿠폰 타입 할인 테스트 ', () => {
+    it('discountType이 fixed이며 discount가 1000 일 경우 천원이 할인된다. ', async () => {
+      const { result } = renderHook(() => useDiscountType(), {
+        wrapper: ({ children }) => (
+          <RecoilRoot
+            initializeState={({ set }) => {
+              set(cartItemsState, mockCartItems);
 
-    fireEvent.click(button);
-    const price = screen.getByTestId('price');
+              cartQuantityPass.forEach((isCartItemQuantity) =>
+                set(
+                  cartItemQuantityState(isCartItemQuantity.id),
+                  isCartItemQuantity.quantity,
+                ),
+              );
+            }}
+          >
+            {children}
+          </RecoilRoot>
+        ),
+      });
 
-    expect(price).toHaveTextContent('6,000원');
+      await waitFor(async () => {
+        result.current.applyCoupon(fixed1000);
+        await expect(result.current.getDiscountAmount).toBe(1000);
+      });
+    });
+
+    it('discountType이 buy1get1 일 경우 수량이 1인 제품 중 가장 비싼 제품 가격만큼 할인된다.  ', async () => {
+      const { result } = renderHook(() => useDiscountType(), {
+        wrapper: ({ children }) => (
+          <RecoilRoot
+            initializeState={({ set }) => {
+              set(cartItemsState, mockCartItems);
+
+              cartQuantityPass.forEach((isCartItemQuantity) =>
+                set(
+                  cartItemQuantityState(isCartItemQuantity.id),
+                  isCartItemQuantity.quantity,
+                ),
+              );
+            }}
+          >
+            {children}
+          </RecoilRoot>
+        ),
+      });
+
+      await waitFor(async () => {
+        result.current.applyCoupon(buy1get1);
+        await expect(result.current.getDiscountAmount).toBe(50000);
+      });
+    });
+
+    it('discountType이 percentageDiscount이며 discount가 20일 경우 전체 가격(270000)의 20%가 할인된다.)', async () => {
+      const { result } = renderHook(() => useDiscountType(), {
+        wrapper: ({ children }) => (
+          <RecoilRoot
+            initializeState={({ set }) => {
+              set(cartItemsState, mockCartItems);
+
+              cartQuantityPass.forEach((isCartItemQuantity) =>
+                set(
+                  cartItemQuantityState(isCartItemQuantity.id),
+                  isCartItemQuantity.quantity,
+                ),
+              );
+            }}
+          >
+            {children}
+          </RecoilRoot>
+        ),
+      });
+
+      await waitFor(async () => {
+        result.current.applyCoupon(percentageDiscount);
+        await expect(result.current.getDiscountAmount).toBe(54000);
+      });
+    });
   });
 
-  // it('할인 타입이 fixed 인 경우', () => {
-  //   //컴포넌ㅇ트 불러오는식으로 구현하기
-  //   const { result } = renderHook(() => useDiscountType(), {
-  //     wrapper: ({ children }) => (
-  //       <RecoilRoot
-  //         initializeState={({ set }) => {
-  //           set(cartItemsState, mockCartItems);
-  //           mockIsCartItemSelecteds.forEach((isCartItemSelected) =>
-  //             set(
-  //               selectedCartItemsState(isCartItemSelected.id),
-  //               isCartItemSelected.boolean,
-  //             ),
-  //           );
-  //           cartQuantity.forEach((isCartItemSelected) =>
-  //             set(
-  //               cartItemQuantityState(isCartItemSelected.id),
-  //               isCartItemSelected.quantity,
-  //             ),
-  //           );
-  //         }}
-  //       >
-  //         {children}
-  //       </RecoilRoot>
-  //     ),
-  //   });
-  //   expect(result.current.applyCoupon(mockCoupons)).toBe(false);
-  // });
-  //   it('주문 금액이 최소 주문 금액 이상이면 쿠폰 적용 가능', () => {
-  //     const { result } = renderHook(() => useCouponApplicabilityChecker(), {
-  //       wrapper: ({ children }) => (
-  //         <RecoilRoot
-  //           initializeState={({ set }) => set(couponsState, mockCoupons)}
-  //         >
-  //           {children}
-  //         </RecoilRoot>
-  //       ),
-  //     });
-  //     expect(result.current.isCouponApplicable(mockCoupons[2], 60000)).toBe(true);
-  //   });
+  describe('쿠폰 타입 우선순위 테스트 ', () => {
+    it('fixed1000과 percentage30 쿠폰이 적용 될 경우 percentage30 가 먼저 적용된 가격으로 할인된다.', async () => {
+      const { result } = renderHook(() => useDiscountType(), {
+        wrapper: ({ children }) => (
+          <RecoilRoot
+            initializeState={({ set }) => {
+              set(cartItemsState, mockCartItems);
+
+              cartQuantityPass.forEach((isCartItemQuantity) =>
+                set(
+                  cartItemQuantityState(isCartItemQuantity.id),
+                  isCartItemQuantity.quantity,
+                ),
+              );
+            }}
+          >
+            {children}
+          </RecoilRoot>
+        ),
+      });
+
+      await waitFor(async () => {
+        result.current.applyCoupon(couponPriority);
+        await expect(result.current.getDiscountAmount).toBe(55000);
+      });
+    });
+  });
 });
-
-// describe('쿠폰 사용 가능 시간 확인', () => {
-//   it('사용 가능 시간 외에는 쿠폰 적용 불가', () => {
-//     const testTime = new Date('2023-05-01T08:00:00');
-
-//     const { result } = renderHook(() => useCouponApplicabilityChecker(), {
-//       wrapper: ({ children }) => (
-//         <RecoilRoot
-//           initializeState={({ set }) => set(couponsState, mockCoupons)}
-//         >
-//           {children}
-//         </RecoilRoot>
-//       ),
-//     });
-//     expect(
-//       result.current.isCouponApplicable(mockCoupons[3], 100000, testTime),
-//     ).toBe(false);
-//   });
-
-//   it('사용 가능 시간 내에는 쿠폰 적용 가능', () => {
-//     const testTime = new Date('2023-05-01T07:00:00');
-//     const { result } = renderHook(() => useCouponApplicabilityChecker(), {
-//       wrapper: ({ children }) => (
-//         <RecoilRoot
-//           initializeState={({ set }) => set(couponsState, mockCoupons)}
-//         >
-//           {children}
-//         </RecoilRoot>
-//       ),
-//     });
-//     expect(
-//       result.current.isCouponApplicable(mockCoupons[3], 100000, testTime),
-//     ).toBe(true);
-//   });
-// });
