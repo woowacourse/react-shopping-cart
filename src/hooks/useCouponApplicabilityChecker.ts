148ed4061254536eec8ed9b525a5f4da2a2a3ede
00kang <kdb070970@gmail.com>
2024-06-03 19:33:58 +0900

    refactor : isMinimumAmountSatisfied과 isWithinAvailableTime로 util 함수 분리

--- src/hooks/useCouponApplicabilityChecker.ts
+++ src/hooks/useCouponApplicabilityChecker.ts
@@ -1,10 +1,5 @@
-import {
-  Coupon,
-  DiscountType,
-  FixedDiscountCoupon,
-  FreeShippingCoupon,
-  PercentageDiscountCoupon,
-} from "../types";
+import { Coupon } from "../types";
+import { isMinimumAmountSatisfied, isWithinAvailableTime } from "../utils/couponChecker";
 import { couponValidator } from "../utils/couponValidator";
 import { useCouponFinder } from "./useCouponFinder";
 
@@ -12,58 +7,16 @@ export const useCouponApplicabilityChecker = () => {
   const { findCouponByCode } = useCouponFinder();
   const { isCouponValid } = couponValidator();
 
-  const isFixedDiscountCoupon = (coupon: Coupon): coupon is FixedDiscountCoupon => {
-    return coupon.discountType === DiscountType.Fixed;
-  };
-
-  const isFreeShippingCoupon = (coupon: Coupon): coupon is FreeShippingCoupon => {
-    return coupon.discountType === DiscountType.FreeShipping;
-  };
-
-  const isPercentageDiscountCoupon = (coupon: Coupon): coupon is PercentageDiscountCoupon => {
-    return coupon.discountType === DiscountType.Percentage;
-  };
-
   const isCouponApplicable = (coupon: Coupon, totalAmount: number, now: Date = new Date()) => {
     const targetCoupon = findCouponByCode(coupon.code);
     if (!targetCoupon || !isCouponValid(targetCoupon)) return false;
 
-    if (
-      (isFixedDiscountCoupon(targetCoupon) || isFreeShippingCoupon(targetCoupon)) &&
-      targetCoupon.minimumAmount &&
-      totalAmount < targetCoupon.minimumAmount
-    ) {
+    if (!isMinimumAmountSatisfied(targetCoupon, totalAmount)) {
       return false;
     }
 
-    if (isPercentageDiscountCoupon(targetCoupon) && targetCoupon.availableTime) {
-      const [startHour, startMinute, startSecond] = targetCoupon.availableTime.start
-        .split(":")
-        .map(Number);
-
-      const [endHour, endMinute, endSecond] = targetCoupon.availableTime.end.split(":").map(Number);
-
-      const startTime = new Date(
-        now.getFullYear(),
-        now.getMonth(),
-        now.getDate(),
-        startHour,
-        startMinute,
-        startSecond
-      );
-
-      const endTime = new Date(
-        now.getFullYear(),
-        now.getMonth(),
-        now.getDate(),
-        endHour,
-        endMinute,
-        endSecond
-      );
-
-      if (now < startTime || now > endTime) {
-        return false;
-      }
+    if (!isWithinAvailableTime(targetCoupon, now)) {
+      return false;
     }
 
     return true;
