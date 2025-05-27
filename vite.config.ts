75f3ee65687270a2c9d9a7416be3b62dc5b70d48
ohgus <anakin1108@naver.com>
2025-05-27 16:37:26 +0900

    chore: 오타 수정

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [
     react({
-      jsxImportSource: '@emiton/react',
+      jsxImportSource: '@emotion/react',
     }),
   ],
 });
