e4e4aa45869e9689ef22cd92fd1bc444fcd6a408
Oliy <eunsoa0310@gmail.com>
2025-05-27 16:47:41 +0900

    feat: MobileLayout 추가
    Co-authored-by: 우혁 <woogur29@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './styles/reset.css';
+import MobileLayout from '@shared/components/MobileLayout/MobileLayout.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <MobileLayout>
+      <App />
+    </MobileLayout>
   </React.StrictMode>,
 );
