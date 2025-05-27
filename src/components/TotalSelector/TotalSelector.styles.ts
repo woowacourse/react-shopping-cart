a3294ac364fdc3a6b3a37a5b33615354b628f763
sooyeoniya <tndus1306403@naver.com>
2025-05-27 16:32:14 +0900

    feat: TotalSelector 컴포넌트 구현

---
+++ src/components/TotalSelector/TotalSelector.styles.ts
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+
+export const TotalSelector = styled.div`
+  display: flex;
+  gap: 8px;
+  align-items: center;
+`;
--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -2,6 +2,7 @@ import Description from "../../components/Description/Description";
 import { FooterButton } from "../../components/FooterButton/FooterButton.styles";
 import { Header } from "../../components/Header/Header.styles";
 import Title from "../../components/Title/Title";
+import TotalSelector from "../../components/TotalSelector/TotalSelector";
 import * as S from "./CartPage.styles";
 
 const CartPage = () => {
@@ -15,6 +16,7 @@ const CartPage = () => {
           <Title>장바구니</Title>
           <Description>현재 2종류의 상품이 담겨있습니다.</Description>
         </S.TitleContainer>
+        <TotalSelector checked={true} />
       </S.Main>
       <FooterButton>주문 확인</FooterButton>
     </>
