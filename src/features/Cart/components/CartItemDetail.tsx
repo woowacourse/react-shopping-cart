00c0027d5b8b5476a104b5c2f7f35b13fd75c393
keemsebeen <sebin0580@gmail.com>
2025-05-27 16:01:56 +0900

    refactor: CartItemDetail 컴포넌트 체크박스 컴포넌트 추가
    Co-Authored-By: YEJI <91647696+yeji0214@users.noreply.github.com>

--- src/features/Cart/components/CartItemDetail.tsx
+++ src/features/Cart/components/CartItemDetail.tsx
@@ -6,73 +6,80 @@ import { Text } from '@/shared/components/Text';
 import { Button } from '@/shared/components/Button';
 import { QuantitySelector } from './QuantitySelector';
 
-export const CartItemDetail = ({
-  id,
-  quantity,
-  product,
-}: {
-  id: number;
-  quantity: number;
-  product: object;
-}) => {
+import { CartItem } from '../types/Cart.types';
+import { CheckBox } from '@/shared/components/CheckBox';
+
+export const CartItemDetail = ({ id, quantity, product }: CartItem) => {
   return (
     <Flex
-      direction="row"
-      justifyContent="space-between"
+      direction="column"
+      justifyContent="flex-start"
       alignItems="flex-start"
-      gap=""
-      // css={css`
-      //   border-top: 1px solid #e5e5e5;
-      //   margin-top: 16px;
-      // `}
+      gap="0"
+      css={css`
+        border-top: 1px solid #e5e5e5;
+        margin-top: 16px;
+      `}
     >
+      <Flex
+        direction={'row'}
+        justifyContent={'space-between'}
+        alignItems={'center'}
+        width="100%"
+        gap="0"
+        margin="10px 0 0 0 "
+      >
+        <CheckBox />
+        <Button
+          variant="outlined"
+          size="xs"
+          color="#e5e5e5"
+          fontColor="black"
+          css={css`
+            margin-top: 8px;
+          `}
+          // onClick={removeCartItem}
+        >
+          삭제
+        </Button>
+      </Flex>
       <Flex
         direction="row"
         justifyContent="flex-start"
         alignItems="center"
         gap="4px"
-        height="100px"
+        height="110px"
         padding="8px 0"
         margin="12px 0 0 0"
       >
-        {/* <StyledCartItemImg src={product.imageUrl} alt={product.} /> */}
+        <StyledCartItemImg src={product.imageUrl} alt={product.imageUrl} />
         <Flex
           direction="column"
           justifyContent="space-between"
           alignItems="flex-start"
           gap="4px"
           width="100%"
-          height="100%"
-          margin="0 0 0 8px"
+          height="100px"
+          margin="0 0 0 16px"
         >
-          <Text type="Body" weight="semibold">
-            dd
-          </Text>
-          <Text type="Caption" weight="regular">
-            0원
-          </Text>
+          <Flex direction="column" justifyContent="flex-start" alignItems="flex-start" gap="0">
+            <Text type="Caption" weight="regular">
+              상품이름
+            </Text>
+            <Text type="Heading" weight="semibold">
+              0원
+            </Text>
+          </Flex>
           <QuantitySelector count={123} onIncrease={() => {}} onDecrease={() => {}} />
         </Flex>
       </Flex>
-      <Button
-        variant="outlined"
-        size="xs"
-        color="#e5e5e5"
-        fontColor="black"
-        // css={css`
-        //   margin-top: 8px;
-        // `}
-        // onClick={removeCartItem}
-      >
-        삭제
-      </Button>
     </Flex>
   );
 };
 
 const StyledCartItemImg = styled.img`
-  width: 90px;
-  height: 90px;
+  width: 110px;
+  height: 110px;
   object-fit: cover;
   border-radius: 8px;
 `;
