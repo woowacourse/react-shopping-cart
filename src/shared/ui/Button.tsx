727f4878c585753eb17a81397c0ca5e426c8230a
eunwoo-levi <eunwoo1341@gmail.com>
2025-05-27 17:33:05 +0900

    styles: Button 컴포넌트 style 구현

--- src/shared/ui/Button.tsx
+++ src/shared/ui/Button.tsx
@@ -1,7 +1,14 @@
+import styled from '@emotion/styled';
+
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   title: string;
 }
 
 export default function Button({ title, ...rest }: ButtonProps) {
-  return <button {...rest}>{title}</button>;
+  return <ButtonStyles {...rest}>{title}</ButtonStyles>;
 }
+
+const ButtonStyles = styled.button`
+  border: none;
+  cursor: pointer;
+`;
