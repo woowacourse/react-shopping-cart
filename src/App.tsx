ad2b0e3588c03d4e716fefd9da5043f07a4f9efb
Jeongeun Lee <leeju1009@gmail.com>
2025-05-27 17:07:52 +0900

    feat: Description 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import "./reset.css";
 import "./App.css";
 import logo from "/public/logo.svg";
 import Header from "./components/Header/Header";
+import Description from "./components/Description/Description";
 
 function App() {
   return (
     <>
       <Header icon={logo} handleIconClick={() => alert("클릭")} />
+      <Description cartItemCount={2} />
     </>
   );
 }
