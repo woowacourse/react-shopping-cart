e23f4922bb8eac6d54a765cfada2e600954545d0
dev-dino22 <wlsskdud02@naver.com>
2025-05-27 23:00:54 +0900

    feat: App 컴포넌트에 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { Global } from "@emotion/react";
+import { ThemeProvider } from "styled-components";
 import "./App.css";
+import { MobileLayout } from "./components/common";
+import { APIDataProvider } from "./context/APIDataProvider";
+import reset from "./global/style/reset";
+import { theme } from "./global/style/theme";
+import ShoppingCartPage from "./pages/shopping-cart/ShoppingCartPage";
 
 function App() {
   return (
     <>
-      <h1>react-shopping-cart</h1>
+      <Global styles={reset} />
+      <ThemeProvider theme={theme}>
+        <MobileLayout>
+          <APIDataProvider>
+            <ShoppingCartPage />
+          </APIDataProvider>
+        </MobileLayout>
+      </ThemeProvider>
     </>
   );
 }
