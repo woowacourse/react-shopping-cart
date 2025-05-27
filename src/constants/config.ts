2cc76b8da4d805f423ac4f81b06511bee491c424
sooyeoniya <tndus1306403@naver.com>
2025-05-27 19:11:36 +0900

    feat: MSW 서버 설정 및 응답 모킹

---
+++ src/constants/config.ts
@@ -0,0 +1,4 @@
+export const BASE_URL = import.meta.env.BASE_URL || "/react-shopping-cart";
+export const API_BASE_URL =
+  "https://techcourse-lv2-alb-974870821.ap-northeast-2.elb.amazonaws.com/";
+export const END_POINT = "cart-items";
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import MobileLayout from "./components/MobileLayout/MobileLayout.tsx";
+import { BASE_URL } from "./constants/config.ts";
 import "./styles/reset.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <MobileLayout>
-      <App />
-    </MobileLayout>
-  </React.StrictMode>
+async function enableMocking() {
+  const { worker } = await import("./mocks/browser");
+
+  return worker.start({
+    serviceWorker: {
+      url: `${window.location.origin}${BASE_URL}mockServiceWorker.js`,
+      options: { scope: BASE_URL },
+    },
+    onUnhandledRequest: "bypass",
+  });
+}
+
+enableMocking().then(() =>
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <MobileLayout>
+        <App />
+      </MobileLayout>
+    </React.StrictMode>
+  )
 );
