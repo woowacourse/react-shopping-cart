f6b382f5d518bd1e847ca877559d20294eeddfcc
thgml05 <aisohii@naver.com>
2025-05-27 17:39:34 +0900

    feat: Text 컴포넌트 추가

---
+++ src/components/common/Text/Text.styles.ts
@@ -0,0 +1,14 @@
+import { css } from 'styled-components';
+import { theme } from '../../../styles';
+
+export const TextStyle = {
+  title: css`
+    ${theme.font.title}
+  `,
+  body: css`
+    ${theme.font.body}
+  `,
+  caption: css`
+    ${theme.font.caption}
+  `,
+};
---
+++ src/styles/theme.styles.ts
@@ -0,0 +1,28 @@
+const theme = {
+  color: {
+    black: '#000',
+    white: '#fff',
+    gray1: '#EAEAEA',
+    gray2: 'rgba(0, 0, 0, 0.10)',
+    gray3: '#333',
+    pink: '#FFC9C9',
+    green: '#90EE90',
+  },
+  font: {
+    title: {
+      fontSize: '2.4rem',
+      fontWeight: '700',
+    },
+    body: {
+      fontSize: '1.6rem',
+      fontWeight: '700',
+      lineHeight: '2rem',
+    },
+    caption: {
+      fontSize: '1.2rem',
+      fontWeight: '500',
+      lineHeight: '2rem',
+    },
+  },
+};
+export default theme;
