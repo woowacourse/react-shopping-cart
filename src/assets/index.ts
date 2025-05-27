d4148dbdae86ccc09b1527762d51346cf4bbdde0
ohgus <anakin1108@naver.com>
2025-05-27 16:54:56 +0900

    feat: HeaderButton 컴포넌트 구현

---
+++ src/assets/index.ts
@@ -0,0 +1,1 @@
+export const Logo = `./icons/logo.svg`;
--- src/components/common/Header/Header.styles.ts
+++ src/components/common/Header/Header.styles.ts
@@ -1,7 +1,17 @@
-import { css } from '@emotion/react';
+import { css } from "@emotion/react";
 
 export const HeaderStyle = css`
   background: #000000;
   width: 100%;
   height: 6.4rem;
+  padding: 2.4rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  box-sizing: border-box;
+`;
+
+export const HeaderButtonStyle = css`
+  width: auto;
+  height: auto;
 `;
