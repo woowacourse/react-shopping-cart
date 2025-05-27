0a7658c65e292d8ff66ca924f928ceb15041950e
H0ngJu <hongju4385@gmail.com>
2025-05-27 15:12:25 +0900

    feat(Text): 공통 Text 컴포넌트 구현

---
+++ src/components/@common/Text/Text.tsx
@@ -0,0 +1,18 @@
+import { css } from "@emotion/css";
+
+type TextType = "small" | "medium" | "large";
+
+interface TextProps {
+  text: string;
+  type?: TextType;
+}
+const Text = ({ text, type = "small" }: TextProps) => {
+  return <div className={TextStyle(type)}>{text}</div>;
+};
+
+export default Text;
+
+const TextStyle = (type: TextType) => css`
+  font-weight: ${type === "small" ? 500 : 700};
+  font-size: ${type === "large" ? "24px" : type === "medium" ? "16px" : "12px"};
+`;
