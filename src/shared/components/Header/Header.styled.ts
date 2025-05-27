3f5a0465d688fb6fa6945129d6e60aeef13bb612
Oliy <eunsoa0310@gmail.com>
2025-05-27 16:48:01 +0900

    feat: Header 컴포넌트 추가
    Co-authored-by: 우혁 <woogur29@gmail.com>

---
+++ src/shared/components/Header/Header.styled.ts
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const Header = styled.div`
+  width: 100%;
+  height: 64px;
+  background-color: #000;
+  color: #fff;
+  font-size: 20px;
+  font-weight: 800;
+  padding: 24px;
+`;
