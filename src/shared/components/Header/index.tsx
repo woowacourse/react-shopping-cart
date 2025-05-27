ba792ae04fc10e79bffd2916cbd01acfe8c75182
keemsebeen <sebin0580@gmail.com>
2025-05-27 15:12:07 +0900

    feat: Header 컴포넌트 구현
    Co-Authored-By: YEJI <91647696+yeji0214@users.noreply.github.com>

---
+++ src/shared/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import { ComponentProps, ReactNode } from 'react';
+
+import { StyledHeader } from './Header.styled';
+
+export type HeaderProps = {
+  left: ReactNode;
+  right?: ReactNode;
+} & ComponentProps<'header'>;
+
+export const Header = ({ left, right, ...props }: HeaderProps) => {
+  return (
+    <StyledHeader right={right} {...props}>
+      {left}
+      {right}
+    </StyledHeader>
+  );
+};
