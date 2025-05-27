d97326ac5bf12a37fbad2b87af626209fe733901
dev-dino22 <wlsskdud02@naver.com>
2025-05-27 22:52:53 +0900

    feat: common 컴포넌트 한 번에 모듈화해서 export

---
+++ src/components/common/index.ts
@@ -0,0 +1,5 @@
+export { default as Arrow } from "./Arrow";
+export { default as Flex } from "./Flex";
+export { default as Header } from "./Header";
+export { default as Loading } from "./Loading";
+export { default as MobileLayout } from "./MobileLayout";
