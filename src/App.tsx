b6095a0d449168802993149af7578a110c2e2ecc
keemsebeen <sebin0580@gmail.com>
2025-05-27 16:02:26 +0900

    feat: App.tsx에 CartPage import
    Co-Authored-By: YEJI <91647696+yeji0214@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import "./App.css";
+import './index.css';
+import { CartPage } from './features/Cart/pages/CartPage';
 
 function App() {
-  return (
-    <>
-      <h1>react-shopping-cart</h1>
-    </>
-  );
+  return <CartPage />;
 }
 
 export default App;
