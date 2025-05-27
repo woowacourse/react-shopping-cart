64370ba97ca7eafa0b10befe2c1e3de272b603b4
마 <126178440+shuyeon@users.noreply.github.com>
2025-05-27 16:51:09 +0900

    feat : CartCount 컴포넌트 구현

---
+++ src/components/feature/CartSection/CartProducts/Card/CartCount/index.tsx
@@ -0,0 +1,37 @@
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+import Button from "../../../../../common/Button";
+
+type Props = {
+  count: number;
+  onPlusCount: () => void;
+  onMinusCount: () => void;
+};
+
+const CartCount = ({ count, onPlusCount, onMinusCount }: Props) => {
+  const className = css`
+    background-color: #fff;
+    border: 1px solid #d5d5d5;
+    border-radius: 10px;
+    padding: 5px 5px;
+  `;
+
+  return (
+    <CartCountSection>
+      <Button
+        onClick={onMinusCount}
+        iconUrl="./minus-icon.svg"
+        css={className}
+      />
+      {count}
+      <Button onClick={onPlusCount} iconUrl="./plus-icon.svg" css={className} />
+    </CartCountSection>
+  );
+};
+export default CartCount;
+
+const CartCountSection = styled.div`
+  display: flex;
+  gap: 13px;
+  align-items: center;
+`;
