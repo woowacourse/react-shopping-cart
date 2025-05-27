ea5679e3832d5a28456c83f713467b887a529299
sooyeoniya <tndus1306403@naver.com>
2025-05-27 17:57:42 +0900

    feat: PriceInfo 컴포넌트 구현

---
+++ src/components/PriceInfo/PriceInfo.styles.ts
@@ -0,0 +1,19 @@
+import styled from "@emotion/styled";
+
+export const PriceInfo = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 8px;
+`;
+
+export const Label = styled.div`
+  font-weight: 700;
+  font-size: 18px;
+`;
+
+export const Price = styled.div`
+  font-weight: 700;
+  font-size: 26px;
+`;
