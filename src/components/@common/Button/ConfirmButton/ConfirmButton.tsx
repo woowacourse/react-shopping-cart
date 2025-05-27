02912232bc4e5a0bce10ed7c02d0752f6740ec95
H0ngJu <hongju4385@gmail.com>
2025-05-27 15:31:47 +0900

    feat(ConfirmButton): ConfirmButton 컴포넌트 구현
    Co-authored-by: mlnwns <mlnwns@users.noreply.github.com>

---
+++ src/components/@common/Button/ConfirmButton/ConfirmButton.tsx
@@ -0,0 +1,29 @@
+import { css } from "@emotion/css";
+import Text from "../../Text/Text";
+
+interface ConfirmButtonProps {
+  text: string;
+  onClick: () => void;
+  disabled: boolean;
+}
+
+const ConfirmButton = ({ text, onClick, disabled }: ConfirmButtonProps) => {
+  return (
+    <button
+      className={ConfirmButtonStyle}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      <Text text={text} type="medium" />
+    </button>
+  );
+};
+
+export default ConfirmButton;
+
+const ConfirmButtonStyle = css`
+  width: 100%;
+  height: 64px;
+  color: white;
+  background-color: #000000;
+`;
