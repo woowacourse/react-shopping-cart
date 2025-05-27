a71f6ef79b3ad54974eec4ff06599933f44acaf4
sooyeoniya <tndus1306403@naver.com>
2025-05-27 15:29:39 +0900

    design: FooterButton 컴포넌트 구현

---
+++ src/components/FooterButton/FooterButton.styles.ts
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+
+export const FooterButton = styled.button`
+  display: flex;
+  position: relative;
+  background-color: #000;
+  width: 100%;
+  height: 64px;
+  justify-content: center;
+  align-items: center;
+  padding: 0 24px;
+  font-weight: 700;
+  font-size: 16px;
+  color: #fff;
+`;
