fefdc4fc9d857af94fa23798535194a6d7f25be8
eunwoo-levi <eunwoo1341@gmail.com>
2025-05-27 15:12:00 +0900

    feat: Footer 컴포넌트 구현 및 Button컴포넌트 적용

---
+++ src/widgets/footer/ui/Footer.styled.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const FooterContainer = styled.footer`
+  width: 100%;
+  height: 64px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
---
+++ src/widgets/footer/ui/Footer.tsx
@@ -0,0 +1,28 @@
+/** @jsxImportSource @emotion/react */
+
+import { css } from '@emotion/react';
+import Button from '../../../shared/ui/Button';
+import * as S from './Footer.styled';
+
+const ButtonCSS = css`
+  width: 100%;
+  height: 100%;
+  background-color: #000;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: 700;
+  transition: background-color 0.2s ease;
+  &:hover {
+    background-color: #333;
+  }
+`;
+
+export default function Footer() {
+  return (
+    <S.FooterContainer>
+      <Button title='주문 확인' css={ButtonCSS} />
+    </S.FooterContainer>
+  );
+}
