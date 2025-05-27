444ed46b1b747f5f4944e4145a64229d0a10988d
ohgus <anakin1108@naver.com>
2025-05-27 16:54:22 +0900

    style: global css 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import "./App.css";
+import Header from "./components/common/Header/Header";
+import HeaderButton from "./components/common/Header/HeaderButton";
+import { Logo } from "./assets";
+import globalStyles from "./styles/global.styles";
+import { Global } from "@emotion/react";
 
 function App() {
   return (
     <>
+      <Global styles={globalStyles} />
+      <Header>
+        <HeaderButton src={Logo} onClick={() => {}} />
+      </Header>
       <h1>react-shopping-cart</h1>
     </>
   );
