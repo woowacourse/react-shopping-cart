68d8bd8e68e3af9b7e305f58d809e7d865c717a9
dev-dino22 <wlsskdud02@naver.com>
2025-05-27 23:00:12 +0900

    feat: ShoppingCartPage 구현

---
+++ src/pages/shopping-cart/ShoppingCartPage.tsx
@@ -0,0 +1,12 @@
+import { Header } from "../../components/common";
+import CartLayout from "./cart-layout/CartLayout";
+
+const ShoppingCartPage = () => {
+  return (
+    <>
+      <Header title="Shop" />
+      <CartLayout />
+    </>
+  );
+};
+export default ShoppingCartPage;
