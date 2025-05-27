ad5c5290a41500175ce40f978e9ff91ed65903e1
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-05-27 16:09:10 +0900

    :art: style: 모바일 레이아웃 적용 및 폰트 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./reset.css";
+import { Global } from '@emotion/react';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import MobileLayout from './components/common/mobileLayout/MobileLayout.tsx';
+import reset from './global/reset.ts';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Global styles={reset} />
+    <MobileLayout>
+      <App />
+    </MobileLayout>
   </React.StrictMode>
 );
