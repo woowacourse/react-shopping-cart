6462a3155d5991ad7ea50fdfa7c7fafe880e973d
bunju20 <ghkd2271@naver.com>
2025-05-27 17:52:31 +0900

    style: checkBox 상태에 따른 UI 구현

--- src/components/CheckBox/CheckBox.tsx
+++ src/components/CheckBox/CheckBox.tsx
@@ -3,7 +3,7 @@ import * as S from "./CheckBox.styled";
 function CheckBox({ isChecked }: { isChecked: boolean }) {
   return (
     <S.CheckBoxWrapper>
-      <S.Input type="checkbox" id="select-all" />
+      <S.Input checked={isChecked} type="checkbox" id="select-all" />
       <S.Label htmlFor="select-all">전체 선택</S.Label>
     </S.CheckBoxWrapper>
   );
