928799b3f5c3b1c4af837a56fe3293aafc50dac6
H0ngJu <hongju4385@gmail.com>
2025-05-28 19:45:42 +0900

    refactor(OrderConfirmPage): 주문 확인 페이지 구현
    Co-authored-by: mlnwns <mlnwns@users.noreply.github.com>

---
+++ src/pages/OrderConfirmPage.tsx
@@ -0,0 +1,57 @@
+import { css } from "@emotion/css";
+import Header from "../components/@common/Header/Header";
+import Text from "../components/@common/Text/Text";
+import { useCartItemContext } from "../contexts/useCartItemContext";
+import { useNavigate } from "react-router";
+import ConfirmButton from "../components/@common/Button/ConfirmButton/ConfirmButton";
+
+const OrderConfirmPage = () => {
+  const { selectedItem, totalPrice } = useCartItemContext();
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className={OrderConfirmPageStyles}>
+        <Header
+          leading="./back-icon.png"
+          onLeadingClick={() => {
+            navigate("/");
+          }}
+        />
+        <section className={ContentStyle}>
+          <Text text="주문 확인" type="large" />
+          <section className={Description}>
+            <Text text={`총 ${selectedItem.size}개의 상품을 주문합니다.`} />
+            <Text text="최종 결제 금액을 확인해 주세요." />
+          </section>
+          <Text text="총 결제 금액" type="medium" />
+          <Text text={`${totalPrice.toLocaleString()}원`} type="large" />
+        </section>
+        <ConfirmButton text="주문하기" onClick={() => {}} disabled={true} />
+      </div>
+    </>
+  );
+};
+
+export default OrderConfirmPage;
+
+const OrderConfirmPageStyles = css`
+  min-height: 100dvh;
+  background-color: #ffffff;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+`;
+
+const ContentStyle = css`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  align-items: center;
+`;
+
+const Description = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+`;
