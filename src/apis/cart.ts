82cf6b4ecb78ccc017264e710226d4b89258adae
aydenote <dslgpgh@gmail.com>
2025-05-28 17:22:54 +0900

    refactor: 장바구니 API 호출 함수 관심사 분리
    Co-authored-by: Luna <ShinjungOh@users.noreply.github.com>

--- src/apis/cart.ts
+++ src/apis/cart.ts
@@ -1,3 +1,4 @@
+import { CartProduct } from '../types/cart';
 import { BASE_URL, USER_TOKEN } from './env';
 
 export const getCartItems = async () => {
@@ -8,10 +9,56 @@ export const getCartItems = async () => {
         'content-type': 'application/json',
       },
     });
-    const data = await response.json();
-
-    return data.content;
+    return await response.json();
   } catch (error) {
     throw new Error('장바구니 목록을 불러오는 중 에러 발생');
+  }
+};
+
+export const patchIncreaseQuantity = async (cartItem: CartProduct) => {
+  try {
+    await fetch(`${BASE_URL}/cart-items/${cartItem.id}`, {
+      method: 'PATCH',
+      headers: {
+        Authorization: `Basic ${USER_TOKEN}`,
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        quantity: cartItem.quantity + 1,
+      }),
+    });
+  } catch (error) {
+    throw new Error('장바구니 상품 증가시 오류 발생');
+  }
+};
+
+export const patchDecreaseQuantity = async (cartItem: CartProduct) => {
+  try {
+    await fetch(`${BASE_URL}/cart-items/${cartItem.id}`, {
+      method: 'PATCH',
+      headers: {
+        Authorization: `Basic ${USER_TOKEN}`,
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        quantity: cartItem.quantity - 1,
+      }),
+    });
+  } catch (error) {
+    throw new Error('장바구니 상품 감소시 오류 발생');
+  }
+};
+
+export const removeCartItem = async (cartItem: CartProduct) => {
+  try {
+    await fetch(`${BASE_URL}/cart-items/${cartItem.id}`, {
+      method: 'DELETE',
+      headers: {
+        Authorization: `Basic ${USER_TOKEN}`,
+        'content-type': 'application/json',
+      },
+    });
+  } catch (error) {
+    throw new Error('장바구니 상품 삭제시 오류 발생');
   }
 };
