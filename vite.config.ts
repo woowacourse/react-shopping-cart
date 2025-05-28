522ebb0a10117f7fc16860e8da99b31087f0aa08
keemsebeen <sebin0580@gmail.com>
2025-05-28 20:37:28 +0900

    chore: vitest config, jest config 설정
    Co-Authored-By: YEJI <91647696+yeji0214@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
+// eslint-disable-next-line no-restricted-exports
 export default defineConfig({
   plugins: [react({ jsxImportSource: '@emotion/react' })],
   resolve: {
