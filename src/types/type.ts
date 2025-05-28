ac77a814f42bd880ba51f6032cf2da6afaa107cc
H0ngJu <hongju4385@gmail.com>
2025-05-28 15:40:47 +0900

    refactor(type): CartItem, Product 타입 재정의
    Co-authored-by: mlnwns <mlnwns@users.noreply.github.com>

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,8 +1,16 @@
 export interface CartItem {
   id: string;
-  imgUrl: string;
-  name: string;
-  price: number;
+  product: Product;
   quantity: number;
   isSelected: boolean;
+}
+
+type CATEGORY = "패션잡화" | "식료품";
+
+export interface Product {
+  id: number;
+  category: CATEGORY;
+  imageUrl: string;
+  name: string;
+  price: number;
 }
