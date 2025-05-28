5746b2c666f3232aae4243e5e28958fd633d6bf9
keemsebeen <sebin0580@gmail.com>
2025-05-28 16:33:41 +0900

    refactor: CartPage 컴포넌트에 funnel 추가
    Co-Authored-By: YEJI <91647696+yeji0214@users.noreply.github.com>

--- src/features/Cart/pages/CartPage.tsx
+++ src/features/Cart/pages/CartPage.tsx
@@ -1,81 +1,23 @@
-import { css } from '@emotion/react';
+import { STEPS } from '@/shared/constants/setStep';
+import { ShoppingContext } from '@/shared/context/shoppingContext';
 
-import { AppLayout } from '@/shared/components/AppLayout';
-import { Button } from '@/shared/components/Button';
-import { CheckBox } from '@/shared/components/CheckBox';
-import { Flex } from '@/shared/components/Flex';
-import { Header } from '@/shared/components/Header';
-import { Text } from '@/shared/components/Text';
-
-import { CartItemDetail } from '../components/CartItemDetail';
-import { PriceSummary } from '../components/PriceSummary';
-import { CartListContainer } from '../container/CartListContainer';
-import { useCart } from '../hooks/useCart';
+import { useFunnel } from '../../../shared/hooks/useFunnel';
+import { CartInfo } from '../components/CartInfo';
+import { OrderConfirm } from '../components/OrderConfirm';
 
 export const CartPage = () => {
-  const { cartItems, toggleCheck, toggleAllCheck, removeCartItem, updateQuantity } = useCart();
-
-  const allChecked = cartItems?.every((item) => item.isChecked);
-  // const productItems = cartItems?.map((item) => item.product);
+  const { Funnel, setStep } = useFunnel<STEPS>('장바구니');
 
   return (
-    <AppLayout>
-      <Header
-        left={
-          <Text type="Heading" weight="semibold" color="white">
-            SHOP
-          </Text>
-        }
-      />
-      <Flex
-        direction="column"
-        justifyContent="flex-start"
-        alignItems="flex-start"
-        gap="10px"
-        width="100%"
-        padding="20px 20px 10px 20px"
-      >
-        <Text type="Heading" weight="semibold">
-          장바구니
-        </Text>
-        <Text type="Caption" weight="regular">
-          현재 2종류의 상품이 담겨있습니다.
-        </Text>
-        <Flex
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
-          gap="10px"
-          margin="10px 0 0 0"
-        >
-          <CheckBox checked={allChecked} onClick={toggleAllCheck} />
-          <Text type="Caption" weight="regular">
-            전체선택
-          </Text>
-        </Flex>
-      </Flex>
-      <CartListContainer>
-        {cartItems?.map((item) => (
-          <CartItemDetail
-            key={item.id}
-            onToggle={toggleCheck}
-            onRemove={removeCartItem}
-            onUpdateQuantity={updateQuantity}
-            {...item}
-          />
-        ))}
-      </CartListContainer>
-      <PriceSummary cartItems={cartItems ?? []} />
-      <Button
-        width="100%"
-        size="xl"
-        shape="square"
-        css={css`
-          position: sticky;
-        `}
-      >
-        주문 확인
-      </Button>
-    </AppLayout>
+    <ShoppingContext>
+      <Funnel>
+        <Funnel.Step name="장바구니">
+          <CartInfo onNext={() => setStep('주문정보')} />
+        </Funnel.Step>
+        <Funnel.Step name="주문정보">
+          <OrderConfirm onPrev={() => setStep('장바구니')} />
+        </Funnel.Step>
+      </Funnel>
+    </ShoppingContext>
   );
 };
