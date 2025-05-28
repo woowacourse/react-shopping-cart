c0612f17acd6d7ccaf4493288b46f42d21efcb3a
hanheel <hanhee630630@gmail.com>
2025-05-28 15:46:10 +0900

    feat: 전체 체크 및 각 체크박스 체크 연결

--- src/components/features/cartItem/CartItem.tsx
+++ src/components/features/cartItem/CartItem.tsx
@@ -1,25 +1,32 @@
-import { deleteCartItem } from '../../../services/cartService';
-import Checkbox from '../../@common/checkbox/Checkbox';
-import CountButton from '../countButton/CountButton';
-import * as S from './CartItem.styles';
-import { CartItemType } from '../../../types/response';
+import { deleteCartItem } from "../../../services/cartService";
+import Checkbox from "../../@common/checkbox/Checkbox";
+import CountButton from "../countButton/CountButton";
+import * as S from "./CartItem.styles";
+import type { CartItemType } from "../../../types/response";
 
 interface CartItemProps {
   cartData: CartItemType;
   updateCartItem: (cartId: number) => void;
   increaseCartItem: (cartId: number, quantity: number) => void;
+  justifyIsChecked: (cartId: number) => boolean;
+  controlCheckBox: (cartId: number) => void;
 }
 
 const CartItem = ({
   cartData,
   updateCartItem,
   increaseCartItem,
+  justifyIsChecked,
+  controlCheckBox,
 }: CartItemProps) => {
   return (
     <>
       <div css={S.cartItemWrapper}>
         <div css={S.cartItemController}>
-          <Checkbox checked={false} />
+          <Checkbox
+            checked={justifyIsChecked(cartData.id)}
+            onChange={() => controlCheckBox(cartData.id)}
+          />
           <button onClick={() => deleteCartItem(cartData.id)}>삭제</button>
         </div>
         <div key={cartData.id} css={S.cartItemStyle}>
--- src/pages/cartPage/CartPage.tsx
+++ src/pages/cartPage/CartPage.tsx
@@ -1,21 +1,45 @@
-import * as S from './CartPage.style';
-import { Title, Subtitle } from '../../styles/@common/title/Title.styles';
-import CartItem from '../../components/features/cartItem/CartItem';
-import CartPrice from '../../components/features/cartPrice/CartPrice';
+import * as S from "./CartPage.style";
+import { Title, Subtitle } from "../../styles/@common/title/Title.styles";
+import CartItem from "../../components/features/cartItem/CartItem";
+import CartPrice from "../../components/features/cartPrice/CartPrice";
 import {
   deleteCartItem,
   getCart,
   modifyCartItem,
-} from '../../services/cartService';
-import { useEffect, useState } from 'react';
-import Checkbox from '../../components/@common/checkbox/Checkbox';
-import { CartItemType } from '../../types/response';
+} from "../../services/cartService";
+import { useEffect, useState } from "react";
+import Checkbox from "../../components/@common/checkbox/Checkbox";
+import type { CartItemType } from "../../types/response";
 
 const CartPage = () => {
   const [cartData, setCartData] = useState<CartItemType[]>([]);
+  const [isCheckedArray, setIsCheckedArray] = useState<number[]>([]);
+
+  const isAllChecked = isCheckedArray.length === cartData.length;
+
+  const justifyIsChecked = (cartId: number) => {
+    const isChecked = isCheckedArray.includes(cartId);
+    return isChecked;
+  };
+
+  const controlCheckBox = (cartId: number) => {
+    if (justifyIsChecked(cartId)) {
+      setIsCheckedArray(isCheckedArray.filter((id) => id !== cartId));
+      return;
+    }
+
+    setIsCheckedArray([...isCheckedArray, cartId]);
+  };
+
+  const controlAllCheckBox = () => {
+    if (isAllChecked) {
+      setIsCheckedArray([]);
+      return;
+    }
+    setIsCheckedArray(cartData.map((item) => item.id));
+  };
 
   const updateCartItem = async (cartId: number) => {
-    console.log(cartData);
     const cartItem = cartData.find((item) => item.id === cartId);
     if (!cartItem) {
       return;
@@ -24,7 +48,6 @@ const CartPage = () => {
       await removeCartItem(cartItem.id);
       return;
     }
-    console.log('클릭됨');
     await increaseCartItem(cartItem.id, cartItem.quantity - 1);
   };
 
@@ -56,7 +79,7 @@ const CartPage = () => {
         <p css={Subtitle}>현재 {cartData.length}종류의 상품이 담겨있습니다.</p>
       </div>
       <div css={S.CartCheckboxContainer}>
-        <Checkbox checked={false} />
+        <Checkbox checked={isAllChecked} onChange={controlAllCheckBox} />
         <p>전체 선택</p>
       </div>
       {cartData.map((item) => (
@@ -65,6 +88,8 @@ const CartPage = () => {
           cartData={item}
           updateCartItem={updateCartItem}
           increaseCartItem={increaseCartItem}
+          justifyIsChecked={justifyIsChecked}
+          controlCheckBox={controlCheckBox}
         />
       ))}
       <CartPrice />
