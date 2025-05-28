9ca77b4a8380041e3a79dc9b486d5884e3b652dc
장정안 <wjddks96@naver.com>
2025-05-28 17:23:39 +0900

    feat: 라우터  App 내 삽입

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import GlobalStyle from "./styles/GlobalStyles";
-import { Global } from "@emotion/react";
-import Layout from "./pages/layout/Layout";
-import { RouterProvider } from "react-router-dom";
-import router from "./router/routes";
+import GlobalStyle from './styles/GlobalStyles';
+import { Global } from '@emotion/react';
+import { RouterProvider } from 'react-router-dom';
+import router from './router/routes';
 
 function App() {
   return (
     <>
       <Global styles={GlobalStyle} />
       <RouterProvider router={router} />
-      <Layout />
     </>
   );
 }
