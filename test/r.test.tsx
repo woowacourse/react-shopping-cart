7765efbcb460e3410caac91353a25b11fd329c62
keemsebeen <sebin0580@gmail.com>
2025-05-28 20:38:53 +0900

    chore: 불필요한 코드 정리
    Co-Authored-By: YEJI <91647696+yeji0214@users.noreply.github.com>

--- test/r.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from '../src/App';
-
-describe('RTL Test', () => {
-  it('should render', () => {
-    render(<App />);
-    expect(screen.getByText('react-shopping-cart')).toBeInTheDocument();
-  });
-});
