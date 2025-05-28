56170df128ba9c8164e0d559f83fccb45d6790ef
sooyeoniya <tndus1306403@naver.com>
2025-05-28 18:12:43 +0900

    feat: 장바구니에 상품이 없는 경우 Fallback UI 추가

---
+++ src/components/Fallback/Empty/EmptyFallback.tsx
@@ -0,0 +1,14 @@
+import Description from "../../Description/Description";
+import EmptyCart from "/planet-empty-cart.png";
+import * as S from "./EmptyFallback.styles";
+
+const EmptyFallback = () => {
+  return (
+    <S.EmptyFallback>
+      <S.EmptyFallbackImage src={EmptyCart} alt="empty-cart" />
+      <Description>장바구니에 담은 상품이 없습니다.</Description>
+    </S.EmptyFallback>
+  );
+};
+
+export default EmptyFallback;
