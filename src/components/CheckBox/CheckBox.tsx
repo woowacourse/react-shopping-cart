134eea358f59d45c800d1445a8aa3ee0efe13f32
mun-kyeong <horse6953@naver.com>
2025-05-28 13:57:19 +0900

    feat: CheckBox 컴포넌트의 스타일 import 수정 및 id prop 추가
    Co-authored-by: kimyou1102 <yoosk5485@naver.com>

--- src/components/CheckBox/CheckBox.tsx
+++ src/components/CheckBox/CheckBox.tsx
@@ -2,8 +2,9 @@ import { CheckBoxLayout } from "./CheckBox.style";
 
 interface CheckBoxProps {
   isChecked: boolean;
+  id?: string;
 }
 
-export function CheckBox({ isChecked = false }: CheckBoxProps) {
-  return <input type="checkbox" css={CheckBoxLayout} />;
+export function CheckBox({ isChecked = false, id }: CheckBoxProps) {
+  return <input type="checkbox" css={CheckBoxLayout} id={id} />;
 }
