a81b63a06f93ea82e037d2c2a267b4557058fe3d
hoyyChoi <hoyeon8165@gmail.com>
2025-05-29 03:24:22 +0900

    feat: OrderConfirmPage 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import * as S from "./styles/Layout.styled";
+import { BrowserRouter, Route, Routes } from "react-router";
 import Header from "./components/Header";
 import CartPage from "./pages/CartPage";
-import * as S from "./styles/Layout.styled";
+import OrderConfirmPage from "./pages/OrderConfirmPage";
 
 function App() {
   return (
     <S.Layout>
       <Header />
-      <CartPage />
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <Routes>
+          <Route path="/" element={<CartPage />} />
+          <Route path="/order-confirm" element={<OrderConfirmPage />} />
+        </Routes>
+      </BrowserRouter>
     </S.Layout>
   );
 }
