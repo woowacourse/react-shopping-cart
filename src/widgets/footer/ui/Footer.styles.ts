1d2d2003a0e9b138588577bede8735f3fe025abf
eunwoo-levi <eunwoo1341@gmail.com>
2025-05-28 20:02:53 +0900

    chore: CartPageFooter 파일 경로 이동

--- src/widgets/footer/ui/Footer.styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-export const FooterContainer = styled.footer`
-  width: 100%;
-  height: 64px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
