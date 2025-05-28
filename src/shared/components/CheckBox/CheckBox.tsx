ee4eb57f547e89b8ffcc4832fc339480bd63ed42
Oliy <eunsoa0310@gmail.com>
2025-05-28 15:46:24 +0900

    feat: 장바구니 체크박스 스타일 수정
    Co-authored-by: 우혁 <woogur29@gmail.com>

--- src/shared/components/CheckBox/CheckBox.tsx
+++ src/shared/components/CheckBox/CheckBox.tsx
@@ -1,6 +1,8 @@
+import { ComponentProps } from 'react';
+
 type CheckBoxProps = {
   isChecked: boolean;
-};
+} & ComponentProps<'button'>;
 
 const palette = {
   checked: {
@@ -15,31 +17,39 @@ const palette = {
   },
 };
 
-export default function CheckBox({ isChecked }: CheckBoxProps) {
+export default function CheckBox({ isChecked, ...rest }: CheckBoxProps) {
   const checked = isChecked ? 'checked' : 'unchecked';
 
   return (
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect
-        x="0.5"
-        y="0.5"
-        width="23"
-        height="23"
-        rx="7.5"
-        fill={palette[checked].background}
-        stroke={palette[checked].stroke}
-      />
-      <g clipPath="url(#clip0_876_282)">
-        <path
-          d="M9 16.17L4.83 12L3.41 13.41L9 19L21 7.00003L19.59 5.59003L9 16.17Z"
-          fill={palette[checked].check}
+    <button type="button" {...rest}>
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect
+          x="0.5"
+          y="0.5"
+          width="23"
+          height="23"
+          rx="7.5"
+          fill={palette[checked].background}
+          stroke={palette[checked].stroke}
         />
-      </g>
-      <defs>
-        <clipPath id="clip0_876_282">
-          <rect width="24" height="24" fill="white" />
-        </clipPath>
-      </defs>
-    </svg>
+        <g clipPath="url(#clip0_876_282)">
+          <path
+            d="M9 16.17L4.83 12L3.41 13.41L9 19L21 7.00003L19.59 5.59003L9 16.17Z"
+            fill={palette[checked].check}
+          />
+        </g>
+        <defs>
+          <clipPath id="clip0_876_282">
+            <rect width="24" height="24" fill="white" />
+          </clipPath>
+        </defs>
+      </svg>
+    </button>
   );
 }
