2816600e6e1eadbdf3a70b3b0b2702137fb3f00f
hoyyChoi <hoyeon8165@gmail.com>
2025-05-28 15:56:36 +0900

    design: css 초기화

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+import "./styles/reset.css";
 
 async function enableMocking() {
   const { worker } = await import("./apis/mocks/browser");
