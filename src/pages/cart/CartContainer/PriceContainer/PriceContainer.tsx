3c90284ac28ed8d88ee0115c7aa8f7a61cf3482c
Oliy <eunsoa0310@gmail.com>
2025-05-28 18:04:06 +0900

    feat: PriceContainer 컴포넌트 추가
    Co-authored-by: 우혁 <woogur29@gmail.com>

---
+++ src/pages/cart/CartContainer/PriceContainer/PriceContainer.tsx
@@ -0,0 +1,40 @@
+import * as S from './PriceContainer.styled';
+import InfoIcon from '@assets/icons/info.svg';
+
+const FREE_DELIVERY_LIMIT = 100_000;
+
+type PriceContainerProps = {
+  orderTotalPrice: number;
+};
+
+export default function PriceContainer({ orderTotalPrice }: PriceContainerProps) {
+  const deliveryPrice = orderTotalPrice >= FREE_DELIVERY_LIMIT ? 0 : 3000;
+  const paymentPrice = orderTotalPrice + deliveryPrice;
+
+  return (
+    <S.Container>
+      <S.InfoContainer>
+        <S.InfoIcon src={InfoIcon} alt="배송비 무료 안내" />
+        <S.InfoText>
+          총 주문 금액이 {FREE_DELIVERY_LIMIT.toLocaleString()}원 이상일 경우 무료 배송됩니다.
+        </S.InfoText>
+      </S.InfoContainer>
+      <S.PriceBox>
+        <S.PriceTextBox>
+          <S.PriceTitle>주문 금액</S.PriceTitle>
+          <S.PriceText>{orderTotalPrice.toLocaleString()}원</S.PriceText>
+        </S.PriceTextBox>
+        <S.PriceTextBox>
+          <S.PriceTitle>배송비</S.PriceTitle>
+          <S.PriceText>{deliveryPrice.toLocaleString()}원</S.PriceText>
+        </S.PriceTextBox>
+      </S.PriceBox>
+      <S.TotalPriceBox>
+        <S.PriceTextBox>
+          <S.PriceTitle>총 결제 금액</S.PriceTitle>
+          <S.PriceText>{paymentPrice.toLocaleString()}원</S.PriceText>
+        </S.PriceTextBox>
+      </S.TotalPriceBox>
+    </S.Container>
+  );
+}
