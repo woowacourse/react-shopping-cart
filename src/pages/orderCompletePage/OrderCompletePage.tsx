24d4c9301e42cf81c384acb837dc7d3220aef1fb
장정안 <wjddks96@naver.com>
2025-05-28 17:24:26 +0900

    feat: 스타일 코드 분리

--- src/pages/orderCompletePage/OrderCompletePage.tsx
+++ src/pages/orderCompletePage/OrderCompletePage.tsx
@@ -1,52 +1,25 @@
-import { css } from "@emotion/react";
+import { useLocation } from 'react-router-dom';
+import * as S from './OrderCompletePage.styles';
 
 const OrderCompletePage = () => {
-  return (
-    <div css={OrderCompleteWrapper}>
-      <div css={OrderCompleteTitle}>주문완료</div>
-      <div css={OrderCompleteDescription}>설명입니다</div>
+  const { state } = useLocation();
+  const { productTypeCount, totalPrice, totalProductCount } = state;
 
-      <div css={OrderCompletePriceContainer}>
-        <div css={OrderCompleteSubtitle}>주문금액</div>
-        <div css={OrderCompleteTotalPrice}>100,000원</div>
+  return (
+    <div css={S.OrderCompleteWrapper}>
+      <div css={S.OrderCompleteTitle}>주문완료</div>
+      <div css={S.OrderCompleteDescription}>
+        총 {productTypeCount}종류의 상품 {totalProductCount}개를 주문합니다.
+      </div>
+
+      <div css={S.OrderCompletePriceContainer}>
+        <div css={S.OrderCompleteSubtitle}>주문금액</div>
+        <div css={S.OrderCompleteTotalPrice}>
+          {totalPrice.toLocaleString()}원
+        </div>
       </div>
     </div>
   );
 };
-
-const OrderCompleteWrapper = css`
-  display: flex;
-  flex-direction: column;
-  gap: 2.4rem;
-  align-items: center;
-  justify-content: center;
-`;
-
-const OrderCompleteTitle = css`
-  font-size: var(--font-size-title);
-  font-weight: var(--font-weight-title);
-`;
-
-const OrderCompleteDescription = css`
-  font-size: var(--font-size-body);
-  font-weight: var(--font-weight-body);
-`;
-
-const OrderCompleteSubtitle = css`
-  font-size: var(--font-size-subtitle);
-  font-weight: var(--font-weight-subtitle);
-`;
-
-const OrderCompletePriceContainer = css`
-  display: flex;
-  flex-direction: column;
-  gap: 1.2rem;
-  align-items: center;
-`;
-
-const OrderCompleteTotalPrice = css`
-  font-size: var(--font-size-title);
-  font-weight: var(--font-weight-title);
-`;
 
 export default OrderCompletePage;
