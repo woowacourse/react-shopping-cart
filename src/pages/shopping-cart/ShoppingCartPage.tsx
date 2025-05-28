3bff7f7cf73987353bba3851e53f858971350fd0
dev-dino22 <wlsskdud02@naver.com>
2025-05-28 21:45:07 +0900

    feat: ShoppingCartPage에 OrderListProvider 적용
    Co-Authored-By: Youjeong Jeong <115888336+jeongyou@users.noreply.github.com>

--- src/pages/shopping-cart/ShoppingCartPage.tsx
+++ src/pages/shopping-cart/ShoppingCartPage.tsx
@@ -1,14 +1,15 @@
 import styled from "@emotion/styled";
 import { Header } from "../../components/common";
 import CartLayout from "./cart-layout/CartLayout";
+import { OrderListProvider } from "./context/OrderListProvider";
 
 const ShoppingCartPage = () => {
   return (
-    <>
+    <OrderListProvider>
       <Header title="Shop" />
       <CartLayout />
       <CheckoutButton>주문 확인</CheckoutButton>
-    </>
+    </OrderListProvider>
   );
 };
 
