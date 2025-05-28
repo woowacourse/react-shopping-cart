3bc245ea9e1fcfba4583e6695716b23e7968254d
Oliy <eunsoa0310@gmail.com>
2025-05-28 14:44:02 +0900

    feat: useFetchData 및 useMutation 훅 추가
    Co-authored-by: 우혁 <woogur29@gmail.com>

---
+++ src/shared/hooks/useFetchData.ts
@@ -0,0 +1,35 @@
+import { useState, useEffect, useCallback } from 'react';
+
+type UseFetchDataParam<T> = {
+  fetchFn: () => Promise<T>;
+};
+
+const useFetchData = <T>({ fetchFn }: UseFetchDataParam<T>) => {
+  const [data, setData] = useState<T | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  const fetchData = useCallback(async () => {
+    setIsLoading(true);
+    setErrorMessage(null);
+
+    try {
+      const data = await fetchFn();
+      setData(data);
+    } catch (error) {
+      if (error instanceof Error) {
+        setErrorMessage(error.message);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return { data, isLoading, errorMessage, setErrorMessage, refetch: fetchData };
+};
+
+export default useFetchData;
