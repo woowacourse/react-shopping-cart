ee1ed5195f40e0763955ddb5362fac4a33ab31a1
sooyeoniya <tndus1306403@naver.com>
2025-05-28 14:49:47 +0900

    chore: delete와 patch 함수명 변경
    복수 -> 단수

--- src/apis/cartItems/patchCartItem.ts
+++ src/apis/cartItems/patchCartItem.ts
@@ -8,7 +8,7 @@ interface Props {
 const ERROR_MESSAGE =
   "장바구니에서 상품의 수량을 조절하던 중 에러가 발생했습니다.";
 
-export const patchCartItems = async ({ cartId, quantity }: Props) => {
+export const patchCartItem = async ({ cartId, quantity }: Props) => {
   const response = await httpClient.patch(`/cart-items/${cartId}`, {
     id: cartId,
     quantity,
