a24c65293630af60e71d111075b8b0e99daa088b
ohgus <anakin1108@naver.com>
2025-05-28 14:52:16 +0900

    feat: cartItem 컴포넌트 구현 마무리
    Co-authored-by: thgml05 <aisohii@naver.com>

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import globalStyles from './styles/global.styles';
 import { Global } from '@emotion/react';
 import ContainerLayout from './components/common/ContainerLayout/ContainerLayout';
 import CartListTitle from './components/CartListTitle/CartListTitle';
+import CartItem from './components/CartItem/CartItem';
 
 function App() {
   const response = fetch(`${import.meta.env.VITE_API_BASE_URL}/cart-items`, {
@@ -21,6 +22,7 @@ function App() {
       </Header>
       <ContainerLayout>
         <CartListTitle />
+        <CartItem />
       </ContainerLayout>
     </>
   );
--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -1,3 +1,4 @@
+import { Minus, Plus } from '../../assets';
 import Text from '../common/Text/Text';
 import {
   CartInfo,
@@ -16,17 +17,24 @@ function CartItem() {
     <li css={CartItemStyle}>
       <div css={ListItemHeaderStyle}>
         <input type="checkbox" name="" id="" css={CheckboxStyle} />
-        <button css={DeleteButtonStyle}></button>
+        <button css={DeleteButtonStyle}>
+          <Text varient="caption">삭제</Text>
+        </button>
       </div>
       <div css={CartItemBodyStyle}>
         <img css={ImageStyle} src="" alt="" />
+
         <div css={CartInfo}>
-          <Text></Text>
-          <Text></Text>
+          <Text varient="caption">해삐</Text>
+          <Text varient="title">10,000원</Text>
           <div css={ControllerBox}>
-            <button css={ControllerButton}></button>
-            <Text></Text>
-            <button css={ControllerButton}></button>
+            <button css={ControllerButton}>
+              <img src={Minus} alt="minus" />
+            </button>
+            <Text varient="caption">1</Text>
+            <button css={ControllerButton}>
+              <img src={Plus} alt="plus" />
+            </button>
           </div>
         </div>
       </div>
