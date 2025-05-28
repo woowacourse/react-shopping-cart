11030f11e760179c6b4af1441c8b63751f7522f0
dev-dino22 <wlsskdud02@naver.com>
2025-05-28 14:51:09 +0900

    feat: page에 주문 확인 버튼 추가
    Co-Authored-By: Youjeong Jeong <115888336+jeongyou@users.noreply.github.com>

--- src/pages/shopping-cart/ShoppingCartPage.tsx
+++ src/pages/shopping-cart/ShoppingCartPage.tsx
@@ -1,3 +1,4 @@
+import styled from "@emotion/styled";
 import { Header } from "../../components/common";
 import CartLayout from "./cart-layout/CartLayout";
 
@@ -6,7 +7,23 @@ const ShoppingCartPage = () => {
     <>
       <Header title="Shop" />
       <CartLayout />
+      <CheckoutButton>주문 확인</CheckoutButton>
     </>
   );
 };
+
 export default ShoppingCartPage;
+
+const CheckoutButton = styled.button`
+  position: sticky;
+  bottom: 0;
+  width: 100%;
+  padding: 16px;
+  background-color: #333;
+  color: white;
+  cursor: pointer;
+  border-radius: 0px;
+  &:hover {
+    background-color: #555;
+  }
+`;
