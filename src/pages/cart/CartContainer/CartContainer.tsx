aad1127884413bda5a3d6ce849c346eaacabeab7
Oliy <eunsoa0310@gmail.com>
2025-05-28 15:46:58 +0900

    feat: 장바구니 전체 선택 기능 추가
    Co-authored-by: 우혁 <woogur29@gmail.com>

--- src/pages/cart/CartContainer/CartContainer.tsx
+++ src/pages/cart/CartContainer/CartContainer.tsx
@@ -2,6 +2,7 @@ import * as S from './CartContainer.styled';
 import CheckBox from '@/shared/components/CheckBox/CheckBox';
 import CartList from './CartList/CartList';
 import { CartItemType } from '@/apis/cartItems/cartItem.type';
+import { useState } from 'react';
 
 type CartContainerProps = {
   cartItems: CartItemType[];
@@ -9,13 +10,14 @@ type CartContainerProps = {
 };
 
 export default function CartContainer({ cartItems, refetchCartItems }: CartContainerProps) {
-  const isAllChecked = true;
+  const [orderList, setOrderList] = useState<CartItemType[]>(cartItems);
+  const isAllChecked = orderList.every((item, idx) => item.id === cartItems[idx].id);
 
   return (
     <S.Container>
       <S.Text>현재 {cartItems.length}종류의 상품이 담겨있습니다.</S.Text>
       <S.AllCheckBox>
-        <CheckBox isChecked={isAllChecked} />
+        <CheckBox isChecked={isAllChecked} onClick={() => setOrderList(cartItems)} />
         <S.Text>전체 선택</S.Text>
       </S.AllCheckBox>
       <CartList cartItems={cartItems} refetchCartItems={refetchCartItems} />
