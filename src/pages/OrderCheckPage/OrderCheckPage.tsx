553d34b57284cdf0e7700b49225171e553cfa22f
AHHYUNJU <wndkgus0225@naver.com>
2025-05-28 14:34:02 +0900

    feat: OrderCheckPage css 구현
    Co-authored-by: kaori-killer <kaori-killer@users.noreply.github.com>

--- src/pages/OrderCheckPage/OrderCheckPage.tsx
+++ src/pages/OrderCheckPage/OrderCheckPage.tsx
@@ -1,7 +1,15 @@
+import * as S from "./OrderCheckPage.styles";
+
 export default function OrderCheckPage() {
   return (
-    <div>
-      <h1>주문 확인 완료</h1>
-    </div>
+    <S.Container>
+      <S.Title>주문 확인</S.Title>
+      <S.Description>
+        <p>총 2종류의 상품 4개를 주문합니다.</p>
+        <p>최종 결제 금액을 확인해 주세요.</p>
+      </S.Description>
+      <S.PriceInfo>총 결제 금액</S.PriceInfo>
+      <S.TotalPrice>120,000원</S.TotalPrice>
+    </S.Container>
   );
 }
