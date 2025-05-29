26f62dc5b695fcfa08ad673e654e6ea994a8c50b
thgml05 <aisohii@naver.com>
2025-05-29 16:56:29 +0900

    chore: base url 수정

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,16 +3,19 @@ import { createBrowserRouter, RouterProvider } from 'react-router';
 import OrderCheckPage from './pages/OrderCheckPage/OrderCheck';
 import CartPage from './pages/CartPage/CartPage';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <CartPage />,
-  },
-  {
-    path: '/order-check',
-    element: <OrderCheckPage />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <CartPage />,
+    },
+    {
+      path: '/order-check',
+      element: <OrderCheckPage />,
+    },
+  ],
+  { basename: '/react-shopping-cart' }
+);
 
 export default function Route() {
   return <RouterProvider router={router} />;
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
-  base: '/react-shopping-cart/',
+  base: command === 'build' ? '/react-shopping-cart/' : '/',
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
