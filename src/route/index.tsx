0fb76ed846394e2804da8ce6ec1408f7434243e4
minji2219 <aabb0219@naver.com>
2025-05-29 16:27:33 +0900

    chore: 배포 router 설정

--- src/route/index.tsx
+++ src/route/index.tsx
@@ -1,24 +1,29 @@
-import { PATH } from "./path";
-import { createBrowserRouter, RouterProvider } from "react-router";
-import App from "../App";
-import NavBar from "../components/layout/NavBar";
-import Confirm from "../pages/Confirm";
+import {PATH} from './path';
+import {createBrowserRouter, RouterProvider} from 'react-router';
+import App from '../App';
+import NavBar from '../components/layout/NavBar';
+import Confirm from '../pages/Confirm';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      element: <NavBar />,
+      children: [
+        {
+          path: PATH.MAIN,
+          element: <App />,
+        },
+        {
+          path: PATH.CONFIRM,
+          element: <Confirm />,
+        },
+      ],
+    },
+  ],
   {
-    element: <NavBar />,
-    children: [
-      {
-        path: PATH.MAIN,
-        element: <App />,
-      },
-      {
-        path: PATH.CONFIRM,
-        element: <Confirm />,
-      },
-    ],
-  },
-]);
+    basename: '/react-shopping-cart',
+  }
+);
 export default function Router() {
   return <RouterProvider router={router} />;
 }
