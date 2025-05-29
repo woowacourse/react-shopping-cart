d8ef6cb56ea232160405cc5a8bb8a4bec37ba55c
장정안 <wjddks96@naver.com>
2025-05-29 13:36:37 +0900

    feat: Toast 컴포넌트 구현

---
+++ src/components/@common/toast/Toast.styles.ts
@@ -0,0 +1,38 @@
+import { css, keyframes } from '@emotion/react';
+
+const fadeInOut = keyframes`
+  0% {
+    opacity: 0;
+  }
+  10% {
+    opacity: 1;
+  }
+  70% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+
+  }
+`;
+
+export const ToastStyle = ({ isSuccess }: { isSuccess: boolean }) => css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  position: fixed;
+  top: 6.4rem;
+  width: 42.9rem;
+  padding: 1.2rem 7.7rem;
+  background-color: ${isSuccess ? 'var(--color-green)' : 'var(--color-pink)'};
+  border-radius: 8px;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.15);
+  animation: ${fadeInOut} 1.5s ease-out forwards;
+`;
+
+export const ToastMessage = css`
+  font-size: var(--font-size-body);
+  font-weight: var(--font-weight-body);
+  color: var(--color-white);
+`;
