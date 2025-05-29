035456beb2000f6648cfb4fc59b7774bab6ad4a4
ohgus <anakin1108@naver.com>
2025-05-29 17:01:11 +0900

    chore: 배포 세팅

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,16 +3,21 @@ import { createBrowserRouter, RouterProvider } from 'react-router';
 import OrderCheckPage from './pages/OrderCheckPage/OrderCheck';
 import CartPage from './pages/CartPage/CartPage';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <CartPage />,
+    },
+    {
+      path: '/order-check',
+      element: <OrderCheckPage />,
+    },
+  ],
   {
-    path: '/',
-    element: <CartPage />,
-  },
-  {
-    path: '/order-check',
-    element: <OrderCheckPage />,
-  },
-]);
+    basename: '/react-shopping-cart',
+  }
+);
 
 export default function Route() {
   return <RouterProvider router={router} />;
