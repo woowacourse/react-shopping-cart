285e669fbfa7b050c740c67165d717d14f367731
eunwoo-levi <eunwoo1341@gmail.com>
2025-05-29 17:56:27 +0900

    refactor: Navbar 올바른 로직으로 리팩토링

--- src/shared/ui/Navbar.tsx
+++ src/shared/ui/Navbar.tsx
@@ -1,10 +1,32 @@
+/** @jsxImportSource @emotion/react */
+
 import { useNavigate } from 'react-router';
 import styled from '@emotion/styled';
+import Button from './Button';
+import { css } from '@emotion/react';
 
 interface NavbarProps {
   title: string;
   url?: string;
 }
+
+const ButtonCSS = css`
+  background-color: #000;
+  border-radius: 8px;
+  padding: 8px;
+  color: #fff;
+  font-size: 20px;
+  font-weight: 700;
+  transition: scale 0.2s ease, background-color 0.2s ease;
+  &:hover {
+    scale: 1.1;
+  }
+
+  &:disabled {
+    background-color: #bebebe;
+    cursor: not-allowed;
+  }
+`;
 
 export default function Navbar({ title, url }: NavbarProps) {
   const navigate = useNavigate();
@@ -14,7 +36,11 @@ export default function Navbar({ title, url }: NavbarProps) {
     navigate(url);
   };
 
-  return <NavbarContainer onClick={handleClick}>{title}</NavbarContainer>;
+  return (
+    <NavbarContainer onClick={handleClick}>
+      <Button css={ButtonCSS} title={title} onClick={handleClick} />
+    </NavbarContainer>
+  );
 }
 
 const NavbarContainer = styled.nav`
