1f4d75e4ce87a5df14155a0560c2a9aba8c220cf
eunwoo-levi <eunwoo1341@gmail.com>
2025-05-29 17:35:35 +0900

    feat: Lazy import 및 suspense 적용하여 Skeleton UI 적용

--- src/pages/CartPage/App.tsx
+++ src/pages/CartPage/App.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState, Suspense } from 'react';
 import * as S from './App.styles';
-import { CartHeader, CartList, OrderPriceSummary } from '../../features/cart/ui';
+import { CartHeader } from '../../features/cart/ui';
 import Navbar from '../../shared/ui/Navbar';
 import CartPageFooter from '../../features/cart/ui/CartPageFooter';
 import { getCartItems } from '../../features/cart/api/getCartItems';
@@ -9,10 +9,14 @@ import { CartItem } from '../../shared/type/cart';
 import EmptyCartItemUI from '../../features/cart/ui/EmptyCartItemUI';
 import { ROUTES } from '../../shared/constants/routeConstants';
 
+const CartList = React.lazy(() => import('../../features/cart/ui/CartList'));
+const OrderPriceSummary = React.lazy(() => import('../../features/cart/ui/OrderPriceSummary'));
+import CartListSkeleton from '../../features/cart/ui/CartListSkeleton';
+
 function App() {
   const { addAllCartItemsInSelected, selectedCartItems } = useSelectedCartContext();
-
   const [cartItems, setCartItems] = useState<CartItem[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchCartItems = async () => {
@@ -26,6 +30,8 @@ function App() {
           console.error('Failed to fetch cart items:', error.message);
           alert('장바구니 아이템을 불러오는 데 실패했습니다. 다시 시도해주세요.');
         }
+      } finally {
+        setLoading(false);
       }
     };
     fetchCartItems();
@@ -36,20 +42,21 @@ function App() {
       <Navbar title={'SHOP'} url={ROUTES.ROOT} />
       <S.AppContent>
         <CartHeader cartTypeQuantity={cartItems.length} />
-        {cartItems.length > 0 ? (
-          <>
+        {loading ? (
+          <CartListSkeleton />
+        ) : cartItems.length > 0 ? (
+          <Suspense fallback={<CartListSkeleton />}>
             <CartList
               cartItems={cartItems}
               setCartItems={setCartItems}
               addAllCartItemsInSelected={addAllCartItemsInSelected}
             />
             <OrderPriceSummary />
-          </>
+          </Suspense>
         ) : (
           <EmptyCartItemUI />
         )}
       </S.AppContent>
-
       <CartPageFooter cartItemQuantity={cartItems.length} />
     </S.AppContainer>
   );
