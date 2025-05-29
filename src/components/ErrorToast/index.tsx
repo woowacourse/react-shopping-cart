e6d2ad5a8f80f845f73aca3d8fc491e564a61cf3
spoyodevelop <kjy9505@gmail.com>
2025-05-29 12:46:26 +0900

    refactor: ErrorToast 컴포넌트의 구조 개선 및 불필요한 상태 관리 제거
    Co-authored-by: 신종욱 <66900221+dlsxjzld@users.noreply.github.com>

--- src/components/ErrorToast/index.tsx
+++ src/components/ErrorToast/index.tsx
@@ -1,44 +1,18 @@
-import { useState, useEffect } from "react";
 import * as styles from "./ErrorToast.style";
 import { createPortal } from "react-dom";
 
 export default function ErrorToast({
   message,
   onClose,
-  duration = 2000,
 }: {
   message: string;
   onClose: () => void;
-  duration?: number;
 }) {
-  const [visible, setVisible] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setVisible(false);
-
-      setTimeout(onClose, 300);
-    }, duration);
-
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [duration, onClose]);
-
-  const handleClose = () => {
-    setVisible(false);
-    setTimeout(onClose, 300);
-  };
-
   return createPortal(
-    <div css={[styles.toastCss, !visible && styles.fadeOutSmoothCss]}>
-      <h2 css={styles.messageCss} id="error-toast-message">
-        {message}
-      </h2>
-      <button css={styles.closeButtonCss} onClick={handleClose}>
-        ✕
-      </button>
-    </div>,
+    <styles.Toast>
+      <styles.Message>{message}</styles.Message>
+      <styles.CloseButton onClick={onClose}>✕</styles.CloseButton>
+    </styles.Toast>,
     document.body
   );
 }
