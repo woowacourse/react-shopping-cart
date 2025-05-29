0f260a316faf81895ba7f5b0e88225e138e9a93e
mlnwns <mlnwns@naver.com>
2025-05-29 15:58:48 +0900

    chore: github-pages 배포 설정

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,16 +2,21 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 import App from "../App";
 import OrderConfirmPage from "../pages/OrderConfirmPage";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+    },
+    {
+      path: "/order-confirm",
+      element: <OrderConfirmPage />,
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/order-confirm",
-    element: <OrderConfirmPage />,
-  },
-]);
+    basename: "/react-shopping-cart/",
+  }
+);
 
 export default function Router() {
   return <RouterProvider router={router} />;
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import { configDefaults } from "vitest/config";
 
 export default defineConfig({
+  base: "/react-shopping-cart/",
   plugins: [react()],
   test: {
     globals: true,
