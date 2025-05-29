87690f434b9bf053a89e88fbfe056d9a5b351dc3
keemsebeen <sebin0580@gmail.com>
2025-05-29 12:29:36 +0900

    chore: test를 위한 mock 데이터 정의
    Co-Authored-By: YEJI <91647696+yeji0214@users.noreply.github.com>

---
+++ test/Cart.data.ts
@@ -0,0 +1,28 @@
+export const cartItems = [
+  {
+    id: 1,
+    quantity: 2,
+    isChecked: true,
+    product: {
+      id: 1,
+      name: '상품1',
+      category: 'dd',
+      quantity: 10,
+      price: 10000,
+      imageUrl: '/test1.jpg',
+    },
+  },
+  {
+    id: 2,
+    quantity: 1,
+    isChecked: false,
+    product: {
+      id: 2,
+      name: '상품2',
+      price: 20000,
+      quantity: 10,
+      category: 'dd',
+      imageUrl: '/test2.jpg',
+    },
+  },
+];
