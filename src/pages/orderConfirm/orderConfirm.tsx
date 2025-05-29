2263c57cc5e3f462edb396d7fa9cce119da7f820
mun-kyeong <horse6953@naver.com>
2025-05-29 13:14:13 +0900

    feat: 주문 확인 페이지 및 스타일 컴포넌트 추가
    Co-authored-by: Yugyeong Kim <kimyou1102@users.noreply.github.com>

---
+++ src/pages/orderConfirm/orderConfirm.tsx
@@ -0,0 +1,38 @@
+import { useLocation } from "react-router-dom";
+import Header from "../../components/layout/Header/Header";
+import Main from "../../components/layout/Main/Main";
+import { PageLayout } from "../../components/layout/PageLayout/PageLayout";
+import {
+  bodyText,
+  confirmLayout,
+  subtitleText,
+  titleText,
+  totalPriceBox,
+} from "./orderConfirm.style";
+
+export function OrderConfirm() {
+  const { state } = useLocation();
+
+  return (
+    <PageLayout>
+      <Header>
+        <p>SHOP</p>
+      </Header>
+      <Main>
+        <div css={confirmLayout}>
+          <p css={titleText}>주문확인</p>
+          <p css={bodyText}>
+            총 {state.selectedCartType}종류의 상품 {state.selectedCartItem}개를
+            주문합니다. 최종 결제 금액을 확인해 주세요.
+          </p>
+          <div css={totalPriceBox}>
+            <p css={subtitleText}>총 결제금액</p>
+            <p css={titleText}>
+              {Number(state.totalPrice).toLocaleString("ko")}
+            </p>
+          </div>
+        </div>
+      </Main>
+    </PageLayout>
+  );
+}
