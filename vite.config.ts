be35321acefa4575eba286eeac0fef0ffc4a9728
yeongipark <yeongi0111@naver.com>
2025-05-29 15:22:01 +0900

    test: 전체선택 버튼 테스트

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({
       jsxImportSource: "@emotion/react",
     }),
   ],
+  test: {
+    environment: "jsdom",
+    globals: true,
+  },
 });
