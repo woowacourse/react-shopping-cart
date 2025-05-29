34403fdb489028383bd09fc1e16845d902ffaf90
eunwoo-levi <eunwoo1341@gmail.com>
2025-05-29 12:43:15 +0900

    fix: vitest setup 에러 해결
    Co-authored-by: 이상희 <sang6299@naver.com>

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,6 +1,4 @@
-import '@testing-library/jest-dom';
-
 import { expect } from 'vitest';
-import matchers from '@testing-library/jest-dom/matchers';
+import * as matchers from '@testing-library/jest-dom/matchers';
 
 expect.extend(matchers);
