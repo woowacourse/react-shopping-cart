cb1dad178c0e3f432df7816ae40c4ea49e1c1399
ohgus <anakin1108@naver.com>
2025-05-29 16:37:43 +0900

    chore: 주석, console.log 제거
    Co-authored-by: thgml05 <aisohii@naver.com>

--- src/hooks/__tests__/useCartList.test.ts
+++ src/hooks/__tests__/useCartList.test.ts
@@ -5,13 +5,9 @@ import mockCart from '../../mocks/mockCart.json';
 
 describe('useCartList 훅 테스트', () => {
   it('초기 cartList의 상태 값은 목 데이터를 받아오고, isError의 상태 값은 "", isLoading의 상태 값은 false', async () => {
-    // server.use(getMockCartList(mockCart));
-
     const { result } = renderHook(() => useCartList());
 
     await act(async () => null);
-
-    console.log(result.current.cartList);
 
     expect(result.current.cartList).toEqual(mockCart);
     expect(result.current.isError).toEqual('');
