fdcc9fb1ffd75cf59a13f41f5e87e8c06c9c10a1
SuHwa Lee <tnghk9611@naver.com>
2025-05-29 15:00:32 +0900

    refactor: 카트 아이템 내부 로직 커스텀 훅으로 분리

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -1,144 +1,47 @@
 import * as S from "./CartItem.styled";
-
 import QuantityButton from "../QuantityButton/QuantityButton";
 import CheckBox from "../CheckBox/CheckBox";
-import updateCartItemApi from "../../api/updateCartItemApi";
 import { ResponseCartItem } from "../../types/types";
-import { useCartDispatch } from "../../stores/CartContext";
-import {
-  useSelectContext,
-  useSelectDispatch,
-} from "../../stores/SelectContext";
+import useCartItemManager from "../../hooks/useCartItemManager";
 
-function CartItem({ cart }: { cart: ResponseCartItem }) {
+interface CartItemProps {
+  cart: ResponseCartItem;
+}
+
+const CartItem = ({ cart }: CartItemProps) => {
   const { price, name, imageUrl } = cart.product;
-  const dispatch = useCartDispatch();
-  const selectState = useSelectContext();
-  const selectDispatch = useSelectDispatch();
 
-  const handleSelect = (id: number) => {
-    const isSelected = selectState.find((item) => item.id === id)?.selected;
-
-    if (isSelected) {
-      selectDispatch({
-        type: "REMOVE_SELECT",
-        payload: { id },
-      });
-    } else {
-      selectDispatch({
-        type: "ADD_SELECT",
-        payload: { id },
-      });
-    }
-  };
-
-  const handleIncrease = async ({
-    id,
-    quantity,
-  }: {
-    id: number;
-    quantity: number;
-  }) => {
-    try {
-      dispatch({
-        type: "ADD_ITEM_QUANTITY",
-        payload: { id, quantity: quantity + 1 },
-      });
-      await updateCartItemApi(id, quantity + 1);
-    } catch (error) {
-      dispatch({
-        type: "SET_CART",
-        payload: { items: [cart] },
-      });
-      console.error("Failed to update cart item:", error);
-    }
-  };
-
-  const handleDecrease = async ({
-    id,
-    quantity,
-  }: {
-    id: number;
-    quantity: number;
-  }) => {
-    try {
-      if (quantity === 1) {
-        dispatch({
-          type: "REMOVE_ITEM",
-          payload: { id },
-        });
-      } else {
-        dispatch({
-          type: "SUB_ITEM_QUANTITY",
-          payload: { id, quantity: quantity - 1 },
-        });
-      }
-      await updateCartItemApi(id, quantity - 1);
-    } catch (error) {
-      dispatch({
-        type: "SET_CART",
-        payload: { items: [cart] },
-      });
-      console.error("Failed to update cart item:", error);
-    }
-  };
-
-  const handleDelete = async () => {
-    try {
-      dispatch({
-        type: "REMOVE_ITEM",
-        payload: { id: cart.id },
-      });
-
-      selectDispatch({
-        type: "REMOVE_SELECT",
-        payload: { id: cart.id },
-      });
-
-      await updateCartItemApi(cart.id, 0);
-    } catch (error) {
-      console.error("Failed to delete cart item:", error);
-      dispatch({
-        type: "SET_CART",
-        payload: { items: [cart] },
-      });
-    }
-  };
+  const {
+    isSelected,
+    handleSelect,
+    handleIncrease,
+    handleDecrease,
+    handleDelete,
+  } = useCartItemManager({ cart });
 
   return (
     <>
       <S.Line />
       <S.ItemContainer>
         <S.CartItemHeader>
-          <CheckBox
-            isChecked={
-              selectState.find((item) => item.id === cart.id)?.selected || false
-            }
-            onClick={() => {
-              handleSelect(cart.id);
-              console.log(selectState);
-            }}
-          />
+          <CheckBox isChecked={isSelected} onClick={handleSelect} />
           <S.DeleteButton onClick={handleDelete}>삭제</S.DeleteButton>
         </S.CartItemHeader>
         <S.ItemInfo>
-          <S.ProductImage src={imageUrl}></S.ProductImage>
+          <S.ProductImage src={imageUrl} alt={name} />
           <S.ItemContent>
             <S.ItemTitle>{name}</S.ItemTitle>
             <S.ItemPrice>{price}원</S.ItemPrice>
             <QuantityButton
               quantity={cart.quantity}
-              onIncrease={() =>
-                handleIncrease({ id: cart.id, quantity: cart.quantity })
-              }
-              onDecrease={() =>
-                handleDecrease({ id: cart.id, quantity: cart.quantity })
-              }
+              onIncrease={handleIncrease}
+              onDecrease={handleDecrease}
             />
           </S.ItemContent>
         </S.ItemInfo>
       </S.ItemContainer>
     </>
   );
-}
+};
+
 export default CartItem;
