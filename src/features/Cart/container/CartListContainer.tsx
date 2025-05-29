2f3b0cbbd46e4ee5083bca51c331a7ff528c2807
keemsebeen <sebin0580@gmail.com>
2025-05-29 13:12:02 +0900

    chore: 사용하지 않는 코드 제거
    Co-Authored-By: YEJI <91647696+yeji0214@users.noreply.github.com>

--- src/features/Cart/container/CartListContainer.tsx
+++ src/features/Cart/container/CartListContainer.tsx
@@ -1,18 +1,8 @@
-import { PropsWithChildren, useRef } from 'react';
+import { PropsWithChildren } from 'react';
 import styled from '@emotion/styled';
 
-import { useScrollStatus } from '../hooks/useScrollStatus';
-
 export const CartListContainer = ({ children }: PropsWithChildren) => {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const { isScrolled } = useScrollStatus(containerRef);
-
-  return (
-    <StyledCartListContainer>
-      <StyledGradientOverlay isScrolled={isScrolled} />
-      {children}
-    </StyledCartListContainer>
-  );
+  return <StyledCartListContainer>{children}</StyledCartListContainer>;
 };
 
 const StyledCartListContainer = styled.div`
@@ -22,17 +12,4 @@ const StyledCartListContainer = styled.div`
   overflow-y: auto;
   padding: 0px 20px;
   flex: 2;
-`;
-
-const StyledGradientOverlay = styled.div<{ isScrolled: boolean }>`
-  position: absolute;
-  top: 0;
-  left: 20px;
-  right: 20px;
-  height: 25px;
-  background: linear-gradient(to bottom, rgba(0, 0, 0, 0.15), transparent);
-  opacity: ${({ isScrolled }) => (isScrolled ? 1 : 0)};
-  transition: opacity 0.3s ease-in-out;
-  pointer-events: none;
-  z-index: 10;
 `;
