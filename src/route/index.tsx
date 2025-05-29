3f6221a9977c2c7d8274268e2063d802497ade21
마 <126178440+shuyeon@users.noreply.github.com>
2025-05-29 16:17:11 +0900

    fix : 경로 수정

--- src/route/index.tsx
+++ src/route/index.tsx
@@ -1,24 +1,25 @@
 import { PATH } from "./path";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router";
 import App from "../App";
 import NavBar from "../components/layout/NavBar";
 import Confirm from "../pages/Confirm";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<NavBar />}>
+      <Route path={PATH.MAIN} element={<App />} />
+      <Route path={PATH.CONFIRM} element={<Confirm />} />
+    </Route>
+  ),
   {
-    element: <NavBar />,
-    children: [
-      {
-        path: PATH.MAIN,
-        element: <App />,
-      },
-      {
-        path: PATH.CONFIRM,
-        element: <Confirm />,
-      },
-    ],
-  },
-]);
+    basename: "/react-shopping-cart",
+  }
+);
 export default function Router() {
   return <RouterProvider router={router} />;
 }
