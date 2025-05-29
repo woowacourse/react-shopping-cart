9cd54b56f3e27cc0086ee2bbf2446447898ac93f
spoyodevelop <kjy9505@gmail.com>
2025-05-29 12:46:50 +0900

    feat: 배송비 및 무료 배송 조건을 상수로 분리하여 관리
    Co-authored-by: 신종욱 <66900221+dlsxjzld@users.noreply.github.com>

--- src/components/Cart/CartList/CartList.tsx
+++ src/components/Cart/CartList/CartList.tsx
@@ -2,6 +2,11 @@ import * as Styled from "./CartList.style";
 
 import { PropsWithChildren } from "react";
 import { CartItem } from "../../../type/CartItem";
+import {
+  FREE_SHIPPING_OVER,
+  SHIPPING_FEE,
+} from "../../../constants/priceSetting";
+import notice from "/notice.svg";
 
 interface CartListProps extends PropsWithChildren {
   cartItemsData: CartItem[];
@@ -13,21 +18,37 @@ function CartList({ cartItemsData, selectedCartIds, children }: CartListProps) {
     .filter((cartItem) => selectedCartIds.includes(cartItem.id.toString()))
     .reduce((total, item) => total + item.product.price * item.quantity, 0);
 
-  const shippingFee = totalPrice >= 100000 ? 0 : 3000;
+  const shippingFee = totalPrice >= FREE_SHIPPING_OVER ? 0 : SHIPPING_FEE;
   const totalPriceWithShipping = totalPrice + shippingFee;
 
   return (
     <Styled.Container>
       <Styled.UlContainer>{children}</Styled.UlContainer>
       <Styled.TotalPriceContainer>
-        <Styled.TotalPrice>
-          <Styled.TitleText>주문 금액</Styled.TitleText>
-          <Styled.PriceText>{totalPrice.toLocaleString()}원</Styled.PriceText>
-        </Styled.TotalPrice>
-        <Styled.ShippingFee>
-          <Styled.TitleText>배송비</Styled.TitleText>
-          <Styled.PriceText>{shippingFee.toLocaleString()}원</Styled.PriceText>
-        </Styled.ShippingFee>
+        <Styled.Notice>
+          <Styled.NoticeIcon src={notice} />
+          <Styled.FreeShippingText>
+            총 주문 금액이 {FREE_SHIPPING_OVER.toLocaleString()}원 이상일 경우
+            무료 배송됩니다.
+          </Styled.FreeShippingText>
+        </Styled.Notice>
+        <Styled.PriceWrapper>
+          <Styled.TotalPrice>
+            <Styled.TitleText>주문 금액</Styled.TitleText>
+            <Styled.PriceText>{totalPrice.toLocaleString()}원</Styled.PriceText>
+          </Styled.TotalPrice>
+
+          <Styled.ShippingFee>
+            {shippingFee !== 0 && (
+              <>
+                <Styled.TitleText>배송비</Styled.TitleText>
+                <Styled.PriceText>
+                  {shippingFee.toLocaleString()}원
+                </Styled.PriceText>
+              </>
+            )}
+          </Styled.ShippingFee>
+        </Styled.PriceWrapper>
         <Styled.TotalPriceTitle>
           <Styled.TitleText>총 결제 금액</Styled.TitleText>
           <Styled.PriceText>
---
+++ src/constants/priceSetting.ts
@@ -0,0 +1,2 @@
+export const FREE_SHIPPING_OVER = 100_000;
+export const SHIPPING_FEE = 3_000;
