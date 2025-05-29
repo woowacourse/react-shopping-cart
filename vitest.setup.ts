4e7f71ec8e6ca655cd71414c49b1c498edb9a8cb
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-05-29 10:52:52 +0900

    ✅ test: msw 모킹 테스트 설정

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom';
+import { setupServer } from 'msw/node';
+import { handlers } from './src/mocks/handlers';
+
+export const server = setupServer(...handlers);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
