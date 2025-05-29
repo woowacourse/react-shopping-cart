002c91f7b8ebb178033d3475ab902f32c9d41204
thgml05 <aisohii@naver.com>
2025-05-29 16:54:47 +0900

    chore: base url 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
-  base: '/',
+  base: '/react-shopping-cart/',
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
