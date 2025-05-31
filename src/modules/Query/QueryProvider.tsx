e1b24889155773751fc36e69ed9389ca785fa42a
guesung <gueit214@naver.com>
2025-05-31 14:13:14 +0900

    feat: Query 모듈

---
+++ src/modules/Query/QueryProvider.tsx
@@ -0,0 +1,60 @@
+import { StrictPropsWithChildren } from "@/types/strict";
+import { createContext, useContext, useState } from "react";
+import { Status } from "./types";
+
+interface QueryProviderProps extends StrictPropsWithChildren {}
+
+interface QueryClientContextType {
+  getQueryData: (queryKey: string) => unknown;
+  setQueryData: (queryKey: string, data: unknown) => void;
+  getQueryStatus: (queryKey: string) => Status;
+  setQueryStatus: (queryKey: string, status: Status) => void;
+}
+
+const QueryClientContext = createContext<QueryClientContextType>({
+  getQueryData: () => {
+    throw new Error("QueryProvider를 찾을 수 없습니다.");
+  },
+  setQueryData: () => {
+    throw new Error("QueryProvider를 찾을 수 없습니다.");
+  },
+  getQueryStatus: () => "idle",
+  setQueryStatus: () => {
+    throw new Error("QueryProvider를 찾을 수 없습니다.");
+  },
+});
+export const useQueryClient = () => useContext(QueryClientContext);
+
+export default function QueryProvider({ children }: QueryProviderProps) {
+  const [data, setData] = useState<Record<string, unknown>>({});
+  const [status, setStatus] = useState<Record<string, Status>>({});
+
+  const setQueryData = (queryKey: string, data: unknown) => {
+    setData((prev) => ({ ...prev, [queryKey]: data }));
+  };
+
+  const getQueryData = (queryKey: string) => {
+    return data[queryKey];
+  };
+
+  const setQueryStatus = (queryKey: string, status: Status) => {
+    setStatus((prev) => ({ ...prev, [queryKey]: status }));
+  };
+
+  const getQueryStatus = (queryKey: string) => {
+    return status[queryKey] ?? "idle";
+  };
+
+  return (
+    <QueryClientContext.Provider
+      value={{
+        getQueryData,
+        setQueryData,
+        getQueryStatus,
+        setQueryStatus,
+      }}
+    >
+      {children}
+    </QueryClientContext.Provider>
+  );
+}
---
+++ src/modules/Query/useMutation.ts
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { useQueryClient } from "./QueryProvider";
+import { Status } from "./types";
+
+interface UseMutationProps<TRequest, TResponse> {
+  mutationFn: (variables: TRequest) => Promise<TResponse>;
+}
+
+interface MutateOptions {
+  onMutate: (queryClient: ReturnType<typeof useQueryClient>) => void;
+}
+
+export default function useMutation<TRequest, TResponse, TOptimisticResponse = TResponse>({
+  mutationFn,
+}: UseMutationProps<TRequest, TResponse>) {
+  const queryClient = useQueryClient();
+
+  const [status, setStatus] = useState<Status>("idle");
+
+  const mutate = async (variables: TRequest, options?: MutateOptions) => {
+    try {
+      setStatus("loading");
+
+      await mutationFn(variables);
+
+      options?.onMutate?.(queryClient);
+
+      setStatus("success");
+    } catch (error) {
+      setStatus("error");
+      throw error;
+    }
+  };
+
+  return { mutate, status };
+}
