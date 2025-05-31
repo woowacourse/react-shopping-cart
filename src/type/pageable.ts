c669a756513ed3881d1a503c5119d78bdc7c0542
minji2219 <aabb0219@naver.com>
2025-05-31 19:17:37 +0900

    type: 타입분리

---
+++ src/type/pageable.ts
@@ -0,0 +1,12 @@
+export interface Pageable {
+  pageNumber: number;
+  pageSize: number;
+  sort: {
+    empty: boolean;
+    sorted: boolean;
+    unsorted: boolean;
+  };
+  offset: number;
+  paged: boolean;
+  unpaged: boolean;
+}
