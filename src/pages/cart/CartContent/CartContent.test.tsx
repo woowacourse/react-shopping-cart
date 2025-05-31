13318f0f9431c0dde75beac0ce1709c33d3adb91
wo-o29 <woogur29@gmail.com>
2025-06-01 05:08:25 +0900

    test: 동적으로 변하는 금액 테스트 추가

--- src/pages/cart/CartContent/CartContent.test.tsx
+++ src/pages/cart/CartContent/CartContent.test.tsx
@@ -1,6 +1,15 @@
-import { act, fireEvent, render, screen, waitFor, within } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
-import CartContent from './CartContent';
+import {
+  act,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+  within,
+} from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import CartContent from "./CartContent";
+import { CartItemProvider } from "../contexts/CartItemProvider";
+import { DELIVERY_PRICE } from "../constants/delivery";
 
 let { mockCartItems } = vi.hoisted(() => {
   return {
@@ -10,10 +19,10 @@ let { mockCartItems } = vi.hoisted(() => {
         quantity: 2,
         product: {
           id: 2,
-          name: '토마토',
-          price: 10000,
-          imageUrl: '2',
-          category: '식료품',
+          name: "토마토",
+          price: 10_000,
+          imageUrl: "2",
+          category: "식료품",
           quantity: 5,
         },
       },
@@ -22,10 +31,10 @@ let { mockCartItems } = vi.hoisted(() => {
         quantity: 2,
         product: {
           id: 3,
-          name: '우비',
-          price: 100000,
-          imageUrl: '1',
-          category: '패션잡화',
+          name: "우비",
+          price: 100_000,
+          imageUrl: "1",
+          category: "패션잡화",
           quantity: 5,
         },
       },
@@ -34,10 +43,10 @@ let { mockCartItems } = vi.hoisted(() => {
         quantity: 1,
         product: {
           id: 4,
-          name: '써밋',
-          price: 100000,
-          imageUrl: '15',
-          category: '패션잡화',
+          name: "써밋",
+          price: 100_000,
+          imageUrl: "15",
+          category: "패션잡화",
           quantity: 1,
         },
       },
@@ -45,45 +54,55 @@ let { mockCartItems } = vi.hoisted(() => {
   };
 });
 
-vi.mock('@/apis/cartItems/getCartItems', () => ({
-  getCartItems: vi.fn().mockImplementation(() => Promise.resolve([...mockCartItems])),
+vi.mock("@/apis/cartItems/getCartItems", () => ({
+  getCartItems: vi
+    .fn()
+    .mockImplementation(() => Promise.resolve([...mockCartItems])),
 }));
 
-vi.mock('@/apis/cartItems/deleteCartItem', () => ({
+vi.mock("@/apis/cartItems/deleteCartItem", () => ({
   deleteCartItem: vi.fn().mockImplementation(({ id: cartItemId }) => {
-    const cartItemIndex = mockCartItems.findIndex(({ id }) => id === Number(cartItemId));
+    const cartItemIndex = mockCartItems.findIndex(
+      ({ id }) => id === Number(cartItemId)
+    );
 
     mockCartItems.splice(cartItemIndex, 1);
     return Promise.resolve();
   }),
 }));
 
-vi.mock('@/apis/cartItems/updateCartItemQuantity', () => ({
-  updateCartItemQuantity: vi.fn().mockImplementation(({ id: cartItemId, quantity }) => {
-    const cartItemIndex = mockCartItems.findIndex(({ id }) => id === Number(cartItemId));
+vi.mock("@/apis/cartItems/updateCartItemQuantity", () => ({
+  updateCartItemQuantity: vi
+    .fn()
+    .mockImplementation(({ id: cartItemId, quantity }) => {
+      const cartItemIndex = mockCartItems.findIndex(
+        ({ id }) => id === Number(cartItemId)
+      );
 
-    mockCartItems[cartItemIndex].quantity = quantity;
-    return Promise.resolve();
-  }),
+      mockCartItems[cartItemIndex].quantity = quantity;
+      return Promise.resolve();
+    }),
 }));
 
 const originalCartItems = [...mockCartItems];
 
-const expectAllItemsCheckedAfterAllCheck = (expectedState: 'checked' | 'unchecked') => {
-  const list = screen.getByRole('list');
+const expectAllItemsCheckedAfterAllCheck = (
+  expectedState: "checked" | "unchecked"
+) => {
+  const list = screen.getByRole("list");
   expect(list).toBeInTheDocument();
 
-  const items = within(list).getAllByRole('listitem');
+  const items = within(list).getAllByRole("listitem");
   items.forEach((item) => {
-    const checkBox = within(item).getByRole('button', { name: '상품 선택' });
+    const checkBox = within(item).getByRole("button", { name: "상품 선택" });
     expect(checkBox).toBeInTheDocument();
-    expect(checkBox).toHaveAttribute('name', expectedState);
+    expect(checkBox).toHaveAttribute("name", expectedState);
   });
 };
 
 type ExpectCartItemQuantityChangeParams = {
   productIndex: number;
-  buttonName: '추가' | '빼기';
+  buttonName: "추가" | "빼기";
   beforeExpected: string;
   afterExpected: string;
 };
@@ -94,223 +113,325 @@ export const expectCartItemQuantityChange = async ({
   beforeExpected,
   afterExpected,
 }: ExpectCartItemQuantityChangeParams) => {
-  const list = screen.getByRole('list');
+  const list = screen.getByRole("list");
   expect(list).toBeInTheDocument();
 
-  const product = within(list).getAllByRole('listitem')[productIndex];
-  const button = within(product).getByRole('button', { name: `수량 1개 ${buttonName}` });
-  const currentQuantity = within(product).getByTestId('current-cart-item-quantity');
+  const product = within(list).getAllByRole("listitem")[productIndex];
+  const button = within(product).getByRole("button", {
+    name: `수량 1개 ${buttonName}`,
+  });
+  const currentQuantity = within(product).getByTestId(
+    "current-cart-item-quantity"
+  );
 
   expect(currentQuantity.textContent).toBe(beforeExpected);
 
   fireEvent.click(button);
 
   await waitFor(() => {
-    const list = screen.getByRole('list');
+    const list = screen.getByRole("list");
     expect(list).toBeInTheDocument();
-    const product = within(list).getAllByRole('listitem')[productIndex];
-    const currentQuantity = within(product).getByTestId('current-cart-item-quantity');
+    const product = within(list).getAllByRole("listitem")[productIndex];
+    const currentQuantity = within(product).getByTestId(
+      "current-cart-item-quantity"
+    );
     expect(currentQuantity.textContent).toBe(afterExpected);
   });
 };
 
-describe('CartPage의 CartContent', () => {
+describe("CartPage의 CartContent", () => {
   beforeEach(() => {
     mockCartItems = [...originalCartItems];
   });
 
-  it('장바구니에 담긴 상품(cartItems)이 없는 경우 장바구니에 담은 상품이 없습니다라는 텍스트가 렌더링된다.', async () => {
+  it("장바구니에 담긴 상품(cartItems)이 없는 경우 장바구니에 담은 상품이 없습니다라는 텍스트가 렌더링된다.", async () => {
     mockCartItems = [];
     await act(async () => {
       render(
-        <MemoryRouter initialEntries={['/']}>
-          <CartContent />
-        </MemoryRouter>,
+        <MemoryRouter initialEntries={["/"]}>
+          <CartItemProvider>
+            <CartContent />
+          </CartItemProvider>
+        </MemoryRouter>
       );
     });
 
-    expect(screen.getByText('장바구니에 담은 상품이 없습니다.')).toBeInTheDocument();
+    expect(
+      screen.getByText("장바구니에 담은 상품이 없습니다.")
+    ).toBeInTheDocument();
   });
 
   it('장바구니에 상품이 3개 담겨 "현재 3종류의 상품이 담겨있습니다." 라는 안내 텍스트가 나온다.', async () => {
     await act(async () => {
       render(
-        <MemoryRouter initialEntries={['/']}>
-          <CartContent />
-        </MemoryRouter>,
+        <MemoryRouter initialEntries={["/"]}>
+          <CartItemProvider>
+            <CartContent />
+          </CartItemProvider>
+        </MemoryRouter>
       );
     });
 
-    expect(screen.getByText('현재 3종류의 상품이 담겨있습니다.')).toBeInTheDocument();
+    expect(
+      screen.getByText("현재 3종류의 상품이 담겨있습니다.")
+    ).toBeInTheDocument();
   });
 
-  it('첫 렌더링 시 전체 선택이 되어있어 모든 상품의 체크 박스가 체크되어 있다. ', async () => {
+  it("첫 렌더링 시 전체 선택이 되어있어 모든 상품의 체크 박스가 체크되어 있다. ", async () => {
     await act(async () => {
       render(
-        <MemoryRouter initialEntries={['/']}>
-          <CartContent />
-        </MemoryRouter>,
+        <MemoryRouter initialEntries={["/"]}>
+          <CartItemProvider>
+            <CartContent />
+          </CartItemProvider>
+        </MemoryRouter>
       );
     });
 
-    expectAllItemsCheckedAfterAllCheck('checked');
+    expectAllItemsCheckedAfterAllCheck("checked");
   });
 
-  it('첫 렌더링 시 전체 선택이 되어있어 전체 선택 체크 박스를 클릭하면 모든 상품 선택 체크 해제된다.', async () => {
+  it("첫 렌더링 시 전체 선택이 되어있어 전체 선택 체크 박스를 클릭하면 모든 상품 선택 체크 해제된다.", async () => {
     await act(async () => {
       render(
-        <MemoryRouter initialEntries={['/']}>
-          <CartContent />
-        </MemoryRouter>,
+        <MemoryRouter initialEntries={["/"]}>
+          <CartItemProvider>
+            <CartContent />
+          </CartItemProvider>
+        </MemoryRouter>
       );
     });
-    const allCheckBox = screen.getByRole('button', { name: '전체 선택' });
+    const allCheckBox = screen.getByRole("button", { name: "전체 선택" });
     fireEvent.click(allCheckBox);
 
-    expectAllItemsCheckedAfterAllCheck('unchecked');
+    expectAllItemsCheckedAfterAllCheck("unchecked");
   });
 
-  it('장바구니에 담긴 상품 목록이 렌더링된다.', async () => {
+  it("장바구니에 담긴 상품 목록이 렌더링된다.", async () => {
     await act(async () => {
       render(
-        <MemoryRouter initialEntries={['/']}>
-          <CartContent />
-        </MemoryRouter>,
+        <MemoryRouter initialEntries={["/"]}>
+          <CartItemProvider>
+            <CartContent />
+          </CartItemProvider>
+        </MemoryRouter>
       );
     });
-    const list = screen.getByRole('list');
+    const list = screen.getByRole("list");
     expect(list).toBeInTheDocument();
 
-    const items = within(list).getAllByRole('listitem');
+    const items = within(list).getAllByRole("listitem");
     items.forEach((_, idx) => {
-      expect(screen.getByText(mockCartItems[idx].product.name)).toBeInTheDocument();
+      expect(
+        screen.getByText(mockCartItems[idx].product.name)
+      ).toBeInTheDocument();
     });
   });
 
   it('상품의 체크박스를 클릭하면 "checked" 상태에서 "unchecked"로 토글된다.', async () => {
     await act(async () => {
       render(
-        <MemoryRouter initialEntries={['/']}>
-          <CartContent />
-        </MemoryRouter>,
+        <MemoryRouter initialEntries={["/"]}>
+          <CartItemProvider>
+            <CartContent />
+          </CartItemProvider>
+        </MemoryRouter>
       );
     });
-    const list = screen.getByRole('list');
+    const list = screen.getByRole("list");
     expect(list).toBeInTheDocument();
 
-    const item = within(list).getAllByRole('listitem')[0];
-    const checkBox = within(item).getByRole('button', { name: '상품 선택' });
-    expect(checkBox).toHaveAttribute('name', 'checked');
+    const item = within(list).getAllByRole("listitem")[0];
+    const checkBox = within(item).getByRole("button", { name: "상품 선택" });
+    expect(checkBox).toHaveAttribute("name", "checked");
     fireEvent.click(checkBox);
-    expect(checkBox).toHaveAttribute('name', 'unchecked');
+    expect(checkBox).toHaveAttribute("name", "unchecked");
   });
 
-  it('상품의 삭제 버튼을 누르면 장바구니(cartItems)에서 제거된다.', async () => {
+  it("상품의 삭제 버튼을 누르면 장바구니(cartItems)에서 제거된다.", async () => {
     await act(async () => {
       render(
-        <MemoryRouter initialEntries={['/']}>
-          <CartContent />
-        </MemoryRouter>,
+        <MemoryRouter initialEntries={["/"]}>
+          <CartItemProvider>
+            <CartContent />
+          </CartItemProvider>
+        </MemoryRouter>
       );
     });
-    const list = screen.getByRole('list');
+    const list = screen.getByRole("list");
     expect(list).toBeInTheDocument();
-    const items = within(list).getAllByRole('listitem');
+    const items = within(list).getAllByRole("listitem");
     expect(items).toHaveLength(3);
 
     const firstItem = items[0];
-    const removeButton = within(firstItem).getByRole('button', { name: '삭제' });
+    const removeButton = within(firstItem).getByRole("button", {
+      name: "삭제",
+    });
     fireEvent.click(removeButton);
 
     await waitFor(() => {
-      const list = screen.getByRole('list');
+      const list = screen.getByRole("list");
       expect(list).toBeInTheDocument();
-      const items = within(list).getAllByRole('listitem');
+      const items = within(list).getAllByRole("listitem");
       expect(items).toHaveLength(2);
     });
   });
 
-  it('장바구니에 "토마토" 상품이 2개가 담겨 있는데 증가 버튼을 클릭하면 3개가 된다.', async () => {
+  it(`장바구니에 ${mockCartItems[0].product.name} 상품이 2개가 담겨 있는데 증가 버튼을 클릭하면 3개가 되고, ${mockCartItems[0].product.price}원이 추가된다.`, async () => {
     await act(async () => {
       render(
-        <MemoryRouter initialEntries={['/']}>
-          <CartContent />
-        </MemoryRouter>,
+        <MemoryRouter initialEntries={["/"]}>
+          <CartItemProvider>
+            <CartContent />
+          </CartItemProvider>
+        </MemoryRouter>
       );
     });
 
+    const mockTotalPrice = mockCartItems.reduce(
+      (acc, { product: { price }, quantity }) => acc + price * quantity,
+      0
+    );
+    expect(screen.getByTestId("order-price").textContent).toBe(
+      `${mockTotalPrice.toLocaleString()}원`
+    );
     expectCartItemQuantityChange({
       productIndex: 0,
-      buttonName: '추가',
-      beforeExpected: '2',
-      afterExpected: '3',
+      buttonName: "추가",
+      beforeExpected: "2",
+      afterExpected: "3",
+    });
+
+    await waitFor(() => {
+      expect(screen.getByTestId("order-price").textContent).toBe(
+        `${(
+          mockTotalPrice + mockCartItems[0].product.price
+        ).toLocaleString()}원`
+      );
     });
   });
 
-  it('장바구니에 "우비" 상품이 2개가 담겨 있는데 빼기 버튼을 클릭하면 1개가 된다.', async () => {
+  it(`장바구니에 ${mockCartItems[1].product.name} 상품이 2개가 담겨 있는데 빼기 버튼을 클릭하면 1개가 되고, 총 주문 금액에서 ${mockCartItems[1].product.price}원이 빠진다.`, async () => {
     await act(async () => {
       render(
-        <MemoryRouter initialEntries={['/']}>
-          <CartContent />
-        </MemoryRouter>,
+        <MemoryRouter initialEntries={["/"]}>
+          <CartItemProvider>
+            <CartContent />
+          </CartItemProvider>
+        </MemoryRouter>
       );
     });
 
+    const mockTotalPrice = mockCartItems.reduce(
+      (acc, { product: { price }, quantity }) => acc + price * quantity,
+      0
+    );
+    expect(screen.getByTestId("order-price").textContent).toBe(
+      `${mockTotalPrice.toLocaleString()}원`
+    );
     expectCartItemQuantityChange({
       productIndex: 1,
-      buttonName: '빼기',
-      beforeExpected: '2',
-      afterExpected: '1',
+      buttonName: "빼기",
+      beforeExpected: "2",
+      afterExpected: "1",
+    });
+
+    await waitFor(() => {
+      expect(screen.getByTestId("order-price").textContent).toBe(
+        `${(
+          mockTotalPrice - mockCartItems[1].product.price
+        ).toLocaleString()}원`
+      );
     });
   });
 
-  it('장바구니에 "써밋" 상품이 1개가 담겨 있는데 빼기 버튼을 클릭하면 장바구니에서 제거된다.', async () => {
+  it(`장바구니에 ${mockCartItems[2].product.name} 상품이 1개가 담겨 있는데 빼기 버튼을 클릭하면 장바구니에서 제거된다.`, async () => {
+    const productName = mockCartItems[2].product.name;
     await act(async () => {
       render(
-        <MemoryRouter initialEntries={['/']}>
-          <CartContent />
-        </MemoryRouter>,
+        <MemoryRouter initialEntries={["/"]}>
+          <CartItemProvider>
+            <CartContent />
+          </CartItemProvider>
+        </MemoryRouter>
       );
     });
 
-    const list = screen.getByRole('list');
+    const list = screen.getByRole("list");
     expect(list).toBeInTheDocument();
 
-    const initialItems = within(list).getAllByRole('listitem');
+    const initialItems = within(list).getAllByRole("listitem");
     expect(initialItems).toHaveLength(3);
-    expect(screen.getByText('써밋')).toBeInTheDocument();
+    expect(screen.getByText(productName)).toBeInTheDocument();
 
-    const product = within(list).getAllByRole('listitem')[2];
-    const button = within(product).getByRole('button', { name: '장바구니에서 제거' });
+    const product = within(list).getAllByRole("listitem")[2];
+    const button = within(product).getByRole("button", {
+      name: "장바구니에서 제거",
+    });
     fireEvent.click(button);
 
     await waitFor(() => {
-      const list = screen.getByRole('list');
+      const list = screen.getByRole("list");
       expect(list).toBeInTheDocument();
 
-      const remainingItems = within(list).getAllByRole('listitem');
+      const remainingItems = within(list).getAllByRole("listitem");
       expect(remainingItems).toHaveLength(2);
-
-      expect(screen.queryByText('써밋')).not.toBeInTheDocument();
+      expect(screen.queryByText(productName)).not.toBeInTheDocument();
     });
   });
 
-  it('주문 목록(orderList)이 비어있다면 주문 확인 버튼이 disabled 된다.', async () => {
+  it("주문 목록(orderList)이 비어있다면 주문 확인 버튼이 disabled 된다.", async () => {
     await act(async () => {
       render(
-        <MemoryRouter initialEntries={['/']}>
-          <CartContent />
-        </MemoryRouter>,
+        <MemoryRouter initialEntries={["/"]}>
+          <CartItemProvider>
+            <CartContent />
+          </CartItemProvider>
+        </MemoryRouter>
       );
     });
 
-    const allCheckBox = screen.getByRole('button', { name: '전체 선택' });
+    const allCheckBox = screen.getByRole("button", { name: "전체 선택" });
     fireEvent.click(allCheckBox);
 
     await waitFor(() => {
-      const orderConfirmButton = screen.getByRole('button', { name: '주문 확인' });
+      const orderConfirmButton = screen.getByRole("button", {
+        name: "주문 확인",
+      });
       expect(orderConfirmButton).toBeInTheDocument();
       expect(orderConfirmButton).toBeDisabled();
+    });
+  });
+
+  it(`주문 목록에 담긴 상품이 없다면 주문 금액이 0원, 배송비는 ${DELIVERY_PRICE}원이 된다.`, async () => {
+    await act(async () => {
+      render(
+        <MemoryRouter initialEntries={["/"]}>
+          <CartItemProvider>
+            <CartContent />
+          </CartItemProvider>
+        </MemoryRouter>
+      );
+    });
+
+    const mockTotalPrice = mockCartItems.reduce(
+      (acc, { product: { price }, quantity }) => acc + price * quantity,
+      0
+    );
+    expect(screen.getByTestId("order-price").textContent).toBe(
+      `${mockTotalPrice.toLocaleString()}원`
+    );
+
+    const allCheckBox = screen.getByRole("button", { name: "전체 선택" });
+    fireEvent.click(allCheckBox);
+
+    expectAllItemsCheckedAfterAllCheck("unchecked");
+
+    await waitFor(() => {
+      expect(screen.getByTestId("order-price").textContent).toBe("0원");
+      expect(screen.getByTestId("delivery-price").textContent).toBe(
+        `${DELIVERY_PRICE.toLocaleString()}원`
+      );
     });
   });
 });
