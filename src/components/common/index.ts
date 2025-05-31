4ec646f1ff3bd7bccba491d5fd2f790090e9dc23
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-05-31 19:25:02 +0900

    refactor: common 컴포넌트 배럴 파일 적용

---
+++ src/components/common/index.ts
@@ -0,0 +1,6 @@
+export * from './footerButton';
+export * from './header';
+export * from './iconButton';
+export * from './mobileLayout';
+export * from './selectBox';
+export * from './separator';
---
+++ src/components/common/mobileLayout/index.ts
@@ -0,0 +1,1 @@
+export { default as MobileLayout } from './MobileLayout';
