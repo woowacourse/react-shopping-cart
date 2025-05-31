fdfeb17afca0e76d9130e889baeab48b781ff407
minji2219 <aabb0219@naver.com>
2025-05-31 23:09:43 +0900

    refactor: context 기본값 부여

--- src/provider/errorProvider.tsx
+++ src/provider/errorProvider.tsx
@@ -5,17 +5,20 @@ import {
   useContext,
   useRef,
   useState,
-} from "react";
+} from 'react';
 
 type ErrorContextType = {
   error: string;
   showError: (msg: string) => void;
 };
 
-const ErrorContext = createContext<ErrorContextType | undefined>(undefined);
+const ErrorContext = createContext<ErrorContextType>({
+  error: '',
+  showError: () => {},
+});
 
-export function ErrorProvider({ children }: { children: ReactNode }) {
-  const [error, setError] = useState("");
+export function ErrorProvider({children}: {children: ReactNode}) {
+  const [error, setError] = useState('');
   const timerRef = useRef<number | null>(null);
 
   const showError = useCallback((errorMessage: string) => {
@@ -26,24 +29,24 @@ export function ErrorProvider({ children }: { children: ReactNode }) {
     }
 
     timerRef.current = window.setTimeout(() => {
-      setError("");
+      setError('');
       timerRef.current = null;
     }, 2000);
   }, []);
 
   return (
-    <ErrorContext.Provider value={{ error, showError }}>
+    <ErrorContext.Provider value={{error, showError}}>
       {children}
     </ErrorContext.Provider>
   );
 }
 
-export const useErrorToast = () => {
-  const error = useContext(ErrorContext);
-  return error?.error;
+export const useError = () => {
+  const {error} = useContext(ErrorContext);
+  return error;
 };
 
 export const useShowError = () => {
-  const error = useContext(ErrorContext);
-  return error?.showError;
+  const {showError} = useContext(ErrorContext);
+  return showError;
 };
