6c07e1b1e1b1accb26959b7974a305706359db22
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-05-31 19:27:18 +0900

    chore: vite "@" alias 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react({ jsxImportSource: '@emotion/react' })],
   publicDir: 'public',
   base: 'https://jaeyoung-kwon.github.io/react-shopping-cart/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 });
