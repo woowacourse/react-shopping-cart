48b7ad5c640008c9400062ad607b9e6819937ca4
youjeong Jeong <wjddbwjd1205@naver.com>
2025-06-01 01:40:10 +0900

    style: 스타일 전용 prop에 $ prefix 적용

--- src/pages/shopping-cart/ShoppingCartPage.tsx
+++ src/pages/shopping-cart/ShoppingCartPage.tsx
@@ -1,9 +1,9 @@
-import styled from "@emotion/styled";
-import { Header } from "../../components/common";
-import CartLayout from "./cart-layout/CartLayout";
-import { useOrderListContext } from "./context/OrderListProvider";
-import { useNavigate } from "react-router-dom";
-import ErrorBoundary from "../../components/features/error-boundary/ErrorBoundary";
+import styled from '@emotion/styled';
+import { Header } from '../../components/common';
+import CartLayout from './cart-layout/CartLayout';
+import { useOrderListContext } from './context/OrderListProvider';
+import { useNavigate } from 'react-router-dom';
+import ErrorBoundary from '../../components/features/error-boundary/ErrorBoundary';
 
 const ShoppingCartPage = () => {
   const { selectionMap } = useOrderListContext();
@@ -13,18 +13,18 @@ const ShoppingCartPage = () => {
   const navigate = useNavigate();
   const handleCheckout = () => {
     if (!isDisabled) {
-      navigate("/order-confirm");
+      navigate('/order-confirm');
     }
   };
   return (
     <ErrorBoundary>
-      <Header left="SHOP" />
+      <Header left='SHOP' />
       <CartLayout />
       <CheckoutButton
-        isDisabled={isDisabled}
+        $isDisabled={isDisabled}
         disabled={isDisabled}
         onClick={handleCheckout}
-        aria-label="주문 확인"
+        aria-label='주문 확인'
       >
         주문 확인
       </CheckoutButton>
@@ -34,11 +34,11 @@ const ShoppingCartPage = () => {
 
 export default ShoppingCartPage;
 
-const CheckoutButton = styled.button<{ isDisabled: boolean }>`
+const CheckoutButton = styled.button<{ $isDisabled: boolean }>`
   width: 100%;
   padding: 16px;
-  background-color: ${({ isDisabled }) => (isDisabled ? "#BDBDBD" : "#333")};
+  background-color: ${({ $isDisabled }) => ($isDisabled ? '#BDBDBD' : '#333')};
   color: white;
-  cursor: ${({ isDisabled }) => (isDisabled ? "not-allowed" : "pointer")};
+  cursor: ${({ $isDisabled }) => ($isDisabled ? 'not-allowed' : 'pointer')};
   border-radius: 0px;
 `;
