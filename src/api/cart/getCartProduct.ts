479d2b645c97e9513d83e09654965609dd2110c5
minji2219 <aabb0219@naver.com>
2025-05-31 19:45:47 +0900

    refactor: page, size 값 인자로 받을 수 있게 구현

--- src/api/cart/getCartProduct.ts
+++ src/api/cart/getCartProduct.ts
@@ -1,5 +1,9 @@
-import { ProductsResponse, CartProduct } from "../../type/cart";
-import { apiClient } from "../apiClient";
+import {CartProduct} from '../../type/cart';
+import {ProductsResponse} from '../../type/products';
+import {apiClient} from '../apiClient';
 
-export const getCartProduct = (): Promise<ProductsResponse<CartProduct>> =>
-  apiClient.get({ endPoint: `/cart-items?page=0&size=20` });
+export const getCartProduct = (
+  page = 0,
+  size = 20
+): Promise<ProductsResponse<CartProduct>> =>
+  apiClient.get({endPoint: `/cart-items?page=${page}&size=${size}`});
