7eb7bd25d8ef0a8ba766c0764bde2b5b61ff4caa
guesung <gueit214@naver.com>
2025-06-01 14:31:46 +0900

    fix: import.meta 이슈 해결

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+import EnvironmentPlugin from "vite-plugin-environment";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({ jsxImportSource: "@emotion/react" })],
+  plugins: [react({ jsxImportSource: "@emotion/react" }), EnvironmentPlugin("all")],
   resolve: {
     alias: {
       "@": "/src",
