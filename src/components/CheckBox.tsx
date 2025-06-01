05449acc4270d26c82f431fdab9107ba3dd1f65f
Minje Sung <minje813@dgu.ac.kr>
2025-06-01 22:45:37 +0900

    design: 체크박스 스타일 적용

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -9,9 +9,18 @@ export default CheckBox;
 
 const S = {
   CheckBox: styled.input<InputHTMLAttributes<HTMLInputElement>>`
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    -o-appearance: none;
+    appearance: none;
+    background: url(${({ checked }) => (checked ? './checked-box.svg' : './unchecked-box.svg')})
+      no-repeat;
+    background-position: center;
+    width: 24px;
+    height: 24px;
     cursor: pointer;
     background-color: ${({ checked }) => (checked ? '#000000' : 'transparent')};
     border: ${({ checked }) => (checked ? '2px solid #000000' : `2px solid #e6e6e6`)};
-    border-radius: 8px;
+    border-radius: 5px;
   `,
 };
