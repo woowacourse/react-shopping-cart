992fdf402db86056c556bc5153deaf1c3fa6e38d
guesung <gueit214@naver.com>
2025-06-01 14:32:13 +0900

    feat: jest msw 세팅

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,12 @@
 import "@testing-library/jest-dom";
+import { setupServer } from "msw/node";
+import { handlers, resetCartItems } from "./src/mocks/handlers";
+
+const server = setupServer(...handlers);
+
+beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
+afterAll(() => server.close());
+afterEach(() => {
+  server.resetHandlers();
+  resetCartItems();
+});
