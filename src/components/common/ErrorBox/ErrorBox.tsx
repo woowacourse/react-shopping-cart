ae9360ad8204dbb508e5a080385d3f5cb984281b
AHHYUNJU <wndkgus0225@naver.com>
2025-06-01 15:44:54 +0900

    fix: 불필요한 코드 삭제

--- src/components/common/ErrorBox/ErrorBox.tsx
+++ src/components/common/ErrorBox/ErrorBox.tsx
@@ -4,7 +4,6 @@ import { StyledDiv, StyledSpan } from "./ErrorBox.styles";
 
 export default function ErrorBox() {
   const { errorMessage } = useErrorContext();
-  console.log(errorMessage);
 
   if (!errorMessage) {
     return null;
