a0193ac2ed5667a3f12be65ad2ff5fffb78e1eba
guesung <gueit214@naver.com>
2025-06-01 14:32:05 +0900

    fix: `Response is not defined` 이슈 해결
    ref : https://mswjs.io/docs/migrations/1.x-to-2.x/#requestresponsetextencoder-is-not-defined-jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,12 @@
 export default {
-  testEnvironment: "jsdom",
+  testEnvironment: "jest-fixed-jsdom",
   transform: {
     "^.+\\.tsx?$": ["ts-jest"],
   },
   moduleNameMapper: {
     "^.+\\.svg$": "jest-svg-transformer",
     "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    "@/(.*)": "<rootDir>/src/$1",
   },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
