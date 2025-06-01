2108699e5b95a3dbd6f8370509b422782f2a0e73
ohgus <anakin1108@naver.com>
2025-06-01 12:00:48 +0900

    test: test 로직 개선

--- src/hooks/__tests__/useCartList.test.ts
+++ src/hooks/__tests__/useCartList.test.ts
@@ -4,6 +4,8 @@ import useCartList from '../useCartList';
 import mockCart from '../../mocks/mockCart.json';
 
 describe('useCartList 훅 테스트', () => {
+  const targetId = mockCart[0].id;
+
   it('초기 cartList의 상태 값은 목 데이터를 받아오고, isError의 상태 값은 "", isLoading의 상태 값은 false', async () => {
     const { result } = renderHook(() => useCartList());
 
@@ -15,49 +17,52 @@ describe('useCartList 훅 테스트', () => {
   });
 
   it('장바구니 상품의 수량을 추가할 수 있다.', async () => {
-    const targetItem = mockCart[0];
-    const updatedMockCart = {
-      ...targetItem,
-      quantity: targetItem.quantity + 1,
-    };
+    const targetItem = mockCart.find((item) => item.id === targetId);
+    const expectedQuantity = targetItem!.quantity + 1;
 
     const { result } = renderHook(() => useCartList());
 
     await act(async () => {
-      await result.current.increaseCartItem(targetItem);
+      await result.current.handleIncreaseCartItem({
+        cartItemId: targetId,
+        quantity: expectedQuantity,
+      });
     });
 
-    expect(result.current.cartList[0]).toEqual(updatedMockCart);
+    expect(
+      result.current.cartList.find((item) => item.id === targetId)?.quantity
+    ).toEqual(expectedQuantity);
     expect(result.current.isError).toBe('');
     expect(result.current.isLoading).toBe(false);
   });
 
   it('장바구니 상품의 수량을 감소할 수 있다.', async () => {
-    const targetItem = mockCart[0];
-
-    const updatedMockCart = {
-      ...targetItem,
-      quantity: targetItem.quantity - 1,
-    };
+    const targetItem = mockCart.find((item) => item.id === targetId);
+    const expectedQuantity = targetItem!.quantity - 1;
 
     const { result } = renderHook(() => useCartList());
 
     await act(async () => {
-      await result.current.decreaseCartItem(targetItem);
+      await result.current.handleDecreaseCartItem({
+        cartItemId: targetId,
+        quantity: expectedQuantity,
+      });
     });
 
-    expect(result.current.cartList[0]).toEqual(updatedMockCart);
+    expect(
+      result.current.cartList.find((item) => item.id === targetId)?.quantity
+    ).toEqual(expectedQuantity);
     expect(result.current.isError).toBe('');
     expect(result.current.isLoading).toBe(false);
   });
 
   it('원하는 상품을 제거할 수 있다.', async () => {
-    const targetItem = mockCart[0];
+    const targetItem = mockCart.find((item) => item.id === targetId);
 
     const { result } = renderHook(() => useCartList());
 
     await act(async () => {
-      await result.current.deleteCartItem(targetItem.id);
+      await result.current.handleDeleteCartItem(targetId);
     });
 
     expect(result.current.cartList).not.toContain(targetItem);
