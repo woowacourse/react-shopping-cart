311a2e18c1754567acbf3ab6204a83ccb4aa9cb7
spoyodevelop <kjy9505@gmail.com>
2025-06-02 17:55:10 +0900

    feat: 주문 페이지 및 단계 관리 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,41 @@
 import "./App.css";
 import { PAGE_URL } from "./constants/PageUrl";
-import Cart from "./page/Cart";
+import { steps } from "@/constants/steps";
 import { Route, Routes } from "react-router";
-import OrderComplete from "./page/OrderComplete";
+import OrderComplete from "./page/OrderComplete/OrderComplete";
+import OrderPage from "./page/OrderPage";
+import { useFunnel } from "./hooks/Funnel/useFunnel";
 
 function App() {
+  const { Funnel, Step, setStep } = useFunnel(steps[0]);
+
+  const nextClickHandler = (nextStep: string) => {
+    setStep(nextStep);
+  };
+  const prevClickHandler = (prevStep: string) => {
+    setStep(prevStep);
+  };
+  const resetClickHandler = () => {
+    setStep(steps[0]);
+  };
+
   return (
     <Routes>
-      <Route path={PAGE_URL.HOME} element={<Cart />} />
-      <Route path={PAGE_URL.ORDER_COMPLETE} element={<OrderComplete />} />
+      <Route
+        path={PAGE_URL.HOME}
+        element={
+          <OrderPage
+            nextClickHandler={nextClickHandler}
+            prevClickHandler={prevClickHandler}
+            Funnel={Funnel}
+            Step={Step}
+          />
+        }
+      />
+      <Route
+        path={PAGE_URL.ORDER_COMPLETE}
+        element={<OrderComplete onReset={resetClickHandler} />}
+      />
     </Routes>
   );
 }
