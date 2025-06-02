9a06d85b1d78f26feacff0a54515e19c3c89bd9d
deokbin <deokbin@naver.com>
2025-06-02 20:37:53 +0900

    refactor: 컴포넌트 분리에 따른 스타일 코드 분리

--- src/pages/CartPage/CartPage.styles.ts
+++ src/pages/CartPage/CartPage.styles.ts
@@ -21,40 +21,8 @@ export const ContentContainer = styled.div`
   flex-grow: 1;
 `;
 
-export const CartContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 32px 0;
-  flex-grow: 1;
-  gap: 20px;
-`;
-
-export const CartItemsContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-  overflow-y: auto;
-  max-height: 400px;
-  padding-right: 12px;
-`;
-
 export const InfoContainer = styled.div`
   display: flex;
   align-items: center;
   gap: 4px;
-`;
-
-export const PriceSummary = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-  padding-top: 16px;
-`;
-
-export const PriceInfoWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 12px;
-  border-top: 1px solid #0000001a;
-  padding-top: 12px;
 `;
