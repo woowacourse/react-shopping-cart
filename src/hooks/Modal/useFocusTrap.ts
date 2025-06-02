9dafb65c085002a5b93fca523ad472c8db30bf85
spoyodevelop <kjy9505@gmail.com>
2025-06-03 00:40:05 +0900

    fix: useFocusTrap 훅에서 첫 번째 요소에 대한 포커스 설정 위치 수정

--- src/hooks/Modal/useFocusTrap.ts
+++ src/hooks/Modal/useFocusTrap.ts
@@ -25,6 +25,7 @@ export function useFocusTrap<T extends HTMLElement>(
       modalEl.focus();
       return;
     }
+    first.focus();
 
     const onKeyDown = (e: KeyboardEvent) => {
       if (e.key === "Tab" && e.shiftKey && document.activeElement === first) {
