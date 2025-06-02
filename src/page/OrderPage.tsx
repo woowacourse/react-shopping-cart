cfa8cc1b262d176bf3f54b89189895fa1569f274
spoyodevelop <kjy9505@gmail.com>
2025-06-02 21:47:09 +0900

    feat: 주문 페이지에서 OrderConfirmation 컴포넌트에 selectedCartItems 전달 및 CartHeader 추가

--- src/page/OrderPage.tsx
+++ src/page/OrderPage.tsx
@@ -5,49 +5,60 @@ import OrderConfirmation from "./OrderConfirmation/OrderConfirmation";
 
 import { useCartContext } from "@/components/Cart/CartContext";
 import CartLayout from "@/layout/CartLayout";
+import OrderConfirmationActions from "./OrderConfirmation/Actions/OrderConfirmationActions";
+import { STEP_NAME, StepName } from "@/constants/steps";
+import CartHeader from "@/components/Cart/CartHeader/CartHeader";
 
 export interface ProfileSetupInterface {
   nextClickHandler: (nextStep: string) => void;
   prevClickHandler: (prevStep: string) => void;
   Funnel: React.ComponentType<FunnelProps>;
   Step: React.ComponentType<StepProps>;
+  currentStep: StepName;
 }
-
-// OrderConfirmation에서 selectedCartItems를 사용하는 컴포넌트
-const OrderConfirmationWrapper = ({ onPrev }: { onPrev: () => void }) => {
-  const { selectedCartItems } = useCartContext();
-  return (
-    <OrderConfirmation cartItemsData={selectedCartItems} onPrev={onPrev} />
-  );
-};
 
 function OrderPage({
   nextClickHandler,
   prevClickHandler,
   Funnel,
   Step,
+  currentStep,
 }: ProfileSetupInterface) {
+  const { selectedCartItems } = useCartContext();
+
   return (
     <CartLayout>
       <Funnel>
         <Step name="구매품 선택">
-          <CartProvider onNext={() => nextClickHandler("쿠폰 적용 및 결제")}>
-            <CartContent.Root>
-              <CartContent.Loading />
-              <CartContent.Header />
-              <CartContent.Items />
-              <CartContent.Actions />
-            </CartContent.Root>
-          </CartProvider>
+          <CartHeader />
+          <CartContent.Root>
+            <CartContent.Loading />
+            <CartContent.Header />
+            <CartContent.Items />
+          </CartContent.Root>
         </Step>
         <Step name="쿠폰 적용 및 결제">
-          <CartProvider onNext={() => {}}>
-            <OrderConfirmationWrapper
+          <CartProvider>
+            <OrderConfirmation
+              selectedCartItems={selectedCartItems}
               onPrev={() => prevClickHandler("구매품 선택")}
             />
           </CartProvider>
         </Step>
       </Funnel>
+
+      {currentStep === STEP_NAME.SELECT_PRODUCT && (
+        <div className="fixed-footer">
+          <CartProvider onNext={() => nextClickHandler("쿠폰 적용 및 결제")}>
+            <CartContent.Actions />
+          </CartProvider>
+        </div>
+      )}
+      {currentStep === STEP_NAME.APPLY_COUPON_AND_PAYMENT && (
+        <div className="fixed-footer">
+          <OrderConfirmationActions selectedCartItems={selectedCartItems} />
+        </div>
+      )}
     </CartLayout>
   );
 }
