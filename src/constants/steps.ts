19cdd4e314a8a5acdf1b65069b8c555b006eba75
spoyodevelop <kjy9505@gmail.com>
2025-06-02 21:47:21 +0900

    feat: steps.ts에 StepName 타입 및 STEP_NAME 상수 추가

--- src/constants/steps.ts
+++ src/constants/steps.ts
@@ -1,1 +1,6 @@
 export const steps = ["구매품 선택", "쿠폰 적용 및 결제"];
+export type StepName = (typeof steps)[number];
+export const STEP_NAME = {
+  SELECT_PRODUCT: "구매품 선택",
+  APPLY_COUPON_AND_PAYMENT: "쿠폰 적용 및 결제",
+} as const;
