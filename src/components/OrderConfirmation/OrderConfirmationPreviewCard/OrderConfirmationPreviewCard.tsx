c3bfa08faec6b48131bc8cd30161a6a29b7e24f8
spoyodevelop <kjy9505@gmail.com>
2025-06-03 01:28:24 +0900

    fix: OrderConfirmationPreviewCard에서 수량 표시 스타일 변경

--- src/components/OrderConfirmation/OrderConfirmationPreviewCard/OrderConfirmationPreviewCard.tsx
+++ src/components/OrderConfirmation/OrderConfirmationPreviewCard/OrderConfirmationPreviewCard.tsx
@@ -22,7 +22,10 @@ function OrderConfirmationPreviewCard({
           <Styled.ProductInfo>
             <Styled.ProductName>{name}</Styled.ProductName>
             <Styled.Price>{price.toLocaleString()}원</Styled.Price>
-            <Styled.CartItemQuantity>{quantity}개</Styled.CartItemQuantity>
+            {/* 이상하게 해당 부분에서 계속 충돌이 일어나는데, 이 부분만 이렇게 바꾸었습니다. */}
+            <span style={{ fontSize: "0.75rem", fontWeight: "500" }}>
+              {quantity}개
+            </span>
           </Styled.ProductInfo>
         </Styled.Wrapper>
       </Styled.Container>
