5b3133012c42de28fd607acfee26dc1fede39197
wo-o29 <woogur29@gmail.com>
2025-06-03 22:10:23 +0900

    refactor: OrderSuccessPage 페이지 폴더명 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { Routes, Route } from 'react-router-dom';
-import CartPage from './pages/cart/CartPage';
-import OrderSuccessPage from './pages/order/OrderSuccessPage';
-import { ROUTES } from './shared/config/routes';
+import { Routes, Route } from "react-router-dom";
+import CartPage from "./pages/cart/CartPage";
+import OrderSuccessPage from "./pages/order-success/OrderSuccessPage";
+import { ROUTES } from "./shared/config/routes";
 
 function App() {
   return (
