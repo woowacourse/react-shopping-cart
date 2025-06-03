cd613a9864e62fbe5d9ef3bc3c8b38f1cc27cf08
yeji0214 <1225yj50@naver.com>
2025-06-03 12:57:34 +0900

    refactor: Toast 컴포넌트의 렌더링 타겟을 parent prop으로 주입 가능하도록 수정

--- src/shared/components/Toast/Toast.tsx
+++ src/shared/components/Toast/Toast.tsx
@@ -5,15 +5,16 @@ import { StyledModalContainer, StyledModalContent } from './Toast.styled';
 
 export type ModalProps = {
   children: React.ReactNode;
+  parent?: Element;
 } & ComponentProps<'div'>;
 
-export const Toast = ({ children, ...props }: ModalProps) => {
+export const Toast = ({ children, parent = document.body, ...props }: ModalProps) => {
   return createPortal(
     <StyledModalContainer>
       <StyledModalContent role="dialog" aria-modal="true" {...props}>
         {children}
       </StyledModalContent>
     </StyledModalContainer>,
-    document.body
+    parent
   );
 };
