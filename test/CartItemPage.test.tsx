e9b2f780d6f91cc878cf975f732bf4dceadef645
mlnwns <mlnwns@naver.com>
2025-06-03 22:56:36 +0900

    test: setupCartPageTest 유틸리티 함수 추가 및 CartItemPage 테스트에서 적용

--- test/CartItemPage.test.tsx
+++ test/CartItemPage.test.tsx
@@ -1,18 +1,14 @@
 import "@testing-library/jest-dom";
 import { vi } from "vitest";
 import cartItemsApi from "../src/apis/cartItems";
-import { fireEvent, render, screen } from "@testing-library/react";
-import CartItemPage from "../src/pages/CartItemPage";
-import { CartItemProvider } from "../src/contexts/CartItemProvider";
-import { MemoryRouter } from "react-router";
+import { fireEvent, screen } from "@testing-library/react";
 import { FREE_SHIPPING_MIN_AMOUNT, SHIPPING_FEE } from "../src/constants";
+import { setupCartPageTest } from "./utils/setupCartItemPageTest";
 
 vi.mock("../apis/cartItems");
 
 describe("장바구니 페이지 테스트", () => {
   it("페이지에 장바구니 상품이 렌더링이 된다.", async () => {
-    cartItemsApi.get = vi.fn();
-
     const mockCartItems = [
       {
         id: 1,
@@ -28,25 +24,12 @@ describe("장바구니 페이지 테스트", () => {
       },
     ];
 
-    (cartItemsApi.get as ReturnType<typeof vi.fn>).mockImplementation(
-      async () => [...mockCartItems]
-    );
-
-    render(
-      <MemoryRouter>
-        <CartItemProvider>
-          <CartItemPage />
-        </CartItemProvider>
-      </MemoryRouter>
-    );
+    setupCartPageTest(mockCartItems);
 
     expect(await screen.findByText("콜라")).toBeInTheDocument();
   });
 
   it("페이지에 장바구니 상품이 렌더링되고 삭제할 수 있다", async () => {
-    cartItemsApi.get = vi.fn();
-    cartItemsApi.delete = vi.fn();
-
     const mockCartItems = [
       {
         id: 1,
@@ -62,24 +45,12 @@ describe("장바구니 페이지 테스트", () => {
       },
     ];
 
-    (cartItemsApi.get as ReturnType<typeof vi.fn>).mockImplementation(
-      async () => [...mockCartItems]
-    );
+    cartItemsApi.delete = vi.fn(async (id: number) => {
+      const index = mockCartItems.findIndex((item) => item.id === id);
+      if (index !== -1) mockCartItems.splice(index, 1);
+    });
 
-    (cartItemsApi.delete as ReturnType<typeof vi.fn>).mockImplementation(
-      async (id: number) => {
-        const index = mockCartItems.findIndex((item) => item.id === id);
-        if (index !== -1) mockCartItems.splice(index, 1);
-      }
-    );
-
-    render(
-      <MemoryRouter>
-        <CartItemProvider>
-          <CartItemPage />
-        </CartItemProvider>
-      </MemoryRouter>
-    );
+    setupCartPageTest(mockCartItems);
 
     expect(await screen.findByText("콜라")).toBeInTheDocument();
 
@@ -92,9 +63,6 @@ describe("장바구니 페이지 테스트", () => {
   });
 
   it("수량 조절 버튼을 클릭해 수량을 조절 할 수 있다", async () => {
-    cartItemsApi.get = vi.fn();
-    cartItemsApi.patch = vi.fn();
-
     const mockCartItems = [
       {
         id: 1,
@@ -110,24 +78,12 @@ describe("장바구니 페이지 테스트", () => {
       },
     ];
 
-    (cartItemsApi.get as ReturnType<typeof vi.fn>).mockImplementation(
-      async () => [...mockCartItems]
-    );
+    cartItemsApi.patch = vi.fn(async (id: number, quantity: number) => {
+      const index = mockCartItems.findIndex((item) => item.id === id);
+      mockCartItems[index].quantity = quantity;
+    });
 
-    (cartItemsApi.patch as ReturnType<typeof vi.fn>).mockImplementation(
-      async (id: number, quantity: number) => {
-        const index = mockCartItems.findIndex((item) => item.id === id);
-        mockCartItems[index].quantity = quantity;
-      }
-    );
-
-    render(
-      <MemoryRouter>
-        <CartItemProvider>
-          <CartItemPage />
-        </CartItemProvider>
-      </MemoryRouter>
-    );
+    setupCartPageTest(mockCartItems);
 
     const increaseButton = await screen.findByTestId("increase-button");
     fireEvent.click(increaseButton);
@@ -136,8 +92,6 @@ describe("장바구니 페이지 테스트", () => {
   });
 
   it(`주문 금액이 ${FREE_SHIPPING_MIN_AMOUNT}원 이상이면 배송비는 0원이다.`, async () => {
-    cartItemsApi.get = vi.fn();
-
     const mockCartItems = [
       {
         id: 1,
@@ -153,25 +107,13 @@ describe("장바구니 페이지 테스트", () => {
       },
     ];
 
-    (cartItemsApi.get as ReturnType<typeof vi.fn>).mockImplementation(
-      async () => [...mockCartItems]
-    );
-
-    render(
-      <MemoryRouter>
-        <CartItemProvider>
-          <CartItemPage />
-        </CartItemProvider>
-      </MemoryRouter>
-    );
+    setupCartPageTest(mockCartItems);
 
     const shippingFee = await screen.findByTestId("shipping-fee");
     expect(shippingFee.textContent).toBe("0원");
   });
 
   it(`주문 금액이 ${FREE_SHIPPING_MIN_AMOUNT}원 미만이면 배송비는 ${SHIPPING_FEE}원이다.`, async () => {
-    cartItemsApi.get = vi.fn();
-
     const mockCartItems = [
       {
         id: 1,
@@ -187,17 +129,7 @@ describe("장바구니 페이지 테스트", () => {
       },
     ];
 
-    (cartItemsApi.get as ReturnType<typeof vi.fn>).mockImplementation(
-      async () => [...mockCartItems]
-    );
-
-    render(
-      <MemoryRouter>
-        <CartItemProvider>
-          <CartItemPage />
-        </CartItemProvider>
-      </MemoryRouter>
-    );
+    setupCartPageTest(mockCartItems);
 
     const shippingFee = await screen.findByTestId("shipping-fee");
     expect(shippingFee.textContent).toBe("3,000원");
---
+++ test/utils/setupCartItemPageTest.tsx
@@ -0,0 +1,20 @@
+import { render } from "@testing-library/react";
+
+import cartItemsApi from "../../src/apis/cartItems";
+import { vi } from "vitest";
+import { CartItem } from "../../src/types/type";
+import { MemoryRouter } from "react-router";
+import CartItemPage from "../../src/pages/CartItemPage";
+import { CartItemProvider } from "../../src/contexts/CartItemProvider";
+
+export const setupCartPageTest = (mockItems: CartItem[]) => {
+  cartItemsApi.get = vi.fn(async () => [...mockItems]);
+
+  return render(
+    <MemoryRouter>
+      <CartItemProvider>
+        <CartItemPage />
+      </CartItemProvider>
+    </MemoryRouter>
+  );
+};
