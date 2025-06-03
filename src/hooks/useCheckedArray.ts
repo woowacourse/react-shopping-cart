49a77a81d714e8d4369f9c963c2e741a4f10a0ac
hanheel <hanhee630630@gmail.com>
2025-06-03 13:24:32 +0900

    refactor: useCheckedArray 범용적인 훅으로 재설계
    1. 제네릭을 통해 데이터 형식을 입력받도록 설정
    2. id 값이 필수로 포함되도록 설정

--- src/hooks/useCheckedArray.ts
+++ src/hooks/useCheckedArray.ts
@@ -1,41 +1,44 @@
 import { useEffect, useState } from "react";
-import type { CartItemType } from "../types/response";
 
-const useCheckedArray = (cartData: CartItemType[]) => {
-  const [isCheckedArray, setIsCheckedArray] = useState<number[]>([]);
+interface Identifiable {
+  id: number | string;
+}
+
+const useCheckedArray = <T extends Identifiable>(initialData: T[]) => {
+  const [isCheckedArray, setIsCheckedArray] = useState<T["id"][]>([]);
 
   useEffect(() => {
-    initIsCheckedArray(cartData);
-  }, [cartData]);
+    initIsCheckedArray(initialData);
+  }, [initialData]);
 
-  const justifyIsChecked = (cartId: number) => {
-    const isChecked = isCheckedArray.includes(cartId);
+  const justifyIsChecked = (id: T["id"]) => {
+    const isChecked = isCheckedArray.includes(id);
     return isChecked;
   };
 
-  const controlCheckBox = (cartId: number) => {
-    if (justifyIsChecked(cartId)) {
-      setIsCheckedArray(isCheckedArray.filter((id) => id !== cartId));
+  const controlCheckBox = (id: T["id"]) => {
+    if (justifyIsChecked(id)) {
+      setIsCheckedArray(isCheckedArray.filter((checkedId) => checkedId !== id));
       return;
     }
 
-    setIsCheckedArray([...isCheckedArray, cartId]);
+    setIsCheckedArray([...isCheckedArray, id]);
   };
 
-  const controlAllCheckBox = (cartData: CartItemType[]) => {
-    if (isCheckedArray.length === cartData.length) {
+  const controlAllCheckBox = (data: T[]) => {
+    if (isCheckedArray.length === data.length) {
       setIsCheckedArray([]);
       return;
     }
-    setIsCheckedArray(cartData.map((item) => item.id));
+    setIsCheckedArray(data.map((item) => item.id));
   };
 
-  const initIsCheckedArray = (cartData: CartItemType[]) => {
+  const initIsCheckedArray = (cartData: T[]) => {
     setIsCheckedArray(cartData.map((item) => item.id));
   };
 
   return {
-    isAllChecked: isCheckedArray.length === cartData.length,
+    isAllChecked: isCheckedArray.length === initialData.length,
     isCheckedArray,
     justifyIsChecked,
     controlCheckBox,
