0fc3ab4c431970cb76eb19a4f56a5b4e25649387
thgml05 <aisohii@naver.com>
2025-06-03 21:38:14 +0900

    style: Toast 위치 오류 스타일 수정

--- src/components/common/Toast/Toast.styles.ts
+++ src/components/common/Toast/Toast.styles.ts
@@ -2,34 +2,36 @@ import { css, keyframes } from '@emotion/react';
 
 const slideIn = keyframes`
   0% {
-    transform: translateY(-100%);
+    transform: translateX(-50%) translateY(-100%);
     opacity: 0;
   }
   10% {
-    transform: translateY(0);
+    transform: translateX(-50%) translateY(0);
     opacity: 1;
   }
   90% {
-    transform: translateY(0);
+    transform: translateX(-50%) translateY(0);
     opacity: 1;
   }
   100% {
-    transform: translateY(-100%);
+    transform: translateX(-50%) translateY(-100%);
     opacity: 0;
   }
 `;
 
 export const ToastContainer = css`
   width: 100%;
+  max-width: 43rem;
   height: 40px;
   padding: 12px 77px 12px 77px;
   background-color: #ffc9c9;
   text-align: center;
 
   position: fixed;
-  // top: 20px;
-  // left: 50%;
-  // transform: translateX(-50%);
+
+  top: 6.4rem;
+  left: 50%;
+
   z-index: 9999;
   animation: ${slideIn} 3s ease-in-out forwards;
 `;
