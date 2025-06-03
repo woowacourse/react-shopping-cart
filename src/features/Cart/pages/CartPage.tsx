754b67a88a6a2a327c7b45617231b45701455350
keemsebeen <sebin0580@gmail.com>
2025-06-03 17:42:30 +0900

    refactor: CartPage 컴포넌트 조건문을 통한 분기 처리

--- src/features/Cart/pages/CartPage.tsx
+++ src/features/Cart/pages/CartPage.tsx
@@ -1,13 +1,22 @@
+import { getCartItemList } from '@/api/cart';
+import { Flex } from '@/shared/components/Flex';
+import { Loading } from '@/shared/components/Loading';
 import { STEPS } from '@/shared/constants/setStep';
+import { useFetchData } from '@/shared/hooks/useFetchData';
 
 import { useFunnel } from '../../../shared/hooks/useFunnel';
 import { CartInfo } from '../components/CartInfo';
+import { EmptyCart } from '../components/EmptyCart';
+import { LoadingCart } from '../components/LoadingCart';
 import { OrderConfirm } from '../components/OrderConfirm';
 import { useCart } from '../hooks/useCart';
+import { CartDataState, CartItem } from '../types/Cart.types';
 
-export const CartPage = () => {
+const CartPageFunnel = ({ cart }: CartDataState) => {
   const { Funnel, setStep } = useFunnel<STEPS>('장바구니');
-  const { cartItems, toggleCheck, toggleAllCheck, removeCartItem, updateQuantity } = useCart();
+  const { cartItems, toggleCheck, toggleAllCheck, removeCartItem, updateQuantity } = useCart({
+    cart,
+  });
 
   return (
     <>
@@ -28,4 +37,18 @@ export const CartPage = () => {
       </Funnel>
     </>
   );
+};
+
+export const CartPage = () => {
+  const cart = useFetchData<CartItem[]>({ autoFetch: getCartItemList });
+
+  if (cart.isInitialLoading) {
+    return <LoadingCart />;
+  }
+
+  if (!cart.data || cart.data.length === 0) {
+    return <EmptyCart />;
+  }
+
+  return <CartPageFunnel cart={cart} />;
 };
