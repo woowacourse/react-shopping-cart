4443470529481ff828f3c5f982005d15931cc060
aydenote <dslgpgh@gmail.com>
2025-06-04 01:22:18 +0900

    feat: 장바구니 API 호출 에러 대응 기능 구현

--- src/apis/cart.ts
+++ src/apis/cart.ts
@@ -1,13 +1,19 @@
 import { CartProduct } from '../types/cart';
+import handleHttpError from '../utils/handleHTTPError';
 import { BASE_URL } from './env';
 import { CART_HEADER } from './options';
 
-export const getCartItems = async () => {
+export const getCartItems = async (page = 0, size = 20) => {
   try {
-    const response = await fetch(`${BASE_URL}/cart-items?page=0&size=20`, {
+    const response = await fetch(`${BASE_URL}/cart-items?page=${page}&size=${size}`, {
       headers: CART_HEADER,
     });
-    return await response.json();
+
+    if (!response.ok) {
+      handleHttpError(response);
+    }
+    const data = await response.json();
+    return data;
   } catch (error) {
     throw new Error('장바구니 목록을 불러오는 중 에러 발생');
   }
@@ -15,13 +21,16 @@ export const getCartItems = async () => {
 
 export const patchIncreaseQuantity = async (cartItem: CartProduct) => {
   try {
-    await fetch(`${BASE_URL}/cart-items/${cartItem.id}`, {
+    const response = await fetch(`${BASE_URL}/cart-items/${cartItem.id}`, {
       method: 'PATCH',
       headers: CART_HEADER,
       body: JSON.stringify({
         quantity: cartItem.quantity + 1,
       }),
     });
+    if (!response.ok) {
+      handleHttpError(response);
+    }
   } catch (error) {
     throw new Error('장바구니 상품 증가시 오류 발생');
   }
@@ -29,13 +38,16 @@ export const patchIncreaseQuantity = async (cartItem: CartProduct) => {
 
 export const patchDecreaseQuantity = async (cartItem: CartProduct) => {
   try {
-    await fetch(`${BASE_URL}/cart-items/${cartItem.id}`, {
+    const response = await fetch(`${BASE_URL}/cart-items/${cartItem.id}`, {
       method: 'PATCH',
       headers: CART_HEADER,
       body: JSON.stringify({
         quantity: cartItem.quantity - 1,
       }),
     });
+    if (!response.ok) {
+      handleHttpError(response);
+    }
   } catch (error) {
     throw new Error('장바구니 상품 감소시 오류 발생');
   }
@@ -43,10 +55,13 @@ export const patchDecreaseQuantity = async (cartItem: CartProduct) => {
 
 export const removeCartItem = async (cartItem: CartProduct) => {
   try {
-    await fetch(`${BASE_URL}/cart-items/${cartItem.id}`, {
+    const response = await fetch(`${BASE_URL}/cart-items/${cartItem.id}`, {
       method: 'DELETE',
       headers: CART_HEADER,
     });
+    if (!response.ok) {
+      handleHttpError(response);
+    }
   } catch (error) {
     throw new Error('장바구니 상품 삭제시 오류 발생');
   }
