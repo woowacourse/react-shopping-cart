35848ca544565719fd8f61a320e2d5959d8f2d72
youjeong Jeong <wjddbwjd1205@naver.com>
2025-06-03 18:38:02 +0900

    chore: vitest 환경 설정 및 setup 파일 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/globals" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
@@ -10,5 +11,10 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
+  },
+  test: {
+    environment: 'jsdom', // 브라우저 DOM 환경
+    globals: true, // describe/test/expect 등을 전역에서 사용
+    setupFiles: './src/__test__/setup.ts', // 테스트 전에 실행할 파일 (matcher 설정 등)
   },
 });
