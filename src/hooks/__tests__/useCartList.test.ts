dee31130aa1814240b67c023a647d600495b61cf
ohgus <anakin1108@naver.com>
2025-06-03 14:30:17 +0900

    test: 서버에러 테스트 추가

--- src/hooks/__tests__/useCartList.test.ts
+++ src/hooks/__tests__/useCartList.test.ts
@@ -2,6 +2,8 @@ import { act, renderHook } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
 import useCartList from '../useCartList';
 import mockCart from '../../mocks/mockCart.json';
+import { server } from '../../mocks/node';
+import { http, HttpResponse } from 'msw';
 
 describe('useCartList 훅 테스트', () => {
   const targetId = mockCart[0].id;
@@ -117,6 +119,29 @@ describe('useCartList 훅 테스트', () => {
 
     await act(async () => {
       await result.current.handleDeleteCartItem(targetId);
+    });
+
+    expect(result.current.isError).not.toBeNull();
+    expect(result.current.isLoading).toBe(false);
+  });
+
+  // 서버 오류 테스트 케이스 추가하기 현재 계속 에러가 발생해 주석처리
+  it('장바구니 상품의 수량을 변경할 때 오류가 발생하면 상태 값이 변경되지 않는다', async () => {
+    server.use(
+      http.patch(`/cart-items/:cartItemId`, () => {
+        return new HttpResponse(null, {
+          status: 500,
+        });
+      })
+    );
+
+    const { result } = renderHook(() => useCartList());
+
+    await act(async () => {
+      await result.current.handleIncreaseCartItem({
+        cartItemId: targetId,
+        quantity: 1,
+      });
     });
 
     expect(result.current.isError).not.toBeNull();
