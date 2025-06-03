87f4cd52d99b6df12f71333ad4390d6db8bec66a
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-06-03 16:28:09 +0900

    feat: 바로 url로 /order-check를 쳐서 들어왔을 때 예외 처리

--- src/pages/OrderCheckPage/OrderCheckPage.tsx
+++ src/pages/OrderCheckPage/OrderCheckPage.tsx
@@ -6,14 +6,23 @@ import * as Styled from "./OrderCheckPage.styles";
 
 export default function OrderCheckPage() {
   const location = useLocation();
+
+if(!location.state) {
+  return (
+    <Styled.Container>
+      <h1>잘못된 접근입니다.</h1>
+      <p>주문 확인 페이지로 올바르게 접근해 주세요.</p>
+    </Styled.Container>
+  )
+}
+
   const { checkedProductsLength, cartItemCheckListTotalQuantity, totalPrice } =
     location.state;
 
   return (
-    <>
-      <Styled.Container>
+      <>
+        <Styled.Container>
         <h1>주문 확인</h1>
-
         <Styled.OrderSummary>
           <p>
             총 {checkedProductsLength}종류의 상품{" "}
@@ -21,14 +30,12 @@ export default function OrderCheckPage() {
           </p>
           <p>최종 결제 금액을 확인해 주세요.</p>
         </Styled.OrderSummary>
-
         <Styled.PriceInfo>총 결제 금액</Styled.PriceInfo>
         <Styled.TotalPrice>
           {Number(totalPrice).toLocaleString()}원
-        </Styled.TotalPrice>
-      </Styled.Container>
-
-      <Footer text="결제하기" active="false" handleClick={() => {}} />
+      </Styled.TotalPrice>
+    </Styled.Container>
+    <Footer text="결제하기" active="false" handleClick={() => { } } />
     </>
   );
 }
