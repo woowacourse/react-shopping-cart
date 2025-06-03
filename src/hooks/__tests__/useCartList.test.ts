50c0d1386ae6d9a343fac309737eb315dcb37163
thgml05 <aisohii@naver.com>
2025-06-03 19:57:09 +0900

    test: useCartList 테스트 코드 api 조회 실패 케이스 추가

--- src/hooks/__tests__/useCartList.test.ts
+++ src/hooks/__tests__/useCartList.test.ts
@@ -1,17 +1,20 @@
-import { act, renderHook } from '@testing-library/react';
-import { describe, it, expect } from 'vitest';
+import { act, renderHook, waitFor } from '@testing-library/react';
+import { describe, it, expect, vi } from 'vitest';
 import useCartList from '../useCartList';
 import mockCart from '../../mocks/mockCart.json';
+import cart from '../../apis/cart';
+import { ERROR_MESSAGE } from '../../constants/errorMessage';
 
 describe('useCartList 훅 테스트', () => {
   it('초기 cartList의 상태 값은 목 데이터를 받아오고, isError의 상태 값은 "", isLoading의 상태 값은 false', async () => {
     const { result } = renderHook(() => useCartList());
 
-    await act(async () => null);
+    await waitFor(() => {
+      expect(result.current.isLoading).toBe(false);
+    });
 
-    expect(result.current.cartList).toEqual(mockCart);
-    expect(result.current.isError).toEqual('');
-    expect(result.current.isLoading).toEqual(false);
+    expect(result.current.data).toEqual(mockCart);
+    expect(result.current.error).toEqual('');
   });
 
   it('장바구니 상품의 수량을 추가할 수 있다.', async () => {
@@ -23,13 +26,16 @@ describe('useCartList 훅 테스트', () => {
 
     const { result } = renderHook(() => useCartList());
 
+    await waitFor(() => {
+      expect(result.current.isLoading).toBe(false);
+    });
+
     await act(async () => {
       await result.current.increaseCartItem(targetItem);
     });
 
-    expect(result.current.cartList[0]).toEqual(updatedMockCart);
-    expect(result.current.isError).toBe('');
-    expect(result.current.isLoading).toBe(false);
+    expect(result.current.data[0]).toEqual(updatedMockCart);
+    expect(result.current.error).toBe('');
   });
 
   it('장바구니 상품의 수량을 감소할 수 있다.', async () => {
@@ -42,13 +48,16 @@ describe('useCartList 훅 테스트', () => {
 
     const { result } = renderHook(() => useCartList());
 
+    await waitFor(() => {
+      expect(result.current.isLoading).toBe(false);
+    });
+
     await act(async () => {
       await result.current.decreaseCartItem(targetItem);
     });
 
-    expect(result.current.cartList[0]).toEqual(updatedMockCart);
-    expect(result.current.isError).toBe('');
-    expect(result.current.isLoading).toBe(false);
+    expect(result.current.data[0]).toEqual(updatedMockCart);
+    expect(result.current.error).toBe('');
   });
 
   it('원하는 상품을 제거할 수 있다.', async () => {
@@ -56,12 +65,73 @@ describe('useCartList 훅 테스트', () => {
 
     const { result } = renderHook(() => useCartList());
 
+    await waitFor(() => {
+      expect(result.current.isLoading).toBe(false);
+    });
+
     await act(async () => {
       await result.current.deleteCartItem(targetItem.id);
     });
 
-    expect(result.current.cartList).not.toContain(targetItem);
-    expect(result.current.isError).toBe('');
-    expect(result.current.isLoading).toBe(false);
+    expect(result.current.data).not.toContain(targetItem);
+    expect(result.current.error).toBe('');
+  });
+});
+
+describe('useCartList 훅 예외 테스트', () => {
+  it('장바구니 상품 목록을 불러올 때 에러가 발생하면 error 상태 값이 변경된다.', async () => {
+    vi.spyOn(cart, 'getCartList').mockRejectedValueOnce(
+      new Error('장바구니 목록 조회 실패')
+    );
+
+    const { result } = renderHook(() => useCartList());
+
+    await waitFor(() => {
+      expect(result.current.isLoading).toBe(false);
+    });
+
+    expect(result.current.error).toBe(ERROR_MESSAGE.CART_LIST);
+  });
+
+  it('장바구니 상품의 수량을 추가할 때 에러가 발생하면 error 상태 값이 변경된다.', async () => {
+    vi.spyOn(cart, 'increaseCartItem').mockRejectedValueOnce(
+      new Error('장바구니 상품 수량 증가 실패')
+    );
+
+    const { result } = renderHook(() => useCartList());
+
+    await act(async () => {
+      await result.current.increaseCartItem(mockCart[0]);
+    });
+
+    expect(result.current.error).toBe(ERROR_MESSAGE.INCREASE_CART_ITEM);
+  });
+
+  it('장바구니 상품의 수량을 감소할 때 에러가 발생하면 error 상태 값이 변경된다.', async () => {
+    vi.spyOn(cart, 'decreaseCartItem').mockRejectedValueOnce(
+      new Error('장바구니 상품 수량 감소 실패')
+    );
+
+    const { result } = renderHook(() => useCartList());
+
+    await act(async () => {
+      await result.current.decreaseCartItem(mockCart[0]);
+    });
+
+    expect(result.current.error).toBe(ERROR_MESSAGE.DECREASE_CART_ITEM);
+  });
+
+  it('원하는 상품을 제거할 때 에러가 발생하면 error 상태 값이 변경된다.', async () => {
+    vi.spyOn(cart, 'deleteCartItem').mockRejectedValueOnce(
+      new Error('장바구니 상품 삭제 실패')
+    );
+
+    const { result } = renderHook(() => useCartList());
+
+    await act(async () => {
+      await result.current.deleteCartItem(mockCart[0].id);
+    });
+
+    expect(result.current.error).toBe(ERROR_MESSAGE.DELETE_CART_ITEM);
   });
 });
