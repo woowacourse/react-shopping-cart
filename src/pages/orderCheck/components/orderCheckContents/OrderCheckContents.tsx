268de7337fdd1171982bad7e81c7f4515cfefdd9
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-06-04 13:56:29 +0900

    feat: 모달 라이브러리 적용

--- src/pages/orderCheck/components/orderCheckContents/OrderCheckContents.tsx
+++ src/pages/orderCheck/components/orderCheckContents/OrderCheckContents.tsx
@@ -7,6 +7,7 @@ import OrderItem from '@/components/features/orderCheck/orderItem/OrderItem';
 import OrderPriceSummary from '@/components/features/orderCheck/orderPriceSummary/OrderPriceSummary';
 import { useState } from 'react';
 import * as S from './OrderCheckContents.styles';
+import { Modal } from '@jae-o/modal-component-module';
 
 interface OrderCheckContentsProps {
   orderItems: CartItemType[];
@@ -21,34 +22,37 @@ function OrderCheckContents({ orderItems }: OrderCheckContentsProps) {
     setIsRemoteArea((prev) => !prev);
   };
   return (
-    <S.Container>
-      <OrderCheckTitle />
-      <S.OrderItemList>
-        {orderItems.map((item) => (
-          <OrderItem key={item.id} cartItem={item} />
-        ))}
-      </S.OrderItemList>
-      <BorderButton onClick={() => console.log('쿠폰 적용 클릭')}>
-        쿠폰 적용
-      </BorderButton>
-      <S.ShippingOptionBox>
-        <S.ShippingOptionTitle>배송 정보</S.ShippingOptionTitle>
-        <S.ShippingOptionSelectRow>
-          <SelectBox
-            id="shippingOption"
-            selected={isRemoteArea}
-            onClick={toggleRemoteArea}
-          />
-          <S.ShippingOptionSelectText htmlFor="shippingOption">
-            제주도 및 도서 산간 지역
-          </S.ShippingOptionSelectText>
-        </S.ShippingOptionSelectRow>
-      </S.ShippingOptionBox>
-      <OrderPriceSummary value={orderPrice} />
-      <FooterButton disabled onClick={() => {}}>
-        결제하기
-      </FooterButton>
-    </S.Container>
+    <Modal>
+      <S.Container>
+        <OrderCheckTitle />
+        <S.OrderItemList>
+          {orderItems.map((item) => (
+            <OrderItem key={item.id} cartItem={item} />
+          ))}
+        </S.OrderItemList>
+        <Modal.OpenTrigger>
+          <BorderButton>쿠폰 적용</BorderButton>
+        </Modal.OpenTrigger>
+        <S.ShippingOptionBox>
+          <S.ShippingOptionTitle>배송 정보</S.ShippingOptionTitle>
+          <S.ShippingOptionSelectRow>
+            <SelectBox
+              id="shippingOption"
+              selected={isRemoteArea}
+              onClick={toggleRemoteArea}
+            />
+            <S.ShippingOptionSelectText htmlFor="shippingOption">
+              제주도 및 도서 산간 지역
+            </S.ShippingOptionSelectText>
+          </S.ShippingOptionSelectRow>
+        </S.ShippingOptionBox>
+        <OrderPriceSummary value={orderPrice} />
+        <FooterButton disabled onClick={() => {}}>
+          결제하기
+        </FooterButton>
+        <Modal.Container title="쿠폰 적용"></Modal.Container>
+      </S.Container>
+    </Modal>
   );
 }
 
