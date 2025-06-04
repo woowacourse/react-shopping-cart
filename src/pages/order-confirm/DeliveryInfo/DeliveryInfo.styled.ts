34e279e14e7926b4b2481fd7e4d21e1621702a48
wo-o29 <woogur29@gmail.com>
2025-06-04 09:24:13 +0900

    feat: DeliveryInfo 컴포넌트 추가

---
+++ src/pages/order-confirm/DeliveryInfo/DeliveryInfo.styled.ts
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  margin: 32px 0;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+export const Title = styled.p`
+  color: #0a0d13;
+  font-size: 16px;
+  font-weight: 700;
+`;
+
+export const RegionCheckBox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
---
+++ src/pages/order-confirm/DeliveryInfo/DeliveryInfo.tsx
@@ -0,0 +1,20 @@
+import CheckBox from "@/shared/components/CheckBox/CheckBox";
+import { useState } from "react";
+import * as S from "./DeliveryInfo.styled";
+
+export default function DeliveryInfo() {
+  const [isChecked, setIsChecked] = useState(false);
+
+  return (
+    <S.Container>
+      <S.Title>배송 정보</S.Title>
+      <S.RegionCheckBox>
+        <CheckBox
+          isChecked={isChecked}
+          onClick={() => setIsChecked((prev) => !prev)}
+        />
+        제주도 및 도서 산간 지역
+      </S.RegionCheckBox>
+    </S.Container>
+  );
+}
