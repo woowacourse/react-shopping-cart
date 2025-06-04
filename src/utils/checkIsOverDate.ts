bb5efc5cf7ee04a0d14b4ed1ddcc33dcd27e964d
Minje Sung <minje813@dgu.ac.kr>
2025-06-04 20:35:44 +0900

    chore: console.log 제거

--- src/utils/checkIsOverDate.ts
+++ src/utils/checkIsOverDate.ts
@@ -2,8 +2,6 @@ const checkIsOverToday = (year: number, month: number, day: number) => {
   const today = new Date();
   const givenDay = new Date(year, month - 1, day - 1, 23, 59, 59);
 
-  console.log(today, givenDay);
-
   return today < givenDay;
 };
 
