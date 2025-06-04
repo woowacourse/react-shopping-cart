8acb11e0fc3dab9b7f53f496875fd61d15518379
thgml05 <aisohii@naver.com>
2025-06-04 17:04:57 +0900

    feat: OrderCheckPage 주문 확인 타이틀과 상세 설명 UI 구현

--- src/routes/pages/OrderCheckPage/OrderCheckPage.tsx
+++ src/routes/pages/OrderCheckPage/OrderCheckPage.tsx
@@ -1,27 +1,14 @@
-import { CartItemProps } from '../../../types/cartItem';
 import Header from '../../../components/common/Header/Header';
 import HeaderButton from '../../../components/common/Header/HeaderButton';
 import { Back } from '../../../assets';
-import ContainerLayout from '../../../components/common/ContainerLayout/ContainerLayout';
-import Text from '../../../components/common/Text/Text';
-import { useLocation, useNavigate } from 'react-router';
-import {
-  OrderCheckContainerStyle,
-  orderPriceContainerStyle,
-} from './OrderCheckPage.styles';
+import { useNavigate } from 'react-router';
 import PayButton from '../../../components/PayButton/PayButton';
 import { TEXT } from '../../../constants/text';
+import CartListTitle from '../../../components/CartListTitle/CartListTitle';
+import ContainerLayout from '../../../components/common/ContainerLayout/ContainerLayout';
 
 function OrderCheck() {
   const navigate = useNavigate();
-  const { selectedCartData, totalPrice } = useLocation().state as {
-    selectedCartData: CartItemProps[];
-    totalPrice: number;
-  };
-  const totalProductQuantity = selectedCartData.reduce(
-    (acc, curr) => acc + curr.quantity,
-    0
-  );
 
   return (
     <>
@@ -31,20 +18,10 @@ function OrderCheck() {
         </HeaderButton>
       </Header>
       <ContainerLayout>
-        <div css={OrderCheckContainerStyle}>
-          <Text varient="title">{TEXT.ORDER_CHECK}</Text>
-          <div>
-            <Text varient="caption">
-              총 {selectedCartData.length}종류의 상품 {totalProductQuantity}개를
-              주문합니다.
-            </Text>
-            <Text varient="caption">{TEXT.TOTAL_PRICE_CHECK_MESSAGE}</Text>
-          </div>
-          <div css={orderPriceContainerStyle}>
-            <Text varient="body">{TEXT.TOTAL_PRICE}</Text>
-            <Text varient="title">{totalPrice.toLocaleString()}원</Text>
-          </div>
-        </div>
+        <CartListTitle
+          title={TEXT.ORDER_CHECK}
+          description={`총 [동적연결필요]종류의 상품 [동적연결필요]개를 주문합니다.\n최종 결제 금액을 확인해주세요.`}
+        />
       </ContainerLayout>
       <PayButton />
     </>
