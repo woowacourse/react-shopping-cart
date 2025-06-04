c545777604901898cfbb2ac7cc35ecc646288e48
ohgus <anakin1108@naver.com>
2025-06-04 20:25:58 +0900

    chore: 닫기 버튼 아이콘 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,5 +3,6 @@ export const Minus = `./icons/minus.svg`;
 export const Plus = `./icons/plus.svg`;
 export const Info = `./icons/info.svg`;
 export const Back = `./icons/back.svg`;
+export const Close = `./icons/close.svg`;
 
 export const Default = `./images/default.png`;
