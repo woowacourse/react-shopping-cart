da2f7ea88b0a618b130888ff177ad2c4c7b6fcc5
kimyou1102 <yoosk5485@naver.com>
2025-06-04 16:37:43 +0900

    fix: 경로 문제 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [
     react({
-      jsxImportSource: "@emotion/react",
+      jsxImportSource: '@emotion/react',
     }),
   ],
-  base: "/react-shopping-cart",
+  base: '/react-shopping-cart/',
 });
