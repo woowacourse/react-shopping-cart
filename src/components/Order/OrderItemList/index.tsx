cccd00c98a804f6aa7d024ce0f32fb0dc0248e91
hoyyChoi <hoyeon8165@gmail.com>
2025-06-05 01:39:52 +0900

    feat: OrderItemList 컴포넌트

---
+++ src/components/Order/OrderItemList/index.tsx
@@ -0,0 +1,29 @@
+import { UseCartReturnType } from "../../../hooks/useCart";
+import Text from "../../common/Text";
+import ItemCard from "../../ItemCard";
+import * as S from "./OrderItemList.styled";
+
+const OrderItemList = ({ orderItems }: { orderItems: UseCartReturnType["orderConfirmPageData"]["orderItems"] }) => {
+  return (
+    <S.List>
+      {orderItems.map((item) => {
+        const { id, quantity, product } = item;
+        return (
+          <ItemCard gap={12} key={id}>
+            <ItemCard.Content gap={24}>
+              <ItemCard.Image src={product.imageUrl} alt={product.name} />
+              <ItemCard.Information gap={24}>
+                <ItemCard.Information gap={4}>
+                  <ItemCard.Title text={product.name} />
+                  <ItemCard.Price price={product.price} />
+                </ItemCard.Information>
+                <Text variant="body-3">{quantity}개</Text>
+              </ItemCard.Information>
+            </ItemCard.Content>
+          </ItemCard>
+        );
+      })}
+    </S.List>
+  );
+};
+export default OrderItemList;
