f129dbdd2485905ee21b4a61dec13b403908cef2
spoyodevelop <kjy9505@gmail.com>
2025-06-04 11:19:57 +0900

    feat: OrderPage 컴포넌트의 import 정리 및 불필요한 주석 제거

--- src/page/OrderPage.tsx
+++ src/page/OrderPage.tsx
@@ -1,19 +1,18 @@
-import CartContent from "@/components/Cart/CartContent/CartContent";
-import { FunnelProps, StepProps } from "@/hooks/Funnel/useFunnel";
-import OrderConfirmation from "./OrderConfirmation/OrderConfirmation";
-
-import { useCartContext } from "@/components/Cart/CartContext";
-import CartLayout from "@/layout/CartLayout";
-import OrderConfirmationActions from "./OrderConfirmation/Actions/OrderConfirmationActions";
 import { STEP_NAME, StepName } from "@/constants/steps";
-import CartHeader from "@/components/Cart/CartHeader/CartHeader";
-import * as Styled from "./OrderPage.style";
-import useCouponFetch from "@/hooks/Coupon/useCouponFetch";
-import { useCouponApply } from "@/hooks/Coupon/useCouponApply";
-import useCouponSelection from "@/hooks/Coupon/useCouponSelection";
+
 import { useMemo } from "react";
+import { FunnelProps, StepProps } from "@/hooks/Funnel/useFunnel";
+import { useCartContext } from "@/components/Cart/CartContext";
+import { useCouponFetch } from "@/hooks/Coupon/useCouponFetch";
+import { useCouponApply } from "@/hooks/Coupon/useCouponApply";
+import { useCouponSelection } from "@/hooks/Coupon/useCouponSelection";
 import { useCouponDiscount } from "@/hooks/Coupon/useCouponDiscount";
 
+import CartLayout from "@/layout/CartLayout";
+import CartHeader from "@/components/Cart/CartHeader/CartHeader";
+import CartContent from "@/components/Cart/CartContent/CartContent";
+import OrderConfirmation from "./OrderConfirmation/OrderConfirmation";
+import OrderConfirmationActions from "./OrderConfirmation/Actions/OrderConfirmationActions";
 export interface ProfileSetupInterface {
   nextClickHandler: (nextStep: string) => void;
   prevClickHandler: (prevStep: string) => void;
@@ -32,7 +31,6 @@ function OrderPage({
   const { selectedCartItems } = useCartContext();
   const { couponsData } = useCouponFetch();
 
-  // // 모든 쿠폰으로 초기 계산하여 최적의 쿠폰들 찾기
   const allCouponsResult = useCouponApply({
     coupons: couponsData || [],
     selectedShoppingCartItems: selectedCartItems,
@@ -80,11 +78,9 @@ function OrderPage({
       </Funnel>
 
       {currentStep === STEP_NAME.SELECT_PRODUCT && (
-        <Styled.FixedFooter>
-          <CartContent.Actions
-            onNext={() => nextClickHandler("쿠폰 적용 및 결제")}
-          />
-        </Styled.FixedFooter>
+        <CartContent.Actions
+          onNext={() => nextClickHandler("쿠폰 적용 및 결제")}
+        />
       )}
       {currentStep === STEP_NAME.APPLY_COUPON_AND_PAYMENT && (
         <OrderConfirmationActions
