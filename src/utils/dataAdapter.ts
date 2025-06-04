ed4a457918aa8d7f47066b2a141e78dcb27ad2da
Jeongeun Lee <leeju1009@gmail.com>
2025-06-05 02:42:25 +0900

    feat: 서버의 쿠폰 데이터를 사용할 형식에 맞게 가공하는 함수 구현

--- src/utils/dataAdapter.ts
+++ src/utils/dataAdapter.ts
@@ -1,31 +1,85 @@
-import { CouponDataType, CouponType } from "../types/response";
+import {
+  BuyXGetYCouponDataType,
+  BuyXGetYCouponType,
+  FixedCouponDataType,
+  FixedCouponType,
+  FreeShippingCouponDataType,
+  FreeShippingCouponType,
+  PercentageCouponDataType,
+  PercentageCouponType,
+} from "../types/response";
+import { decideHourPeriod, splitTime } from "./time";
 
-export const adaptCoupons = (coupons: CouponDataType[]): CouponType[] => {
-  return coupons.map(
-    ({
-      id,
-      code,
-      description,
-      expirationDate,
-      discountType,
-      discount,
-      minimumAmount,
-    }) => {
-      const [year, month, day] = expirationDate.split("-");
+export const adaptFixedCoupon = (
+  coupon: FixedCouponDataType
+): FixedCouponType => {
+  const { expirationDate } = coupon;
+  const [year, month, day] = expirationDate.split("-");
 
-      return {
-        id,
-        code,
-        description,
-        expirationDate: {
-          year,
-          month,
-          day,
-        },
-        discountType,
-        discount,
-        minimumAmount,
-      };
-    }
+  return {
+    ...coupon,
+    expirationDate: {
+      year,
+      month,
+      day,
+    },
+  };
+};
+
+export const adaptBuyXGetYCoupon = (
+  coupon: BuyXGetYCouponDataType
+): BuyXGetYCouponType => {
+  const { expirationDate } = coupon;
+  const [year, month, day] = expirationDate.split("-");
+
+  return {
+    ...coupon,
+    expirationDate: {
+      year,
+      month,
+      day,
+    },
+  };
+};
+
+export const adaptFreeShippingCoupon = (
+  coupon: FreeShippingCouponDataType
+): FreeShippingCouponType => {
+  const { expirationDate } = coupon;
+  const [year, month, day] = expirationDate.split("-");
+
+  return {
+    ...coupon,
+    expirationDate: {
+      year,
+      month,
+      day,
+    },
+  };
+};
+
+export const adaptPercentageCoupon = (
+  coupon: PercentageCouponDataType
+): PercentageCouponType => {
+  const { expirationDate, availableTime } = coupon;
+  const [year, month, day] = expirationDate.split("-");
+  const { hour: startHour, minute: startMinute } = splitTime(
+    availableTime.start
   );
+  const { hour: endHour, minute: endMinute } = splitTime(availableTime.end);
+  const startPeriod = decideHourPeriod(startHour);
+  const endPeriod = decideHourPeriod(endHour);
+
+  return {
+    ...coupon,
+    expirationDate: {
+      year,
+      month,
+      day,
+    },
+    availableTime: {
+      start: { hourPeriod: startPeriod, hour: startHour, minute: startMinute },
+      end: { hourPeriod: endPeriod, hour: endHour, minute: endMinute },
+    },
+  };
 };
