a556220150992d4b199da97c8f073fc437e5d2cb
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-06-04 11:28:22 +0900

    ♻️ refactor: BackButton 컴포넌트 위치 변경

--- src/pages/orderConfirm/OrderConfirmPage.tsx
+++ src/pages/orderConfirm/OrderConfirmPage.tsx
@@ -1,6 +1,6 @@
 import Header from '../../components/common/header/Header';
 import OrderConfirmContents from '../../components/features/orderConfirm/OrderConfirmContents';
-import BackButton from '../../components/common/backButton/BackButton';
+import BackButton from '../../components/features/orderConfirm/backButton/BackButton';
 
 function OrderConfirmPage() {
   return (
