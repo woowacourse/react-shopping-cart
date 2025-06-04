d7f733c9356ed7275d8fd2b094dc31fe979b2966
deokbin <deokbin@naver.com>
2025-06-04 14:55:15 +0900

    feat: OrderPage 컴포넌트 세부 UI 추가

--- src/pages/OrderPage/OrderPage.styles.ts
+++ src/pages/OrderPage/OrderPage.styles.ts
@@ -4,8 +4,7 @@ export const Main = styled.div`
   display: flex;
   flex-direction: column;
   flex-grow: 1;
-  justify-content: center;
-  align-items: center;
+  padding: 32px 24px 36px;
 `;
 
 export const BackIcon = styled.img`
@@ -18,8 +17,8 @@ export const DescriptionContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 8px;
-  align-items: center;
-  margin: 40px 0;
+  margin-top: 16px;
+  flex-grow: 1;
 `;
 
 export const PriceContainer = styled.div`
@@ -37,4 +36,93 @@ export const Label = styled.div`
 export const Price = styled.div`
   font-weight: 700;
   font-size: 26px;
+`;
+
+export const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`;
+
+export const CartItemsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  overflow-y: auto;
+  max-height: 200px;
+  padding-right: 12px;
+`;
+
+export const CartItemWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 24px;
+`;
+
+export const CartItemImageWrapper = styled.div`
+  width: 112px;
+  height: 112px;
+`;
+
+export const CartItemImage = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 8px;
+  object-fit: cover;
+  object-position: center;
+  background-color: #f0f0f0;
+`;
+
+export const CartItemInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 13px;
+  justify-content: center;
+`;
+
+export const CartItemName = styled.p`
+  font-weight: 500;
+  font-size: 16px;
+`;
+
+export const CartItemPrice = styled.p`
+  font-weight: 700;
+  font-size: 26px;
+`;
+
+export const ShippingInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  padding: 20px 0;
+`;
+
+export const CheckboxContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 8px;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-grow: 1;
+  width: 100%;
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  height: 48px;
+  border: 1px solid rgba(51, 51, 51, 0.25);
+  border-radius: 5px;
+  font-size: 16px;
+  font-weight: bold;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #e0e0e0;
+  }
 `;
--- src/pages/OrderPage/OrderPage.tsx
+++ src/pages/OrderPage/OrderPage.tsx
@@ -3,14 +3,21 @@ import Header from "../../components/@common/Header/Header";
 import BackIcon from "/left-arrow.svg";
 import * as S from "./OrderPage.styles";
 import Title from "../../components/@common/Title/Title";
-import Description from "../../components/@common/Description/Description";
 import useCart from "../../hooks/useCart";
 import { useNavigate } from "react-router-dom";
 import useCartCalculations from "../../hooks/useCartCaculations";
+import InfoMessage from "../../components/InfoMessage/InfoMessage";
+import PriceSummary from "../../components/PriceSummary/PriceSummary";
+import Checkbox from "../../components/@common/Checkbox/Checkbox";
+import Description from "../../components/@common/Description/Description";
+import InfoIcon from "/Info.svg";
+
+const IMG_BASE_URL = "/react-shopping-cart";
+const DEFAULT_IMAGE_URL = "/planet-default-image.svg";
 
 const OrderPage = () => {
-  const { orderItemCount } = useCart();
-  const { orderQuantity, totalPrice } = useCartCalculations();
+  const { orderItemCount, cartItemsCheckData } = useCart();
+  const { orderQuantity } = useCartCalculations();
 
   const navigate = useNavigate();
 
@@ -32,15 +39,52 @@ const OrderPage = () => {
       <S.Main>
         <Title>주문 확인</Title>
         <S.DescriptionContainer>
-          <Description>
-            총 {orderItemCount}종류의 상품 {orderQuantity}개를 주문합니다.
-          </Description>
-          <Description>최종 결제 금액을 확인해 주세요.</Description>
+          <InfoMessage
+            message={`총 ${orderItemCount}종류의 상품 ${orderQuantity}개를 주문합니다.`}
+          />
+          <InfoMessage message={`최종 결제 금액을 확인해 주세요.`} />
         </S.DescriptionContainer>
-        <S.PriceContainer>
-          <S.Label>총 결제 금액</S.Label>
-          <S.Price>{totalPrice.toLocaleString()}원</S.Price>
-        </S.PriceContainer>
+        <S.CartItemsContainer>
+          {cartItemsCheckData
+            .filter((item) => item.checked)
+            .map((item) => (
+              <S.CartItemWrapper key={item.id}>
+                <S.CartItemImageWrapper>
+                  <S.CartItemImage
+                    src={
+                      item.imageUrl
+                        ? item.imageUrl
+                        : IMG_BASE_URL + DEFAULT_IMAGE_URL
+                    }
+                    alt={item.name}
+                  />
+                </S.CartItemImageWrapper>
+                <S.CartItemInfo>
+                  <S.CartItemName>{item.name}</S.CartItemName>
+                  <S.CartItemPrice>
+                    {item.price.toLocaleString()}원
+                  </S.CartItemPrice>
+                  <div>{item.quantity}개</div>
+                </S.CartItemInfo>
+              </S.CartItemWrapper>
+            ))}
+        </S.CartItemsContainer>
+        <S.ButtonContainer>
+          <S.Button onClick={() => {}}>쿠폰 적용</S.Button>
+        </S.ButtonContainer>
+        <S.ShippingInfo>
+          <S.Label>배송 정보</S.Label>
+          <S.CheckboxContainer>
+            <Checkbox checked={true} onClick={() => {}} />
+            <Description>제주도 및 도서 산간 지역</Description>
+          </S.CheckboxContainer>
+        </S.ShippingInfo>
+        <InfoMessage
+          message={`총 주문 금액이 100,000원 이상일 경우 무료 배송됩니다.`}
+          imageSrc={InfoIcon}
+          imageAlt="info"
+        />
+        <PriceSummary showDiscount />
       </S.Main>
       <FooterButton disabled={true}>결제하기</FooterButton>
     </>
