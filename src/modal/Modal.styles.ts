3789464303248344ad693c5a78dbbb3b63e6c5bc
Jeongeun Lee <leeju1009@gmail.com>
2025-06-04 21:51:15 +0900

    feat: Modal 컴포넌트 구현

---
+++ src/modal/Modal.styles.ts
@@ -0,0 +1,34 @@
+import { css } from "@emotion/react";
+
+export const ModalHeader = css`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Title = css`
+  display: flex;
+
+  font-size: var(--font-size-medium);
+  font-weight: var(--font-weight-bold);
+`;
+
+export const CloseButton = css`
+  background: none;
+  border: none;
+  cursor: pointer;
+`;
+
+export const SubmitButton = css`
+  width: 100%;
+  height: 44px;
+  padding: 11px 0;
+
+  background-color: var(--color-black);
+  border: none;
+  border-radius: 5px;
+
+  font-size: var(--font-size-medium);
+  font-weight: var(--font-weight-bold);
+  color: var(--color-white);
+  text-align: center;
+`;
