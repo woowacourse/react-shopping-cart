01e519f08e65a019d388129605b7a88f599e2907
deokbin <deokbin@naver.com>
2025-06-04 14:54:57 +0900

    feat: showDiscount 설정 추가 및 쿠폰 할인 금액 추가

--- src/components/PriceSummary/PriceSummary.tsx
+++ src/components/PriceSummary/PriceSummary.tsx
@@ -1,14 +1,18 @@
-// import useCart from "../../hooks/useCart";
 import useCartCalculations from "../../hooks/useCartCaculations";
 import PriceInfo from "../PriceInfo/PriceInfo";
 import * as S from "./PriceSummary.styles";
 
-const PriceSummary = () => {
+interface PriceSummaryProps {
+  showDiscount?: boolean;
+}
+
+const PriceSummary = ({ showDiscount = false }: PriceSummaryProps) => {
   const { orderPrice, shippingFee, totalPrice } = useCartCalculations();
   return (
     <S.PriceSummary>
       <S.PriceInfoWrapper>
         <PriceInfo label="주문 금액" price={orderPrice} />
+        {showDiscount && <PriceInfo label="쿠폰 할인 금액" price={-6000} />}
         <PriceInfo label="배송비" price={shippingFee} />
       </S.PriceInfoWrapper>
       <S.PriceInfoWrapper>
