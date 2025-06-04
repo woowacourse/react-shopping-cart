dc61423f82462e575a90932e0a9a11b3a027c7e9
guesung <gueit214@naver.com>
2025-06-04 14:49:50 +0900

    feat: 로컬 스토리지와 연동하는 `usePersistState`훅

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,3 +2,5 @@ export { default as useCartItem } from "./useCartItem/useCartItem";
 export { default as useAutoFocus } from "./useAutoFocus/useAutoFocus";
 export { default as useDevice } from "./useDevice/useDevice";
 export * from "./useDevice";
+
+export { default as usePersistState } from "./usePersistState/usePersistState";
