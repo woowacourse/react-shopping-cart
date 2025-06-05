1104d700529ded47079d3732831d24d590308fa9
eunwoo-levi <eunwoo1341@gmail.com>
2025-06-06 00:51:51 +0900

    refactor: OrderPiceSummary 컴포넌트를 쿠폰 로직에서도 쓰일 수 있도록 리팩토링

--- src/features/cart/ui/OrderPriceSummary.styles.ts
+++ src/features/cart/ui/OrderPriceSummary.styles.ts
@@ -2,10 +2,23 @@ import styled from '@emotion/styled';
 
 export const OrderPriceSummaryContainer = styled.section`
   width: 100%;
-  height: 125px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  gap: 10px;
+`;
+
+export const SuburbExtraFeeContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: start;
+  align-items: center;
+  gap: 10px;
+`;
+
+export const DeliveryFeeHeaderLabel = styled.div`
+  font-size: 16px;
+  font-weight: 700;
 `;
 
 export const DeliveryFeeLabel = styled.div`
@@ -34,18 +47,21 @@ const baseStyle = `
 export const TotalOrderPrice = styled.div`
   ${baseStyle}
   border-top: 2px solid rgba(0, 0, 0, 0.1);
-  padding-top: 10px;
+  padding-top: 12px;
+`;
+
+export const CouponDiscountAmount = styled.div`
+  ${baseStyle}
 `;
 
 export const DeliveryFee = styled.div`
   ${baseStyle}
   border-bottom: 2px solid rgba(0, 0, 0, 0.1);
-  padding-bottom: 10px;
+  padding-bottom: 12px;
 `;
 
 export const TotalPurchasePrice = styled.div`
   ${baseStyle}
-  padding-top: 10px;
 `;
 
 export const PriceBox = styled.span`
--- src/features/cart/ui/OrderPriceSummary.tsx
+++ src/features/cart/ui/OrderPriceSummary.tsx
@@ -1,7 +1,9 @@
 import { useCartContext } from '../../../shared/context/useCartContext';
 import { DELIVERY_FEE, DELIVERY_FEE_THRESHOLD } from '../constants/orderPriceSummary';
 import * as S from './OrderPriceSummary.styles';
-export default function OrderPriceSummary() {
+import SelectInput from '../../../shared/ui/SelectInput';
+
+export default function OrderPriceSummary({ useCoupon = false }: { useCoupon?: boolean }) {
   const { selectedCartItems } = useCartContext();
 
   const totalPrice = selectedCartItems.reduce((acc, item) => acc + item.product.price * item.quantity, 0);
@@ -10,6 +12,15 @@ export default function OrderPriceSummary() {
 
   return (
     <S.OrderPriceSummaryContainer>
+      {useCoupon && (
+        <>
+          <S.DeliveryFeeHeaderLabel>배송 정보</S.DeliveryFeeHeaderLabel>
+          <S.SuburbExtraFeeContainer>
+            <SelectInput type='checkbox' />
+            <S.DeliveryFeeLabel>제주도 및 도서 산간 지역</S.DeliveryFeeLabel>
+          </S.SuburbExtraFeeContainer>
+        </>
+      )}
       <S.DeliveryFeeLabel>
         <S.DeliveryFeeIcon src='./infoLabelIcon.svg' alt='Delivery Fee Label Icon' />총 주문 금액이 100,000원 이상일
         경우 무료 배송됩니다.
@@ -18,6 +29,12 @@ export default function OrderPriceSummary() {
         주문 금액
         <S.PriceBox>{totalPrice.toLocaleString()}원</S.PriceBox>
       </S.TotalOrderPrice>
+      {useCoupon && (
+        <S.CouponDiscountAmount data-testid='coupon-discount-amount'>
+          쿠폰 할인 금액
+          <S.PriceBox>-0원</S.PriceBox>
+        </S.CouponDiscountAmount>
+      )}
       <S.DeliveryFee data-testid='delivery-fee'>
         배송비
         <S.PriceBox>{deliveryFee.toLocaleString()}원</S.PriceBox>
