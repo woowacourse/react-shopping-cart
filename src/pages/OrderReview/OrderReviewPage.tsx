6386dbb2aaaee6e596723a2629e6bb5391636250
eunwoo-levi <eunwoo1341@gmail.com>
2025-06-06 00:51:21 +0900

    feat: 주문 확인 페이지 구현

--- src/pages/OrderReview/OrderReviewPage.tsx
+++ src/pages/OrderReview/OrderReviewPage.tsx
@@ -1,3 +1,5 @@
+/** @jsxImportSource @emotion/react */
+
 import * as S from './OrderReviewPage.styles';
 import { CartHeader, OrderPriceSummary } from '../../features/cart/ui';
 import EmptyCartItemUI from '../../features/cart/ui/EmptyCartItemUI';
@@ -6,9 +8,30 @@ import { useCartContext } from '../../shared/context/useCartContext';
 import Navbar from '../../shared/ui/Navbar';
 import CartPageFooter from '../../features/cart/ui/CartPageFooter';
 import ReviewCartList from '../../features/review/ui/ReviewCartList';
+import Button from '../../shared/ui/Button';
+import { css } from '@emotion/react';
+import { useState } from 'react';
+import CustomModal from '../../shared/ui/CustomModal';
+import CouponList from '../../features/coupon/ui/CouponList';
+
+const CouponButtonCSS = css`
+  width: 100%;
+  padding: 12px 0;
+  border: 2px solid rgba(0, 0, 0, 0.1);
+  border-radius: 5px;
+  font-size: 15px;
+  font-weight: 700;
+  margin: 30px 0;
+
+  transition: background-color 0.2s ease;
+  &:hover {
+    background-color: #e7e7e7;
+  }
+`;
 
 export default function OrderReviewPage() {
   const { selectedCartItems } = useCartContext();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <S.OrderPageContainer>
@@ -18,13 +41,19 @@ export default function OrderReviewPage() {
         {selectedCartItems.length > 0 ? (
           <>
             <ReviewCartList />
-            <OrderPriceSummary />
+            <Button onClick={() => setIsModalOpen(!isModalOpen)} title='쿠폰 적용' css={CouponButtonCSS} />
+            <OrderPriceSummary useCoupon={true} />
           </>
         ) : (
           <EmptyCartItemUI />
         )}
       </S.OrderPageContent>
       <CartPageFooter title='결제하기' url={ROUTES.CONFIRMATION} cartItemQuantity={selectedCartItems.length} />
+      {isModalOpen && (
+        <CustomModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} position='center'>
+          <CouponList onClose={() => setIsModalOpen(false)} />
+        </CustomModal>
+      )}
     </S.OrderPageContainer>
   );
 }
