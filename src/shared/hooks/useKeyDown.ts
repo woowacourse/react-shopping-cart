72b51a263245223a841546cbb857838000f3afdf
wo-o29 <woogur29@gmail.com>
2025-06-05 21:23:16 +0900

    feat: useKeyDown 커스텀 훅 추가

---
+++ src/shared/hooks/useKeyDown.ts
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+
+interface UseKeyDownPrams {
+  keys: string[];
+  callback: () => void;
+}
+
+const useKeyDown = ({ keys, callback }: UseKeyDownPrams) => {
+  useEffect(() => {
+    const handleDocumentKeyDown = (e: KeyboardEvent) => {
+      if (keys.includes(e.key)) {
+        callback();
+      }
+    };
+
+    document.addEventListener("keydown", handleDocumentKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleDocumentKeyDown);
+    };
+  }, [keys, callback]);
+};
+
+export default useKeyDown;
