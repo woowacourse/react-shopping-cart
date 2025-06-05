df85f586cba49f3380272097207828a7d45eea42
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-06-05 21:30:47 +0900

    style: CartCard 스타일 수정 및 시맨틱 태그 설정

--- src/components/Cart/CartCard/CartCard.tsx
+++ src/components/Cart/CartCard/CartCard.tsx
@@ -1,21 +1,21 @@
-import { CartItem } from "../../../type/CartItem";
-import ProductQuantityControl from "../CartQuantityControl/CartQuantityControl";
-import * as Styled from "./CartCard.style";
-import unChecked from "/unChecked.svg";
-import checked from "/checked.svg";
+import { CartItem } from "../../../type/CartItem"
+import ProductQuantityControl from "../CartQuantityControl/CartQuantityControl"
+import * as Styled from "./CartCard.style"
+import unChecked from "/unChecked.svg"
+import checked from "/checked.svg"
 
 interface CartCardProps {
-  cartItem: CartItem;
-  handleDeleteCartItem: (id: number) => Promise<void>;
+  cartItem: CartItem
+  handleDeleteCartItem: (id: number) => Promise<void>
   handleCartItemQuantity: (params: {
-    id: number;
-    quantity: number;
-  }) => Promise<void>;
-  handleRemoveSelectCartItem: (id: number) => void;
-  handleAddSelectCartItem: (id: number) => void;
-  isDeleteItemLoading: boolean;
-  isQuantityUpdateLoading: boolean;
-  isSelected: boolean;
+    id: number
+    quantity: number
+  }) => Promise<void>
+  handleRemoveSelectCartItem: (id: number) => void
+  handleAddSelectCartItem: (id: number) => void
+  isDeleteItemLoading: boolean
+  isQuantityUpdateLoading: boolean
+  isSelected: boolean
 }
 
 function CartCard({
@@ -28,8 +28,8 @@ function CartCard({
   isQuantityUpdateLoading,
   isSelected,
 }: CartCardProps) {
-  const { product, quantity, id } = cartItem;
-  const { name, price, imageUrl } = product;
+  const { product, quantity, id } = cartItem
+  const { name, price, imageUrl } = product
 
   return (
     <li>
@@ -41,21 +41,30 @@ function CartCard({
                 ? handleRemoveSelectCartItem(id)
                 : handleAddSelectCartItem(id)
             }
+            type="button"
+            aria-label={isSelected ? "상품 선택 해제" : "상품 선택"}
           >
-            <Styled.SelectIcon src={isSelected ? checked : unChecked} />
+            <Styled.SelectIcon
+              src={isSelected ? checked : unChecked}
+              alt={isSelected ? "선택됨" : "선택 안 됨"}
+            />
           </Styled.SelectButton>
           <Styled.DeleteButton
             disabled={isDeleteItemLoading}
             onClick={async () => {
-              handleRemoveSelectCartItem(id);
-              await handleDeleteCartItem(id);
+              handleRemoveSelectCartItem(id)
+              await handleDeleteCartItem(id)
             }}
+            aria-label={`${name} 상품 삭제`}
           >
             삭제
           </Styled.DeleteButton>
         </Styled.ButtonWrapper>
         <Styled.Wrapper>
-          <Styled.Image src={imageUrl} alt={name} />
+          <Styled.Image
+            src={imageUrl}
+            alt={`${name} 상품 이미지`}
+          />
           <Styled.ProductInfo>
             <Styled.ProductName>{name}</Styled.ProductName>
             <Styled.Price>{price.toLocaleString()}원</Styled.Price>
@@ -79,7 +88,7 @@ function CartCard({
         </Styled.Wrapper>
       </Styled.Container>
     </li>
-  );
+  )
 }
 
-export default CartCard;
+export default CartCard
