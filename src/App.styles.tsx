63abdd4fce220bb67092d6521f66ce467d9df171
guesung <gueit214@naver.com>
2025-06-05 16:45:10 +0900

    design: 다양한 기기의 width 지원

--- src/App.styles.tsx
+++ src/App.styles.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const AppWrapper = styled.div`
   position: relative;
-  width: 432px;
+  max-width: 432px;
   height: 100vh;
   background-color: white;
   margin: 0 auto;
