a5302e43dbe558b9d9313ac6730951716593462b
deokbin <deokbin@naver.com>
2025-06-05 09:29:05 +0900

    feat: Coupon 인터페이스 추가

--- src/types/response.ts
+++ src/types/response.ts
@@ -12,3 +12,13 @@ interface Product {
   category: string;
   stock: number;
 }
+export interface Coupon {
+  id: number;
+  code: string;
+  description: string;
+  expirationDate: string;
+  discount: number;
+  availableTime: { start: string; end: string };
+  minimumAmount: number;
+  discountType: string;
+}
