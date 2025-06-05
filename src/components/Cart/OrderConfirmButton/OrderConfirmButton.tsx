74589153dfeb619ed7df206c599dcc8c70c11c3b
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-06-05 21:32:46 +0900

    refactor: OrderConfirmButton 컴포넌트의 버튼에 aria-label과 type 설정

--- src/components/Cart/OrderConfirmButton/OrderConfirmButton.tsx
+++ src/components/Cart/OrderConfirmButton/OrderConfirmButton.tsx
@@ -1,11 +1,11 @@
-import useOrderConfirm from "../../../hooks/useOrderConfirm";
-import { CartItem } from "../../../type/CartItem";
+import useOrderConfirm from "../../../hooks/useOrderConfirm"
+import { CartItem } from "../../../type/CartItem"
 
-import * as Styled from "./OrderConfirmButton.style";
+import * as Styled from "./OrderConfirmButton.style"
 
 interface OrderConfirmButtonProps {
-  selectedCartIds: number[];
-  cartItemsData: CartItem[];
+  selectedCartIds: number[]
+  cartItemsData: CartItem[]
 }
 
 function OrderConfirmButton({
@@ -15,16 +15,18 @@ function OrderConfirmButton({
   const { handleOrderConfirm } = useOrderConfirm({
     cartItemsData,
     selectedCartIds,
-  });
+  })
 
   return (
     <Styled.OrderConfirmButton
       disabled={selectedCartIds.length === 0}
       onClick={handleOrderConfirm}
+      type="button"
+      aria-label="주문 확인"
     >
       주문 확인
     </Styled.OrderConfirmButton>
-  );
+  )
 }
 
-export default OrderConfirmButton;
+export default OrderConfirmButton
