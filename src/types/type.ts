6f6a8f6f38a02a846a9c646ace814c491e73492f
H0ngJu <hongju4385@gmail.com>
2025-06-05 16:57:56 +0900

    refactor(type): Coupon 타입 추가

--- src/types/type.ts
+++ src/types/type.ts
@@ -13,3 +13,48 @@ export interface Product {
   name: string;
   price: number;
 }
+
+export interface AvailableTime {
+  start: string;
+  end: string;
+}
+
+type BaseCoupon = {
+  id: number;
+  code: string;
+  description: string;
+  expirationDate: string;
+  discountType: "fixed" | "buyXgetY" | "freeShipping" | "percentage";
+};
+
+type FixedDiscountCoupon = BaseCoupon & {
+  discountType: "fixed";
+  discount: number;
+  minimumAmount: number;
+};
+
+type BuyXGetYCoupon = BaseCoupon & {
+  discountType: "buyXgetY";
+  buyQuantity: number;
+  getQuantity: number;
+};
+
+type FreeShippingCoupon = BaseCoupon & {
+  discountType: "freeShipping";
+  minimumAmount: number;
+};
+
+export type PercentageDiscountCoupon = BaseCoupon & {
+  discountType: "percentage";
+  discount: number;
+  availableTime: {
+    start: string;
+    end: string;
+  };
+};
+
+export type Coupon =
+  | FixedDiscountCoupon
+  | BuyXGetYCoupon
+  | FreeShippingCoupon
+  | PercentageDiscountCoupon;
