2a9e18910a5819edf61391174577fc9ce04be890
Minje Sung <minje813@dgu.ac.kr>
2025-06-05 19:55:23 +0900

    test: 명세 변경에 따른 인터페이스 변경

--- test/utils/getDiscountPrice.test.ts
+++ test/utils/getDiscountPrice.test.ts
@@ -4,7 +4,6 @@ import { mockCartItems } from '../mocks';
 
 describe('쿠폰 적용된 금액을 반환하는 함수 테스트', () => {
   const checkedCartIds = [2, 3, 4];
-  const checkedItems = getCheckedItems(mockCartItems, checkedCartIds);
   const deliveryPrice = 3000;
   const fixedCoupon = {
     id: 1,
@@ -49,55 +48,49 @@ describe('쿠폰 적용된 금액을 반환하는 함수 테스트', () => {
   } as const;
 
   describe('고정 쿠폰 테스트', () => {
-    it('선택한 상품 목록과 배송비, 그리고 고정 쿠폰 정보를 제공하면 할인된 금액을 반환한다.', () => {
-      expect(getDiscountPrice(checkedItems, deliveryPrice, fixedCoupon)).toBe(
-        getOrderPrice(mockCartItems, checkedCartIds) + deliveryPrice - fixedCoupon.discount
-      );
+    it('선택한 상품 목록과 배송비, 그리고 고정 쿠폰 정보를 제공하면 할인 금액을 반환한다.', () => {
+      const checkedItems = getCheckedItems(mockCartItems, checkedCartIds);
+      expect(getDiscountPrice(checkedItems, deliveryPrice, fixedCoupon)).toBe(fixedCoupon.discount);
     });
 
-    it('선택한 상품 목록과 배송비, 그리고 고정 쿠폰 정보를 제공했을 때 최소 금액을 충족하지 못하면 처음 금액 그대로 반환한다.', () => {
+    it('선택한 상품 목록과 배송비, 그리고 고정 쿠폰 정보를 제공했을 때 최소 금액을 충족하지 못하면 할인 금액은 없다.', () => {
       const checkedCartIds = [3, 4];
-      expect(getDiscountPrice(checkedItems, deliveryPrice, fixedCoupon)).toBe(
-        getOrderPrice(mockCartItems, checkedCartIds) + deliveryPrice
-      );
+      const checkedItems = getCheckedItems(mockCartItems, checkedCartIds);
+      expect(getDiscountPrice(checkedItems, deliveryPrice, fixedCoupon)).toBe(0);
     });
   });
 
   describe('X+Y 쿠폰 테스트', () => {
-    it('선택한 상품 목록과 배송비, 그리고 X+Y 쿠폰 정보를 제공하면 할인된 금액을 반환한다.', () => {
-      expect(getDiscountPrice(checkedItems, deliveryPrice, bogoCoupon)).toBe(
-        getOrderPrice(mockCartItems, checkedCartIds) + deliveryPrice - fixedCoupon.discount
-      );
+    it('선택한 상품 목록과 배송비, 그리고 X+Y 쿠폰 정보를 제공하면 할인 금액을 반환한다.', () => {
+      const checkedItems = getCheckedItems(mockCartItems, checkedCartIds);
+      expect(getDiscountPrice(checkedItems, deliveryPrice, bogoCoupon)).toBe(fixedCoupon.discount);
     });
 
-    it('선택한 상품 목록과 배송비, 그리고 X+Y 쿠폰 정보를 제공했을 때 X개의 상품이 없다면 처음 금액 그대로 반환한다.', () => {
+    it('선택한 상품 목록과 배송비, 그리고 X+Y 쿠폰 정보를 제공했을 때 X+Y개의 상품이 없다면 할인 금액은 없다.', () => {
       const checkedCartIds = [4];
-      expect(getDiscountPrice(checkedItems, deliveryPrice, bogoCoupon)).toBe(
-        getOrderPrice(mockCartItems, checkedCartIds) + deliveryPrice
-      );
+      const checkedItems = getCheckedItems(mockCartItems, checkedCartIds);
+      expect(getDiscountPrice(checkedItems, deliveryPrice, bogoCoupon)).toBe(0);
     });
   });
 
   describe('무료 배송 쿠폰 테스트', () => {
-    it('선택한 상품 목록과 배송비, 그리고 무료 배송 쿠폰 정보를 제공하면 할인된 금액을 반환한다.', () => {
-      expect(getDiscountPrice(checkedItems, deliveryPrice, freeShippingCoupon)).toBe(
-        getOrderPrice(mockCartItems, checkedCartIds)
-      );
+    it('선택한 상품 목록과 배송비, 그리고 무료 배송 쿠폰 정보를 제공하면 할인 금액을 반환한다.', () => {
+      const checkedItems = getCheckedItems(mockCartItems, checkedCartIds);
+      expect(getDiscountPrice(checkedItems, deliveryPrice, freeShippingCoupon)).toBe(deliveryPrice);
     });
 
-    it('선택한 상품 목록과 배송비, 그리고 무료 배송 쿠폰 정보를 제공했을 때 최소 금액을 충족하지 못하면 처음 금액 그대로 반환한다.', () => {
+    it('선택한 상품 목록과 배송비, 그리고 무료 배송 쿠폰 정보를 제공했을 때 최소 금액을 충족하지 못하면 할인 금액은 없다.', () => {
       const checkedCartIds = [3, 4];
-      expect(getDiscountPrice(checkedItems, deliveryPrice, freeShippingCoupon)).toBe(
-        getOrderPrice(mockCartItems, checkedCartIds) + deliveryPrice
-      );
+      const checkedItems = getCheckedItems(mockCartItems, checkedCartIds);
+      expect(getDiscountPrice(checkedItems, deliveryPrice, freeShippingCoupon)).toBe(0);
     });
   });
 
   describe('퍼센트 할인 쿠폰 테스트', () => {
-    it('선택한 상품 목록과 배송비, 그리고 퍼센트 할인 쿠폰 정보를 제공하면 할인된 금액을 반환한다.', () => {
+    it('선택한 상품 목록과 배송비, 그리고 퍼센트 할인 쿠폰 정보를 제공하면 할인 금액을 반환한다.', () => {
+      const checkedItems = getCheckedItems(mockCartItems, checkedCartIds);
       expect(getDiscountPrice(checkedItems, deliveryPrice, percentageCoupon)).toBe(
-        (getOrderPrice(mockCartItems, checkedCartIds) + deliveryPrice) *
-          ((100 - fixedCoupon.discount) / 100)
+        getOrderPrice(mockCartItems, checkedCartIds) * (fixedCoupon.discount / 100)
       );
     });
   });
