0eea5495b541eb0fa40d04c60a6c8f843289ae05
eunwoo-levi <eunwoo1341@gmail.com>
2025-06-06 02:33:35 +0900

    refactor: coupon type 추가

--- src/features/coupon/types/coupon.ts
+++ src/features/coupon/types/coupon.ts
@@ -1,9 +1,11 @@
 export interface Coupon {
   code: string;
   description: string;
-  discount: number;
+  discount?: number;
   discountType: string;
   expirationDate: string;
   id: number;
   minimumAmount?: number;
+  buyQuantity?: number;
+  getQuantity?: number;
 }
