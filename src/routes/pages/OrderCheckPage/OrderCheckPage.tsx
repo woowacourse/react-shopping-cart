09273f02d01bedc68c834084dd5460ac769fac9d
thgml05 <aisohii@naver.com>
2025-06-05 09:07:53 +0900

    feat: OrderCheckPage 상품, 쿠폰 버튼, 배송 정보, 가격 정보 UI 추가

--- src/routes/pages/OrderCheckPage/OrderCheckPage.tsx
+++ src/routes/pages/OrderCheckPage/OrderCheckPage.tsx
@@ -1,14 +1,23 @@
+import { CartItemProps } from '../../../types/cartItem';
 import Header from '../../../components/common/Header/Header';
 import HeaderButton from '../../../components/common/Header/HeaderButton';
 import { Back } from '../../../assets';
-import { useNavigate } from 'react-router';
+import { useLocation, useNavigate } from 'react-router';
 import PayButton from '../../../components/PayButton/PayButton';
 import { TEXT } from '../../../constants/text';
 import CartListTitle from '../../../components/CartListTitle/CartListTitle';
 import ContainerLayout from '../../../components/common/ContainerLayout/ContainerLayout';
+import OrderCartItem from '../../../components/CartItem/OrderCartItem';
+import CouponButton from '../../../components/CouponButton/CouponButton';
+import DeliverInfo from '../../../components/DeliverInfo/DeliverInfo';
+import CartPriceCouponInfo from '../../../components/CartPriceInfo/CartPriceCouponInfo';
 
 function OrderCheck() {
   const navigate = useNavigate();
+  // const { selectedCartData, totalPrice } = useLocation().state as {
+  //   selectedCartData: CartItemProps[];
+  //   totalPrice: number;
+  // };
 
   return (
     <>
@@ -22,6 +31,13 @@ function OrderCheck() {
           title={TEXT.ORDER_CHECK}
           description={`총 [동적연결필요]종류의 상품 [동적연결필요]개를 주문합니다.\n최종 결제 금액을 확인해주세요.`}
         />
+        <ul>
+          <OrderCartItem />
+          <OrderCartItem />
+        </ul>
+        <CouponButton />
+        <DeliverInfo />
+        <CartPriceCouponInfo />
       </ContainerLayout>
       <PayButton />
     </>
