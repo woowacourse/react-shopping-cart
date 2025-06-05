a34d3879dc27917c2202d68a645b9ea9793b1bea
guesung <gueit214@naver.com>
2025-06-05 16:14:42 +0900

    refactor: 주석 제거

--- src/hooks/mutations/useCartItemPatchMutation/useCartItemPatchMutation.ts
+++ src/hooks/mutations/useCartItemPatchMutation/useCartItemPatchMutation.ts
@@ -1,16 +1,8 @@
-import { useMutation } from "@/modules/Query";
 import { CartItemApi, PatchCartItemsParams } from "@/apis";
-import { useError } from "@/context";
+import { useMutation } from "@/modules/Query";
 
 export default function useCartItemPatchMutation() {
-  const { showError } = useError();
-
   return useMutation<PatchCartItemsParams, void>({
     mutationFn: CartItemApi.patchCartItems,
-    // onMutate: () => optimisticIncreaseCartItem(productId),
-    // onError: () => {
-    //   refetchCartItems();
-    //   showError({ type: "server", message: "재고가 부족합니다." });
-    // },
   });
 }
--- src/hooks/mutations/useCartItemPostMutation/useCartItemPostMutation.ts
+++ src/hooks/mutations/useCartItemPostMutation/useCartItemPostMutation.ts
@@ -1,12 +1,8 @@
-import { useMutation } from "@/modules/Query";
 import { CartItemApi, PostCartItemsParams } from "@/apis";
-import { useError } from "@/context";
+import { useMutation } from "@/modules/Query";
 
 export default function useCartItemPostMutation() {
-  const { showError } = useError();
-
   return useMutation<PostCartItemsParams, void>({
     mutationFn: CartItemApi.postCartItems,
-    onError: () => showError({ type: "server", message: "재고가 부족합니다." }),
   });
 }
