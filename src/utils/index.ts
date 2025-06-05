40e8964e46ebb664771a082594fe091e109af8ca
Minje Sung <minje813@dgu.ac.kr>
2025-06-05 21:04:04 +0900

    feat: 적용할 쿠폰 목록을 제공하면 앞에서 순서대로 적용하여 할인금액을 반환하는 함수 구현

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,4 @@ export { default as checkIsBetweenTime } from './checkIsBetweenTime';
 export { default as checkIsOverDate } from './checkIsOverDate';
 export { default as getCheckedItems } from './getCheckedItems';
 export { default as getDiscountPrice } from './getDiscountPrice';
+export { default as applyCouponsToItems } from './applyCouponsToItems';
