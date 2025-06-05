032396f5900d0ed29ed1b42a9b0d78581386193e
guesung <gueit214@naver.com>
2025-06-05 16:44:59 +0900

    fix: queryKey 수정

--- src/hooks/useCartItem/utils.ts
+++ src/hooks/useCartItem/utils.ts
@@ -1,30 +1,31 @@
+import { QUERY_KEY } from "@/constants";
 import { getQueryData, setQueryData } from "@/modules/Query/QueryStore";
 import { GetCartItemsResponse } from "@/types";
 
 export function optimisticIncreaseCartItem(productId: number) {
-  const prevCartItems = getQueryData("cartItems") as GetCartItemsResponse;
+  const prevCartItems = getQueryData(QUERY_KEY.cartItem) as GetCartItemsResponse;
   const currentCartItemIndex = prevCartItems.content.findIndex((item) => item.product.id === productId);
   const newCartContent = [...prevCartItems.content];
   newCartContent[currentCartItemIndex] = {
     ...newCartContent[currentCartItemIndex],
     quantity: newCartContent[currentCartItemIndex].quantity + 1,
   };
-  setQueryData("cartItems", { ...prevCartItems, content: newCartContent });
+  setQueryData(QUERY_KEY.cartItem, { ...prevCartItems, content: newCartContent });
 }
 
 export function optimisticDecreaseCartItem(productId: number) {
-  const prevCartItems = getQueryData("cartItems") as GetCartItemsResponse;
+  const prevCartItems = getQueryData(QUERY_KEY.cartItem) as GetCartItemsResponse;
   const currentCartItemIndex = prevCartItems.content.findIndex((item) => item.product.id === productId);
   const newCartContent = [...prevCartItems.content];
   newCartContent[currentCartItemIndex] = {
     ...newCartContent[currentCartItemIndex],
     quantity: newCartContent[currentCartItemIndex].quantity - 1,
   };
-  setQueryData("cartItems", { ...prevCartItems, content: newCartContent });
+  setQueryData(QUERY_KEY.cartItem, { ...prevCartItems, content: newCartContent });
 }
 
 export function optimisticDeleteCartItem(cartItemId: number) {
-  const prevCartItems = getQueryData("cartItems") as GetCartItemsResponse;
+  const prevCartItems = getQueryData(QUERY_KEY.cartItem) as GetCartItemsResponse;
   const newCartContent = prevCartItems.content.filter((item) => item.id !== cartItemId);
-  setQueryData("cartItems", { ...prevCartItems, content: newCartContent });
+  setQueryData(QUERY_KEY.cartItem, { ...prevCartItems, content: newCartContent });
 }
