43d42ed2e064b816276942da390a741c7dadad66
guesung <gueit214@naver.com>
2025-06-06 16:32:37 +0900

    feat: emotion theme 설정

---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+export const theme = {
+  colors: {
+    black: "#000",
+    white: "#fff",
+    gray: "#e5e5e5",
+    primary: "#22c55e",
+    secondary: "#ef4444",
+    success: "#22c55e",
+    error: "#ef4444",
+    warning: "#f59e0b",
+  },
+};
