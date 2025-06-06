675fded58c1559fa048add98f3e6ebf0f7a61d26
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-06-06 23:36:52 +0900

    refactor: OrderConfirmation 페이지 구조 개선 및 불필요한 스타일 제거

--- src/page/orderConfirmation/OrderConfirmation.tsx
+++ src/page/orderConfirmation/OrderConfirmation.tsx
@@ -1,8 +1,9 @@
 import { useLocation, useNavigate } from "react-router";
 import { PAGE_URL } from "../../constants/PageUrl";
 import * as Styled from "./OrderConfirmation.style";
-import OrderConfirmationHeader from "../../components/OrderConfirmation/OrderConfirmationHeader/OrderConfirmationHeader";
 import type { OrderConfirmationLocationState } from "../../type/OrderConfirmation";
+import OrderConfirmationHeader from "../../components/OrderConfirmation/OrderConfirmationHeader/OrderConfirmationHeader";
+import OrderConfirmationContent from "../../components/OrderConfirmation/OrderConfirmationContent/OrderConfirmationContent";
 
 function OrderConfirmation() {
   const location = useLocation();
@@ -21,38 +22,22 @@ function OrderConfirmation() {
     );
   }
 
-  const { selectedCartItemsLength, selectedCartItemsCount, totalPrice } =
-    location.state as OrderConfirmationLocationState;
+  const {
+    selectedCartItemsLength,
+    selectedCartItemsCount,
+    totalPrice,
+    shippingFee,
+    selectedCartItemsData,
+  } = location.state as OrderConfirmationLocationState;
 
   return (
     <Styled.Container>
-      <OrderConfirmationHeader
-        handleGoBackToHomeButton={handleGoBackToHomeButton}
-      />
-
       <Styled.Wrapper>
-        <Styled.ContentSection>
-          <Styled.Title>주문 확인</Styled.Title>
-          <Styled.Description>
-            총 {selectedCartItemsLength}종류의 상품 {selectedCartItemsCount}개를
-            주문합니다.
-          </Styled.Description>
-          <Styled.Description>
-            최종 결제 금액을 확인해 주세요.
-          </Styled.Description>
-        </Styled.ContentSection>
-
-        <Styled.PriceSection>
-          <Styled.PriceLabel>총 결제 금액</Styled.PriceLabel>
-          <Styled.PriceAmount>
-            {totalPrice.toLocaleString()}원
-          </Styled.PriceAmount>
-        </Styled.PriceSection>
+        <OrderConfirmationHeader
+          handleGoBackToHomeButton={handleGoBackToHomeButton}
+        />
+        <OrderConfirmationContent />
       </Styled.Wrapper>
-
-      <Styled.OrderConfirmButton disabled={true}>
-        결제하기
-      </Styled.OrderConfirmButton>
     </Styled.Container>
   );
 }
