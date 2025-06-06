c16a88349559ccb0736978466368c9d42d4e454b
deokbin <deokbin@naver.com>
2025-06-06 14:35:30 +0900

    feat: Coupon 타입 추가

--- src/apis/coupons/getCoupons.ts
+++ src/apis/coupons/getCoupons.ts
@@ -1,11 +1,11 @@
+import { Coupon } from "../../types/response";
 import { httpClient } from "../httpClient";
 
 const ERROR_MESSAGE = "쿠폰 정보를 가져오는 데 실패했습니다.";
 
-export const getCoupons = async (): any => {
+export const getCoupons = async (): Promise<Coupon[]> => {
   try {
     const data = await httpClient.get(`/coupons`);
-    console.log(data);
     return data;
   } catch (error) {
     throw new Error(ERROR_MESSAGE);
