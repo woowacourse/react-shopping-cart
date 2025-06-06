7bcc5edd77aaec799dcef8c90d90a0469f2430bb
thgml05 <aisohii@naver.com>
2025-06-06 22:56:53 +0900

    feat: CouponAcceptButton(쿠폰 사용 버튼) UI 컴포넌트와 스타일링 추가

---
+++ src/components/CouponAcceptButton/CouponAcceptButton.styles.ts
@@ -0,0 +1,13 @@
+import { css } from '@emotion/react';
+
+export const CouponAcceptButtonStyle = css`
+  width: 100%;
+  height: 4.8rem;
+  color: #fff;
+  text-align: center;
+  border: 1px solid #e0e0e0;
+  border-radius: 5px;
+  background: #333;
+  margin-top: 1.2rem;
+  padding: 1.2rem 3rem;
+`;
