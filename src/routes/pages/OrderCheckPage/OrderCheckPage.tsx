dc00ddc14bda4ce5e0dc660ea2e3565a6f9eb500
thgml05 <aisohii@naver.com>
2025-06-06 16:34:46 +0900

    feat: 주문 확인 페이지 장바구니 상품 종류 개수와 총 상품 개수 동적 연결

--- src/routes/pages/OrderCheckPage/OrderCheckPage.tsx
+++ src/routes/pages/OrderCheckPage/OrderCheckPage.tsx
@@ -2,7 +2,7 @@ import { CartItemProps } from '../../../types/cartItem';
 import Header from '../../../components/common/Header/Header';
 import HeaderButton from '../../../components/common/Header/HeaderButton';
 import { Back } from '../../../assets';
-import { useLocation, useNavigate } from 'react-router';
+import { useNavigate } from 'react-router';
 import PayButton from '../../../components/PayButton/PayButton';
 import { TEXT } from '../../../constants/text';
 import CartListTitle from '../../../components/CartListTitle/CartListTitle';
@@ -11,13 +11,25 @@ import OrderCartItem from '../../../components/CartItem/OrderCartItem';
 import CouponButton from '../../../components/CouponButton/CouponButton';
 import DeliverInfo from '../../../components/DeliverInfo/DeliverInfo';
 import CartPriceCouponInfo from '../../../components/CartPriceInfo/CartPriceCouponInfo';
+import { getLocalStorage } from '../../../utils/localStorage';
 
 function OrderCheck() {
   const navigate = useNavigate();
-  // const { selectedCartData, totalPrice } = useLocation().state as {
-  //   selectedCartData: CartItemProps[];
-  //   totalPrice: number;
-  // };
+  // 1. 상품 종류와 총 개수 상태 세팅하기
+  // 1) 종류 개수 : typeCount
+  // - 체크된 id 개수 (selectedItems 필요)
+  // 2) 총 개수 : totalCount
+  // - 체크된 id의 상품 개수 (cartList, selectedItems 필요)
+  const cartList = getLocalStorage('cartList');
+  const selectedItems = getLocalStorage('selectedItems');
+
+  const typeCount = selectedItems.length; //
+  const totalCount = selectedItems.reduce((acc: number, curr: number) => {
+    const findCartItem = cartList.find(
+      (item: CartItemProps) => item.id === curr
+    );
+    return acc + findCartItem?.quantity;
+  }, 0);
 
   return (
     <>
@@ -29,7 +41,7 @@ function OrderCheck() {
       <ContainerLayout>
         <CartListTitle
           title={TEXT.ORDER_CHECK}
-          description={`총 [동적연결필요]종류의 상품 [동적연결필요]개를 주문합니다.\n최종 결제 금액을 확인해주세요.`}
+          description={`총 ${typeCount}종류의 상품 ${totalCount}개를 주문합니다.\n최종 결제 금액을 확인해주세요.`}
         />
         <ul>
           <OrderCartItem />
