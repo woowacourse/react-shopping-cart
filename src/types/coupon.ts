3a8e0fd460768a58647a847098c0fca4e4ab4c61
hoyyChoi <hoyeon8165@gmail.com>
2025-06-06 18:32:13 +0900

    feat: coupon response 타입 추가

---
+++ src/types/coupon.ts
@@ -0,0 +1,18 @@
+export type DiscountType = "fixed" | "buyXgetY" | "freeShipping" | "percentage";
+
+export interface CouponResponse {
+  id: number;
+  code: string;
+  description: string;
+  expirationDate: string;
+  discountType: DiscountType;
+
+  discount?: number;
+  minimumAmount?: number;
+  buyQuantity?: number;
+  getQuantity?: number;
+  availableTime?: {
+    start: string;
+    end: string;
+  };
+}
