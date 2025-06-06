b6d04ab707de7096e843a506eb1ca0db860dbead
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-06-07 00:12:50 +0900

    feat: 주문 목록 컴포넌트 및 스타일 추가

---
+++ src/components/OrderConfirmation/OrderList/OrderList.style.tsx
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const UlContainer = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  border-bottom: 1px solid #0000001a;
+  max-height: 430px;
+  overflow-y: scroll;
+`;
