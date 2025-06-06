f618d36abe8e6f7edaf2659cbb34515b0ad2416b
mun-kyeong <horse6953@naver.com>
2025-06-07 01:58:54 +0900

    feat: 쇼핑 카트 라우터 추가

---
+++ src/route/shoppingCartRoute.tsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import { ShoppingCart } from "../domains/shopping-cart/page/shoppingCart";
+import { PaymentComplete } from "../domains/paymentComplete/page/PaymentComplete";
+
+export const shoppingCartRoute = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <ShoppingCart />,
+      errorElement: <div>페이지를 찾을 수 없습니다. (404)</div>,
+    },
+    {
+      path: "/confirm",
+      element: <PaymentComplete />,
+    },
+  ],
+  {
+    basename: "/react-shopping-cart",
+  }
+);
