ee100c2cb622ee9c5d00ac9862bb936d1c71faad
Oliy <eunsoa0310@gmail.com>
2025-06-07 05:16:59 +0900

    feat: 라우팅 업데이트 - OrderPage 및 PaymentPage 추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
-import CartPage from '../pages/cart/CartPage';
-import OrderSuccessPage from '../pages/order/OrderSuccessPage';
 import { ROUTES } from '../shared/config/routes';
+import CartPage from '@pages/cart/CartPage';
+import OrderPage from '@pages/order/OrderPage';
+import PaymentPage from '@pages/payment/PaymentPage';
 
 function App() {
   return (
     <Routes>
       <Route path={ROUTES.CART} element={<CartPage />} />
-      <Route path={ROUTES.ORDER_SUCCESS} element={<OrderSuccessPage />} />
+      <Route path={ROUTES.ORDER} element={<OrderPage />} />
+      <Route path={ROUTES.PAYMENT} element={<PaymentPage />} />
     </Routes>
   );
 }
