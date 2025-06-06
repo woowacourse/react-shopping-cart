5b793fb2efa073f59565e79f3ad4dcabb667e8b0
mlnwns <mlnwns@naver.com>
2025-06-06 17:53:55 +0900

    fix: 장바구니 페이지 최소 높이 조정

--- src/pages/CartItemPage.tsx
+++ src/pages/CartItemPage.tsx
@@ -80,7 +80,7 @@ export default CartItemPage;
 
 const CartItemPageStyles = css`
   padding: 24px;
-  min-height: calc(100vh - 64px);
+  min-height: calc(100vh - 128px);
   justify-content: center;
 `;
 
