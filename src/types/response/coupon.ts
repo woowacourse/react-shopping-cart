a1bb748184f29d21d4d32fdbf789ca81222c0782
sooyeoniya <tndus1306403@naver.com>
2025-06-07 01:49:32 +0900

    feat: coupons response 타입 설정

---
+++ src/types/response/coupon.ts
@@ -0,0 +1,17 @@
+export interface Coupon {
+  id: number;
+  code: string;
+  description: string;
+  expirationDate: string;
+  discountType: string;
+  discount?: number;
+  minimumAmount?: number;
+  buyQuantity?: number;
+  getQuantity?: number;
+  availableTime?: AvailableTime;
+}
+
+export interface AvailableTime {
+  start: string;
+  end: string;
+}
