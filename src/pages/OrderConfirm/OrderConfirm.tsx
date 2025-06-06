f51ed8d31fa16141eb72231aa884fbcd6932b868
kimyou1102 <yoosk5485@naver.com>
2025-06-07 03:35:26 +0900

    refactor: 확인 페이지에 더미데이터 대신 실제 cartItems 데이터로 변경

--- src/pages/OrderConfirm/OrderConfirm.tsx
+++ src/pages/OrderConfirm/OrderConfirm.tsx
@@ -21,8 +21,12 @@ import { SelectedCartProductContainer } from '../../components/CartItem/Selected
 import { CheckBox } from '../../components/Common/CheckBox/CheckBox';
 import { useState } from 'react';
 import { PaymentSummary } from '../../components/Payment/PaymentSummary/PaymentSummary';
+import { useCartItemsContext } from '../../components/Common/CartItemsProvider/CartItemsProvider';
+import { getCartItemSummary } from '../../utils/getCartItemSummary';
 
 export function OrderConfirm() {
+  const { cartItems } = useCartItemsContext();
+
   const [isChecked, setIsChecked] = useState(false);
   const navigate = useNavigate();
 
@@ -32,36 +36,15 @@ export function OrderConfirm() {
 
   const selectedCartItemIds = getItem(SELECTED_CART_ITEM_IDS, []);
 
-  const CART_ITEMS_DUMMY = [
-    {
-      id: 13117,
-      quantity: 4,
-      product: {
-        id: 28,
-        name: '아샷추',
-        price: 3800,
-        imageUrl:
-          'https://d2afncas1tel3t.cloudfront.net/wp-content/uploads/2023/12/%EC%95%84%EC%83%B7%EC%B6%94%EC%95%84%EC%9D%B4%EC%8A%A4%ED%8B%B0%EC%83%B7%EC%B6%94%EC%B9%B4%EB%94%94%EC%B9%B4%ED%8E%98%EC%9D%B8_1.png',
-        category: '식료품',
-      },
-    },
-    {
-      id: 13121,
-      quantity: 3,
-      product: {
-        id: 137,
-        name: '[빙그래] 요맘때 파인트 710mL 3종 (택1)',
-        price: 5000,
-        imageUrl:
-          'https://product-image.kurly.com/hdims/resize/%5E%3E360x%3E468/cropcenter/360x468/quality/85/src/product/image/73061aab-a2e2-443a-b0f9-f19b7110045e.jpg',
-        category: '공백제이',
-      },
-    },
-  ];
-
   const handleCheckBoxChange = () => {
     setIsChecked((prev) => !prev);
   };
+
+  const { totalPrice } = getCartItemSummary(
+    cartItems,
+    selectedCartItemIds.map(String)
+  );
+
   return (
     <PageLayout>
       <Header>
@@ -79,7 +62,7 @@ export function OrderConfirm() {
             <p css={subTitleStyle}>최종 결제 금액을 확인해 주세요.</p>
           </div>
         </div>
-        <SelectedCartProductContainer cartItems={CART_ITEMS_DUMMY} />
+        <SelectedCartProductContainer cartItems={cartItems} />
         <button css={applyButton} onClick={() => {}}>
           쿠폰 적용
         </button>
@@ -97,7 +80,11 @@ export function OrderConfirm() {
             </label>
           </div>
         </div>
-        <PaymentSummary price={0} CouponDiscountAmount={0} deliveryFee={0} />
+        <PaymentSummary
+          price={totalPrice}
+          CouponDiscountAmount={0}
+          deliveryFee={0}
+        />
       </Main>
       <Footer>
         <Button onClick={() => {}} type="submit" size="full" style="primary">
