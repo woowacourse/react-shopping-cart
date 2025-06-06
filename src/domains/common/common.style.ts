7410288f900f4156163f26fb5ae9c361e719e1ec
mun-kyeong <horse6953@naver.com>
2025-06-07 02:30:39 +0900

    feat: 공통 스타일 파일 추가

---
+++ src/domains/common/common.style.ts
@@ -0,0 +1,19 @@
+import { css } from "@emotion/react";
+
+const titleBox = css`
+  display: flex;
+  gap: 15px;
+  flex-direction: column;
+`;
+
+const titleStyle = css`
+  font-weight: 700;
+  font-size: 24px;
+`;
+
+const subTitleStyle = css`
+  font-weight: 500;
+  font-size: 12px;
+`;
+
+export { titleBox, titleStyle, subTitleStyle };
