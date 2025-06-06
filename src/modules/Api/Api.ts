80fbf5951f41fe9100e0f815d58e2fbf523a3ddd
guesung <gueit214@naver.com>
2025-06-06 15:20:37 +0900

    feat: 도메인 의존성 분리한 Api 모듈

---
+++ src/modules/Api/Api.ts
@@ -0,0 +1,63 @@
+export default class Api {
+  #baseUrl: string;
+  #defaultOptions: RequestInit;
+
+  constructor(baseUrl: string, defaultOptions?: RequestInit) {
+    this.#baseUrl = baseUrl;
+    this.#defaultOptions = defaultOptions || {};
+  }
+
+  async get(path: string, options?: RequestInit) {
+    const response = await fetch(`${this.#baseUrl}${path}`, {
+      method: "GET",
+      ...this.#defaultOptions,
+      ...options,
+    });
+
+    if (!response.ok) throw new Error(`${response.status} ${response.statusText}`);
+    return response.json();
+  }
+
+  async post(path: string, options?: RequestInit) {
+    const response = await fetch(`${this.#baseUrl}${path}`, {
+      method: "POST",
+      ...this.#defaultOptions,
+      ...options,
+      body: JSON.stringify(options?.body),
+    });
+    if (!response.ok) throw new Error(`${response.status} ${response.statusText}`);
+  }
+
+  async put(path: string, options?: RequestInit) {
+    const response = await fetch(`${this.#baseUrl}${path}`, {
+      method: "PUT",
+      ...this.#defaultOptions,
+      ...options,
+      body: JSON.stringify(options?.body),
+    });
+
+    if (!response.ok) throw new Error(`${response.status} ${response.statusText}`);
+  }
+
+  async patch(path: string, options?: RequestInit) {
+    const response = await fetch(`${this.#baseUrl}${path}`, {
+      method: "PATCH",
+      ...this.#defaultOptions,
+      ...options,
+      body: JSON.stringify(options?.body),
+    });
+
+    if (!response.ok) throw new Error(`${response.status} ${response.statusText}`);
+  }
+
+  async delete(path: string, options?: RequestInit) {
+    const response = await fetch(`${this.#baseUrl}${path}`, {
+      method: "DELETE",
+      ...this.#defaultOptions,
+      ...options,
+      body: JSON.stringify(options?.body),
+    });
+
+    if (!response.ok) throw new Error(`${response.status} ${response.statusText}`);
+  }
+}
