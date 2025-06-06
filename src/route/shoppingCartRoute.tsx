e169b61c7fb320d7e45dc08e65ba559e2fdf6a71
mun-kyeong <horse6953@naver.com>
2025-06-07 02:20:32 +0900

    feat: 주문 확인 경로 추가

--- src/route/shoppingCartRoute.tsx
+++ src/route/shoppingCartRoute.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import { ShoppingCart } from "../domains/shopping-cart/page/shoppingCart";
 import { PaymentComplete } from "../domains/paymentComplete/page/PaymentComplete";
 import NotFoundPage from "../page/NotFoundPage";
+import OrderConfirm from "../domains/orderConfirm/orderConfirm";
 
 export const shoppingCartRoute = createBrowserRouter(
   [
@@ -14,6 +15,13 @@ export const shoppingCartRoute = createBrowserRouter(
       path: "/payment-complete",
       element: <PaymentComplete />,
     },
+    {
+      path: "/order-confirm",
+      element: <OrderConfirm />,
+    },
   ],
-  {}
+  {
+    basename: "/react-shopping-cart",
+    // basename은 배포 시에 사용되는 경로를 지정합니다.
+  }
 );
