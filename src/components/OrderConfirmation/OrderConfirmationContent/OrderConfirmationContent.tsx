42cd9b8d40e92946721d971962c55c1197316468
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-06-07 22:30:08 +0900

    refactor: useCoupons 훅을 사용하여 실제 쿠폰 데이터 가져오기

--- src/components/OrderConfirmation/OrderConfirmationContent/OrderConfirmationContent.tsx
+++ src/components/OrderConfirmation/OrderConfirmationContent/OrderConfirmationContent.tsx
@@ -5,48 +5,7 @@ import { getSelectedCartItemsCount } from "../../../util/cart/getSelectedCartIte
 import OrderList from "../OrderList/OrderList";
 import OrderCard from "../OrderCard/OrderCard";
 import ApplyCoupon from "../Coupon/ApplyCoupon/ApplyCoupon";
-import { Coupon } from "../../../type/Coupons";
-
-const coupons: Coupon[] = [
-  {
-    id: 1,
-    code: "FIXED5000",
-    description: "5,000원 할인 쿠폰",
-    expirationDate: "2025-11-30",
-    discount: 5000,
-    minimumAmount: 100000,
-    discountType: "fixed",
-  },
-  {
-    id: 2,
-    code: "BOGO",
-    description: "2개 구매 시 1개 무료 쿠폰",
-    expirationDate: "2025-06-30",
-    buyQuantity: 2,
-    getQuantity: 1,
-    discountType: "buyXgetY",
-  },
-  {
-    id: 3,
-    code: "FREESHIPPING",
-    description: "5만원 이상 구매 시 무료 배송 쿠폰",
-    expirationDate: "2025-08-31",
-    minimumAmount: 50000,
-    discountType: "freeShipping",
-  },
-  {
-    id: 4,
-    code: "MIRACLESALE",
-    description: "미라클모닝 30% 할인 쿠폰",
-    expirationDate: "2025-07-31",
-    discount: 30,
-    availableTime: {
-      start: "04:00:00",
-      end: "07:00:00",
-    },
-    discountType: "percentage",
-  },
-];
+import useCoupons from "../../../hooks/useCoupons/useCoupons";
 
 interface OrderConfirmationContentProps {
   selectedCartItems: CartItem[];
@@ -57,10 +16,7 @@ function OrderConfirmationContent({
 }: OrderConfirmationContentProps) {
   const selectedCartIds = selectedCartItems.map((item) => item.id);
   const selectedCartItemsCount = getSelectedCartItemsCount(selectedCartItems);
-
-  // coupons 가져오기
-  // 쿠폰 적용시키기
-  // 선택된 쿠폰
+  const { coupons } = useCoupons();
 
   return (
     <Styled.Container>
