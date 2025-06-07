3db14ca02e3ac61da3036b659fc946f21bee697f
dev-dino22 <wlsskdud02@naver.com>
2025-06-08 03:49:57 +0900

    refactor: 상품 아이템 수량 표시 추가 및 스타일 수정

--- src/pages/order-confirm/order-content/OrderList.tsx
+++ src/pages/order-confirm/order-content/OrderList.tsx
@@ -28,10 +28,13 @@ function OrderList() {
               />
 
               <ProductInfo aria-label="상품 정보" role="cart-product-info">
-                <ProductName>{cart.product.name}</ProductName>
-                <ProductPrice>
-                  {formatKRWString(cart.product.price)}
-                </ProductPrice>
+                <NamePriceContainer>
+                  <ProductName>{cart.product.name}</ProductName>
+                  <ProductPrice>
+                    {formatKRWString(cart.product.price)}
+                  </ProductPrice>
+                </NamePriceContainer>
+                <ProductQuantity>{cart.quantity}개</ProductQuantity>
               </ProductInfo>
             </ItemContainer>
           </ItemWithCheckboxContainer>
@@ -69,11 +72,21 @@ const ItemContainer = styled.div`
   align-items: flex-start;
 `;
 
+const NamePriceContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 4px;
+  padding: 4px 0;
+`;
+
 const ProductInfo = styled.div`
   flex: 1;
   display: flex;
   flex-direction: column;
   gap: 8px;
+  height: 100%;
+  justify-content: space-between;
 
   align-items: flex-start;
 `;
@@ -83,3 +96,8 @@ const ProductName = styled.p`
 `;
 
 const ProductPrice = styled.p``;
+
+const ProductQuantity = styled.p`
+  font-size: 14px;
+  color: #888;
+`;
