aeaf8d6504f7a35fb3c43cca27f61283f08f20de
wo-o29 <woogur29@gmail.com>
2025-06-07 20:20:10 +0900

    refactor: ApplyCouponModal 도메인 로직 추가

--- src/pages/order-confirm/OrderConfirmContent/PaymentContent/ApplyCouponModal/ApplyCouponModal.tsx
+++ src/pages/order-confirm/OrderConfirmContent/PaymentContent/ApplyCouponModal/ApplyCouponModal.tsx
@@ -4,41 +4,71 @@ import Modal from "@/shared/components/Modal";
 import * as S from "./ApplyCouponModal.styled";
 import CouponList from "./CouponList/CouponList";
 import CouponItem from "./CouponList/CouponItem/CouponItem";
-import { getCouponInfo } from "./utils/getCouponInfo";
+import { useCoupon } from "@/pages/order-confirm/hooks/useCoupon";
+import { CartItemType } from "@/apis/cartItems/cartItem.type";
+import { useEffect } from "react";
+import { MAX_SELECTED_COUPON_COUNT } from "@/domains/constants/coupon";
 
 type ApplyCouponModalProps = {
   isOpen: boolean;
+  orderList: CartItemType[];
   couponList: Coupon[];
+  deliveryPrice: number;
   onRequestClose: () => void;
-  onApplyCoupon: () => void;
+  onApplyCoupon: (discount: number) => void;
 };
 
 export default function ApplyCouponModal({
   isOpen,
+  orderList,
   couponList,
+  deliveryPrice,
   onRequestClose,
   onApplyCoupon,
 }: ApplyCouponModalProps) {
+  const {
+    getIsSelectedId,
+    toggleSelectedId,
+    getIsCouponIdDisabled,
+    discountAmount,
+  } = useCoupon({ orderList, couponList, deliveryPrice });
+
+  useEffect(
+    function autoSelectMaxDiscount() {
+      onApplyCoupon(discountAmount);
+    },
+    [onApplyCoupon, discountAmount]
+  );
+
   if (!isOpen) {
     return null;
   }
 
+  const handleApplyCouponButtonClick = () => {
+    onApplyCoupon(discountAmount);
+    onRequestClose();
+  };
+
   return (
     <Modal title="쿠폰을 선택해 주세요" onRequestClose={onRequestClose}>
       <S.ApplyCouponModalContainer>
-        <InfoText>쿠폰은 최대 2개까지 사용할 수 있습니다.</InfoText>
+        <InfoText>
+          쿠폰은 최대 {MAX_SELECTED_COUPON_COUNT}개까지 사용할 수 있습니다.
+        </InfoText>
         <CouponList>
           {couponList.map((coupon) => (
             <CouponItem
               key={coupon.id}
               coupon={coupon}
-              couponInfoList={getCouponInfo(coupon)}
+              isSelected={getIsSelectedId(coupon.id)}
+              isDisabled={getIsCouponIdDisabled(coupon.id)}
+              onToggleSelect={toggleSelectedId}
             />
           ))}
         </CouponList>
       </S.ApplyCouponModalContainer>
-      <S.ApplyButton type="button" onClick={onApplyCoupon}>
-        총 {"6,000원"} 할인 쿠폰 사용하기
+      <S.ApplyButton type="button" onClick={handleApplyCouponButtonClick}>
+        총 {discountAmount.toLocaleString()}원 할인 쿠폰 사용하기
       </S.ApplyButton>
     </Modal>
   );
