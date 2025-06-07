593cac19ec9b45ec86bc07a728a63590fb4b9537
thgml05 <aisohii@naver.com>
2025-06-07 15:22:07 +0900

    feat: useValidateCoupon 훅에 최소 주문 금액 유효성 검사하는 로직 추가

--- src/hooks/useValidateCoupon.ts
+++ src/hooks/useValidateCoupon.ts
@@ -1,18 +1,24 @@
 import { AvailableCoupon, Coupon } from '../types/coupon';
-import { isExpired, isMiracleMorning } from '../utils/coupon';
+import { isExpired, isMiracleMorning, isMinimumAmount } from '../utils/coupon';
 
-function useValidateCoupon(couponList: Coupon[]) {
+function useValidateCoupon(couponList: Coupon[], subTotal: number) {
   const availableCouponList: AvailableCoupon[] = [];
 
   couponList.forEach((coupon) => {
-    const expired = isExpired(coupon);
+    let expiredFlag = isExpired(coupon.expirationDate);
 
     if (coupon.description.includes('미라클모닝')) {
-      const nowExpired = !isMiracleMorning();
-      availableCouponList.push({ ...coupon, isExpired: nowExpired });
-    } else {
-      availableCouponList.push({ ...coupon, isExpired: expired });
+      expiredFlag = !isMiracleMorning();
     }
+
+    if (
+      coupon.minimumAmount !== undefined &&
+      !isMinimumAmount(coupon.minimumAmount, subTotal)
+    ) {
+      expiredFlag = true;
+    }
+
+    availableCouponList.push({ ...coupon, isExpired: expiredFlag });
   });
 
   return { availableCouponList };
