c4ad835d52f6f57ef4862e95ec04fad7e476005e
Minje Sung <minje813@dgu.ac.kr>
2025-06-07 18:21:16 +0900

    remove: 미사용 유틸 함수 삭제

--- src/utils/checkIsBetweenTime.ts
+++
@@ -1,8 +0,0 @@
-const checkIsBetweenTime = (startHour: number, endHour: number) => {
-  const now = new Date();
-  const currentHour = now.getHours();
-
-  return currentHour >= startHour && currentHour < endHour;
-};
-
-export default checkIsBetweenTime;
--- src/utils/checkIsOverDate.ts
+++
@@ -1,8 +0,0 @@
-const checkIsOverToday = (year: number, month: number, day: number) => {
-  const today = new Date();
-  const givenDay = new Date(year, month - 1, day - 1, 23, 59, 59);
-
-  return today < givenDay;
-};
-
-export default checkIsOverToday;
