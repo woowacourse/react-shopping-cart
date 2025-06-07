f49b55bbe5411fad52899d9558bc8402654ec50b
guesung <gueit214@naver.com>
2025-06-07 11:21:33 +0900

    fix: BaseApi 대문자 제거

--- src/apis/baseApi.ts
+++
@@ -1,9 +0,0 @@
-import { CONFIG } from "@/constants";
-import Api from "@/modules/Api/Api";
-
-export const baseApi = new Api(CONFIG.baseUrl, {
-  headers: {
-    Authorization: `Basic ${CONFIG.token}`,
-    "Content-Type": "application/json",
-  },
-});
