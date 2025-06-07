b8bdd8153a5fafdc157c6520d6ec2f5c1fb69fb8
thgml05 <aisohii@naver.com>
2025-06-07 21:28:25 +0900

    feat: useCouponCombos 커스텀 훅 추가
    - 쿠폰 경우의 수 계산
    - 최대 할인 적용 쿠폰 결과 계산

---
+++ src/hooks/useCouponCombos.ts
@@ -0,0 +1,32 @@
+import { CartItemProps } from '../types/cartItem';
+import { Coupon } from '../types/coupon';
+import { simulateCombo } from '../utils/coupon';
+
+const useCouponCombos = (
+  cartItem: CartItemProps[],
+  couponList: Coupon[],
+  subTotal: number,
+  baseShippingFee: number
+) => {
+  const combos: Coupon[][] = [];
+
+  for (let i = 0; i < couponList.length; i++) {
+    combos.push([couponList[i]]);
+  }
+
+  for (let i = 0; i < couponList.length; i++) {
+    for (let j = i + 1; j < couponList.length; j++) {
+      combos.push([couponList[i], couponList[j]]);
+    }
+  }
+
+  const result = combos.map((combo) => {
+    return simulateCombo(cartItem, combo, subTotal, baseShippingFee);
+  });
+
+  result.sort((a, b) => a.finalPayable - b.finalPayable);
+
+  return result[0];
+};
+
+export default useCouponCombos;
