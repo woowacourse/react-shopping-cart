42d6ef4ce73c06be59598bde591278f8b0785108
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-06-07 20:56:15 +0900

    feat: CouponCard 컴포넌트에 CommonCoupon 추가 및 props 수정

--- src/components/OrderConfirmation/Coupon/CouponCard/CouponCard.tsx
+++ src/components/OrderConfirmation/Coupon/CouponCard/CouponCard.tsx
@@ -1,14 +1,27 @@
 import { Coupon } from "../../../../type/Coupons";
 import * as Styled from "./CouponCard.style";
+import CommonCoupon from "./SubCouponCards/CommonCoupon/CommonCoupon";
 
 interface CouponCardProps {
   coupon: Coupon;
+  isSelected: boolean;
+  handleSelectCoupon: (id: number) => void;
 }
 
-function CouponCard({ coupon }: CouponCardProps) {
+function CouponCard({
+  coupon,
+  isSelected,
+  handleSelectCoupon,
+}: CouponCardProps) {
   return (
     <li>
-      <Styled.Container></Styled.Container>
+      <Styled.Container>
+        <CommonCoupon
+          coupon={coupon}
+          isSelected={true}
+          handleSelectCoupon={() => {}}
+        />
+      </Styled.Container>
     </li>
   );
 }
