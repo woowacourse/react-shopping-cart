b5c438664dd7a62f522a6f396ae6bcd151deb9c8
hoyyChoi <hoyeon8165@gmail.com>
2025-06-07 21:29:24 +0900

    feat: couponModal 상세 정보 ui 추가

--- src/pages/OrderConfirmPage/components/CouponModal/index.tsx
+++ src/pages/OrderConfirmPage/components/CouponModal/index.tsx
@@ -1,8 +1,11 @@
+import * as S from "./CouponModal.styled";
 import { Modal } from "hoyychoi-modal-component";
 import Button from "../../../../components/common/Button";
 import { CouponResponse } from "../../../../types/coupon";
 import CouponList from "../CouponList";
-import { useTempCoupon } from "./useTempCoupon";
+import { useTempCoupon } from "../../../../hooks/order/useTempCoupon";
+import Text from "../../../../components/common/Text";
+import GuideSign from "../../../../components/icons/GuideSign";
 
 interface CouponModalProps {
   coupons: CouponResponse[];
@@ -33,7 +36,11 @@ const CouponModal = ({
           <Modal.Title>쿠폰을 선택해 주세요</Modal.Title>
         </Modal.Header>
 
-        <Modal.Body>
+        <Modal.Body style={{ display: "flex", flexDirection: "column", gap: "17px" }}>
+          <S.Wrap gap={4}>
+            <GuideSign />
+            <Text variant="body-2">쿠폰은 최대 2개까지 사용할 수 있습니다.</Text>
+          </S.Wrap>
           <CouponList
             couponData={coupons}
             availableCoupons={availableCoupons}
