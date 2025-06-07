d7b0ec4e347000a18457830cd13b0f1b95ac75d1
AHHYUNJU <wndkgus0225@naver.com>
2025-06-08 01:32:19 +0900

    feat: Modal 레이아웃 구현

--- src/components/OrderCheck/Modal/Modal.styles.ts
+++ src/components/OrderCheck/Modal/Modal.styles.ts
@@ -21,9 +21,9 @@ export const ModalBackground = styled.div<{
 
 export const ModalContainer = styled.div`
   position: absolute;
-  top: 161px;
+  top: 135px;
   width: 382px;
-  height: 614px;
+  height: 620px;
   background-color: white;
   gap: 32px;
   border-radius: 8px;
@@ -50,4 +50,72 @@ export const ModalHeaderTitle = styled.span`
 export const ModalHeaderCloseImg = styled.img`
   width: 14px;
   height: 14px;
+`;
+
+export const ModalBody = styled.div`
+  width: 318px;
+  height: 432px;
+  gap: 16px;
+`;
+
+export const Info = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 4px;
+`;
+
+export const InfoImg = styled.img`
+  width: 16px;
+  height: 16px;
+`;
+
+export const InfoDescription = styled.span`
+  font-weight: 500;
+  font-size: 12px;
+`;
+
+export const CouponList = styled.div`
+  width: 318px;
+  height: 400px;
+  gap: 24px;
+`;
+
+export const Item = styled.div`
+  width: 318px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  padding: 10px 0;
+`;
+
+export const ItemTitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+export const ItemTitle = styled.span`
+  font-weight: 700;
+  font-size: 16px;
+`;
+
+export const ItemInfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
+
+export const CouponInfo = styled.span`
+  font-weight: 500;
+  font-size: 12px;
+`;
+
+export const ModalButton = styled.button`
+  width: 318px;
+  height: 44px;
+  gap: 12px;
+  border-radius: 8px;
+  background-color: #333333;
+  color: #ffffff;
 `;
--- src/components/OrderCheck/Modal/Modal.tsx
+++ src/components/OrderCheck/Modal/Modal.tsx
@@ -1,4 +1,8 @@
 import Close from "../../../assets/Close.png";
+import Info from "../../../assets/Info.png";
+import Hr from "../../common/Hr/Hr";
+import CheckBox from "../../common/CheckBox/CheckBox";
+import { coupons } from "../Coupon/mocks/coupons";
 import * as S from "./Modal.styles";
 
 interface ModalProps {
@@ -21,6 +25,33 @@ export default function Modal({ isModalOpen, onClose }: ModalProps) {
             </S.ModalHeaderTitle>
             <S.ModalHeaderCloseImg src={Close} onClick={onClose} />
           </S.ModalHeader>
+          <S.ModalBody>
+            <S.Info>
+              <S.InfoImg src={Info}></S.InfoImg>
+              <S.InfoDescription>
+                쿠폰은 최대 2개까지 사용할 수 있습니다.
+              </S.InfoDescription>
+            </S.Info>
+            <S.CouponList>
+              {coupons.map((coupon) => (
+                <S.Item key={coupon.id}>
+                  <Hr />
+                  <S.ItemTitleWrapper>
+                    <CheckBox type="checkbox" />
+                    <S.ItemTitle>{coupon.title}</S.ItemTitle>
+                  </S.ItemTitleWrapper>
+                  <S.ItemInfoWrapper>
+                    <S.CouponInfo>{coupon.expiry}</S.CouponInfo>
+                    {coupon.minOrder && (
+                      <S.CouponInfo>{coupon.minOrder}</S.CouponInfo>
+                    )}
+                    {coupon.info && <S.CouponInfo>{coupon.info}</S.CouponInfo>}
+                  </S.ItemInfoWrapper>
+                </S.Item>
+              ))}
+            </S.CouponList>
+          </S.ModalBody>
+          <S.ModalButton>총 6,000원 할인 쿠폰 사용하기</S.ModalButton>
         </S.ModalContainer>
       </S.ModalBackground>
     </>
