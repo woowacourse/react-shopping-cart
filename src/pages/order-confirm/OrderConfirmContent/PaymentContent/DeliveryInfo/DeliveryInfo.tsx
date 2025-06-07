01e49009ce7519d62658e5e3f08404f624dcfd4b
wo-o29 <woogur29@gmail.com>
2025-06-07 21:01:58 +0900

    feat: 제주도 및 도서 산간 지역 aria-label 속성 추가

--- src/pages/order-confirm/OrderConfirmContent/PaymentContent/DeliveryInfo/DeliveryInfo.tsx
+++ src/pages/order-confirm/OrderConfirmContent/PaymentContent/DeliveryInfo/DeliveryInfo.tsx
@@ -13,7 +13,11 @@ export default function DeliveryInfo({
   return (
     <S.Container>
       <S.Title>배송 정보</S.Title>
-      <CheckBox isChecked={isChecked} onClick={onClick}>
+      <CheckBox
+        isChecked={isChecked}
+        onClick={onClick}
+        aria-label="제주도 및 도서 산간 지역"
+      >
         제주도 및 도서 산간 지역
       </CheckBox>
     </S.Container>
