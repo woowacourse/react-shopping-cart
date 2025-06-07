f9d8fbb7678cd43483bbbd947a4ff2b7d30e0d12
SuHwa Lee <tnghk9611@naver.com>
2025-06-07 17:47:05 +0900

    feat: 쿠폰 받아오는 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,21 @@ import CartPage from "./pages/CartPage/CartPage";
 import { CartProvider } from "./stores/CartContext";
 import { SelectProvider } from "./stores/SelectContext";
 import OrderCompletePage from "./pages/OrderCompletePage/OrderCompletePage";
+import { CouponProvider } from "./stores/CouponContext";
+import OrderCheckPage from "./pages/OrderCheckPage/OrderCheckPage";
 
 function App() {
   return (
     <BrowserRouter>
       <CartProvider>
         <SelectProvider>
-          <Routes>
-            <Route path="/" element={<CartPage />} />
-            <Route path="/complete" element={<OrderCompletePage />} />
-          </Routes>
+          <CouponProvider>
+            <Routes>
+              <Route path="/" element={<CartPage />} />
+              <Route path="/check" element={<OrderCheckPage />} />
+              <Route path="/complete" element={<OrderCompletePage />} />
+            </Routes>
+          </CouponProvider>
         </SelectProvider>
       </CartProvider>
     </BrowserRouter>
--- src/types/types.ts
+++ src/types/types.ts
@@ -17,3 +17,45 @@ export interface ResponseCartItem {
 export interface ResponseData<T> {
   content: T[];
 }
+
+type DiscountType = "fixed" | "buyXgetY" | "freeShipping" | "percentage";
+
+interface BaseCoupon {
+  id: number;
+  code: string;
+  description: string;
+  expirationDate: string;
+  discountType: DiscountType;
+}
+
+interface FixedCoupon extends BaseCoupon {
+  discountType: "fixed";
+  discount: number;
+  minimumAmount: number;
+}
+
+interface BuyXGetYCoupon extends BaseCoupon {
+  discountType: "buyXgetY";
+  buyQuantity: number;
+  getQuantity: number;
+}
+
+interface FreeShippingCoupon extends BaseCoupon {
+  discountType: "freeShipping";
+  minimumAmount: number;
+}
+
+interface PercentageCoupon extends BaseCoupon {
+  discountType: "percentage";
+  discount: number;
+  availableTime: {
+    start: string;
+    end: string;
+  };
+}
+
+export type CouponType =
+  | FixedCoupon
+  | BuyXGetYCoupon
+  | FreeShippingCoupon
+  | PercentageCoupon;
