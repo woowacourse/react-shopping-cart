46fb17c699fe641033585355a3739e95c3f3a773
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-06-07 21:15:57 +0900

    feat: CouponCard 컴포넌트에 FixedDiscountCoupon 렌더링 기능 추가

--- src/components/OrderConfirmation/Coupon/CouponCard/CouponCard.tsx
+++ src/components/OrderConfirmation/Coupon/CouponCard/CouponCard.tsx
@@ -1,6 +1,7 @@
-import { Coupon } from "../../../../type/Coupons";
+import { Coupon, CouponType } from "../../../../type/Coupons";
 import * as Styled from "./CouponCard.style";
 import CommonCoupon from "./SubCouponCards/CommonCoupon/CommonCoupon";
+import FixedDiscountCoupon from "./SubCouponCards/FixedDiscountCoupon/FixedDiscountCoupon";
 
 interface CouponCardProps {
   coupon: Coupon;
@@ -13,6 +14,13 @@ function CouponCard({
   isSelected,
   handleSelectCoupon,
 }: CouponCardProps) {
+  const renderSpecificCoupon = () => {
+    switch (coupon.discountType) {
+      case CouponType.FIXED:
+        return <FixedDiscountCoupon coupon={coupon} />;
+    }
+  };
+
   return (
     <li>
       <Styled.Container>
@@ -21,6 +29,7 @@ function CouponCard({
           isSelected={true}
           handleSelectCoupon={() => {}}
         />
+        {renderSpecificCoupon()}
       </Styled.Container>
     </li>
   );
