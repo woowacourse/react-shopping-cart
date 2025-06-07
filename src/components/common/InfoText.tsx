66802d0d508ea71aba7013f203690250a8ca66b6
dev-dino22 <wlsskdud02@naver.com>
2025-06-07 18:54:30 +0900

    feat: InfoText 공용 컴포넌트 구현

---
+++ src/components/common/InfoText.tsx
@@ -0,0 +1,28 @@
+import styled from "@emotion/styled";
+
+function InfoText({ contentText }: { contentText: string }) {
+  return (
+    <InfoBox>
+      <InfoIcon src="./assets/icons/Info.svg" alt="info 아이콘" />
+      <InfoMessage>{contentText}</InfoMessage>
+    </InfoBox>
+  );
+}
+
+export default InfoText;
+
+const InfoBox = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  gap: 4px;
+`;
+const InfoMessage = styled.p`
+  font-size: 12px;
+  font-weight: 500;
+`;
+
+const InfoIcon = styled.img`
+  width: 14px;
+  height: 14px;
+`;
