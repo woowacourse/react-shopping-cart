6e771e3df9acb2fabd903c73949af722878df8b5
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-06-07 21:28:41 +0900

    feat: CouponCard 컴포넌트에 FreeShippingCoupon 렌더링 기능 추가

--- src/components/OrderConfirmation/Coupon/CouponCard/CouponCard.tsx
+++ src/components/OrderConfirmation/Coupon/CouponCard/CouponCard.tsx
@@ -2,6 +2,7 @@ import { Coupon, CouponType } from "../../../../type/Coupons";
 import * as Styled from "./CouponCard.style";
 import CommonCoupon from "./SubCouponCards/CommonCoupon/CommonCoupon";
 import FixedDiscountCoupon from "./SubCouponCards/FixedDiscountCoupon/FixedDiscountCoupon";
+import FreeShippingCoupon from "./SubCouponCards/FreeShippingCoupon/FreeShippingCoupon";
 
 interface CouponCardProps {
   coupon: Coupon;
@@ -18,6 +19,12 @@ function CouponCard({
     switch (coupon.discountType) {
       case CouponType.FIXED:
         return <FixedDiscountCoupon coupon={coupon} />;
+      case CouponType.FREE_SHIPPING:
+        return <FreeShippingCoupon coupon={coupon} />;
+
+      case CouponType.BUY_X_GET_Y:
+      default:
+        return null;
     }
   };
 
