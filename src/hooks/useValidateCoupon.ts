248d5fd9de269c5814d09a503ed9f943530a909d
thgml05 <aisohii@naver.com>
2025-06-07 15:01:49 +0900

    refactor: useValidateCoupon 훅 미라클모닝 시간 유효 검사 로직 오류 수정
    - available 쿠폰 리스트에 불필요하게 쿠폰 정보가 한번 더 업데이트 되는 오류 수정

--- src/hooks/useValidateCoupon.ts
+++ src/hooks/useValidateCoupon.ts
@@ -1,18 +1,17 @@
 import { AvailableCoupon, Coupon } from '../types/coupon';
-import { isExpired } from '../utils/coupon';
+import { isExpired, isMiracleMorning } from '../utils/coupon';
 
 function useValidateCoupon(couponList: Coupon[]) {
   const availableCouponList: AvailableCoupon[] = [];
 
   couponList.forEach((coupon) => {
-    if (!isExpired(coupon)) {
-      availableCouponList.push({ ...coupon, isExpired: false });
-    } else {
-      availableCouponList.push({ ...coupon, isExpired: true });
-    }
+    const expired = isExpired(coupon);
 
     if (coupon.description.includes('미라클모닝')) {
-      availableCouponList.push({ ...coupon, isExpired: true });
+      const nowExpired = !isMiracleMorning();
+      availableCouponList.push({ ...coupon, isExpired: nowExpired });
+    } else {
+      availableCouponList.push({ ...coupon, isExpired: expired });
     }
   });
 
