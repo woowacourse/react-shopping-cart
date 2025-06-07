03afdfeed2bf1b1b78e4cfba624925dd51362894
wo-o29 <woogur29@gmail.com>
2025-06-07 20:14:54 +0900

    feat: 쿠폰이 disabled 한지 검사하는 유틸 함수 추가

---
+++ src/pages/order-confirm/utils/getCouponDisabled.ts
@@ -0,0 +1,105 @@
+import { CartItemType } from "@/apis/cartItems/cartItem.type";
+import {
+  BuyXGetYCoupon,
+  Coupon,
+  FixedCoupon,
+  FreeShippingCoupon,
+  PercentageCoupon,
+} from "@/apis/coupon/coupon.type";
+import { FREE_DELIVERY_LIMIT } from "@/domains/constants/delivery";
+import { getOrderTotalPrice } from "@/domains/utils/getOrderTotalPrice";
+import { getIsExpiredDate } from "@/shared/utils/getIsExpiredDate";
+import { getIsTimeAvailable } from "@/shared/utils/getIsTimeAvailable";
+
+export const getIsCouponDisabled = (
+  coupon: Coupon,
+  orderList: CartItemType[]
+) => {
+  switch (coupon.discountType) {
+    case "fixed": {
+      return getIsFixedCouponDisabled(coupon, orderList);
+    }
+    case "buyXgetY": {
+      return getIsBuyXGetYCouponDisabled(coupon, orderList);
+    }
+    case "freeShipping": {
+      return getIsFreeShippingCouponDisabled(coupon, orderList);
+    }
+    case "percentage": {
+      return getIsPercentageCouponDisabled(coupon);
+    }
+    default:
+      return false;
+  }
+};
+
+const getIsFixedCouponDisabled = (
+  coupon: FixedCoupon,
+  orderList: CartItemType[]
+) => {
+  const { minimumAmount, expirationDate } = coupon;
+  if (getIsExpiredDate(expirationDate)) {
+    return true;
+  }
+
+  const orderTotalPrice = getOrderTotalPrice(orderList);
+  if (orderTotalPrice < minimumAmount) {
+    return true;
+  }
+
+  return false;
+};
+
+const getIsBuyXGetYCouponDisabled = (
+  coupon: BuyXGetYCoupon,
+  orderList: CartItemType[]
+) => {
+  const { expirationDate, buyQuantity, getQuantity } = coupon;
+  if (getIsExpiredDate(expirationDate)) {
+    return true;
+  }
+
+  const isAllBelowRequiredQuantity = orderList.every(
+    ({ quantity }) => quantity < buyQuantity + getQuantity
+  );
+  if (isAllBelowRequiredQuantity) {
+    return true;
+  }
+
+  return false;
+};
+
+const getIsFreeShippingCouponDisabled = (
+  coupon: FreeShippingCoupon,
+  orderList: CartItemType[]
+) => {
+  const { expirationDate, minimumAmount } = coupon;
+  if (getIsExpiredDate(expirationDate)) {
+    return true;
+  }
+
+  const orderTotalPrice = getOrderTotalPrice(orderList);
+  if (orderTotalPrice < minimumAmount) {
+    return true;
+  }
+
+  if (orderTotalPrice >= FREE_DELIVERY_LIMIT) {
+    return true;
+  }
+
+  return false;
+};
+
+const getIsPercentageCouponDisabled = (coupon: PercentageCoupon) => {
+  const { expirationDate, availableTime } = coupon;
+  if (getIsExpiredDate(expirationDate)) {
+    return true;
+  }
+
+  const { start, end } = availableTime;
+  if (!getIsTimeAvailable(start, end)) {
+    return true;
+  }
+
+  return false;
+};
