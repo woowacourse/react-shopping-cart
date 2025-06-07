230bb533c2f1f7f13b472ff3fb35d288d5361d1a
dev-dino22 <wlsskdud02@naver.com>
2025-06-07 18:26:44 +0900

    feat: Button 공용 컴포넌트 구현

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,33 @@
+import styled from "@emotion/styled";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  onClickHandler?: () => void;
+  backgroundColor?: string;
+  type?: "button" | "submit" | "reset";
+}
+
+function Button({ backgroundColor, children, ...props }: ButtonProps) {
+  return (
+    <ButtonContainer style={{ backgroundColor }} {...props}>
+      {children}
+    </ButtonContainer>
+  );
+}
+
+export default Button;
+
+const ButtonContainer = styled.button`
+  width: 100%;
+  background-color: #ffffff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 0;
+  color: #333333bf;
+  border: 1px solid #33333340;
+  border-radius: 4px;
+  &:hover {
+    border: 1px solid #333333;
+    color: #333333;
+  }
+`;
