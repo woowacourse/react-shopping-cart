ede173b2a5e2f9075824891bbfc0f5f31b2e28e1
ShinjungOh <tlswjd9260@gmail.com>
2025-06-09 06:37:00 +0900

    feat: 쿠폰 데이터 및 타입 지정

--- src/types/coupon.ts
+++ src/types/coupon.ts
@@ -1,9 +1,18 @@
 export interface Coupon {
-  id: string;
-  name: string;
-  minimumOrderAmount?: number;
-  discountAmount?: number;
-  discountRate?: number;
+  id: number;
+  code: string;
   description?: string;
   expirationDate: string;
+  discountType?: string;
+  discount?: number; // 할인 금액 or 할인율
+  minimumAmount?: number; // 최소 구매 금액
+  applicableProducts?: string[];
+  applicableCategories?: string[];
+  maxDiscountAmount?: number;
+  buyQuantity?: number;
+  getQuantity?: number;
+  availableTime?: {
+    start: string;
+    end: string;
+  };
 }
