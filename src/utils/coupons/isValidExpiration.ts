39e5fbe23e14d38afb4ee1e05a0feb10c8d7cf22
H0ngJu <hongju4385@gmail.com>
2025-06-09 01:26:42 +0900

    refactor(isValidExpiration): month에 -1 하도록 수정(0부터 시작)

--- src/utils/coupons/isValidExpiration.ts
+++ src/utils/coupons/isValidExpiration.ts
@@ -2,7 +2,7 @@ export const isValidExpiration = (expiration: string): boolean => {
   const [year, month, date] = expiration.split("-").map(Number);
 
   const today = new Date();
-  const expirationDate = new Date(year, month, date);
+  const expirationDate = new Date(year, month - 1, date);
 
   return (
     expirationDate >=
