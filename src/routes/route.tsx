d0702f703d88407e09712f2efb585bf00112dbb3
keemsebeen <sebin0580@gmail.com>
2025-06-09 03:05:14 +0900

    feat: route 수정

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -4,24 +4,38 @@ import { App } from '@/App';
 import { CartPage } from '@/pages/CartPage';
 import { OrderCheckoutPage } from '@/pages/OrderCheckoutPage';
 import { OrderConfirmPage } from '@/pages/OrderConfirmPage';
+import { NotFound } from '@/shared/components/NotFound';
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        {
+          index: true,
+          element: <CartPage />,
+        },
+        {
+          path: '/cart',
+          element: <CartPage />,
+        },
+        {
+          path: '/order-checkout',
+          element: <OrderCheckoutPage />,
+        },
+        {
+          path: '/order-confirm',
+          element: <OrderConfirmPage />,
+        },
+      ],
+    },
+    {
+      path: '*',
+      element: <NotFound />,
+    },
+  ],
   {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '/cart',
-        element: <CartPage />,
-      },
-      {
-        path: '/order-checkout',
-        element: <OrderCheckoutPage />,
-      },
-      {
-        path: '/order-confirm',
-        element: <OrderConfirmPage />,
-      },
-    ],
-  },
-]);
+    basename: '/react-shopping-cart',
+  }
+);
