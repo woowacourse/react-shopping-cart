a9dc7f0c3617cb5c6e37ffdd2c6f7b04443b10a5
jin123457 <jinsol9909@gmail.com>
2025-06-08 18:47:19 +0900

    refactor: cartItem localstorage 유지 로직 수정

--- src/pages/CartPage/hooks/useCartSelection.ts
+++ src/pages/CartPage/hooks/useCartSelection.ts
@@ -1,22 +1,12 @@
 import { CartItemContent } from "../../../types/cartItem";
 import { usePersistedState } from "../../../hooks/usePersistedState";
 import { STORAGE_KEYS } from "../../../constants";
-import { useEffect } from "react";
 
 type HandleCheckChangeType = ({ action, id }: { action: "all" | "each"; id?: number }) => void;
 
 export const useCartSelection = (cartItems: CartItemContent[]) => {
   const initialCheckedIds = cartItems.map((item) => item.id);
   const [checkedIds, setCheckedIds] = usePersistedState<number[]>(STORAGE_KEYS.CART_CHECKED_IDS, initialCheckedIds);
-
-  useEffect(() => {
-    const validIds = cartItems.map((item) => item.id);
-    const filteredIds = checkedIds.filter((id) => validIds.includes(id));
-
-    if (filteredIds.length !== checkedIds.length) {
-      setCheckedIds(filteredIds);
-    }
-  }, [cartItems, checkedIds, setCheckedIds]);
 
   const handleCheckChange: HandleCheckChangeType = ({ action, id }) => {
     if (action === "all") {
