39f7736ad427c5921938c7cb57b1e06c207f2fd1
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-06-08 18:35:11 +0900

    chore: 파일명 대소문자 변경

--- src/components/CouponModal/CouponModal.tsx
+++ src/components/CouponModal/CouponModal.tsx
@@ -2,8 +2,8 @@ import { useState, useEffect } from "react";
 import { createPortal } from "react-dom";
 import { Modal } from "@kaori-killer/modal-component";
 
-import * as CartListStyled from "../../components/ShoppingCart/CartList/CartList.styles";
-import * as ItemStyled from "../../components/ShoppingCart/Item/Item.styles";
+import * as CartListStyled from "../ShoppingCart/CartList/CartList.styles";
+import * as ItemStyled from "../ShoppingCart/Item/Item.styles";
 import WarningBox from "../common/WarningBox/WarningBox";
 import Hr from "../common/Hr/Hr";
 
