e3491e81c8a8a9549fbbfbc1012a4d57651fe0a2
deokbin <deokbin@naver.com>
2025-06-09 01:27:47 +0900

    feat: CouponProvider 추가

--- src/test-utils/wrapper.tsx
+++ src/test-utils/wrapper.tsx
@@ -1,11 +1,14 @@
 import { MemoryRouter } from "react-router-dom";
 import { ToastProvider } from "../contexts/ToastContext";
 import { CartProvider } from "../contexts/CartContext";
+import { CouponProvider } from "../contexts/CouponContext";
 
 const wrapper = ({ children }: React.PropsWithChildren) => (
   <MemoryRouter>
     <ToastProvider>
-      <CartProvider>{children}</CartProvider>
+      <CartProvider>
+        <CouponProvider>{children}</CouponProvider>
+      </CartProvider>
     </ToastProvider>
   </MemoryRouter>
 );
