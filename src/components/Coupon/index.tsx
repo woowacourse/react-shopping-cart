c04776a652e7395a284954ef499fffb48d81fb56
hoyyChoi <hoyeon8165@gmail.com>
2025-06-08 13:56:22 +0900

    fix: 폴더 명 변경

--- src/components/Coupon/index.tsx
+++ src/components/Coupon/index.tsx
@@ -1,4 +1,4 @@
-import formatTime from "../../utils/\bcoupon/formatTime";
+import formatTime from "../../utils/coupon/formatTime";
 import Text from "../common/Text";
 import * as S from "./Coupon.styled";
 import { createContext, useContext } from "react";
