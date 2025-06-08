fde2ff7958703f4a7fa4df300d02b98f69ee6f04
mun-kyeong <horse6953@naver.com>
2025-06-08 20:33:15 +0900

    test: getDeliveryFee 함수 테스트 추가

---
+++ src/domains/orderConfirm/utils/getDeliveryFee.test.ts
@@ -0,0 +1,19 @@
+import { describe, it, expect } from "vitest";
+import { getDeliveryFee } from "./getDeliveryFee";
+
+describe("getDeliveryFee 함수 테스트", () => {
+  it.each([
+    [100_000, false, 0],
+    [150_000, false, 0],
+    [99_999, false, 3000],
+    [50_000, false, 3000],
+    [100_000, true, 3000],
+    [50_000, true, 6000],
+  ])(
+    "orderPrice: %i, isExtraDeliveryArea: %s → deliveryFee: %i",
+    (orderPrice, isExtraDeliveryArea, expected) => {
+      const result = getDeliveryFee({ orderPrice, isExtraDeliveryArea });
+      expect(result).toBe(expected);
+    }
+  );
+});
