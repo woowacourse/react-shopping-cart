b8aee819a28fe624a04f33028d6131addee5152f
H0ngJu <hongju4385@gmail.com>
2025-06-08 12:39:29 +0900

    feat(calculateFreeShippingDiscount): 10만원 이상인 경우 배송비 무료 쿠폰 적용 불가

--- src/utils/calculateFreeShippingDiscount.ts
+++ src/utils/calculateFreeShippingDiscount.ts
@@ -1,4 +1,4 @@
-import { SHIPPING_FEE } from "../constants";
+import { FREE_SHIPPING_MIN_AMOUNT, SHIPPING_FEE } from "../constants";
 import { FreeShippingCoupon } from "../types/type";
 import { isValidExpiration } from "./isValidExpiration";
 
@@ -8,6 +8,7 @@ export const calculateFreeShippingDiscount = (
   isRemoteArea: boolean
 ) => {
   if (!isValidExpiration(coupon.expirationDate)) return 0;
-  if (coupon.minimumAmount > price) return 0;
+  if (coupon.minimumAmount > price || price > FREE_SHIPPING_MIN_AMOUNT)
+    return 0;
   return SHIPPING_FEE + (isRemoteArea ? 3000 : 0);
 };
