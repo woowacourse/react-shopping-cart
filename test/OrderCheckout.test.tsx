8d3264cd02ae5a7fd9d1d52ec538f35221b54fbf
keemsebeen <sebin0580@gmail.com>
2025-06-09 01:42:25 +0900

    test: 10만원이 넘은 경우 무료배송 적용 test 구현

--- test/OrderCheckout.test.tsx
+++ test/OrderCheckout.test.tsx
@@ -56,4 +56,17 @@ describe('주문 확인 페이지를 렌더링 한다.', () => {
       screen.getByText(/총 2종류의 상품 3개를 주문합니다.*최종 결제 금액을 확인해 주세요/)
     ).toBeInTheDocument();
   });
+
+  it('장바구니에 담긴 상품의 총 금액이 10만원을 넘었을 때, 무료배송 쿠폰이 적용된다.', async () => {
+    // Given: 주문 결제 페이지를 렌더링한다.
+    const cartItems = [createCartItem(1, '상품 1', 50000), createCartItem(2, '상품 2', 60000)];
+
+    await act(async () => {
+      renderOrderCheckoutPage(cartItems);
+    });
+
+    // When: 주문 결제 페이지에 진입했을 때, 상품의 총 금액이 10만원을 넘었을 때 무료배송 쿠폰이 적용된다.
+    // Then: 무료배송 쿠폰이 적용되어 배송비가 0원으로 표시된다.
+    expect(screen.getByText('0원')).toBeInTheDocument();
+  });
 });
