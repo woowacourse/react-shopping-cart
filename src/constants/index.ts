71ebb07868c86a5364876d4969c6e1fd04415e31
jin123457 <jinsol9909@gmail.com>
2025-06-08 14:17:50 +0900

    feat: 체크 상태 local storage로 관리

---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+export const STORAGE_KEYS = {
+  CART_CHECKED_IDS: "cartCheckedIds",
+};
--- src/pages/CartPage/hooks/useCartSelection.ts
+++ src/pages/CartPage/hooks/useCartSelection.ts
@@ -1,30 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { CartItemContent, HandleCartItemChangeType } from "../../../types/cartItem";
 import { useCartApi } from "./useCartApi";
+import { storageHandler } from "../../../utils/storageHandler";
+import { STORAGE_KEYS } from "../../../constants";
 
 type HandleCheckChangeType = ({ action, id }: { action: "all" | "each"; id?: number }) => void;
 
+const storageKey = STORAGE_KEYS.CART_CHECKED_IDS;
+
 export const useCartSelection = (cartItems: CartItemContent[]) => {
-  const [checkedIds, setCheckedIds] = useState<number[]>(cartItems.map((item) => item.id));
+  const [checkedIds, setCheckedIds] = useState<number[]>(() => {
+    const savedIds = storageHandler.getItem(storageKey);
+    const validIds = cartItems.map((item) => item.id);
+
+    return savedIds.filter((id: number) => validIds.includes(id));
+  });
+
+  useEffect(() => {
+    storageHandler.setItem<number[]>(storageKey, checkedIds);
+  }, [checkedIds]);
 
   const handleCheckChange: HandleCheckChangeType = ({ action, id }) => {
     if (action === "all") {
       const isAllChecked = cartItems.every((item) => checkedIds.includes(item.id));
-      if (isAllChecked) {
-        setCheckedIds([]);
-      } else {
-        setCheckedIds(cartItems.map((item) => item.id));
-      }
+      const newCheckedIds = isAllChecked ? [] : cartItems.map((item) => item.id);
+      setCheckedIds(newCheckedIds);
     }
 
     if (action === "each" && id) {
       setCheckedIds((prev) => {
-        const newChecked = [...prev];
-        if (newChecked.includes(id)) {
-          return newChecked.filter((checkedId) => checkedId !== id);
+        if (prev.includes(id)) {
+          return prev.filter((checkedId) => checkedId !== id);
         }
-
-        return [...newChecked, id];
+        return [...prev, id];
       });
     }
   };
@@ -33,7 +41,10 @@ export const useCartSelection = (cartItems: CartItemContent[]) => {
 
   const handleCartItemChange: HandleCartItemChangeType = ({ action, id, quantity }) => {
     if (action === "patch") patchCartItem({ id, quantity: quantity! });
-    if (action === "delete") deleteCartItem({ id });
+    if (action === "delete") {
+      deleteCartItem({ id });
+      setCheckedIds((prev) => prev.filter((checkedId) => checkedId !== id));
+    }
   };
 
   return { checkedIds, handleCartItemChange, handleCheckChange };
