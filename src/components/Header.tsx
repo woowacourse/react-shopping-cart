b687a834330685f0aeca756d21c7b7afe84641ea
Ko Daeun <drechsu@gmail.com>
2025-06-08 11:40:52 +0900

    fix : 경로 오류 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import { usePageContext } from '../contexts/PageContext';
+import { usePageContext } from '../contexts/Page/PageContext';
 
 const Header = () => {
   const { page, setPage } = usePageContext();
