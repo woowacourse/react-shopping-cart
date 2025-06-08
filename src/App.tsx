223d47995cdc24f9111113fc452c67e960fff936
ShinjungOh <tlswjd9260@gmail.com>
2025-06-08 11:24:03 +0900

    feat: 결제 금액 확인 페이지 UI 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { BrowserRouter, Route, Routes } from 'react-router';
-import { Global } from '@emotion/react';
 import Layout from './components/Layout/Layout';
 import CartPage from './pages/CartPage';
 import GlobalStyle from './styles/globalStyle';
 import OrderConfirmPage from './pages/OrderConfirmPage';
+import PriceConfirmPage from './pages/PriceConfirmPage';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import { Global } from '@emotion/react';
 import { DataProvider } from './context/DataContext';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
             <Routes>
               <Route path="/" element={<CartPage />} />
               <Route path="/orderConfirm" element={<OrderConfirmPage />} />
+              <Route path="/priceConfirm" element={<PriceConfirmPage />} />
             </Routes>
           </BrowserRouter>
         </DataProvider>
