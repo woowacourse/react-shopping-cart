b1bceadd18e576872cc8c17f9540e7dd4852e245
eunwoo-levi <eunwoo1341@gmail.com>
2025-06-09 03:47:45 +0900

    feat: 쿠폰 로직을 관리하는 useCoupon 훅 구현

---
+++ src/features/coupon/hooks/useCoupons.ts
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react';
+import { Coupon } from '../types/coupon';
+import { getCoupons } from '../api/getCoupons';
+import { CartItem } from '../../cart/api/types/cart';
+import { getCouponDiscountPrice } from '../utils/getCouponDiscountPrice';
+import { validateExpirationDate } from '../utils/validateExpirationDate';
+import { validateAvailableTime } from '../utils/validateAvailableTime';
+import { validateMinimumAmount } from '../utils/validateMinimumAmount';
+
+const useCoupons = () => {
+  const [coupons, setCoupons] = useState<Coupon[]>([]);
+
+  useEffect(() => {
+    const fetchCoupons = async () => {
+      try {
+        const response = await getCoupons();
+        setCoupons(response);
+      } catch (error) {
+        if (error instanceof Error) {
+          console.error('쿠폰 목록을 가져오는 중 오류 발생:', error.message);
+        }
+      }
+    };
+
+    fetchCoupons();
+  }, []);
+
+  const getInvalidCouponIds = (totalPrice: number): number[] => {
+    const currentDate = new Date();
+
+    return coupons
+      .filter(
+        (coupon) =>
+          !validateExpirationDate(currentDate, coupon.expirationDate) ||
+          (coupon.availableTime && !validateAvailableTime(currentDate, coupon.availableTime)) ||
+          (coupon.minimumAmount && !validateMinimumAmount(totalPrice, coupon))
+      )
+      .map((coupon) => coupon.id);
+  };
+
+  const getBestTwoCoupons = (cartItem: CartItem, totalPrice: number, deliveryFee: number): Coupon[] => {
+    const invalidCouponIds = getInvalidCouponIds(totalPrice);
+    const validCoupons = coupons.filter((coupon) => !invalidCouponIds.includes(coupon.id));
+
+    const couponsWithDiscountPrice = validCoupons.map((coupon) => {
+      const discountPrice = getCouponDiscountPrice({ coupon, cartItem, totalPrice, deliveryFee });
+      return { discountPrice, coupon };
+    });
+
+    couponsWithDiscountPrice.sort((a, b) => b.discountPrice - a.discountPrice);
+    return couponsWithDiscountPrice.slice(0, 2).map((item) => item.coupon);
+  };
+
+  return { coupons, getBestTwoCoupons, getInvalidCouponIds };
+};
+
+export default useCoupons;
