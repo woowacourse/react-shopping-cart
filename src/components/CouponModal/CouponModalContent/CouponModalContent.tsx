f6ee719d7df661f49dc46ab624c98d58543faef1
kimyou1102 <yoosk5485@naver.com>
2025-06-08 16:40:34 +0900

    refactor: 쿠폰 목록 더미데이터에서 실제 api 호출값으로 변경

--- src/components/CouponModal/CouponModalContent/CouponModalContent.tsx
+++ src/components/CouponModal/CouponModalContent/CouponModalContent.tsx
@@ -11,7 +11,7 @@ import {
 import Button from '../../Common/Button/Button';
 import { css } from '@emotion/react';
 import { CouponItem } from '../CouponItem/CouponItem';
-import { Coupon } from '../../../types/coupon';
+import useFetchCoupons from '../../../hooks/useFetchCoupons';
 
 interface CouponModalContentProps {
   onChange: (id: string) => void;
@@ -28,46 +28,7 @@ export function CouponModalContent({
 }: CouponModalContentProps) {
   const totalPrice = 6000;
 
-  const DUMMY: Coupon[] = [
-    {
-      id: 1,
-      code: 'FIXED5000',
-      description: '5,000원 할인 쿠폰',
-      expirationDate: '2025-11-30',
-      discount: 5000,
-      minimumAmount: 100000,
-      discountType: 'fixed',
-    },
-    {
-      id: 2,
-      code: 'BOGO',
-      description: '2개 구매 시 1개 무료 쿠폰',
-      expirationDate: '2025-06-30',
-      buyQuantity: 2,
-      getQuantity: 1,
-      discountType: 'buyXgetY',
-    },
-    {
-      id: 3,
-      code: 'FREESHIPPING',
-      description: '5만원 이상 구매 시 무료 배송 쿠폰',
-      expirationDate: '2025-08-31',
-      minimumAmount: 50000,
-      discountType: 'freeShipping',
-    },
-    {
-      id: 4,
-      code: 'MIRACLESALE',
-      description: '미라클모닝 30% 할인 쿠폰',
-      expirationDate: '2025-07-31',
-      discount: 30,
-      availableTime: {
-        start: '04:00:00',
-        end: '07:00:00',
-      },
-      discountType: 'percentage',
-    },
-  ];
+  const { coupons } = useFetchCoupons();
 
   return (
     <>
@@ -82,11 +43,11 @@ export function CouponModalContent({
         <p css={deliveryInfo}>쿠폰은 최대 2개까지 사용할 수 있습니다.</p>
       </div>
       <div css={couponList}>
-        {DUMMY.map((couponData) => (
+        {coupons.map((coupon) => (
           <CouponItem
-            couponData={couponData}
+            couponData={coupon}
             onChange={onChange}
-            isChecked={selectedCouponIds.includes(couponData.id.toString())}
+            isChecked={selectedCouponIds.includes(coupon.id.toString())}
           />
         ))}
       </div>
