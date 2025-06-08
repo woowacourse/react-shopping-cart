36818d5ad9d54ae20768aeddb3bb6905c58434e7
youjeong Jeong <wjddbwjd1205@naver.com>
2025-06-08 21:35:21 +0900

    refactor: useMemo를 사용하여 쿠폰 유효성 검사 최적화

--- src/pages/order-review/hooks/useAvailableCoupons.ts
+++ src/pages/order-review/hooks/useAvailableCoupons.ts
@@ -1,6 +1,7 @@
 import { CouponContent } from '@/api/type';
 import { useOrderListContext } from '@/pages/shopping-cart/context/OrderListProvider';
 import { couponValidator } from '../utils/couponValidator';
+import { useMemo } from 'react';
 
 export const useAvailableCoupons = (
   coupons: CouponContent[],
@@ -8,12 +9,14 @@ export const useAvailableCoupons = (
 ) => {
   const { selectedItems, orderPrice } = useOrderListContext();
 
-  const availableCoupons = couponValidator(
-    coupons,
-    isJejuOrRemoteArea,
-    selectedItems,
-    orderPrice
-  );
+  const availableCoupons = useMemo(() => {
+    return couponValidator(
+      coupons,
+      isJejuOrRemoteArea,
+      selectedItems,
+      orderPrice
+    );
+  }, [coupons, isJejuOrRemoteArea, selectedItems, orderPrice]);
 
   return { availableCoupons };
 };
