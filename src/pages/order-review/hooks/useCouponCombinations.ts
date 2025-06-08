f291ade738750e4f6a4b770b671edcc899006c9e
youjeong Jeong <wjddbwjd1205@naver.com>
2025-06-09 02:10:11 +0900

    refactor: 불필요한 커스텀 훅, 함수 삭제

--- src/pages/order-review/hooks/useCouponCombinations.ts
+++
@@ -1,7 +0,0 @@
-import { CouponContent } from '@/api/type';
-import { getAllCouponCombinationIds } from '../utils/getAllCouponCombinationIds';
-
-export const useCouponCombinations = (availableCoupons: CouponContent[]) => {
-  const allCouponCombinationIds = getAllCouponCombinationIds(availableCoupons);
-  return { allCouponCombinationIds };
-};
