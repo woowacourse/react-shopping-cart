72012d92f4061a55d9edb9a091d8503f2c2e14c2
Ko Daeun <drechsu@gmail.com>
2025-06-08 14:05:32 +0900

    feat : 모달 컴포넌트 구현

---
+++ src/components/Modal/Modal.styled.ts
@@ -0,0 +1,131 @@
+import styled from '@emotion/styled';
+
+type PositionProps = {
+  $position: 'center' | 'bottom';
+};
+type SizeProps = {
+  $size: 'small' | 'medium' | 'large';
+};
+const BackDrop = styled.div<PositionProps>`
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  justify-content: ${({ $position }) =>
+    $position === 'center' ? 'center' : 'flex-end'};
+  align-items: center;
+  top: 0;
+  width: 500px;
+  height: 100%;
+  z-index: 20;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+const ModalLayout = styled.div<PositionProps & SizeProps>`
+  position: relative;
+  width: ${({ $position, $size }) =>
+    $position === 'bottom' ? '100%' : getModalWidth($size)};
+  max-height: 100%;
+  z-index: 500;
+  background-color: white;
+  border-radius: 8px;
+  padding: 20px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+`;
+
+function getModalWidth(size: 'small' | 'medium' | 'large') {
+  switch (size) {
+    case 'small':
+      return '320px';
+    case 'medium':
+      return '480px';
+    case 'large':
+      return '600px';
+    default:
+      return '100%';
+  }
+}
+
+const CloseIcon = styled.img`
+  position: absolute;
+  top: 30px;
+  right: 20px;
+  cursor: pointer;
+`;
+
+const ModalTitle = styled.div`
+  font-size: 24px;
+  font-weight: 900;
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  text-align: start;
+`;
+
+const ModalContents = styled.div`
+  display: flex;
+  margin-top: 56px;
+  height: 100%;
+`;
+
+type ModalButtonProps = {
+  $backgroundColor?: string;
+  $textColor?: string;
+  $size?: 'small' | 'medium' | 'large';
+  $border?: string;
+};
+
+const ModalButton = styled.button<ModalButtonProps>`
+  background-color: ${({ $backgroundColor }) => $backgroundColor || '#ffffff'};
+  color: ${({ $textColor }) => $textColor || '#000000'};
+  width: ${({ $size }) => getSize($size)};
+  border: ${({ $border }) => $border || 'none'};
+  border-radius: 4px;
+  padding: 10px 20px;
+  cursor: pointer;
+  font-size: 16px;
+  margin-top: 20px;
+  &:focus {
+    border: none;
+    outline: none;
+  }
+`;
+
+function getSize(size: 'small' | 'medium' | 'large' | undefined) {
+  switch (size) {
+    case 'small':
+      return '80px';
+    case 'medium':
+      return '120px';
+    case 'large':
+      return '300px';
+    default:
+      return '100%';
+  }
+}
+
+const ModalButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+  width: 100%;
+`;
+
+const ModalInput = styled.input`
+  width: 100%;
+  padding: 10px;
+  border: 1px solid;
+  border-radius: 4px;
+`;
+
+export {
+  BackDrop,
+  ModalLayout,
+  CloseIcon,
+  ModalTitle,
+  ModalContents,
+  ModalButton,
+  ModalButtonContainer,
+  ModalInput,
+};
---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,168 @@
+import closeIcon from '../../assets/close-icon.png';
+import {
+  BackDrop,
+  ModalLayout,
+  CloseIcon,
+  ModalTitle,
+  ModalContents,
+  ModalButton,
+  ModalButtonContainer,
+  ModalInput,
+} from './Modal.styled';
+import { useEffect, useRef } from 'react';
+import { createContext } from 'react';
+import useModalContext from './hooks/useModalContext';
+
+type ModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+  position?: 'center' | 'bottom';
+  size?: 'small' | 'medium' | 'large';
+};
+
+export const ModalContext = createContext<ModalProps | null>(null);
+
+const FOCUSABLE_SELECTORS = `
+  a[href], area[href], input:not([disabled]),
+  select:not([disabled]), textarea:not([disabled]),
+  button:not([disabled]), iframe, object, embed,
+  [tabindex]:not([tabindex="-1"])
+`;
+
+const Modal = ({
+  isOpen = true,
+  onClose,
+  children,
+  position = 'center',
+  size = 'small',
+}: ModalProps) => {
+  const value: ModalProps = {
+    isOpen,
+    onClose,
+    children,
+    position,
+  };
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const modalElement = modalRef.current;
+    if (!modalElement) return;
+
+    const focusableElements =
+      modalRef.current.querySelectorAll<HTMLElement>(FOCUSABLE_SELECTORS);
+    const first = focusableElements[0];
+    const last = focusableElements[focusableElements.length - 1];
+    first?.focus();
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        onClose();
+      }
+
+      if (event.shiftKey && event.key === 'Tab') {
+        // Shift + Tab
+        if (document.activeElement === first) {
+          event.preventDefault();
+          last?.focus();
+        }
+      } else if (event.key === 'Tab') { 
+        // Tab
+        if (document.activeElement === last) {
+          event.preventDefault();
+          first?.focus();
+        }
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClose]);
+
+  return (
+    <>
+      {isOpen && (
+        <ModalContext.Provider value={value}>
+          <BackDrop onClick={onClose} $position={position}>
+            <ModalLayout
+              ref={modalRef}
+              $position={position}
+              $size={size}
+              onClick={(event) => event.stopPropagation()}
+            >
+              {children}
+            </ModalLayout>
+          </BackDrop>
+        </ModalContext.Provider>
+      )}
+    </>
+  );
+};
+
+interface ModalTitleProps {
+  title: string;
+}
+
+const Title = ({ title }: ModalTitleProps) => {
+  return <ModalTitle>{title}</ModalTitle>;
+};
+
+const CloseButton = () => {
+  const modalContext = useModalContext();
+
+  return <CloseIcon src={closeIcon} onClick={modalContext.onClose} />;
+};
+
+interface ModalContentsProps {
+  children: React.ReactNode;
+}
+
+const Contents = ({ children }: ModalContentsProps) => {
+  return <ModalContents>{children}</ModalContents>;
+};
+
+type ButtonProps = {
+  title: string;
+  backgroundColor?: string;
+  textColor?: string;
+  size?: 'small' | 'medium' | 'large';
+  onClick?: () => void;
+  border?: string;
+};
+
+const Button = ({
+  title,
+  backgroundColor,
+  textColor,
+  size,
+  border,
+  onClick,
+}: ButtonProps) => {
+  return (
+    <ModalButtonContainer>
+      <ModalButton
+        $backgroundColor={backgroundColor}
+        $textColor={textColor}
+        $size={size}
+        $border={border}
+        onClick={onClick}
+      >
+        {title}
+      </ModalButton>
+    </ModalButtonContainer>
+  );
+};
+
+const Input = ({ placeholder }: { placeholder: string }) => {
+  return <ModalInput placeholder={placeholder} />;
+};
+
+Modal.Title = Title;
+Modal.CloseButton = CloseButton;
+Modal.Contents = Contents;
+Modal.Button = Button;
+Modal.Input = Input;
+
+export default Modal;
