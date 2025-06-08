c04956d8fc68fbb58635fc35a1f1cac163d3eb20
keemsebeen <sebin0580@gmail.com>
2025-06-09 01:40:15 +0900

    test: 주문 확인 페이지 렌더링 테스트 작성

--- test/OrderCheckout.test.tsx
+++ test/OrderCheckout.test.tsx
@@ -1,4 +1,5 @@
 import { act, render, screen, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 import { vi } from 'vitest';
 
@@ -7,6 +8,15 @@ import { CartItem } from '@/features/Cart/types/Cart.types';
 import { OrderCheckoutPage } from '@/pages/OrderCheckoutPage';
 
 import { createTestCartItems } from './Cart.test';
+import { Coupons } from './Coupons.data';
+import {
+  createCartItem,
+  expectAllCheckboxesDisabled,
+  expectCouponState,
+  expectCouponUsageMessage,
+  mockExpiredCouponsScenario,
+  mockTime,
+} from './test-helper';
 
 const renderOrderCheckoutPage = (customCartItems?: CartItem[]) => {
   const cartItems = customCartItems || createTestCartItems();
@@ -20,3 +30,30 @@ const renderOrderCheckoutPage = (customCartItems?: CartItem[]) => {
 
 vi.mock('@/api/order');
 const mockCouponApi = vi.mocked(couponApi);
+
+describe('주문 확인 페이지를 렌더링 한다.', () => {
+  let user: ReturnType<typeof userEvent.setup>;
+
+  beforeEach(() => {
+    user = userEvent.setup();
+    vi.clearAllMocks();
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
+  it('주문 확인 페이지에 진입했을 때, 주문 확인 페이지가 렌더링된다.', async () => {
+    // Given: 주문 결제 페이지를 렌더링한다.
+    await act(async () => {
+      renderOrderCheckoutPage();
+    });
+
+    // When: 주문 결제 페이지에 진입했을 때, 주문 결제 페이지가 렌더링된다.
+    // Then: 주문 결제 페이지가 렌더링된다.
+    expect(screen.getByText('주문 확인')).toBeInTheDocument();
+    expect(
+      screen.getByText(/총 2종류의 상품 3개를 주문합니다.*최종 결제 금액을 확인해 주세요/)
+    ).toBeInTheDocument();
+  });
+});
