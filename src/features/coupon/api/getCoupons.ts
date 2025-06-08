184b4fb191c523eb34f9dae8a16ff3f021d91581
sanghee01 <sang6299@naver.com>
2025-06-09 00:33:20 +0900

    fix: getCoupons response 타입 올바르게 설정

--- src/features/coupon/api/getCoupons.ts
+++ src/features/coupon/api/getCoupons.ts
@@ -1,10 +1,6 @@
 import { httpClient } from '../../../shared/api/httpClient';
 import { Coupon } from '../types/coupon';
 
-interface CouponsResponse {
-  content: Coupon[];
-}
-
 export const getCoupons = () => {
-  return httpClient.get<CouponsResponse>('/coupons');
+  return httpClient.get<Coupon[]>('/coupons');
 };
