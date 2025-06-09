e04958b5db5c351f7f6fb471389b6322877ada22
sanghee01 <sang6299@naver.com>
2025-06-09 14:44:26 +0900

    refactor: css 코드 컴포넌트 코드 아래에 배치

--- src/shared/ui/NavFooter.tsx
+++ src/shared/ui/NavFooter.tsx
@@ -4,24 +4,6 @@ import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import Button from './Button';
 
-const ButtonCSS = css`
-  width: 100%;
-  height: 100%;
-  background-color: #000;
-  color: #fff;
-  font-size: 16px;
-  font-weight: 700;
-  transition: background-color 0.2s ease;
-  &:hover {
-    background-color: #333;
-  }
-
-  &:disabled {
-    background-color: #bebebe;
-    cursor: not-allowed;
-  }
-`;
-
 interface NavFooterProps {
   title: string;
   onClick: () => void;
@@ -39,4 +21,22 @@ export default function NavFooter({ title, onClick, isDisabled }: NavFooterProps
 const FooterContainer = styled.footer`
   width: 100%;
   height: 64px;
+`;
+
+const ButtonCSS = css`
+  width: 100%;
+  height: 100%;
+  background-color: #000;
+  color: #fff;
+  font-size: 16px;
+  font-weight: 700;
+  transition: background-color 0.2s ease;
+  &:hover {
+    background-color: #333;
+  }
+
+  &:disabled {
+    background-color: #bebebe;
+    cursor: not-allowed;
+  }
 `;
