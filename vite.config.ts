e31e3bc5f1186de075431cfaa8a00cd704739d73
SuHwa Lee <tnghk9611@naver.com>
2025-06-09 17:03:34 +0900

    chore: base 경로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/react-shopping-cart/" : "/",
   plugins: [react()],
   test: {
     environment: "jsdom",
