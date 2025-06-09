be7132ded04c60309947a6ec5f3d4fbc46249878
kimyou1102 <yoosk5485@naver.com>
2025-06-09 13:15:55 +0900

    feat: 결제 확인 페이지 UI 제작

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { OrderConfirm } from './pages/OrderConfirm/OrderConfirm';
 import { ShoppingCart } from './pages/ShoppingCart/ShoppingCart';
+import { Confirm } from './pages/Confirm.tsx/Confirm';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<ShoppingCart />}></Route>
         <Route path="/confirm" element={<OrderConfirm />}></Route>
+        <Route path="/payment-confirm" element={<Confirm />}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Confirm.tsx/Confirm.style.tsx
@@ -0,0 +1,60 @@
+import { css } from '@emotion/react';
+
+const confirmLayout = css`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  align-items: center;
+  justify-content: center;
+  gap: 24px;
+`;
+
+const totalPriceBox = css`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+
+const titleText = css`
+  font-weight: 700;
+  font-size: 24px;
+`;
+
+const subtitleText = css`
+  font-weight: 700;
+  font-size: 16px;
+`;
+
+const bodyTextBox = css`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  align-items: center;
+`;
+const bodyText = css`
+  font-weight: 500;
+  font-size: 12px;
+`;
+
+const backButton = css`
+  border: none;
+  padding: 0px;
+  background-color: transparent;
+  cursor: pointer;
+`;
+
+const backImg = css`
+  width: 21px;
+  height: 21px;
+`;
+
+export {
+  confirmLayout,
+  titleText,
+  subtitleText,
+  totalPriceBox,
+  bodyTextBox,
+  bodyText,
+  backButton,
+  backImg,
+};
