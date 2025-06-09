168fe87369be0c60cbd84688a0510595fa1c956f
thgml05 <aisohii@naver.com>
2025-06-09 13:40:49 +0900

    chore: 아이콘과 이미지 경로 수정

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,7 +1,7 @@
-export const Logo = `./icons/logo.svg`;
-export const Minus = `./icons/minus.svg`;
-export const Plus = `./icons/plus.svg`;
-export const Info = `./icons/info.svg`;
-export const Back = `./icons/back.svg`;
+export const Logo = import.meta.env.BASE_URL + `icons/logo.svg`;
+export const Minus = import.meta.env.BASE_URL + `icons/minus.svg`;
+export const Plus = import.meta.env.BASE_URL + `icons/plus.svg`;
+export const Info = import.meta.env.BASE_URL + `icons/info.svg`;
+export const Back = import.meta.env.BASE_URL + `icons/back.svg`;
 
-export const Default = `./images/default.png`;
+export const Default = import.meta.env.BASE_URL + `images/default.png`;
