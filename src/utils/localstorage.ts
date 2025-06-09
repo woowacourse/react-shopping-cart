6f5050a7df465390e3d1145bcf3d0fc689dd549a
yeongipark <yeongi0111@naver.com>
2025-06-09 15:07:29 +0900

    chore: localstorage 저장 불필요한 로직 삭제

--- src/utils/localstorage.ts
+++ src/utils/localstorage.ts
@@ -1,17 +1,8 @@
 export function setLocalStorageItem(key: string, value: unknown): void {
-  try {
-    localStorage.setItem(key, JSON.stringify(value));
-  } catch (error) {
-    console.error(`Error setting localStorage item with key "${key}":`, error);
-  }
+  localStorage.setItem(key, JSON.stringify(value));
 }
 
 export function getLocalStorageItem<T>(key: string): T | null {
-  try {
-    const item = localStorage.getItem(key);
-    return item ? JSON.parse(item) : null;
-  } catch (error) {
-    console.error(`Error getting localStorage item with key "${key}":`, error);
-    return null;
-  }
+  const item = localStorage.getItem(key);
+  return item ? JSON.parse(item) : null;
 }
