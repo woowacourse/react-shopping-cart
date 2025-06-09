2ffd7609636df3af53f613d17c015b97ea02cb6c
aydenote <dslgpgh@gmail.com>
2025-06-09 17:18:15 +0900

    feat: 쿠폰 타입 정의 및 인터페이스 구현

--- src/types/cart.ts
+++ src/types/cart.ts
@@ -16,3 +16,59 @@ export interface CartInfo {
   id: number;
   quantity: number;
 }
+
+export interface CouponType {
+  id: number;
+  code: string;
+  description: string;
+  discountType: 'fixed' | 'percentage' | 'freeShipping' | 'buyXgetY';
+  discount?: number;
+  minimumAmount?: number;
+  expirationDate: string;
+  availableTime?: {
+    start: string;
+    end: string;
+  };
+  buyQuantity?: number;
+  getQuantity?: number;
+}
+
+export interface GetCartItemsResponse {
+  totalElements: number;
+  totalPages: number;
+  size: number;
+  content: CartItem[];
+  number: number;
+  sort: Sort;
+  pageable: Pageable;
+  numberOfElements: number;
+  first: boolean;
+  last: boolean;
+  empty: boolean;
+}
+export interface CartItem {
+  id: number;
+  quantity: number;
+  product: Product;
+}
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  imageUrl: string;
+  category: string;
+  quantity?: number;
+}
+export interface Sort {
+  empty: boolean;
+  sorted: boolean;
+  unsorted: boolean;
+}
+export interface Pageable {
+  offset: number;
+  sort: Sort;
+  paged: boolean;
+  pageNumber: number;
+  pageSize: number;
+  unpaged: boolean;
+}
