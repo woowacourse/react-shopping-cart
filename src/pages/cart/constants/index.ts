7d0ed2b2599fe8b9b415f1d7c4fb8bf46a3c65b1
hoyyChoi <hoyeon8165@gmail.com>
2025-06-11 01:54:17 +0900

    feat: storage 키값 상수화

--- src/pages/cart/constants/index.ts
+++ src/pages/cart/constants/index.ts
@@ -1,2 +1,5 @@
 export const FREE_DELIVERY_MINIMUM_ORDER_PRICE = 100000;
 export const DEFAULT_DELIVERY_PRICE = 3000;
+
+export const CART_ITEM_IDS_STORAGE_KEY = "cartItemIds";
+export const INITIALIZED_KEY = "isInitialized";
