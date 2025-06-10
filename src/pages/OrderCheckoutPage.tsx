0ca0362039b5e67d9f866ac0d85931adad628b46
keemsebeen <sebin0580@gmail.com>
2025-06-11 08:55:23 +0900

    feat: OrderCheckoutPage에 location.state에 대한 타입 가드 추가

--- src/pages/OrderCheckoutPage.tsx
+++ src/pages/OrderCheckoutPage.tsx
@@ -1,13 +1,14 @@
 import { useLocation } from 'react-router-dom';
 
 import { OrderCheckout } from '@/features/Order/Checkout/components/OrderCheckout';
+import { isCartItemArray } from '@/features/Order/Checkout/utils/isCartItemArray';
 import { EmptyOrder } from '@/features/Order/Confirm/components/EmptyOrder';
 
 export const OrderCheckoutPage = () => {
   const location = useLocation();
   const cartItems = location.state;
 
-  if (cartItems === null) {
+  if (!isCartItemArray(cartItems)) {
     return <EmptyOrder />;
   }
 
