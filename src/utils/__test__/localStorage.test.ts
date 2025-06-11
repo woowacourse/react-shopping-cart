ae1d092daaaf0713f7c5753b832eda8350ab9604
thgml05 <aisohii@naver.com>
2025-06-11 09:50:52 +0900

    test: localStorage 초기값 테스트 코드 추가

--- src/utils/__test__/localStorage.test.ts
+++ src/utils/__test__/localStorage.test.ts
@@ -5,6 +5,12 @@ import {
 } from '../localStorage';
 
 describe('localStorage 유틸 함수 테스트', () => {
+  it('localStorage 데이터가 존재하지 않으면 빈 배열을 반환한다.', () => {
+    const key = 'test';
+    const result = getLocalStorage(key);
+    expect(result).toEqual([]);
+  });
+
   it('getLocalStorage 함수는 존재하는 키에 대한 데이터를 반환한다.', () => {
     const key = 'test';
     const value = 'test';
