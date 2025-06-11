9cea6109d445cf762ebdc5fe13da4f74869c2fa0
guesung <gueit214@naver.com>
2025-06-11 16:58:01 +0900

    chore: 로컬 스토리지 에러 핸들링

--- src/hooks/common/usePersistState/usePersistState.ts
+++ src/hooks/common/usePersistState/usePersistState.ts
@@ -1,8 +1,16 @@
 import { useEffect, useState } from "react";
 
 const getInitialState = <T>(key: string, initialState: T): T => {
-  const persistedState = localStorage.getItem(key);
-  return persistedState ? JSON.parse(persistedState) : initialState;
+  try {
+    const persistedState = localStorage.getItem(key);
+    if (!persistedState) return initialState;
+
+    return JSON.parse(persistedState);
+  } catch (error) {
+    console.warn(`로컬 스토리지 "${key}" 손상된 데이터 제거:`, error);
+    localStorage.removeItem(key);
+    return initialState;
+  }
 };
 
 export default function usePersistState<T>(key: string, initialState: T): [T, React.Dispatch<React.SetStateAction<T>>] {
