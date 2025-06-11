761df8cc9910a077cab4fa2b8a855f258f5908bb
ohgus <anakin1108@naver.com>
2025-06-11 20:28:35 +0900

    refactor: 2자리 숫자에 대응할 수 있게 수정

--- src/routes/pages/OrderCheckPage/utils/hour.ts
+++ src/routes/pages/OrderCheckPage/utils/hour.ts
@@ -1,5 +1,5 @@
 export const getHourFromTime = (time: string = '00:00:00') => {
-  const hour = time.split(':')[0].slice(1);
+  const hourString = time.split(':')[0];
 
-  return hour;
+  return String(parseInt(hourString, 10));
 };
