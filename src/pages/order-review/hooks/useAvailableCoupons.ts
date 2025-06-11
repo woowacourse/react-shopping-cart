0a8682ca12de7c15bddc669bc01ed03146fd1d52
youjeong Jeong <wjddbwjd1205@naver.com>
2025-06-11 21:45:39 +0900

    refactor: 불필요한 useMemo 제거

--- src/pages/order-review/hooks/useAvailableCoupons.ts
+++ src/pages/order-review/hooks/useAvailableCoupons.ts
@@ -1,7 +1,6 @@
 import { CouponContent } from '@/api/type';
 import { useOrderListContext } from '@/pages/shopping-cart/context/OrderListProvider';
 import { couponValidator } from '../utils/couponValidator';
-import { useMemo } from 'react';
 
 export const useAvailableCoupons = (
   coupons: CouponContent[],
@@ -9,14 +8,12 @@ export const useAvailableCoupons = (
 ) => {
   const { selectedItems, orderPrice } = useOrderListContext();
 
-  const availableCoupons = useMemo(() => {
-    return couponValidator(
-      coupons,
-      isJejuOrRemoteArea,
-      selectedItems,
-      orderPrice
-    );
-  }, [coupons, isJejuOrRemoteArea, selectedItems, orderPrice]);
+  const availableCoupons = couponValidator(
+    coupons,
+    isJejuOrRemoteArea,
+    selectedItems,
+    orderPrice
+  );
 
   return { availableCoupons };
 };
