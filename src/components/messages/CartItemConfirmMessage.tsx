7a45e143131e0fd1bb579ea7e84c46e3af502f1c
Ko Daeun <drechsu@gmail.com>
2025-06-12 10:45:14 +0900

    fix : 담긴 카트아이템의 종류, 개수를 선택된 것을 기준으로 변경

--- src/components/messages/CartItemConfirmMessage.tsx
+++ src/components/messages/CartItemConfirmMessage.tsx
@@ -1,15 +1,22 @@
 import styled from '@emotion/styled';
 import { useCartItemsContext } from '../../contexts/CartItems/CartItemsContext';
+import { useCheckCartIdsContext } from '../../contexts/CheckedCartIds/CheckedCartIdsContext';
 
 const CartItemConfirmMessage = () => {
   const { cartItems } = useCartItemsContext();
-  const totalCount = cartItems.reduce(
+  const { checkedCartIds } = useCheckCartIdsContext();
+  const selectedCartItems = cartItems.filter((item) =>
+    checkedCartIds.includes(item.id)
+  );
+
+  const totalCount = selectedCartItems.reduce(
     (total, item) => total + item.quantity,
     0
   );
+
   return (
     <S.message>
-      {`총 ${cartItems.length}종류의 상품 ${totalCount}개를 주문합니다`}
+      {`총 ${checkedCartIds.length}종류의 상품 ${totalCount}개를 주문합니다`}
       <br />
       최종 결제 금액을 확인해 주세요.
     </S.message>
