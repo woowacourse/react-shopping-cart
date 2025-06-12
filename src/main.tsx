3d19e3b68f3ec47ecb89a899c090d61153a2a71c
aydenote <dslgpgh@gmail.com>
2025-06-13 02:43:44 +0900

    fix: render가 중복되어 발생하는 오류 수정

--- src/main.tsx
+++ src/main.tsx
@@ -13,15 +13,10 @@ async function enableMocking() {
 }
 
 enableMocking().then(() => {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
+  const root = ReactDOM.createRoot(document.getElementById('root')!);
+  root.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>,
   );
 });
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
