ef25cdb7b1821da4b939e4a2c777da6dfea61c28
kimyou1102 <yoosk5485@naver.com>
2025-06-12 21:32:23 +0900

    refactor: 사용하지 않는 훅 제거

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,4 @@
 export * from './useCouponSelection';
-export * from './useCouponUI';
 export * from './useFetchCartItems';
 export * from './useFetchCoupons';
 export * from './useSelectedCartItems';
