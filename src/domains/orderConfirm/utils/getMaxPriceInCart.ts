b9ad0c32c57d9e9bfd201fd366521b6add133b71
mun-kyeong <horse6953@naver.com>
2025-06-13 07:01:15 +0900

    refactor: getMaxPriceInCart 함수의 주석 수정

--- src/domains/orderConfirm/utils/getMaxPriceInCart.ts
+++ src/domains/orderConfirm/utils/getMaxPriceInCart.ts
@@ -7,10 +7,9 @@ interface GetMaxPriceInSelectedCartParams {
 // ================== 최개 가격 계산 함수 ==================
 
 /**
- * 2+1 적용이 가능한 아이템들 중에서 최대 가격을 계산하는 함수입니다.
- * @parm {cartItems} - 장바구니 아이템들의 배열
- * @parm {selectedCartIds} - 선택된 아이템들의 ID 배열
- * @returns {number} - 선택된 아이템들의 최대 가격. 선택된 아이템이 없으면 0을 반환합니다.
+ * 주어진 장바구니 아이템들 중에서 최대 가격을 반환합니다.
+ * @param {CartItemTypes[]} selectedCartItems - 선택된 장바구니 아이템들
+ * @returns {number} - 선택된 장바구니 아이템들 중 최대 가격
  */
 export function getMaxPriceInCart({
   selectedCartItems,
