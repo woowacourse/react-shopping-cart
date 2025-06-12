9b9e2260c0ebcc9de004fc0988bdcc993850de8a
Ko Daeun <drechsu@gmail.com>
2025-06-12 11:24:36 +0900

    refactor : calculateDeliveryPrice 로직에서 let 제거

--- src/utils/calculateDeliveryPrice.ts
+++ src/utils/calculateDeliveryPrice.ts
@@ -4,14 +4,10 @@ export default function calculateDeliveryPrice(
   orderPrice: number,
   isRemoteArea = false
 ): number {
-  let price = 0;
-  if (orderPrice === 0 || orderPrice >= DELIVERY_PRICE_THRESHOLD) {
-    price = 0;
-  } else {
-    price = DELIVERY_PRICE;
-  }
-  if (isRemoteArea && price > 0) {
-    price += 3000;
-  }
-  return price;
+  const basePrice =
+    orderPrice === 0 || orderPrice >= DELIVERY_PRICE_THRESHOLD
+      ? 0
+      : DELIVERY_PRICE;
+
+  return isRemoteArea && basePrice > 0 ? basePrice + 3000 : basePrice;
 }
